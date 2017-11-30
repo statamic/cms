281e8dfd8746b9359c6fd80c3cd4c22aeac48849
Jason Varga <jason@pixelfear.com>
2017-11-30 09:57:41 -0500

    macros.yaml goes in the resources directory

--- app/API/Theme.php
+++ app/API/Theme.php
@@ -15,9 +15,9 @@ class Theme
      */
     public static function getMacro($macro)
     {
-        $path = 'settings/macros.yaml';
+        $path = base_path('resources/macros.yaml');
 
-        $macros = array_reindex(YAML::parse(File::disk('theme')->get($path)));
+        $macros = array_reindex(YAML::parse(File::get($path)));
 
         return array_get($macros, $macro);
     }
