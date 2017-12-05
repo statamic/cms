e4352fdb4c7da1aa7acd378f50dce8f26decb1bf
Jason Varga <jason@pixelfear.com>
2017-12-05 14:15:18 -0500

    Bring over elixir

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+var elixir = require('laravel-elixir');
+var gulp = require('gulp');
+
+require('laravel-elixir-vueify');
+require('laravel-elixir-svg-symbols');
+
+// Vendor paths
+var paths = {
+    'sweetalert': './node_modules/sweetalert/dist/'
+};
+
+elixir(function(mix) {
+
+    /**
+     * This is the tale / Of Captain Jack Sparrow
+     * Pirate so brave / On the Seven Seas
+     * A mystical quest / To the Isle of Tortuga
+     * Raven locks sway / On the ocean's breeze
+     */
+    mix.scripts([
+        "./node_modules/jquery/dist/jquery.js",
+        "./node_modules/bootstrap/dist/js/bootstrap.js",
+        "./node_modules/underscore/underscore.js",
+        "./node_modules/vue/dist/vue.js",
+        "./node_modules/vue-resource/dist/vue-resource.js",
+        "./node_modules/moment/moment.js",
+        "./node_modules/jquery-serializejson/jquery.serializejson.js",
+        "./node_modules/autosize/dist/autosize.js",
+        "./node_modules/trix/dist/trix.js",
+        "./node_modules/slug/slug.js",
+        "./node_modules/tippy.js/dist/tippy.min.js",
+        "./node_modules/luminous-lightbox/dist/luminous.js",
+        "app/statamic/resources/js/vendor/dmuploader.js",
+        "app/statamic/resources/js/vendor/calendar.js",
+        "app/statamic/resources/js/vendor/redactor.js",
+        "app/statamic/resources/js/vendor/redactor/assets.js",
+        "app/statamic/resources/js/vendor/selectize.js",
+        "app/statamic/resources/js/vendor/jquery-ui.js",
+        "app/statamic/resources/js/vendor/nested-sortable.js",
+    ], 'app/statamic/resources/dist/js/johnny-deps.js');
+
+    mix.browserify('cp.js', 'app/statamic/resources/dist/js/cp.js');
+    mix.browserify('jabbascripts.js', 'app/statamic/resources/dist/js/jabbascripts.js');
+    mix.browserify('app.js', 'app/statamic/resources/dist/js/app.js');
+
+    // Build css file
+    mix.sass('cp.scss', 'app/statamic/resources/dist/css/cp.css', {
+        includePaths: [
+            paths.sweetalert
+        ]
+    });
+
+    mix.svgSprite('app/statamic/resources/svg/', 'app/statamic/resources/dist/svg/');
+
+    // Copy assets into a location ready for distribution
+    mix.copy('app/statamic/resources/img/**/*.*', 'app/statamic/resources/dist/img/');
+    mix.copy('app/statamic/resources/svg/**/*.*', 'app/statamic/resources/dist/svg/');
+    mix.copy('app/statamic/resources/audio/**/*.*', 'app/statamic/resources/dist/audio/');
+    mix.copy('app/statamic/resources/fonts/**/*.*', 'app/statamic/resources/dist/fonts/');
+});
