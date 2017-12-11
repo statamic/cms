57d7ee06c9538f39f65155569b8698a3d4b34a84
Jason Varga <jason@pixelfear.com>
2017-12-11 16:07:05 -0500

    Add more to the manifest

--- app/Extend/Management/Manifest.php
+++ app/Extend/Management/Manifest.php
@@ -2,6 +2,10 @@
 
 namespace Statamic\Extend\Management;
 
+use ReflectionClass;
+use Statamic\API\Arr;
+use Statamic\API\Str;
+use Statamic\API\File;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Foundation\PackageManifest;
 
@@ -19,18 +23,46 @@ class Manifest extends PackageManifest
             return array_get($package, 'type') === 'statamic-addon';
         })->keyBy('name')->map(function ($package) {
             return $this->formatPackage($package);
-        })->all());
+        })->filter()->all());
 
         $this->getManifest();
     }
 
     protected function formatPackage($package)
     {
-        $provider = $package['extra']['laravel']['providers'][0];
-        $namespace = join('\\', explode('\\', $provider, -1));
+        if (! $provider = $package['extra']['laravel']['providers'][0] ?? null) {
+            return;
+        }
+
+        $reflector = new ReflectionClass($provider);
+        $providerParts = explode('\\', $provider, -1);
+        $namespace = join('\\', $providerParts);
+
+        $autoload = $package['autoload']['psr-4'][$namespace.'\\'];
+        $directory = Str::removeRight(dirname($reflector->getFileName()), $autoload);
+
+        $json = json_decode(File::get($directory.'/composer.json'), true);
+        $statamic = $json['extra']['statamic'] ?? [];
+        $author = $json['authors'][0] ?? null;
 
         return [
+            'id' => $id = Arr::last($providerParts),
+            'package' => $package['name'],
+            'name' => $statamic['name'] ?? $id,
+            'description' => $statamic['description'] ?? $package['description'] ?? null,
             'namespace' => $namespace,
+            'directory' => $directory,
+            'autoload' => $autoload,
+            'url' => $statamic['url'] ?? null,
+            'developer' => $statamic['developer'] ?? $author['name'] ?? null,
+            'developerUrl' => $statamic['developer-url'] ?? $author['homepage'] ?? null,
+            'email' => $package['support']['email'] ?? null,
+            'version' => $package['version'],
         ];
+    }
+
+    public function addons()
+    {
+        return collect($this->getManifest());
     }
 }
