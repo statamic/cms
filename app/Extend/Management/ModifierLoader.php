5b76e54410cc50f124799b544743a5e9c8f8339a
Jason Varga <jason@pixelfear.com>
2017-12-11 16:07:51 -0500

    Modifier loader uses the container bindings

--- app/Extend/Management/ModifierLoader.php
+++ app/Extend/Management/ModifierLoader.php
@@ -7,31 +7,21 @@ use Statamic\Exceptions\ResourceNotFoundException;
 
 class ModifierLoader
 {
-    public function load($modifier)
+    /**
+     * Loads a modifier.
+     *
+     * @return array  The class, and the method to be called.
+     */
+    public function load($name)
     {
-        // An addon may contain multiple modifiers. You may specify a "secondary" modifier by delimiting with a dot.
-        // For example, "{{ var | bacon.bits }}" would reference the "BitsModifier" in the "Bacon" addon.
-        if (Str::contains($modifier, '.')) {
-            list($addon, $name) = explode('.', $modifier);
-        } else {
-            $addon = $name = $modifier;
+        if (! ($modifiers = app('statamic.modifiers'))->has($name)) {
+            throw new ResourceNotFoundException("Could not find files to load the `{$name}` modifier.");
         }
 
-        $name = Str::studly($name);
-        $addon = Str::studly($addon);
-        $root = "Statamic\\Addons\\$addon";
-
-        // Modifiers may be stored in the root of the addon directory, named using YourAddonModifier.php or
-        // secondary ones may be named SecondaryModifier.php. Classes in the root will take precedence.
-        if (class_exists($rootClass = "{$root}\\{$name}Modifier")) {
-            return app($rootClass);
+        if (str_contains($class = $modifiers->get($name), 'BaseModifiers@')) {
+            list($class, $method) = explode('@', $class);
         }
 
-        // Alternatively, modifiers may be placed in a "Modifiers" namespace.
-        if (class_exists($namespacedClass = "{$root}\\Modifiers\\{$name}Modifier")) {
-            return app($namespacedClass);
-        }
-
-        throw new ResourceNotFoundException("Could not find files to load the `{$modifier}` modifier.");
+        return [app($class), $method ?? 'index'];
     }
 }
