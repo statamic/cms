eb66603fac5a2e16e357015d4c7e4d326108a707
Jason Varga <jason@pixelfear.com>
2018-01-25 09:27:51 -0500

    Our provider no longer loads the framework providers

--- app/Providers/StatamicServiceProvider.php
+++ app/Providers/StatamicServiceProvider.php
@@ -13,28 +13,6 @@ class StatamicServiceProvider extends AggregateServiceProvider
      * @var array
      */
     protected $providers = [
-        \Illuminate\Auth\AuthServiceProvider::class,
-        \Illuminate\Broadcasting\BroadcastServiceProvider::class,
-        \Illuminate\Bus\BusServiceProvider::class,
-        \Illuminate\Cache\CacheServiceProvider::class,
-        \Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
-        \Illuminate\Cookie\CookieServiceProvider::class,
-        \Illuminate\Database\DatabaseServiceProvider::class,
-        \Illuminate\Encryption\EncryptionServiceProvider::class,
-        \Illuminate\Filesystem\FilesystemServiceProvider::class,
-        \Illuminate\Foundation\Providers\FoundationServiceProvider::class,
-        \Illuminate\Hashing\HashServiceProvider::class,
-        \Illuminate\Mail\MailServiceProvider::class,
-        \Illuminate\Notifications\NotificationServiceProvider::class,
-        \Illuminate\Pagination\PaginationServiceProvider::class,
-        \Illuminate\Pipeline\PipelineServiceProvider::class,
-        \Illuminate\Queue\QueueServiceProvider::class,
-        \Illuminate\Redis\RedisServiceProvider::class,
-        \Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
-        \Illuminate\Session\SessionServiceProvider::class,
-        \Illuminate\Translation\TranslationServiceProvider::class,
-        \Illuminate\Validation\ValidationServiceProvider::class,
-
         ViewServiceProvider::class,
         AppServiceProvider::class,
         CollectionsServiceProvider::class,
