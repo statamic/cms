c3f92ed5e229ad95e1fe05f9d2a40555b23bdd07
Jason Varga <jason@pixelfear.com>
2018-02-06 16:23:33 -0500

    Fake views testing trait

--- tests/FakesViews.php
+++ tests/FakesViews.php
@@ -2,25 +2,35 @@
 
 namespace Tests;
 
-use Mockery;
 use Illuminate\View\View;
 use Illuminate\View\Factory;
-use Illuminate\Contracts\View\Engine;
 
 trait FakesViews
 {
-    protected $mockViewEngine;
-
-    protected function withFakeViews()
+    public function withFakeViews()
     {
-        $this->mockViewEngine = Mockery::mock(Engine::class);
-        $this->app->instance('engine-fake', $this->mockViewEngine);
+        $this->fakeView = app(FakeViewEngine::class);
+        $this->app->instance('FakeViewEngine', $this->fakeView);
 
         $this->app->bind('view', function ($app) {
             return new FakeViewFactory($app['view.engine.resolver'], $app['view.finder'], $app['events']);
         });
+    }
 
-        return $this;
+    public function withStandardFakeViews()
+    {
+        $this->withFakeViews();
+
+        $this->fakeView->rawContents('layout', '{{ template_content }}');
+        $this->fakeView->rawContents('default', '{{ content }}');
+    }
+
+    public function withStandardFakeErrorViews()
+    {
+        $this->withFakeViews();
+
+        $this->fakeView->rawContents('errors.layout', '{{ template_contents }}');
+        $this->fakeView->rawContents('errors.404', 'The 404 template.');
     }
 }
 
@@ -28,6 +38,40 @@ class FakeViewFactory extends Factory
 {
     public function make($view, $data = [], $mergeData = [])
     {
-        return new View($this, app('engine-fake'), $view, $view, $data);
+        return new View($this, app('FakeViewEngine'), $view, $view, $data);
+    }
+}
+
+class FakeViewEngine extends \Statamic\View\Antlers\Engine
+{
+    protected $rawContents = [];
+    protected $renderedContents = [];
+
+    public function rawContents($view, $contents)
+    {
+        $this->rawContents[$view] = $contents;
+    }
+
+    public function renderedContents($view, $contents)
+    {
+        $this->renderedContents[$view] = $contents;
+    }
+
+    public function get($path, array $data = [])
+    {
+        if (isset($this->renderedContents[$path])) {
+            return $this->renderedContents[$path];
+        }
+
+        return parent::get($path, $data);
+    }
+
+    protected function getContents($path)
+    {
+        if (isset($this->rawContents[$path])) {
+            return $this->rawContents[$path];
+        }
+
+        return parent::getContents($path);
     }
 }
