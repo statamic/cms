cc70651bef15c68172f3c2e7072dc1abccc9ba43
Jason Varga <jason@pixelfear.com>
2018-02-08 16:57:14 -0500

    Site name uses site name

--- config/sites.php
+++ config/sites.php
@@ -29,7 +29,7 @@ return [
     'sites' => [
 
         'default' => [
-            'name' => 'Statamic',
+            'name' => config('app.name'),
             'locale' => 'en_US',
             'url' => '/',
         ]
