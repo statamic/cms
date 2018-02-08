0dcba41c83dc43cab8d473cf9737f9639be52312
Jason Varga <jason@pixelfear.com>
2018-02-08 16:57:05 -0500

    fix tpyo

--- config/sites.php
+++ config/sites.php
@@ -9,7 +9,7 @@ return [
     |
     | The default site will hold the majority of the content on your website.
     | Additional sites with unlocalized content will fall back to the data
-    | defined in the default. You should specific a site handle below.
+    | defined in the default. You should specify a site handle below.
     |
     */
 
