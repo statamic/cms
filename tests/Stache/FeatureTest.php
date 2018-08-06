0f57ea7ed8a6945d9aaa6c82427dad79e39186fc
Jason Varga <jason@pixelfear.com>
2018-08-06 14:52:05 -0400

    Use real API and Stache

--- tests/Stache/FeatureTest.php
+++ tests/Stache/FeatureTest.php
@@ -3,10 +3,11 @@
 namespace Tests\Stache;
 
 use Tests\TestCase;
+use Statamic\API\Entry;
 use Statamic\Stache\Stache;
+use Statamic\API\Collection;
 use Statamic\Stache\Fakes\YAML;
 use Illuminate\Support\Facades\Cache;
-use Facades\Statamic\Stache\API\Entry;
 use Statamic\Stache\Stores\BasicStore;
 use Statamic\Stache\Stores\EntriesStore;
 use Statamic\Stache\Stores\AggregateStore;
@@ -17,51 +18,26 @@ class FeatureTest extends TestCase
     public function setUp()
     {
         parent::setUp();
-        $this->stache = (new Stache)->sites(['en', 'es']);
-        $this->app->instance(Stache::class, $this->stache);
+
+        $this->stache = $this->app->make('stache');
+        $this->stache->store('collections')->directory(__DIR__.'/__fixtures__/content/collections');
+        $this->stache->store('entries')->directory(__DIR__.'/__fixtures__/content/collections');
+        $this->stache->boot();
     }
 
     /** @test */
     function it_gets_all_collections()
     {
-        $this->stache->registerStore(
-            (new CollectionsStore($this->stache))
-                ->directory(__DIR__.'/__fixtures__/content/collections/')
-        )->boot();
-
-        $this->assertEquals(4, $this->allCollections()->count());
+        $this->assertEquals(4, Collection::all()->count());
     }
 
     /** @test */
     function it_gets_all_entries()
     {
-        $this->stache->registerStore(
-            (new EntriesStore($this->stache))
-                ->directory(__DIR__.'/__fixtures__/content/collections/')
-        )->boot();
-
-        $this->assertEquals(14, $this->allEntries()->count());
-        $this->assertEquals(3, $this->entryWhereCollection('alphabetical')->count());
-        $this->assertEquals(2, $this->entryWhereCollection('blog')->count());
-        $this->assertEquals(3, $this->entryWhereCollection('numeric')->count());
-        $this->assertEquals(6, $this->entryWhereCollection('pages')->count());
-    }
-
-    private function allCollections()
-    {
-        // A fake implementation of Statamic\API\Collection::all();
-        return $this->stache->store('collections')->getItems();
-    }
-
-    private function allEntries()
-    {
-        // A fake implementation of Statamic\API\Entry::all();
-        return $this->stache->store('entries')->getItemsWithoutLoading()->flatMap->all();
-    }
-
-    protected function entryWhereCollection($collection)
-    {
-        // A fake implementation of Statamic\API\Entry::whereCollection();
-        return $this->stache->store('entries')->store($collection)->getItemsWithoutLoading();
+        $this->assertEquals(14, Entry::all()->count());
+        $this->assertEquals(3, Entry::whereCollection('alphabetical')->count());
+        $this->assertEquals(2, Entry::whereCollection('blog')->count());
+        $this->assertEquals(3, Entry::whereCollection('numeric')->count());
+        $this->assertEquals(6, Entry::whereCollection('pages')->count());
     }
 }
