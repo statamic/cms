be266ed6c714ecb95e7c83a0b8a24d14834b30e3
Jack McDade <jack@jackmcdade.com>
2018-08-13 09:02:45 -0400

    svgs: optimize and inline styles with SVGO to enable tailwind’s stroke-current approach to coloring.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,16 +1,15 @@
 @extends('statamic::layout')
-@section('content-class', 'dashboard')
 
 @section('content')
     <div class="flexy mb-3">
         <h1 class="fill">{{ __('Dashboard') }}</h1>
     </div>
 
-    <div class="widgets">
+    {{-- <div class="widgets">
         @foreach($widgets as $widget)
             <div class="widget {{ array_get($widget, 'width', 'half')}}">
                 {!! $widget['html'] !!}
             </div>
         @endforeach
-    </div>
+    </div> --}}
 @stop
--- resources/views/partials/nav-main-new.blade.php
+++ resources/views/partials/nav-main-new.blade.php
@@ -1,76 +1,84 @@
 @section('nav-main')
-    <nav class="nav-main new pl-3">
-        <ul class="mt-0 mb-3 antialiased">
-            <li class="p-0 mb-1">
+    <nav class="nav-main new antialiased pl-3">
+        <ul>
+            <li class="current">
                 <a href="{{ route('dashboard') }}">
                     <i>@svg('new/pie-line-graph-desktop')</i><span>Dashboard</span>
                 </a>
             </li>
         </ul>
         <h6 class="mb-1">Content</h6>
-        <ul class="mt-0 mb-3 antialiased">
-            <li class="p-0 mb-1">
+        <ul>
+            <li>
                 <a href="">
-                    <i>@svg('new/content-pencil-write')</i><span>Entries</span>
+                    <i class="">@svg('new/content-pencil-write')</i><span>Entries</span>
                 </a>
             </li>
-            <li class="p-0 mb-1">
-                <a href="" class="text-blue-darker font-medium">
-                    <i class="bg-white">@svg('new/hierarchy-files-1')</i><span>Structure</span>
+            <li>
+                <a href="" class="active">
+                    <i>@svg('new/hierarchy-files-1')</i><span>Structure</span>
                 </a>
             </li>
-            <li class="p-0 mb-1">
+            <li>
                 <a href="">
                     <i>@svg('new/tags-1')</i><span>Tags</span>
                 </a>
             </li>
-            <li class="p-0 mb-1">
+            <li>
                 <a href="">
                     <i>@svg('new/picture-polaroid-landscape')</i><span>Assets</span>
                 </a>
             </li>
-            <li class="p-0 mb-1">
+            <li>
                 <a href="">
                     <i>@svg('new/earth-1')</i><span>Globals</span>
                 </a>
             </li>
         </ul>
+        <h6 class="mb-1">Tools</h6>
+        <ul>
+            <li>
+                <a href="">
+                    <i>@svg('new/drawer-file')</i><span>Forms</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i>@svg('new/loading-bar-1')</i><span>Updates</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i>@svg('new/seo-search-graph')</i><span>SEO Pro</span>
+                </a>
+            </li>
+        </ul>
         <h6 class="mb-1">Users</h6>
-        <ul class="mt-0 mb-3 antialiased">
-            <li class="p-0 mb-1">
+        <ul>
+            <li>
                 <a href="">
                     <i>@svg('new/single-neutral-folder-box')</i><span>Users</span>
                 </a>
             </li>
-            <li class="p-0 mb-1">
+            <li>
                 <a href="">
                     <i>@svg('new/multiple-users-2')</i><span>Groups</span>
                 </a>
             </li>
-            <li class="p-0 mb-1">
+            <li>
                 <a href="">
                     <i>@svg('new/shield-key')</i><span>Permissions</span>
                 </a>
             </li>
         </ul>
-        <h6 class="mb-1">Tools</h6>
-        <ul class="mt-0 mb-3 antialiased">
-            <li class="p-0 mb-1">
+        <h6 class="mb-1">Site</h6>
+        <ul>
+            <li>
                 <a href="">
-                    <i>@svg('new/drawer-file')</i><span>Forms</span>
+                    <i>@svg('new/module-three-2')</i><span>Addons</span>
                 </a>
             </li>
-            <li class="p-0 mb-1">
-                <a href="">
-                    <i>@svg('new/loading-bar-1')</i><span>Updates</span>
-                </a>
-            </li>
-            <li class="p-0 mb-1">
-                <a href="">
-                    <i>@svg('new/seo-search-graph')</i><span>SEO Pro</span>
-                </a>
-            </li>
-            <li class="p-0 mb-1">
+            <li>
                 <a href="">
                     <i>@svg('new/hammer-wench')</i><span>Preferences</span>
                 </a>
