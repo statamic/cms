0e2578bdf3620cafa08327e7c130f765c9c573df
Jack McDade <jack@jackmcdade.com>
2018-08-28 10:01:11 -0400

    hello proper breadcrumb

--- resources/views/collections/show.blade.php
+++ resources/views/collections/show.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     <div class="flex mb-3">
         <h1 class="flex-1">
-            {{ __('Collections') }}
-            <svg width="24" height="24" viewBox="0 0 24 24" class="align-middle"><path d="M10.414 7.05l4.95 4.95-4.95 4.95L9 15.534 12.536 12 9 8.464z" fill="currentColor" fill-rule="evenodd"></path></svg>
+            <a href="{{ cp_route('collections.index')}}">{{ __('Collections') }}</a>
+            @svg('new/chevron-right')
             {{ $collection->title() }}
         </h1>
         <a href="{{ cp_route('collections.entries.create', $collection->path()) }}" class="btn btn-primary">{{ __('Create Entry') }}</a>
