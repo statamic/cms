c7800cb9a2c30f7541be171e120cc2737e9d7585
Jack McDade <jack@jackmcdade.com>
2018-09-07 13:08:33 -0400

    oops hello slugFieldtype!

---
+++ resources/js/components/fieldtypes/SlugFieldtype.vue
@@ -0,0 +1,20 @@
+<template>
+
+    <text-fieldtype
+        class="font-mono text-xs"
+        name="slug"
+        :config="config"
+        :value="value"
+        @updated="update" />
+
+</template>
+
+<script>
+import Fieldtype from './Fieldtype.vue';
+
+export default {
+
+    mixins: [Fieldtype]
+
+}
+</script>
