94a96eb68f88b4d8b32f995e4e5be95efd89096b
Jason Varga <jason@pixelfear.com>
2018-09-21 12:34:07 -0400

    Integer fieldtype should also cast to integer when preprocessing

--- bundles/Integer/IntegerFieldtype.php
+++ bundles/Integer/IntegerFieldtype.php
@@ -6,6 +6,11 @@ use Statamic\Fields\Fieldtype;
 
 class IntegerFieldtype extends Fieldtype
 {
+    public function preProcess($data)
+    {
+        return (int) $data;
+    }
+
     public function process($data)
     {
         if ($data === null || $data === '') {
