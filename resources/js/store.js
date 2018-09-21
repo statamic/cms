87ab52cf3c2f128bc5002edad58cf7464ff5d1eb
Jason Varga <jason@pixelfear.com>
2018-09-21 12:43:05 -0400

    mixin to get fieldtypes one time and put them in vuex

--- resources/js/store.js
+++ resources/js/store.js
@@ -3,13 +3,18 @@ export default {
     namespaced: true,
 
     state: {
-        windowWidth: null
+        windowWidth: null,
+        fieldtypes: null,
     },
 
     mutations: {
 
         windowWidth(state, width) {
             state.windowWidth = width;
+        },
+
+        fieldtypes(state, fieldtypes) {
+            state.fieldtypes = fieldtypes;
         }
 
     }
