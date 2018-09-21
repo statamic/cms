4571655c2c83dfceb06a984d4695fc71bc924a8c
Jason Varga <jason@pixelfear.com>
2018-09-21 12:27:37 -0400

    Move Grid fieldtype (and nested fields) and add tests

---
+++ app/Fields/Fieldtypes/Grid.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Statamic\Fields\Fieldtypes;
+
+use Statamic\API\Helper;
+use Statamic\Fields\Fields;
+use Statamic\Fields\Fieldset;
+use Statamic\Fields\Fieldtype;
+use Statamic\Fields\Validation;
+use Statamic\CP\FieldtypeFactory;
+
+class Grid extends Fieldtype
+{
+    protected $defaultable = false;
+
+    protected $configFields = [
+        'mode' => [
+            'type' => 'select',
+            'default' => 'table',
+            'options' => ['table' => 'Table', 'stacked' => 'Stacked'],
+        ],
+        'max_rows' => ['type' => 'integer'],
+        'min_rows' => ['type' => 'integer'],
+        'add_row' => ['type' => 'text'],
+        'fields' => ['type' => 'fields'],
+    ];
+
+    public function process($data)
+    {
+        return collect($data)->map(function ($row) {
+            return $this->processRow($row);
+        })->all();
+    }
+
+    private function processRow($row)
+    {
+        $row = array_except($row, '_id');
+
+        $fields = $this->fields()->addValues($row)->process()->values();
+
+        return array_merge($row, $fields);
+    }
+
+    public function preProcess($data)
+    {
+        return collect($data)->map(function ($row) {
+            return $this->preProcessRow($row);
+        })->all();
+    }
+
+    private function preProcessRow($row)
+    {
+        $fields = $this->fields()->addValues($row)->preProcess()->values();
+
+        return array_merge($row, $fields);
+    }
+
+    private function fields()
+    {
+        return new Fields(collect($this->config('fields'))->map(function ($field) {
+            $handle = array_pull($field, 'handle');
+            return compact('handle', 'field');
+        }));
+    }
+
+    public function rules(): array
+    {
+        $rules = ['array'];
+
+        if ($min = $this->config('min_rows')) {
+            $rules[] = 'min:' . $min;
+        }
+
+        if ($max = $this->config('max_rows')) {
+            $rules[] = 'max:' . $min;
+        }
+
+        return $rules;
+    }
+
+    // public function extraRules(): array
+    // {
+    //     // TODO
+    //     $fieldset = (new Fieldset)->contents(['fields' => $this->config('fields')]);
+    //     $rules = (new Validation)->fieldset($fieldset)->rules();
+
+    //     return collect($rules)->mapWithKeys(function ($rules, $handle) {
+    //         return ["{$this->getName()}.*.{$handle}" => $rules];
+    //     })->all();
+    // }
+}
