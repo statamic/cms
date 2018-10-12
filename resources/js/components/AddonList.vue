00c32b02d4ae6817c4c1778fca297c386635643b
Jesse Leite <jesseleite@gmail.com>
2018-10-12 16:35:55 -0400

    Adapt to new API.

--- resources/js/components/AddonList.vue
+++ resources/js/components/AddonList.vue
@@ -14,7 +14,7 @@
                     <div class="h-64 rounded-t bg-cover" :style="'background-image: url(\''+getCover(addon)+'\')'"></div>
                     <div class="px-3 mb-2 relative text-center">
                         <a :href="addon.seller.website" class="relative">
-                            <img :src="this.domain+'/images/storage/'+addon.seller.avatar" :alt="addon.seller.name" class="rounded-full h-14 w-14 z-30 bg-white relative -mt-4 border-2 border-white">
+                            <img :src="addon.seller.avatar" :alt="addon.seller.name" class="rounded-full h-14 w-14 z-30 bg-white relative -mt-4 border-2 border-white">
                         </a>
                         <div class="addon-card-title mb-2 text-lg font-bold text-center">{{ addon.name }}</div>
                         <p v-text="addon.variants[0].summary" class="text-sm"></p>
@@ -85,13 +85,13 @@ export default {
     methods: {
         getAddons() {
             this.axios.get(this.api + this.endpoints.addons).then(response => {
-                this.rows = response.data;
+                this.rows = response.data.data;
                 this.loaded = true;
             });
         },
 
         getCover(addon) {
-            return (addon.variants[0].assets.length) ? this.domain + '/images/storage/' + addon.variants[0].assets[0].path : '';
+            return (addon.variants[0].assets.length) ? addon.variants[0].assets[0].url : '';
         },
 
         showAddon(addon) {
