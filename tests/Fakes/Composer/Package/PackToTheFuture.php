198a1d2724afe3b18e0285f94c5f235248a8c83b
Jesse Leite <jesseleite@gmail.com>
2018-10-15 14:16:02 -0400

    Allow PackToTheFuture to fake multiple addon packages.

--- tests/Fakes/Composer/Package/PackToTheFuture.php
+++ tests/Fakes/Composer/Package/PackToTheFuture.php
@@ -6,15 +6,28 @@ use Illuminate\Support\Facades\File;
 
 class PackToTheFuture
 {
+    const DEFAULT_TEST_PACKAGE = 'test/package';
+
     /**
-     * Set version on our test package.
+     * Set version on default test package.
      *
      * @param string $version
      */
     public static function setVersion(string $version)
     {
+        static::setPackage(static::DEFAULT_TEST_PACKAGE, $version);
+    }
+
+    /**
+     * Set custom package name and version on our test package.
+     *
+     * @param string $package
+     * @param string $version
+     */
+    public static function setPackage(string $package, string $version)
+    {
         File::put(__DIR__.'/test-package/composer.json', json_encode([
-            'name' => 'test/package',
+            'name' => $package,
             'version' => $version,
         ], JSON_UNESCAPED_SLASHES));
     }
