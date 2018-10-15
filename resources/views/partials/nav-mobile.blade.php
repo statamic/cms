5a407e1d553efa49e3e9c3aae5487f10da88e249
Jack McDade <jack@jackmcdade.com>
2018-10-15 17:28:29 -0400

    correct references to old svg names

--- resources/views/partials/nav-mobile.blade.php
+++ resources/views/partials/nav-mobile.blade.php
@@ -1,17 +1,17 @@
 <nav class="nav-mobile">
     <a href="">
-        <i>@svg('new/pie-line-graph-desktop')</i><span>Dashboard</span>
+        <i>@svg('charts')</i><span>Dashboard</span>
     </a>
     <a href="">
-        <i class="">@svg('new/content-pencil-write')</i><span>Content</span>
+        <i class="">@svg('content-writing')</i><span>Content</span>
     </a>
     <a href="">
-        <i>@svg('new/multiple-users-2')</i><span>Users</span>
+        <i>@svg('users-multiple')</i><span>Users</span>
     </a>
     <a href="">
-        <i>@svg('new/drawer-file')</i><span>Tools</span>
+        <i>@svg('drawer-file')</i><span>Tools</span>
     </a>
     <a href="">
-        <i>@svg('new/hammer-wench')</i><span>Site</span>
+        <i>@svg('hammer-wench')</i><span>Site</span>
     </a>
 </nav>
