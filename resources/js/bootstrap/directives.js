29170c61b1e292e3b9f3b95813511300c04ea6ac
Jason Varga <jason@pixelfear.com>
2018-11-14 13:21:13 -0500

    Change focus directive to the example used in the vue docs

--- resources/js/bootstrap/directives.js
+++ resources/js/bootstrap/directives.js
@@ -6,14 +6,8 @@ import Elastic from '../directives/elastic';
 Vue.directive('elastic', Elastic);
 Vue.directive('tip', Tip);
 
-Vue.directive('focus', function (focusable) {
-    if (! focusable) {
-        return;
+Vue.directive('focus', {
+    inserted: function (el) {
+        el.focus();
     }
-
-    if ($('[autofocus]').length > 0 && ! $(this.el).within('.form-group').length) {
-      return;
-    }
-
-    this.vm.$nextTick(() => this.el.focus());
 })
