81b86b280dbcaf2f92ed0ffc584c00c391b38c60
Jason Varga <jason@pixelfear.com>
2018-11-15 10:30:46 -0500

    Convert Form submission email to a Mailable

---
+++ app/Forms/Email.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace Statamic\Forms;
+
+use Statamic\API\Parse;
+use Statamic\API\Config;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class Email extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    protected $submission;
+    protected $config;
+
+    public function __construct(Submission $submission, array $config)
+    {
+        $this->submission = $submission;
+        $this->config = $this->parseConfig($config);
+    }
+
+    public function build()
+    {
+        $this
+            ->subject($this->config['subject'] ?? __('Form Submission'))
+            ->buildAddresses()
+            ->buildView();
+    }
+
+    protected function buildAddresses()
+    {
+        $this->to($this->addresses(array_get($this->config, 'to')));
+
+        if ($from = array_get($this->config, 'from')) {
+            $this->from($this->addresses($from));
+        }
+
+        if ($replyTo = array_get($this->config, 'reply_to')) {
+            $this->replyTo($this->addresses($replyTo));
+        }
+
+        if ($cc = array_get($this->config, 'cc')) {
+            $this->cc($this->addresses($cc));
+        }
+
+        if ($bcc = array_get($this->config, 'bcc')) {
+            $this->bcc($this->addresses($bcc));
+        }
+
+        return $this;
+    }
+
+    protected function buildView()
+    {
+        $html = array_get($this->config, 'html');
+        $text = array_get($this->config, 'text');
+        $markdown = array_get($this->config, 'markdown');
+
+        if (!$text && !$html) {
+            return $this->automagic();
+        }
+
+        if ($text) {
+            $this->text($text);
+        }
+
+        if (array_get($this->config, 'markdown')) {
+            $this->markdown($html);
+        } else {
+            $this->view($html);
+        }
+
+        return $this->addData();
+    }
+
+    protected function addData()
+    {
+        $data = array_merge($this->submission->toArray(), [
+            'site_url'   => Config::getSiteUrl(),
+            'date'       => now(),
+            'now'        => now(),
+            'today'      => now(),
+            'site'     => site_handle(),
+            'locale'     => site_locale()
+        ]);
+
+        return $this->with($data);
+    }
+
+    protected function automagic()
+    {
+        $html = collect($this->submission->toArray())->map(function ($value, $key) {
+            $value = is_array($value) ? json_encode($value) : $value;
+            return "<b>{$key}:</b> {$value}";
+        })->implode("<br>\n");
+
+        return $this->html($html);
+    }
+
+    protected function addresses($addresses)
+    {
+        if (! $addresses) {
+            return;
+        }
+
+        return collect(explode(',', $addresses))->map(function ($address) {
+            $name = null;
+            $email = trim($address);
+
+            if (str_contains($email, '<')) {
+                preg_match('/^(.*) \<(.*)\>$/', $email, $matches);
+                $name = $matches[1];
+                $email = $matches[2];
+            }
+
+            return [
+                'email' => $email,
+                'name' => $name
+            ];
+        })->all();
+    }
+
+    protected function parseConfig(array $config)
+    {
+        $data = $this->submission->toArray();
+
+        return collect($config)->map(function ($value) use ($data) {
+            return Parse::template(Parse::env($value), $data);
+        });
+    }
+}
--- app/Forms/Listeners/SendEmails.php
+++ app/Forms/Listeners/SendEmails.php
@@ -2,20 +2,12 @@
 
 namespace Statamic\Forms\Listeners;
 
-use Carbon\Carbon;
-use Statamic\API\Form;
-use Statamic\API\Email;
-use Statamic\API\Parse;
-use Statamic\API\Config;
+use Statamic\Forms\Email;
+use Illuminate\Support\Facades\Mail;
 use Statamic\Contracts\Forms\Submission;
 
 class SendEmails
 {
-    /**
-     * Trigger sending of emails after a form submission.
-     *
-     * @param  Submission $submission
-     */
     public function handle(Submission $submission)
     {
         $config = $submission->formset()->get('email', []);
@@ -24,84 +16,7 @@ class SendEmails
         $config = (isset($config['to'])) ? [$config] : $config;
 
         foreach ($config as $c) {
-            $this->sendEmail($submission, $c);
+            Mail::send(new Email($submission, $c));
         }
-    }
-
-    /**
-     * Send an email
-     *
-     * @param  Submission $submission
-     * @param  array      $config
-     * @return void
-     */
-    private function sendEmail(Submission $submission, array $config)
-    {
-        $email = Email::create();
-
-        $config = $this->parseConfig($config, $submission->toArray());
-
-        $email->to($config['to']);
-
-        if ($from = array_get($config, 'from')) {
-            $email->from($from);
-        }
-
-        if ($reply_to = array_get($config, 'reply_to')) {
-            $email->replyTo($reply_to);
-        }
-
-        if ($cc = array_get($config, 'cc')) {
-            $email->cc($cc);
-        }
-
-        if ($bcc = array_get($config, 'bcc')) {
-            $email->bcc($bcc);
-        }
-
-        if ($subject = array_get($config, 'subject')) {
-            $email->subject($subject);
-        }
-
-        if ($template = array_get($config, 'template')) {
-            $email->template($template)
-                ->with(array_merge($this->loadKeyVars(), $submission->toArray()));
-        } else {
-            $email->automagic()->with($submission->toArray());
-        }
-
-        $email->send();
-    }
-
-    /**
-     * Parse the config values as templates so submission values may be used within them.
-     *
-     * @param  array  $config
-     * @param  array  $data
-     * @return array
-     */
-    private function parseConfig(array $config, array $data)
-    {
-        foreach ($config as $key => &$value) {
-            $value = Parse::template(Parse::env($value), $data);
-        }
-
-        return $config;
-    }
-
-    /**
-     * Bring in a couple common global vars that might be needed in the email template
-     *
-     * @return array
-     */
-    private function loadKeyVars()
-    {
-        return [
-                'site_url'   => Config::getSiteUrl(),
-                'date'       => Carbon::now(),
-                'now'        => Carbon::now(),
-                'today'      => Carbon::now(),
-                'locale'     => site_locale()
-        ];
     }
 }
