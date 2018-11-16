32ad1a24bcfcac34091e4a01922c52b05191417a
Jesse Leite <jesseleite@gmail.com>
2018-11-16 15:07:09 -0500

    Rip the last of PackToTheFuture out of production code.

--- app/Http/Controllers/CP/AddonsController.php
+++ app/Http/Controllers/CP/AddonsController.php
@@ -8,12 +8,6 @@ use Facades\Statamic\Extend\AddonInstaller;
 
 class AddonsController extends CpController
 {
-    public function __construct()
-    {
-        // Temporarily using PackToTheFuture to fake version tags until we get this hooked up to statamic/cms.
-        require(base_path('vendor/statamic/cms/tests/Fakes/Composer/Package/PackToTheFuture.php'));
-    }
-
     public function index()
     {
         return view('statamic::addons.index', [
@@ -23,18 +17,11 @@ class AddonsController extends CpController
 
     public function install(Request $request)
     {
-        // if ($changelog = Changelog::product($request->addon)->latest()) {
-
-        // }
-        // \Tests\Fakes\Composer\Package\PackToTheFuture::setAddon($request->addon, '1.0.0'); // Temp!
-
         return AddonInstaller::install($request->addon);
     }
 
     public function uninstall(Request $request)
     {
-        \Tests\Fakes\Composer\Package\PackToTheFuture::setAddon($request->addon, '1.0.0'); // Temp!
-
         return AddonInstaller::uninstall($request->addon);
     }
 }
