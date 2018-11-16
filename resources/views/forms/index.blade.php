770db9863547b0a381057db324636f5589dce387
Jack McDade <jack@jackmcdade.com>
2018-11-16 17:07:22 -0500

    add a proper screen-center utility and json_encode the form data

--- resources/views/forms/index.blade.php
+++ resources/views/forms/index.blade.php
@@ -2,21 +2,18 @@
 
 @section('content')
 
-        @if(count($forms) == 0)
-            <div class="text-center max-w-sm mx-auto pt-8">
-                {{-- @TODO: create a proper "center of screen while ignoring nav" utility class --}}
-                <div class="md:-ml-8">
-                    @svg('empty/form')
-                    <h1 class="my-3">{{ __('Create your first Form now') }}</h1>
-                    <p class="text-grey mb-3">
-                        {{ __('Forms are used to collect information from your visitors and dispatch notifications to you and your team of new submissions') }}
-                    </p>
-                    @can('super')
-                        <a href="{{ cp_route('forms.create') }}" class="btn-primary btn-lg">{{ __('Create Form') }}</a>
-                    @endcan
-                </div>
-            </div>
-        @endif
+    @if(count($forms) == 0)
+        <div class="text-center max-w-sm mx-auto pt-8 screen-centered">
+            @svg('empty/form')
+            <h1 class="my-3">{{ __('Create your first Form now') }}</h1>
+            <p class="text-grey mb-3">
+                {{ __('Forms are used to collect information from your visitors and dispatch notifications to you and your team of new submissions') }}
+            </p>
+            @can('super')
+                <a href="{{ cp_route('forms.create') }}" class="btn-primary btn-lg">{{ __('Create Form') }}</a>
+            @endcan
+        </div>
+    @endif
 
     @if(count($forms) > 0)
         <div class="flex items-center mb-3">
@@ -26,7 +23,7 @@
             @endcan
         </div>
 
-        <form-listing :forms="{{ $forms }}"></form-listing>
+        <form-listing :forms="{{ json_encode($forms) }}"></form-listing>
     @endif
 
 @endsection
