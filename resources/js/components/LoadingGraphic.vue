386d6acc1c303c5abe852ea7a074fd6c8498e812
Jason Varga <jason@pixelfear.com>
2018-11-26 15:34:41 -0500

    nicer loader with inline option

--- resources/js/components/LoadingGraphic.vue
+++ resources/js/components/LoadingGraphic.vue
@@ -1,11 +1,32 @@
 <template>
-    <div>
-        <svg
+    <div :class="{
+        'flex items-center leading-loose': inline
+    }">
+        <svg xmlns="http://www.w3.org/2000/svg"
             :width="`${size}px`"
             :height="`${size}px`"
-            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" class="lds-dual-ring" style="background: none;"><circle cx="50" cy="50"  fill="none" stroke-linecap="round" r="40" stroke-width="10" stroke="#737f8c" stroke-dasharray="62.83185307179586 62.83185307179586" transform="rotate(107.924 50 50)"><animateTransform attributeName="transform" type="rotate" calcMode="linear" values="0 50 50;360 50 50" keyTimes="0;1" dur="1s" begin="0s" repeatCount="indefinite"></animateTransform></circle></svg>
+            viewBox="0 0 40 40"
+            stroke="#737f8c">
+            <g fill="none" fill-rule="evenodd">
+                <g transform="translate(2 2)" stroke-width="4">
+                    <circle stroke-opacity=".5" cx="18" cy="18" r="18"/>
+                    <path d="M36 18c0-9.94-8.06-18-18-18">
+                        <animateTransform
+                            attributeName="transform"
+                            type="rotate"
+                            from="0 18 18"
+                            to="360 18 18"
+                            dur="1s"
+                            repeatCount="indefinite"/>
+                    </path>
+                </g>
+            </g>
+        </svg>
 
-        <div class="mt-1">{{ text }}</div>
+        <div :class="{
+            'ml-1 text-sm text-grey': inline,
+            'mt-1': !inline
+        }">{{ text }}</div>
     </div>
 </template>
 
@@ -20,6 +41,10 @@ export default {
         size: {
             type: Number,
             default: 24
+        },
+        inline: {
+            type: Boolean,
+            default: false
         }
     }
 
