cc1e1455f05702d0977edf23a51a76472603a5c2
Jason Varga <jason@pixelfear.com>
2018-11-28 14:48:18 -0500

    Add 403 error view

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,22 +1,7 @@
 @extends('statamic::outside')
 
-@section('title')
-    <h1>{{ t('permission_denied') }}</h1>
-    <hr>
-@endsection
-
 @section('content')
-
-    <p>{{ t('permission_denied_instructions') }}</p>
-
-    <br>
-
-    <div>
-        @if (Auth::check())
-            <a class="btn btn-primary btn-block" href="{{ route('logout') }}">{{ t('logout') }}</a>
-        @else
-            <a class="btn btn-primary btn-block" href="{{ route('login') }}">{{ t('login') }}</a>
-        @endif
+    <div class="min-h-screen flex items-center justify-center">
+        <h1 class="text-3xl tracking-tight mb-5 opacity-50 text-center">{{ __('Unauthorized') }}</h1>
     </div>
-
 @endsection
