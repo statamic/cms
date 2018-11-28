53c21527f2d61bdbcf52ede5e55061edeb4eac35
Jason Varga <jason@pixelfear.com>
2018-11-28 17:15:25 -0500

    Redirect with flash

--- routes/cp.php
+++ routes/cp.php
@@ -15,7 +15,11 @@ Route::group([
 ], function () {
     Statamic::additionalCpRoutes();
 
-    Route::redirect('/', 'cp/dashboard')->name('index');
+    Route::get('/', function () {
+        session()->reflash();
+        return redirect()->route('statamic.cp.dashboard');
+    })->name('index');
+
     Route::get('dashboard', 'DashboardController@index')->name('dashboard');
 
     // Structures
