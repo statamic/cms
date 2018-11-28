398556d50875c9f74a0b7016d44f60c4a2a306ba
Jason Varga <jason@pixelfear.com>
2018-11-28 14:42:33 -0500

    Use our own error views in the CP

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,8 @@
 namespace Statamic\Exceptions;
 
 use Exception;
+use Statamic\Statamic;
+use Illuminate\Support\Facades\View;
 use App\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\Debug\Exception\FlattenException;
 
@@ -30,5 +32,19 @@ class Handler extends ExceptionHandler
         return (new SymfonyExceptionHandler($debug))->getHtml(
             FlattenException::create($e)
         );
+    }
+
+    /**
+     * The Control Panel should use its own error views.
+     */
+    protected function registerErrorViewPaths()
+    {
+        if (! Statamic::isCpRoute()) {
+            return parent::registerErrorViewPaths();
+        }
+
+        $path = View::getFinder()->getHints()['statamic'][0] . '/errors';
+
+        View::replaceNamespace('errors', $path);
     }
 }
