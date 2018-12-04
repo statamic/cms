96ffe44993ee8c501c16efbc580a8ade1526c2c7
Jesse Leite <jesseleite@gmail.com>
2018-12-04 14:32:49 -0500

    If HTML form comes back with validation errors?

--- resources/views/partials/alerts.blade.php
+++ resources/views/partials/alerts.blade.php
@@ -1,9 +1,15 @@
 <div class="flashdance" v-cloak></div>
 
-@if (session('error'))
+@if (session('error') || count($errors) > 0)
     <div class="page-wrapper">
         <div class="alert alert-danger">
-            <p>{{ session('error') }}</p>
+            @if (session('error'))
+                <p>{{ session('error') }}</p>
+            @else
+                @foreach ($errors->all() as $error)
+                    <p>{{ $error }}</p>
+                @endforeach
+            @endif
         </div>
     </div>
 @endif
