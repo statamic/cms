f0bdf78f194f61f3e347a7df283ad0e05a7637fe
Jack McDade <jack@jackmcdade.com>
2018-12-11 15:10:30 -0500

    Structures empty state

--- resources/views/structures/index.blade.php
+++ resources/views/structures/index.blade.php
@@ -2,48 +2,52 @@
 
 @section('content')
 
-    <div class="flex mb-3">
-        <h1 class="flex-1">{{ __('Structures') }}</h1>
-
-        @can('create', 'Statamic\Contracts\Data\Structures\Structure')
-            <a href="{{ cp_route('structures.create') }}" class="btn">{{ __('Create Structure') }}</a>
-        @endcan
-    </div>
-
     @if (! count($structures))
-    <div class="card">
-        <div class="no-results">
-            <div class="mx-auto w-32 h-32 p-4 border rounded-full text-grey-light">@svg('hierarchy-files')</div>
-            <h2>{{ __('Structures') }}</h2>
-            <h3>{{ __('There are no structures.') }}</h3>
+        <div class="text-center max-w-md mx-auto mt-5 screen-centered border-2 border-dashed rounded-lg px-4 py-8">
+            @svg('empty/structure')
+            <h1 class="my-3">{{ __('Create your first Structure now') }}</h1>
+            <p class="text-grey mb-3">
+                {{ __('Structures are heirarchial arrangements of your content, most often used to represent forms of site navigation.') }}
+            </p>
+            @can('create', 'Statamic\Contracts\Data\Structures\Structure')
+                <a href="{{ cp_route('structures.create') }}" class="btn-primary btn-lg">{{ __('Create Structure') }}</a>
+            @endcan
         </div>
-    </div>
-    @else
-    <div class="card flush">
-        <div class="dossier-table-wrapper">
-            <table class="dossier">
-                <tbody>
-                    @foreach($structures as $structure)
-                    <tr>
-                        <td class="cell-title first-cell flex items-center">
-                            <span class="column-label">{{ _('Title' )}}</span>
-                            <div class="stat">
-                                <i class="icon icon-documents"></i>
-                                {{ $structure->flattenedPages()->count() }}
-                            </div>
-                            <a href="{{ cp_route('structures.edit', $structure->handle()) }}">{{ $structure->title() }}</a>
+    @endif
 
-                            @can('delete', $structure)
-                                <form method="POST" action="{{ cp_route('structures.destroy', $structure->handle()) }}">
-                                    @csrf @method('delete') <button>Delete</button>
-                                </form>
-                            @endcan
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+    @if(count($structures))
+        <div class="flex mb-3">
+            <h1 class="flex-1">{{ __('Structures') }}</h1>
+
+            @can('create', 'Statamic\Contracts\Data\Structures\Structure')
+                <a href="{{ cp_route('structures.create') }}" class="btn">{{ __('Create Structure') }}</a>
+            @endcan
         </div>
-    </div>
+        <div class="card flush">
+            <div class="dossier-table-wrapper">
+                <table class="dossier">
+                    <tbody>
+                        @foreach($structures as $structure)
+                        <tr>
+                            <td class="cell-title first-cell flex items-center">
+                                <span class="column-label">{{ _('Title' )}}</span>
+                                <div class="stat">
+                                    <i class="icon icon-documents"></i>
+                                    {{ $structure->flattenedPages()->count() }}
+                                </div>
+                                <a href="{{ cp_route('structures.edit', $structure->handle()) }}">{{ $structure->title() }}</a>
+
+                                @can('delete', $structure)
+                                    <form method="POST" action="{{ cp_route('structures.destroy', $structure->handle()) }}">
+                                        @csrf @method('delete') <button>Delete</button>
+                                    </form>
+                                @endcan
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
     @endif
 @endsection
