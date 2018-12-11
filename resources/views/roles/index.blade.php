5d8e65a7465660b2045875abe3693965c7a38940
Jack McDade <jack@jackmcdade.com>
2018-12-11 11:35:54 -0500

    empty state and icon for roles

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -2,6 +2,18 @@
 
 @section('content')
 
+    @if(count($roles) == 0)
+        <div class="text-center max-w-md mx-auto mt-5 screen-centered border-2 border-dashed rounded-lg px-4 py-8">
+            @svg('empty/permission')
+            <h1 class="my-3">{{ __('Create your first Role now') }}</h1>
+            <p class="text-grey mb-3">
+                {{ __('Roles are groups of access and action permissions in the Control Panel that can be assigned to users and user groups.') }}
+            </p>
+            <a href="{{ cp_route('roles.create') }}" class="btn-primary btn-lg">{{ __('Create Role') }}</a>
+        </div>
+    @endif
+
+    @if(count($roles) > 0)
     <div class="flex mb-3">
         <h1 class="flex-1">
             {{ __('Roles & Permissions') }}
@@ -10,5 +22,6 @@
     </div>
 
     <role-listing :initial-rows="{{ json_encode($roles) }}"></role-listing>
+    @endif
 
 @endsection
