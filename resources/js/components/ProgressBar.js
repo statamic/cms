0811f8eb2d4b6a2c454654fe19baac99f96c162c
Jason Varga <jason@pixelfear.com>
2018-12-19 14:31:21 -0500

    progress bar can track multiple items and only completes when they're all done.

--- resources/js/components/ProgressBar.js
+++ resources/js/components/ProgressBar.js
@@ -1,28 +1,79 @@
 import Vue from 'vue'
 const progress = require('nprogress');
 
-class ProgressBar {
-    constructor(instance) {
-        this.instance = instance;
+const vm = new Vue({
+
+    data: {
+        progressing: false,
+        names: []
+    },
+
+    watch: {
+
+        names(names) {
+            if (names.length > 0 && !this.progressing) {
+                this.start();
+            }
+
+            if (names.length === 0 && this.progressing) {
+                this.stop();
+            }
+        }
+
+    },
+
+    methods: {
+
+        start() {
+            progress.start();
+            this.progressing = true;
+        },
+
+        stop() {
+            progress.done();
+            this.progressing = false;
+        },
+
+        add(name) {
+            if (this.names.indexOf(name) == -1) {
+                this.names.push(name);
+            }
+        },
+
+        remove(name) {
+            const i = this.names.indexOf(name);
+            this.names.splice(i, 1);
+        }
+
     }
 
-    loading(loading) {
+});
+
+class ProgressBar {
+    loading(name, loading) {
         loading ? this.start() : this.complete();
     }
-
-    start() {
-        progress.start();
+    start(name) {
+        vm.add(name);
     }
-
-    complete() {
-        progress.done();
+    complete(name) {
+        vm.remove(name);
+    }
+    names() {
+        return vm.names;
+    }
+    count() {
+        return vm.names.length;
+    }
+    isComplete() {
+        return this.count() === 0;
     }
 }
 
 Object.defineProperties(Vue.prototype, {
     $progress: {
         get() {
-            return new ProgressBar(this);
+            return new ProgressBar;
         }
     }
 });
