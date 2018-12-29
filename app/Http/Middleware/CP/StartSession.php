8067acfb8f877b35710708ecd7b3e7edfdec9ed3
Jason Varga <jason@pixelfear.com>
2018-12-29 13:50:57 -0500

    Extend StartSession middleware that tracks last activity time, and doesnt save session on ping route

---
+++ app/Http/Middleware/CP/StartSession.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Statamic\Http\Middleware\CP;
+
+use Closure;
+use Illuminate\Session\Middleware\StartSession as Middleware;
+
+class StartSession extends Middleware
+{
+    public function handle($request, Closure $next)
+    {
+        $response = parent::handle($request, $next);
+
+        session()->put('last_activity', now()->timestamp);
+
+        return $response;
+    }
+
+    public function terminate($request, $response)
+    {
+        if ($request->route()->named('statamic.cp.session.timeout')) {
+            return;
+        }
+
+        parent::terminate($request, $response);
+    }
+}
