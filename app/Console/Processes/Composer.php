7d2ec1747f250c6d5b1252cdb422a62ed75fe051
Jesse Leite <jesseleite@gmail.com>
2019-01-08 19:10:32 -0500

    Revert "Make this public so that we can new up a raw Symfony Process for CLI output."
    This reverts commit 9f7ba288be70dda0f8bd52909262826e6b448bfe.

--- app/Console/Processes/Composer.php
+++ app/Console/Processes/Composer.php
@@ -136,21 +136,6 @@ class Composer extends Process
     }
 
     /**
-     * Propare process arguments.
-     *
-     * @param array $parts
-     * @return array
-     */
-    public function prepareProcessArguments($parts)
-    {
-        return array_merge([
-            $this->phpBinary(),
-            "-d memory_limit={$this->memoryLimit}",
-            'vendor/bin/composer'
-        ], $parts);
-    }
-
-    /**
      * Run composer command.
      *
      * @param mixed $parts
@@ -173,6 +158,21 @@ class Composer extends Process
         $parts = array_merge([$command, $package], $extraParams);
 
         dispatch(new RunComposer($this->prepareProcessArguments($parts), $this->getCacheKey($package)));
+    }
+
+    /**
+     * Propare process arguments.
+     *
+     * @param array $parts
+     * @return array
+     */
+    private function prepareProcessArguments($parts)
+    {
+        return array_merge([
+            $this->phpBinary(),
+            "-d memory_limit={$this->memoryLimit}",
+            'vendor/bin/composer'
+        ], $parts);
     }
 
     /**
