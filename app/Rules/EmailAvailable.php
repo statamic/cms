686e7225301fe24fab43bddb8c335e6f9edebcf5
Jesse Leite <jesseleite@gmail.com>
2019-01-11 18:39:51 -0500

    Add email available rule for email form and generator.

---
+++ app/Rules/EmailAvailable.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Statamic\Rules;
+
+use Statamic\API\User;
+use Illuminate\Contracts\Validation\Rule;
+
+class EmailAvailable implements Rule
+{
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        return ! User::all()->first(function ($user) use ($value) {
+            return $user->email() === trim($value);
+        });
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'A user with this email already exists.';
+    }
+}
