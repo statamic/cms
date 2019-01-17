fba242833ae13961f88e6e19606372daf172c585
Jason Varga <jason@pixelfear.com>
2019-01-17 10:56:59 -0500

    extra inline publish forms into a mixin

--- resources/js/components/inputs/relationship/InlineCreateForm.vue
+++ resources/js/components/inputs/relationship/InlineCreateForm.vue
@@ -1,93 +1,28 @@
-<template>
-
-    <div class="h-full overflow-auto p-4 bg-grey-lighter h-full">
-
-        <div v-if="loading" class="absolute pin z-200 flex items-center justify-center text-center">
-            <loading-graphic />
-        </div>
-
-        <entry-publish-form
-            v-if="blueprint"
-            :action="action"
-            method="post"
-            :publish-container="publishContainer"
-            :collection-title="collection.title"
-            :collection-url="collection.url"
-            :initial-title="__('Create')"
-            :initial-fieldset="blueprint"
-            :initial-values="initialValues"
-            :initial-meta="initialMeta"
-            @saved="saved"
-        >
-            <template slot="action-buttons-right">
-                <button class="btn ml-1" v-text="__('Cancel')" @click="close" />
-            </template>
-        </entry-publish-form>
-
-    </div>
-
-</template>
-
 <script>
-import axios from 'axios';
+import InlinePublishForm from './InlinePublishForm.vue';
 
 export default {
 
+    mixins: [InlinePublishForm],
+
     data() {
         return {
-            action: null,
-            loading: true,
-            blueprint: null,
-            values: null,
-            initialValues: null,
-            initialMeta: null,
-            collection: Object
+            method: 'post',
+            action: 'store',
+            title: __('Create'),
+            itemUrl: cp_url('collections/blog/entries/create')
         }
-    },
-
-    computed: {
-
-        publishContainer() {
-            return `relate-fieldtype-inline-${this._uid}`;
-        }
-
-    },
-
-    created() {
-        this.getItem();
     },
 
     methods: {
 
-        getItem() {
-            const url = cp_url('collections/blog/entries/create');
-
-            axios.get(url).then(response => {
-                const data = response.data;
-                this.blueprint = data.blueprint;
-                this.values = this.initialValues = data.values;
-                this.initialMeta = data.meta;
-                this.action = data.actions.store;
-                this.collection = data.collection;
-                this.loading = false;
-            });
-        },
-
         saved(response) {
             this.$emit('created', response.data.entry);
             this.$nextTick(() => this.close());
-        },
-
-        close() {
-            if (this.$dirty.has(this.publishContainer)) {
-                if (! confirm('Are you sure? Unsaved changes will be lost.')) {
-                    return;
-                }
-            }
-
-            this.$emit('closed');
         }
+
     }
 
 }
 </script>
+
--- resources/js/components/inputs/relationship/InlineEditForm.vue
+++ resources/js/components/inputs/relationship/InlineEditForm.vue
@@ -1,37 +1,9 @@
-<template>
-
-    <div class="h-full overflow-auto p-4 bg-grey-lighter h-full">
-
-        <div v-if="loading" class="absolute pin z-200 flex items-center justify-center text-center">
-            <loading-graphic />
-        </div>
-
-        <entry-publish-form
-            v-if="blueprint"
-            :action="action"
-            method="patch"
-            :publish-container="publishContainer"
-            :collection-title="collection.title"
-            :collection-url="collection.url"
-            :initial-title="item.title"
-            :initial-fieldset="blueprint"
-            :initial-values="initialValues"
-            :initial-meta="initialMeta"
-            @saved="saved"
-        >
-            <template slot="action-buttons-right">
-                <button class="btn ml-1" v-text="__('Cancel')" @click="close" />
-            </template>
-        </entry-publish-form>
-
-    </div>
-
-</template>
-
 <script>
-import axios from 'axios';
+import InlinePublishForm from './InlinePublishForm.vue';
 
 export default {
+
+    mixins: [InlinePublishForm],
 
     props: {
         item: Object
@@ -39,56 +11,20 @@ export default {
 
     data() {
         return {
-            action: null,
-            loading: true,
-            blueprint: null,
-            values: null,
-            initialValues: null,
-            initialMeta: null,
-            collection: Object
+            method: 'patch',
+            action: 'update',
+            title: this.item.title,
+            itemUrl: this.item.edit_url
         }
-    },
-
-    computed: {
-
-        publishContainer() {
-            return `relate-fieldtype-inline-${this._uid}`;
-        }
-
-    },
-
-    created() {
-        this.getItem();
     },
 
     methods: {
 
-        getItem() {
-            axios.get(this.item.edit_url).then(response => {
-                const data = response.data;
-                this.blueprint = data.blueprint;
-                this.values = this.initialValues = data.values;
-                this.initialMeta = data.meta;
-                this.action = data.actions.update;
-                this.collection = data.collection;
-                this.loading = false;
-            });
-        },
-
         saved(response) {
             this.$emit('updated', response.data);
             this.$nextTick(() => this.close());
-        },
-
-        close() {
-            if (this.$dirty.has(this.publishContainer)) {
-                if (! confirm('Are you sure? Unsaved changes will be lost.')) {
-                    return;
-                }
-            }
-
-            this.$emit('closed');
         }
+
     }
 
 }
