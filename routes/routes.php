d3ab93af81455cbf4dcff1ad338229aaf0d833b0
Jesse Leite <jesseleite@gmail.com>
2019-01-22 14:58:51 -0500

    Hook up `enabled` route config.

--- routes/routes.php
+++ routes/routes.php
@@ -1,16 +1,20 @@
 <?php
 
-Route::middleware('api')
-    ->name('statamic.api.')
-    ->prefix(config('statamic.api.route'))
-    ->namespace('Statamic\Http\Controllers\API')
-    ->group(__DIR__.'/api.php');
+if (config('statamic.api.enabled')) {
+    Route::middleware('api')
+        ->name('statamic.api.')
+        ->prefix(config('statamic.api.route'))
+        ->namespace('Statamic\Http\Controllers\API')
+        ->group(__DIR__.'/api.php');
+}
 
-Route::middleware('web')
-    ->name('statamic.cp.')
-    ->prefix(config('statamic.cp.route'))
-    ->namespace('Statamic\Http\Controllers\CP')
-    ->group(__DIR__.'/cp.php');
+if (config('statamic.cp.enabled')) {
+    Route::middleware('web')
+        ->name('statamic.cp.')
+        ->prefix(config('statamic.cp.route'))
+        ->namespace('Statamic\Http\Controllers\CP')
+        ->group(__DIR__.'/cp.php');
+}
 
 Route::middleware('web')
      ->namespace('Statamic\Http\Controllers')
