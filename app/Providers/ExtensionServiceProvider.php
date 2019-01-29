31880f7e9008f5a6c491b1c666bd9e99098e5e00
Jason Varga <jason@pixelfear.com>
2019-01-29 09:15:10 -0500

    change to loop

--- app/Providers/ExtensionServiceProvider.php
+++ app/Providers/ExtensionServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Statamic\Providers;
 
+use Statamic\Filters;
 use Statamic\DataStore;
 use Statamic\Extend\Modifier;
 use Statamic\Fields\Fieldtypes;
@@ -250,8 +251,14 @@ class ExtensionServiceProvider extends ServiceProvider
     {
         $this->app->instance('statamic.filters', collect());
 
-        $this->app['statamic.filters']['site'] = \Statamic\Filters\Site::class;
-        $this->app['statamic.filters']['user_group'] = \Statamic\Filters\UserGroup::class;
+        $filters = [
+            Filters\Site::class,
+            Filters\UserGroup::class,
+        ];
+
+        foreach ($filters as $filter) {
+            $this->app['statamic.filters'][$filter::handle()] = $filter;
+        }
 
         $this->registerExtensionsInAppFolder('Filters');
     }
