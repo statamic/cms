61f448b18891f0e3daeb0ef529b6de243d610781
Jason Varga <jason@pixelfear.com>
2019-01-29 09:42:19 -0500

    dont mess with nav

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $this->messingWithNav();
+        // $this->messingWithNav();
 
         $this->swapSessionMiddleware();
 
