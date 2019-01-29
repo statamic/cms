1559051abf4443a9278d3c989e919763cde5a0a7
Jesse Leite <jesseleite@gmail.com>
2019-01-29 12:16:42 -0500

    Begin stubbing out default nav items.

---
+++ app/CP/Navigation/DefaultNav.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Statamic\CP\Navigation;
+
+use Statamic\API\Nav;
+
+class DefaultNav
+{
+    /**
+     * Make default nav items.
+     */
+    public static function make()
+    {
+        (new static)
+            ->makeContentSection()
+            ->makeToolsSection()
+            ->makeUsersSection()
+            ->makeSiteSection();
+    }
+
+    /**
+     * Make content section items.
+     *
+     * @return $this
+     */
+    protected function makeContentSection()
+    {
+        Nav::content('Collections')->route('collections.index')->icon('content-writing');
+        // Nav::content('Structure')->route('structures.index')->icon('hierarchy-files');
+        // Nav::content('Taxonomies')->route('')->icon('tags');
+        // Nav::content('')->route('assets.index')->icon('assets');
+        // Nav::content('')->route('')->icon('');
+
+        return $this;
+    }
+
+    /**
+     * Make tools section items.
+     *
+     * @return $this
+     */
+    protected function makeToolsSection()
+    {
+        // Nav::tools('Forms')->route('forms.index')->icon('drawer-file');
+        // Nav::tools('Updates')->route('')->icon('loading-bar')->view('nav.updates');
+
+        // Nav::tools('Utilities')
+        //     ->route('utilities.phpinfo')
+        //     ->icon('settings-slider')
+        //     ->children([
+
+        //     ]);
+
+        return $this;
+    }
+
+    /**
+     * Make users section items.
+     *
+     * @return $this
+     */
+    protected function makeUsersSection()
+    {
+        // Nav::users('')->route('')->icon('');
+        // Nav::users('')->route('')->icon('');
+        // Nav::users('')->route('')->icon('');
+
+        return $this;
+    }
+
+    /**
+     * Make site section items.
+     *
+     * @return $this
+     */
+    protected function makeSiteSection()
+    {
+        // Nav::site('')->route('')->icon('');
+        // Nav::site('')->route('')->icon('');
+        // Nav::site('')->route('')->icon('');
+        // Nav::site('')->route('')->icon('');
+
+        return $this;
+    }
+}
