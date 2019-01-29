886e53540a045bcbfb6ed2d7fc894bb73099fcc4
Jack McDade <jack@jackmcdade.com>
2019-01-29 11:10:49 -0500

    style Filter component

--- resources/js/components/data-list/Filters.vue
+++ resources/js/components/data-list/Filters.vue
@@ -1,10 +1,14 @@
 <template>
     <dropdown-list>
         <button class="btn btn-icon-only antialiased ml-2 dropdown-toggle relative" slot="trigger">
-            <svg-icon name="filter" class="h-4 w-4 text-current"></svg-icon>
+            <svg-icon name="filter" class="h-4 w-4 mr-1 text-current"></svg-icon>
+            <span>{{ __('Filter') }}</span>
             <div v-if="activeFilterCount" class="badge ml-1 bg-blue" v-text="activeFilterCount" />
         </button>
         <ul class="dropdown-menu">
+            <li><h6>{{ __('Filter List') }}</h6></li>
+            <li class="divider"></li>
+
             <data-list-filter
                 v-for="filter in filters"
                 :key="filter.handle"
@@ -13,15 +17,15 @@
                 @changed="filterChanged(filter.handle, $event)"
             />
 
-            <li>
-                <h6>Per Page</h6>
-                <select class="w-full" :value="perPage" @change="$emit('per-page-changed', parseInt($event.target.value))">
+            <li class="flex items-center">
+                <select class="w-auto" :value="perPage" @change="$emit('per-page-changed', parseInt($event.target.value))">
                     <option
                         v-for="value in perPageOptions"
                         :key="value"
                         :value="value"
                         v-text="value" />
                 </select>
+                <span class='ml-1 text-2xs font-medium' v-text="__('Per Page')" />
             </li>
         </ul>
     </dropdown-list>
