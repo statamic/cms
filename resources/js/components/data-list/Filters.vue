2de6831875b295ede9b3441bde14ab7087f151f5
Jack McDade <jack@jackmcdade.com>
2019-01-29 10:48:44 -0500

    replace inline svg with external

--- resources/js/components/data-list/Filters.vue
+++ resources/js/components/data-list/Filters.vue
@@ -1,7 +1,7 @@
 <template>
     <dropdown-list>
         <button class="btn btn-icon-only antialiased ml-2 dropdown-toggle relative" slot="trigger">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.8 2.1A1 1 0 0 0 21 .5H2.981a1 1 0 0 0-.8 1.6l7.808 10.491V22.5a1 1 0 0 0 1.6.8l2-1.5a1 1 0 0 0 .4-.8v-8.41z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/></svg>
+            <svg-icon name="filter" class="h-4 w-4 text-current"></svg-icon>
             <div v-if="activeFilterCount" class="badge ml-1 bg-blue" v-text="activeFilterCount" />
         </button>
         <ul class="dropdown-menu">
