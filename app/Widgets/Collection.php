8bf0e5867137e9639b79fbcb6b0d7377072a1336
Jesse Leite <jesseleite@gmail.com>
2019-02-01 17:06:40 -0500

    Move Collection widget.

--- app/Widgets/Collection.php
+++ app/Widgets/Collection.php
@@ -1,18 +1,18 @@
 <?php
 
-namespace Statamic\Addons\Collection;
+namespace Statamic\Widgets;
 
-use Statamic\API\Collection;
+use Statamic\API\Collection as CollectionAPI;
 use Statamic\API\Config;
 use Statamic\API\Content;
 use Statamic\API\User;
 use Statamic\Extend\Widget;
 
-class CollectionWidget extends Widget
+class Collection extends Widget
 {
     public function html()
     {
-        $collection = $this->get('collection');
+        $collection = $this->config('collection');
 
         if (! Collection::handleExists($collection)) {
             return "Error: Collection [$collection] doesn't exist.";
