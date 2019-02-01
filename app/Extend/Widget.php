fc150cac491dd351e6f69e1da15b0aa325cecc7f
Jesse Leite <jesseleite@gmail.com>
2019-02-01 11:18:23 -0500

    Widget needs handle()?

--- app/Extend/Widget.php
+++ app/Extend/Widget.php
@@ -2,15 +2,22 @@
 
 namespace Statamic\Extend;
 
-class Widget
+use Statamic\API\Str;
+use Statamic\Extend\HasTitleAndHandle;
+
+abstract class Widget
 {
-    /**
-     * Provides access to methods for retrieving parameters
-     */
-    use HasParameters;
+    use HasParameters, HasTitleAndHandle {
+        handle as protected traitHandle;
+    }
 
     public function setParameters($parameters)
     {
         $this->parameters = $parameters;
+    }
+
+    public static function handle()
+    {
+        return Str::removeRight(static::traitHandle(), '_widget');
     }
 }
