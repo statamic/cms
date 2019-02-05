4babb3d0a4b1c58316ccddfd00a12367fd6866f3
Jesse Leite <jesseleite@gmail.com>
2019-02-05 15:43:24 -0500

    Allow listeners to prevent saving of data by returning false.

--- app/Events/Data/DataSaving.php
+++ app/Events/Data/DataSaving.php
@@ -18,4 +18,14 @@ class DataSaving extends Event
     {
         $this->data = $data;
     }
+
+    /**
+     * Dispatch the event with the given arguments, and halt on first non-null listener response.
+     *
+     * @return mixed
+     */
+    public static function dispatch()
+    {
+        return event(new static(...func_get_args()), [], true);
+    }
 }
