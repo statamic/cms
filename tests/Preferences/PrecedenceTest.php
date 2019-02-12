d43ab4dc629c0133ad4bd595a35a81d3f99b8d80
Jesse Leite <jesseleite@gmail.com>
2019-02-12 16:03:44 -0500

    Add API\Preference tests, specifically ensuring proper precedence.

---
+++ tests/Preferences/PrecedenceTest.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace Tests\API;
+
+use Tests\TestCase;
+use Tests\FakesRoles;
+use Statamic\API\Role;
+use Statamic\API\User;
+use Statamic\API\Preference;
+
+class PreferenceTest extends TestCase
+{
+    use FakesRoles;
+
+    /** @test */
+    function it_can_get_user_preferences()
+    {
+        $preferences = [
+            'site' => 'english',
+            'columns' => [
+                'collections' => [
+                    'blog' => [
+                        'title',
+                        'slug'
+                    ]
+                ]
+            ]
+        ];
+
+        $this->actingAs(User::make()->preferences($preferences));
+
+        $this->assertEquals('english', Preference::get('site'));
+        $this->assertEquals(['title', 'slug'], Preference::get('columns.collections.blog'));
+        $this->assertEquals($preferences, Preference::all());
+    }
+
+    /** @test */
+    function it_can_fallback_when_preference_doesnt_exist()
+    {
+        $this->actingAs(User::make()->makeSuper());
+
+        $this->assertEquals('saints', Preference::get('nfl.teams.favorite', 'saints'));
+    }
+
+    /** @test */
+    function it_can_get_user_role_preferences()
+    {
+        $preferences = [
+            'site' => 'english',
+            'columns' => [
+                'collections' => [
+                    'blog' => [
+                        'title',
+                        'slug'
+                    ]
+                ]
+            ]
+        ];
+
+        $this->setTestRoles(['author' => Role::make()->permissions('super')->preferences($preferences)]);
+        $this->actingAs(User::make()->assignRole('author'));
+
+        $this->assertEquals('english', Preference::get('site'));
+        $this->assertEquals(['title', 'slug'], Preference::get('columns.collections.blog'));
+        $this->assertEquals($preferences, Preference::all());
+    }
+
+    /** @test */
+    function it_gives_precedence_to_higher_roles_over_lower_roles_as_defined_on_user()
+    {
+        $this->setTestRoles([
+            'bear' => Role::make()->permissions('super')->preferences([
+                'actions' => [
+                    'eats' => 'meat',
+                    'hibernates' => true
+                ]
+            ]),
+            'rabbit' => Role::make()->permissions('super')->preferences([
+                'actions' => [
+                    'eats' => 'lettuce',
+                    'hops' => true
+                ]
+            ]),
+        ]);
+
+        $this->actingAs(User::make()->assignRole('rabbit')->assignRole('bear'));
+
+        $this->assertEquals('lettuce', Preference::get('actions.eats'));
+        $this->assertTrue(Preference::get('actions.hibernates'));
+        $this->assertTrue(Preference::get('actions.hops'));
+    }
+
+    /** @test */
+    function it_gives_precedence_to_user_preferences_over_role_preferences()
+    {
+        $this->setTestRoles([
+            'bear' => Role::make()->permissions('super')->preferences([
+                'actions' => [
+                    'eats' => 'meat',
+                    'hibernates' => true
+                ]
+            ]),
+            'rabbit' => Role::make()->permissions('super')->preferences([
+                'actions' => [
+                    'eats' => 'lettuce',
+                    'hops' => true
+                ]
+            ]),
+        ]);
+
+        $this->actingAs(User::make()->assignRole('rabbit')->assignRole('bear')->preferences([
+            'actions' => [
+                'hibernates' => false,
+            ]
+        ]));
+
+        $this->assertEquals('lettuce', Preference::get('actions.eats'));
+        $this->assertFalse(Preference::get('actions.hibernates'));
+        $this->assertTrue(Preference::get('actions.hops'));
+    }
+}
