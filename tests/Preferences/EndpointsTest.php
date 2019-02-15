492fe9466c71478cb1506c58ffa66dcf5f60f1cb
Jesse Leite <jesseleite@gmail.com>
2019-02-15 16:40:53 -0500

    Spec out ability to remove a preference array value via ajax request.

--- tests/Preferences/EndpointsTest.php
+++ tests/Preferences/EndpointsTest.php
@@ -91,4 +91,37 @@ class EndpointsTest extends TestCase
 
         $this->assertEquals($expected, Preference::all());
     }
+
+    /** @test */
+    function it_can_remove_a_preference_array_value()
+    {
+        $user = User::make()
+            ->makeSuper()
+            ->setPreferences([
+                'favorites' => [
+                    'foods' => [
+                        'pizza',
+                        'lasagna',
+                        'spaghetti'
+                    ]
+                ]
+            ])
+            ->save();
+
+        $expected = [
+            'favorites' => [
+                'foods' => [
+                    'pizza',
+                    'spaghetti'
+                ]
+            ]
+        ];
+
+        $response = $this
+            ->actingAs($user)
+            ->delete(cp_route('preferences.destroy', 'favorites.foods'), ['value' => 'lasagna'])
+            ->assertExactJson($expected);
+
+        $this->assertEquals($expected, Preference::all());
+    }
 }
