d057309fc321311cc30f66979908986b3f817c2c
Jason Varga <jason@pixelfear.com>
2019-03-06 15:10:35 -0500

    id doesnt need to be a string (eg. when using an eloquent driver, they would be integers)

--- resources/js/components/assets/Editor/Editor.vue
+++ resources/js/components/assets/Editor/Editor.vue
@@ -201,7 +201,9 @@ export default {
 
 
     props: {
-        id: String,
+        id: {
+            required: true
+        },
         allowDeleting: {
             type: Boolean,
             default() {
