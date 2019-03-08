2ee54ae17e10ac1018faed6c79c6a7c838811e66
Jesse Leite <jesseleite@gmail.com>
2019-03-08 15:47:31 -0500

    Add sortable array helpers.

--- resources/js/components/sortable/SortableHelpers.js
+++ resources/js/components/sortable/SortableHelpers.js
@@ -10,12 +10,24 @@ export default {
             return _.map(clone(obj), (value, key) => new SortableKeyValue(key, value));
         },
 
+        arrayToSortable(arr) {
+            return _.map(clone(arr), value => new SortableKeyValue(null, value));
+        },
+
         sortableToObject(sortable) {
             let obj = {};
 
             _.each(sortable, sortableKeyValue => obj[sortableKeyValue.key] = sortableKeyValue.value);
 
             return obj;
+        },
+
+        sortableToArray(sortable) {
+            let arr = [];
+
+            _.each(sortable, sortableKeyValue => arr.push(sortableKeyValue.value));
+
+            return arr;
         }
     }
 }
