7388023ba56075ae991b5cfb2a5021d322b53c8a
Jesse Leite <jesseleite@gmail.com>
2019-03-14 16:19:54 -0400

    Create partial for 'Create your first ____' welcome view.

---
+++ resources/views/partials/create-first.blade.php
@@ -0,0 +1,10 @@
+<div class="text-center max-w-md mx-auto mt-5 screen-centered border-2 border-dashed rounded-lg px-4 py-8">
+    @svg($svg)
+    <h1 class="my-3">{{ __("Create your first {$resource} now") }}</h1>
+    <p class="text-grey mb-3">
+        {{ __($description) }}
+    </p>
+    @can($can ?? 'super')
+        <a href="{{ $route }}" class="btn-primary btn-lg">{{ __("Create {$resource}") }}</a>
+    @endcan
+</div>
