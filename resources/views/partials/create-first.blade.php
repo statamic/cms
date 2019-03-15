bc0b409f6b1233dfa29ba970a60cf5505c6a1541
Jesse Leite <jesseleite@gmail.com>
2019-03-15 12:41:10 -0400

    Make description optional, and allow for optional button slot.

--- resources/views/partials/create-first.blade.php
+++ resources/views/partials/create-first.blade.php
@@ -1,12 +1,23 @@
 <div class="border-dashed border-2">
     <div class="text-center max-w-md mx-auto mt-5 rounded-lg px-4 py-8">
+
         @svg($svg)
+
         <h1 class="my-3">{{ __("Create your first {$resource} now") }}</h1>
-        <p class="text-grey mb-3">
-            {{ __($description) }}
-        </p>
+
+        @if ($description ?? false)
+            <p class="text-grey mb-3">
+                {{ __($description) }}
+            </p>
+        @endif
+
         @can($can ?? 'super')
-            <a href="{{ $route }}" class="btn-primary btn-lg">{{ __("Create {$resource}") }}</a>
+            @if($button ?? false)
+                {{ $button }}
+            @else
+                <a href="{{ $route ?? null }}" class="btn-primary btn-lg">{{ __("Create {$resource}") }}</a>
+            @endif
         @endcan
+
     </div>
 </div>
