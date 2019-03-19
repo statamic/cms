83697b353718edcfd24d743ab0475deef4559490
Jason Varga <jason@pixelfear.com>
2019-03-19 14:57:53 +0100

    Remove type hints

--- app/Contracts/Assets/AssetRepository.php
+++ app/Contracts/Assets/AssetRepository.php
@@ -2,18 +2,16 @@
 
 namespace Statamic\Contracts\Assets;
 
-use Statamic\Assets\AssetCollection;
-
 interface AssetRepository
 {
-    public function all(): AssetCollection;
-    public function whereContainer(string $container): AssetCollection;
-    public function whereFolder(string $folder, string $container): AssetCollection;
-    public function find(string $asset): ?Asset;
-    public function findByUrl(string $url): ?Asset;
-    public function findById(string $id): ?Asset;
-    public function findByPath(string $path): ?Asset;
-    public function make(): Asset;
+    public function all();
+    public function whereContainer(string $container);
+    public function whereFolder(string $folder, string $container);
+    public function find(string $asset);
+    public function findByUrl(string $url);
+    public function findById(string $id);
+    public function findByPath(string $path);
+    public function make();
     public function query();
-    public function save(Asset $asset);
+    public function save($asset);
 }
