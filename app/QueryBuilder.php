5ff4ba97ee6a0d04c8b1c1fca349f9d76c7a7148
Jason Varga <jason@pixelfear.com>
2019-03-21 16:17:32 +0100

    Add operator

--- app/QueryBuilder.php
+++ app/QueryBuilder.php
@@ -16,6 +16,7 @@ abstract class QueryBuilder
     protected $orderDirection;
     protected $operators = [
         '=' => 'Equals',
+        '<>' => 'NotEquals',
         'like' => 'Like',
     ];
 
