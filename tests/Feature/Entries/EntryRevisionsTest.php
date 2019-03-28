d635eab41414cfd104e2b0e2af7fa8ea3d020490
Jason Varga <jason@pixelfear.com>
2019-03-28 10:02:34 -0400

    Consolidate revision related tests into one class

--- tests/Feature/Entries/EntryRevisionsTest.php
+++ tests/Feature/Entries/EntryRevisionsTest.php
@@ -10,11 +10,14 @@ use Statamic\API\Entry;
 use Statamic\API\Folder;
 use Statamic\Fields\Fields;
 use Statamic\Fields\Blueprint;
+use Illuminate\Support\Carbon;
+use Statamic\Revisions\Revision;
+use Statamic\Revisions\WorkingCopy;
 use Facades\Tests\Factories\EntryFactory;
 use Tests\PreventSavingStacheItemsToDisk;
 use Facades\Statamic\Fields\BlueprintRepository;
 
-class PublishEntryTest extends TestCase
+class EntryRevisionsTest extends TestCase
 {
     use FakesRoles;
     use PreventSavingStacheItemsToDisk;
@@ -129,6 +132,50 @@ class PublishEntryTest extends TestCase
         $this->assertEquals('Test!', $revision->message());
     }
 
+    /** @test */
+    function it_restores_an_entry_to_another_revision()
+    {
+        $this->withoutExceptionHandling();
+        $this->setTestBlueprint('test', ['foo' => ['type' => 'text']]);
+        $this->setTestRoles(['test' => ['access cp', 'edit blog entries']]);
+        $user = User::make()->id('user-1')->assignRole('test')->save();
+
+        $revision = tap((new Revision)
+            ->key('collections/blog/en/123')
+            ->date(Carbon::createFromTimestamp('1553546421'))
+            ->attributes([
+                'published' => false,
+                'slug' => 'existing-slug',
+                'data' => ['foo' => 'existing foo']
+            ]))->save();
+
+        WorkingCopy::fromRevision($revision)->save();
+
+        $entry = EntryFactory::id('123')
+            ->slug('test')
+            ->collection('blog')
+            ->published(true)
+            ->data([
+                'blueprint' => 'test',
+                'title' => 'Title',
+                'foo' => 'bar',
+            ])->create();
+
+        $this->assertTrue($entry->published());
+
+        $this
+            ->actingAs($user)
+            ->restore($entry, ['revision' => '1553546421'])
+            ->assertOk()
+            ->assertSessionHas('success');
+
+        $entry = Entry::find($entry->id());
+        $this->assertEquals('existing-slug', $entry->slug());
+        $this->assertEquals(['foo' => 'existing foo'], $entry->data());
+        $this->assertFalse($entry->published());
+        $this->assertFalse($entry->hasWorkingCopy());
+    }
+
     private function publish($entry, $payload)
     {
         return $this->post($entry->publishUrl(), $payload);
@@ -137,6 +184,11 @@ class PublishEntryTest extends TestCase
     private function unpublish($entry, $payload)
     {
         return $this->delete($entry->publishUrl(), $payload);
+    }
+
+    private function restore($entry, $payload)
+    {
+        return $this->post($entry->restoreRevisionUrl(), $payload);
     }
 
     private function setTestBlueprint($handle, $fields)
