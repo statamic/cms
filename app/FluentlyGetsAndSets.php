92fc1b40942c764cf2e20143890db6765813b248
Jesse Leite <jesseleite@gmail.com>
2019-03-30 01:21:06 -0400

    Improve `fluentlyGetOrSet()`.

--- app/FluentlyGetsAndSets.php
+++ app/FluentlyGetsAndSets.php
@@ -5,48 +5,13 @@ namespace Statamic;
 trait FluentlyGetsAndSets
 {
     /**
-     * Fluently get or set property.
+     * Fluently get or set property using the FluentGetterSetter helper class.
      *
      * @param string $property
-     * @param mixed $value
-     * @param null|\Closure $additionalSetLogic
-     * @return $this
+     * @return FluentGetterSetter
      */
-    public function fluentlyGetOrSet($property, $value = null, $additionalSetLogic = null)
+    public function fluentlyGetOrSet($property)
     {
-        if (is_null($value)) {
-            return $this->{$property};
-        }
-
-        $this->{$property} = $value;
-
-        if ($additionalSetLogic) {
-            $additionalSetLogic();
-        }
-
-        return $this;
-    }
-
-    /**
-     * Fluently get or set property from func_get_args(), for when we want to allow the setting of `null`.
-     *
-     * @param string $property
-     * @param array $funcArgs
-     * @param null|\Closure $additionalSetLogic
-     * @return $this
-     */
-    public function fluentlyGetOrSetFromArgs($property, $funcArgs, $additionalSetLogic = null)
-    {
-        if (count($funcArgs) === 0) {
-            return $this->{$property};
-        }
-
-        $this->{$property} = $funcArgs[0];
-
-        if ($additionalSetLogic) {
-            $additionalSetLogic();
-        }
-
-        return $this;
+        return new FluentGetterSetter($this, $property);
     }
 }
