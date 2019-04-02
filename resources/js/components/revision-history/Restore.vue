ae9ebae70d06071facfe8a020710b05288bdb5b9
Jason Varga <jason@pixelfear.com>
2019-04-02 16:41:47 -0400

    Adjust message and remove field

--- resources/js/components/revision-history/Restore.vue
+++ resources/js/components/revision-history/Restore.vue
@@ -13,8 +13,7 @@
             @cancel="confirming = false"
         >
             <p class="mb-2">Are you sure you want to restore this revision?</p>
-            <p class="mb-3">A new revision will be created based on the state of the revision you've selected.</p>
-            <text-input v-model="revisionMessage" :placeholder="__('Notes about this revision')" @keydown.enter="restore" autofocus />
+            <p class="mb-3">Your working copy will be replaced by the contents of this revision.</p>
         </confirmation-modal>
     </div>
 
@@ -31,7 +30,6 @@ export default {
     data() {
         return {
             confirming: false,
-            revisionMessage: null
         }
     },
 
@@ -40,7 +38,6 @@ export default {
         restore() {
             const payload = {
                 revision: this.revision.date,
-                message: this.revisionMessage,
             };
 
             this.$axios.post(this.url, payload).then(response => {
