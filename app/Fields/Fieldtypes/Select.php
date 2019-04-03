16fe76dc48b2f224ad379a8e36ed9c481c3a07c8
Jesse Leite <jesseleite@gmail.com>
2019-04-03 16:07:40 -0400

    Show `Value` and `Text` in select options config.

--- app/Fields/Fieldtypes/Select.php
+++ app/Fields/Fieldtypes/Select.php
@@ -9,6 +9,10 @@ class Select extends Fieldtype
     protected $configFields = [
         'options' => [
             'type' => 'array',
+            'field' => [
+                'key_header' => 'Value',
+                'value_header' => 'Text'
+            ]
         ]
     ];
 }
