a3158f4afe516a540a8d341cdb43e4c9415365ff
Jason Varga <jason@pixelfear.com>
2019-04-09 14:04:41 -0400

    Hook up the majority of the toolbar buttons

--- resources/js/components/fieldtypes/tiptap/TiptapFieldtype.vue
+++ resources/js/components/fieldtypes/tiptap/TiptapFieldtype.vue
@@ -10,9 +10,12 @@
                 :style="`left: ${menu.left}px; bottom: ${menu.bottom}px;`"
             >
                 <button
-                    :class="{ 'active': isActive.bold() }"
-                    @click="commands.bold"
-                    v-text="'B'" />
+                    v-for="button in buttons"
+                    :key="button.name"
+                    :class="{ 'active': isActive[button.command](button.args) }"
+                    v-tooltip="button.text"
+                    @click="commands[button.command](button.args)"
+                    v-html="button.html" />
             </div>
         </editor-menu-bubble>
 
@@ -57,8 +60,23 @@
 
 <script>
 import { Editor, EditorContent, EditorFloatingMenu, EditorMenuBubble } from 'tiptap';
-import { Bold, Italic, HardBreak, Heading, History } from 'tiptap-extensions';
+import {
+    Blockquote,
+    CodeBlock,
+    HardBreak,
+    Heading,
+    OrderedList,
+    BulletList,
+    ListItem,
+    Bold,
+    Code,
+    Italic,
+    Strike,
+    Underline,
+    History
+} from 'tiptap-extensions';
 import Set from './Set';
+import { availableButtons, addButtonHtml } from '../bard/buttons';
 
 export default {
 
@@ -84,22 +102,33 @@ export default {
             allowSource: true, // todo
             showSource: false, // todo
             fullScreenMode: false, // todo
+            buttons: [],
         }
     },
 
     mounted() {
+        this.initToolbarButtons();
+
         let content = this.value.length
             ? { type: 'doc', content: this.value }
             : null;
 
         this.editor = new Editor({
             extensions: [
+                new Blockquote(),
+                new BulletList(),
+                new CodeBlock(),
                 new HardBreak(),
+                new Heading({ levels: [1, 2, 3, 4, 5, 6] }),
+                new ListItem(),
+                new OrderedList(),
                 new Bold(),
+                new Code(),
                 new Italic(),
-                new Heading({ levels: [2] }),
-                new Set(),
+                new Strike(),
+                new Underline(),
                 new History(),
+                new Set(),
             ],
             content,
             onUpdate: ({ getJSON, getHTML }) => {
@@ -138,6 +167,35 @@ export default {
 
         toggleFullscreen() {
             // todo
+        },
+
+        initToolbarButtons() {
+            const selectedButtons = this.config.buttons || [
+                'h2', 'h3', 'bold', 'italic', 'unorderedlist', 'orderedlist', 'removeformat', 'quote', 'anchor',
+            ];
+
+            // Get the configured buttons and swap them with corresponding objects
+            let buttons = selectedButtons.map(button => {
+                return _.findWhere(availableButtons(), { name: button.toLowerCase() })
+                    || button;
+            });
+
+            // Let addons add, remove, or control the position of buttons.
+            Statamic.$config.get('bard').buttons.forEach(callback => callback.call(null, buttons));
+
+            // Remove any non-objects. This would happen if you configure a button name that doesn't exist.
+            buttons = buttons.filter(button => typeof button != 'string');
+
+            // Generate fallback html for each button
+            buttons = addButtonHtml(buttons);
+
+            // Remove buttons that don't pass conditions.
+            // eg. only the insert asset button can be shown if a container has been set.
+            buttons = buttons.filter(button => {
+                return (button.condition) ? button.condition.call(null, this.config) : true;
+            });
+
+            this.buttons = buttons;
         }
 
     }
