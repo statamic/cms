3aaf99eebfd6b6e6103c17227414b91c6786df1e
Jesse Leite <jesseleite@gmail.com>
2019-04-09 17:25:45 -0400

    Stub out example laravel validation rules.

---
+++ resources/js/components/field-validation/Rules.js
@@ -0,0 +1,19 @@
+export default [
+    {
+        text: 'Required',
+        value: 'required',
+    },
+    {
+        text: 'Min',
+        value: 'min:',
+        help: 'min:value',
+        minVersion: '5.8.0'
+    },
+    {
+        text: 'Max',
+        value: 'max:',
+        help: 'max:value',
+        minVersion: '5.6.0',
+        maxVersion: '5.7.0'
+    }
+];
