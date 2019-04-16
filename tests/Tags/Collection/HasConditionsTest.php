fd3d205950f8ba0c01b493b96f95174f77f7d0bc
Jesse Leite <jesseleite@gmail.com>
2019-04-16 14:05:35 -0400

    Spec out `not` condition parameters.

--- tests/Tags/Collection/HasConditionsTest.php
+++ tests/Tags/Collection/HasConditionsTest.php
@@ -36,7 +36,30 @@ class HasConditionsTest extends TestCase
         $this->makeEntry()->set('title', 'Cat')->save();
 
         $this->assertCount(2, $this->getEntries());
+
         $this->assertCount(1, $this->getEntries(['title:is' => 'Dog']));
         $this->assertCount(1, $this->getEntries(['title:equals' => 'Dog']));
+
+        $this->assertEquals('Dog', $this->getEntries(['title:is' => 'Dog'])->first()->get('title'));
+        $this->assertEquals('Dog', $this->getEntries(['title:equals' => 'Dog'])->first()->get('title'));
+    }
+
+    /** @test */
+    function it_filters_by_not_condition()
+    {
+        $this->makeEntry()->set('title', 'Dog')->save();
+        $this->makeEntry()->set('title', 'Cat')->save();
+
+        $this->assertCount(2, $this->getEntries());
+
+        $this->assertCount(1, $this->getEntries(['title:not' => 'Dog']));
+        $this->assertCount(1, $this->getEntries(['title:isnt' => 'Dog']));
+        $this->assertCount(1, $this->getEntries(['title:aint' => 'Dog']));
+        $this->assertCount(1, $this->getEntries(['title:¯\\_(ツ)_/¯' => 'Dog']));
+
+        $this->assertEquals('Cat', $this->getEntries(['title:not' => 'Dog'])->first()->get('title'));
+        $this->assertEquals('Cat', $this->getEntries(['title:isnt' => 'Dog'])->first()->get('title'));
+        $this->assertEquals('Cat', $this->getEntries(['title:aint' => 'Dog'])->first()->get('title'));
+        $this->assertEquals('Cat', $this->getEntries(['title:¯\\_(ツ)_/¯' => 'Dog'])->first()->get('title'));
     }
 }
