ae08277e506ab9245e3d8c7231ebd1139ebd6555
Jason Varga <jason@pixelfear.com>
2019-04-18 16:17:24 -0400

    Fieldtypes emit focus and blur events. By default it just checks for the first input, but the plan is to allow a custom method.

--- resources/js/components/fieldtypes/Fieldtype.vue
+++ resources/js/components/fieldtypes/Fieldtype.vue
@@ -33,6 +33,16 @@ export default {
         isReadOnly() {
             return this.readOnly || this.config.read_only || false;
         }
+    },
+
+    mounted() {
+        // todo: check if a focus function exists and use that
+
+        let inputs = this.$el.querySelectorAll('input, textarea, select');
+        if (!inputs.length) return;
+        let input = inputs[0];
+        input.addEventListener('focus', e => this.$emit('focus'));
+        input.addEventListener('blur', e => this.$emit('blur'));
     }
 
 }
