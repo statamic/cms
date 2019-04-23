55b1a976ac1b526fa7aa00c72de02e1db0300651
Jason Varga <jason@pixelfear.com>
2019-04-23 17:36:52 -0400

    Throw error when used as single tag

--- app/Tags/Scope.php
+++ app/Tags/Scope.php
@@ -8,6 +8,8 @@ class Scope extends Tags
 {
     public function __call($method, $args)
     {
+        throw_unless($this->isPair, new \Exception('Scope tag must be a pair'));
+
         app(Cascade::class)->set($this->method, $this->context);
 
         return $this->context;
