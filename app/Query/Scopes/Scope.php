ab2c43187459051ce3cc07ec345c8f42bf92bf75
Jesse Leite <jesseleite@gmail.com>
2019-04-24 17:31:07 -0400

    Define abstract `apply()` requirement.

--- app/Query/Scopes/Scope.php
+++ app/Query/Scopes/Scope.php
@@ -10,4 +10,13 @@ abstract class Scope
     use RegistersItself, HasHandle;
 
     protected static $binding = 'scopes';
+
+    /**
+     * Apply the scope to a given query builder.
+     *
+     * @param \Statamic\Query\Builder $builder
+     * @param array $values
+     * @return void
+     */
+    abstract public function apply($query, $values);
 }
