726cba7a951f95486a645de3ff377d0c917dc344
Jason Varga <jason@pixelfear.com>
2019-05-08 10:34:51 -0400

    Asset container create and update forms using blueprints. Closes #285

---
+++ resources/js/components/asset-containers/PublishForm.vue
@@ -0,0 +1,79 @@
+<template>
+
+    <publish-container
+        v-if="blueprint"
+        ref="container"
+        name="asset-container"
+        :fieldset="blueprint"
+        :values="values"
+        reference="asset-container"
+        :meta="meta"
+        :errors="errors"
+        @updated="values = $event"
+    >
+        <div slot-scope="{ setValue }">
+            <div class="flex items-center mb-3">
+                <h1 class="flex-1" v-text="title" />
+                <button type="submit" class="btn btn-primary" @click="submit">{{ __('Save') }}</button>
+            </div>
+
+            <publish-sections @updated="setValue" />
+        </div>
+    </publish-container>
+
+</template>
+
+<script>
+export default {
+
+    props: {
+        blueprint: Object,
+        initialValues: Object,
+        meta: Object,
+        initialTitle: String,
+        url: String,
+        action: String,
+    },
+
+    data() {
+        return {
+            title: this.initialTitle,
+            values: this.initialValues,
+            error: null,
+            errors: {},
+        }
+    },
+
+    methods: {
+
+        clearErrors() {
+            this.error = null;
+            this.errors = {};
+        },
+
+        submit() {
+            this.saving = true;
+            this.clearErrors();
+
+            this.$axios[this.action](this.url, this.values).then(response => {
+                this.saving = false;
+                this.submitComplete(response);
+            }).catch(e => this.handleAxiosError(e));
+        },
+
+        handleAxiosError(e) {
+            this.saving = false;
+            if (e.response && e.response.status === 422) {
+                const { message, errors } = e.response.data;
+                this.error = message;
+                this.errors = errors;
+                this.$notify.error(message);
+            } else {
+                this.$notify.error('Something went wrong');
+            }
+        },
+
+    }
+
+}
+</script>
