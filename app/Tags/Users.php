3446ee219f4d744862238513a15f231761bf836b
Jesse Leite <jesseleite@gmail.com>
2019-05-10 15:29:07 -0400

    Update users tag to use users query builder.

--- app/Tags/Users.php
+++ app/Tags/Users.php
@@ -2,79 +2,30 @@
 
 namespace Statamic\Tags;
 
-use Statamic\API\Role;
-use Statamic\API\Term;
 use Statamic\API\User;
 use Statamic\API\UserGroup;
 
-class Users extends Collection
+class Users extends Tags
 {
+    use OutputsItems;
+
+    protected $defaultAsKey = 'users';
+
+    /**
+     * {{ get_content from="" }} ... {{ /get_content }}
+     */
     public function index()
     {
-        $this->collection = $this->getCollection();
+        $query = User::query();
 
         if ($group = $this->get('group')) {
-            $this->filterByGroup($group);
+            $query->where('group', $group);
         }
 
         if ($role = $this->get('role')) {
-            $this->filterByRole($role);
+            $query->where('role', $role);
         }
 
-        $this->filter();
-
-        if ($this->collection->isEmpty()) {
-            return $this->parseNoResults();
-        }
-
-        return $this->output();
-    }
-
-    private function getCollection()
-    {
-        if ($this->getBool('taxonomy')) {
-            return $this->getTaxonomyCollection();
-        }
-
-        return collect_content(User::all());
-    }
-
-    private function getTaxonomyCollection()
-    {
-        $data = Term::whereSlug(
-            array_get($this->context, 'page.default_slug'),
-            array_get($this->context, 'page.taxonomy')
-        );
-
-        if (! $data) {
-            return collect_content();
-        }
-
-        return $data->collection()->filter(function ($item) {
-            return $item instanceof \Statamic\Contracts\Data\Users\User;
-        });
-    }
-
-    public function getSortOrder()
-    {
-        return $this->get('sort', 'username');
-    }
-
-    protected function filterByGroup($group)
-    {
-        $group = UserGroup::whereHandle($group);
-
-        $this->collection = $this->collection->filter(function ($user) use ($group) {
-            return $user->inGroup($group);
-        });
-    }
-
-    protected function filterByRole($role)
-    {
-        $role = Role::whereHandle($role);
-
-        $this->collection = $this->collection->filter(function ($user) use ($role) {
-            return $user->hasRole($role);
-        });
+        return $this->output($query->get()->values());
     }
 }
