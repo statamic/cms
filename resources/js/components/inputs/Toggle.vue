9a563972e774736acf7533b80cdf91452a2ed3d5
Jack McDade <jack@jackmcdade.com>
2019-05-14 12:48:53 -0400

    :sparkles: new Toggle primitive input

---
+++ resources/js/components/inputs/Toggle.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="toggle-container" :class="{ 'on': value }" @click="toggle" role="checkbox">
+        <div class="toggle-slider">
+            <div class="toggle-knob" tabindex="0" @keyup.prevent.space.enter="toggle" ref="knob" />
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: ["value"],
+
+    methods: {
+        toggle() {
+            this.$emit("input", !this.value)
+        }
+    }
+
+}
+</script>
