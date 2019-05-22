ec0308ea74c1e209b34ecfe1a674d54c37f4c64f
Jason Varga <jason@pixelfear.com>
2019-05-22 14:08:00 -0400

    Comment

--- config/revisions.php
+++ config/revisions.php
@@ -2,6 +2,17 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Revisions
+    |--------------------------------------------------------------------------
+    |
+    | Revisions must be enabled per-collection by adding `revisions: true` to
+    | the collection's yaml file. Here you may disable revisions completely
+    | in one go. This is useful for disabling revisions per environment.
+    |
+    */
+
     'enabled' => false,
 
     /*
