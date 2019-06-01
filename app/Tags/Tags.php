ee1ca444f8f4812fa1ddef0d4cb8c0bb5a27cb49
Jason Varga <jason@pixelfear.com>
2019-06-01 14:54:56 -0400

    Tag parameters is now a class with a more explicit syntax. Old methods remain as aliases temporarily.

--- app/Tags/Tags.php
+++ app/Tags/Tags.php
@@ -9,6 +9,7 @@ use Statamic\API\Antlers;
 use Statamic\Extend\HasHandle;
 use Statamic\Extend\HasAliases;
 use Statamic\Extend\HasContext;
+use Statamic\Extend\Parameters;
 use Statamic\Data\DataCollection;
 use Statamic\Extend\HasParameters;
 use Statamic\Extend\RegistersItself;
@@ -74,38 +75,10 @@ abstract class Tags
         $this->parser      = $properties['parser'];
         $this->content     = $properties['content'];
         $this->context     = $properties['context'];
-        $this->parameters  = $this->setUpParameters($properties['parameters']);
+        $this->parameters  = new Parameters($properties['parameters'], $this->context);
         $this->isPair      = $this->content !== '';
         $this->tag         = array_get($properties, 'tag');
         $this->method      = array_get($properties, 'tag_method');
-    }
-
-    /**
-     * Perform set-up on any parameters
-     *
-     * @param array $params
-     * @return array
-     */
-    private function setUpParameters($params)
-    {
-        foreach ($params as $param => $value) {
-            // Values in parameters prefixed with a colon should be treated as the corresponding
-            // field's value in the context. If it doesn't exist, the value remains the literal.
-            if (Str::startsWith($param, ':')) {
-                $params[substr($param, 1)] = array_get_colon($this->context, $value, $value);
-                unset($params[$param]);
-            }
-
-            if ($value === 'true') {
-                $params[$param] = true;
-            }
-
-            if ($value === 'false') {
-                $params[$param] = false;
-            }
-        }
-
-        return $params;
     }
 
     /**
