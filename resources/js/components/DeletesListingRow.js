09f7a7127f04f44cca82cc371617248e6efd96a4
Jesse Leite <jesseleite@gmail.com>
2019-06-13 11:46:11 -0400

    Just reload page when deleting collection from listing.

--- resources/js/components/DeletesListingRow.js
+++ resources/js/components/DeletesListingRow.js
@@ -23,23 +23,11 @@ export default {
         deleteRow(resourceRoute) {
             this.$axios.delete(`${resourceRoute}/${this.deletingRow.id}`)
                 .then(() => {
-                    this.removeRowOrReload();
+                    location.reload();
                 })
                 .catch(() => {
                     this.$notify.error(__('Something went wrong'));
                 });
-        },
-
-        removeRowOrReload(index) {
-            this.rows.splice(this.deletingRow.index, 1);
-
-            // If there are no rows left, reload browser to show fresh 'create first' state.
-            if (this.rows.length === 0) {
-                location.reload();
-                return;
-            }
-
-            this.cancelDeleteRow();
         },
 
         cancelDeleteRow() {
