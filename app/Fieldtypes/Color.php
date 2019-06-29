fe46db897a1e9765996db58266bf4745261df191
Jack McDade <jack@jackmcdade.com>
2019-06-29 10:54:26 -0400

    :arrow_up: Update Color fieldtype, fixes positioning, adds configurable theme and color modes

--- app/Fieldtypes/Color.php
+++ app/Fieldtypes/Color.php
@@ -10,7 +10,30 @@ class Color extends Fieldtype
         'swatches' => [
             'type' => 'list'
         ],
-        'default_mode' => [
+        'swatches' => [
+            'type' => 'list'
+        ],
+        'theme' => [
+            'type' => 'select',
+            'default' => 'classic',
+            'options' => [
+                'classic' => 'Classic',
+                'nano' => 'Mini'
+            ],
+        ],
+        'color_modes' => [
+            'type' => 'checkboxes',
+            'inline' => 'true',
+            'options' => [
+                'hex' => 'HEX',
+                'rgba' => 'RGBA',
+                'hsla' => 'HSLA',
+                'hsva' => 'HSVA',
+                'cmyk' => 'CMYK'
+            ],
+            'default' => ['HEXA']
+        ],
+        'default_color_mode' => [
             'type' => 'select',
             'default' => 'HEXA',
             'options' => ['HEXA', 'RGBA', 'HSLA', 'HSVA', 'CMYK'],
