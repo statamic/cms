52c3be9832814bbb680b082abb027a4cec683571
Jesse Leite <jesseleite@gmail.com>
2019-07-02 13:23:16 -0400

    Extract updates header card to partial.

--- resources/views/updater/index.blade.php
+++ resources/views/updater/index.blade.php
@@ -2,18 +2,8 @@
 @section('title', __('Updater'))
 
 @section('content')
-    <div class="card p-5 content">
-        <div class="flex">
-            <div class="max-w-lg pr-4">
-                <h1 class="mb-2 text-3xl">Updates</h1>
-                <p class="text-base text-grey-80 mb-3">From new features, enhancements, and performance improvements, to bug and security fixes, keeping Statamic up to date is an important part of maintaining your site. Updates are handled by <a href="https://getcomposer.org/" target="_blank">Composer</a> and may take anywhere from a few seconds to a few minutes to run.</p>
-                <p><a href="" class="font-bold text-blue">Learn more about updates in the docs</a><span class="inline-block text-blue w-4 h-4 ml-1">@svg('external-link')</span></p>
-            </div>
-            <div class="p-2 text-center">
-                @svg('marketing/shield-check')
-            </div>
-        </div>
-    </div>
+
+    @include('statamic::updater.partials.header')
 
     <h6 class="mt-4">Core</h6>
     <div class="card p-0 mt-1">
