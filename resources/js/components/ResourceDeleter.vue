51570b9b41af0adcce557e9c43992f724117c7aa
Jesse Leite <jesseleite@gmail.com>
2019-07-04 11:08:07 -0400

    Add resource-deleter component for blade dropdowns, etc.

---
+++ resources/js/components/ResourceDeleter.vue
@@ -0,0 +1,64 @@
+<template>
+    <confirmation-modal
+        v-if="deleting"
+        :title="modalTitle"
+        :bodyText="modalBody"
+        :buttonText="__('Delete')"
+        :danger="true"
+        @confirm="confirmed"
+        @cancel="cancel"
+    >
+    </confirmation-modal>
+</template>
+
+<script>
+export default {
+
+    props: {
+        resourceTitle: {
+            type: String,
+            required: true
+        },
+        route: {
+            type: String,
+            required: true
+        }
+    },
+
+    data() {
+        return {
+            deleting: false,
+        }
+    },
+
+    computed: {
+        modalTitle() {
+            return __('Delete') + ' ' + this.resourceTitle;
+        },
+
+        modalBody() {
+            return __('Are you sure you want to delete this') + ' ' + this.resourceTitle.toLowerCase();
+        }
+    },
+
+    methods: {
+        confirm(id) {
+            this.deleting = true;
+        },
+
+        confirmed() {
+            this.$axios.delete(this.route)
+                .then(() => {
+                    location.reload();
+                })
+                .catch(() => {
+                    this.$notify.error(__('Something went wrong'));
+                });
+        },
+
+        cancel() {
+            this.deleting = false;
+        }
+    }
+}
+</script>
