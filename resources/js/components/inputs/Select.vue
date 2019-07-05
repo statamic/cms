489673ae9b15b3125ccc24d61880897c6c1c0aeb
Jesse Leite <jesseleite@gmail.com>
2019-07-04 21:03:15 -0400

    Add reset-on-change prop to our select (to reset to placeholder).

--- resources/js/components/inputs/Select.vue
+++ resources/js/components/inputs/Select.vue
@@ -1,25 +1,48 @@
 <template>
+
     <div class="select-input-container">
-        <select class="select-input" :name="name" @change="change" :value="value" :disabled="isReadOnly" @focus="$emit('focus')" @blur="$emit('blur')">
-            <option v-if="placeholder" v-text="placeholder" value="" disabled :selected="! value"></option>
+
+        <select
+            v-if="display"
+            class="select-input"
+            :name="name"
+            @change="change"
+            :value="value"
+            :disabled="isReadOnly"
+            @focus="$emit('focus')"
+            @blur="$emit('blur')"
+        >
+
+            <option
+                v-if="placeholder"
+                v-text="placeholder"
+                value=""
+                disabled
+                :selected="value === null" />
+
             <option
                 v-for="option in options"
                 :key="option.value"
                 v-text="option.label"
                 :value="option.value"
-                :selected="isOptionSelected(option)"
-            ></option>
+                :selected="isOptionSelected(option)" />
+
         </select>
+
         <div class="select-input-toggle">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                 <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/>
             </svg>
-         </div>
+        </div>
+
      </div>
+
 </template>
 
 <script>
+
 export default {
+
     props: {
         name: {},
         disabled: { default: false },
@@ -28,11 +51,19 @@ export default {
             required: false,
             default: 'Choose...'
         },
-        value: { required: true },
-        isReadOnly: { type: Boolean }
+        value: {},
+        isReadOnly: { type: Boolean },
+        resetOnChange: { default: false }
+    },
+
+    data() {
+        return {
+            display: true,
+        };
     },
 
     methods: {
+
         isOptionSelected(option) {
             return this.placeholder === false && this.value === undefined
                 ? option.value == this.options[0].value
@@ -40,8 +71,18 @@ export default {
         },
 
         change(event) {
+            if (this.resetOnChange) {
+                this.reset();
+            }
+
             this.$emit('input', event.target.value)
+        },
+
+        reset() {
+            this.display = false;
+            this.$nextTick(() => this.display = true);
         }
+
     }
 
 }
