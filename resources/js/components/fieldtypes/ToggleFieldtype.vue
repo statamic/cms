b16ceebdfba46aa0a7521d2d7d0490262859a428
Jack McDade <jack@jackmcdade.com>
2019-07-15 12:47:17 -0400

    :fire: Reduce complexity and just wrap the toggle component.

--- resources/js/components/fieldtypes/ToggleFieldtype.vue
+++ resources/js/components/fieldtypes/ToggleFieldtype.vue
@@ -1,55 +1,11 @@
 <template>
     <div class="toggle-fieldtype-wrapper">
-        <div class="toggle-container" :class="{ 'on': isOn }" @click="toggle">
-            <div class="toggle-slider" :class="{disabled: isReadOnly}">
-                <div class="toggle-knob" tabindex="0" @keyup.prevent.space.enter="toggle" ref="knob"></div>
-                <input type="hidden" :name="name" :value="state ? 1 : 0">
-            </div>
-        </div>
+        <toggle-input :value="value" @input="update" />
     </div>
 </template>
 
 <script>
 export default {
-
     mixins: [Fieldtype],
-
-    data() {
-        return {
-            state: this.value ||this.config.default || false
-        }
-    },
-
-    computed: {
-        isOn: function () {
-            let match = true;
-
-            // Allow the "on" state to be on when it's falsey.
-            // Useful for example if the variable is "hidden" but the label is "visible".
-            if (this.config && this.config.reverse) {
-                match = false;
-            }
-
-            return this.state === match;
-        }
-    },
-
-    watch: {
-        value(value) {
-            this.state = this.value;
-        }
-    },
-
-    methods: {
-        toggle() {
-            if (! this.isReadOnly) {
-                this.state = !this.state;
-                this.update(this.state);
-            }
-        },
-        focus() {
-            this.$refs.knob.focus();
-        }
-    }
 };
 </script>
