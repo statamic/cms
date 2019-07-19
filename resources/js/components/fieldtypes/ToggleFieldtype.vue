501ec3b743291cd3e27a9e41bdad82776d9ee299
Jesse Leite <jesseleite@gmail.com>
2019-07-19 10:43:10 -0400

    Revert "Fix handling of initial / default value in ToggleFieldtype.vue"
    This reverts commit 3d9caf3e962b69429babcd4db13a965a220799a1.

--- resources/js/components/fieldtypes/ToggleFieldtype.vue
+++ resources/js/components/fieldtypes/ToggleFieldtype.vue
@@ -1,43 +1,11 @@
 <template>
     <div class="toggle-fieldtype-wrapper">
-        <toggle-input :value="state" @input="state = $event" :read-only="isReadOnly" />
+        <toggle-input :value="value" @input="update" :read-only="isReadOnly" />
     </div>
 </template>
 
 <script>
 export default {
-
     mixins: [Fieldtype],
-
-    data() {
-        return {
-            state: null
-        }
-    },
-
-    watch: {
-        state(value) {
-            this.update(value);
-        }
-    },
-
-    created() {
-        this.state = this.initialValue();
-    },
-
-    methods: {
-        initialValue() {
-            if (typeof this.value === 'boolean') {
-                return this.value;
-            }
-
-            if (typeof this.config.default === 'boolean') {
-                return this.config.default;
-            }
-
-            return false;
-        }
-    }
-
 };
 </script>
