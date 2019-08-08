09545b21ebf6875496e3158f2e7d6d3e8fd1120a
Jason Varga <jason@pixelfear.com>
2019-08-08 16:51:13 -0400

    Fix 2 word modifiers not being called. Closes #597

--- app/Extend/Modifier.php
+++ app/Extend/Modifier.php
@@ -2,12 +2,20 @@
 
 namespace Statamic\Extend;
 
+use Statamic\API\Str;
+
 /**
  * Modify values within templates
  */
 class Modifier
 {
-    use RegistersItself, HasHandle;
+    use HasHandle;
 
     protected static $binding = 'modifiers';
+
+    public static function register()
+    {
+        // Not using RegistersItself trait because modifiers bind with camel cased keys, not snake case.
+        return app('statamic.'.static::$binding)[Str::camel(static::handle())] = static::class;
+    }
 }
