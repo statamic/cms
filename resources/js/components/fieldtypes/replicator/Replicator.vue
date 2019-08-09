f76fee91d14f1cd9531ae2945c19536ae125b6c4
Jason Varga <jason@pixelfear.com>
2019-08-09 13:28:28 -0400

    Handle an empty state

--- resources/js/components/fieldtypes/replicator/Replicator.vue
+++ resources/js/components/fieldtypes/replicator/Replicator.vue
@@ -168,6 +168,11 @@ export default {
 
         updateSetMeta(set, value) {
             const meta = clone(this.meta);
+
+            // If there were no existing sets, PHP would have supplied an empty
+            // array, but we need it to be an object so we can assign by key.
+            if (Array.isArray(meta.existing)) meta.existing = {};
+
             meta.existing[set._id] = value;
             this.updateMeta(meta);
         },
