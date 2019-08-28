86a12f7881c3a149ca31d5034ea9bc34ee34b24d
Jason Varga <jason@pixelfear.com>
2019-08-28 17:16:01 -0400

    Add comment blocks

--- config/stache.php
+++ config/stache.php
@@ -4,7 +4,27 @@ use Statamic\Stache\Stores;
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | File Watcher
+    |--------------------------------------------------------------------------
+    |
+    | File changes will be noticed and data will be updated accordingly.
+    | This can be disabled to reduce overhead, but you will need to
+    | either update the cache manually or use the Control Panel.
+    |
+    */
+
     'watcher' => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Stores
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure which stores are used inside the Stache.
+    |
+    */
 
     'stores' => [
 
@@ -48,6 +68,21 @@ return [
             'directory' => base_path('users'),
         ],
 
-    ]
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Indexes
+    |--------------------------------------------------------------------------
+    |
+    | Here you may define any additional indexes that will be inherited
+    | by each store in the Stache. You may also define indexes on a
+    | per-store level by adding an "indexes" key to its config.
+    |
+    */
+
+    'indexes' => [
+        //
+    ],
 
 ];
