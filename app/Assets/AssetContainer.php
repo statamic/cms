1e35d8911d90996688fc2caefa13cdb29aa2ea55
Jason Varga <jason@pixelfear.com>
2019-08-29 12:42:46 -0400

    Fix mystery characters

--- app/Assets/AssetContainer.php
+++ app/Assets/AssetContainer.php
@@ -27,7 +27,7 @@ class AssetContainer implements AssetContainerContract, Augmentable
     protected $title;
     protected $handle;
     protected $disk;
-    protected $blueprinttt;
+    protected $blueprint;
     protected $private;
     protected $allowUploads;
     protected $allowDownloading;
