608ae55febc6185a503f40c7358403bb0da01d45
Jason Varga <jason@pixelfear.com>
2019-09-11 18:42:20 -0400

    Point facade to repository

--- app/API/GlobalSet.php
+++ app/API/GlobalSet.php
@@ -3,11 +3,12 @@
 namespace Statamic\API;
 
 use Illuminate\Support\Facades\Facade;
+use Statamic\Contracts\Data\Repositories\GlobalRepository;
 
 class GlobalSet extends Facade
 {
     protected static function getFacadeAccessor()
     {
-        return Endpoint\GlobalSet::class;
+        return GlobalRepository::class;
     }
 }
