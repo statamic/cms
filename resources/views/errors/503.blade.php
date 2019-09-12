d52888267469fcd1000d3c3f43c4cb9d8d20a1f4
Jason Varga <jason@pixelfear.com>
2019-09-12 15:16:03 -0400

    Make a simpler 503

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,47 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Be right back.</title>
+@extends('statamic::outside')
 
-    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-    <style>
-        html, body {
-            height: 100%;
-        }
-
-        body {
-            margin: 0;
-            padding: 0;
-            width: 100%;
-            color: #B0BEC5;
-            display: table;
-            font-weight: 100;
-            font-family: 'Lato', sans-serif;
-        }
-
-        .container {
-            text-align: center;
-            display: table-cell;
-            vertical-align: middle;
-        }
-
-        .content {
-            text-align: center;
-            display: inline-block;
-        }
-
-        .title {
-            font-size: 72px;
-            margin-bottom: 40px;
-        }
-    </style>
-</head>
-<body>
-<div class="container">
-    <div class="content">
-        <div class="title">{{ t('be_right_back') }}</div>
+@section('content')
+    <div class="min-h-screen flex items-center justify-center">
+        <h1 class="text-3xl tracking-tight mb-5 opacity-50 text-center">503 | {{ __('Service Unavailable') }}</h1>
     </div>
-</div>
-</body>
-</html>
+@endsection
