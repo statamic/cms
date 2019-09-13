2f798a6c01647592b6f1b8e55230262003c007f4
Jason Varga <jason@pixelfear.com>
2019-09-13 14:04:39 -0400

    Listen for input instead of updated. Closes #652

--- resources/js/components/live-preview/LivePreview.vue
+++ resources/js/components/live-preview/LivePreview.vue
@@ -24,7 +24,7 @@
                                 :key="handle"
                                 :is="component"
                                 :value="extras[handle]"
-                                @updated="componentUpdated(handle, $event)"
+                                @input="componentUpdated(handle, $event)"
                                 class="ml-2" />
 
                             <button
