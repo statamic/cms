e607e65020bbe6f6e349493f587497531c1534a0
Jason Varga <jason@pixelfear.com>
2019-09-18 12:39:55 -0400

    Fields are sortable when configuring a grid

--- resources/js/components/fieldtypes/grid/FieldsFieldtype.vue
+++ resources/js/components/fieldtypes/grid/FieldsFieldtype.vue
@@ -15,6 +15,7 @@
 
 <script>
 import Fields from '../../blueprints/Fields.vue';
+import {Sortable, Plugins} from '@shopify/draggable';
 
 export default {
 
@@ -29,6 +30,10 @@ export default {
             fields: this.value,
             editingField: null,
         }
+    },
+
+    mounted() {
+        this.makeSortable();
     },
 
     methods: {
@@ -57,6 +62,17 @@ export default {
             }
         },
 
+        makeSortable() {
+            new Sortable(this.$el.querySelector('.blueprint-section-draggable-zone'), {
+                draggable: '.blueprint-section-field',
+                handle: '.blueprint-drag-handle',
+                mirror: { constrainDimensions: true },
+                plugins: [Plugins.SwapAnimation]
+            }).on('sortable:stop', e => {
+                this.fields.splice(e.newIndex, 0, this.fields.splice(e.oldIndex, 1)[0]);
+                this.update(this.fields);
+            });
+        }
     }
 
 }
