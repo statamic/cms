0a4947f14df1147dc04eaef4a6c8bd6fa7bc2ea7
Jack McDade <jack@jackmcdade.com>
2019-10-02 09:08:06 -0400

    :iphone: Reponsive get started widget

--- resources/views/widgets/getting-started.blade.php
+++ resources/views/widgets/getting-started.blade.php
@@ -3,11 +3,11 @@
         <h1>Getting started with Statamic</h1>
         <p>To begin building your new Statamic site, we recommend starting with these steps.</p>
     </div>
-    <div class="p-3 border-t flex items-center">
-        <div class="h-8 w-8 mr-2 text-blue">
+    <div class="p-3 border-t md:flex items-center">
+        <div class="h-8 w-8 mr-2 hidden md:block text-blue">
             @svg('book-pages')
         </div>
-        <div class="flex-1 mr-3">
+        <div class="flex-1 mb-2 md:mb-0 md:mr-3">
             <h3 class="mb-0">Read the Documentation</h3>
             <p>Get to know Statamic understand its capabilities the right way.</p>
         </div>
@@ -15,11 +15,11 @@
             Visit the Docs
         </a>
     </div>
-    <div class="p-3 border-t flex items-center">
-        <div class="h-8 w-8 mr-2 text-blue">
+    <div class="p-3 border-t md:flex items-center">
+        <div class="h-8 w-8 mr-2 hidden md:block text-blue">
             @svg('content-writing')
         </div>
-        <div class="flex-1 mr-3">
+        <div class="flex-1 mb-2 md:mb-0 md:mr-3">
             <h3 class="mb-0">Collections</h3>
             <p>Collections contain the different types of content in your site.</p>
         </div>
@@ -27,11 +27,11 @@
             Create Collection
         </a>
     </div>
-    <div class="p-3 border-t flex items-center">
-        <div class="h-8 w-8 mr-2 text-blue">
+    <div class="p-3 border-t md:flex items-center">
+        <div class="h-8 w-8 mr-2 hidden md:block text-blue">
             @svg('blueprints')
         </div>
-        <div class="flex-1 mr-3">
+        <div class="flex-1 mb-2 md:mb-0 md:mr-3">
             <h3 class="mb-0">Blueprints</h3>
             <p>Blueprints define the custom fields used to create and store your content.</p>
         </div>
@@ -39,11 +39,11 @@
             Create Blueprint
         </a>
     </div>
-    <div class="p-3 border-t flex items-center">
-        <div class="h-8 w-8 mr-2 text-blue">
+    <div class="p-3 border-t md:flex items-center">
+        <div class="h-8 w-8 mr-2 hidden md:block text-blue">
             @svg('addons')
         </div>
-        <div class="flex-1 mr-3">
+        <div class="flex-1 mb-2 md:mb-0 md:mr-3">
             <h3 class="mb-0">Explore the Addon Marketplace</h3>
             <p>Addons extend Statamic's core capabilities. Go see what's possible!</p>
         </div>
