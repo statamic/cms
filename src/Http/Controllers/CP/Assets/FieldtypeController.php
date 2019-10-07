dc83697451d8be495482e97b11e20efd395145c5
Jason Varga <jason@pixelfear.com>
2019-10-07 16:25:38 -0400

    Fix old asset class reference. Closes #763

--- src/Http/Controllers/CP/Assets/FieldtypeController.php
+++ src/Http/Controllers/CP/Assets/FieldtypeController.php
@@ -4,7 +4,7 @@ namespace Statamic\Http\Controllers\CP\Assets;
 
 use Illuminate\Http\Request;
 use Statamic\Http\Controllers\CP\CpController;
-use Statamic\Fieldtypes\Assets as AssetsFieldtype;
+use Statamic\Fieldtypes\Assets\Assets as AssetsFieldtype;
 
 class FieldtypeController extends CpController
 {
