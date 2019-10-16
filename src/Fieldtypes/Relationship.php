statamic/cms

commit bf5055b276966b1507423f9fa4d3e4bb74d12743

When max items is 1, it should augment to a single value

src/Fieldtypes/Relationship.php
@@ -202,9 +202,11 @@ abstract class Relationship extends Fieldtype
 
     public function augment($values)
     {
-        return collect($values)->map(function ($value) {
+        $values = collect($values)->map(function ($value) {
             return $this->augmentValue($value);
         });
+
+        return $this->config('max_items') === 1 ? $values->first() : $values;
     }
 
     protected function augmentValue($value)

commit 17e2208ee655ab7f455eb6744ec730aac7233786

Make sure we have a collection when preprocessing. Broken by bf5055b2

src/Fieldtypes/Relationship.php
@@ -58,7 +58,15 @@ abstract class Relationship extends Fieldtype
 
     public function preProcessIndex($data)
     {
-        return $this->augment($data)->map(function ($item) use ($data) {
+        if (! $items = $this->augment($data)) {
+            return [];
+        }
+
+        if ($this->config('max_items') === 1) {
+            $items = collect([$items]);
+        }
+
+        return $items->map(function ($item) use ($data) {
             return [
                 'id' => method_exists($item, 'id') ? $item->id() : $item->handle(),
                 'title' => method_exists($item, 'title') ? $item->title() : $item->get('title'),