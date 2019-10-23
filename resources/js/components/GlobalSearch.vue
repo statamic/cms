e92a3dc2dafad83e44040f01d22f549a727720d0
Jason Varga <jason@pixelfear.com>
2019-10-23 16:58:00 -0400

    Ability to remove favorites from the favorites menu

--- resources/js/components/GlobalSearch.vue
+++ resources/js/components/GlobalSearch.vue
@@ -26,7 +26,7 @@
 
             <div v-show="focused && (hasResults || hasFavorites)" class="global-search-results">
 
-                <div v-if="hasResults" v-for="(result, index) in results" class="global-search-result-item flex items-center" :class="{ 'active': current == index }" @mousedown="hit" @mousemove="setActive(index)">
+                <div v-if="hasResults" v-for="(result, index) in results" class="global-search-result-item p-1 flex items-center" :class="{ 'active': current == index }" @click="hit" @mousemove="setActive(index)">
                     <svg-icon :name="getResultIcon(result)" class="icon"></svg-icon>
                     <div class="flex-1 ml-1 title" v-html="result.title"></div>
                     <span class="rounded px-sm py-px text-2xs uppercase bg-grey-20 text-grey">
@@ -39,9 +39,12 @@
                 <div v-if="! hasResults && hasFavorites">
                     <div class="px-1.5 py-1 text-grey uppercase text-3xs">{{ __('Your Favorites') }}</div>
 
-                    <div v-for="(favorite, index) in favorites" class="global-search-result-item flex items-center" :class="{ 'active': current == index }" @mousedown="hit" @mousemove="setActive(index)">
-                        <svg-icon name="pin" class="icon"></svg-icon>
-                        <div class="flex-1 ml-1 title" v-html="favorite.name"></div>
+                    <div v-for="(favorite, index) in favorites" class="global-search-result-item flex items-center" :class="{ 'active': current == index }" @mousemove="setActive(index)">
+                        <div class="flex items-center flex-1 p-1" @click="hit">
+                            <svg-icon name="pin" class="icon"></svg-icon>
+                            <div class="ml-1 title" v-text="favorite.name"></div>
+                        </div>
+                        <div class="p-1 text-grey-60 hover:text-grey-80" @click="removeFavorite(favorite)">&times;</div>
                     </div>
 
                     <div class="text-grey text-xs px-1.5 py-1 border-t text-center"><b class="tracking-wide uppercase text-3xs">{{ __('Pro Tip')}}:</b> You can open global search using the <span class="rounded px-sm pb-px text-2xs border text-grey-50">/</span> key</div>
@@ -161,6 +164,12 @@ export default {
             if (result.is_asset) return 'assets'
             if (result.is_user) return 'user'
             return 'content-writing';
+        },
+
+        removeFavorite(favorite) {
+            this.$preferences.remove('favorites', favorite).then(response => {
+                this.$toast.success(__('Favorite removed'));
+            });
         }
     },
 
