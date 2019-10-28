1d091f27df51264a16638d0c196b8205538c6b2c
Jason Varga <jason@pixelfear.com>
2019-10-28 12:59:34 -0400

    String json files and keyed php files can be generated, and merged when they already exist.

--- src/Translator/Commands/Generate.php
+++ src/Translator/Commands/Generate.php
@@ -8,14 +8,17 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\VarExporter\VarExporter;
 
 class Generate extends Command
 {
     protected $discovery;
+    protected $files;
 
-    public function __construct(MethodDiscovery $discovery)
+    public function __construct(MethodDiscovery $discovery, Filesystem $files)
     {
         $this->discovery = $discovery;
+        $this->files = $files;
         parent::__construct();
     }
 
@@ -28,23 +31,87 @@ class Generate extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $json = $this->discovery->discover()
-            ->sort()
-            ->mapWithKeys(function ($string) {
-                return [$string => ''];
-            })
-            ->toJson(JSON_PRETTY_PRINT);
+        $this->input = $input;
+        $this->output = $output;
 
-        $langs = explode(',', $input->getArgument('lang'));
+        $this->generateStringFiles();
+        $this->generateKeyFiles();
+    }
 
-        foreach ($langs as $lang) {
-            if ($lang === 'en') {
+    protected function generateStringFiles()
+    {
+        $strings = $this->discovery->withStrings()->discover()->sort();
+
+        foreach ($this->languages() as $lang) {
+            if ($lang !== 'en') {
                 continue;
             }
 
-            $path = 'resources/lang/'.$lang.'.json';
-            (new Filesystem)->put(__DIR__.'/../../../'.$path, $json);
-            $output->writeln("<info>Translation file for <comment>$lang</comment> written to <comment>$path</comment></info>");
+            $this->generateStringFile($lang, $strings);
         }
+    }
+
+    protected function generateStringFile($lang, $strings)
+    {
+        $path = 'resources/lang/'.$lang.'.json';
+        $fullPath = __DIR__.'/../../../'.$path;
+
+        $exists = file_exists($fullPath);
+        $existing = $exists ? json_decode($this->files->get($fullPath), true) : [];
+
+        $json = $strings->mapWithKeys(function ($string) use ($existing) {
+            return [$string => $existing[$string] ?? ''];
+        })->toJson(JSON_PRETTY_PRINT);
+
+        $this->files->put($fullPath, $json);
+        $this->output->writeln($exists
+            ? "<info>Translation file for <comment>$lang</comment> merged into <comment>$path</comment></info>"
+            : "<info>Translation file for <comment>$lang</comment> created at <comment>$path</comment></info>"
+        );
+    }
+
+    protected function generateKeyFiles()
+    {
+        $this->discovery->withKeys()->discover()
+            ->map(function ($string) {
+                [$file, $string] = explode('.', $string, 2);
+                return compact('file', 'string');
+            })
+            ->groupBy('file')
+            ->each(function ($items, $file) {
+                foreach ($this->languages() as $lang) {
+                    $this->generateKeyFile($lang, $file, $items->map->string);
+                }
+            });
+    }
+
+    protected function generateKeyFile($lang, $file, $strings)
+    {
+        $path = 'resources/lang/'.$lang.'/'.$file.'.php';
+        $fullPath =__DIR__.'/../../../'.$path;
+
+        $exists = file_exists($fullPath);
+        $existing = $exists ? require $fullPath : [];
+
+        $strings = $strings->sort()->values()
+            ->mapWithKeys(function ($key) use ($existing) {
+                $translation = $existing[$key] ?? '';
+                return [$key => $translation];
+            })->all();
+
+        $contents = "<?php\n\nreturn " . VarExporter::export($strings) . ";\n";
+
+        $this->files->makeDirectory(dirname($fullPath), 0755, true, true);
+        $this->files->put($fullPath, $contents);
+
+        $this->output->writeln($exists
+            ? "<info>Translation file for <comment>$lang</comment> merged into <comment>$path</comment></info>"
+            : "<info>Translation file for <comment>$lang</comment> created at <comment>$path</comment></info>"
+        );
+    }
+
+    protected function languages()
+    {
+        return explode(',', $this->input->getArgument('lang'));
     }
 }
