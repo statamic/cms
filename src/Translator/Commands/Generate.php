c75ecd8e46deeb59d90fbf988ed20bf7fadb3178
Jason Varga <jason@pixelfear.com>
2019-11-01 12:58:45 -0400

    Use the full path

--- src/Translator/Commands/Generate.php
+++ src/Translator/Commands/Generate.php
@@ -144,7 +144,7 @@ class Generate extends Command
         foreach ($this->manualFiles as $file) {
             $source = 'resources/lang/en/'.$file.'.php';
             $fullSourcePath = __DIR__.'/../../../'.$source;
-            $sourceStrings = collect(require $source);
+            $sourceStrings = collect(require $fullSourcePath);
 
             foreach ($this->languages() as $lang) {
                 if ($lang === 'en') {
