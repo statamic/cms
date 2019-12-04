8ea88dd80da1c0a030c1ba0c72921cdfb65277e5
Jack McDade <jack@jackmcdade.com>
2019-12-03 20:02:47 -0500

    :sparkles: Link to edit Blueprint from entry

--- resources/js/components/entries/PublishForm.vue
+++ resources/js/components/entries/PublishForm.vue
@@ -13,10 +13,14 @@
                 </div>
             </h1>
 
+            <dropdown-list class="mr-2">
+                <dropdown-item :text="__('Edit Blueprint')" :redirect="actions.editBlueprint" />
+            </dropdown-list>
+
             <div class="pt-px text-2xs text-grey-60 flex mr-2" v-if="readOnly">
                 <svg-icon name="lock" class="w-4 mr-sm -mt-sm" /> {{ __('Read Only') }}
             </div>
-
+            
             <div class="hidden md:flex items-center">
                 <button
                     v-if="!readOnly"
