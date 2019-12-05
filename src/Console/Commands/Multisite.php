150a4680c71b3c1258c04af2b24df936b14a9410
Jason Varga <jason@pixelfear.com>
2019-12-05 12:20:23 -0500

    Look for globals and structures when checking if the command had been run. Ref #1065

--- src/Console/Commands/Multisite.php
+++ src/Console/Commands/Multisite.php
@@ -141,10 +141,27 @@ class Multisite extends Command
 
     protected function commandMayHaveBeenRan()
     {
-        $collection = Collection::all()->first();
+        return $this->collectionsHaveBeenMoved()
+            || $this->globalsHaveBeenMoved()
+            || $this->structuresHaveBeenMoved();
+    }
 
-        $dir = "content/collections/{$collection->handle()}/{$this->siteOne}";
+    protected function collectionsHaveBeenMoved()
+    {
+        if (! $collection = Collection::all()->first()) {
+            return false;
+        }
 
-        return File::isDirectory($dir);
+        return File::isDirectory("content/collections/{$collection->handle()}/{$this->siteOne}");
+    }
+
+    protected function globalsHaveBeenMoved()
+    {
+        return File::isDirectory("content/globals/{$this->siteOne}");
+    }
+
+    protected function structuresHaveBeenMoved()
+    {
+        return File::isDirectory("content/structures/{$this->siteOne}");
     }
 }
