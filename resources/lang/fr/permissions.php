2bba9778cfb5477e82c3fdbe5b493b1ef8e5af66
Emmanuel Beauchamps <e.bops@hotmail.fr>
2019-12-08 20:52:44 +0100

    less french words...better French
     Replaced all quotes with better quotes (just kidding)

--- resources/lang/fr/permissions.php
+++ resources/lang/fr/permissions.php
@@ -2,48 +2,48 @@
 
 return [
     'super' => 'Super utilisateur',
-    'super_desc' => "Cela accorde à l'utilisateur un accès illimité à tout. Faites attention.",
+    'super_desc' => 'Cela accorde à l’utilisateur un accès illimité à tout. Faites attention.',
     
     'group_cp' => 'Panneau de contrôle',
     'access_cp' => 'Accéder au panneau de contrôle',
-    'access_cp_desc' => "Permet l'accès au panneau de contrôle, mais ne garantit pas que vous puissiez y faire quoi que ce soit une fois à l'intérieur !",
+    'access_cp_desc' => 'Permet l’accès au panneau de contrôle, mais ne garantit pas que vous puissiez y faire quoi que ce soit une fois à l’intérieur !',
     
     'group_collections' => 'Collections',
     'configure_collections' => 'Configurer les collections',
-    'configure_collections_desc' => "Permet d'accéder à toutes les autorisations liées à la collection.",
+    'configure_collections_desc' => 'Permet d’accéder à toutes les autorisations liées à la collection.',
     'view_{collection}_entries' => 'Voir les entrées de :collection',
     'edit_{collection}_entries' => 'Modifier les entrées',
     'create_{collection}_entries' => 'Créer de nouvelles entrées',
     'delete_{collection}_entries' => 'Supprimer des entrées',
-    'publish_{collection}_entries' => "Gérer l'état de publication",
+    'publish_{collection}_entries' => 'Gérer l’état de publication',
     'publish_{collection}_entries_desc' => 'Possibilité de passer du brouillon au publié et vice versa',
     'reorder_{collection}_entries' => 'Réorganiser les entrées',
     'reorder_{collection}_entries_desc' => 'Active la réorganisation par glisser-déposer',
     
     'group_taxonomies' => 'Taxonomies',
     'configure_taxonomies' => 'Configurer les taxonomies',
-    'configure_taxonomies_desc' => "Permet d'accéder à toutes les autorisations liées à la taxonomie",
+    'configure_taxonomies_desc' => 'Permet d’accéder à toutes les autorisations liées à la taxonomie',
     'view_{taxonomy}_terms' => 'Voir les termes de :taxonomy',
     'edit_{taxonomy}_terms' => 'Modifier les termes',
     'create_{taxonomy}_terms' => 'Créer de nouveaux termes',
     'delete_{taxonomy}_terms' => 'Supprimer les termes',
-    'publish_{taxonomy}_terms' => "Gérer l'état de publication",
+    'publish_{taxonomy}_terms' => 'Gérer l’état de publication',
     'reorder_{taxonomy}_terms' => 'Réorganiser les termes',
     
     'group_structures' => 'Structures',
     'configure_structures' => 'Configurer les structures',
-    'configure_structures_desc' => "Permet d'accéder à toutes les autorisations liées à la structure",
+    'configure_structures_desc' => 'Permet d’accéder à toutes les autorisations liées à la structure',
     'view_{structure}_structure' => 'Voir la structure :structure',
     'edit_{structure}_structure' => 'Modifier la structure',
     
     'group_globals' => 'Globales',
     'configure_globals' => 'Configurer les globales',
-    'configure_globals_desc' => "Permet d'accéder à toutes les autorisations liées aux globales.",
+    'configure_globals_desc' => 'Permet d’accéder à toutes les autorisations liées aux globales.',
     'edit_{global}_globals' => 'Modifier les globales :global',
     
     'group_assets' => 'Ressources',
     'configure_asset_containers' => 'Configurer les conteneurs de ressources',
-    'configure_asset_containers_desc' => "Permet d'accéder à toutes les autorisations liées aux ressources.",
+    'configure_asset_containers_desc' => 'Permet d’accéder à toutes les autorisations liées aux ressources.',
     'view_{container}_assets' => 'Voir les ressources de :container',
     'upload_{container}_assets' => 'Télécharger de nouvelles ressources',
     'edit_{container}_assets' => 'Modifier les ressources',
@@ -53,7 +53,7 @@ return [
     
     'group_forms' => 'Formulaires',
     'configure_forms' => 'Configurer les formulaires',
-    'configure_forms_desc' => "Permet d'accéder à toutes les autorisations liées aux formulaires.",
+    'configure_forms_desc' => 'Permet d’accéder à toutes les autorisations liées aux formulaires.',
     'view_{form}_form_submissions' => 'Voir les soumissions de :form',
     'delete_{form}_form_submissions' => 'Supprimer les soumissions de :form',
     
@@ -72,7 +72,7 @@ return [
     
     'group_utilities' => 'Utilitaires',
     'access_utility' => ':title',
-    'access_utility_desc' => "Permet d'accéder à l'utilitaire :title",
+    'access_utility_desc' => 'Permet d’accéder à l’utilitaire :title',
     
     'group_misc' => 'Divers',
 ];
