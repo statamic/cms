808303d4cc402d15c79e1e7d6ccdd91a893b6920
Jason Varga <jason@pixelfear.com>
2019-12-09 11:01:39 -0500

    Adjust imports ...
    Because we upgraded to mix 5 and therefore vue-loader 15, our imports need to be updated. require() needs to change to require().default
    
    Or, use the import syntax.
    
    See https://github.com/JeffreyWay/laravel-mix/releases/tag/v4.0.0
    
    The

--- resources/js/bootstrap/components.js
+++ resources/js/bootstrap/components.js
@@ -5,75 +5,75 @@ import vSelect from 'vue-select'
 Vue.component('v-select', vSelect)
 
 // Core
-Vue.component('asset-manager', require('../components/assets/AssetManager.vue'));
-Vue.component('asset-browser', require('../components/assets/Browser/Browser.vue'));
-Vue.component('updater', require('../components/Updater.vue'));
-Vue.component('updates-badge', require('../components/UpdatesBadge.vue'));
-Vue.component('composer-output', require('../components/ComposerOutput.vue'));
+Vue.component('asset-manager', require('../components/assets/AssetManager.vue').default);
+Vue.component('asset-browser', require('../components/assets/Browser/Browser.vue').default);
+Vue.component('updater', require('../components/Updater.vue').default);
+Vue.component('updates-badge', require('../components/UpdatesBadge.vue').default);
+Vue.component('composer-output', require('../components/ComposerOutput.vue').default);
 
 // Publish
-Vue.component('publish-container', require('../components/publish/Container.vue'));
-Vue.component('publish-form', require('../components/publish/PublishForm.vue'));
-Vue.component('publish-fields', require('../components/publish/Fields.vue'));
-Vue.component('publish-field-meta', require('../components/publish/FieldMeta.vue'));
-Vue.component('publish-sections', require('../components/publish/Sections.vue'));
-Vue.component('publish-validation-errors', require('../components/publish/ValidationErrors'));
-Vue.component('form-group', require('../components/publish/FormGroup.vue'));
+Vue.component('publish-container', require('../components/publish/Container.vue').default);
+Vue.component('publish-form', require('../components/publish/PublishForm.vue').default);
+Vue.component('publish-fields', require('../components/publish/Fields.vue').default);
+Vue.component('publish-field-meta', require('../components/publish/FieldMeta.vue').default);
+Vue.component('publish-sections', require('../components/publish/Sections.vue').default);
+Vue.component('publish-validation-errors', require('../components/publish/ValidationErrors').default);
+Vue.component('form-group', require('../components/publish/FormGroup.vue').default);
 
-Vue.component('live-preview', require('../components/live-preview/LivePreview.vue'));
-Vue.component('live-preview-popout', require('../components/live-preview/Popout.vue'));
+Vue.component('live-preview', require('../components/live-preview/LivePreview.vue').default);
+Vue.component('live-preview-popout', require('../components/live-preview/Popout.vue').default);
 
-Vue.component('EntryPublishForm', require('../components/entries/PublishForm.vue'));
-Vue.component('TermPublishForm', require('../components/terms/PublishForm.vue'));
-Vue.component('UserPublishForm', require('../components/users/PublishForm.vue'));
+Vue.component('EntryPublishForm', require('../components/entries/PublishForm.vue').default);
+Vue.component('TermPublishForm', require('../components/terms/PublishForm.vue').default);
+Vue.component('UserPublishForm', require('../components/users/PublishForm.vue').default);
 
 // Data List
-Vue.component('data-list', require('../components/data-list/DataList.vue'));
-Vue.component('data-list-search', require('../components/data-list/Search.vue'));
-Vue.component('data-list-bulk-actions', require('../components/data-list/BulkActions.vue'));
-Vue.component('data-list-inline-actions', require('../components/data-list/InlineActions.vue'));
-Vue.component('data-list-column-picker', require('../components/data-list/ColumnPicker.vue'));
-Vue.component('data-list-toggle-all', require('../components/data-list/ToggleAll.vue'));
-Vue.component('data-list-pagination', require('../components/data-list/Pagination.vue'));
-Vue.component('data-list-filters', require('../components/data-list/Filters.vue'));
-Vue.component('data-list-table', require('../components/data-list/Table.vue'));
+Vue.component('data-list', require('../components/data-list/DataList.vue').default);
+Vue.component('data-list-search', require('../components/data-list/Search.vue').default);
+Vue.component('data-list-bulk-actions', require('../components/data-list/BulkActions.vue').default);
+Vue.component('data-list-inline-actions', require('../components/data-list/InlineActions.vue').default);
+Vue.component('data-list-column-picker', require('../components/data-list/ColumnPicker.vue').default);
+Vue.component('data-list-toggle-all', require('../components/data-list/ToggleAll.vue').default);
+Vue.component('data-list-pagination', require('../components/data-list/Pagination.vue').default);
+Vue.component('data-list-filters', require('../components/data-list/Filters.vue').default);
+Vue.component('data-list-table', require('../components/data-list/Table.vue').default);
 
 // Resource Type Lists
-Vue.component('entry-list', require('../components/entries/Listing.vue'));
-Vue.component('collection-list', require('../components/collections/Listing.vue'));
-Vue.component('taxonomy-list', require('../components/taxonomies/Listing.vue'));
-Vue.component('term-list', require('../components/terms/Listing.vue'));
-Vue.component('asset-container-list', require('../components/AssetContainerList.vue'));
-Vue.component('addon-list', require('../components/AddonList.vue'));
-Vue.component('addon-details', require('../components/AddonDetails.vue'));
+Vue.component('entry-list', require('../components/entries/Listing.vue').default);
+Vue.component('collection-list', require('../components/collections/Listing.vue').default);
+Vue.component('taxonomy-list', require('../components/taxonomies/Listing.vue').default);
+Vue.component('term-list', require('../components/terms/Listing.vue').default);
+Vue.component('asset-container-list', require('../components/AssetContainerList.vue').default);
+Vue.component('addon-list', require('../components/AddonList.vue').default);
+Vue.component('addon-details', require('../components/AddonDetails.vue').default);
 
 // Widgets
-Vue.component('collection-widget', require('../components/entries/Widget.vue'));
+Vue.component('collection-widget', require('../components/entries/Widget.vue').default);
 
 // Reusable
-Vue.component('svg-icon', require('../components/SvgIcon.vue'));
-Vue.component('file-icon', require('../components/FileIcon.vue'));
-Vue.component('loading-graphic', require('../components/LoadingGraphic.vue'));
-Vue.component('dropdown-list', require('../components/DropdownList.vue'));
-Vue.component('dropdown-item', require('../components/DropdownItem.vue'));
-Vue.component('validation-errors', require('../components/ValidationErrors'));
-Vue.component('slugify', require('../components/Slugify.vue'));
-Vue.component('element-container', require('../components/ElementContainer.vue'));
-Vue.component('avatar', require('../components/Avatar.vue'));
-Vue.component('breadcrumbs', require('../components/Breadcrumbs.vue'));
+Vue.component('svg-icon', require('../components/SvgIcon.vue').default);
+Vue.component('file-icon', require('../components/FileIcon.vue').default);
+Vue.component('loading-graphic', require('../components/LoadingGraphic.vue').default);
+Vue.component('dropdown-list', require('../components/DropdownList.vue').default);
+Vue.component('dropdown-item', require('../components/DropdownItem.vue').default);
+Vue.component('validation-errors', require('../components/ValidationErrors').default);
+Vue.component('slugify', require('../components/Slugify.vue').default);
+Vue.component('element-container', require('../components/ElementContainer.vue').default);
+Vue.component('avatar', require('../components/Avatar.vue').default);
+Vue.component('breadcrumbs', require('../components/Breadcrumbs.vue').default);
 
 // Recursive
-Vue.component('role-permission-tree', require('../components/roles/PermissionTree.vue'));
+Vue.component('role-permission-tree', require('../components/roles/PermissionTree.vue').default);
 
 // Modals
-Vue.component('modal', require('../components/Modal.vue'));
-Vue.component('confirmation-modal', require('../components/modals/ConfirmationModal.vue'));
-Vue.component('favorite-creator', require('../components/FavoriteCreator.vue'));
-Vue.component('keyboard-shortcuts-modal', require('../components/modals/KeyboardShortcutsModal.vue'));
-Vue.component('resource-deleter', require('../components/ResourceDeleter.vue'));
-// Vue.component('modal-dialog', require('../components/ModalDialog.vue'));
+Vue.component('modal', require('../components/Modal.vue').default);
+Vue.component('confirmation-modal', require('../components/modals/ConfirmationModal.vue').default);
+Vue.component('favorite-creator', require('../components/FavoriteCreator.vue').default);
+Vue.component('keyboard-shortcuts-modal', require('../components/modals/KeyboardShortcutsModal.vue').default);
+Vue.component('resource-deleter', require('../components/ResourceDeleter.vue').default);
+// Vue.component('modal-dialog', require('../components/ModalDialog.vue').default);
 
-Vue.component('stack', require('../components/stacks/Stack.vue'));
-Vue.component('stack-test', require('../components/stacks/StackTest.vue'));
+Vue.component('stack', require('../components/stacks/Stack.vue').default);
+Vue.component('stack-test', require('../components/stacks/StackTest.vue').default);
 
-Vue.component('pane', require('../components/panes/Pane.vue'));
+Vue.component('pane', require('../components/panes/Pane.vue').default);
--- resources/js/bootstrap/fieldtypes.js
+++ resources/js/bootstrap/fieldtypes.js
@@ -1,14 +1,14 @@
 import Vue from "vue";
 
-Vue.component('select-input', require('../components/inputs/Select.vue'));
-Vue.component('text-input', require('../components/inputs/Text.vue'));
-Vue.component('textarea-input', require('../components/inputs/Textarea.vue'));
-Vue.component('toggle-input', require('../components/inputs/Toggle.vue'));
-Vue.component('relationship-input', require('../components/inputs/relationship/RelationshipInput.vue'));
+Vue.component('select-input', require('../components/inputs/Select.vue').default);
+Vue.component('text-input', require('../components/inputs/Text.vue').default);
+Vue.component('textarea-input', require('../components/inputs/Textarea.vue').default);
+Vue.component('toggle-input', require('../components/inputs/Toggle.vue').default);
+Vue.component('relationship-input', require('../components/inputs/relationship/RelationshipInput.vue').default);
 
-Vue.component('text-fieldtype', require('../components/fieldtypes/TextFieldtype.vue'));
-Vue.component('textarea-fieldtype', require('../components/fieldtypes/TextareaFieldtype.vue'));
-Vue.component('slug-fieldtype', require('../components/fieldtypes/SlugFieldtype.vue'));
+Vue.component('text-fieldtype', require('../components/fieldtypes/TextFieldtype.vue').default);
+Vue.component('textarea-fieldtype', require('../components/fieldtypes/TextareaFieldtype.vue').default);
+Vue.component('slug-fieldtype', require('../components/fieldtypes/SlugFieldtype.vue').default);
 
 // Fieldtypes
 import FieldsetFieldtype from '../components/fieldtypes/FieldsetFieldtype.vue'
@@ -18,43 +18,43 @@ import RoutesFieldtype from '../components/fieldtypes/RoutesFieldtype.vue'
 import StatusFieldtype from '../components/fieldtypes/StatusFieldtype.vue'
 import TemplateFieldtype from '../components/fieldtypes/TemplateFieldtype.vue'
 
-Vue.component('array-fieldtype', require('../components/fieldtypes/ArrayFieldtype.vue'));
-Vue.component('assets-fieldtype', require('../components/fieldtypes/assets/AssetsFieldtype.vue'));
-Vue.component('assets-fieldtype-index', require('../components/fieldtypes/assets/AssetsIndexFieldtype.vue'));
-Vue.component('asset_folder-fieldtype', require('../components/fieldtypes/AssetFolderFieldtype.vue'));
-Vue.component('bard-fieldtype', require('../components/fieldtypes/bard/BardFieldtype.vue'));
-Vue.component('bard_buttons_setting-fieldtype', require('../components/fieldtypes/bard/BardButtonsSettingFieldtype.vue'));
-Vue.component('checkboxes-fieldtype', require('../components/fieldtypes/CheckboxesFieldtype.vue'));
-Vue.component('code-fieldtype', require('../components/fieldtypes/CodeFieldtype.vue'));
-Vue.component('color-fieldtype', require('../components/fieldtypes/ColorFieldtype.vue'));
-Vue.component('date-fieldtype', require('../components/fieldtypes/DateFieldtype.vue'));
+Vue.component('array-fieldtype', require('../components/fieldtypes/ArrayFieldtype.vue').default);
+Vue.component('assets-fieldtype', require('../components/fieldtypes/assets/AssetsFieldtype.vue').default);
+Vue.component('assets-fieldtype-index', require('../components/fieldtypes/assets/AssetsIndexFieldtype.vue').default);
+Vue.component('asset_folder-fieldtype', require('../components/fieldtypes/AssetFolderFieldtype.vue').default);
+Vue.component('bard-fieldtype', require('../components/fieldtypes/bard/BardFieldtype.vue').default);
+Vue.component('bard_buttons_setting-fieldtype', require('../components/fieldtypes/bard/BardButtonsSettingFieldtype.vue').default);
+Vue.component('checkboxes-fieldtype', require('../components/fieldtypes/CheckboxesFieldtype.vue').default);
+Vue.component('code-fieldtype', require('../components/fieldtypes/CodeFieldtype.vue').default);
+Vue.component('color-fieldtype', require('../components/fieldtypes/ColorFieldtype.vue').default);
+Vue.component('date-fieldtype', require('../components/fieldtypes/DateFieldtype.vue').default);
 Vue.component('fieldset-fieldtype', FieldsetFieldtype);
-Vue.component('fields-fieldtype', require('../components/fieldtypes/grid/FieldsFieldtype.vue'));
-Vue.component('grid-fieldtype', require('../components/fieldtypes/grid/Grid.vue'));
-Vue.component('grid-fieldtype-index', require('../components/fieldtypes/grid/GridIndex.vue'));
-Vue.component('hidden-fieldtype', require('../components/fieldtypes/HiddenFieldtype.vue'));
-Vue.component('integer-fieldtype', require('../components/fieldtypes/IntegerFieldtype.vue'));
-Vue.component('list-fieldtype', require('../components/fieldtypes/ListFieldtype.vue'));
-Vue.component('markdown-fieldtype', require('../components/fieldtypes/MarkdownFieldtype.vue'));
-Vue.component('radio-fieldtype', require('../components/fieldtypes/RadioFieldtype.vue'));
-Vue.component('range-fieldtype', require('../components/fieldtypes/RangeFieldtype.vue'));
+Vue.component('fields-fieldtype', require('../components/fieldtypes/grid/FieldsFieldtype.vue').default);
+Vue.component('grid-fieldtype', require('../components/fieldtypes/grid/Grid.vue').default);
+Vue.component('grid-fieldtype-index', require('../components/fieldtypes/grid/GridIndex.vue').default);
+Vue.component('hidden-fieldtype', require('../components/fieldtypes/HiddenFieldtype.vue').default);
+Vue.component('integer-fieldtype', require('../components/fieldtypes/IntegerFieldtype.vue').default);
+Vue.component('list-fieldtype', require('../components/fieldtypes/ListFieldtype.vue').default);
+Vue.component('markdown-fieldtype', require('../components/fieldtypes/MarkdownFieldtype.vue').default);
+Vue.component('radio-fieldtype', require('../components/fieldtypes/RadioFieldtype.vue').default);
+Vue.component('range-fieldtype', require('../components/fieldtypes/RangeFieldtype.vue').default);
 Vue.component('relate-fieldtype', RelateFieldtype);
-Vue.component('relationship-fieldtype', require('../components/fieldtypes/relationship/RelationshipFieldtype.vue'));
-Vue.component('relationship-fieldtype-index', require('../components/fieldtypes/relationship/RelationshipIndexFieldtype.vue'));
-Vue.component('replicator-fieldtype', require('../components/fieldtypes/replicator/Replicator.vue'));
-Vue.component('replicator-fieldtype-index', require('../components/fieldtypes/replicator/ReplicatorIndex.vue'));
+Vue.component('relationship-fieldtype', require('../components/fieldtypes/relationship/RelationshipFieldtype.vue').default);
+Vue.component('relationship-fieldtype-index', require('../components/fieldtypes/relationship/RelationshipIndexFieldtype.vue').default);
+Vue.component('replicator-fieldtype', require('../components/fieldtypes/replicator/Replicator.vue').default);
+Vue.component('replicator-fieldtype-index', require('../components/fieldtypes/replicator/ReplicatorIndex.vue').default);
 Vue.component('revealer-fieldtype', RevealerFieldtype);
 Vue.component('routes-fieldtype', RoutesFieldtype);
-Vue.component('section-fieldtype', require('../components/fieldtypes/SectionFieldtype.vue'));
-Vue.component('select-fieldtype', require('../components/fieldtypes/SelectFieldtype.vue'));
-Vue.component('sets-fieldtype', require('../components/fieldtypes/replicator/SetsFieldtype.vue'));
+Vue.component('section-fieldtype', require('../components/fieldtypes/SectionFieldtype.vue').default);
+Vue.component('select-fieldtype', require('../components/fieldtypes/SelectFieldtype.vue').default);
+Vue.component('sets-fieldtype', require('../components/fieldtypes/replicator/SetsFieldtype.vue').default);
 Vue.component('status-fieldtype', StatusFieldtype);
-Vue.component('structure_sites-fieldtype', require('../components/structures/Sites.vue'));
-Vue.component('table-fieldtype', require('../components/fieldtypes/TableFieldtype.vue'));
-Vue.component('tags-fieldtype', require('../components/fieldtypes/TagsFieldtype.vue'));
-Vue.component('tags-fieldtype-index', require('../components/fieldtypes/TagsIndexFieldtype.vue'));
+Vue.component('structure_sites-fieldtype', require('../components/structures/Sites.vue').default);
+Vue.component('table-fieldtype', require('../components/fieldtypes/TableFieldtype.vue').default);
+Vue.component('tags-fieldtype', require('../components/fieldtypes/TagsFieldtype.vue').default);
+Vue.component('tags-fieldtype-index', require('../components/fieldtypes/TagsIndexFieldtype.vue').default);
 Vue.component('template-fieldtype', TemplateFieldtype);
-Vue.component('time-fieldtype', require('../components/fieldtypes/TimeFieldtype.vue'));
-Vue.component('toggle-fieldtype', require('../components/fieldtypes/ToggleFieldtype.vue'));
-Vue.component('video-fieldtype', require('../components/fieldtypes/VideoFieldtype.vue'));
-Vue.component('yaml-fieldtype', require('../components/fieldtypes/YamlFieldtype.vue'));
+Vue.component('time-fieldtype', require('../components/fieldtypes/TimeFieldtype.vue').default);
+Vue.component('toggle-fieldtype', require('../components/fieldtypes/ToggleFieldtype.vue').default);
+Vue.component('video-fieldtype', require('../components/fieldtypes/VideoFieldtype.vue').default);
+Vue.component('yaml-fieldtype', require('../components/fieldtypes/YamlFieldtype.vue').default);
