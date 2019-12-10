0fa0b07383ae0d147acbcf0b5613360cecbec249
Jason Varga <jason@pixelfear.com>
2019-12-10 16:30:33 -0500

    Add default values for any missing blueprint fields ...
    Should solve a long standing friction point where a missing field in a loop would inherit the value from the parent context.
    
    The behavior only happens if the field is in the blueprint, so users should make sure it has what they need.
    
    Collection tag test covers this, but since its in the augmentable trait, it should apply all over the place. In the taxonomy tag, etc.

--- src/Data/Augmentable.php
+++ src/Data/Augmentable.php
@@ -14,15 +14,25 @@ trait Augmentable
             $fields = collect();
         }
 
-        return collect($this->augmentedArrayData())
-            ->map(function ($value, $handle) use ($fields) {
-                if (! $fields->has($handle)) {
-                    return $value;
-                }
+        // Convert provided data into values.
+        $values = collect($this->augmentedArrayData())->map(function ($value, $handle) use ($fields) {
+            if (! $fields->has($handle)) {
+                return $value;
+            }
 
-                return new Value($value, $handle, $fields->get($handle)->fieldtype(), $this);
-            })
-            ->all();
+            return new Value($value, $handle, $fields->get($handle)->fieldtype(), $this);
+        });
+
+        // Any values that aren't in the data but are in the blueprint should have their defaults/nulls added.
+        if ($blueprint) {
+            foreach ($fields as $handle => $field) {
+                if (! $values->has($handle)) {
+                    $values[$handle] = new Value($field->defaultValue(), $handle, $field->fieldtype(), $this);
+                }
+            }
+        }
+
+        return $values->all();
     }
 
     public function augmentedArrayData()
