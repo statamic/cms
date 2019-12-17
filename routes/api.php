ba24eadd7f785452b0ac7d3e4ced27844162b5f1
Jesse Leite <jesseleite@gmail.com>
2019-12-17 13:53:20 -0500

    Remove collection, form, and taxonomy API endpoints.

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,7 @@
 <?php
 
 Route::resource('assets', 'AssetsController');
-Route::resource('collections', 'CollectionsController');
 Route::resource('collections/{collection}/entries', 'CollectionEntriesController');
-Route::resource('forms', 'FormsController');
 Route::resource('globals', 'GlobalsController');
-Route::resource('taxonomies', 'TaxonomiesController');
 Route::resource('taxonomies/{taxonomy}/terms', 'TaxonomyTermsController');
 Route::resource('users', 'UsersController');
