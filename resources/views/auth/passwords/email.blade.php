c40f188a7fe5570108b02bc59374e415ddfa2455
Steven Thomas <stvn.thomas@gmail.com>
2020-01-02 14:09:29 -0800

    Fix untranslated content
    Signed-off-by: Steven Thomas <stvn.thomas@gmail.com>

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -9,7 +9,7 @@
     <div class="card auth-card mx-auto">
         <div class="text-center pb-2 mb-2">
             <h1 class="mb-2 text-lg text-grey-80">{{ __('Forgot Your Password?') }}</h1>
-            <p class="text-sm text-grey">{{ __('messages.forgot_password_enter_email') }}</p>
+            <p class="text-sm text-grey">{{ __('statamic::messages.forgot_password_enter_email') }}</p>
         </div>
 
         @if (session('status'))
