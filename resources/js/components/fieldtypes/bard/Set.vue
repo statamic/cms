54c1821394bc669d67b9b5fa013b4cd0b68d9c52
Jason Varga <jason@pixelfear.com>
2020-01-07 11:32:39 -0500

    Add copy.stop, fixes #1131

--- resources/js/components/fieldtypes/bard/Set.vue
+++ resources/js/components/fieldtypes/bard/Set.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <div class="bard-set whitespace-normal my-3 rounded bg-white border shadow" contenteditable="false" @paste.stop>
+    <div class="bard-set whitespace-normal my-3 rounded bg-white border shadow" contenteditable="false" @copy.stop @paste.stop>
         <div ref="content" hidden />
         <div class="replicator-set-header" :class="{'collapsed': collapsed}">
             <div class="item-move sortable-handle" data-drag-handle />
