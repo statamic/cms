85064c7b5bcb6a6f07b8ab03a8d384b01e04b310
Jason Varga <jason@pixelfear.com>
2020-01-08 10:40:14 -0500

    Parsers can be saved

--- src/Markdown/Manager.php
+++ src/Markdown/Manager.php
@@ -2,24 +2,48 @@
 
 namespace Statamic\Markdown;
 
+use InvalidArgumentException;
 use Statamic\Markdown\Parser;
 
 class Manager
 {
-    protected $defaultParser;
-
-    public function __construct($defaultParser)
-    {
-        $this->defaultParser = $defaultParser;
-    }
+    protected $parsers = [];
 
     public function __call($method, $args)
     {
-        return $this->defaultParser->$method(...$args);
+        return $this->defaultParser()->$method(...$args);
     }
 
     public function makeParser(array $config = []): Parser
     {
         return new Parser($config);
+    }
+
+    public function defaultParser()
+    {
+        if (! $this->hasParser('default')) {
+            $this->setParser('default', $this->makeParser());
+        }
+
+        return $this->parser('default');
+    }
+
+    public function parser(string $name)
+    {
+        if (! $this->hasParser($name)) {
+            throw new InvalidArgumentException("Markdown parser [$name] is not defined.");
+        }
+
+        return $this->parsers[$name];
+    }
+
+    public function setParser(string $name, Parser $parser = null)
+    {
+        $this->parsers[$name] = $parser;
+    }
+
+    public function hasParser(string $name): bool
+    {
+        return isset($this->parsers[$name]);
     }
 }
--- tests/Markdown/ManagerTest.php
+++ tests/Markdown/ManagerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Markdown;
 
+use InvalidArgumentException;
 use Mockery;
 use PHPUnit\Framework\TestCase;
 use Statamic\Markdown\Manager;
@@ -20,22 +21,42 @@ class ManagerTest extends TestCase
         $defaultParser = Mockery::mock(Parser::class);
         $defaultParser->shouldReceive('foo')->once()->andReturn('bar');
 
-        $this->assertEquals('bar', (new Manager($defaultParser))->foo());
+        $manager = new Manager;
+        $manager->setParser('default', $defaultParser);
+
+        $this->assertEquals('bar', $manager->foo());
     }
 
     /** @test */
     function it_makes_a_new_parser_instance()
     {
-        $defaultParser = new Parser;
+        $manager = new Manager;
+        $manager->setParser('default', $defaultParser = new Parser);
 
-        $manager = new Manager($defaultParser);
-
-        $config = ['foo' => 'bar'];
-
-        $parser = $manager->makeParser($config);
+        $parser = $manager->makeParser($config = ['foo' => 'bar']);
 
         $this->assertInstanceOf(Parser::class, $parser);
         $this->assertNotSame($parser, $defaultParser);
         $this->assertEquals('bar', $parser->environment()->getConfig('foo'));
+    }
+
+    /** @test */
+    function parser_instances_can_be_saved_and_retrieved()
+    {
+        $manager = new Manager;
+
+        try {
+            $parser = $manager->parser('a');
+        } catch (InvalidArgumentException $e) {
+            $this->assertEquals('Markdown parser [a] is not defined.', $e->getMessage());
+        }
+
+        $parserA = $manager->makeParser();
+        $parserB = $manager->makeParser();
+
+        $manager->setParser('a', $parserA);
+
+        $this->assertSame($parserA, $manager->parser('a'));
+        $this->assertNotSame($parserB, $manager->parser('a'));
     }
 }
