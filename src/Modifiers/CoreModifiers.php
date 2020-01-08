fe671d5885d90d39bbe34e1dbaea9df9da479f14
Jason Varga <jason@pixelfear.com>
2020-01-08 13:55:58 -0500

    Allow the markdown modifier to pick a parser

--- src/Modifiers/CoreModifiers.php
+++ src/Modifiers/CoreModifiers.php
@@ -20,6 +20,7 @@ use Statamic\Facades\Theme;
 use Statamic\Facades\Config;
 use Statamic\Facades\Helper;
 use Statamic\Facades\Localization;
+use Statamic\Facades\Markdown;
 use Stringy\StaticStringy as Stringy;
 use Statamic\Modifiers\Modifier;
 
@@ -1099,9 +1100,15 @@ class CoreModifiers extends Modifier
      * @param $value
      * @return mixed
      */
-    public function markdown($value)
+    public function markdown($value, $params)
     {
-        return Html::markdown($value);
+        $parser = $params[0] ?? 'default';
+
+        if (in_array($parser, [true, 'true', ''])) {
+            $parser = 'default';
+        }
+
+        return Markdown::parser($parser)->parse($value);
     }
 
     /**
