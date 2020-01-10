09e770a323037df8128db89dd37a3e426c60b2fc
Jack McDade <jack@jackmcdade.com>
2020-01-10 10:44:03 -0500

    :construction: wip settings

--- src/Fieldtypes/Template.php
+++ src/Fieldtypes/Template.php
@@ -6,4 +6,18 @@ use Statamic\Fields\Fieldtype;
 
 class Template extends Fieldtype
 {
+    protected $configFields = [
+        'hide_partials' => [
+            'type' => 'toggle',
+            'default' => true,
+            'width' => 50,
+            'instructions' => 'Partials are rarely intended to be used as templates.'
+        ],
+        'hide_error_templates' => [
+            'type' => 'toggle',
+            'default' => true,
+            'width' => 50,
+            'instructions' => 'Error templates are intended to be a last resort, not first pick.'
+        ],
+    ];
 }
