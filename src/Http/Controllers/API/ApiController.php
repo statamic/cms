a298671a915f59fdfe6f83a70516958f7d98e232
Jesse Leite <jesseleite@gmail.com>
2020-01-13 09:39:53 -0500

    Extract base API controller.

---
+++ src/Http/Controllers/API/ApiController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Statamic\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use Statamic\Http\Controllers\Controller;
+use Statamic\Support\Str;
+use Statamic\Tags\Query\HasConditions;
+
+class ApiController extends Controller
+{
+    use HasConditions;
+
+    /**
+     * @var \Illuminate\Http\Request
+     */
+    protected $request;
+
+    /**
+     * Create a new CpController
+     *
+     * @param \Illuminate\Http\Request $request
+     */
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * Filter, sort, and paginate query for API resource output.
+     *
+     * @param \Statamic\Query\Builder $query
+     * @return \Statamic\Extensions\Pagination\LengthAwarePaginator
+     */
+    protected function filterSortAndPaginate($query)
+    {
+        return $this
+            ->filter($query)
+            ->sort($query)
+            ->paginate($query);
+    }
+
+    /**
+     * Filters a query based on conditions in the filter parameter.
+     *
+     * /endpoint?filter[field:condition]=foo&filter[anotherfield]=bar
+     *
+     * @param \Statamic\Query\Builder $query
+     * @return $this
+     */
+    protected function filter($query)
+    {
+        collect($this->request->filter ?? [])
+            ->each(function ($value, $filter) use ($query) {
+                if (Str::contains($filter, ':')) {
+                    [$field, $condition] = explode(':', $filter);
+                } else {
+                    $field = $filter;
+                    $condition = 'equals';
+                }
+
+                $this->queryCondition($query, $field, $condition, $value);
+            });
+
+        return $this;
+    }
+
+    /**
+     * Sorts the query based on the sort parameter.
+     *
+     * Fields can be prexied with a hyphen to sort descending.
+     *
+     * /endpoint?sort=field
+     * /endpoint?sort=field,anotherfield
+     * /endpoint?sort=-field
+     *
+     * @return $this
+     */
+    protected function sort($query)
+    {
+        if (! $sorts = $this->request->sort) {
+            return $this;
+        }
+
+        collect(explode(',', $sorts))
+            ->each(function ($sort) use ($query) {
+                $order = 'asc';
+
+                if (Str::startsWith($sort, '-')) {
+                    $sort = substr($sort, 1);
+                    $order = 'desc';
+                }
+
+                $query->orderBy($sort, $order);
+            });
+
+        return $this;
+    }
+
+    /**
+     * Gets a paginator, limited if requested by the limit paramter.
+     *
+     * /endpoint?limit=10
+     *
+     * @param \Statamic\Query\Builder $query
+     * @return \Statamic\Extensions\Pagination\LengthAwarePaginator
+     */
+    protected function paginate($query)
+    {
+        return $query
+            ->paginate($this->request->input('limit', 25))
+            ->appends($this->request->only(['filter', 'limit', 'page']));
+    }
+}
