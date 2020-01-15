0b453a2599101b26e3e9c830bbf8eac94a9194e9
Jason Varga <jason@pixelfear.com>
2020-01-15 10:41:08 -0500

    Use collection method

--- src/Tags/Dump.php
+++ src/Tags/Dump.php
@@ -2,13 +2,12 @@
 
 namespace Statamic\Tags;
 
-use Statamic\Support\Arr;
 use Statamic\Tags\Tags;
 
 class Dump extends Tags
 {
     public function index()
     {
-        dump(Arr::except($this->context->all(), ['__env', 'app']));
+        dump($this->context->except(['__env', 'app'])->all());
     }
 }
