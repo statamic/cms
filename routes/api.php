0d9c48c383ccadb2d388917d21ab976ed0cc56c2
Jesse Leite <jesseleite@gmail.com>
2020-01-16 11:29:52 -0500

    Tweak API routes.

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
-Route::resource('assets', 'AssetsController');
-Route::resource('collections/{collection}/entries', 'CollectionEntriesController');
+Route::resource('collections.entries', 'CollectionEntriesController');
+Route::resource('taxonomies.terms', 'TaxonomyTermsController');
 Route::resource('globals', 'GlobalsController');
-Route::resource('taxonomies/{taxonomy}/terms', 'TaxonomyTermsController');
 Route::resource('users', 'UsersController');
+
+Route::get('assets/{asset_container}', 'AssetsController@index');
+Route::get('assets/{asset_container}/{asset}', 'AssetsController@show')->where('asset', '.*');
