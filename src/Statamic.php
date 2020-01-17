249252de6aa7cdfceaa831dc05d88af52018c8b2
Jesse Leite <jesseleite@gmail.com>
2020-01-17 13:14:38 -0500

    Fix indentation.

--- src/Statamic.php
+++ src/Statamic.php
@@ -69,63 +69,63 @@ class Statamic
         return static::$styles;
     }
 
-   public static function style($name, $path)
-   {
-       static::$styles[$name] = str_finish($path, '.css');
+    public static function style($name, $path)
+    {
+        static::$styles[$name] = str_finish($path, '.css');
 
-       return new static;
-   }
+        return new static;
+    }
 
-   public static function pushWebRoutes(Closure $routes)
-   {
+    public static function pushWebRoutes(Closure $routes)
+    {
         static::$webRoutes[] = $routes;
 
         return new static;
-   }
+    }
 
-   public static function pushCpRoutes(Closure $routes)
-   {
+    public static function pushCpRoutes(Closure $routes)
+    {
         static::$cpRoutes[] = $routes;
 
         return new static;
-   }
+    }
 
-   public static function pushActionRoutes(Closure $routes)
-   {
+    public static function pushActionRoutes(Closure $routes)
+    {
         static::$actionRoutes[] = $routes;
 
         return new static;
-   }
+    }
 
-   public static function additionalCpRoutes()
-   {
+    public static function additionalCpRoutes()
+    {
         foreach (static::$cpRoutes as $routes) {
             $routes();
         }
-   }
+    }
 
-   public static function additionalWebRoutes()
-   {
+    public static function additionalWebRoutes()
+    {
         foreach (static::$webRoutes as $routes) {
             $routes();
         }
-   }
+    }
 
-   public static function additionalActionRoutes()
-   {
+    public static function additionalActionRoutes()
+    {
         foreach (static::$actionRoutes as $routes) {
             $routes();
         }
-   }
+    }
 
-   public static function isCpRoute()
-   {
-       if (! config('statamic.cp.enabled')) {
-           return false;
-       }
+    public static function isCpRoute()
+    {
+        if (! config('statamic.cp.enabled')) {
+            return false;
+        }
 
         return starts_with(request()->path(), config('statamic.cp.route'));
-   }
+    }
 
     public static function cpRoute($route, $params = [])
     {
@@ -143,14 +143,14 @@ class Statamic
         return $route;
     }
 
-   public static function isApiRoute()
-   {
-       if (! config('statamic.api.enabled')) {
-           return false;
-       }
+    public static function isApiRoute()
+    {
+        if (! config('statamic.api.enabled')) {
+            return false;
+        }
 
         return starts_with(request()->path(), config('statamic.api.route'));
-   }
+    }
 
     public static function apiRoute($route, $params = [])
     {
@@ -168,8 +168,8 @@ class Statamic
         return $route;
     }
 
-   public static function isAmpRequest()
-   {
+    public static function isAmpRequest()
+    {
         if (! config('statamic.amp.enabled')) {
             return false;
         }
@@ -179,7 +179,7 @@ class Statamic
         );
 
         return starts_with($url, '/' . config('statamic.amp.route'));
-   }
+    }
 
     public static function jsonVariables(Request $request)
     {
