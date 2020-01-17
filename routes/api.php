8366688fc7416c1caafee461e850e00af039d44c
Jesse Leite <jesseleite@gmail.com>
2020-01-17 11:25:10 -0500

    Add names to asset api routes.

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,5 @@ Route::resource('taxonomies.terms', 'TaxonomyTermsController');
 Route::resource('globals', 'GlobalsController');
 Route::resource('users', 'UsersController');
 
-Route::get('assets/{asset_container}', 'AssetsController@index');
-Route::get('assets/{asset_container}/{asset}', 'AssetsController@show')->where('asset', '.*');
+Route::name('assets.index')->get('assets/{asset_container}', 'AssetsController@index');
+Route::name('assets.show')->get('assets/{asset_container}/{asset}', 'AssetsController@show')->where('asset', '.*');
