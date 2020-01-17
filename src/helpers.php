9a43e5b5187a911566411a40d2e40d45323525f2
Jesse Leite <jesseleite@gmail.com>
2020-01-17 11:24:07 -0500

    Add `api_route()` helper.

--- src/helpers.php
+++ src/helpers.php
@@ -18,6 +18,22 @@ function cp_route($route, $params = [])
     return $route;
 }
 
+function api_route($route, $params = [])
+{
+    if (! config('statamic.api.enabled')) {
+        return null;
+    }
+
+    $route = route('statamic.api.' . $route, $params);
+
+    // TODO: This is a temporary workaround to routes like
+    // `route('assets.browse.edit', 'some/image.jpg')` outputting two slashes.
+    // Can it be fixed with route regex, or is it a laravel bug?
+    $route = preg_replace('/(?<!:)\/\//', '/', $route);
+
+    return $route;
+}
+
 function statamic_path($path = null)
 {
     return Path::tidy(__DIR__ . '/../' . $path);
