c9d8a7280c1c1c82386bb5dde25b70a27d14c570
Jason Varga <jason@pixelfear.com>
2020-01-20 13:24:26 -0500

    recursive augmenting when json-ing

--- tests/Fields/ValueTest.php
+++ tests/Fields/ValueTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Fields;
 
+use Statamic\Facades\Blueprint;
 use Statamic\Fields\Fieldtype;
 use Statamic\Fields\Value;
 use Tests\TestCase;
@@ -38,5 +39,48 @@ class ValueTest extends TestCase
         $value = new Value(['foo' => 'bar', 'baz' => 'qux'], null, $fieldtype);
 
         $this->assertEquals('{"foo":"BAR!","baz":"QUX!"}', json_encode($value));
+    }
+
+    /** @test */
+    function it_converts_to_json_and_augments_child_values()
+    {
+        $fieldtype = new class extends Fieldtype {
+            public function augment($data)
+            {
+                return array_map(function ($item) {
+                    return is_string($item) ? strtoupper($item) . '!' : $item;
+                }, $data);
+            }
+        };
+
+        $fieldtypeTwo = new class extends Fieldtype {
+            public function augment($data)
+            {
+                return new class($data) implements \Statamic\Contracts\Data\Augmentable {
+                    use \Statamic\Data\Augmentable;
+                    public function __construct($id) {
+                        $this->id = $id;
+                    }
+                    public function blueprint()
+                    {
+                        return Blueprint::make();
+                    }
+                    public function augmentedArrayData()
+                    {
+                        return [
+                            'id' => $this->id,
+                            'title' => 'Title for '.$this->id,
+                        ];
+                    }
+                };
+            }
+        };
+
+        $value = new Value([
+            'foo' => 'bar',
+            'baz' => new Value('123', null, $fieldtypeTwo)
+        ], null, $fieldtype);
+
+        $this->assertEquals('{"foo":"BAR!","baz":{"id":"123","title":"Title for 123"}}', json_encode($value));
     }
 }
