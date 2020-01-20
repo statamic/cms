5dfb871941c39f374bab1822e2d2835194ddd5f7
Jesse Leite <jesseleite@gmail.com>
2020-01-20 18:45:34 -0500

    Ensure resource gets localized global set variables.

--- src/Http/Controllers/API/GlobalsController.php
+++ src/Http/Controllers/API/GlobalsController.php
@@ -4,14 +4,17 @@ namespace Statamic\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use Statamic\Facades\GlobalSet;
+use Statamic\Facades\Site;
 use Statamic\Http\Resources\API\GlobalSetResource;
 
 class GlobalsController extends ApiController
 {
     public function index(Request $request)
     {
+        $site = Site::default()->handle();
+
         return app(GlobalSetResource::class)::collection(
-            GlobalSet::all()
+            GlobalSet::all()->map->in($site)
         );
     }
 
