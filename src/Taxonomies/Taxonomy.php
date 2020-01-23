42da5b9f4d5331086cd4ea0e3187c1627e9778c3
Jason Varga <jason@pixelfear.com>
2020-01-23 14:07:49 -0500

    If setting an empty array, just save nothing...
    Prevents an empty array being saved, which could result in a missing "create entry"/term button.
    
    No specified blueprints should result in falling back to the "default" blueprint.

--- src/Taxonomies/Taxonomy.php
+++ src/Taxonomies/Taxonomy.php
@@ -77,6 +77,9 @@ class Taxonomy implements Contract, Responsable
                     return Blueprint::find($blueprint);
                 });
             })
+            ->setter(function ($blueprints) {
+                return empty($blueprints) ? null : $blueprints;
+            })
             ->args(func_get_args());
     }
 
