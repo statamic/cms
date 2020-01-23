f6c65587cfb099195e9b347cb39f070a7dfd85e4
Jack McDade <jack@jackmcdade.com>
2020-01-23 12:00:50 -0500

    :sparkles: Option to filter out partials in template fieldtype

--- src/Fieldtypes/Template.php
+++ src/Fieldtypes/Template.php
@@ -13,11 +13,5 @@ class Template extends Fieldtype
             'width' => 50,
             'instructions' => 'Partials are rarely intended to be used as templates.'
         ],
-        'hide_error_templates' => [
-            'type' => 'toggle',
-            'default' => true,
-            'width' => 50,
-            'instructions' => 'Error templates are intended to be a last resort, not first pick.'
-        ],
     ];
 }
