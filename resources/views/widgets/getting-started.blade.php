4a92f08141a9471084a4364caf0a79f12ae0461a
Jack McDade <jack@jackmcdade.com>
2020-02-14 13:49:24 -0500

    :lipstick: Getting started widgets uses new ui style

--- resources/views/widgets/getting-started.blade.php
+++ resources/views/widgets/getting-started.blade.php
@@ -1,72 +1,48 @@
 <div class="card p-0 content">
-    <div class="bg-pink rounded-t px-3 py-2">
+    <div class="bg-blue-500 rounded-t px-3 py-2">
         <p class="font-bold text-white text-lg">Welcome to the Statamic 3 Beta!</p>
-        <p class="text-white text-sm">Remember: Statamic is paid, commercial software. After the beta you may need to purchase an appropriate license to continue using Statamic.</p>
+        <p class="text-white text-sm"><b>Statamic is commercial software:</b> you may need to purchase an appropriate license to continue using Statamic after the beta.</p>
     </div>
     <div class="p-3">
         <h1>{{ __('statamic::messages.getting_started_widget_header') }}</h1>
         <p>{{ __('statamic::messages.getting_started_widget_intro') }}</p>
     </div>
     <div class="flex flex-wrap">
-        <div class="w-full lg:w-1/2 p-3 border-t md:flex items-start">
+        <a href="https://statamic.dev" class="w-full lg:w-1/2 p-3 border-t lg:border-r md:flex items-start hover:bg-grey-10 group">
             <div class="h-8 w-8 mr-2 hidden md:block text-blue">
                 @svg('book-pages')
             </div>
             <div class="flex-1 mb-2 md:mb-0 md:mr-3">
-                <h3 class="mb-0">{{ __('Read the Documentation') }}</h3>
+                <h3 class="mb-1 text-blue group-hover:text-grey-80">{{ __('Read the Documentation') }} &rarr;</h3>
                 <p>{{ __('statamic::messages.getting_started_widget_docs') }}</p>
-                <a href="https://statamic.dev" class="text-blue text-sm font-bold">
-                    {{ __('Read the Docs') }} &rarr;
-                </a>
             </div>
-        </div>
-        <div class="w-full lg:w-1/2 p-3 border-t md:flex items-start">
+        </a>
+        <a href="{{ cp_route('collections.create') }}" class="w-full lg:w-1/2 p-3 border-t md:flex items-start hover:bg-grey-10 group">
             <div class="h-8 w-8 mr-2 hidden md:block text-blue">
                 @svg('content-writing')
             </div>
             <div class="flex-1 mb-2 md:mb-0 md:mr-3">
-                <h3 class="mb-0">{{ __('Collections') }}</h3>
+                <h3 class="mb-1 text-blue group-hover:text-grey-80">{{ __('Create a Collection') }} &rarr;</h3>
                 <p>{{ __('statamic::messages.getting_started_widget_collections') }}</p>
-                <a href="{{ cp_route('collections.create') }}" class="text-blue text-sm font-bold">
-                    {{ __('Create Collection') }} &rarr;
-                </a>
             </div>
-        </div>
-        <div class="w-full lg:w-1/2 p-3 border-t md:flex items-start">
+        </a>
+        <a href="{{ cp_route('blueprints.create') }}" class="w-full lg:w-1/2 p-3 border-t lg:border-r md:flex items-start hover:bg-grey-10 group">
             <div class="h-8 w-8 mr-2 hidden md:block text-blue">
                 @svg('blueprints')
             </div>
             <div class="flex-1 mb-2 md:mb-0 md:mr-3">
-                <h3 class="mb-0">{{ __('Blueprints') }}</h3>
+                <h3 class="mb-1 text-blue group-hover:text-grey-80">{{ __('Create a Blueprint') }} &rarr;</h3>
                 <p>{{ __('statamic::messages.getting_started_widget_blueprints') }}</p>
-                <a href="{{ cp_route('blueprints.create') }}" class="text-blue text-sm font-bold">
-                    {{ __('Create Blueprint') }} &rarr;
-                </a>
             </div>
-        </div>
-        <div class="w-full lg:w-1/2 p-3 border-t md:flex items-start">
+        </a>
+        <div class="w-full lg:w-1/2 p-3 border-t lg:border-r md:flex items-start hover:bg-grey-10 group">
             <div class="h-8 w-8 mr-2 hidden md:block text-blue">
                 @svg('structures')
             </div>
-            <div class="flex-1 mb-2 md:mb-0 md:mr-3">
-                <h3 class="mb-0">{{ __('Structures') }}</h3>
+            <a href="{{ cp_route('structures.create') }}" class="flex-1 mb-2 md:mb-0 md:mr-3">
+                <h3 class="mb-1 text-blue group-hover:text-grey-80">{{ __('Create a Structure') }} &rarr;</h3>
                 <p>{{ __('statamic::messages.getting_started_widget_structures') }}</p>
-                <a href="{{ cp_route('structures.create') }}" class="text-blue text-sm font-bold">
-                    {{ __('Create Structure') }} &rarr;
-                </a>
-            </div>
-        </div>
-        {{-- <div class="p-3 border-t md:flex items-center">
-            <div class="h-8 w-8 mr-2 hidden md:block text-blue">
-                @svg('addons')
-            </div>
-            <div class="flex-1 mb-2 md:mb-0 md:mr-3">
-                <h3 class="mb-0">__('Explore the Addon Marketplace') }}</h3>
-                <p>{{ __('statamic::messages.getting_started_widget_addons') }}</p>
-                <a href="{{ cp_route('addons.index') }}" class="btn-primary min-w-xs block">
-             __('Explore Addons') }}
             </a>
-            </div>
-        </div> --}}
+        </div>
     </div>
 </div>
