af6d2682c02a099c1f5b06dc7a6a0e73e57c16c3
Tom Diggelmann <diggelmann@novu.ch>
2020-02-28 13:49:06 +0100

    add download file handler on action response

--- resources/js/components/data-list/Actions.vue
+++ resources/js/components/data-list/Actions.vue
@@ -34,12 +34,29 @@ export default {
                 values
             };
 
-            this.$axios.post(this.url, payload).then(response => {
+            this.$axios.post(this.url, payload, { responseType: 'blob' }).then(response => {
                 this.$emit('completed');
+
+                if (response.headers['content-disposition']) {
+                    this.downloadFileFromResponse(response)
+                }
             }).catch(error => {
                 this.$toast.error(error.response.data.message);
                 this.$emit('completed');
             });
+        },
+
+        downloadFileFromResponse(response) {
+            const attachmentMatch = response.headers['content-disposition'].match(/^attachment.+filename="([^"]+)"/i)
+            if (attachmentMatch && attachmentMatch.length) {
+                const filename = attachmentMatch.length >= 2 ? attachmentMatch[1] : 'file.txt'
+                const url = window.URL.createObjectURL(new Blob([response.data]));
+                const link = document.createElement('a');
+                link.href = url;
+                link.setAttribute('download', filename);
+                document.body.appendChild(link);
+                link.click();
+            }
         }
 
     }
