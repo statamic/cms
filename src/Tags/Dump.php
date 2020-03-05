cce4ccc84df89abae92ed5eda2f0041a6bdb130c
Jesse Leite <jesseleite@gmail.com>
2020-03-05 15:26:33 -0500

    Add `dump:var` wildcard to cherry-pick a specific var.

--- src/Tags/Dump.php
+++ src/Tags/Dump.php
@@ -6,8 +6,19 @@ use Statamic\Tags\Tags;
 
 class Dump extends Tags
 {
+    /**
+     * {{ dump }}
+     */
     public function index()
     {
         dump($this->context->except(['__env', 'app'])->all());
+    }
+
+    /**
+     * {{ dump:* }}
+     */
+    public function wildcard($var)
+    {
+        dump($this->context->get($var));
     }
 }
