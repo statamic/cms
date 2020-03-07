46e918ce0c17de54486533cb52b9d08c092de55b
Jesse Leite <jesseleite@gmail.com>
2020-03-06 23:00:41 -0500

    Remove old/unused Fuse search functionality.

--- resources/js/components/data-list/DataList.vue
+++ resources/js/components/data-list/DataList.vue
@@ -59,9 +59,7 @@ export default {
     computed: {
 
         filteredRows() {
-            let rows = this.rows;
-            rows = this.filterBySearch(rows);
-            return this.sortRows(rows);
+            return this.sortRows(this.rows);
         },
 
         visibleColumns() {
@@ -114,19 +112,6 @@ export default {
             let firstVisibleColumn = this.visibleColumns[0];
             firstVisibleColumn = firstVisibleColumn ? firstVisibleColumn.field : columns[0].field;
             this.sharedState.sortColumn = this.sortColumn || (this.sort ? firstVisibleColumn : null);
-        },
-
-        filterBySearch(rows) {
-            if (!this.search || !this.searchQuery) return rows;
-
-            const fuse = new Fuse(rows, {
-                findAllMatches: true,
-                threshold: 0.1,
-                minMatchCharLength: 2,
-                keys: this.columns.length ? this.visibleColumns : Object.keys(rows[0])
-            });
-
-            return fuse.search(this.searchQuery);
         },
 
         sortRows(rows) {
