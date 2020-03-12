83779962ce047b9596753c4fe626a730269af5f4
Jesse Leite <jesseleite@gmail.com>
2020-03-12 18:42:33 -0400

    Add status filter.

---
+++ src/Query/Scopes/Filters/Status.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Statamic\Query\Scopes\Filters;
+
+use Illuminate\Support\Carbon;
+use Statamic\Facades\Collection;
+use Statamic\Query\Scopes\Filter;
+
+class Status extends Filter
+{
+    public $pinned = true;
+
+    public function fieldItems()
+    {
+        $options = collect([
+            'published' => __('Published'),
+            'scheduled' => __('Scheduled'),
+            'draft' => __('Draft'),
+        ]);
+
+        if (! $this->collection()->dated()) {
+            $options->forget('scheduled');
+        }
+
+        return [
+            'status' => [
+                'type' => 'radio',
+                'options' => $options->all(),
+            ],
+        ];
+    }
+
+    public function apply($query, $values)
+    {
+        if ($values['status'] === 'published') {
+            $query->where('published', true);
+        } elseif ($values['status'] === 'scheduled') {
+            $query->where('published', true)->where('date', '>', Carbon::now());
+        } elseif ($values['status'] === 'draft') {
+            $query->where('published', false);
+        }
+    }
+
+    public function visibleTo($key)
+    {
+        return $key === 'entries';
+    }
+
+    protected function collection()
+    {
+        return Collection::findByHandle($this->context['collection']);
+    }
+}
