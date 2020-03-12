7b3e4e405eb22d0c34f767a9a796fc037f42d796
Jack McDade <jack@jackmcdade.com>
2020-03-12 14:32:42 -0400

    :lipstick: Nicer pagination style

--- resources/js/components/data-list/Pagination.vue
+++ resources/js/components/data-list/Pagination.vue
@@ -4,23 +4,22 @@
 
         <div class="flex-1"></div>
 
-        <ul v-if="hasMultiplePages" class="flex justify-center items-center list-reset">
-            <li v-if="hasPrevious" class="mx-1">
-                <a @click.prevent="selectPreviousPage"><span>&laquo;</span></a>
+        <ul v-if="hasMultiplePages" class="pagination">
+            <li v-if="hasPrevious">
+                <a @click="selectPreviousPage"><span class="text-xs">&larr;</span></a>
             </li>
 
             <li
                 v-for="(page, i) in pages"
                 :key="i"
-                class="mx-1"
-                :class="{ 'font-bold': page == currentPage }"
+                :class="{ 'current': page == currentPage }"
             >
                 <span v-if="page === 'separator'">...</span>
-                <a v-else @click.prevent="selectPage(page)">{{ page }}</a>
+                <a v-else @click="selectPage(page)">{{ page }}</a>
             </li>
 
-            <li v-if="hasNext" class="mx-1">
-                <a @click.prevent="selectNextPage"><span>&raquo;</span></a>
+            <li v-if="hasNext">
+                <a @click="selectNextPage"><span class="text-xs">&rarr;</span></a>
             </li>
         </ul>
 
