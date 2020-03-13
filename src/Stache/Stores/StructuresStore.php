c97f65ea9beee472d49073e8c5859921ffa1a528
Jason Varga <jason@pixelfear.com>
2020-03-13 11:16:56 -0400

    Bring back old store class, deprecated, to prevent an error to ease migration

---
+++ src/Stache/Stores/StructuresStore.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Statamic\Stache\Stores;
+
+/**
+ * Exists only to ease migration from structures to navigation in beta 20
+ *
+ * @deprecated
+ **/
+class StructuresStore extends NavigationStore
+{
+    //
+}
