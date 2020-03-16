a6eb93a12864f33a9282403d3d144dceffc0e42a
Jack McDade <jack@jackmcdade.com>
2020-03-16 19:59:22 -0400

    :lipstick: Replace flat togglers with clicky togglers

--- resources/js/components/collections/View.vue
+++ resources/js/components/collections/View.vue
@@ -14,10 +14,10 @@
                 </dropdown-list>
 
                 <div class="btn-group mr-2" v-if="canUseStructureTree && !treeIsDirty">
-                    <button class="btn-flat px-2" @click="view = 'tree'" :class="{'active': view === 'tree'}">
+                    <button class="btn px-2" @click="view = 'tree'" :disabled="view === 'tree'">
                         <svg-icon name="structures" class="h-4 w-4"/>
                     </button>
-                    <button class="btn-flat px-2" @click="view = 'list'" :class="{'active': view === 'list'}">
+                    <button class="btn px-2" @click="view = 'list'" :disabled="view === 'list'">
                         <svg-icon name="assets-mode-table" class="h-4 w-4" />
                     </button>
                 </div>
