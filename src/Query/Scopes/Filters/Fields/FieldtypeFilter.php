fa9ba17c8c09593bdcac6b873ff9cb88f9d16c1d
Jesse Leite <jesseleite@gmail.com>
2020-03-19 11:37:10 -0400

    Add `FieldtypeFilter` subclass.

---
+++ src/Query/Scopes/Filters/Fields/FieldtypeFilter.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Statamic\Query\Scopes\Filters\Fields;
+
+use Statamic\Support\Arr;
+
+class FieldtypeFilter
+{
+    protected $fieldtype;
+
+    public function __construct($fieldtype)
+    {
+        $this->fieldtype = $fieldtype;
+    }
+
+    public function fieldItems()
+    {
+        return [
+            'operator' => [
+                'type' => 'select',
+                'options' => [
+                    '=' => __('Is'),
+                    '<>' => __('Isn\'t'),
+                    'like' => __('Contains'),
+                ]
+            ],
+            'value' => [
+                'type' => $this->fieldtype->field()->toPublishArray(),
+            ],
+        ];
+    }
+
+    public function apply($query, $handle, $values)
+    {
+        $operator = $values['operator'];
+        $value = $values['value'];
+
+        if ($operator === 'like') {
+            $value = Str::ensureLeft($value, '%');
+            $value = Str::ensureRight($value, '%');
+        }
+
+        $query->where($handle, $operator, $value);
+    }
+
+    public function badge($handle, $values)
+    {
+        $field = $this->fieldtype->field()->display();
+        $operator = $values['operator'];
+        $translatedOperator = Arr::get($this->fieldItems(), "operator.options.{$operator}");
+        $value = $values['value'];
+
+        return strtolower($field) . ' ' . strtolower($translatedOperator) . ' ' . $value;
+    }
+}
