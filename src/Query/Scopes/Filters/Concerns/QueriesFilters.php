3231d950215a54b5291ba3443b546b88c740757d
Jesse Leite <jesseleite@gmail.com>
2020-03-19 15:20:21 -0400

    When querying filters, return `badge` outside of each filter's `values`.

--- src/Query/Scopes/Filters/Concerns/QueriesFilters.php
+++ src/Query/Scopes/Filters/Concerns/QueriesFilters.php
@@ -18,20 +18,18 @@ trait QueriesFilters
         return collect($filters)
             ->map(function ($values, $handle) use ($context) {
                 return (object) [
-                    'filter' => Scope::find($handle, $context),
+                    'filterInstance' => Scope::find($handle, $context),
                     'values' => $values,
                 ];
             })
-            ->each(function ($operation) use ($query) {
-                $operation->filter->apply($query, $operation->values);
+            ->each(function ($activeFilter) use ($query) {
+                $activeFilter->filterInstance->apply($query, $activeFilter->values);
             })
-            ->map(function ($operation) {
-                return collect($operation->values)
-                    ->merge(['badge' => $operation->filter->badge($operation->values)])
-                    ->filter(function ($value, $key) {
-                        return $key === 'badge' ? $value : true;
-                    })
-                    ->all();
+            ->map(function ($activeFilter) {
+                return [
+                    'values' => $activeFilter->values,
+                    'badge' => $activeFilter->filterInstance->badge($activeFilter->values),
+                ];
             });
     }
 }
