c7a9c846c707668d478ada98262b834a3e1a979d
Jack McDade <jack@jackmcdade.com>
2020-03-30 11:30:00 -0400

    :iphone: Improve the global header on smaller breakpoints

--- resources/views/partials/global-header.blade.php
+++ resources/views/partials/global-header.blade.php
@@ -1,19 +1,19 @@
 <div class="global-header">
-    <div class="md:w-56 pl-3 h-full flex items-center">
-        <button class="nav-toggle ml-sm" @click="toggleNav">@svg('burger')</button>
+    <div class="lg:w-56 pl-1 md:pl-3 h-full flex items-center">
+        <button class="nav-toggle ml-sm flex-no-shrink" @click="toggleNav">@svg('burger')</button>
         <a href="{{ route('statamic.cp.index') }}" class="flex items-end">
-            <div v-tooltip="version" class="hidden md:block">
+            <div v-tooltip="version" class="hidden md:block flex-no-shrink">
                 @svg('statamic-wordmark')
             </div>
         </a>
     </div>
 
-    <div class="px-4 w-full flex-1 mx-auto" :class="wrapperClass">
+    <div class="sm:px-4 w-full flex-1 mx-auto" :class="wrapperClass">
         <global-search endpoint="{{ cp_route('search') }}" placeholder="{{ __('Search...') }}">
         </global-search>
     </div>
 
-    <div class="absolute pin-t pin-r head-link h-full md:pr-3 flex items-center">
+    <div class="lg:absolute pin-t pin-r head-link h-full md:pr-3 flex items-center">
 
         @if (Statamic\Facades\Site::hasMultiple())
             <site-selector>
@@ -55,7 +55,7 @@
         </a>
         <dropdown-list v-cloak>
             <template v-slot:trigger>
-                <a class="dropdown-toggle items-center ml-2 hide md:flex">
+                <a class="dropdown-toggle items-center ml-2 h-full hide flex">
                     @if ($user->avatar())
                         <div class="icon-header-avatar"><img src="{{ $user->avatar() }}" /></div>
                     @else
