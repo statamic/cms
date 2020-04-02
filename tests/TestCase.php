a3bb908aaf84db53cba651e77e3e6f189ab45e07
Jason Varga <jason@pixelfear.com>
2020-04-02 09:51:50 -0400

    Remove exception handler

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -13,7 +13,6 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
     protected function setUp(): void
     {
         require_once(__DIR__.'/ConsoleKernel.php');
-        require_once(__DIR__.'/ExceptionHandler.php');
 
         parent::setUp();
 
