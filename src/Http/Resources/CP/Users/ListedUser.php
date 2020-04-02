c5c543492e15d2dce3227d9846f93a21f18f8a1d
Jason Varga <jason@pixelfear.com>
2020-04-02 11:11:46 -0400

    Replace more deprecated resource class usages

--- src/Http/Resources/CP/Users/ListedUser.php
+++ src/Http/Resources/CP/Users/ListedUser.php
@@ -2,11 +2,11 @@
 
 namespace Statamic\Http\Resources\CP\Users;
 
-use Illuminate\Http\Resources\Json\Resource;
+use Illuminate\Http\Resources\Json\JsonResource;
 use Statamic\Facades\Action;
 use Statamic\Facades\User;
 
-class ListedUser extends Resource
+class ListedUser extends JsonResource
 {
     protected $blueprint;
     protected $columns;
