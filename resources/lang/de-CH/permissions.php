4bb8a359edbb6fe7d6be63df8f2947c081f30ebf
helloDanuk <hello@danielkaufmann.ch>
2020-04-09 17:50:31 +0200

    Brand new `de-CH` translation

---
+++ resources/lang/de-CH/permissions.php
@@ -0,0 +1,82 @@
+<?php
+
+return [
+
+    'super' => 'Super User',
+    'super_desc' => 'Super-Admins haben die vollständige Kontrolle und den Zugang auf alles im Control Panel. Erteile diese Rolle mit Verstand.',
+
+    'group_cp' => 'Control Panel',
+    'access_cp' => 'Zugriff aufs Control Panel',
+    'access_cp_desc' => 'Erteilt den Zugriff aufs Control Panel, garantiert aber nicht, dass darin etwas geändert werden kann.',
+    'configure_fields' => 'Felder konfigurieren',
+    'configure_fields_desc' => 'Ermöglicht die Bearbeitung von Blueprints, Fieldsets und deren Felder',
+
+    'group_collections' => 'Sammlungen',
+    'configure_collections' => 'Sammlungen konfigurieren',
+    'configure_collections_desc' => 'Erteilt Zugriffsrechte auf die Verwaltung der Sammlungen',
+    'view_{collection}_entries' => ':collection Einträge anzeigen',
+    'edit_{collection}_entries' => 'Einträge bearbeiten',
+    'create_{collection}_entries' => 'Einträge erstellen',
+    'delete_{collection}_entries' => 'Einträge löschen',
+    'publish_{collection}_entries' => 'Veröffentlichung verwalten',
+    'publish_{collection}_entries_desc' => 'Berechtigung vom Entwurf zur Veröffentlichung und umgekehrt zu wechseln',
+    'reorder_{collection}_entries' => 'Einträge neu anordnen',
+    'reorder_{collection}_entries_desc' => 'Aktiviert die Neuanordnung per Drag & Drop',
+
+    'group_taxonomies' => 'Taxonomien',
+    'configure_taxonomies' => 'Taxonomien konfigurieren',
+    'configure_taxonomies_desc' => 'Erteilt Zugriffrechte auf die Verwaltung der Taxonomien',
+    'view_{taxonomy}_terms' => ':taxonomy Begriffe anzeigen',
+    'edit_{taxonomy}_terms' => 'Begriffe bearbeiten',
+    'create_{taxonomy}_terms' => 'Neue Begriffe erstellen',
+    'delete_{taxonomy}_terms' => 'Begriffe löschen',
+    'publish_{taxonomy}_terms' => 'Veröffentlichung verwalten',
+    'reorder_{taxonomy}_terms' => 'Begriffe neu anordnen',
+
+    'group_navigation' => 'Navigation',
+    'configure_navs' => 'Navigation(en) bearbeiten',
+    'configure_navs_desc' => 'Erteilt Zugriffsrechte auf die Verwaltung der Navigation(en)',
+    'view_{nav}_nav' => ':nav Navigation anzeigen',
+    'edit_{nav}_nav' => 'Navigation bearbeiten',
+
+    'group_globals' => 'Gobale Inhalte',
+    'configure_globals' => 'Globale Sets konfigurieren',
+    'configure_globals_desc' => 'Erteilt Zugriffsrechte auf die Verwaltung der Globalen Sets',
+    'edit_{global}_globals' => ':global Inhalte bearbeiten',
+
+    'group_assets' => 'Dateien',
+    'configure_asset_containers' => 'Container konfigurieren',
+    'configure_asset_containers_desc' => 'Erteilt Zugriffsrechte auf die Verwaltung der Container',
+    'view_{container}_assets' => 'Dateien in «:container» anzeigen',
+    'upload_{container}_assets' => 'Neue Dateien hochladen',
+    'edit_{container}_assets' => 'Dateien bearbeiten',
+    'move_{container}_assets' => 'Dateien verschieben',
+    'rename_{container}_assets' => 'Dateien umbenennen',
+    'delete_{container}_assets' => 'Dateien löschen',
+
+    'group_forms' => 'Formulare',
+    'configure_forms' => 'Formulare konfigurieren',
+    'configure_forms_desc' => 'Erteilt Zugriffsrechte auf die Verwaltung der Formulare',
+    'view_{form}_form_submissions' => ':form Antworten anzeigen',
+    'delete_{form}_form_submissions' => ':form Antworten löschen',
+
+    'group_users' => 'Benutzer*innen',
+    'view_users' => 'Benutzer anzeigen',
+    'edit_users' => 'Benutzer bearbeiten',
+    'create_users' => 'Benutzer erstellen',
+    'delete_users' => 'Benutzer löschen',
+    'change_passwords' => 'Passwörter ändern',
+    'edit_user_groups' => 'Gruppen bearbeiten',
+    'edit_roles' => 'Rollen bearbeiten',
+
+    'group_updates' => 'Updates',
+    'view_updates' => 'Updates anzeigen',
+    'perform_updates' => 'Updates durchführen',
+
+    'group_utilities' => 'Hilfsmittel',
+    'access_utility' => ':title',
+    'access_utility_desc' => 'Zugriffsrechte auf das Hilfsmittel :title',
+
+    'group_misc' => 'Sonstiges',
+
+];
