9e3059deb23287b8a9422ff5da7dda28bd5b22f4
Duncan McClean <damcclean@users.noreply.github.com>
2020-04-17 17:18:37 +0100

    Range Tag (#1701)
    * Created the range tag
    
    * Add range tag to service provider
    
    * Made requested changes
    
    * Do some refactoring
    
    * Added the alias

---
+++ src/Tags/Range.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Statamic\Tags;
+
+use Statamic\Tags\Tags;
+
+class Range extends Tags
+{
+    protected static $aliases = ['loop'];
+
+    public function index()
+    {
+        $from   = $this->params->int('from', 1);
+        $to     = $this->params->int(['to', 'times']);
+        $vars = [];
+
+        foreach(range($from, $to) as $i) {
+            $vars[] = [
+                'value' => $i,
+            ];
+        }
+
+        return $vars;
+    }
+}
