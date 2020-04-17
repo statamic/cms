1c845a60c3e42c71a0cfd026e6cc995eff8577d4
Duncan McClean <duncan@mcclean.co.uk>
2020-04-17 15:41:22 +0100

    Add range tag to service provider

--- src/Providers/ExtensionServiceProvider.php
+++ src/Providers/ExtensionServiceProvider.php
@@ -133,6 +133,7 @@ class ExtensionServiceProvider extends ServiceProvider
         Tags\Partial::class,
         Tags\Path::class,
         Tags\Query::class,
+        Tags\Range::class,
         Tags\Redirect::class,
         Tags\Relate::class,
         Tags\Rotate::class,
