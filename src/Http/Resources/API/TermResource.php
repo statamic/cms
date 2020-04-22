a5a1d6233e07ff44e6cbd3ef2bca2d2ee5f7b854
Jason Varga <jason@pixelfear.com>
2020-04-22 15:34:53 -0400

    Fix taxonomy term augmentation in api

--- src/Http/Resources/API/TermResource.php
+++ src/Http/Resources/API/TermResource.php
@@ -14,6 +14,9 @@ class TermResource extends JsonResource
      */
     public function toArray($request)
     {
-        return $this->resource->toAugmentedCollection();
+        return $this->resource
+            ->toAugmentedCollection()
+            ->withShallowNesting()
+            ->toArray();
     }
 }
