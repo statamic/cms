afc4c61915c56b355eec48c00b21dcb596dad535
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2020-04-29 22:13:00 +0200

    Make your IDE love Statamic Collection, Compare, COnfig Data and Entry facades

--- src/Facades/Compare.php
+++ src/Facades/Compare.php
@@ -5,6 +5,13 @@ namespace Statamic\Facades;
 use Statamic\Support\Comparator;
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static int values($one, $two)
+ * @method static int strings(string $one, string $two)
+ * @method static int numbers($one, $two)
+ *
+ * @see \Statamic\Support\Comparator
+ */
 class Compare extends Facade
 {
     protected static function getFacadeAccessor()
--- src/Facades/Config.php
+++ src/Facades/Config.php
@@ -4,6 +4,24 @@ namespace Statamic\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static mixed get($key, $default = false)
+ * @method static mixed set($key, $value)
+ * @method static array all()
+ * @method static string getAppKey()
+ * @method static string|null getLicenseKey()
+ * @method static mixed getSite($locale = null)
+ * @method static string getFullLocale($locale = null)
+ * @method static string getShortLocale($locale = null)
+ * @method static string getLocaleName($locale = null)
+ * @method static array getLocales()
+ * @method static mixed getDefaultLocale()
+ * @method static array getOtherLocales($locale = null)
+ * @method static mixed getSiteUrl($locale = null)
+ * @method static array getImageManipulationPresets()
+ *
+ * @see \Statamic\Config
+ */
 class Config extends Facade
 {
     protected static function getFacadeAccessor()
