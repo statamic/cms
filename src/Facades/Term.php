de83dd102741dd04481169d43cdb69a5eec6c25a
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2020-04-29 23:42:07 +0200

    Make your IDE love Statamic Taxonomy, Term, URL, User, UserGroup, Utility and YAML facades

--- src/Facades/Term.php
+++ src/Facades/Term.php
@@ -5,6 +5,20 @@ namespace Statamic\Facades;
 use Illuminate\Support\Facades\Facade;
 use Statamic\Contracts\Taxonomies\TermRepository;
 
+/**
+ * @method static all()
+ * @method static whereTaxonomy(string $handle)
+ * @method static whereInTaxonomy(array $handles)
+ * @method static find($id)
+ * @method static findByUri(string $uri)
+ * @method static findBySlug(string $slug, string $collection)
+ * @method static make(string $slug = null)
+ * @method static query()
+ * @method static save($entry)
+ * @method static delete($entry)
+ *
+ * @see \Statamic\Contracts\Taxonomies\TermRepository
+ */
 class Term extends Facade
 {
     protected static function getFacadeAccessor()
--- src/Facades/Utility.php
+++ src/Facades/Utility.php
@@ -5,6 +5,16 @@ namespace Statamic\Facades;
 use Illuminate\Support\Facades\Facade;
 use Statamic\CP\Utilities\UtilityRepository;
 
+/**
+ * @method static mixed|null make($handle)
+ * @method static self push(Utility $utility)
+ * @method static mixed all()
+ * @method static mixed authorized()
+ * @method static mixed find($handle)
+ * @method static void routes()
+ *
+ * @see \Statamic\CP\Utilities\UtilityRepository
+ */
 class Utility extends Facade
 {
     protected static function getFacadeAccessor()
