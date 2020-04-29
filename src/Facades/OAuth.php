34cfe8f64542ceb31507be49d09eb3305accbbfe
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2020-04-29 23:05:27 +0200

    Make your IDE love Statamic Nav, OAuth, Parse, Path, Pattern, Permissions, Preference and Revision facades

--- src/Facades/OAuth.php
+++ src/Facades/OAuth.php
@@ -5,6 +5,13 @@ namespace Statamic\Facades;
 use Statamic\OAuth\Manager;
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static bool enabled()
+ * @method static mixed|\Statamic\OAuth\Provider Providerprovider($provider)
+ * @method static mixed providers()
+ *
+ * @see \Statamic\OAuth\Provider
+ */
 class OAuth extends Facade
 {
     protected static function getFacadeAccessor()
--- src/Facades/Preference.php
+++ src/Facades/Preference.php
@@ -5,6 +5,12 @@ namespace Statamic\Facades;
 use Statamic\Preferences\Preferences;
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static array all()
+ * @method static mixed get($key, $fallback = null)
+ *
+ * @see \Statamic\Preferences\Preferences
+ */
 class Preference extends Facade
 {
     protected static function getFacadeAccessor()
