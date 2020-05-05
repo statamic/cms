8f437b77850fded6761e1987bc40892d89ea44b8
Jason Varga <jason@pixelfear.com>
2020-05-05 17:43:37 -0400

    Link

--- resources/views/updater/partials/header.blade.php
+++ resources/views/updater/partials/header.blade.php
@@ -5,7 +5,7 @@
                 {{ $product ?? null }} Updates
             </h1>
             <p class="text-base text-grey-80 mb-3">From new features, enhancements, and performance improvements, to bug and security fixes, keeping Statamic up to date is an important part of maintaining your site. Updates are handled by <a href="https://getcomposer.org/" target="_blank">Composer</a> and may take anywhere from a few seconds to a few minutes to run.</p>
-            <p><a href="" class="font-bold text-blue">Learn more about updates in the docs</a><span class="inline-block text-blue w-4 h-4 ml-1">@svg('external-link')</span></p>
+            <p><a href="{{ Statamic::docsUrl('installation#updating') }}" class="font-bold text-blue">Learn more about updates in the docs</a><span class="inline-block text-blue w-4 h-4 ml-1">@svg('external-link')</span></p>
         </div>
         <div class="p-2 text-center">
             @svg('marketing/tooter-yay')
