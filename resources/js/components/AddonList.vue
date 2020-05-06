c458392939afa0d78ad94e5fbaa0f075463679cb
Jason Varga <jason@pixelfear.com>
2020-05-06 16:05:31 -0400

    Add inline

--- resources/js/components/AddonList.vue
+++ resources/js/components/AddonList.vue
@@ -19,7 +19,7 @@
                         <div class="h-64 rounded-t bg-cover" :style="'background-image: url(\''+getCover(addon)+'\')'"></div>
                         <div class="px-3 mb-2 relative text-center">
                             <a :href="addon.seller.website" class="relative">
-                                <img :src="addon.seller.avatar" :alt="addon.seller.name" class="rounded-full h-14 w-14 z-30 bg-white relative -mt-4 border-2 border-white">
+                                <img :src="addon.seller.avatar" :alt="addon.seller.name" class="rounded-full h-14 w-14 z-30 bg-white relative -mt-4 border-2 border-white inline">
                             </a>
                             <div class="addon-card-title mb-2 text-lg font-bold text-center">{{ addon.name }}</div>
                             <p v-text="addon.variants[0].summary" class="text-sm"></p>
