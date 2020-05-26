a22e39f787f9debb4ec609e2917aff1d530861d4
Jesse Leite <jesseleite@gmail.com>
2020-05-25 20:16:04 -0400

    Remove encrypted form params and handle redirects.

--- src/Http/Controllers/FormController.php
+++ src/Http/Controllers/FormController.php
@@ -4,13 +4,13 @@ namespace Statamic\Http\Controllers;
 
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\MessageBag;
 use Statamic\Contracts\Forms\Submission;
 use Statamic\Exceptions\PublishException;
 use Statamic\Exceptions\SilentFormFailureException;
 use Statamic\Facades\Form;
 use Statamic\Support\Arr;
+use Statamic\Support\Str;
 
 class FormController extends Controller
 {
@@ -21,25 +21,22 @@ class FormController extends Controller
      */
     public function submit($form)
     {
-        $fields = request()->all();
+        $data = request()->all();
 
-        if (! $params = request()->input('_params')) {
-            return response('Invalid request.', 400);
-        }
-
-        $params = Crypt::decrypt($params);
-        unset($fields['_params']);
-
-        $form = Form::find($handle);
+        $params = collect($data)
+            ->filter(function ($value, $key) {
+                return Str::startsWith($key, '_');
+            })
+            ->all();
 
         $submission = $form->createSubmission();
 
         if ($form->sanitize()) {
-            $fields = Arr::sanitize($fields);
+            $data = Arr::sanitize($data);
         }
 
         try {
-            $submission->data($fields);
+            $submission->data($data);
             $submission->uploadFiles();
 
             // Allow addons to prevent the submission of the form, return
@@ -81,11 +78,11 @@ class FormController extends Controller
             ]);
         }
 
-        $redirect = array_get($params, 'redirect');
+        $redirect = Arr::get($params, '_redirect');
 
-        $response = ($redirect) ? redirect($redirect) : back();
+        $response = $redirect ? redirect($redirect) : back();
 
-        session()->flash("form.{$submission->form()->handle()}.success", true);
+        session()->flash("form.{$submission->form()->handle()}.success", __('Submission successful.'));
         session()->flash('submission', $submission);
 
         return $response;
@@ -107,14 +104,11 @@ class FormController extends Controller
             ], 400);
         }
 
-        // Set up where to be taken in the event of an error.
-        if ($error_redirect = array_get($params, 'error_redirect')) {
-            $error_redirect = redirect($error_redirect);
-        } else {
-            $error_redirect = back();
-        }
+        $redirect = Arr::get($params, '_error_redirect');
 
-        return $error_redirect->withInput()->withErrors($errors, 'form.'.$form);
+        $response = $redirect ? redirect($redirect) : back();
+
+        return $response->withInput()->withErrors($errors, 'form.'.$form);
     }
 
     /**
