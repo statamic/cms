e7b0a65d1b758177786a893a89602ff20a810cdf
Jack McDade <jack@jackmcdade.com>
2020-05-27 14:48:58 -0400

    :lipstick: Slightly smaller h1s, slightly nicer keyboard shortcuts

--- resources/js/components/modals/KeyboardShortcutsModal.vue
+++ resources/js/components/modals/KeyboardShortcutsModal.vue
@@ -3,16 +3,16 @@
         <h1 class="p-2 bg-grey-20 border-b text-center">
             {{ __('Keyboard Shortcuts') }}
         </h1>
-        <div class="p-4 relative">
+        <div class="p-3 relative">
             <div class="shortcut-pair">
-                <span class="shortcut-key">
+                <span class="shortcut-combo">
                     <span class="shortcut">shift</span><span class="shortcut-joiner">+</span><span class="shortcut">?</span>
                 </span>
                 <span class="shortcut-value">{{ __('Show Keyboard Shortcuts') }}</span>
             </div>
 
             <div class="shortcut-pair">
-                <span class="shortcut-key">
+                <span class="shortcut-combo">
                     <span class="shortcut">/</span> <span class="shortcut-joiner">or</span>
                     <span class="shortcut">ctrl</span><span class="shortcut-joiner">+</span><span class="shortcut">f</span>
                 </span>
@@ -20,21 +20,21 @@
             </div>
 
             <div class="shortcut-pair">
-                <span class="shortcut-key">
-                    <span class="shortcut">cmd</span><span class="shortcut-joiner">+</span><span class="shortcut">s</span>
+                <span class="shortcut-combo">
+                    <span class="shortcut">⌘</span><span class="shortcut-joiner">+</span><span class="shortcut">s</span>
                 </span>
                 <span class="shortcut-value">{{ __('Save Content') }}</span>
             </div>
 
             <div class="shortcut-pair">
-                <span class="shortcut-key">
-                    <span class="shortcut">cmd</span><span class="shortcut-joiner">+</span><span class="shortcut">\</span>
+                <span class="shortcut-combo">
+                    <span class="shortcut">⌘</span><span class="shortcut-joiner">+</span><span class="shortcut">\</span>
                 </span>
                 <span class="shortcut-value">{{ __('Toggle Sidebar') }}</span>
             </div>
 
             <div class="shortcut-pair mb-0">
-                <span class="shortcut-key">
+                <span class="shortcut-combo">
                     <span class="shortcut">esc</span>
                 </span>
                 <span class="shortcut-value">{{ __('Close Modal') }}</span>
