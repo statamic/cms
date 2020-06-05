ec1e2644f41517cd4b31d8599b728ef32e779c9d
Jason Varga <jason@pixelfear.com>
2020-06-05 12:28:11 -0400

    Remove buttons that dont do anything

--- resources/views/addons/index.blade.php
+++ resources/views/addons/index.blade.php
@@ -5,8 +5,6 @@
 
     <div class="flex mb-3">
         <h1 class="flex-1">{{ __('Addons') }}</h1>
-        <a href="" class="btn">{{ __('Refresh') }}</a>
-        <a href="" class="btn-primary ml-2">{{ __('Connect to Your Account') }}</a>
     </div>
 
     <addon-list></addon-list>
