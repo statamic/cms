3e0604c85c69bf768c51436a8b6fad02a4a3c0c5
Jason Varga <jason@pixelfear.com>
2020-06-17 12:02:45 -0400

    Various noparse related things ...
    - An antlers parse returns an AntlersString, a thing wrapper allowing you to opt out of injecting noparse extractions.
    - Antlers::parse() etc will re-inject noparse extractions by default
    - Antlers::parseLoop() gets a AntlersLoop object that lets you opt out of injecting noparse too. It moves the parsing a little later, when you convert it to a string.
    - When tags parse/parseLoop, they don't inject noparse extractions. They'll leave it for the View to do that at the end.
    - Value objects with fields configured with antlers: false (the default) will have any antlers tags extracted (by adding @ before them, acting as noparses)
    - Context class doesn't parse values, it just gives back the value objects. The parser will deal with parsing.
    - Remove value method from Context class. It was unused.

--- src/Tags/Context.php
+++ src/Tags/Context.php
@@ -3,45 +3,13 @@
 namespace Statamic\Tags;
 
 use Statamic\Fields\Value;
-use Statamic\View\Antlers\Parser;
 
 class Context extends ArrayAccessor
 {
-    protected $parser;
-
-    public function setParser(Parser $parser)
-    {
-        $this->parser = $parser;
-
-        return $this;
-    }
-
-    public function get($key, $default = null)
-    {
-        $value = parent::get($key, $default);
-
-        if ($value instanceof Value) {
-            $value = $value->parseUsing($this->parser, $this->items)->value();
-        }
-
-        return $value;
-    }
-
     public function raw($key, $default = null)
     {
         $value = parent::get($key, $default);
 
         return $value instanceof Value ? $value->raw() : $value;
-    }
-
-    public function value($key, $default = null)
-    {
-        $value = parent::get($key, $default);
-
-        if (! $value instanceof Value) {
-            $value = new Value($value);
-        }
-
-        return $value->parseUsing($this->parser, $this->items);
     }
 }
