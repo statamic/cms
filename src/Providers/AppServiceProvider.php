809012899f1e0de60555aadfd28871b4fb0b2fae
Jason Varga <jason@pixelfear.com>
2020-06-25 15:25:43 -0400

    Publish the git config

--- src/Providers/AppServiceProvider.php
+++ src/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
     protected $root = __DIR__.'/../..';
 
     protected $configFiles = [
-        'amp', 'api', 'assets', 'cp', 'forms', 'live_preview', 'oauth', 'protect', 'revisions',
+        'amp', 'api', 'assets', 'cp', 'forms', 'git', 'live_preview', 'oauth', 'protect', 'revisions',
         'routes', 'search', 'static_caching', 'sites', 'stache', 'system', 'users',
     ];
 
