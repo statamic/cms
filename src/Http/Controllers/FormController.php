13fa8f3a8f4fb68e2a403318a3aa4bca87fbe157
Jesse Leite <jesseleite@gmail.com>
2020-07-03 11:56:00 -0400

    Add keyed `error` array to ajax form submission, to match `form:create` tag's `error` array.

--- src/Http/Controllers/FormController.php
+++ src/Http/Controllers/FormController.php
@@ -99,6 +99,9 @@ class FormController extends Controller
         if (request()->ajax()) {
             return response([
                 'errors' => (new MessageBag($errors))->all(),
+                'error' => collect($errors)->map(function ($errors, $field) {
+                    return $errors[0];
+                })->all(),
             ], 400);
         }
 
