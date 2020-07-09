b24fbdd71fc1aad04110e19e1e8f98915cb04808
Jason Varga <jason@pixelfear.com>
2020-07-09 09:55:28 -0400

    No breadcrumb on listing

--- resources/js/components/AddonList.vue
+++ resources/js/components/AddonList.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class="breadcrumb flex">
+        <div class="breadcrumb flex" v-if="showingAddon">
             <button
                 @click="showingAddon = false"
                 class="flex-initial flex p-1 -m-1 items-center text-xs text-grey-70 hover:text-grey-90"
