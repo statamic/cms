e9caec4346329940c4f3c7907fa86b45698c04e1
Jason Varga <jason@pixelfear.com>
2020-07-20 15:35:38 -0400

    Remove Saving abstract class

--- src/Events/EntrySaving.php
+++ src/Events/EntrySaving.php
@@ -2,12 +2,22 @@
 
 namespace Statamic\Events;
 
-class EntrySaving extends Saving
+class EntrySaving extends Event
 {
     public $entry;
 
     public function __construct($entry)
     {
         $this->entry = $entry;
+    }
+
+    /**
+     * Dispatch the event with the given arguments, and halt on first non-null listener response.
+     *
+     * @return mixed
+     */
+    public static function dispatch()
+    {
+        return event(new static(...func_get_args()), [], true);
     }
 }
--- src/Events/FormSubmitted.php
+++ src/Events/FormSubmitted.php
@@ -2,12 +2,22 @@
 
 namespace Statamic\Events;
 
-class FormSubmitted extends Saving
+class FormSubmitted extends Event
 {
     public $form;
 
     public function __construct($form)
     {
         $this->form = $form;
+    }
+
+    /**
+     * Dispatch the event with the given arguments, and halt on first non-null listener response.
+     *
+     * @return mixed
+     */
+    public static function dispatch()
+    {
+        return event(new static(...func_get_args()), [], true);
     }
 }
