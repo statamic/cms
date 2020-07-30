a9ae7932366361474164dc9082229d37babde0e2
Arno Hoogma <arno@hoog.ma>
2020-07-30 17:59:06 +0200

    Add params to GlideImageGenerated event

--- src/Events/GlideImageGenerated.php
+++ src/Events/GlideImageGenerated.php
@@ -5,9 +5,11 @@ namespace Statamic\Events;
 class GlideImageGenerated extends Event
 {
     public $path;
+    public $params;
 
-    public function __construct($path)
+    public function __construct($path, $params)
     {
         $this->path = $path;
+        $this->params = $params;
     }
 }
