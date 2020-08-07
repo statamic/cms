dcbdc888f2f0f0a146f22cfe8bbf35a47eece3e2
Jesse Leite <jesseleite@gmail.com>
2020-08-07 18:43:21 -0400

    Replace old `parameters` usages in `GetsQueryResults` trait.

--- src/Tags/Concerns/GetsQueryResults.php
+++ src/Tags/Concerns/GetsQueryResults.php
@@ -8,15 +8,15 @@ trait GetsQueryResults
     {
         $this->setPaginationParameterPrecedence();
 
-        if ($paginate = $this->parameters->get('paginate')) {
+        if ($paginate = $this->params->get('paginate')) {
             return $this->paginatedResults($query, $paginate);
         }
 
-        if ($limit = $this->parameters->get('limit')) {
+        if ($limit = $this->params->get('limit')) {
             $query->limit($limit);
         }
 
-        if ($offset = $this->parameters->get('offset')) {
+        if ($offset = $this->params->get('offset')) {
             $query->offset($offset);
         }
 
@@ -25,14 +25,14 @@ trait GetsQueryResults
 
     protected function setPaginationParameterPrecedence()
     {
-        if ($this->parameters->get('paginate') === true) {
-            $this->parameters->put('paginate', $this->parameters->get('limit'));
+        if ($this->params->get('paginate') === true) {
+            $this->params->put('paginate', $this->params->get('limit'));
         }
     }
 
     protected function paginatedResults($query, $perPage)
     {
-        if ($offset = $this->parameters->get('offset')) {
+        if ($offset = $this->params->get('offset')) {
             $this->queryPaginationFriendlyOffset($query, $offset);
         }
 
