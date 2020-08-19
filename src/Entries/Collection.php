8a99a5b5de6b9fa12d300f2e64f1deca6e164221
Jesse Leite <jesseleite@gmail.com>
2020-08-19 12:42:12 -0400

    Add collection/taxonomy as blueprint's parent for when creating entries/terms.

--- src/Entries/Collection.php
+++ src/Entries/Collection.php
@@ -214,7 +214,7 @@ class Collection implements Contract, AugmentableContract
         $blueprint = $blueprint ? $this->ensureEntryBlueprintFields($blueprint) : null;
 
         if ($blueprint) {
-            EntryBlueprintFound::dispatch($blueprint->setParent($entry), $entry);
+            EntryBlueprintFound::dispatch($blueprint->setParent($entry ?? $this), $entry);
         }
 
         return $blueprint;
--- src/Taxonomies/Taxonomy.php
+++ src/Taxonomies/Taxonomy.php
@@ -103,7 +103,7 @@ class Taxonomy implements Contract, Responsable, AugmentableContract
         $blueprint ? $this->ensureTermBlueprintFields($blueprint) : null;
 
         if ($blueprint) {
-            TermBlueprintFound::dispatch($blueprint->setParent($term), $term);
+            TermBlueprintFound::dispatch($blueprint->setParent($term ?? $this), $term);
         }
 
         return $blueprint;
