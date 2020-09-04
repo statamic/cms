statamic/cms

commit 6d361454b78183d58f7e10237c146cb3124081ff

Fix cascading data. Closes #1550

src/View/Antlers/Parser.php
@@ -595,9 +595,15 @@ class Parser
                     }
 
                     if (! empty($values)) {
-                        // parse the tag found with the value(s) related to it
-                        $tmpname = md5($name);
-                        $replacement = $this->parseVariables("{{ $tmpname }}$content{{ /$tmpname }}", [$tmpname => $values]);
+                        if (Arr::isAssoc($values)) {
+                            $replacement = $this->parse($content, array_merge($data, $values));
+                        } else {
+                            $values = $this->addLoopIterationVariables($values);
+                            $replacement = collect($values)
+                                ->map(function ($value) use ($content, $data) {
+                                    return (string) $this->parse($content, array_merge($data, $value));
+                                })->join('');
+                        }
                     }
                 } else {
                     // nope, this must be a callback

commit ea6759e332a0d7ebc0b2bbe0a15ec7b329317b13

Handle collections. Fixes issue introduced in 6d361454

src/View/Antlers/Parser.php
@@ -595,6 +595,10 @@ class Parser
                     }
 
                     if (! empty($values)) {
+                        if ($values instanceof Collection) {
+                            $values = $values->all();
+                        }
+
                         if (Arr::isAssoc($values)) {
                             $replacement = $this->parse($content, array_merge($data, $values));
                         } else {

tests/View/Antlers/ParserTest.php
@@ -521,6 +521,15 @@ EOT;
         $this->assertEquals('one', $this->parse($template, $this->variables));
     }
 
+    public function testArrayModifiersOnCollectionsGetParsed()
+    {
+        $template = '{{ simple limit="1" }}{{ value }}{{ /simple }}';
+
+        $this->assertEquals('one', $this->parse($template, [
+            'simple' => collect(['one', 'two', 'three']),
+        ]));
+    }
+
     public function testRecursiveChildren()
     {
         // the variables are inside RecursiveChildren@index