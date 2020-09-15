12433f481d0739654c190ac12ac0fe0720b54f17
Duncan McClean <duncan@mcclean.co.uk>
2020-09-15 07:40:09 +0100

    enable_cache_tags configuration option

--- config/system.php
+++ config/system.php
@@ -83,6 +83,19 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Enable Cache Tags
+    |--------------------------------------------------------------------------
+    |
+    | Sometimes you'll want to be able to disable the {{ cache }} tags in
+    | Antlers, so here is where you can do that. Otherwise, it will be
+    | enabled all the time.
+    |
+    */
+
+    'enable_cache_tags' => true,
+
+    /*
+    |--------------------------------------------------------------------------
     | Intensive Operations
     |--------------------------------------------------------------------------
     |
