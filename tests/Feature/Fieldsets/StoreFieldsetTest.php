c7b2e599d49892406d8164bc7235cac4a05d8380
Jack McDade <jack@jackmcdade.com>
2020-09-30 09:17:19 -0400

    ...and then ACTUALLY fix the test

--- tests/Feature/Fieldsets/StoreFieldsetTest.php
+++ tests/Feature/Fieldsets/StoreFieldsetTest.php
@@ -50,13 +50,12 @@ class StoreFieldsetTest extends TestCase
         $this
             ->actingAs($user)
             ->post(cp_route('fieldsets.store'), ['title' => 'Test', 'handle' => 'test'])
-            ->assertRedirect(cp_route('fieldsets.edit', 'test'))
+            ->assertJson(['redirect' => cp_route('fieldsets.edit', 'test')])
             ->assertSessionHas('success');
 
         $this->assertCount(1, Facades\Fieldset::all());
         $this->assertEquals([
             'title' => 'Test',
-            'handle' => 'test',
             'fields' => [],
         ], Facades\Fieldset::find('test')->contents());
     }
