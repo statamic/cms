8363448ed379663742b5296d34ad3387e1d41505
Luca <mail@lucakiehn.de>
2020-10-01 19:17:41 +0200

    fix test running without legacy-factories package

--- tests/Auth/Eloquent/EloquentUserTest.php
+++ tests/Auth/Eloquent/EloquentUserTest.php
@@ -2,8 +2,6 @@
 
 namespace Tests\Auth\Eloquent;
 
-use Faker\Generator as Faker;
-use Illuminate\Database\Eloquent\Factory;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Collection;
 use Statamic\Auth\Eloquent\User as EloquentUser;
@@ -14,10 +12,11 @@ use Tests\Auth\PermissibleContractTests;
 use Tests\Auth\UserContractTests;
 use Tests\Preferences\HasPreferencesTests;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
 
 class EloquentUserTest extends TestCase
 {
-    use UserContractTests, PermissibleContractTests, HasPreferencesTests;
+    use UserContractTests, PermissibleContractTests, HasPreferencesTests, WithFaker;
 
     public function setUp(): void
     {
@@ -29,16 +28,7 @@ class EloquentUserTest extends TestCase
 
         // TODO: The migration has been added into the test, but the implementation could be broken if the real
         // migration is different from what's in here. We should find a way to reference the actual migrations.
-        $this->loadMigrationsFrom(__DIR__.'/__migrations__');
-
-        app(Factory::class)->define(User::class, function (Faker $faker) {
-            return [
-                'name' => $faker->name,
-                'email' => $faker->unique()->safeEmail,
-                // 'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-                'remember_token' => str_random(10),
-            ];
-        });
+        $this->loadMigrationsFrom(__DIR__ . '/__migrations__');
     }
 
     /** @test */
@@ -98,7 +88,12 @@ class EloquentUserTest extends TestCase
     public function makeUser()
     {
         return (new EloquentUser)
-            ->model(factory(User::class)->create());
+            ->model(User::create([
+                'name' => $this->faker->name,
+                'email' => $this->faker->unique()->safeEmail,
+                // 'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+                'remember_token' => str_random(10),
+            ]));
     }
 
     public function createPermissible()
