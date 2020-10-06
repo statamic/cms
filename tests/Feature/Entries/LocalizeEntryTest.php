ad1c9b48119c254f68c397be9b13d41b481b3fd3
Jason Varga <jason@pixelfear.com>
2020-10-06 16:17:59 -0400

    Place entries in structure when localizing. Fixes #2471

--- tests/Feature/Entries/LocalizeEntryTest.php
+++ tests/Feature/Entries/LocalizeEntryTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Entries;
 
 use Facades\Tests\Factories\EntryFactory;
+use Statamic\Facades\Collection;
 use Statamic\Facades\Site;
 use Statamic\Facades\User;
 use Tests\FakesRoles;
@@ -58,6 +59,131 @@ class LocalizeEntryTest extends TestCase
             ->localize($entry, ['site' => ''])
             ->assertRedirect('/original')
             ->assertSessionHasErrors('site');
+    }
+
+    /** @test */
+    public function it_adds_an_entry_to_the_structure_tree_if_its_nested()
+    {
+        $collection = tap(Collection::make('pages')->sites(['en', 'fr']))->save();
+        $enHome = EntryFactory::collection($collection)->slug('home')->locale('en')->id('home')->create();
+        $fnHome = EntryFactory::collection($collection)->slug('home')->locale('fr')->id('fr-home')->origin('home')->create();
+        $enAbout = EntryFactory::collection($collection)->slug('about')->locale('en')->id('about')->create();
+        $frAbout = EntryFactory::collection($collection)->slug('about')->locale('fr')->id('fr-about')->origin('about')->create();
+        $enTeam = EntryFactory::collection($collection)->slug('team')->locale('en')->id('team')->create();
+        $this->assertNull($enTeam->in('fr'));
+        $collection->structureContents(['root' => true, 'tree' => [
+            'en' => [
+                ['entry' => 'home'],
+                ['entry' => 'about', 'children' => [
+                    ['entry' => 'team'],
+                ]],
+            ],
+            'fr' => [
+                ['entry' => 'fr-home'],
+                ['entry' => 'fr-about'],
+            ],
+        ]])->save();
+
+        $this->assertEquals([
+            ['entry' => 'fr-home'],
+            ['entry' => 'fr-about'],
+        ], Collection::findByHandle('pages')->structure()->in('fr')->tree());
+
+        $this
+            ->actingAs($this->user())
+            ->localize($enTeam, ['site' => 'fr'])
+            ->assertOk();
+
+        $frTeam = $enTeam->fresh()->in('fr');
+        $this->assertNotNull($frTeam);
+
+        $this->assertEquals([
+            ['entry' => 'fr-home'],
+            ['entry' => 'fr-about', 'children' => [
+                ['entry' => $frTeam->id()],
+            ]],
+        ], Collection::findByHandle('pages')->structure()->in('fr')->tree());
+    }
+
+    /** @test */
+    public function it_adds_an_entry_to_the_end_of_the_structure_tree_if_the_parent_is_the_root()
+    {
+        $collection = tap(Collection::make('pages')->sites(['en', 'fr']))->save();
+        $enHome = EntryFactory::collection($collection)->slug('home')->locale('en')->id('home')->create();
+        $fnHome = EntryFactory::collection($collection)->slug('home')->locale('fr')->id('fr-home')->origin('home')->create();
+        $enAbout = EntryFactory::collection($collection)->slug('about')->locale('en')->id('about')->create();
+        $frAbout = EntryFactory::collection($collection)->slug('about')->locale('fr')->id('fr-about')->origin('about')->create();
+        $enTeam = EntryFactory::collection($collection)->slug('team')->locale('en')->id('team')->create();
+        $this->assertNull($enTeam->in('fr'));
+        $collection->structureContents(['root' => true, 'tree' => [
+            'en' => [
+                ['entry' => 'home'],
+                ['entry' => 'about'],
+                ['entry' => 'team'],
+            ],
+            'fr' => [
+                ['entry' => 'fr-home'],
+                ['entry' => 'fr-about'],
+            ],
+        ]])->save();
+
+        $this->assertEquals([
+            ['entry' => 'fr-home'],
+            ['entry' => 'fr-about'],
+        ], Collection::findByHandle('pages')->structure()->in('fr')->tree());
+
+        $this
+            ->actingAs($this->user())
+            ->localize($enTeam, ['site' => 'fr'])
+            ->assertOk();
+
+        $frTeam = $enTeam->fresh()->in('fr');
+        $this->assertNotNull($frTeam);
+
+        $this->assertEquals([
+            ['entry' => 'fr-home'],
+            ['entry' => 'fr-about'],
+            ['entry' => $frTeam->id()],
+        ], Collection::findByHandle('pages')->structure()->in('fr')->tree());
+    }
+
+    /** @test */
+    public function it_adds_an_entry_to_the_end_of_the_structure_tree_if_the_parent_doesnt_exist_in_that_site()
+    {
+        $collection = tap(Collection::make('pages')->sites(['en', 'fr']))->save();
+        $enHome = EntryFactory::collection($collection)->slug('home')->locale('en')->id('home')->create();
+        $fnHome = EntryFactory::collection($collection)->slug('home')->locale('fr')->id('fr-home')->origin('home')->create();
+        $enAbout = EntryFactory::collection($collection)->slug('about')->locale('en')->id('about')->create();
+        $enTeam = EntryFactory::collection($collection)->slug('team')->locale('en')->id('team')->create();
+        $this->assertNull($enTeam->in('fr'));
+        $collection->structureContents(['root' => true, 'tree' => [
+            'en' => [
+                ['entry' => 'home'],
+                ['entry' => 'about', 'children' => [
+                    ['entry' => 'team'],
+                ]],
+            ],
+            'fr' => [
+                ['entry' => 'fr-home'],
+            ],
+        ]])->save();
+
+        $this->assertEquals([
+            ['entry' => 'fr-home'],
+        ], Collection::findByHandle('pages')->structure()->in('fr')->tree());
+
+        $this
+            ->actingAs($this->user())
+            ->localize($enTeam, ['site' => 'fr'])
+            ->assertOk();
+
+        $frTeam = $enTeam->fresh()->in('fr');
+        $this->assertNotNull($frTeam);
+
+        $this->assertEquals([
+            ['entry' => 'fr-home'],
+            ['entry' => $frTeam->id()],
+        ], Collection::findByHandle('pages')->structure()->in('fr')->tree());
     }
 
     private function localize($entry, $params = [])
