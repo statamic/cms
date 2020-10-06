43465b93b6c35eabf08dc72a018a76911d74a60a
Duncan McClean <duncan@mcclean.co.uk>
2020-10-06 21:33:49 +0100

    Automatically set an app key and set the user repository to file.

--- src/Testing/TestCase.php
+++ src/Testing/TestCase.php
@@ -2,9 +2,11 @@
 
 namespace Statamic\Testing;
 
+use Illuminate\Encryption\Encrypter;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
 use Statamic\Extend\Manifest;
 use Statamic\Providers\StatamicServiceProvider;
+use Statamic\Stache\Stores\UsersStore;
 use Statamic\Statamic;
 
 abstract class TestCase extends OrchestraTestCase
@@ -49,5 +51,11 @@ abstract class TestCase extends OrchestraTestCase
         foreach ($configs as $config) {
             $app['config']->set("statamic.$config", require(__DIR__."/../../config/{$config}.php"));
         }
+
+        $app['config']->set('app.key', 'base64:'.base64_encode(
+            Encrypter::generateKey($app['config']['app.cipher'])
+        ));
+
+        $app['config']->set('statamic.users.repository', 'file');
     }
 }
