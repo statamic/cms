67b87c5fdc0fe331ab62a8bb1ba654491556ea67
Jacob Pilon <piljac1@hotmail.com>
2020-10-09 23:12:05 -0400

    Clean up unit tests

--- tests/Query/BuilderTest.php
+++ tests/Query/BuilderTest.php
@@ -10,18 +10,21 @@ class BuilderTest extends TestCase
 {
     use PreventSavingStacheItemsToDisk;
 
+    private function createDummyCollectionAndEntries()
+    {
+        Collection::make('posts')->save();
+
+        EntryFactory::id('1')->slug('post-1')->collection('posts')->data(['title' => 'Post 1', 'author' => 'John Doe'])->create();
+        $entry = EntryFactory::id('2')->slug('post-2')->collection('posts')->data(['title' => 'Post 2', 'author' => 'John Doe'])->create();
+        EntryFactory::id('3')->slug('post-3')->collection('posts')->data(['title' => 'Post 3', 'author' => 'John Doe'])->create();
+
+        return $entry;
+    }
+
     /** @test **/
     public function entry_is_found_within_all_created_entries_using_entry_facade_with_find_method()
     {
-        Collection::make('posts')->structureContents([
-            'root' => true,
-            'tree' => [['entry' => '2']],
-        ])->save();
-
-        EntryFactory::id('1')->slug('post-1')->collection('posts')->data(['title' => 'Post 1', 'author' => 'John Doe'])->create();
-        $searchedEntry = EntryFactory::id('2')->slug('post-2')->collection('posts')->data(['title' => 'Post 2', 'author' => 'John Doe'])->create();
-        EntryFactory::id('3')->slug('post-3')->collection('posts')->data(['title' => 'Post 3', 'author' => 'John Doe'])->create();
-
+        $searchedEntry = $this->createDummyCollectionAndEntries();
         $retrievedEntry = Entry::query()->find($searchedEntry->id());
 
         $this->assertSame($searchedEntry, $retrievedEntry);
@@ -30,17 +33,8 @@ class BuilderTest extends TestCase
     /** @test **/
     public function entry_is_found_within_all_created_entries_and_select_query_columns_are_set_using_entry_facade_with_find_method_with_columns_param()
     {
-        Collection::make('posts')->structureContents([
-            'root' => true,
-            'tree' => [['entry' => '2']],
-        ])->save();
-
-        EntryFactory::id('1')->slug('post-1')->collection('posts')->data(['title' => 'Post 1', 'author' => 'John Doe'])->create();
-        $searchedEntry = EntryFactory::id('2')->slug('post-2')->collection('posts')->data(['title' => 'Post 2', 'author' => 'John Doe'])->create();
-        EntryFactory::id('3')->slug('post-3')->collection('posts')->data(['title' => 'Post 3', 'author' => 'John Doe'])->create();
-
+        $searchedEntry = $this->createDummyCollectionAndEntries();
         $columns = ['title'];
-
         $retrievedEntry = Entry::query()->find($searchedEntry->id(), $columns);
 
         $this->assertSame($searchedEntry, $retrievedEntry);
