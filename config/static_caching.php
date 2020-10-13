053978a6392517629fa21f8ae95a9119f8fc4f0c
Jonas Siewertsen <mail@jonassiewertsen.com>
2020-10-13 23:29:53 +0200

    Make statamic caching strategies available in your .env file (#2648)

--- config/static_caching.php
+++ config/static_caching.php
@@ -12,7 +12,7 @@ return [
     |
     */
 
-    'strategy' => null,
+    'strategy' => env('STATAMIC_STATIC_CACHING_STRATEGY', null),
 
     /*
     |--------------------------------------------------------------------------
