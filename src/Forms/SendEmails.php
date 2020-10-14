a1c200a67bd35f5952920e33f64a96b902cbffad
Jason Varga <jason@pixelfear.com>
2020-10-14 16:41:05 -0400

    Pass the locale into the mailable

--- src/Forms/SendEmails.php
+++ src/Forms/SendEmails.php
@@ -17,9 +17,12 @@ class SendEmails implements ShouldQueue
 
     protected $submission;
 
-    public function __construct(Submission $submission)
+    protected $locale;
+
+    public function __construct(Submission $submission, $locale)
     {
         $this->submission = $submission;
+        $this->locale = $locale;
     }
 
     /**
@@ -30,7 +33,7 @@ class SendEmails implements ShouldQueue
     public function handle()
     {
         $this->parseEmailConfigs($this->submission)->each(function ($config) {
-            Mail::send(new Email($this->submission, $config));
+            Mail::send((new Email($this->submission, $config))->locale($this->locale));
         });
     }
 
