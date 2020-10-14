4ea69afc0153530d6620c1b9a0d819f7fa2f620b
Jason Varga <jason@pixelfear.com>
2020-10-14 17:13:25 -0400

    test locale gets set

--- tests/Forms/EmailTest.php
+++ tests/Forms/EmailTest.php
@@ -14,19 +14,36 @@ class EmailTest extends TestCase
     /** @test */
     public function it_sets_html_view_as_string()
     {
-        /** @var Submission */
-        $submission = m::mock(Submission::class);
-        $submission->shouldReceive('toArray')->andReturn([]);
-        $submission->shouldReceive('toAugmentedArray')->andReturn([]);
-
-        $email = new Email($submission, [
-            'to' => Antlers::parse('test@example.com'),
-            'html' => Antlers::parse('emails/test'),
-        ], Site::default());
+        $email = $this->makeEmail();
 
         $email->build();
 
         $this->assertTrue(is_string($email->view), 'View is not a string.');
         $this->assertEquals('emails/test', $email->view);
+    }
+
+    /** @test */
+    public function the_sites_locale_gets_used_on_the_mailable()
+    {
+        Site::setConfig(['sites' => [
+            'one' => ['locale' => 'en_US', 'url' => '/one'],
+            'two' => ['locale' => 'fr_Fr', 'url' => '/two'],
+        ]]);
+
+        $this->assertEquals('en', $this->makeEmail(Site::get('one'))->locale);
+        $this->assertEquals('fr', $this->makeEmail(Site::get('two'))->locale);
+    }
+
+    private function makeEmail($site = null)
+    {
+        /** @var Submission */
+        $submission = m::mock(Submission::class);
+        $submission->shouldReceive('toArray')->andReturn([]);
+        $submission->shouldReceive('toAugmentedArray')->andReturn([]);
+
+        return new Email($submission, [
+            'to' => Antlers::parse('test@example.com'),
+            'html' => Antlers::parse('emails/test'),
+        ], $site ?? Site::default());
     }
 }
