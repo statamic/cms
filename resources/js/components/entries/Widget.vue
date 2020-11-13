a3769af89d94620014d489ef46ddd1c6c38ddc3e
Jesse Leite <jesseleite@gmail.com>
2020-11-13 16:11:11 -0500

    Show better empty state when entries don't exist in selected site.

--- resources/js/components/entries/Widget.vue
+++ resources/js/components/entries/Widget.vue
@@ -5,7 +5,7 @@
         </div>
 
         <data-list
-            v-if="!initializing"
+            v-if="!initializing && items.length"
             :rows="items"
             :columns="cols"
             :sort="false"
@@ -29,6 +29,10 @@
                 />
             </div>
         </data-list>
+
+        <p v-else-if="!initializing && !items.length" class="p-2 pt-1 text-sm text-grey-50">
+            {{ __('There are no entries in this site') }}
+        </p>
 
     </div>
 </template>
