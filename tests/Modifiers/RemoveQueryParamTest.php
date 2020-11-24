14e7c2ccf6e0e29848dffe1fe822254a505dee99
Jacob Pilon <piljac1@hotmail.com>
2020-11-24 18:17:43 -0500

    Additional unit tests

--- tests/Modifiers/RemoveQueryParamTest.php
+++ tests/Modifiers/RemoveQueryParamTest.php
@@ -14,10 +14,14 @@ class RemoveQueryParamTest extends TestCase
     public function it_removes_an_existing_query_param()
     {
         // Removal of a simple query param
+        $this->assertSame($this->baseUrl, $this->modify("{$this->baseUrl}?q", $this->queryParamKey));
         $this->assertSame($this->baseUrl, $this->modify("{$this->baseUrl}?q=statamic", $this->queryParamKey));
         $this->assertSame("{$this->baseUrl}#test", $this->modify("{$this->baseUrl}?q=statamic#test", $this->queryParamKey));
+        $this->assertSame("{$this->baseUrl}?sourceid=chrome", $this->modify("{$this->baseUrl}?q&sourceid=chrome", $this->queryParamKey));
         $this->assertSame("{$this->baseUrl}?sourceid=chrome", $this->modify("{$this->baseUrl}?q=statamic&sourceid=chrome", $this->queryParamKey));
         $this->assertSame("{$this->baseUrl}?sourceid=chrome", $this->modify("{$this->baseUrl}?sourceid=chrome&q=statamic", $this->queryParamKey));
+        $this->assertSame("{$this->baseUrl}?a[0]=a&b[0][0]=b&q[0][test]=statamic&q[0][0]=test1&q[0][1]=test2&z[0][0]=z", $this->modify("{$this->baseUrl}?a[0]=a&b[0][0]=b&q[0][test]=statamic&q[0][0]=test1&q[0][1]=test2&x=test&z[0][0]=z", 'x'));
+        $this->assertSame("{$this->baseUrl}?a[0]=a&b[0][0]=b&q[0][test]=statamic&q[0][0]=test1&q[0][1]=test2&z[0][0]=z", $this->modify("{$this->baseUrl}?a[0]=a&b[0][0]=b&q[0][test]=statamic&q[0][0]=test1&q[0][1]=test2&x=test&z[0][0]=z", ['x', true]));
 
         // Removal of an array query param WITH reordering
         $this->assertSame($this->baseUrl, $this->modify("{$this->baseUrl}?q[0]=statamic", ['q[0]', true]));
@@ -25,6 +29,7 @@ class RemoveQueryParamTest extends TestCase
         $this->assertSame("{$this->baseUrl}?q[0][0]=statamic&q[0][1]=test2", $this->modify("{$this->baseUrl}?q[0][0]=statamic&q[0][1]=test1&q[0][2]=test2", ['q[0][1]', true]));
         $this->assertSame("{$this->baseUrl}?q[0][test]=statamic&q[0][0]=test2", $this->modify("{$this->baseUrl}?q[0][test]=statamic&q[0][0]=test1&q[0][1]=test2", ['q[0][0]', true]));
         $this->assertSame("{$this->baseUrl}?a[0]=a&b[0][0]=b&q[0][test]=statamic&q[0][0]=test2&z[0][0]=z", $this->modify("{$this->baseUrl}?a[0]=a&b[0][0]=b&q[0][test]=statamic&q[0][0]=test1&q[0][1]=test2&z[0][0]=z", ['q[0][0]', true]));
+        $this->assertSame("{$this->baseUrl}?a[0]=a&b[0][0]=b&q[0][test]=statamic&q[0][0]=test2&x=test&z[0][0]=z", $this->modify("{$this->baseUrl}?a[0]=a&b[0][0]=b&q[0][test]=statamic&q[0][0]=test1&q[0][1]=test2&x=test&z[0][0]=z", ['q[0][0]', true]));
 
         // Removal of an array query param WITHOUT reordering
         $this->assertSame($this->baseUrl, $this->modify("{$this->baseUrl}?q[0]=statamic", 'q[0]'));
@@ -32,6 +37,7 @@ class RemoveQueryParamTest extends TestCase
         $this->assertSame("{$this->baseUrl}?q[0][0]=statamic&q[0][2]=test2", $this->modify("{$this->baseUrl}?q[0][0]=statamic&q[0][1]=test1&q[0][2]=test2", 'q[0][1]'));
         $this->assertSame("{$this->baseUrl}?q[0][test]=statamic&q[0][1]=test2", $this->modify("{$this->baseUrl}?q[0][test]=statamic&q[0][0]=test1&q[0][1]=test2", 'q[0][0]'));
         $this->assertSame("{$this->baseUrl}?a[0]=a&b[0][0]=b&q[0][test]=statamic&q[0][1]=test2&z[0][0]=z", $this->modify("{$this->baseUrl}?a[0]=a&b[0][0]=b&q[0][test]=statamic&q[0][0]=test1&q[0][1]=test2&z[0][0]=z", 'q[0][0]'));
+        $this->assertSame("{$this->baseUrl}?a[0]=a&b[0][0]=b&q[0][test]=statamic&q[0][1]=test2&x=test&z[0][0]=z", $this->modify("{$this->baseUrl}?a[0]=a&b[0][0]=b&q[0][test]=statamic&q[0][0]=test1&q[0][1]=test2&x=test&z[0][0]=z", 'q[0][0]'));
     }
 
     /** @test */
@@ -41,6 +47,7 @@ class RemoveQueryParamTest extends TestCase
         $this->assertSame("{$this->baseUrl}#test", $this->modify("{$this->baseUrl}#test", $this->queryParamKey));
         $this->assertSame("{$this->baseUrl}?sourceid=chrome", $this->modify("{$this->baseUrl}?sourceid=chrome", $this->queryParamKey));
         $this->assertSame("{$this->baseUrl}?q[0]=statamic", $this->modify("{$this->baseUrl}?q[0]=statamic", 'q[1]'));
+        $this->assertSame("{$this->baseUrl}?q[1]=statamic", $this->modify("{$this->baseUrl}?q[1]=statamic", ['q[2]', true]));
     }
 
     /** @test */
