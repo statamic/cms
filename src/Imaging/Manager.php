df4191563b400e993bb0518d62b5d8c1fd9a847f
Jason Varga <jason@pixelfear.com>
2020-11-24 13:05:00 -0500

    Remove toenail thumbnail references, we no longer use them.

--- src/Imaging/Manager.php
+++ src/Imaging/Manager.php
@@ -52,7 +52,6 @@ class Manager
             'cp_thumbnail_small_landscape' => ['w' => '400', 'h' => '300', 'fit' => 'crop'],
             'cp_thumbnail_small_portrait' => ['h' => '300', 'fit' => 'crop'],
             'cp_thumbnail_small_square' => ['w' => '300', 'h' => '300'],
-            'cp_thumbnail_large' => ['w' => '1000', 'h' => '1000'],
         ];
     }
 }
