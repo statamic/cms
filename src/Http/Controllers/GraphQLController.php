2c6d86dabe2bd654dfe8b2587733da003e8c08ed
Jason Varga <jason@pixelfear.com>
2020-11-25 16:44:11 -0500

    GraphQL route and controller (that does nothing yet)

---
+++ src/Http/Controllers/GraphQLController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Statamic\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GraphQLController
+{
+    public function index(Request $request)
+    {
+        return 'ok';
+    }
+}
