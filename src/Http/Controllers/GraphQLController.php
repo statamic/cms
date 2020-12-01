f29e8cca24e761f4258b85ed9a30cdae5c3df44f
Jason Varga <jason@pixelfear.com>
2020-12-01 15:43:31 -0500

    Hook up most basic ping pong graphql request and response

--- src/Http/Controllers/GraphQLController.php
+++ src/Http/Controllers/GraphQLController.php
@@ -2,12 +2,35 @@
 
 namespace Statamic\Http\Controllers;
 
+use GraphQL\GraphQL;
+use GraphQL\Type\Definition\ObjectType;
+use GraphQL\Type\Definition\Type;
+use GraphQL\Type\Schema;
 use Illuminate\Http\Request;
 
 class GraphQLController
 {
     public function index(Request $request)
     {
-        return 'ok';
+        $result = GraphQL::executeQuery($this->schema(), $request->input('query'));
+
+        return $result->toArray();
+    }
+
+    protected function schema()
+    {
+        return new Schema([
+            'query' => new ObjectType([
+                'name' => 'Query',
+                'fields' => [
+                    'ping' => [
+                        'type' => Type::string(),
+                        'resolve' => function () {
+                            return 'pong';
+                        },
+                    ],
+                ],
+            ]),
+        ]);
     }
 }
