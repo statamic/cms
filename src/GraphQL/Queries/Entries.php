fa0a032c07984fcca235c63c289cb9024dd3a364
Jason Varga <jason@pixelfear.com>
2020-12-02 09:50:47 -0500

    Don't need it as an array

--- src/GraphQL/Queries/Entries.php
+++ src/GraphQL/Queries/Entries.php
@@ -25,7 +25,7 @@ class Entries
                     $query->whereIn('collection', $collection);
                 }
 
-                return $query->get()->all();
+                return $query->get();
             },
         ];
     }
