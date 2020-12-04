a9b4e0d0d4328154e1f8ad861844535f366c4fd9
Jelle Roorda <9193686+jelleroorda@users.noreply.github.com>
2020-12-04 17:29:27 +0100

    Fix 500 errors for an empty search string.
    When you search for a space or other whitespace in the CP, a 500 error would be returned. This solves it, by gracefully returning an empty result set.

--- src/Http/Controllers/CP/SearchController.php
+++ src/Http/Controllers/CP/SearchController.php
@@ -5,26 +5,31 @@ namespace Statamic\Http\Controllers\CP;
 use Illuminate\Http\Request;
 use Statamic\Facades\Search;
 use Statamic\Facades\User;
+use Statamic\Search\Comb\Exceptions\NoQuery;
 
 class SearchController extends CpController
 {
     public function __invoke(Request $request)
     {
-        return Search::index()
-            ->ensureExists()
-            ->search($request->query('q'))
-            ->get()
-            ->filter(function ($item) {
-                return User::current()->can('view', $item);
-            })
-            ->take(10)
-            ->map(function ($item) {
-                return $item->toAugmentedCollection([
-                    'title', 'edit_url',
-                    'collection', 'is_entry',
-                    'taxonomy', 'is_term',
-                    'container', 'is_asset',
-                ])->withShallowNesting();
-            });
+        try {
+            return Search::index()
+                ->ensureExists()
+                ->search($request->query('q'))
+                ->get()
+                ->filter(function ($item) {
+                    return User::current()->can('view', $item);
+                })
+                ->take(10)
+                ->map(function ($item) {
+                    return $item->toAugmentedCollection([
+                        'title', 'edit_url',
+                        'collection', 'is_entry',
+                        'taxonomy', 'is_term',
+                        'container', 'is_asset',
+                    ])->withShallowNesting();
+                });
+        } catch (NoQuery $e) {
+            return [];
+        }
     }
 }
