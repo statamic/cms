6705d2dc04e83a541faf7617a7953ea11860777b
Jason Varga <jason@pixelfear.com>
2020-12-08 09:47:52 -0500

    Collections query and type

---
+++ src/GraphQL/Types/CollectionType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Statamic\GraphQL\Types;
+
+use GraphQL\Type\Definition\Type;
+use Statamic\Fields\Value;
+
+class CollectionType extends \Rebing\GraphQL\Support\Type
+{
+    const NAME = 'Collection';
+
+    protected $attributes = [
+        'name' => self::NAME,
+    ];
+
+    public function fields(): array
+    {
+        return collect([
+            'handle' => [
+                'type' => Type::nonNull(Type::string()),
+            ],
+            'title' => [
+                'type' => Type::nonNull(Type::string()),
+            ],
+        ])->map(function (array $arr) {
+            $arr['resolve'] = $this->resolver();
+
+            return $arr;
+        })
+        ->all();
+    }
+
+    private function resolver()
+    {
+        return function ($collection, $args, $context, $info) {
+            $value = $collection->augmentedValue($info->fieldName);
+
+            if ($value instanceof Value) {
+                $value = $value->value();
+            }
+
+            return $value;
+        };
+    }
+}
