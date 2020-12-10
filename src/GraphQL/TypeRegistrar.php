b7aba19d3ec77fa899c01b2cb3a226bcb9226618
Jason Varga <jason@pixelfear.com>
2020-12-10 16:03:48 -0500

    Assets query

--- src/GraphQL/TypeRegistrar.php
+++ src/GraphQL/TypeRegistrar.php
@@ -3,6 +3,7 @@
 namespace Statamic\GraphQL;
 
 use Rebing\GraphQL\Support\Facades\GraphQL;
+use Statamic\GraphQL\Types\AssetInterface;
 use Statamic\GraphQL\Types\CollectionType;
 use Statamic\GraphQL\Types\EntryInterface;
 use Statamic\GraphQL\Types\JsonArgument;
@@ -20,6 +21,7 @@ class TypeRegistrar
         GraphQL::addType(JsonArgument::class);
         GraphQL::addType(CollectionType::class);
         EntryInterface::addTypes();
+        AssetInterface::addTypes();
 
         $this->registered = true;
     }
---
+++ src/GraphQL/Types/AssetType.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Statamic\GraphQL\Types;
+
+use Rebing\GraphQL\Support\Facades\GraphQL;
+use Statamic\Contracts\Assets\Asset;
+use Statamic\Contracts\Assets\AssetContainer;
+use Statamic\Fields\Value;
+use Statamic\Support\Str;
+
+class AssetType extends \Rebing\GraphQL\Support\Type
+{
+    private $container;
+    private $blueprint;
+
+    public function __construct($container)
+    {
+        $this->container = $container;
+        $this->blueprint = $container->blueprint();
+        $this->attributes['name'] = static::buildName($container);
+    }
+
+    public static function buildName(AssetContainer $container): string
+    {
+        return 'Asset_'.Str::studly($container->handle());
+    }
+
+    public function interfaces(): array
+    {
+        return [
+            GraphQL::type(AssetInterface::NAME),
+        ];
+    }
+
+    public function fields(): array
+    {
+        return $this->blueprint->fields()->toGraphQL()
+            ->merge((new AssetInterface)->fields())
+            ->map(function (array $arr) {
+                $arr['resolve'] = $this->resolver();
+
+                return $arr;
+            })
+            ->all();
+    }
+
+    private function resolver()
+    {
+        return function (Asset $asset, $args, $context, $info) {
+            $value = $asset->augmentedValue($info->fieldName);
+
+            if ($value instanceof Value) {
+                $value = $value->value();
+            }
+
+            return $value;
+        };
+    }
+}
