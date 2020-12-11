59d651a2de21642463836f8f2998611e0d89a147
Jason Varga <jason@pixelfear.com>
2020-12-11 10:27:49 -0500

    Adjust and add tests for join modifier. Ref #3001

--- tests/Modifiers/JoinTest.php
+++ tests/Modifiers/JoinTest.php
@@ -8,13 +8,30 @@ use Tests\TestCase;
 class JoinTest extends TestCase
 {
     /** @test */
+    public function it_joins_values_of_an_array_using_a_comma_by_default()
+    {
+        $joined = $this->modify(['foo', 'bar']);
+
+        $this->assertEquals('foo, bar', $joined);
+    }
+
+    /** @test */
+    public function it_joins_values_of_an_array_using_a_custom_delimiter()
+    {
+        $joined = $this->modify(['foo', 'bar'], '+');
+
+        $this->assertEquals('foo+bar', $joined);
+    }
+
+    /** @test */
     public function it_returns_empty_string_when_value_is_null()
     {
-        $joined = $this->modify(null);
+        $joined = $this->modify(null, ' ');
+
         $this->assertEquals('', $joined);
     }
 
-    public function modify($arr, $delimiter = ' ')
+    public function modify($arr, $delimiter = null)
     {
         return Modify::value($arr)->join($delimiter)->fetch();
     }
