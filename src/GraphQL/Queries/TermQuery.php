0eb79ef77b6a2ac27df0210da52fdb1321378b4c
Jason Varga <jason@pixelfear.com>
2020-12-11 17:52:22 -0500

    Single term query

---
+++ src/GraphQL/Queries/TermQuery.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Statamic\GraphQL\Queries;
+
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+use Statamic\Facades\Term;
+use Statamic\GraphQL\Types\TermInterface;
+
+class TermQuery extends Query
+{
+    public function type(): Type
+    {
+        return GraphQL::type(TermInterface::NAME);
+    }
+
+    public function args(): array
+    {
+        return [
+            'id' => Type::string(),
+        ];
+    }
+
+    public function resolve($root, $args)
+    {
+        $query = Term::query();
+
+        if ($id = $args['id']) {
+            $query->where('id', $id);
+        }
+
+        return $query->limit(1)->get()->first();
+    }
+}
