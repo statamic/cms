3da3397c9035bf6445e19011d9d632d859125313
edalzell <erin@thedalzells.org>
2020-12-10 19:06:08 -0800

    test not needed (#3000)

--- tests/Modifiers/JoinTest.php
+++ tests/Modifiers/JoinTest.php
@@ -8,15 +8,6 @@ use Tests\TestCase;
 class JoinTest extends TestCase
 {
     /** @test */
-    public function it_limits_arrays()
-    {
-        $arr = ['one', 'two', 'three', 'four', 'five'];
-
-        $this->assertEquals(['one', 'two'], $this->modify($arr, 2));
-        $this->assertEquals(['one', 'two', 'three'], $this->modify($arr, 3));
-    }
-
-    /** @test */
     public function it_returns_empty_string_when_value_is_null()
     {
         $joined = $this->modify(null);
