c4ca364a103f711ad8eb6844d02a35af5cf97090
edalzell <erin@thedalzells.org>
2020-12-12 20:13:32 -0800

    update method signatures

--- src/Facades/Term.php
+++ src/Facades/Term.php
@@ -3,19 +3,23 @@
 namespace Statamic\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Statamic\Contracts\Taxonomies\Term as TermContract;
 use Statamic\Contracts\Taxonomies\TermRepository;
+use Statamic\Stache\Query\TermQueryBuilder;
+use Statamic\Taxonomies\TermCollection;
 
 /**
- * @method static all()
- * @method static whereTaxonomy(string $handle)
- * @method static whereInTaxonomy(array $handles)
- * @method static find($id)
- * @method static findByUri(string $uri)
- * @method static findBySlug(string $slug, string $taxonomy)
- * @method static make(string $slug = null)
- * @method static query()
- * @method static save($entry)
- * @method static delete($entry)
+ * @method static all(): TermCollection.
+ * @method static whereTaxonomy(string $handle): TermCollection
+ * @method static whereInTaxonomy(array $handles): TermCollection
+ * @method static find($id): ?TermContract
+ * @method static findByUri(string $uri, string $site = null): ?TermContract
+ * @method static findBySlug(string $slug, string $taxonomy): ?TermContract
+ * @method static save($term)
+ * @method static delete($term)
+ * @method static query(): TermQueryBuilder
+ * @method static make(string $slug = null): Term
+ * @method static entriesCount(Term $term): int
  *
  * @see \Statamic\Contracts\Taxonomies\TermRepository
  */
