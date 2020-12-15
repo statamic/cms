536cd09d4c603b7c2da4bf85bdcc884eedd97dc2
Jason Varga <jason@pixelfear.com>
2020-12-15 10:47:54 -0500

    Extract test case to make actual tests simpler

---
+++ tests/Feature/GraphQL/Fieldtypes/FieldtypeTestCase.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Feature\GraphQL\Fieldtypes;
+
+use Facades\Statamic\Fields\BlueprintRepository;
+use Facades\Tests\Factories\EntryFactory;
+use Statamic\Facades\Blueprint;
+use Tests\PreventSavingStacheItemsToDisk;
+use Tests\TestCase;
+
+abstract class FieldtypeTestCase extends TestCase
+{
+    use PreventSavingStacheItemsToDisk;
+
+    protected function createEntryWithFields($arr)
+    {
+        EntryFactory::collection('test')->id('1')->data(
+            collect($arr)->map->value->all()
+        )->create();
+
+        $blueprint = Blueprint::makeFromFields(
+            collect($arr)->map->field->all()
+        );
+
+        BlueprintRepository::shouldReceive('in')->with('collections/test')->andReturn(collect([
+            'blueprint' => $blueprint->setHandle('blueprint'),
+        ]));
+    }
+
+    protected function assertGqlEntryHas($query, $expected)
+    {
+        $query = <<<GQL
+{
+    entry(id: "1") {
+        ... on Entry_Test_Blueprint {
+            $query
+        }
+    }
+}
+GQL;
+
+        $this
+            ->withoutExceptionHandling()
+            ->post('/graphql', ['query' => $query])
+            ->assertGqlOk()
+            ->assertExactJson(['data' => [
+                'entry' => $expected,
+            ]]);
+    }
+}
--- tests/Feature/GraphQL/Fieldtypes/ToggleFieldtypeTest.php
+++ tests/Feature/GraphQL/Fieldtypes/ToggleFieldtypeTest.php
@@ -2,57 +2,31 @@
 
 namespace Tests\Feature\GraphQL\Fieldtypes;
 
-use Facades\Statamic\Fields\BlueprintRepository;
-use Facades\Tests\Factories\EntryFactory;
-use Statamic\Facades\Blueprint;
-use Tests\PreventSavingStacheItemsToDisk;
-use Tests\TestCase;
-
 /** @group graphql */
-class ToggleFieldtypeTest extends TestCase
+class ToggleFieldtypeTest extends FieldtypeTestCase
 {
-    use PreventSavingStacheItemsToDisk;
-
     /** @test */
     public function it_gets_a_boolean()
     {
-        EntryFactory::collection('blog')->id('1')->data([
-            'yup' => true,
-            'nope' => false,
-        ])->create();
-
-        $article = Blueprint::makeFromFields([
-            'yup' => ['type' => 'toggle'],
-            'nope' => ['type' => 'toggle'],
-            'undefined' => ['type' => 'toggle'],
+        $this->createEntryWithFields([
+            'yup' => [
+                'value' => true,
+                'field' => ['type' => 'toggle'],
+            ],
+            'nope' => [
+                'value' => false,
+                'field' => ['type' => 'toggle'],
+            ],
+            'undefined' => [
+                'value' => null,
+                'field' => ['type' => 'toggle'],
+            ],
         ]);
 
-        BlueprintRepository::shouldReceive('in')->with('collections/blog')->andReturn(collect([
-            'article' => $article->setHandle('article'),
-        ]));
-
-        $query = <<<'GQL'
-{
-    entry(id: "1") {
-        ... on Entry_Blog_Article {
-            yup
-            nope
-            undefined
-        }
-    }
-}
-GQL;
-
-        $this
-            ->withoutExceptionHandling()
-            ->post('/graphql', ['query' => $query])
-            ->assertGqlOk()
-            ->assertExactJson(['data' => [
-                'entry' => [
-                    'yup' => true,
-                    'nope' => false,
-                    'undefined' => false,
-                ],
-            ]]);
+        $this->assertGqlEntryHas('yup, nope, undefined', [
+            'yup' => true,
+            'nope' => false,
+            'undefined' => false,
+        ]);
     }
 }
