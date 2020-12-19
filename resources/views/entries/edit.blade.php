4ca93c4fd8f8ff0eeb521f79d4755248cbe2b953
Duncan McClean <duncan@mcclean.co.uk>
2020-12-19 18:59:23 +0000

    Read-only Publish toggle on Entries

--- resources/views/entries/edit.blade.php
+++ resources/views/entries/edit.blade.php
@@ -30,6 +30,7 @@
         :preloaded-assets="{{ json_encode($preloadedAssets) }}"
         :breadcrumbs="{{ $breadcrumbs->toJson() }}"
         :can-edit-blueprint="{{ $str::bool($user->can('configure fields')) }}"
+        :can-manage-publish-state="{{ $str::bool($user->can('publish '.$collection.' entries')) }}"
         create-another-url="{{ cp_route('collections.entries.create', [$collection, $locale]) }}"
         listing-url="{{ cp_route('collections.show', $collection) }}"
     ></entry-publish-form>
