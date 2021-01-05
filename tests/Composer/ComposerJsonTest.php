6eee485df5fcc4fc0c4fc18b91473cacafa7626d
Jesse Leite <jesseleite@gmail.com>
2021-01-05 14:03:30 -0500

    Add helper to insert Statamic's `pre-update-cmd` script into user's composer.json file.

--- tests/Composer/ComposerJsonTest.php
+++ tests/Composer/ComposerJsonTest.php
@@ -49,6 +49,145 @@ class ComposerJsonTest extends TestCase
         $this->assertFalse(Json::isMissingPreUpdateCmd());
     }
 
+    /** @test */
+    public function it_can_add_pre_update_cmd_array_when_doesnt_exist()
+    {
+        $this->files->put($this->path, <<<'EOT'
+{
+    "scripts": {
+        "post-autoload-dump": [
+            "Illuminate\\Foundation\\ComposerScripts::postAutoloadDump",
+            "@php artisan package:discover --ansi",
+            "@php artisan statamic:install --ansi"
+        ]
+    }
+}
+EOT
+        );
+
+        $this->assertTrue(Json::isMissingPreUpdateCmd());
+
+        Json::addPreUpdateCmd();
+
+        $this->assertFalse(Json::isMissingPreUpdateCmd());
+
+        $expected = <<<'EOT'
+{
+    "scripts": {
+        "pre-update-cmd": [
+            "Statamic\\Console\\Composer\\Scripts::preUpdateCmd"
+        ],
+        "post-autoload-dump": [
+            "Illuminate\\Foundation\\ComposerScripts::postAutoloadDump",
+            "@php artisan package:discover --ansi",
+            "@php artisan statamic:install --ansi"
+        ]
+    }
+}
+EOT;
+
+        $this->assertEquals($expected, $this->files->get($this->path));
+    }
+
+    /** @test */
+    public function it_can_add_statamic_pre_update_cmd_to_existing_array()
+    {
+        $this->files->put($this->path, <<<'EOT'
+{
+    "scripts": {
+        "pre-update-cmd": [
+            "Some\\Other\\Package\\Scripts::preUpdateCmd"
+        ],
+        "post-autoload-dump": [
+            "Illuminate\\Foundation\\ComposerScripts::postAutoloadDump",
+            "@php artisan package:discover --ansi",
+            "@php artisan statamic:install --ansi"
+        ]
+    }
+}
+EOT
+        );
+
+        $this->assertTrue(Json::isMissingPreUpdateCmd());
+
+        Json::addPreUpdateCmd();
+
+        $this->assertFalse(Json::isMissingPreUpdateCmd());
+
+        $expected = <<<'EOT'
+{
+    "scripts": {
+        "pre-update-cmd": [
+            "Statamic\\Console\\Composer\\Scripts::preUpdateCmd",
+            "Some\\Other\\Package\\Scripts::preUpdateCmd"
+        ],
+        "post-autoload-dump": [
+            "Illuminate\\Foundation\\ComposerScripts::postAutoloadDump",
+            "@php artisan package:discover --ansi",
+            "@php artisan statamic:install --ansi"
+        ]
+    }
+}
+EOT;
+
+        $this->assertEquals($expected, $this->files->get($this->path));
+    }
+
+    /** @test */
+    public function it_does_nothing_if_pre_update_cmd_already_exists()
+    {
+        $composerJson = <<<'EOT'
+{
+    "scripts": {
+        "pre-update-cmd": [
+            "Statamic\\Console\\Composer\\Scripts::preUpdateCmd",
+            "Some\\Other\\Package\\Scripts::preUpdateCmd"
+        ],
+        "post-autoload-dump": [
+            "Illuminate\\Foundation\\ComposerScripts::postAutoloadDump",
+            "@php artisan package:discover --ansi",
+            "@php artisan statamic:install --ansi"
+        ]
+    }
+}
+EOT;
+
+        $this->files->put($this->path, $composerJson);
+
+        $this->assertFalse(Json::isMissingPreUpdateCmd());
+
+        $attempted = Json::addPreUpdateCmd();
+
+        $this->assertFalse($attempted);
+        $this->assertEquals($composerJson, $this->files->get($this->path));
+    }
+
+    /** @test */
+    public function it_will_throw_error_when_it_unsuccessfully_adds_pre_update_cmd()
+    {
+        $invalidJson = <<<'EOT'
+{
+    "scripts": {
+        "pre-update-cmd": [
+            "Some\\Other\\Package\\Scripts::preUpdateCmd"
+}
+EOT;
+
+        $this->files->put($this->path, $invalidJson);
+
+        $this->assertEquals($invalidJson, $this->files->get($this->path));
+
+        try {
+            Json::addPreUpdateCmd();
+        } catch (\Exception $exception) {
+            // Catch the exception so we can also assert that it didn't make any changes to the original composer.json file.
+        }
+
+        $this->assertEquals('Statamic had trouble adding the `pre-update-cmd` to your composer.json file.', $exception->getMessage());
+
+        $this->assertEquals($invalidJson, $this->files->get($this->path));
+    }
+
     private function backup()
     {
         $this->files->copy($this->path, $this->backupPath);
