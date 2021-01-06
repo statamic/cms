94210433fc8453661578e8fbfff30be232657d87
Jesse Leite <jesseleite@gmail.com>
2021-01-06 15:33:17 -0500

    Add ability to track error output when using `NullConsole`.

--- src/Console/NullConsole.php
+++ src/Console/NullConsole.php
@@ -4,6 +4,31 @@ namespace Statamic\Console;
 
 class NullConsole
 {
+    protected $errors = [];
+
+    /**
+     * Store error output.
+     *
+     * @param string $error
+     * @return $this
+     */
+    public function error($error)
+    {
+        $this->errors[] = $error;
+
+        return $this;
+    }
+
+    /**
+     * Get error output.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public function getErrors()
+    {
+        return collect($this->errors);
+    }
+
     /**
      * Any calls to this class just return itself so you can chain forever and ever.
      *
