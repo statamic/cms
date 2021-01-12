statamic/cms

commit c975aa3478ddf0c896de7191cb47b20c05fde73e

Hook up top down reactivity

resources/js/components/fieldtypes/TableFieldtype.vue
@@ -100,6 +100,11 @@ export default {
             handler (data) {
                 this.update(this.sortableToArray(data));
             }
+        },
+
+        value(value, oldValue) {
+            if (JSON.stringify(value) == JSON.stringify(oldValue)) return;
+            this.data = this.arrayToSortable(value);
         }
     },
 

resources/js/components/fieldtypes/CheckboxesFieldtype.vue
@@ -35,6 +35,10 @@ export default {
 
         values(values) {
             this.update(values);
+        },
+
+        value(value) {
+            this.values = value;
         }
 
     },

resources/js/components/fieldtypes/RadioFieldtype.vue
@@ -10,6 +10,7 @@
                 @input="update($event.target.value)"
                 :value="option.value"
                 :id="name + $index"
+                :checked="value === option.value"
             />
             <label :for="name + $index">{{ option.text }}</label>
         </div>

resources/js/components/fieldtypes/ToggleFieldtype.vue
@@ -33,6 +33,13 @@ export default {
             return this.state === match;
         }
     },
+
+    watch: {
+        value(value) {
+            this.state = this.value;
+        }
+    },
+
     methods: {
         toggle() {
             this.state = !this.state;

resources/js/components/fieldtypes/VideoFieldtype.vue
@@ -31,6 +31,10 @@ export default {
 
         data(value) {
             this.update(value);
+        },
+
+        value(value) {
+            this.data = value;
         }
 
     },

commit b693f9efe1ab94b06ce0281a72ef13cccabb4aa1

Remove arrayToSortable override in favor of smarter value watcher ...
Closes #2470

Adding top down reactivity in c975aa3 caused #1447 which was solved by 4536ef2 but then caused #2470

This commit implements c975aa3 in a smarter way, which will prevent all the sortable _id keys being regenerated which prevents a dom update, which caused #1447.

Got that? Phew.

resources/js/components/fieldtypes/TableFieldtype.vue
@@ -107,6 +107,7 @@ export default {
 
         value(value, oldValue) {
             if (JSON.stringify(value) == JSON.stringify(oldValue)) return;
+            if (JSON.stringify(value) == JSON.stringify(this.sortableToArray(this.data))) return;
             this.data = this.arrayToSortable(value);
         }
     },
@@ -202,13 +203,6 @@ export default {
         deleteCancelled() {
             this.deletingRow = false;
             this.deletingColumn = false;
-        },
-
-        arrayToSortable(arr) {
-            return _.map(arr, value => {
-                return this.data.find(v => JSON.stringify(v.value) == JSON.stringify(value))
-                    || new SortableKeyValue(null, value);
-            });
         }
     }
 