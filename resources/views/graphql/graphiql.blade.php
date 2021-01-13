dfae86a17e153c497eae2af08929d6f966ef6996
Jason Varga <jason@pixelfear.com>
2021-01-13 16:40:53 -0500

    dedicated graphiql stylesheet

--- resources/views/graphql/graphiql.blade.php
+++ resources/views/graphql/graphiql.blade.php
@@ -1,20 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <style>
-        html,
-        body {
-            height: 100%;
-            margin: 0;
-            overflow: hidden;
-            width: 100%;
-        }
-        #graphiql {
-            height: 100vh;
-        }
-    </style>
-
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/graphiql-with-extensions@0.14.3/graphiqlWithExtensions.css" integrity="sha384-GBqwox+q8UtVEyBLBKloN5QDlBDsQnuoSUfMeJH1ZtDiCrrk103D7Bg/WjIvl4ya" crossorigin="anonymous" />
+    <link href="{{ Statamic::cpAssetUrl('css/graphiql.css') }}?v={{ Statamic::version() }}" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/whatwg-fetch@2.0.3/fetch.min.js" integrity="sha384-dcF7KoWRaRpjcNbVPUFgatYgAijf8DqW6NWuqLdfB5Sb4Cdbb8iHX7bHsl9YhpKa" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/react@16.8.6/umd/react.production.min.js" integrity="sha384-qn+ML/QkkJxqn4LLs1zjaKxlTg2Bl/6yU/xBTJAgxkmNGc6kMZyeskAG0a7eJBR1" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/react-dom@16.8.6/umd/react-dom.production.min.js" integrity="sha384-85IMG5rvmoDsmMeWK/qUU4kwnYXVpC+o9hoHMLi4bpNR+gMEiPLrvkZCgsr7WWgV" crossorigin="anonymous"></script>
