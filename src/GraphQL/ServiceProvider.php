19ecfbcdea36a8dd88b09ee614eb2400dc828884
Jason Varga <jason@pixelfear.com>
2021-01-14 16:12:36 -0500

    Move schema into dedicated class

--- src/GraphQL/ServiceProvider.php
+++ src/GraphQL/ServiceProvider.php
@@ -3,23 +3,6 @@
 namespace Statamic\GraphQL;
 
 use Illuminate\Support\ServiceProvider as LaravelProvider;
-use Statamic\GraphQL\Queries\AssetContainerQuery;
-use Statamic\GraphQL\Queries\AssetContainersQuery;
-use Statamic\GraphQL\Queries\AssetQuery;
-use Statamic\GraphQL\Queries\AssetsQuery;
-use Statamic\GraphQL\Queries\CollectionQuery;
-use Statamic\GraphQL\Queries\CollectionsQuery;
-use Statamic\GraphQL\Queries\EntriesQuery;
-use Statamic\GraphQL\Queries\EntryQuery;
-use Statamic\GraphQL\Queries\GlobalSetQuery;
-use Statamic\GraphQL\Queries\GlobalSetsQuery;
-use Statamic\GraphQL\Queries\NavQuery;
-use Statamic\GraphQL\Queries\NavsQuery;
-use Statamic\GraphQL\Queries\PingQuery;
-use Statamic\GraphQL\Queries\TaxonomiesQuery;
-use Statamic\GraphQL\Queries\TaxonomyQuery;
-use Statamic\GraphQL\Queries\TermQuery;
-use Statamic\GraphQL\Queries\TermsQuery;
 
 class ServiceProvider extends LaravelProvider
 {
@@ -32,31 +15,6 @@ class ServiceProvider extends LaravelProvider
 
     private function setDefaultSchema()
     {
-        $schema = [
-            'query' => [
-                'ping' => PingQuery::class,
-                'entries' => EntriesQuery::class,
-                'entry' => EntryQuery::class,
-                'collections' => CollectionsQuery::class,
-                'collection' => CollectionQuery::class,
-                'assets' => AssetsQuery::class,
-                'asset' => AssetQuery::class,
-                'assetContainers' => AssetContainersQuery::class,
-                'assetContainer' => AssetContainerQuery::class,
-                'taxonomies' => TaxonomiesQuery::class,
-                'taxonomy' => TaxonomyQuery::class,
-                'terms' => TermsQuery::class,
-                'term' => TermQuery::class,
-                'globalSets' => GlobalSetsQuery::class,
-                'globalSet' => GlobalSetQuery::class,
-                'navs' => NavsQuery::class,
-                'nav' => NavQuery::class,
-            ],
-            'mutation' => [],
-            'middleware' => [],
-            'method' => ['get', 'post'],
-        ];
-
-        config(['graphql.schemas.default' => $schema]);
+        config(['graphql.schemas.default' => DefaultSchema::config()]);
     }
 }
