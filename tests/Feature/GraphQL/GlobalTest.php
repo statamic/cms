f783a0795495b1d71c2b478cffef5e522da59ad6
Jason Varga <jason@pixelfear.com>
2021-01-20 11:24:10 -0500

    Additional fields can be added to globals

--- tests/Feature/GraphQL/GlobalTest.php
+++ tests/Feature/GraphQL/GlobalTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\GraphQL;
 use Facades\Statamic\Fields\BlueprintRepository;
 use Facades\Tests\Factories\GlobalFactory;
 use Statamic\Facades\Blueprint;
+use Statamic\Facades\GraphQL;
 use Tests\PreventSavingStacheItemsToDisk;
 use Tests\TestCase;
 
@@ -42,5 +43,92 @@ GQL;
                 'handle' => 'social',
                 'twitter' => '@statamic',
             ]]]);
+    }
+
+    /** @test */
+    public function it_can_add_custom_fields_to_interface()
+    {
+        GraphQL::addField('GlobalSetInterface', 'one', function () {
+            return [
+                'type' => GraphQL::string(),
+                'resolve' => function ($a) {
+                    return 'first';
+                },
+            ];
+        });
+
+        GraphQL::addField('GlobalSetInterface', 'two', function () {
+            return [
+                'type' => GraphQL::string(),
+                'resolve' => function ($a) {
+                    return 'second';
+                },
+            ];
+        });
+
+        GlobalFactory::handle('social')->data(['twitter' => '@statamic'])->create();
+        $social = Blueprint::makeFromFields(['twitter' => ['type' => 'text']])->setHandle('social')->setNamespace('globals');
+        BlueprintRepository::shouldReceive('find')->with('globals.social')->andReturn($social);
+
+        $query = <<<'GQL'
+{
+    globalSet(handle: "social") {
+        handle
+        one
+        two
+    }
+}
+GQL;
+
+        $this
+            ->withoutExceptionHandling()
+            ->post('/graphql', ['query' => $query])
+            ->assertGqlOk()
+            ->assertExactJson(['data' => [
+                'globalSet' => [
+                    'handle' => 'social',
+                    'one' => 'first',
+                    'two' => 'second',
+                ],
+            ]]);
+    }
+
+    /** @test */
+    public function it_can_add_custom_fields_to_an_implementation()
+    {
+        GraphQL::addField('GlobalSet_Social', 'one', function () {
+            return [
+                'type' => GraphQL::string(),
+                'resolve' => function ($a) {
+                    return 'first';
+                },
+            ];
+        });
+
+        GlobalFactory::handle('social')->data(['twitter' => '@statamic'])->create();
+        $social = Blueprint::makeFromFields(['twitter' => ['type' => 'text']])->setHandle('social')->setNamespace('globals');
+        BlueprintRepository::shouldReceive('find')->with('globals.social')->andReturn($social);
+
+        $query = <<<'GQL'
+{
+    globalSet(handle: "social") {
+        handle
+        ... on GlobalSet_Social {
+            one
+        }
+    }
+}
+GQL;
+
+        $this
+            ->withoutExceptionHandling()
+            ->post('/graphql', ['query' => $query])
+            ->assertGqlOk()
+            ->assertExactJson(['data' => [
+                'globalSet' => [
+                    'handle' => 'social',
+                    'one' => 'first',
+                ],
+            ]]);
     }
 }
