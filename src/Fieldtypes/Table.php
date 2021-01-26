3246c2a4f6a54fe3a8a57010b40a546824c064b9
Jason Varga <jason@pixelfear.com>
2021-01-26 14:53:33 -0500

    Table fieldtype

--- src/Fieldtypes/Table.php
+++ src/Fieldtypes/Table.php
@@ -2,9 +2,14 @@
 
 namespace Statamic\Fieldtypes;
 
+use Statamic\Facades\GraphQL;
 use Statamic\Fields\Fieldtype;
+use Statamic\GraphQL\Types\TableRowType;
 
 class Table extends Fieldtype
 {
-    //
+    public function toGqlType()
+    {
+        return GraphQL::listOf(GraphQL::type(TableRowType::NAME));
+    }
 }
