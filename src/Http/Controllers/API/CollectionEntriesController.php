580f7854d5866a872545766191778ee4bf352174
Jesse Leite <jesseleite@gmail.com>
2021-01-27 12:05:15 -0500

    Cache collection entries API endpoints.

--- src/Http/Controllers/API/CollectionEntriesController.php
+++ src/Http/Controllers/API/CollectionEntriesController.php
@@ -3,19 +3,24 @@
 namespace Statamic\Http\Controllers\API;
 
 use Illuminate\Http\Request;
+use Statamic\API\Cacher;
 use Statamic\Http\Resources\API\EntryResource;
 
 class CollectionEntriesController extends ApiController
 {
     public function index($collection, Request $request)
     {
-        return app(EntryResource::class)::collection(
-            $this->filterSortAndPaginate($collection->queryEntries())
-        );
+        return app(Cacher::class)->remember($request, function () use ($collection) {
+            return app(EntryResource::class)::collection(
+                $this->filterSortAndPaginate($collection->queryEntries())
+            );
+        });
     }
 
     public function show($collection, $entry)
     {
-        return app(EntryResource::class)::make($entry);
+        return app(Cacher::class)->remember($request, function () use ($entry) {
+            return app(EntryResource::class)::make($entry);
+        });
     }
 }
