10cadb0e209f2e556d2a8a1c9c7f29d4b19ae95d
Duncan McClean <duncan@mcclean.co.uk>
2021-03-16 20:55:10 +0000

    GlobalSet: Add saving tests

--- tests/Data/Globals/GlobalSetTest.php
+++ tests/Data/Globals/GlobalSetTest.php
@@ -2,6 +2,10 @@
 
 namespace Tests\Data\Globals;
 
+use Illuminate\Support\Facades\Event;
+use Statamic\Events\GlobalSetCreated;
+use Statamic\Events\GlobalSetSaved;
+use Statamic\Events\GlobalSetSaving;
 use Statamic\Facades\Site;
 use Statamic\Globals\GlobalSet;
 use Tests\TestCase;
@@ -72,5 +76,104 @@ title: 'The title'
 
 EOT;
         $this->assertEquals($expected, $set->fileContents());
+    }
+
+    /** @test */
+    public function it_saves_through_the_api()
+    {
+        Event::fake();
+
+        Site::setConfig([
+            'default' => 'en',
+            'sites' => [
+                'en' => ['name' => 'English', 'locale' => 'en_US', 'url' => 'http://test.com/'],
+                'fr' => ['name' => 'French', 'locale' => 'fr_FR', 'url' => 'http://fr.test.com/'],
+                'de' => ['name' => 'German', 'locale' => 'de_DE', 'url' => 'http://test.com/de/'],
+            ],
+        ]);
+
+        $set = (new GlobalSet)->title('SEO Settings');
+
+        $set->in('en', function ($loc) {
+            $loc->data([
+                'array' => ['first one', 'second one'],
+                'string' => 'The string',
+            ]);
+        });
+
+        $set->save();
+
+        Event::assertDispatched(GlobalSetSaving::class, function ($event) use ($set) {
+            return $event->globals === $set;
+        });
+
+        Event::assertDispatched(GlobalSetCreated::class, function ($event) use ($set) {
+            return $event->globals === $set;
+        });
+
+        Event::assertDispatched(GlobalSetSaved::class, function ($event) use ($set) {
+            return $event->globals === $set;
+        });
+    }
+
+    /** @test */
+    public function it_dispatches_global_set_created_only_once()
+    {
+        Event::fake();
+
+        Site::setConfig([
+            'default' => 'en',
+            'sites' => [
+                'en' => ['name' => 'English', 'locale' => 'en_US', 'url' => 'http://test.com/'],
+                'fr' => ['name' => 'French', 'locale' => 'fr_FR', 'url' => 'http://fr.test.com/'],
+                'de' => ['name' => 'German', 'locale' => 'de_DE', 'url' => 'http://test.com/de/'],
+            ],
+        ]);
+
+        $set = (new GlobalSet)->title('SEO Settings');
+
+        $set->in('en', function ($loc) {
+            $loc->data([
+                'array' => ['first one', 'second one'],
+                'string' => 'The string',
+            ]);
+        });
+
+        $set->save();
+        $set->save();
+        $set->save();
+
+        Event::assertDispatched(GlobalSetSaved::class, 3);
+        Event::assertDispatched(GlobalSetCreated::class, 1); // TODO: fix this
+    }
+
+    /** @test */
+    public function it_saves_quietly()
+    {
+        Event::fake();
+
+        Site::setConfig([
+            'default' => 'en',
+            'sites' => [
+                'en' => ['name' => 'English', 'locale' => 'en_US', 'url' => 'http://test.com/'],
+                'fr' => ['name' => 'French', 'locale' => 'fr_FR', 'url' => 'http://fr.test.com/'],
+                'de' => ['name' => 'German', 'locale' => 'de_DE', 'url' => 'http://test.com/de/'],
+            ],
+        ]);
+
+        $set = (new GlobalSet)->title('SEO Settings');
+
+        $set->in('en', function ($loc) {
+            $loc->data([
+                'array' => ['first one', 'second one'],
+                'string' => 'The string',
+            ]);
+        });
+
+        $set->saveQuietly();
+
+        Event::assertNotDispatched(GlobalSetSaving::class);
+        Event::assertNotDispatched(GlobalSetSaved::class);
+        Event::assertNotDispatched(GlobalSetCreated::class);
     }
 }
