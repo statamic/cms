28e2290a05e9dc8b7bbfee83634604a66b895bdf
Jack McDade <jack@jackmcdade.com>
2021-03-16 13:38:32 -0400

    Add icon to the collection widget

--- resources/views/widgets/collection.blade.php
+++ resources/views/widgets/collection.blade.php
@@ -1,6 +1,11 @@
 <div class="card p-0 overflow-hidden">
     <div class="flex justify-between items-center p-2">
-        <h2>{{ $title }}</h2>
+        <h2 class="flex items-center">
+            <div class="h-6 w-6 mr-1 text-grey-80">
+                @cp_svg('content-writing')
+            </div>
+            <span>{{ $title  }}</span>
+        </h2>
         <a href="{{ $collection->createEntryUrl() }}" class="text-blue hover:text-blue-dark text-sm">{{ $button }}</a>
     </div>
     <collection-widget
