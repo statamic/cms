46a61dc5f4e02e0c9a1575eecd26ed4b68e3b8c5
Duncan McClean <duncan@mcclean.co.uk>
2021-03-22 21:21:34 +0000

    add id to entry path

--- tests/Stache/FeatureTest.php
+++ tests/Stache/FeatureTest.php
@@ -282,7 +282,7 @@ class FeatureTest extends TestCase
             ->data(['title' => 'Test Entry', 'foo' => 'bar'])
         )->save();
 
-        $this->assertFileExists(__DIR__.'/__fixtures__/content/collections/blog/2017-07-04.test-entry.md');
+        $this->assertFileExists(__DIR__.'/__fixtures__/content/collections/blog/2017-07-04.test-entry.123.md');
 
         $entry->delete();
     }
