70250d9982d07a657955553ef86645e3b93981ec
Jack McDade <jack@jackmcdade.com>
2021-06-29 16:22:11 -0400

    Should not do should not do either

--- resources/js/components/data-list/Table.vue
+++ resources/js/components/data-list/Table.vue
@@ -234,7 +234,6 @@ export default {
         checkboxClicked($event, index) {
             this.$refs.table.focus();
             if ($event.shiftKey && this.lastItemClicked !== null) {
-                console.log('should not do')
                 this.selectRange(
                     Math.min(this.lastItemClicked, index),
                     Math.max(this.lastItemClicked, index)
