4e3b475e9fb865e93dc8e7680ba33df247e19727
Jason Varga <jason@pixelfear.com>
2021-07-05 18:35:15 -0400

    Merge title/url validation rules with any that may already exist on the blueprint from the user

--- src/Http/Controllers/CP/Structures/NavigationPagesController.php
+++ src/Http/Controllers/CP/Structures/NavigationPagesController.php
@@ -4,6 +4,7 @@ namespace Statamic\Http\Controllers\CP\Structures;
 
 use Illuminate\Http\Request;
 use Statamic\Facades\Nav;
+use Statamic\Fields\Blueprint;
 use Statamic\Http\Controllers\CP\CpController;
 
 class NavigationPagesController extends CpController
@@ -12,13 +13,32 @@ class NavigationPagesController extends CpController
     {
         $nav = Nav::find($nav);
 
-        $blueprint = $nav->blueprint();
+        $blueprint = $this->ensureFields($nav->blueprint(), $request);
 
+        $blueprint->fields()
+            ->addValues($request->values)
+            ->validator()
+            ->withRules($this->extraRules($request))
+            ->validate();
+    }
+
+    private function ensureFields(Blueprint $blueprint, $request)
+    {
+        // Add fields so that the validation rules will display with the correct names
         if ($request->type === 'url') {
-            $blueprint->ensureField('title', ['display' => __('Title'), 'validate' => 'required_without:url']);
-            $blueprint->ensureField('url', ['display' => __('URL'), 'validate' => 'required_without:title']);
+            $blueprint
+                ->ensureField('title', ['display' => __('Title')])
+                ->ensureField('url', ['display' => __('URL')]);
         }
 
-        $blueprint->fields()->addValues($request->values)->validate();
+        return $blueprint;
+    }
+
+    private function extraRules($request)
+    {
+        return $request->type === 'url' ? [
+            'title' => ['required_without:url'],
+            'url' => ['required_without:title'],
+        ] : [];
     }
 }
