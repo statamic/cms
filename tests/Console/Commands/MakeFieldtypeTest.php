e9011154de467e7601df602ca3655f53864ccf7a
Jesse Leite <jesseleite@gmail.com>
2021-07-07 23:38:29 -0400

    Flesh out `make:fieldtype` tests.

--- tests/Console/Commands/MakeFieldtypeTest.php
+++ tests/Console/Commands/MakeFieldtypeTest.php
@@ -2,11 +2,20 @@
 
 namespace Tests\Console\Commands;
 
+use Facades\Statamic\Console\Processes\Composer;
+use Illuminate\Filesystem\Filesystem;
 use Tests\TestCase;
 
 class MakeFieldtypeTest extends TestCase
 {
     use Concerns\PreparesGeneratedPaths;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->files = app(Filesystem::class);
+    }
 
     public function tearDown(): void
     {
@@ -21,19 +30,62 @@ class MakeFieldtypeTest extends TestCase
         $this->assertFileNotExists(base_path('app/Fieldtypes/KnightRider.php'));
         $this->assertFileNotExists(resource_path('js/components/fieldtypes/KnightRider.vue'));
 
-        $this->makeFieldtype('KnightRider');
+        $this->artisan('statamic:make:fieldtype', ['name' => 'KnightRider']);
 
-        $this->assertFileExists(base_path('app/Fieldtypes/KnightRider.php'));
+        $this->assertFileExists($fieldtype = base_path('app/Fieldtypes/KnightRider.php'));
+        $this->assertStringContainsString('namespace App\Fieldtypes;', $this->files->get($fieldtype));
+
         $this->assertFileExists(resource_path('js/components/fieldtypes/KnightRider.vue'));
 
         // @TODO: Test for webpack/cp.js injection or output instructions
     }
 
-    private function makeFieldtype($name, $options = [])
+    /** @test */
+    public function it_will_not_overwrite_an_existing_fieldtype()
     {
-        $this->artisan('statamic:make:fieldtype', array_merge([
-            'name' => $name,
-            '--no-interaction' => true,
-        ], $options));
+        $path = base_path('app/Fieldtypes/KnightRider.php');
+
+        $this->assertFileNotExists($path);
+
+        $this->artisan('statamic:make:fieldtype', ['name' => 'KnightRider']);
+        $this->files->put($path, 'overwritten fieldtype');
+
+        $this->assertStringContainsString('overwritten fieldtype', $this->files->get($path));
+
+        $this->artisan('statamic:make:fieldtype', ['name' => 'KnightRider']);
+
+        $this->assertStringContainsString('overwritten fieldtype', $this->files->get($path));
+    }
+
+    /** @test */
+    public function using_force_option_will_overwrite_original_fieldtype()
+    {
+        $path = base_path('app/Fieldtypes/KnightRider.php');
+
+        $this->artisan('statamic:make:fieldtype', ['name' => 'KnightRider']);
+        $this->files->put($path, 'overwritten fieldtype');
+
+        $this->assertStringContainsString('overwritten fieldtype', $this->files->get($path));
+
+        $this->artisan('statamic:make:fieldtype', ['name' => 'KnightRider', '--force' => true]);
+
+        $this->assertStringNotContainsString('overwritten fieldtype', $this->files->get($path));
+    }
+
+    /** @test */
+    public function it_can_make_a_fieldtype_into_an_addon()
+    {
+        $path = base_path('addons/yoda/bag-odah');
+
+        $this->artisan('statamic:make:addon', ['addon' => 'yoda/bag-odah']);
+
+        Composer::shouldReceive('installedPath')->andReturn($path);
+
+        $this->assertFileNotExists($fieldtype = "$path/src/Fieldtypes/Yoda.php");
+
+        $this->artisan('statamic:make:fieldtype', ['name' => 'Yoda', 'addon' => 'yoda/bag-odah']);
+
+        $this->assertFileExists($fieldtype);
+        $this->assertStringContainsString('namespace Yoda\BagOdah\Fieldtypes;', $this->files->get($fieldtype));
     }
 }
