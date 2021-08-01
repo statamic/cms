7f9ea12d22c8cef1e6e6d58437f77fa9fdd97a47
Michael Rook <michael@rook.net.au>
2021-08-01 10:31:51 +1000

    Add nova svg

--- resources/views/partials/global-header.blade.php
+++ resources/views/partials/global-header.blade.php
@@ -37,7 +37,7 @@
         @endif
         @if (Route::has('nova.index') && $user->can('viewNova'))
             <a class="hidden md:block h-6 w-6 p-sm text-grey ml-2 hover:text-grey-80" href="{{ route('nova.index') }}" target="_blank" v-tooltip="'Laravel Nova'">
-                @cp_svg('charts')
+                @cp_svg('nova')
             </a>
         @endif
         <dropdown-list v-cloak>
