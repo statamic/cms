cbae3f2ded1eb2d4234dd3b8245491b171d55c98
Michael Rook <54159303+michaelr0@users.noreply.github.com>
2021-08-03 10:46:33 +1000

    Add Laravel Horizon link to CP (#4074)

--- resources/views/partials/global-header.blade.php
+++ resources/views/partials/global-header.blade.php
@@ -35,6 +35,11 @@
                 @cp_svg('telescope')
             </a>
         @endif
+        @if (Route::has('horizon.index') && \Laravel\Horizon\Horizon::check(request()))
+            <a class="hidden md:block h-6 w-6 p-sm text-grey ml-2 hover:text-grey-80" href="{{ route('horizon.index') }}" target="_blank" v-tooltip="'Laravel Horizon'">
+                @cp_svg('horizon')
+            </a>
+        @endif
         <dropdown-list v-cloak>
             <template v-slot:trigger>
                 <button class="hidden md:block h-6 w-6 ml-2 p-sm text-grey hover:text-grey-80" v-tooltip="__('Useful Links')" aria-label="{{ __('View Useful Links') }}">
