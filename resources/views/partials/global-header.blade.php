58600d17b9b6e4ae946d0367360558ed28a59a09
Michael Rook <54159303+michaelr0@users.noreply.github.com>
2021-08-03 10:52:52 +1000

    Check permissions for Telescope (#4075)

--- resources/views/partials/global-header.blade.php
+++ resources/views/partials/global-header.blade.php
@@ -30,8 +30,8 @@
 
         <favorite-creator class="hidden md:block"></favorite-creator>
 
-        @if (config('telescope.enabled'))
-            <a class="hidden md:block h-6 w-6 p-sm text-grey ml-2 hover:text-grey-80" href="/{{ config('telescope.path') }}" target="_blank" v-tooltip="'Laravel Telescope'">
+        @if (Route::has('telescope') && \Laravel\Telescope\Telescope::check(request()))
+            <a class="hidden md:block h-6 w-6 p-sm text-grey ml-2 hover:text-grey-80" href="{{ route('telescope') }}" target="_blank" v-tooltip="'Laravel Telescope'">
                 @cp_svg('telescope')
             </a>
         @endif
