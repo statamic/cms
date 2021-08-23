5903b406ecf07d89a36fd5722d74de55020b2171
Steven Thomas <stvn.thomas@gmail.com>
2021-08-23 09:19:38 -0700

    Use `urlWithoutRedirect` to determine `is_current` (#4058)

--- src/Tags/Nav.php
+++ src/Tags/Nav.php
@@ -46,7 +46,7 @@ class Nav extends Structure
         }
 
         $crumbs = $crumbs->values()->map(function ($crumb) {
-            $crumb->setSupplement('is_current', URL::getCurrent() === $crumb->url());
+            $crumb->setSupplement('is_current', URL::getCurrent() === $crumb->urlWithoutRedirect());
 
             return $crumb;
         });
