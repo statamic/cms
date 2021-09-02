6d1ebe950ca397e98b0a20ef4c9a6dfd6b21ec25
Jason Varga <jason@pixelfear.com>
2021-09-02 17:21:57 -0400

    Read inline svgs from composer vendor directory (#4216)

--- src/Statamic.php
+++ src/Statamic.php
@@ -228,7 +228,7 @@ class Statamic
         }
 
         $svg = StaticStringy::collapseWhitespace(
-            File::get(public_path("vendor/statamic/cp/svg/{$name}.svg"))
+            File::get(statamic_path("resources/svg/{$name}.svg"))
         );
 
         return str_replace('<svg', sprintf('<svg%s', $attrs), $svg);
