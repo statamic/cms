91316b1217e109bfd831a49a61bdeed83ff9a65d
Jason Varga <jason@pixelfear.com>
2021-09-02 15:42:31 -0400

    Use the non-dist version

--- src/Statamic.php
+++ src/Statamic.php
@@ -228,7 +228,7 @@ class Statamic
         }
 
         $svg = StaticStringy::collapseWhitespace(
-            File::get(base_path("vendor/statamic/cms/resources/dist/svg/{$name}.svg"))
+            File::get(statamic_path("resources/svg/{$name}.svg"))
         );
 
         return str_replace('<svg', sprintf('<svg%s', $attrs), $svg);
