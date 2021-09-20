d7c2ca05688aa5bd180d5414d82bc711c95a76c2
Jason Varga <jason@pixelfear.com>
2021-09-20 13:49:29 -0400

    test

--- tests/Feature/Globals/ConfigureGlobalsTest.php
+++ tests/Feature/Globals/ConfigureGlobalsTest.php
@@ -3,13 +3,12 @@
 namespace Tests\Feature\Globals;
 
 use Facades\Tests\Factories\GlobalFactory;
-use Statamic\Facades\Blueprint;
 use Statamic\Facades\User;
 use Tests\FakesRoles;
 use Tests\PreventSavingStacheItemsToDisk;
 use Tests\TestCase;
 
-class EditGlobalVariablesTest extends TestCase
+class ConfigureGlobalsTest extends TestCase
 {
     use FakesRoles;
     use PreventSavingStacheItemsToDisk;
@@ -24,7 +23,7 @@ class EditGlobalVariablesTest extends TestCase
         $this
             ->from('/original')
             ->actingAs($user)
-            ->get($global->in('en')->editUrl())
+            ->get($global->editUrl())
             ->assertRedirect('/original')
             ->assertSessionHas('error');
     }
@@ -32,22 +31,14 @@ class EditGlobalVariablesTest extends TestCase
     /** @test */
     public function it_shows_the_form()
     {
-        $blueprint = Blueprint::make()->setContents(['fields' => [
-            ['handle' => 'foo', 'field' => ['type' => 'text']],
-            ['handle' => 'unused', 'field' => ['type' => 'text']],
-        ]]);
-        $userBlueprint = Blueprint::make();
-        Blueprint::shouldReceive('find')->with('globals.test')->andReturn($blueprint);
-        Blueprint::shouldReceive('find')->with('user')->andReturn($userBlueprint);
-        $this->setTestRoles(['test' => ['access cp', 'edit test globals']]);
+        $this->setTestRoles(['test' => ['access cp', 'configure globals']]);
         $user = User::make()->assignRole('test')->save();
 
         $global = GlobalFactory::handle('test')->data(['foo' => 'bar'])->create();
 
         $this
             ->actingAs($user)
-            ->get($global->in('en')->editUrl())
-            ->assertSuccessful()
-            ->assertViewHas('values', ['foo' => 'bar', 'unused' => null]);
+            ->get($global->editUrl())
+            ->assertSuccessful();
     }
 }
