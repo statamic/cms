742fa7316c491ca8a823f0c76c1846ee72d1d26b
Jason Varga <jason@pixelfear.com>
2021-09-27 10:58:28 -0400

    Add test

---
+++ tests/Modifiers/GroupByTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Modifiers;
+
+use Statamic\Modifiers\Modify;
+use Tests\TestCase;
+
+class GroupByTest extends TestCase
+{
+    /** @test */
+    public function it_groups_an_array()
+    {
+        $items = [
+            ['sport' => 'basketball', 'team' => 'jazz'],
+            ['sport' => 'baseball', 'team' => 'yankees'],
+            ['sport' => 'basketball', 'team' => 'bulls'],
+        ];
+
+        $expected = [
+            'basketball' => [
+                ['sport' => 'basketball', 'team' => 'jazz'],
+                ['sport' => 'basketball', 'team' => 'bulls'],
+            ],
+            'baseball' => [
+                ['sport' => 'baseball', 'team' => 'yankees'],
+            ],
+        ];
+
+        $this->assertEquals($expected, $this->modify($items, 'sport'));
+    }
+
+    public function modify($items, $value)
+    {
+        return Modify::value($items)->groupBy($value)->fetch();
+    }
+}
