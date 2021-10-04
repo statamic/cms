2bb9514185a61d07ed77710d866de6fc99e82885
Jason Varga <jason@pixelfear.com>
2021-10-04 19:29:38 -0400

    simplify by using the collection tag

--- tests/Antlers/ScratchTest.php
+++ tests/Antlers/ScratchTest.php
@@ -4,8 +4,6 @@ namespace Tests\Antlers;
 
 use Facades\Tests\Factories\EntryFactory;
 use Statamic\Facades\Antlers;
-use Statamic\Facades\Collection;
-use Statamic\Structures\CollectionStructure;
 use Tests\PreventSavingStacheItemsToDisk;
 use Tests\TestCase;
 
@@ -14,19 +12,13 @@ class ScratchTest extends TestCase
     use PreventSavingStacheItemsToDisk;
 
     /** @test */
-    public function nav_tag_variables_should_not_leak_outside_its_tag_pair()
+    public function tag_variables_should_not_leak_outside_its_tag_pair()
     {
-        $collection = Collection::make('test');
-        $structure = (new CollectionStructure)->maxDepth(3)->expectsRoot(false);
-        $collection->structure($structure)->save();
         EntryFactory::collection('test')->id('one')->slug('one')->data(['title' => 'One'])->create();
         EntryFactory::collection('test')->id('two')->slug('two')->data(['title' => 'Two'])->create();
-        $collection->structure()->in('en')->tree([
-            ['entry' => 'one'],
-            ['entry' => 'two'],
-        ])->save();
 
-        $template = '{{ title }} {{ nav:collection:test }}{{ title }} {{ /nav:collection:test }} {{ title }}';
+        // note: not specific to the collection tag
+        $template = '{{ title }} {{ collection:test }}{{ title }} {{ /collection:test }} {{ title }}';
         $expected = 'Outside One Two  Outside';
 
         $parsed = (string) Antlers::parse($template, ['title' => 'Outside']);
