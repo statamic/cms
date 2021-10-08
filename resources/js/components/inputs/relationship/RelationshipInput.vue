15a6252e41d1f1b86956d38b2a52cdddf22fc7d3
Tobi <mail@tobiasholst.de>
2021-10-08 13:52:07 +0200

    fix

--- resources/js/components/inputs/relationship/RelationshipInput.vue
+++ resources/js/components/inputs/relationship/RelationshipInput.vue
@@ -151,7 +151,9 @@ export default {
 
         items() {
             return this.value.map(selection => {
-                const data = _.findWhere(this.data, { id: selection });
+                const data = _.find(this.data, (item) => {
+                    return item.id == selection
+                });
 
                 if (! data) return { id: selection, title: selection };
 
