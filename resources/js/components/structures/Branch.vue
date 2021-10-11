10e6a9022846d370b796ac546713bb49040b2234
Jason Varga <jason@pixelfear.com>
2021-10-11 13:09:08 -0400

    Show / for root

--- resources/js/components/structures/Branch.vue
+++ resources/js/components/structures/Branch.vue
@@ -11,8 +11,8 @@
                     :class="{ 'text-sm font-medium': isTopLevel }"
                     v-text="title" />
 
-                    {{ page.slug }}
                 <span v-if="showSlugs" class="ml-1 font-mono text-grey-70 text-2xs pt-px">
+                    {{ isRoot ? '/' : page.slug }}
                 </span>
 
                 <button
