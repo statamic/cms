4fe7a782de39b685ff72062b2f4b32d8c886b909
Florian Jahn <florian@involve.at>
2021-10-12 11:09:19 +0200

    Inject toasts into responses

---
+++ src/Http/Middleware/CP/AddToasts.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Statamic\Http\Middleware\CP;
+
+use Closure;
+use Illuminate\Http\Request;
+use Statamic\CP\Toasts\Toast;
+use Statamic\CP\Toasts\ToastsHolder;
+use Symfony\Component\HttpFoundation\Response;
+
+class AddToasts
+{
+    /**
+     * @var ToastsHolder
+     */
+    protected $toastsHolder;
+
+    public function __construct(ToastsHolder $toastsHolder)
+    {
+        $this->toastsHolder = $toastsHolder;
+    }
+
+    public function handle(Request $request, Closure $next)
+    {
+        $response = $next($request);
+
+        return $this->addToastsTo($response);
+    }
+
+    private function addToastsTo(Response $response): Response
+    {
+        $toasts = $this->getToastsAsArray();
+
+        if (empty($toasts)) {
+            return $response;
+        }
+
+        $parsedContent = $this->unwrapAndParseContentAsArray($response);
+
+        if ($parsedContent == null) {
+            return $response;
+        }
+
+        $parsedContent['_toasts'] = $toasts;
+        $newContent = json_encode($parsedContent);
+
+        return $response->setContent($newContent);
+    }
+
+    private function unwrapAndParseContentAsArray(Response $response): ?array
+    {
+        $content = $response->getContent();
+
+        if ($content === false) {
+            return null;
+        }
+
+        $parsedContent = json_decode($content, true);
+
+        if ($parsedContent === null) {
+            return null;
+        }
+
+        if (! is_array($parsedContent)) {
+            return null;
+        }
+
+        return $parsedContent;
+    }
+
+    private function getToastsAsArray(): array
+    {
+        $toasts = $this->toastsHolder->all();
+
+        return array_map(function (Toast $toast) {
+            return $toast->toArray();
+        }, $toasts);
+    }
+}
