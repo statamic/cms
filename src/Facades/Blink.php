63c833a36de1a50888a44848fc5c69ed2a1e6ed1
Duncan McClean <duncan@mcclean.co.uk>
2021-10-12 21:20:50 +0100

    Add Blink's built-in methods to Blink facade to allow for better IDE completion

--- src/Facades/Blink.php
+++ src/Facades/Blink.php
@@ -5,6 +5,23 @@ namespace Statamic\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static \Spatie\Blink\Blink put($key, $value = null)
+ * @method static null|string|array get(string $key, $default = null)
+ * @method bool has(string $key)
+ * @method array all()
+ * @method array allStartingWith(string $startingWith = '')
+ * @method \Spatie\Blink\Blink forget(string $key)
+ * @method \Spatie\Blink\Blink flush()
+ * @method \Spatie\Blink\Blink flushStartingWith(string $startingWith = '')
+ * @method null|string pull(string $key)
+ * @method int|null|string increment(string $key, int $by = 1)
+ * @method int|null|string decrement(string $key, int $by = 1)
+ * @method bool offsetExists($offset)
+ * @method mixed offsetGet($offset)
+ * @method void offsetSet($offset, $value)
+ * @method void offsetUnset($offset)
+ * @method int count()
+ * @method mixed once($key, callable $callable)
  * @method static mixed|\Spatie\Blink\Blink store($name = 'default')
  *
  * @see Statamic\Support\Blink
