6e58f2a3b6655b5eb81f6c1d57de9796cdd3bb2f
Sam Spinoy <sam@adaptagency.com>
2021-10-20 21:15:02 +0200

    Make remember me cookie configurable (#4415)
    Co-authored-by: Jason Varga <jason@pixelfear.com>

--- config/oauth.php
+++ config/oauth.php
@@ -13,4 +13,17 @@ return [
         'callback' => 'oauth/{provider}/callback',
     ],
 
+    /*
+    |--------------------------------------------------------------------------
+    | Remember Me
+    |--------------------------------------------------------------------------
+    |
+    | Whether or not the "remember me" functionality should be used when
+    | authenticating using OAuth. When enabled, the user will remain
+    | logged in indefinitely, or until they manually log out.
+    |
+    */
+
+    'remember_me' => true,
+
 ];
