15eddaa72086af36afaf2fd0a32dd9ea8062850f
Jason Varga <jason@pixelfear.com>
2021-10-20 12:53:07 -0400

    facade hints

--- src/Facades/CP/Toast.php
+++ src/Facades/CP/Toast.php
@@ -8,6 +8,12 @@ use Statamic\CP\Toasts\Manager;
 /**
  * @method static void push(\Statamic\CP\Toasts\Toast $toast)
  * @method static \Statamic\CP\Toasts\Toast[] all()
+ * @method static \Illuminate\Support\Collection collect()
+ * @method static array toArray()
+ * @method static \Statamic\CP\Toasts\Toast info(string $message)
+ * @method static \Statamic\CP\Toasts\Toast error(string $message)
+ * @method static \Statamic\CP\Toasts\Toast success(string $message)
+ * @method static void clear()
  *
  * @see \Statamic\CP\Toasts\Manager
  */
