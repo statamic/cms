3b90dab5fe279197070b0a3f78e4ded9290f6db4
Jason Varga <jason@pixelfear.com>
2021-10-20 15:13:57 -0400

    comment

--- config/oauth.php
+++ config/oauth.php
@@ -13,7 +13,17 @@ return [
         'callback' => 'oauth/{provider}/callback',
     ],
 
-    // Set "remember me" cookie when logging in via Oauth.
+    /*
+    |--------------------------------------------------------------------------
+    | Remember Me
+    |--------------------------------------------------------------------------
+    |
+    | Whether or not the "remember me" functionality should be used when
+    | authenticating using OAuth. When enabled, the user will remain
+    | logged in indefinitely, or until they manually log out.
+    |
+    */
+
     'remember_me' => true,
 
 ];
