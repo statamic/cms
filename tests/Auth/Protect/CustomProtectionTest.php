1a6d2d42527f6fa441496887e3b06ea1fc462bc2
Jason Varga <jason@pixelfear.com>
2021-10-25 15:40:31 -0400

    tweak test

--- tests/Auth/Protect/CustomProtectionTest.php
+++ tests/Auth/Protect/CustomProtectionTest.php
@@ -2,38 +2,27 @@
 
 namespace Tests\Auth\Protect;
 
-
 use Statamic\Auth\Protect\ProtectorManager;
 use Statamic\Auth\Protect\Protectors\Protector;
 
 class CustomProtectionTest extends PageProtectionTestCase
 {
-
     /** @test */
     public function the_config_and_scheme_are_set_on_custom_protectors()
     {
-        $protector = $this->mock(Protector::class);
-        $configValues = [
-            'driver'     => 'custom',
+        config(['statamic.protect.schemes.test' => $config = [
+            'driver' => 'custom',
             'config_var' => 'custom_value',
-        ];
+        ]]);
 
-        config(['statamic.protect.schemes.custom' => $configValues]);
+        $protector = $this->mock(Protector::class);
+        $protector->shouldReceive('setConfig')->with($config)->once()->andReturnSelf();
+        $protector->shouldReceive('setScheme')->with('test')->once()->andReturnSelf();
 
-        app(ProtectorManager::class)->extend('custom', function ($app) use ($protector) {
+        app(ProtectorManager::class)->extend('custom', function () use ($protector) {
             return $protector;
         });
 
-        $protector->shouldReceive('setConfig')
-                  ->with($configValues)
-                  ->once()
-                  ->andReturn($protector);
-
-        $protector->shouldReceive('setScheme')
-                  ->with('custom')
-                  ->once()
-                  ->andReturn($protector);
-
-        $this->requestPageProtectedBy('custom');
+        $this->requestPageProtectedBy('test');
     }
 }
