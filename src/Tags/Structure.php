8a5b1695dd863cd59cec7f2960fb98c41ae15bde
Jason Varga <jason@pixelfear.com>
2021-10-26 14:40:22 -0400

    cache the similar bits of the structure tree

--- src/Tags/Structure.php
+++ src/Tags/Structure.php
@@ -2,10 +2,12 @@
 
 namespace Statamic\Tags;
 
+use Illuminate\Support\Facades\Cache;
 use Statamic\Contracts\Structures\Structure as StructureContract;
 use Statamic\Facades\Site;
 use Statamic\Facades\URL;
 use Statamic\Structures\TreeBuilder;
+use Statamic\Support\Arr;
 
 class Structure extends Tags
 {
@@ -32,7 +34,7 @@ class Structure extends Tags
             $handle = $handle->handle();
         }
 
-        $tree = (new TreeBuilder)->build([
+        $tree = (new TreeBuilder)->build($config = [
             'structure' => $handle,
             'include_home' => $this->params->get('include_home'),
             'show_unpublished' => $this->params->get('show_unpublished', false),
@@ -41,7 +43,12 @@ class Structure extends Tags
             'max_depth' => $this->params->get('max_depth'),
         ]);
 
-        return $this->toArray($tree);
+        $key = 'structure-tag-tree-'.md5(serialize($config));
+        $arr = Cache::rememberForever($key, function () use ($tree) {
+            return $this->toArray($tree);
+        });
+
+        return $this->addDynamicBitsToTree($arr);
     }
 
     public function toArray($tree, $parent = null, $depth = 1)
@@ -59,10 +66,23 @@ class Structure extends Tags
                 'count'       => $index + 1,
                 'first'       => $index === 0,
                 'last'        => $index === count($tree) - 1,
-                'is_current'  => rtrim(URL::getCurrent(), '/') == rtrim($page->urlWithoutRedirect(), '/'),
-                'is_parent'   => Site::current()->absoluteUrl() === $page->absoluteUrl() ? false : URL::isAncestorOf(URL::getCurrent(), $page->urlWithoutRedirect()),
-                'is_external' => URL::isExternal($page->absoluteUrl()),
+                'urlWithoutRedirect' => $page->urlWithoutRedirect(),
             ]);
         })->filter()->values()->all();
+    }
+
+    private function addDynamicBitsToTree($tree)
+    {
+        return collect($tree)->map(function ($item) {
+            $item['children'] = $this->addDynamicBitsToTree($item['children']);
+
+            $arr = array_merge($item, [
+                'is_current'  => rtrim(URL::getCurrent(), '/') == rtrim($item['urlWithoutRedirect'], '/'),
+                'is_parent'   => Site::current()->absoluteUrl() === $item['permalink'] ? false : URL::isAncestorOf(URL::getCurrent(), $item['urlWithoutRedirect']),
+                'is_external' => URL::isExternal($item['permalink']),
+            ]);
+
+            return Arr::except($arr, 'urlWithoutRedirect');
+        })->all();
     }
 }
