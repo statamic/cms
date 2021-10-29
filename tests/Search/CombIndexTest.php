ac47f011e56250d850a39cbcb341658b0a522aec
Jelle Roorda <info@roordaict.nl>
2021-10-29 14:02:12 +0200

    Add test for finding users by their email address.

--- tests/Search/CombIndexTest.php
+++ tests/Search/CombIndexTest.php
@@ -4,6 +4,10 @@ namespace Tests\Search;
 
 use Illuminate\Contracts\Filesystem\Filesystem;
 use Mockery;
+use Statamic\Auth\UserCollection;
+use Statamic\Facades\Search;
+use Statamic\Facades\Stache;
+use Statamic\Facades\User;
 use Statamic\Search\Comb\Index;
 use Tests\TestCase;
 
@@ -38,5 +42,43 @@ class CombIndexTest extends TestCase
     public function getIndex()
     {
         return app(Index::class);
+    }
+
+    /** @test */
+    public function it_can_find_users_by_their_email_address()
+    {
+        config([
+            'statamic.search.indexes.users' => [
+                'driver' => 'local',
+                'searchables' => 'users',
+                'fields' => ['name', 'email'],
+            ]
+        ]);
+
+        $john = User::make()
+            ->set('name', 'John Doe')
+            ->email('john@doe.com')
+            ->save();
+
+        $jane = User::make()
+            ->set('name', 'Jane Doe')
+            ->email('jane@doe.com')
+            ->save();
+
+        $index = Search::index('users');
+
+        $index->update();
+
+        $users = $index->search('john@doe.com')->get();
+
+        $this->assertCount(1, $users, 'User could not be found by his email address');
+        $this->assertEquals($john->id(), $users->first()->id());
+        $this->assertNotEquals($jane->id(), $users->first()->id());
+
+        // Clean up the created users
+        $path = __DIR__.'/../__fixtures__/users';
+        $this->files = app(\Illuminate\Filesystem\Filesystem::class);
+        $this->files->cleanDirectory($path);
+        $this->files->put($path.'/.gitkeep', null);
     }
 }
