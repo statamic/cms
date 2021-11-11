a325f9927fdb79578518060f3a9cf23b2a37dc27
Ryan Mitchell <ryan@thoughtcollective.com>
2021-11-11 08:01:38 +0000

    If $value is an array, count intersection

--- src/Stache/Query/Builder.php
+++ src/Stache/Query/Builder.php
@@ -131,6 +131,9 @@ abstract class Builder extends BaseBuilder
     protected function filterWhereIn($values, $where)
     {
         return $values->filter(function ($value) use ($where) {
+            if (is_array($value)) {
+                return count(array_intersect($value, $where['values']));
+            }
             return in_array($value, $where['values']);
         });
     }
@@ -138,6 +141,9 @@ abstract class Builder extends BaseBuilder
     protected function filterWhereNotIn($values, $where)
     {
         return $values->filter(function ($value) use ($where) {
+            if (is_array($value)) {
+                return ! count(array_intersect($value, $where['values']));
+            }
             return ! in_array($value, $where['values']);
         });
     }
