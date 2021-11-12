4eb125780cbffd5b811f022d7d109097a33dc48e
Jack Sleight <jacksleight@gmail.com>
2021-11-12 16:55:18 +0000

    Always return $this

--- src/Forms/Email.php
+++ src/Forms/Email.php
@@ -86,7 +86,7 @@ class Email extends Mailable
     protected function addAttachments()
     {
         if (! array_get($this->config, 'attachments')) {
-            return;
+            return $this;
         }
 
         $augmented = $this->submission->toAugmentedArray();
