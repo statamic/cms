e22099e54fa983bb10269db64a0576ab97548964
Ryan Mitchell <ryan@thoughtcollective.com>
2021-11-16 09:36:23 +0000

    Changed the wrong one

--- src/Stache/Query/Builder.php
+++ src/Stache/Query/Builder.php
@@ -149,7 +149,7 @@ abstract class Builder extends BaseBuilder
                 return false;
             }
 
-            return count(array_intersect($value, $where['values']));
+            return ! empty(array_intersect($value, $where['values']));
         });
     }
 
@@ -171,7 +171,7 @@ abstract class Builder extends BaseBuilder
                 return false;
             }
 
-            return ! empty($value) == $where['value'];
+            return count($value) == $where['value'];
         });
     }
 }
