689d56d599df1840797d28b8fa2a073c008ef6b9
Ryan Mitchell <ryan@thoughtcollective.com>
2021-11-19 06:37:28 +0000

    Add AssetQueryBuilder tests

--- tests/Data/Assets/AssetQueryBuilderTest.php
+++ tests/Data/Assets/AssetQueryBuilderTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Data\Assets;
 
 use Illuminate\Support\Facades\Storage;
+use Statamic\Facades\Asset;
 use Statamic\Facades\AssetContainer;
 use Tests\PreventSavingStacheItemsToDisk;
 use Tests\TestCase;
@@ -69,5 +70,69 @@ class AssetQueryBuilderTest extends TestCase
 
         $this->assertCount(2, $assets);
         $this->assertEquals(['d', 'e'], $assets->map->filename()->all());
+    }
+
+    /** @test **/
+    public function assets_are_found_using_where_between()
+    {
+        Asset::find('test::a.jpg')->data(['number_field' => 8])->save();
+        Asset::find('test::b.txt')->data(['number_field' => 9])->save();
+        Asset::find('test::c.txt')->data(['number_field' => 10])->save();
+        Asset::find('test::d.jpg')->data(['number_field' => 11])->save();
+        Asset::find('test::e.jpg')->data(['number_field' => 12])->save();
+        Asset::find('test::f.jpg')->data([])->save();
+
+        $assets = $this->container->queryAssets()->whereBetween('number_field', [9, 11])->get();
+
+        $this->assertCount(3, $assets);
+        $this->assertEquals(['b', 'c', 'd'], $assets->map->filename()->all());
+    }
+
+    /** @test **/
+    public function assets_are_found_using_where_not_between()
+    {
+        Asset::find('test::a.jpg')->data(['number_field' => 8])->save();
+        Asset::find('test::b.txt')->data(['number_field' => 9])->save();
+        Asset::find('test::c.txt')->data(['number_field' => 10])->save();
+        Asset::find('test::d.jpg')->data(['number_field' => 11])->save();
+        Asset::find('test::e.jpg')->data(['number_field' => 12])->save();
+        Asset::find('test::f.jpg')->data([])->save();
+
+        $assets = $this->container->queryAssets()->whereNotBetween('number_field', [9, 11])->get();
+
+        $this->assertCount(3, $assets);
+        $this->assertEquals(['a', 'e', 'f'], $assets->map->filename()->all());
+    }
+
+    /** @test **/
+    public function assets_are_found_using_or_where_between()
+    {
+        Asset::find('test::a.jpg')->data(['number_field' => 8])->save();
+        Asset::find('test::b.txt')->data(['number_field' => 9])->save();
+        Asset::find('test::c.txt')->data(['number_field' => 10])->save();
+        Asset::find('test::d.jpg')->data(['number_field' => 11])->save();
+        Asset::find('test::e.jpg')->data(['number_field' => 12])->save();
+        Asset::find('test::f.jpg')->data([])->save();
+
+        $assets = $this->container->queryAssets()->whereBetween('number_field', [9, 10])->orWhereBetween('number_field', [11, 12])->get();
+
+        $this->assertCount(4, $assets);
+        $this->assertEquals(['b', 'c', 'd', 'e'], $assets->map->filename()->all());
+    }
+
+    /** @test **/
+    public function assets_are_found_using_or_where_not_between()
+    {
+        Asset::find('test::a.jpg')->data(['text' => 'a', 'number_field' => 8])->save();
+        Asset::find('test::b.txt')->data(['text' => 'b', 'number_field' => 9])->save();
+        Asset::find('test::c.txt')->data(['text' => 'c', 'number_field' => 10])->save();
+        Asset::find('test::d.jpg')->data(['text' => 'd', 'number_field' => 11])->save();
+        Asset::find('test::e.jpg')->data(['text' => 'e', 'number_field' => 12])->save();
+        Asset::find('test::f.jpg')->data([])->save();
+
+        $assets = $this->container->queryAssets()->where('text', 'e')->orWhereNotBetween('number_field', [10, 12])->get();
+
+        $this->assertCount(4, $assets);
+        $this->assertEquals(['e', 'a', 'b', 'f'], $assets->map->filename()->all());
     }
 }
