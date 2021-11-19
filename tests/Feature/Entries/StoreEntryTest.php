f09096795e68286cd1d41ed346f72ebedbee57a4
Jason Varga <jason@pixelfear.com>
2021-11-19 16:23:31 -0500

    add some tests

--- tests/Feature/Entries/StoreEntryTest.php
+++ tests/Feature/Entries/StoreEntryTest.php
@@ -2,6 +2,11 @@
 
 namespace Tests\Feature\Entries;
 
+use Facades\Statamic\Fields\BlueprintRepository;
+use Statamic\Facades\Blueprint;
+use Statamic\Facades\Collection;
+use Statamic\Facades\Entry;
+use Statamic\Facades\User;
 use Tests\FakesRoles;
 use Tests\PreventSavingStacheItemsToDisk;
 use Tests\TestCase;
@@ -14,18 +19,106 @@ class StoreEntryTest extends TestCase
     /** @test */
     public function it_denies_access_if_you_dont_have_permission()
     {
-        $this->markTestIncomplete();
+        $this->setTestRoles(['test' => ['access cp']]);
+        $user = tap(User::make()->assignRole('test'))->save();
+        $collection = tap(Collection::make('test'))->save();
+
+        $this
+            ->actingAs($user)
+            ->from('/original')
+            ->submit($collection, [])
+            ->assertRedirect('/original');
     }
 
     /** @test */
     public function entry_gets_created()
     {
-        $this->markTestIncomplete();
+        [$user, $collection] = $this->seedUserAndCollection();
+
+        $this->assertCount(0, Entry::all());
+
+        $this
+            ->actingAs($user)
+            ->submit($collection, ['title' => 'My Entry', 'slug' => 'my-entry'])
+            ->assertOk();
+
+        // todo: assert response contents
+
+        $this->assertCount(1, Entry::all());
+        $entry = Entry::all()->first();
+        $this->assertEquals('My Entry', $entry->value('title'));
+        $this->assertEquals('my-entry', $entry->slug());
     }
 
     /** @test */
-    public function validation_error_returns_back()
+    public function slug_is_not_required_and_will_get_created_from_the_submitted_title()
     {
-        $this->markTestIncomplete();
+        [$user, $collection] = $this->seedUserAndCollection();
+
+        $this->assertCount(0, Entry::all());
+
+        $this
+            ->from('/original')
+            ->actingAs($user)
+            ->submit($collection, ['title' => 'Test Entry', 'slug' => ''])
+            ->assertOk();
+
+        $this->assertCount(1, Entry::all());
+        $entry = Entry::all()->first();
+        $this->assertEquals('Test Entry', $entry->value('title'));
+        $this->assertEquals('test-entry', $entry->slug());
+    }
+
+    /** @test */
+    public function slug_is_not_required_and_will_get_created_from_auto_generated_title_when_using_title_format()
+    {
+        [$user, $collection] = $this->seedUserAndCollection();
+        $collection->titleFormats('Auto {foo}')->save();
+        $this->seedBlueprintFields($collection, ['foo' => ['type' => 'text']]);
+
+        $this->assertCount(0, Entry::all());
+
+        $this
+            ->actingAs($user)
+            ->submit($collection, [
+                'title' => '',
+                'slug' => '',
+                'foo' => 'bar',
+            ])->assertOk();
+
+        $this->assertCount(1, Entry::all());
+        $entry = Entry::all()->first();
+        $this->assertEquals('Auto bar', $entry->value('title'));
+        $this->assertEquals('auto-bar', $entry->slug());
+    }
+
+    private function seedUserAndCollection()
+    {
+        $this->setTestRoles(['test' => ['access cp', 'create test entries']]);
+        $user = tap(User::make()->assignRole('test'))->save();
+        $collection = tap(Collection::make('test'))->save();
+
+        return [$user, $collection];
+    }
+
+    private function seedBlueprintFields($collection, $fields)
+    {
+        $blueprint = Blueprint::makeFromFields($fields);
+
+        BlueprintRepository::shouldReceive('in')
+            ->with('collections/'.$collection->handle())
+            ->andReturn(collect([$blueprint]));
+    }
+
+    private function submit($collection, $attrs = [])
+    {
+        $url = cp_route('collections.entries.store', [$collection->handle(), 'en']);
+
+        $payload = array_merge([
+            'title' => 'Test entry',
+            'slug' => 'test-entry',
+        ], $attrs);
+
+        return $this->post($url, $payload);
     }
 }
--- tests/Feature/Entries/UpdateEntryTest.php
+++ tests/Feature/Entries/UpdateEntryTest.php
@@ -2,6 +2,10 @@
 
 namespace Tests\Feature\Entries;
 
+use Facades\Tests\Factories\EntryFactory;
+use Statamic\Facades\Collection;
+use Statamic\Facades\Entry;
+use Statamic\Facades\User;
 use Tests\FakesRoles;
 use Tests\PreventSavingStacheItemsToDisk;
 use Tests\TestCase;
@@ -14,7 +18,95 @@ class UpdateEntryTest extends TestCase
     /** @test */
     public function it_denies_access_if_you_dont_have_permission()
     {
-        $this->markTestIncomplete();
+        $this->setTestRoles(['test' => ['access cp']]);
+        $user = tap(User::make()->assignRole('test'))->save();
+        $collection = tap(Collection::make('test'))->save();
+
+        $entry = EntryFactory::collection($collection)
+            ->slug('existing-entry')
+            ->data(['title' => 'Existing Entry'])
+            ->create();
+
+        $this
+            ->actingAs($user)
+            ->from('/original')
+            ->update($entry)
+            ->assertRedirect('/original');
+
+        $this->assertCount(1, Entry::all());
+        $this->assertEquals('Existing Entry', $entry->fresh()->value('title'));
+    }
+
+    /** @test */
+    public function entry_gets_updated()
+    {
+        [$user, $collection] = $this->seedUserAndCollection();
+
+        $entry = EntryFactory::collection($collection)
+            ->slug('existing-entry')
+            ->data(['title' => 'Existing Entry'])
+            ->create();
+
+        $this->assertCount(1, Entry::all());
+
+        $this
+            ->actingAs($user)
+            ->update($entry, ['title' => 'Updated Entry', 'slug' => 'updated-entry'])
+            ->assertOk();
+
+        // todo: assert about response content
+
+        $this->assertCount(1, Entry::all());
+        $entry = $entry->fresh();
+        $this->assertEquals('Updated Entry', $entry->value('title'));
+        $this->assertEquals('updated-entry', $entry->slug());
+    }
+
+    /** @test */
+    public function slug_is_not_required_and_will_get_created_from_the_submitted_title()
+    {
+        [$user, $collection] = $this->seedUserAndCollection();
+
+        $entry = EntryFactory::collection($collection)
+            ->slug('existing-entry')
+            ->data(['title' => 'Existing Entry'])
+            ->create();
+
+        $this->assertCount(1, Entry::all());
+
+        $this
+            ->actingAs($user)
+            ->update($entry, ['title' => 'Foo Bar Baz', 'slug' => ''])
+            ->assertOk();
+
+        $this->assertCount(1, Entry::all());
+        $entry = $entry->fresh();
+        $this->assertEquals('Foo Bar Baz', $entry->value('title'));
+        $this->assertEquals('foo-bar-baz', $entry->slug());
+    }
+
+    /** @test */
+    public function slug_is_not_required_and_will_get_created_from_auto_generated_title_when_using_title_format()
+    {
+        [$user, $collection] = $this->seedUserAndCollection();
+        $collection->titleFormats('Auto {foo}')->save();
+
+        $entry = EntryFactory::collection($collection)
+            ->slug('existing-entry')
+            ->data(['title' => 'Existing Entry', 'foo' => 'bar'])
+            ->create();
+
+        $this->assertCount(1, Entry::all());
+
+        $this
+            ->actingAs($user)
+            ->update($entry, ['title' => '', 'slug' => '', 'foo' => 'bar'])
+            ->assertOk();
+
+        $this->assertCount(1, Entry::all());
+        $entry = $entry->fresh();
+        $this->assertEquals('Auto bar', $entry->value('title'));
+        $this->assertEquals('auto-bar', $entry->slug());
     }
 
     /** @test */
@@ -41,8 +133,22 @@ class UpdateEntryTest extends TestCase
         $this->markTestIncomplete();
     }
 
-    private function save($entry, $payload)
+    private function seedUserAndCollection()
     {
+        $this->setTestRoles(['test' => ['access cp', 'edit test entries']]);
+        $user = tap(User::make()->assignRole('test'))->save();
+        $collection = tap(Collection::make('test'))->save();
+
+        return [$user, $collection];
+    }
+
+    private function update($entry, $attrs = [])
+    {
+        $payload = array_merge([
+            'title' => 'Updated entry',
+            'slug' => 'updated-entry',
+        ], $attrs);
+
         return $this->patch($entry->updateUrl(), $payload);
     }
 }
