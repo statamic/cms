25119b152bbf350534d2591f2c231f11ec60bce8
Jack Sleight <jacksleight@gmail.com>
2021-11-25 10:41:39 +0000

    Fix eloquent user last login

--- src/Auth/SetLastLoginTimestamp.php
+++ src/Auth/SetLastLoginTimestamp.php
@@ -3,15 +3,12 @@
 namespace Statamic\Auth;
 
 use Illuminate\Auth\Events\Login;
-use Statamic\Contracts\Auth\User as StatamicUser;
 use Statamic\Facades\User;
 
 class SetLastLoginTimestamp
 {
     public function handle(Login $event)
     {
-        if ($event->user instanceof StatamicUser) {
-            User::fromUser($event->user)->setLastLogin(now());
-        }
+        User::fromUser($event->user)->setLastLogin(now());
     }
 }
