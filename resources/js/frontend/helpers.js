f56f4df67e3b7caef7eb7f93dc985cdc28b3942b
Jesse Leite <jesseleite@gmail.com>
2021-12-21 18:11:58 -0500

    These dependencies are required for now (WIP, will clean up).

--- resources/js/frontend/helpers.js
+++ resources/js/frontend/helpers.js
@@ -1,5 +1,16 @@
 import FieldConditions from './components/FieldConditions.js';
 
+// TODO: How to handle dependency?
+window._ = require('underscore');
+
+// TODO: How to handle dependency?
+window.data_get = function(obj, path, fallback=null) {
+    // Source: https://stackoverflow.com/a/22129960
+    var properties = Array.isArray(path) ? path : path.split('.');
+    var value = properties.reduce((prev, curr) => prev && prev[curr], obj);
+    return value !== undefined ? value : fallback;
+};
+
 class Statamic {
     constructor() {
         this.$conditions = new FieldConditions;
