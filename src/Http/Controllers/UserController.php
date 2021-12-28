9dffa326aef413371298e5bb92104508d462df27
Arthur Perton <arthur@webenapp.nl>
2021-12-28 21:45:37 +0100

    Fix code style

--- src/Http/Controllers/UserController.php
+++ src/Http/Controllers/UserController.php
@@ -29,7 +29,7 @@ class UserController extends Controller
         if ($validator->passes()) {
             if ($this->hasTooManyLoginAttempts($request)) {
                 $this->fireLockoutEvent($request);
-    
+
                 return $this->sendLockoutResponse($request);
             }
 
