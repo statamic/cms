a5e4fb23cd007c9312d86c299fa58331c75a5b1d
Jason Varga <jason@pixelfear.com>
2022-01-11 15:42:14 -0500

    Allow the section/yield tag to get/set sections in the view factory

--- src/Tags/Section.php
+++ src/Tags/Section.php
@@ -10,6 +10,27 @@ class Section extends Tags
     {
         $name = explode(':', $this->tag)[1];
 
-        Cascade::instance()->sections()->put($name, $this->parse());
+        $contents = $this->parse();
+
+        // Store the contents in both the view factory and the cascade. This way, it's retrievable in
+        // a Blade template via the @yield directive, or in an Antlers template using the yield tag.
+        // It would be nice to be able to just store it in the view factory, but when a Blade view
+        // doesn't use an @extends, the sections get flushed and lost once the view is rendered.
+        $this->storeInCascade($name, $contents);
+        $this->storeInViewFactory($name, $contents);
+    }
+
+    private function storeInCascade($name, $contents)
+    {
+        Cascade::instance()->sections()->put($name, $contents);
+    }
+
+    private function storeInViewFactory($name, $contents)
+    {
+        tap(view()->shared('__env'), function ($view) use ($name, $contents) {
+            $view->startSection($name);
+            echo $contents;
+            $view->stopSection($name);
+        });
     }
 }
