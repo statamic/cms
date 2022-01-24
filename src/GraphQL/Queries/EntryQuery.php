6e1b5f067080180befca59fbddb115e104f3cbca
Manuel Strebel <manuel.strebel@novu.ch>
2022-01-24 17:11:10 +0100

    Filter entry by published status unless otherwise defined

--- src/GraphQL/Queries/EntryQuery.php
+++ src/GraphQL/Queries/EntryQuery.php
@@ -6,9 +6,14 @@ use GraphQL\Type\Definition\Type;
 use Statamic\Facades;
 use Statamic\Facades\GraphQL;
 use Statamic\GraphQL\Types\EntryInterface;
+use Statamic\GraphQL\Types\JsonArgument;
+use Statamic\Support\Arr;
+use Statamic\Tags\Concerns\QueriesConditions;
 
 class EntryQuery extends Query
 {
+    use QueriesConditions;
+
     protected $attributes = [
         'name' => 'entry',
     ];
@@ -26,6 +31,7 @@ class EntryQuery extends Query
             'collection' => GraphQL::string(),
             'uri' => GraphQL::string(),
             'site' => GraphQL::string(),
+            'filter' => GraphQL::type(JsonArgument::NAME),
         ];
     }
 
@@ -53,6 +59,33 @@ class EntryQuery extends Query
             $query->where('site', $site);
         }
 
+        $this->filterQuery($query, $args['filter'] ?? []);
+
         return $query->limit(1)->get()->first();
+    }
+
+    private function filterQuery($query, $filters)
+    {
+        if (!isset($filters['status']) && !isset($filters['published'])) {
+            $filters['status'] = 'published';
+        }
+
+        foreach ($filters as $field => $definitions) {
+            if (!is_array($definitions)) {
+                $definitions = [['equals' => $definitions]];
+            }
+
+            if (Arr::assoc($definitions)) {
+                $definitions = collect($definitions)->map(function ($value, $key) {
+                    return [$key => $value];
+                })->values()->all();
+            }
+
+            foreach ($definitions as $definition) {
+                $condition = array_keys($definition)[0];
+                $value = array_values($definition)[0];
+                $this->queryCondition($query, $field, $condition, $value);
+            }
+        }
     }
 }
