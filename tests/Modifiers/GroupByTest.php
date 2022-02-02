932819c5b896a9300018e581f6a46d4b676ea46c
John Koster <john@stillat.com>
2022-02-01 18:03:11 -0600

    Updates test date to match upstream

--- tests/Modifiers/GroupByTest.php
+++ tests/Modifiers/GroupByTest.php
@@ -207,7 +207,7 @@ class GroupByTest extends TestCase
     /** @test */
     public function it_groups_by_date_with_custom_group_format()
     {
-        Carbon::setTestNow('September 15');
+        Carbon::setTestNow(Carbon::parse('2022-09-01'));
 
         $items = [
             ['when' => now()->setHour(14), 'title' => '2pm'],
