c6840e25c13a3c8a4ae9d8c2e34046da201bf873
Jack McDade <jack@jackmcdade.com>
2022-02-25 16:15:29 -0500

    Wire up an almost functional zoom

--- resources/js/components/assets/Editor/PdfViewer.vue
+++ resources/js/components/assets/Editor/PdfViewer.vue
@@ -1,9 +1,24 @@
 <template>
-    <pdf :src="src">
-        <template slot="loading">
-            loading content here...
-        </template>
-    </pdf>
+    <div>
+        <div class="flex bg-black text-white px-2 py-1 justify-center space-x-4 text-sm">
+            <div class="flex">
+                <a :title="__('Zoom In')" class="text-white p-1 text-xl leading-none" @click="scaleDown">
+                    -
+                </a>
+                <a class="text-white p-1">
+                    {{ formattedZoom }} %
+                </a>
+                <a title="__('Zoom Out')" class="text-white p-1 text-xl leading-none" @click="scaleUp">
+                    +
+                </a>
+            </div>
+        </div>
+        <pdf :src="src" :scale.sync="scale" :page="page" :annotation="true" :resize="true" class="w-full h-full mx-2">
+            <template slot="loading">
+                {{ __('A wild PDF appears!') }}
+            </template>
+        </pdf>
+    </div>
 </template>
 
 <script>
@@ -17,14 +32,33 @@ export default {
 
     props: {
         src: {
-            type: Array,
+            type: String,
             required: true
         }
     },
 
-    computed: {},
+    data () {
+        return {
+            page: 1,
+            numPages: 0,
+            scale: 'page-width'
+        }
+    },
 
-    methods: {}
+    computed: {
+        formattedZoom () {
+            return Number.parseInt(this.scale * 100)
+        },
+    },
+
+    methods: {
+        scaleDown() {
+            this.scale = this.scale * 0.85;
+        },
+        scaleUp() {
+            this.scale = this.scale * 1.15;
+        }
+    },
 
 }
 </script>
