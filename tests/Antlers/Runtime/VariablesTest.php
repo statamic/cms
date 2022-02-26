a1cc889d2f3ae9349b678a48431db6b5cbf0b849
John Koster <john@stillat.com>
2022-02-26 08:52:54 -0600

    Allows Value (and other objects) to be used directly as an array key
    This allows the more "natural" syntax to "just work" when passing value-objects as an array key.
    
    Previously, it would return the augmented instance internally to the data manager. Now it will preferentially cast values to their string values to assist with array lookups.

---
+++ tests/Antlers/Runtime/VariablesTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Antlers\Runtime;
+
+use Statamic\Fields\LabeledValue;
+use Tests\Antlers\ParserTestCase;
+
+class VariablesTest extends ParserTestCase
+{
+
+    public function test_value_objects_can_be_used_for_array_lookups()
+    {
+        $template = <<<'EOT'
+<{{ sizes[size] }}><{{ sizes[size_two] }}><{{ sizes[size_three] }}><{{ view:sizes[size] }}><{{ view:sizes[size_two] }}><{{ view:sizes[size_three] }}>
+EOT;
+
+        $value = new LabeledValue('xl', 'XL');
+        $valueTwo = new LabeledValue('md', 'MD');
+        $valueThree = new LabeledValue('lg', 'LG');
+
+        $sizes = [
+            'md' => '(min-width: 768px) 55vw, 90vw',
+            'lg' => '(min-width: 768px) 75vw, 90vw',
+            'xl' => '90vw'
+        ];
+
+        $data = [
+            'sizes' => $sizes,
+            'view' => [
+                'sizes' => $sizes,
+            ],
+            'size' => $value,
+            'size_two' => $valueTwo,
+            'size_three' => $valueThree
+        ];
+
+        $this->assertSame('<90vw><(min-width: 768px) 55vw, 90vw><(min-width: 768px) 75vw, 90vw><90vw><(min-width: 768px) 55vw, 90vw><(min-width: 768px) 75vw, 90vw>', $this->renderString(
+            $template, $data
+        ));
+    }
+
+}
