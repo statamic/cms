1fc23bf06a0b2fcfca42b4018ad689b07a2e3045
Jack McDade <jack@jackmcdade.com>
2022-02-28 13:56:17 -0500

    Use PDFObject from Mozilla for a super simple local PDF viewing experience
    5 stars would PDF again

--- resources/js/components/assets/Editor/PdfViewer.vue
+++ resources/js/components/assets/Editor/PdfViewer.vue
@@ -1,64 +1,20 @@
 <template>
-    <div>
-        <div class="flex bg-black text-white px-2 py-1 justify-center space-x-4 text-sm">
-            <div class="flex">
-                <a :title="__('Zoom In')" class="text-white p-1 text-xl leading-none" @click="scaleDown">
-                    -
-                </a>
-                <a class="text-white p-1">
-                    {{ formattedZoom }} %
-                </a>
-                <a title="__('Zoom Out')" class="text-white p-1 text-xl leading-none" @click="scaleUp">
-                    +
-                </a>
-            </div>
-        </div>
-        <pdf :src="src" :scale.sync="scale" :page="page" :annotation="true" :resize="true" class="w-full h-full mx-2">
-            <template slot="loading">
-                {{ __('A wild PDF appears!') }}
-            </template>
-        </pdf>
-    </div>
+    <div id="pdfViewer" class="h-full text-white text-center flex flex-col justify-center items-center"></div>
 </template>
 
 <script>
-import Pdf from 'pdfvuer';
+import PDFObject from 'PDFObject';
 
 export default {
 
-    components: {
-        Pdf
-    },
-
     props: {
         src: {
-            type: String,
             required: true
         }
     },
 
-    data () {
-        return {
-            page: 1,
-            numPages: 0,
-            scale: 'page-width'
-        }
+    mounted() {
+        PDFObject.embed(this.src, "#pdfViewer");
     },
-
-    computed: {
-        formattedZoom () {
-            return Number.parseInt(this.scale * 100)
-        },
-    },
-
-    methods: {
-        scaleDown() {
-            this.scale = this.scale * 0.85;
-        },
-        scaleUp() {
-            this.scale = this.scale * 1.15;
-        }
-    },
-
 }
 </script>
