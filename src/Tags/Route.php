160493c0e3246a315ba14fd010b034975d799179
Ryan Mitchell <ryan@thoughtcollective.com>
2022-03-04 08:34:27 +0000

    Add index method to route tag

--- src/Tags/Route.php
+++ src/Tags/Route.php
@@ -4,6 +4,11 @@ namespace Statamic\Tags;
 
 class Route extends Tags
 {
+    public function index()
+    {
+        return $this->wildcard($this->params->get('name'));
+    }
+
     public function wildcard($tag)
     {
         return route($tag, $this->params->except('name')->all());
