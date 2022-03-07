cc5f08e3149408c6bb1eaa3dca0f1a76d97602c2
Jason Varga <jason@pixelfear.com>
2022-03-07 10:30:57 -0500

    Extract method and use carbon instances ...
    At the moment search indexes will contain date strings, but since this test is about the querying part, it's fine to just plop carbon instances in there.
    Getting carbon instances into the search indexes can be solved separately.

--- tests/Search/QueryBuilderTest.php
+++ tests/Search/QueryBuilderTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Search;
 
+use Illuminate\Support\Carbon;
 use Statamic\Search\QueryBuilder;
 use Tests\PreventSavingStacheItemsToDisk;
 use Tests\TestCase;
@@ -56,13 +57,7 @@ class QueryBuilderTest extends TestCase
     /** @test **/
     public function results_are_found_using_where_date()
     {
-        $items = collect([
-            ['reference' => 'a', 'test_date' => 1637008264],
-            ['reference' => 'b', 'test_date' => '2021-11-14 09:00:00'],
-            ['reference' => 'c', 'test_date' => '2021-11-15'],
-            ['reference' => 'd', 'test_date' => 1627008264],
-            ['reference' => 'e', 'test_date' => null],
-        ]);
+        $items = $this->createWhereDateTestItems();
 
         $results = (new FakeQueryBuilder($items))->withoutData()->whereDate('test_date', '2021-11-15')->get();
 
@@ -83,13 +78,7 @@ class QueryBuilderTest extends TestCase
     /** @test **/
     public function results_are_found_using_where_month()
     {
-        $items = collect([
-            ['reference' => 'a', 'test_date' => 1637008264],
-            ['reference' => 'b', 'test_date' => '2021-11-14 09:00:00'],
-            ['reference' => 'c', 'test_date' => '2021-11-15'],
-            ['reference' => 'd', 'test_date' => 1627008264],
-            ['reference' => 'e', 'test_date' => null],
-        ]);
+        $items = $this->createWhereDateTestItems();
 
         $results = (new FakeQueryBuilder($items))->withoutData()->whereMonth('test_date', 11)->get();
 
@@ -105,13 +94,7 @@ class QueryBuilderTest extends TestCase
     /** @test **/
     public function results_are_found_using_where_day()
     {
-        $items = collect([
-            ['reference' => 'a', 'test_date' => 1637008264],
-            ['reference' => 'b', 'test_date' => '2021-11-14 09:00:00'],
-            ['reference' => 'c', 'test_date' => '2021-11-15'],
-            ['reference' => 'd', 'test_date' => 1627008264],
-            ['reference' => 'e', 'test_date' => null],
-        ]);
+        $items = $this->createWhereDateTestItems();
 
         $results = (new FakeQueryBuilder($items))->withoutData()->whereDay('test_date', 15)->get();
 
@@ -120,20 +103,14 @@ class QueryBuilderTest extends TestCase
 
         $results = (new FakeQueryBuilder($items))->withoutData()->whereDay('test_date', '<', 15)->get();
 
-        $this->assertCount(1, $results);
-        $this->assertEquals(['b'], $results->map->reference->all());
+        $this->assertCount(2, $results);
+        $this->assertEquals(['b', 'd'], $results->map->reference->all());
     }
 
     /** @test **/
     public function results_are_found_using_where_year()
     {
-        $items = collect([
-            ['reference' => 'a', 'test_date' => 1637008264],
-            ['reference' => 'b', 'test_date' => '2021-11-14 09:00:00'],
-            ['reference' => 'c', 'test_date' => '2021-11-15'],
-            ['reference' => 'd', 'test_date' => 1600008264],
-            ['reference' => 'e', 'test_date' => null],
-        ]);
+        $items = $this->createWhereDateTestItems();
 
         $results = (new FakeQueryBuilder($items))->withoutData()->whereYear('test_date', 2021)->get();
 
@@ -149,13 +126,7 @@ class QueryBuilderTest extends TestCase
     /** @test **/
     public function results_are_found_using_where_time()
     {
-        $items = collect([
-            ['reference' => 'a', 'test_date' => 1637008264],
-            ['reference' => 'b', 'test_date' => '2021-11-14 09:00:00'],
-            ['reference' => 'c', 'test_date' => '2021-11-15'],
-            ['reference' => 'd', 'test_date' => 1600008264],
-            ['reference' => 'e', 'test_date' => null],
-        ]);
+        $items = $this->createWhereDateTestItems();
 
         $results = (new FakeQueryBuilder($items))->withoutData()->whereTime('test_date', '09:00')->get();
 
@@ -166,6 +137,17 @@ class QueryBuilderTest extends TestCase
 
         $this->assertCount(2, $results);
         $this->assertEquals(['a', 'd'], $results->map->reference->all());
+    }
+
+    private function createWhereDateTestItems()
+    {
+        return collect([
+            ['reference' => 'a', 'test_date' => Carbon::parse('2021-11-15 20:31:04')],
+            ['reference' => 'b', 'test_date' => Carbon::parse('2021-11-14 09:00:00')],
+            ['reference' => 'c', 'test_date' => Carbon::parse('2021-11-15 00:00:00')],
+            ['reference' => 'd', 'test_date' => Carbon::parse('2020-09-13 14:44:24')],
+            ['reference' => 'e', 'test_date' => null],
+        ]);
     }
 
     /** @test */
