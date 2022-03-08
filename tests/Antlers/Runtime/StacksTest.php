798faa37828157b19607a6506aa07ea19f856fda
John Koster <john@stillat.com>
2022-03-08 09:46:19 -0600

    Antlers Runtime Bugfixes (#5428)

--- tests/Antlers/Runtime/StacksTest.php
+++ tests/Antlers/Runtime/StacksTest.php
@@ -41,4 +41,40 @@ EOT;
 
         $this->assertSame('BEFOREPrepend 2Prepend 1Push 1Push 2AFTER', trim($this->renderString($template, [], true)));
     }
+
+    public function test_stacks_and_sections_work_from_partials()
+    {
+        $template = <<<'EOT'
+{{ partial:stacksections }}
+{{ stack:the_stack }}
+{{ yield:the_section }}
+
+{{ push:the_stack }}<More stack content>{{ /push:the_stack }}
+
+EOT;
+
+        $expected = <<<'EOT'
+<The stack content><More stack content>
+<Section Content>
+EOT;
+
+        $this->assertSame($expected, trim($this->renderString($template, [], true)));
+
+        // The section content should be replaced with "Different Section Content" since it is processed _after_ the partial.
+        $template = <<<'EOT'
+{{ partial:stacksections }}
+{{ stack:the_stack }}
+{{ yield:the_section }}
+
+{{ push:the_stack }}<More stack content>{{ /push:the_stack }}
+{{ section:the_section }}<Different Section Content>{{ /section:the_section }}
+EOT;
+
+        $expected = <<<'EOT'
+<The stack content><More stack content>
+<Different Section Content>
+EOT;
+
+        $this->assertSame($expected, trim($this->renderString($template, [], true)));
+    }
 }
