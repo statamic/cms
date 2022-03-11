cf7f4b719c09160de8d7e1b3c8623728b30b0053
Jesse Leite <jesseleite@gmail.com>
2022-03-11 13:10:13 -0500

    Fix `ExtractInfo` size call between Laravel 8 and 9 (#5470)

---
+++ tests/Assets/ExtractInfoTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Assets;
+
+use Statamic\Assets\Asset;
+use Statamic\Assets\AssetContainer;
+use Statamic\Assets\ExtractInfo;
+use Tests\TestCase;
+
+class ExtractInfoTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        config(['filesystems.disks.test' => [
+            'driver' => 'local',
+            'root' => __DIR__.'/__fixtures__/container',
+        ]]);
+
+        $this->container = (new AssetContainer)
+            ->handle('test_container')
+            ->disk('test');
+    }
+
+    /** @test */
+    public function it_can_extract_basic_id3_info_from_text_asset()
+    {
+        $asset = (new Asset)->container($this->container)->path('a.txt');
+
+        $extracted = (new ExtractInfo)->fromAsset($asset);
+
+        $expected = [
+            'filesize' => $this->isRunningWindows() ? 3 : 2,
+            'filename' => 'a.txt',
+            'encoding' => 'UTF-8',
+        ];
+
+        $this->assertArraySubset($expected, $extracted);
+    }
+}
