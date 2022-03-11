8a9939991d85dfea09d8be70789d2e00145f0f68
Russell Galpin <russ@lrhosting.net>
2022-03-11 13:47:33 +0000

    Make backwards compatible

--- src/Assets/ExtractInfo.php
+++ src/Assets/ExtractInfo.php
@@ -11,6 +11,6 @@ class ExtractInfo
         $disk = $asset->disk()->filesystem();
         $path = $asset->path();
 
-        return (new \getID3)->analyze($path, $disk->fileSize($path), '', $disk->readStream($path));
+        return (new \getID3)->analyze($path, method_exists($disk, 'getSize') ? $disk->getSize($path) : $disk->fileSize($path), '', $disk->readStream($path));
     }
 }
