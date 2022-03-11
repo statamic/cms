9f708fd96fd97444c3c5ef3c0467b1a5fff73a56
Russell Galpin <russ@lrhosting.net>
2022-03-11 13:37:30 +0000

    Flysystem V2 API change

--- src/Assets/ExtractInfo.php
+++ src/Assets/ExtractInfo.php
@@ -11,6 +11,6 @@ class ExtractInfo
         $disk = $asset->disk()->filesystem();
         $path = $asset->path();
 
-        return (new \getID3)->analyze($path, $disk->getSize($path), '', $disk->readStream($path));
+        return (new \getID3)->analyze($path, $disk->fileSize($path), '', $disk->readStream($path));
     }
 }
