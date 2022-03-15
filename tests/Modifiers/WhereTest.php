ae15c9dccc769fc0095c580c0b05637d0937c285
Jason Varga <jason@pixelfear.com>
2022-03-14 22:31:54 -0400

    Support colon syntax in where modifier (#5499)

--- tests/Modifiers/WhereTest.php
+++ tests/Modifiers/WhereTest.php
@@ -23,6 +23,22 @@ class WhereTest extends TestCase
         $this->assertEquals($expected, array_pluck($modified, 'title'));
     }
 
+    /** @test */
+    public function it_has_a_workaround_for_colon_syntax()
+    {
+        // Before the runtime parser fixed the argument inconsistency, many
+        // people probably used the colon syntax. We'll just keep it working.
+
+        $games = [
+            ['feeling' => 'love', 'title' => 'Dominion'],
+            ['feeling' => 'love', 'title' => 'Netrunner'],
+            ['feeling' => 'hate', 'title' => 'Chutes and Ladders'],
+        ];
+        $expected = ['Dominion', 'Netrunner'];
+        $modified = $this->modify($games, ['feeling:love']);
+        $this->assertEquals($expected, array_pluck($modified, 'title'));
+    }
+
     private function modify($value, array $params)
     {
         return Modify::value($value)->where($params)->fetch();
