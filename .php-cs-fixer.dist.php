314a09476a44a8326251062a8785ac346595adca
Jesse Leite <jesseleite@gmail.com>
2022-03-16 18:38:39 -0400

    Update rules.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -17,11 +17,14 @@ $rules = [
     'braces' => true,
     'cast_spaces' => true,
     'class_attributes_separation' => [
-        'elements' => ['method']
+        'elements' => ['method' => 'one']
     ],
     'class_definition' => true,
     'concat_space' => [
         'spacing' => 'none'
+    ],
+    'constant_case' => [
+        'case' => 'lower'
     ],
     'declare_equal_normalize' => true,
     'elseif' => true,
@@ -30,6 +33,7 @@ $rules = [
     'fully_qualified_strict_types' => true, // added by Shift
     'function_declaration' => true,
     'function_typehint_space' => true,
+    'general_phpdoc_tag_rename' => true,
     'heredoc_to_nowdoc' => true,
     'include' => true,
     'increment_style' => ['style' => 'post'],
@@ -37,7 +41,6 @@ $rules = [
     'linebreak_after_opening_tag' => true,
     'line_ending' => true,
     'lowercase_cast' => true,
-    'lowercase_constants' => true,
     'lowercase_keywords' => true,
     'lowercase_static_reference' => true, // added from Symfony
     'magic_method_casing' => true, // added from Symfony
@@ -85,20 +88,21 @@ $rules = [
     'normalize_index_brace' => true,
     'not_operator_with_successor_space' => true,
     'object_operator_without_whitespace' => true,
-    'ordered_imports' => ['sortAlgorithm' => 'alpha'],
+    'ordered_imports' => ['sort_algorithm' => 'alpha'],
     'phpdoc_indent' => true,
-    'phpdoc_inline_tag' => true,
+    'phpdoc_inline_tag_normalizer' => true,
     'phpdoc_no_access' => true,
     'phpdoc_no_package' => true,
     'phpdoc_no_useless_inheritdoc' => true,
     'phpdoc_scalar' => true,
     'phpdoc_single_line_var_spacing' => true,
     'phpdoc_summary' => true,
+    'phpdoc_tag_type' => true,
     'phpdoc_to_comment' => true,
     'phpdoc_trim' => true,
     'phpdoc_types' => true,
     'phpdoc_var_without_name' => true,
-    'psr4' => true,
+    'psr_autoloading' => true,
     'self_accessor' => true,
     'short_scalar_cast' => true,
     'simplified_null_return' => false, // disabled by Shift
@@ -116,7 +120,9 @@ $rules = [
     'switch_case_semicolon_to_colon' => true,
     'switch_case_space' => true,
     'ternary_operator_spaces' => true,
-    'trailing_comma_in_multiline_array' => true,
+    'trailing_comma_in_multiline' => [
+        'elements' => ['arrays']
+    ],
     'trim_array_spaces' => true,
     'unary_operator_spaces' => true,
     'visibility_required' => [
