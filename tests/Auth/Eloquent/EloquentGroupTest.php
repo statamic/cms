4df6334fcd46379c243d2e46aa6f24a16a440dd0
Ryan Mitchell <ryan@thoughtcollective.com>
2022-03-29 20:13:47 +0100

    Add basic tests

---
+++ tests/Auth/Eloquent/EloquentGroupTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Tests\Auth\Eloquent;
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Schema;
+use Statamic\Auth\Eloquent\UserGroup as EloquentGroup;
+use Statamic\Auth\Eloquent\UserGroupModel;
+use Statamic\Auth\Eloquent\User as EloquentUser;
+use Statamic\Console\Please\Kernel;
+use Statamic\Facades;
+use Tests\TestCase;
+
+class EloquentGroupTest extends TestCase
+{
+    use WithFaker;
+
+    public static $migrationsGenerated = false;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        Carbon::setTestNow(Carbon::create(2019, 11, 21, 23, 39, 29));
+
+        config([
+            'statamic.users.repository' => 'eloquent',
+            'statamic.users.tables.groups' => 'groups',
+        ]);
+
+        $this->migrationsDir = __DIR__.'/__migrations__';
+
+        if (! self::$migrationsGenerated) {
+            $this->please('auth:migration', ['--path' => $this->migrationsDir]);
+
+            self::$migrationsGenerated = true;
+        }
+
+        $this->loadMigrationsFrom($this->migrationsDir);
+    }
+
+    public function tearDown(): void
+    {
+        // our down() migration sets password to not be nullable, so change it back
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('password')->nullable(true)->change();
+        });
+    }
+
+    private function please($command, $parameters = [])
+    {
+        return $this->app[Kernel::class]->call($command, $parameters);
+    }
+
+    public function makeGroup()
+    {
+        return (new EloquentGroup)
+            ->model(UserGroupModel::create([
+                'handle' => $this->faker->word,
+                'title' => $this->faker->words(2, true),
+                'roles' => [],
+            ])
+        );
+    }
+
+    public function makeUser()
+    {
+        return (new EloquentUser)
+            ->model(User::create([
+                'name' => $this->faker->name,
+                'email' => $this->faker->unique()->safeEmail,
+                // 'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+                'remember_token' => str_random(10),
+            ])
+        );
+    }
+
+    /** @test */
+    public function it_creates_a_group()
+    {
+        $group = $this->makeGroup();
+
+        $this->assertInstanceOf(EloquentGroup::class, $group);
+    }
+}
---
+++ tests/Auth/Eloquent/EloquentRoleTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Tests\Auth\Eloquent;
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Schema;
+use Statamic\Auth\Eloquent\Role as EloquentRole;
+use Statamic\Auth\Eloquent\RoleModel;
+use Statamic\Auth\Eloquent\User as EloquentUser;
+use Statamic\Console\Please\Kernel;
+use Statamic\Facades;
+use Tests\TestCase;
+
+class EloquentRoleTest extends TestCase
+{
+    use WithFaker;
+
+    public static $migrationsGenerated = false;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        Carbon::setTestNow(Carbon::create(2019, 11, 21, 23, 39, 29));
+
+        config([
+            'statamic.users.repository' => 'eloquent',
+            'statamic.users.tables.roles' => 'roles',
+        ]);
+
+        $this->migrationsDir = __DIR__.'/__migrations__';
+
+        if (! self::$migrationsGenerated) {
+            $this->please('auth:migration', ['--path' => $this->migrationsDir]);
+
+            self::$migrationsGenerated = true;
+        }
+
+        $this->loadMigrationsFrom($this->migrationsDir);
+    }
+
+    public function tearDown(): void
+    {
+        // our down() migration sets password to not be nullable, so change it back
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('password')->nullable(true)->change();
+        });
+    }
+
+    private function please($command, $parameters = [])
+    {
+        return $this->app[Kernel::class]->call($command, $parameters);
+    }
+
+    public function makeRole()
+    {
+        return (new EloquentRole)
+            ->model(RoleModel::create([
+                'handle' => $this->faker->word,
+                'title' => $this->faker->words(2, true),
+                'permissions' => [],
+                'preferences' => [],
+            ])
+        );
+    }
+
+    public function makeUser()
+    {
+        return (new EloquentUser)
+            ->model(User::create([
+                'name' => $this->faker->name,
+                'email' => $this->faker->unique()->safeEmail,
+                // 'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+                'remember_token' => str_random(10),
+            ])
+        );
+    }
+
+    /** @test */
+    public function it_creates_a_role()
+    {
+        $role = $this->makeRole();
+
+        $this->assertInstanceOf(EloquentRole::class, $role);
+    }
+}
