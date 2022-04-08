22b9843a6e5e2ff7426085230885ef8214719e72
Jesse Leite <jesseleite@gmail.com>
2022-04-08 19:00:26 -0400

    Handle hidden fields within bard json values.

--- resources/js/components/data-list/HasHiddenFields.js
+++ resources/js/components/data-list/HasHiddenFields.js
@@ -13,6 +13,12 @@ export default {
                 .pick(hidden => hidden)
                 .keys()
                 .each(dottedKey => {
+                    let parent = this.dottedKeyToParentHandle(dottedKey);
+
+                    if (this.parentFieldSubmitsJson(parent)) {
+                        return visibleValues[parent] = this.forgetFromChildJson(visibleValues[parent], dottedKey);
+                    }
+
                     eval('delete visibleValues.' + this.dottedKeyToJsProperty(dottedKey));
                 });
 
@@ -25,6 +31,42 @@ export default {
 
         dottedKeyToJsProperty(dottedKey) {
             return dottedKey.replace(/\.*(\d+)\./g, '[$1].');
+        },
+
+        dottedKeyToParentHandle(dottedKey) {
+            return dottedKey.replace(/\..*$/, '');
+        },
+
+        dottedKeyToChildHandle(dottedKey) {
+            return dottedKey.replace(/^[^\.]*\./, '');
+        },
+
+        getParentFieldType(handle) {
+            let fields = [];
+
+            this.$store.state.publish[this.publishContainer].blueprint.sections.forEach(section => {
+                fields = fields.concat(section.fields);
+            });
+
+            let parentFields = _.object(_.map(fields, function(field) {
+               return [field.handle, field];
+            }));
+
+            return parentFields[handle].type;
+        },
+
+        parentFieldSubmitsJson(handle) {
+            return this.getParentFieldType(handle) === 'bard';
+        },
+
+        forgetFromChildJson(json, dottedKey) {
+            let updatedJsonObject = JSON.parse(json);
+
+            let childKey = this.dottedKeyToChildHandle(dottedKey);
+
+            eval('delete updatedJsonObject' + this.dottedKeyToJsProperty(childKey));
+
+            return JSON.stringify(updatedJsonObject);
         },
 
     },
