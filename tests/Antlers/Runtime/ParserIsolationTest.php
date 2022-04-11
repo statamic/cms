9028f248f43933c49dca2289bb06db4abc8aac26
Jason Varga <jason@pixelfear.com>
2022-04-11 16:03:00 -0400

    make test less reliant on taxonomy

--- tests/Antlers/Runtime/ParserIsolationTest.php
+++ tests/Antlers/Runtime/ParserIsolationTest.php
@@ -2,12 +2,9 @@
 
 namespace Tests\Antlers\Runtime;
 
-use Facades\Statamic\Fields\BlueprintRepository;
 use Facades\Tests\Factories\EntryFactory;
-use Statamic\Facades\Blueprint;
 use Statamic\Facades\Collection;
-use Statamic\Facades\Taxonomy;
-use Statamic\Facades\Term;
+use Statamic\View\Antlers\Language\Utilities\StringUtilities;
 use Tests\FakesViews;
 use Tests\PreventSavingStacheItemsToDisk;
 use Tests\TestCase;
@@ -19,36 +16,19 @@ class ParserIsolationTest extends TestCase
 
     private function createBlueprintsAndData()
     {
-        Taxonomy::make('topics')->save();
-        Term::make()->taxonomy('topics')->inDefaultLocale()->slug('dance')->data([])->save();
+        Collection::make('news')->routes('{foo}/{slug}')->save();
 
-        $news = Blueprint::makeFromFields([
-            'title' => ['type' => 'text'],
-            'content' => ['type' => 'text'],
-            'topic' => [
-                'type' => 'terms',
-                'taxonomies' => ['topics'],
-                'max_items' => 1,
-            ],
-        ]);
-
-        BlueprintRepository::shouldReceive('in')->with('collections/news')->andReturn(collect([
-            'news' => $news->setHandle('news'),
-        ]));
-
-        Collection::make('news')->routes(['en' => '{topic}/{slug}'])->save();
-
-        EntryFactory::collection('news')->id('1')
-                ->slug('news-1')->data([
-                    'title' => 'News 1',
-                    'content' => 'News 1 Content',
-                ])->create();
-        EntryFactory::collection('news')->id('2')
-                ->slug('news-2')->data([
-                    'title' => 'News 2',
-                    'content' => 'News 2 Content',
-                    'topic' => 'dance',
-                ])->create();
+        EntryFactory::collection('news')->id('1')->slug('news-1')->data([
+            'title' => 'News 1',
+            'foo' => 'alfa',
+        ])->create();
+        EntryFactory::collection('news')->id('2')->slug('news-2')->data([
+            'title' => 'News 2',
+            'foo' => 'bravo',
+        ])->create();
+        EntryFactory::collection('news')->id('3')->slug('news-3')->data([
+            'title' => 'News 3',
+        ])->create();
     }
 
     public function test_context_data_does_not_leak_when_resolving_augmented_Values()
@@ -58,18 +38,22 @@ class ParserIsolationTest extends TestCase
         $this->viewShouldReturnRaw('layout', '{{ template_content }}');
 
         $template = <<<'EOT'
-{{ collection:news }}{{ /collection:news }}
-<{{ title }}><{{ content }}>
+Entry: <{{ title }}><{{ url }}>
+Loop: {{ collection:news }}<{{ title }}><{{ uri }}>{{ /collection:news }}
 EOT;
 
         $this->app['config']->set('statamic.antlers.fatalErrorOnPrintObjects', true);
         $this->viewShouldReturnRaw('default', $template);
 
-        $responseOne = $this->get('dance/news-2')->assertOk();
+        $expected = <<<'EOT'
+Entry: <News 2></bravo/news-2>
+Loop: <News 1></alfa/news-1><News 2></bravo/news-2><News 3></news-3>
+EOT;
 
-        $responseTwo = $this->get('news-1')->assertOk();
+        $response = $this
+            ->get('bravo/news-2')
+            ->assertOk();
 
-        $this->assertSame('<News 2><News 2 Content>', trim($responseOne->content()));
-        $this->assertSame('<News 1><News 1 Content>', trim($responseTwo->content()));
+        $this->assertSame($expected, StringUtilities::normalizeLineEndings($response->content()));
     }
 }
