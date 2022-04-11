31bfee10590c625889df32c328c7b155f5cfd1a2
Jesse Leite <jesseleite@gmail.com>
2022-04-11 19:40:53 -0400

    Remove old filtering logic in favour of more robust `Omitter.js` implementation.

--- resources/js/components/data-list/HasHiddenFields.js
+++ resources/js/components/data-list/HasHiddenFields.js
@@ -1,3 +1,5 @@
+import HiddenValuesOmitter from '../field-conditions/Omitter.js';
+
 export default {
 
     computed: {
@@ -6,69 +8,19 @@ export default {
             return this.$store.state.publish[this.publishContainer].hiddenFields;
         },
 
-        visibleValues() {
-            let visibleValues = clone(this.values);
+        jsonSubmittingFields() {
+            return this.$store.state.publish[this.publishContainer].jsonSubmittingFields;
+        },
 
-            let hiddenKeys = _.chain(this.hiddenFields)
+        visibleValues() {
+            let hiddenFields = _.chain(this.hiddenFields)
                 .pick(hidden => hidden)
                 .keys()
-                .each(dottedKey => {
-                    let parent = this.dottedKeyToParentHandle(dottedKey);
+                .value();
 
-                    if (this.parentFieldSubmitsJson(parent)) {
-                        return visibleValues[parent] = this.forgetFromChildJson(visibleValues[parent], dottedKey);
-                    }
-
-                    eval('delete visibleValues.' + this.dottedKeyToJsProperty(dottedKey));
-                });
-
-            return visibleValues;
+            return new HiddenValuesOmitter(this.values, this.jsonSubmittingFields).omit(hiddenFields);
         },
 
-    },
-
-    methods: {
-
-        dottedKeyToJsProperty(dottedKey) {
-            return dottedKey.replace(/\.*(\d+)\./g, '[$1].');
-        },
-
-        dottedKeyToParentHandle(dottedKey) {
-            return dottedKey.replace(/\..*$/, '');
-        },
-
-        dottedKeyToChildHandle(dottedKey) {
-            return dottedKey.replace(/^[^\.]*\./, '');
-        },
-
-        getParentFieldType(handle) {
-            let fields = [];
-
-            this.$store.state.publish[this.publishContainer].blueprint.sections.forEach(section => {
-                fields = fields.concat(section.fields);
-            });
-
-            let parentFields = _.object(_.map(fields, function(field) {
-               return [field.handle, field];
-            }));
-
-            return parentFields[handle].type;
-        },
-
-        parentFieldSubmitsJson(handle) {
-            return this.getParentFieldType(handle) === 'bard';
-        },
-
-        forgetFromChildJson(json, dottedKey) {
-            let updatedJsonObject = JSON.parse(json);
-
-            let childKey = this.dottedKeyToChildHandle(dottedKey);
-
-            eval('delete updatedJsonObject' + this.dottedKeyToJsProperty(childKey));
-
-            return JSON.stringify(updatedJsonObject);
-        },
-
-    },
+    }
 
 }
