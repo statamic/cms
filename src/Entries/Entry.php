ffddb6a81195427449408a179616c1519c058f70
Ryan Mitchell <ryan@thoughtcollective.com>
2022-04-12 14:20:30 +0100

    Fix bug when adding time enabled date

--- src/Entries/Entry.php
+++ src/Entries/Entry.php
@@ -443,6 +443,10 @@ class Entry implements Contract, Augmentable, Responsable, Localization, Protect
                     return Carbon::createFromFormat('Y-m-d', $date)->startOfDay();
                 }
 
+                if (strlen($date) === 17) {
+                    return Carbon::createFromFormat('Y-m-d-His', $date);
+                }
+
                 return Carbon::createFromFormat('Y-m-d-Hi', $date);
             })
             ->args(func_get_args());
