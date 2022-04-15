6cd963b2a6f7735b5dbef2f5e3fe1ebe40ed5655
Duncan McClean <duncan@mcclean.co.uk>
2022-04-15 15:35:30 +0100

    Bulk Actions

--- resources/js/components/forms/Listing.vue
+++ resources/js/components/forms/Listing.vue
@@ -1,7 +1,14 @@
 <template>
     <data-list :visible-columns="columns" :columns="columns" :rows="items">
         <div class="card p-0" slot-scope="{ filteredRows: rows }">
-            <data-list-table>
+            <data-list-bulk-actions
+                class="rounded"
+                :url="actionUrl"
+                @started="actionStarted"
+                @completed="actionCompleted"
+            />
+
+            <data-list-table :allow-bulk-actions="true">
                 <template slot="cell-title" slot-scope="{ row: form }">
                     <a :href="form.show_url">{{ form.title }}</a>
                 </template>
