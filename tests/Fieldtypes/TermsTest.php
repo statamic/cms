3397ff1a9394c30801e4f236d027490e36f3b453
Jason Varga <jason@pixelfear.com>
2022-04-19 14:26:56 -0400

    attempt to make this test clearer

--- tests/Fieldtypes/TermsTest.php
+++ tests/Fieldtypes/TermsTest.php
@@ -338,7 +338,7 @@ class TermsTest extends TestCase
      * @test
      * @dataProvider collectionAttachmentProvider
      **/
-    public function it_attaches_collection_during_augmentation($parentIsEntry, $handle, $isRootLevel, $collectionUsesTaxonomy)
+    public function it_attaches_collection_during_augmentation($expectCollection, $parentIsEntry, $handle, $isRootLevel, $collectionUsesTaxonomy)
     {
         if ($collectionUsesTaxonomy) {
             Facades\Collection::find('blog')->taxonomies(['tags'])->save();
@@ -368,7 +368,7 @@ class TermsTest extends TestCase
 
         $collection = $augmented->first()->collection();
 
-        if ($parentIsEntry && $handle === 'tags' && $isRootLevel && $collectionUsesTaxonomy) {
+        if ($expectCollection) {
             $this->assertInstanceOf(CollectionContract::class, $collection);
         } else {
             $this->assertNull($collection);
@@ -377,26 +377,60 @@ class TermsTest extends TestCase
 
     public function collectionAttachmentProvider()
     {
+        $expectCollection = $parentIsEntry = $isRootLevel = $collectionUsesTaxonomy = true;
+        $dontExpectCollection = $parentIsNotEntry = $isNested = $collectionDoesNotUseTaxonomy = false;
+        $fieldHandleMatchesTaxonomy = 'tags';
+        $fieldHandleDoesNotMatchTaxonomy = 'related_tags';
+
         return [
-            'parent is entry and handle matches taxonomy' => [true, 'tags', true, true],
-            'parent is entry and handle does not match taxonomy' => [true, 'related_tags', true, true],
-            'parent is not entry and handle matches taxonomy' => [false, 'tags', true, true],
-            'parent is not entry and handle does not match taxonomy' => [false, 'related_tags', true, true],
-
-            'parent is entry, handle matches taxonomy, nested field' => [true, 'tags', false, true],
-            'parent is entry, handle does not match taxonomy, nested field' => [true, 'related_tags', false, true],
-            'parent is not entry, handle matches taxonomy, nested field' => [false, 'tags', false, true],
-            'parent is not entry, handle does not match taxonomy, nested field' => [false, 'related_tags', false, true],
-
-            'parent is entry and handle matches taxonomy, collection doesnt use taxonomy' => [true, 'tags', true, false],
-            'parent is entry and handle does not match taxonomy, collection doesnt use taxonomy' => [true, 'related_tags', true, false],
-            'parent is not entry and handle matches taxonomy, collection doesnt use taxonomy' => [false, 'tags', true, false],
-            'parent is not entry and handle does not match taxonomy, collection doesnt use taxonomy' => [false, 'related_tags', true, false],
-
-            'parent is entry, handle matches taxonomy, nested field, collection doesnt use taxonomy' => [true, 'tags', false, false],
-            'parent is entry, handle does not match taxonomy, nested field, collection doesnt use taxonomy' => [true, 'related_tags', false, false],
-            'parent is not entry, handle matches taxonomy, nested field, collection doesnt use taxonomy' => [false, 'tags', false, false],
-            'parent is not entry, handle does not match taxonomy, nested field, collection doesnt use taxonomy' => [false, 'related_tags', false, false],
+            'parent is entry and handle matches taxonomy' => [
+                $expectCollection, $parentIsEntry, $fieldHandleMatchesTaxonomy, $isRootLevel, $collectionUsesTaxonomy,
+            ],
+            'parent is entry and handle does not match taxonomy' => [
+                $dontExpectCollection, $parentIsEntry, $fieldHandleDoesNotMatchTaxonomy, $isRootLevel, $collectionUsesTaxonomy,
+            ],
+            'parent is not entry and handle matches taxonomy' => [
+                $dontExpectCollection, $parentIsNotEntry, $fieldHandleMatchesTaxonomy, $isRootLevel, $collectionUsesTaxonomy,
+            ],
+            'parent is not entry and handle does not match taxonomy' => [
+                $dontExpectCollection, $parentIsNotEntry, $fieldHandleDoesNotMatchTaxonomy, $isRootLevel, $collectionUsesTaxonomy,
+            ],
+            'parent is entry, handle matches taxonomy, nested field' => [
+                $dontExpectCollection, $parentIsEntry, $fieldHandleMatchesTaxonomy, $isNested, $collectionUsesTaxonomy,
+            ],
+            'parent is entry, handle does not match taxonomy, nested field' => [
+                $dontExpectCollection, $parentIsEntry, $fieldHandleDoesNotMatchTaxonomy, $isNested, $collectionUsesTaxonomy,
+            ],
+            'parent is not entry, handle matches taxonomy, nested field' => [
+                $dontExpectCollection, $parentIsNotEntry, $fieldHandleMatchesTaxonomy, $isNested, $collectionUsesTaxonomy,
+            ],
+            'parent is not entry, handle does not match taxonomy, nested field' => [
+                $dontExpectCollection, $parentIsNotEntry, $fieldHandleDoesNotMatchTaxonomy, $isNested, $collectionUsesTaxonomy,
+            ],
+            'parent is entry and handle matches taxonomy, collection doesnt use taxonomy' => [
+                $dontExpectCollection, $parentIsEntry, $fieldHandleMatchesTaxonomy, $isRootLevel, $collectionDoesNotUseTaxonomy,
+            ],
+            'parent is entry and handle does not match taxonomy, collection doesnt use taxonomy' => [
+                $dontExpectCollection, $parentIsEntry, $fieldHandleDoesNotMatchTaxonomy, $isRootLevel, $collectionDoesNotUseTaxonomy,
+            ],
+            'parent is not entry and handle matches taxonomy, collection doesnt use taxonomy' => [
+                $dontExpectCollection, $parentIsNotEntry, $fieldHandleMatchesTaxonomy, $isRootLevel, $collectionDoesNotUseTaxonomy,
+            ],
+            'parent is not entry and handle does not match taxonomy, collection doesnt use taxonomy' => [
+                $dontExpectCollection, $parentIsNotEntry, $fieldHandleDoesNotMatchTaxonomy, $isRootLevel, $collectionDoesNotUseTaxonomy,
+            ],
+            'parent is entry, handle matches taxonomy, nested field, collection doesnt use taxonomy' => [
+                $dontExpectCollection, $parentIsEntry, $fieldHandleMatchesTaxonomy, $isNested, $collectionDoesNotUseTaxonomy,
+            ],
+            'parent is entry, handle does not match taxonomy, nested field, collection doesnt use taxonomy' => [
+                $dontExpectCollection, $parentIsEntry, $fieldHandleDoesNotMatchTaxonomy, $isNested, $collectionDoesNotUseTaxonomy,
+            ],
+            'parent is not entry, handle matches taxonomy, nested field, collection doesnt use taxonomy' => [
+                $dontExpectCollection, $parentIsNotEntry, $fieldHandleMatchesTaxonomy, $isNested, $collectionDoesNotUseTaxonomy,
+            ],
+            'parent is not entry, handle does not match taxonomy, nested field, collection doesnt use taxonomy' => [
+                $dontExpectCollection, $parentIsNotEntry, $fieldHandleDoesNotMatchTaxonomy, $isNested, $collectionDoesNotUseTaxonomy,
+            ],
         ];
     }
 
