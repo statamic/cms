d77e5cd1b57488fe87dc4021e25d949834ef5c77
Jason Varga <jason@pixelfear.com>
2022-04-20 11:48:12 -0400

    fix

--- resources/js/components/fieldtypes/bard/Image.vue
+++ resources/js/components/fieldtypes/bard/Image.vue
@@ -112,7 +112,8 @@ export default {
             this.assetId = src.substr(7);
         }
 
-        this.loadAsset(this.assetId || src);
+        let id = this.assetId || src;
+        if (id) this.loadAsset(id);
     },
 
     watch: {
@@ -155,9 +156,6 @@ export default {
         },
 
         loadAsset(id) {
-            if(id === null)
-                return;
-
             let preloaded = _.find(this.$store.state.publish[this.storeName].preloadedAssets, asset => asset.id === id);
 
             if (preloaded) {
