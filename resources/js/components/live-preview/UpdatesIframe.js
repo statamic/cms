e8e9ca3cb349e9d38669c2904a604be95b1f40b9
Tom Diggelmann <tom.diggelmann@novu.ch>
2022-04-25 19:58:31 +0200

    add config to use post message as live preview updating method

--- resources/js/components/live-preview/UpdatesIframe.js
+++ resources/js/components/live-preview/UpdatesIframe.js
@@ -8,9 +8,20 @@ export default {
             this.setIframeAttributes(iframe);
 
             const container = this.$refs.contents;
-            container.firstChild
-                ? container.replaceChild(iframe, container.firstChild)
-                : container.appendChild(iframe);
+
+            if (this.$config.get('livePreview.post_message_data')) {
+                const targetOrigin = /^https?:\/\//.test(url) ? (new URL(url))?.origin : window.origin
+                container.firstChild
+                    ? container.firstChild.contentWindow.postMessage(
+                        this.$config.get('livePreview.post_message_data'),
+                        targetOrigin
+                    )
+                    : container.appendChild(iframe);
+            } else {
+                container.firstChild
+                    ? container.replaceChild(iframe, container.firstChild)
+                    : container.appendChild(iframe);
+            }
 
             // todo: maintain scroll position
         },
