b42cfe91618524dc5ac38b169aa5a2c79facc468
Michael Aerni <hello@michaelaerni.ch>
2022-04-27 12:28:29 +0200

    Automatically apply the scopes to the builders

--- src/Query/Scopes/Scope.php
+++ src/Query/Scopes/Scope.php
@@ -2,6 +2,7 @@
 
 namespace Statamic\Query\Scopes;
 
+use Statamic\Statamic;
 use Statamic\Extend\HasHandle;
 use Statamic\Extend\RegistersItself;
 
@@ -10,6 +11,7 @@ abstract class Scope
     use RegistersItself, HasHandle;
 
     protected static $binding = 'scopes';
+    protected static $builders = [];
 
     /**
      * Apply the scope to a given query builder.
@@ -19,4 +21,16 @@ abstract class Scope
      * @return void
      */
     abstract public function apply($query, $values);
+
+    /**
+     * Return the query builders that this scope supports.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public static function builders()
+    {
+        return collect(static::$builders)->map(function ($builder) {
+            return Statamic::query($builder)::class;
+        });
+    }
 }
--- src/Stache/Query/Builder.php
+++ src/Stache/Query/Builder.php
@@ -3,6 +3,7 @@
 namespace Statamic\Stache\Query;
 
 use Statamic\Data\DataCollection;
+use Statamic\Facades\Scope;
 use Statamic\Query\Builder as BaseBuilder;
 use Statamic\Stache\Stores\Store;
 
@@ -328,5 +329,33 @@ abstract class Builder extends BaseBuilder
     protected function getWhereColumnKeyValuesByIndex($column)
     {
         return $this->store->index($column)->items();
+    }
+
+    public function __call($method, $args)
+    {
+        $this->appplyScope($method);
+
+        return $this;
+    }
+
+    public function appplyScope($method)
+    {
+        // Throw an exception if the scope doesn't exist.
+        if (! $scope = Scope::find(snake_case($method))) {
+            throw new \Exception("The [$method] scope does not exist.");
+        }
+
+        // Apply the scope to all builders if none were defined.
+        if ($scope->builders()->isEmpty()) {
+            return $scope->apply($this, []);
+        }
+
+        // Only apply the scope to the defined builders.
+            return $scope->apply($this, []);
+        if ($scope->builders()->contains($this::class)) {
+        }
+
+        // Throw an exception if a user is trying to access a scope that is not supported by this builder.
+        throw new \Exception("The [" . static::class . "] query builder does not support the [$method] scope.");
     }
 }
