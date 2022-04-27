eb079a85bffae3a46b8744bf443c81046deaa71f
Frederik Sauer <fritten.keez@gmail.com>
2022-04-27 09:00:00 +0200

    Added str_pad() modifiers (#5920)
    Co-authored-by: Jason Varga <jason@pixelfear.com>

---
+++ tests/Modifiers/StrPadTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Modifiers;
+
+use Statamic\Modifiers\Modify;
+use Tests\TestCase;
+
+class StrPadTest extends TestCase
+{
+    public function paddington(): array
+    {
+        return [
+            'pads_4_default' => ['test', 'test', [4]],
+            'pads_8_default' => ['test    ', 'test', [8]],
+            'pads_8_tilde_both' => ['~~test~~', 'test', [8, '~', 'both']],
+            'pads_8_tilde_left' => ['~~~~test', 'test', [8, '~', 'left']],
+            'pads_8_tilde_right' => ['test~~~~', 'test', [8, '~', 'right']],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider paddington
+     */
+    public function it_pads_a_string(string $expected, string $input, array $params): void
+    {
+        $modified = $this->modify($input, $params);
+        $this->assertEquals($expected, $modified);
+    }
+
+    private function modify(string $value, array $params): string
+    {
+        return Modify::value($value)->strPad($params)->fetch();
+    }
+}
