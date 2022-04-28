b2d93877a0581bd75a1d13d1f4a853519112c822
Michael Aerni <hello@michaelaerni.ch>
2022-04-28 15:27:11 +0200

    Add tests

---
+++ tests/Query/ScopeTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Tests\Query;
+
+use Exception;
+use Tests\TestCase;
+use Statamic\Statamic;
+use Statamic\Facades\Term;
+use Statamic\Facades\User;
+use Statamic\Facades\Asset;
+use Statamic\Facades\Entry;
+use Statamic\Query\OrderBy;
+use Statamic\Query\Scopes\Scope;
+use Illuminate\Support\Collection;
+use Statamic\Facades\AssetContainer;
+use Illuminate\Support\Facades\Storage;
+use Tests\PreventSavingStacheItemsToDisk;
+
+class ScopeTest extends TestCase
+{
+    use PreventSavingStacheItemsToDisk;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        app('statamic.scopes')[ScopeForAllBuilders::handle()] = ScopeForAllBuilders::class;
+        app('statamic.scopes')[ScopeForLimitedBuilders::handle()] = ScopeForLimitedBuilders::class;
+    }
+
+    /** @test **/
+    public function can_get_builders()
+    {
+        $builders = collect(['entries', 'terms'])->map(function ($builder) {
+            return get_class(Statamic::query($builder));
+        });
+
+        $this->assertEquals(ScopeForAllBuilders::builders(), collect());
+        $this->assertEquals(ScopeForLimitedBuilders::builders(), $builders);
+    }
+
+    /** @test **/
+    public function can_use_scope_on_all_builders()
+    {
+        Storage::fake('test', ['url' => '/assets']);
+        tap(AssetContainer::make('test')->disk('test'))->save();
+
+        try {
+            Entry::query()->scopeForAllBuilders()->get();
+            Term::query()->scopeForAllBuilders()->get();
+            Asset::query()->where('container', 'test')->scopeForAllBuilders()->get();
+            User::query()->scopeForAllBuilders()->get();
+            $this->assertTrue(true);
+        } catch (\Throwable $th) {
+            $this->assertTrue(false);
+        }
+    }
+
+    /** @test **/
+    public function can_only_use_scope_on_defined_builders()
+    {
+        $this->expectException(Exception::class);
+
+        Entry::query()->scopeForLimitedBuilders()->get();
+        Term::query()->scopeForLimitedBuilders()->get();
+        Asset::query()->scopeForLimitedBuilders()->get();
+        User::query()->scopeForLimitedBuilders()->get();
+    }
+}
+
+class ScopeForAllBuilders extends Scope
+{
+    public function apply($query, $params)
+    {
+        $query->where('title', 'Post 1');
+    }
+}
+
+class ScopeForLimitedBuilders extends Scope
+{
+    protected static $builders = ['entries', 'terms'];
+
+    public function apply($query, $params)
+    {
+        $query->where('title', 'Post 1');
+    }
+}
