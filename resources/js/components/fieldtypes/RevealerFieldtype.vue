d116ee656f3f1f1013e5ca10c63adba2d9f1715d
Jesse Leite <jesseleite@gmail.com>
2022-05-17 12:21:38 -0400

    Render `mode: toggle` on revealer fieldtype.

--- resources/js/components/fieldtypes/RevealerFieldtype.vue
+++ resources/js/components/fieldtypes/RevealerFieldtype.vue
@@ -1,13 +1,23 @@
 <template>
 
     <div>
-        <button
-            v-if="! isRevealed"
-            @click="reveal"
-            class="btn"
-            :disabled="isReadOnly"
-            :v-tooltip="config.instructions"
-            v-text="config.display" />
+        <template v-if="isToggleMode">
+            <label class="block pb-1">{{ config.display }}</label>
+            <div class="toggle-fieldtype-wrapper">
+                <toggle-input :value="isRevealed" @input="update" :read-only="isReadOnly" />
+                <label v-if="config.inline_label" class="ml-1 font-normal">{{ config.inline_label }}</label>
+            </div>
+        </template>
+
+        <template v-else>
+            <button
+                v-if="! isRevealed"
+                @click="reveal"
+                class="btn"
+                :disabled="isReadOnly"
+                :v-tooltip="config.instructions"
+                v-text="config.display" />
+        </template>
     </div>
 
 </template>
@@ -21,7 +31,11 @@ export default {
 
         isRevealed() {
             return this.value;
-        }
+        },
+
+        isToggleMode() {
+            return data_get(this.config, 'mode') === 'toggle';
+        },
 
     },
 
