63eab1ac9eccebfcc2697a531b3daf4b67cb747b
Jonas Siewertsen <jonas.siewertsen@spiegel.de>
2022-05-17 11:24:39 +0200

    Remove the starter kit so single PHP extensions can be replaced as well

--- src/Providers/BardServiceProvider.php
+++ src/Providers/BardServiceProvider.php
@@ -2,34 +2,40 @@
 
 namespace Statamic\Providers;
 
-use Tiptap\Nodes\Table;
-use Tiptap\Nodes\TableRow;
-use Tiptap\Marks\Subscript;
-use Tiptap\Nodes\TableCell;
-use Tiptap\Marks\Superscript;
-use Tiptap\Nodes\TableHeader;
-use Tiptap\Extensions\StarterKit;
-use Statamic\Fieldtypes\Bard\SetNode;
-use Statamic\Fieldtypes\Bard\LinkMark;
 use Illuminate\Support\ServiceProvider;
 use Statamic\Fieldtypes\Bard\Augmentor;
-use Statamic\Fieldtypes\Bard\Marks\Small;
 
 class BardServiceProvider extends ServiceProvider
 {
     public function boot()
     {
         Augmentor::addExtensions([
-            'link' => new LinkMark(),
-            'bard-set' => new SetNode(),
-            'starterkit' => new StarterKit(),
-            'subscript' => new Subscript(),
-            'superscript' => new Superscript(),
-            'small' => new Small(),
-            'table' => new Table(),
-            'table-cell' => new TableCell(),
-            'table-header' => new TableHeader(),
-            'table-row' => new TableRow(),
+            'bard-set' => new \Statamic\Fieldtypes\Bard\SetNode(),
+            'blockquoute' => new \Tiptap\Nodes\Blockquote(),
+            'bold' => new \Tiptap\Marks\Bold(),
+            'bulletlist' => new \Tiptap\Nodes\BulletList(),
+            'code' => new \Tiptap\Marks\Code(),
+            'code-block' => new \Tiptap\Nodes\CodeBlock(),
+            'document' => new \Tiptap\Nodes\Document(),
+            'hard-break' => new \Tiptap\Nodes\HardBreak(),
+            'heading' => new \Tiptap\Nodes\Heading(),
+            'horizontal-rule' => new \Tiptap\Nodes\HorizontalRule(),
+            'italic' => new \Tiptap\Marks\Italic(),
+            'link' => new \Statamic\Fieldtypes\Bard\LinkMark(),
+            'list-item' => new \Tiptap\Nodes\ListItem(),
+            'ordered-list' => new \Tiptap\Nodes\OrderedList(),
+            'paragraph' => new \Tiptap\Nodes\Paragraph(),
+            'subscript' => new \Tiptap\Marks\Subscript(),
+            'superscript' => new \Tiptap\Marks\Superscript(),
+            'small' => new \Statamic\Fieldtypes\Bard\Marks\Small(),
+            'strike' => new \Tiptap\Marks\Strike(),
+            'table' => new \Tiptap\Nodes\Table(),
+            'table-cell' => new \Tiptap\Nodes\TableCell(),
+            'table-header' => new \Tiptap\Nodes\TableHeader(),
+            'table-row' => new \Tiptap\Nodes\TableRow(),
+            'text' => new \Tiptap\Nodes\Text(),
         ]);
     }
 }
+
+
