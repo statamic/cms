550273553edea34156d9ffe77abc169404574470
Simon Hamp <simon.hamp@me.com>
2022-05-25 20:38:42 +0100

    Typo

--- config/structures.php
+++ config/structures.php
@@ -2,7 +2,7 @@
 
 return [
     /**
-     * The Strucutre cache allows Nav and Collection Trees to be cached across page requests, speeding up secondary
+     * The Structure cache allows Nav and Collection Trees to be cached across page requests, speeding up secondary
      * requests for the Tree, which is really useful when you use {{ nav }} tags across multiple pages.
      *
      * You can completely disable this cache by setting the Time To Live value to `0`.
