b260e2b577ccddbb9ad9f80b0bd13eba53a67de6
Jesse Leite <jesseleite@gmail.com>
2022-06-09 16:53:57 -0400

    Get columns off user blueprint, instead of hardcoding.

--- src/Http/Resources/CP/Users/Users.php
+++ src/Http/Resources/CP/Users/Users.php
@@ -3,12 +3,18 @@
 namespace Statamic\Http\Resources\CP\Users;
 
 use Illuminate\Http\Resources\Json\ResourceCollection;
+use Statamic\CP\Column;
+use Statamic\Http\Resources\CP\Concerns\HasRequestedColumns;
+use Statamic\Statamic;
 
 class Users extends ResourceCollection
 {
+    use HasRequestedColumns;
+
     public $collects = ListedUser::class;
     protected $blueprint;
     protected $columns;
+    protected $columnPreferenceKey;
 
     public function blueprint($blueprint)
     {
@@ -17,24 +23,51 @@ class Users extends ResourceCollection
         return $this;
     }
 
-    public function columns($columns)
+    public function columnPreferenceKey($key)
     {
-        $this->columns = collect($columns);
+        $this->columnPreferenceKey = $key;
+
+        return $this;
+    }
+
+    private function setColumns()
+    {
+        $columns = $this->blueprint->columns();
+
+        if (Statamic::pro()) {
+            $columns->put('roles', $columns->get('roles')->fieldtype('relationship')->sortable(false));
+            $columns->put('groups', $columns->get('groups')->fieldtype('relationship')->sortable(false));
+        } else {
+            $columns->forget('roles');
+            $columns->forget('groups');
+        }
+
+        $columns->forget('avatar');
+
+        $columns->put('last_login', Column::make('last_login')->label(__('Last Login'))->sortable(false));
+
+        if ($key = $this->columnPreferenceKey) {
+            $columns->setPreferred($key);
+        }
+
+        $this->columns = $columns->rejectUnlisted()->values();
 
         return $this;
     }
 
     public function toArray($request)
     {
+        $this->setColumns();
+
         return [
             'data' => $this->collection->each(function ($user) {
                 $user
                     ->blueprint($this->blueprint)
-                    ->columns($this->columns);
+                    ->columns($this->requestedColumns());
             }),
 
             'meta' => [
-                'columns' => $this->columns,
+                'columns' => $this->visibleColumns(),
             ],
         ];
     }
