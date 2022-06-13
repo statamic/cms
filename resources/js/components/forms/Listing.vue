993911b15ebc1456cac370cdeb7cbac44db3f068
Jason Varga <jason@pixelfear.com>
2022-06-13 15:35:53 -0400

    make relative so the bar doesnt fly away

--- resources/js/components/forms/Listing.vue
+++ resources/js/components/forms/Listing.vue
@@ -1,6 +1,6 @@
 <template>
     <data-list :visible-columns="columns" :columns="columns" :rows="items">
-        <div class="card p-0" slot-scope="{ filteredRows: rows }">
+        <div class="card p-0 relative" slot-scope="{ filteredRows: rows }">
             <data-list-bulk-actions
                 class="rounded"
                 :url="actionUrl"
