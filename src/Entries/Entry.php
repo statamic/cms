a8e18ac4fd741b6b7f46469ec9df82066bca9035
Duncan McClean <duncan@mcclean.co.uk>
2022-06-13 21:08:02 +0100

    This should be using `$withEvents`

--- src/Entries/Entry.php
+++ src/Entries/Entry.php
@@ -297,7 +297,7 @@ class Entry implements Contract, Augmentable, Responsable, Localization, Protect
         $afterSaveCallbacks = $this->afterSaveCallbacks;
         $this->afterSaveCallbacks = [];
 
-        if ($this->withEvents) {
+        if ($withEvents) {
             if (EntrySaving::dispatch($this) === false) {
                 return false;
             }
