5e14af25adc2d0ecb702a97ba7e3e193dd186939
Jason Varga <jason@pixelfear.com>
2022-06-20 13:54:34 -0400

    fix indentation

--- src/StaticCaching/Replacers/NoCacheReplacer.php
+++ src/StaticCaching/Replacers/NoCacheReplacer.php
@@ -7,70 +7,70 @@ use Statamic\StaticCaching\NoCache\NoCacheManager;
 use Statamic\StaticCaching\Replacer;
 use Symfony\Component\HttpFoundation\Response;
 
- class NoCacheReplacer implements Replacer
- {
-     private $manager;
+class NoCacheReplacer implements Replacer
+{
+    private $manager;
 
-     public function __construct(NoCacheManager $manager)
-     {
-         $this->manager = $manager;
-     }
+    public function __construct(NoCacheManager $manager)
+    {
+        $this->manager = $manager;
+    }
 
-     public function prepareResponseToCache(Response $cached, Response $response)
-     {
-         if (! $content = $response->getContent()) {
-             return;
-         }
+    public function prepareResponseToCache(Response $cached, Response $response)
+    {
+        if (! $content = $response->getContent()) {
+            return;
+        }
 
-         $content = $this->replace($content);
+        $content = $this->replace($content);
 
-         $response->setContent($content);
-     }
+        $response->setContent($content);
+    }
 
-     public function replaceInCachedResponse(Response $response)
-     {
-         if (! $content = $response->getContent()) {
-             return;
-         }
+    public function replaceInCachedResponse(Response $response)
+    {
+        if (! $content = $response->getContent()) {
+            return;
+        }
 
-         $this->manager->session()->restore();
+        $this->manager->session()->restore();
 
-         $response->setContent($this->replace($content));
-     }
+        $response->setContent($this->replace($content));
+    }
 
-     private function replace(string $content)
-     {
-         view()->addNamespace('nocache', $this->viewPath());
-         File::makeDirectory($this->viewPath());
+    private function replace(string $content)
+    {
+        view()->addNamespace('nocache', $this->viewPath());
+        File::makeDirectory($this->viewPath());
 
-         return preg_replace_callback('/<no_cache_section:([\w\d]+)>/', function ($matches) {
-             if (! $section = $matches[1] ?? null) {
-                 return '';
-             }
+        return preg_replace_callback('/<no_cache_section:([\w\d]+)>/', function ($matches) {
+            if (! $section = $matches[1] ?? null) {
+                return '';
+            }
 
-             $session = $this->manager->session();
+            $session = $this->manager->session();
 
-             $view = $session->getView($section);
+            $view = $session->getView($section);
 
-             $this->createTemporaryView($section, $view['engine'], $view['view']);
+            $this->createTemporaryView($section, $view['engine'], $view['view']);
 
-             return view('nocache::'.$section, $session->getViewData($section))->render();
-         }, $content);
-     }
+            return view('nocache::'.$section, $session->getViewData($section))->render();
+        }, $content);
+    }
 
-     private function viewPath()
-     {
-         return config('view.compiled').'/nocache';
-     }
+    private function viewPath()
+    {
+        return config('view.compiled').'/nocache';
+    }
 
-     private function createTemporaryView($section, $engine, $contents)
-     {
-         $viewPath = sprintf('%s/%s.%s', $this->viewPath(), $section, $engine);
+    private function createTemporaryView($section, $engine, $contents)
+    {
+        $viewPath = sprintf('%s/%s.%s', $this->viewPath(), $section, $engine);
 
-         if (File::exists($viewPath)) {
-             return;
-         }
+        if (File::exists($viewPath)) {
+            return;
+        }
 
-         File::put($viewPath, $contents);
-     }
- }
+        File::put($viewPath, $contents);
+    }
+}
