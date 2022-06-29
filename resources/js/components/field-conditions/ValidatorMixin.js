309653eae2157627ebc4fb7b1ca5bbbde515dcf5
Jason Varga <jason@pixelfear.com>
2022-06-29 14:19:49 -0400

    change back to lets to lessen the diff

--- resources/js/components/field-conditions/ValidatorMixin.js
+++ resources/js/components/field-conditions/ValidatorMixin.js
@@ -10,8 +10,8 @@ export default {
 
     methods: {
         showField(field, dottedKey) {
-            var dottedFieldPath = dottedKey || field.handle;
-            var dottedPrefix = dottedKey ? dottedKey.replace(new RegExp('\.'+field.handle+'$'), '') : '';
+            let dottedFieldPath = dottedKey || field.handle;
+            let dottedPrefix = dottedKey ? dottedKey.replace(new RegExp('\.'+field.handle+'$'), '') : '';
 
             // If we know the field is to permanently hidden, bypass validation.
             if (field.visibility === 'hidden' || this.shouldForceHiddenField(dottedFieldPath)) {
@@ -25,12 +25,12 @@ export default {
             }
 
             // Use validation to determine whether field should be shown.
-            var validator = new Validator(field, this.values, this.$store, this.storeName);
-            var passes = validator.passesConditions();
+            let validator = new Validator(field, this.values, this.$store, this.storeName);
+            let passes = validator.passesConditions();
 
             // Ensure DOM is updated to ensure all revealers are properly loaded and tracked before committing to store.
             this.$nextTick(() => {
-                var hasRevealerCondition = validator.hasRevealerCondition(dottedPrefix);
+                let hasRevealerCondition = validator.hasRevealerCondition(dottedPrefix);
 
                 this.$store.commit(`publish/${this.storeName}/setHiddenField`, {
                     dottedKey: dottedFieldPath,
