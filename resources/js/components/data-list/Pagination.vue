a8d63159cdea5593238874f3c3940c4495985bda
Jonas Siewertsen <jonas.siewertsen@spiegel.de>
2022-06-30 20:57:10 +0200

    Make pagination per page configurable (#6215)

--- resources/js/components/data-list/Pagination.vue
+++ resources/js/components/data-list/Pagination.vue
@@ -135,7 +135,7 @@ export default {
 
         perPageOptions() {
             let defaultPaginationSize = Statamic.$config.get('paginationSize');
-            let defaultOptions = [10, 25, 50, 100, 500].filter(size => size !== defaultPaginationSize);
+            let defaultOptions = Statamic.$config.get('paginationSizeOptions').filter(size => size !== defaultPaginationSize);
             let options = this.normalizeInputOptions(defaultOptions);
 
             options.push({
--- src/Http/View/Composers/JavascriptComposer.php
+++ src/Http/View/Composers/JavascriptComposer.php
@@ -37,6 +37,7 @@ class JavascriptComposer
             'focalPointEditorEnabled' => config('statamic.assets.focal_point_editor'),
             'user' => $this->user($user),
             'paginationSize' => config('statamic.cp.pagination_size'),
+            'paginationSizeOptions' => config('statamic.cp.pagination_size_options'),
             'translationLocale' => app('translator')->locale(),
             'translations' => app('translator')->toJson(),
             'sites' => $this->sites(),
