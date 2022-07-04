da9dc2c272a1e2091dacfb58122e79bb3ee3638f
Jesse Leite <jesseleite@gmail.com>
2022-07-04 16:59:06 -0400

    Be more forgiving around multiline extends/implements in class definitions.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -25,7 +25,6 @@ $rules = [
     'class_definition' => [
         'multi_line_extends_each_single_line' => true,
         'single_item_single_line' => true,
-        'single_line' => true,
     ],
     'concat_space' => [
         'spacing' => 'none',
