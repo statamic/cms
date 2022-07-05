69afc3420b80626ddf3be2a02ebe92020b8e9f24
Erin Dalzell <erin@silentz.co>
2022-07-05 13:08:04 -0700

    Hide replicator fields in preview (#6022)
    Co-authored-by: Jack McDade <jack@jackmcdade.com>
    Co-authored-by: Jason Varga <jason@pixelfear.com>

--- resources/js/components/fieldtypes/replicator/Replicator.vue
+++ resources/js/components/fieldtypes/replicator/Replicator.vue
@@ -33,6 +33,7 @@
                     :field-path-prefix="fieldPathPrefix || handle"
                     :has-error="setHasError(index)"
                     :previews="previews[set._id]"
+                    :show-field-previews="config.previews"
                     :can-add-set="canAddSet"
                     @collapsed="collapseSet(set._id)"
                     @expanded="expandSet(set._id)"
@@ -178,7 +179,7 @@ export default {
                 set,
                 ...this.value.slice(index + 1)
             ]);
-            
+
             this.expandSet(set._id);
         },
 
