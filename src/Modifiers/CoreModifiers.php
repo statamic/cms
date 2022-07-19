de545894f7b783b13417e139edaa02f8444a5482
Oliver Kaufmann <oliver.kaufmann@novu.ch>
2022-07-19 16:45:15 +0200

    deprecate formatLocalized

--- src/Modifiers/CoreModifiers.php
+++ src/Modifiers/CoreModifiers.php
@@ -671,6 +671,8 @@ class CoreModifiers extends Modifier
     /**
      * Converts a string to a Carbon instance and formats it according to the whim of the Overlord.
      *
+     * @deprecated formatLocalized is deprecated since Carbon 2.55.0. You may want to use formatTranslated instead.
+     *
      * @param $value
      * @param $params
      * @return string
