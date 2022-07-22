781814000029979f9836373477006e36bb4b7507
Jesse Leite <jesseleite@gmail.com>
2022-07-22 15:17:43 -0400

    Move `computedValue()` logic from User classes to `ContainsComputedData` trait.

--- src/Data/ContainsComputedData.php
+++ src/Data/ContainsComputedData.php
@@ -4,14 +4,39 @@ namespace Statamic\Data;
 
 trait ContainsComputedData
 {
+    protected $withComputedData = true;
+
     public function computedData()
     {
-        if (! method_exists($this, 'getComputedCallbacks')) {
+        if (! method_exists($this, 'getComputedCallbacks') || ! $this->withComputedData) {
             return collect();
         }
 
         return collect($this->getComputedCallbacks())->map(function ($callback, $field) {
-            return $callback($this, method_exists($this, 'get') ? $this->get($field) : null);
+            return $callback($this->instanceWithoutComputed(), method_exists($this, 'get') ? $this->get($field) : null);
         });
+    }
+
+    public function computedValue($key, $fallback = null)
+    {
+        if ($this->withComputedData && $callback = $this->getComputedCallbacks()->get($key)) {
+            return $callback($this->instanceWithoutComputed(), $fallback);
+        }
+
+        return $fallback;
+    }
+
+    public function hasComputedCallback($key)
+    {
+        return $this->getComputedCallbacks()->has($key);
+    }
+
+    public function instanceWithoutComputed()
+    {
+        $clone = clone $this;
+
+        $clone->withComputedData = false;
+
+        return $clone;
     }
 }
