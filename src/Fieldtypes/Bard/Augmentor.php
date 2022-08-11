7817d42c1177bceedf1da026fb048548038ab347
Jack Sleight <jacksleight@gmail.com>
2022-08-11 20:08:43 +0100

    Allow bard field config to be passed to PHP TipTap extensions (#6421)
    Co-authored-by: Jason Varga <jason@pixelfear.com>

--- src/Fieldtypes/Bard/Augmentor.php
+++ src/Fieldtypes/Bard/Augmentor.php
@@ -2,6 +2,7 @@
 
 namespace Statamic\Fieldtypes\Bard;
 
+use Closure;
 use Statamic\Fields\Field;
 use Statamic\Fields\Value;
 use Statamic\Fields\Values;
@@ -16,8 +17,8 @@ class Augmentor
     protected $includeDisabledSets = false;
     protected $augmentSets = true;
     protected $withStatamicImageUrls = false;
-    protected static $replaceExtensions = [];
-    private static $extensions = [];
+    protected static $extensions = [];
+    protected static $extensionReplacements = [];
 
     public function __construct($fieldtype)
     {
@@ -113,9 +114,9 @@ class Augmentor
         static::$extensions[$name] = $extension;
     }
 
-    public static function replaceExtension($search, $replacement)
+    public static function replaceExtension($name, $extension)
     {
-        static::$replaceExtensions[$search] = $replacement;
+        static::$extensionReplacements[$name] = $extension;
     }
 
     protected function convertToSets($html)
@@ -176,12 +177,22 @@ class Augmentor
 
     public function extensions()
     {
-        $this->addExtension('image', $this->withStatamicImageUrls ? new StatamicImageNode : new ImageNode);
+        $extensions = [];
+        $options = ['withStatamicImageUrls' => $this->withStatamicImageUrls];
+        $args = [$this->fieldtype, $options];
 
-        foreach (self::$replaceExtensions as $name => $replacement) {
-            self::$extensions[$name] = $replacement;
+        foreach (static::$extensions as $name => $extension) {
+            $extensions[$name] = $extension instanceof Closure
+                ? $extension(...$args)
+                : $extension;
         }
 
-        return Arr::removeNullValues(self::$extensions);
+        foreach (static::$extensionReplacements as $name => $extension) {
+            $extensions[$name] = $extension instanceof Closure
+                ? $extension($extensions[$name] ?? null, ...$args)
+                : $extension;
+        }
+
+        return Arr::removeNullValues($extensions);
     }
 }
