37a0dfae5d197fcfedbe10d34b63f6857982c717
John Koster <john@stillat.com>
2022-08-11 22:11:48 -0500

    Disables resolving Antlers values when there is a "raw" modifier

--- tests/Antlers/Runtime/Fieldtypes/BardFieldtypeTest.php
+++ tests/Antlers/Runtime/Fieldtypes/BardFieldtypeTest.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use Statamic\Fields\Field;
 use Statamic\Fields\Value;
 use Statamic\Fieldtypes\Bard;
+use Tests\Antlers\Fixtures\Addon\Tags\VarTest;
 use Tests\Antlers\ParserTestCase;
 
 class BardFieldtypeTest extends ParserTestCase
@@ -18,6 +19,52 @@ class BardFieldtypeTest extends ParserTestCase
     public function test_raw_parameter_style_modifier_can_be_used_on_values()
     {
         $this->runFieldTypeTest('bard', 'bard_raw_parameter_modifier');
+    }
+
+    public function test_antlers_does_not_get_evaluated_when_using_raw_on_bard()
+    {
+        $bard = new Bard();
+        $field = new Field('main_content', [
+            'type' => 'bard',
+            'antlers' => true,
+        ]);
+        $bard->setField($field);
+
+        $data = [
+            [
+                'type' => 'paragraph',
+                'content' => [
+                    [
+                        'type' => 'text',
+                        'text' => 'Hello',
+                    ],
+                    [
+                        'type' => 'text',
+                        'marks' => [
+                            [
+                                'type' => 'bold',
+                            ]
+                        ],
+                        'text' => 'World',
+                    ],
+                ],
+            ],
+        ];
+
+        $value = new Value($data, 'main_content', $bard);
+
+        VarTest::register();
+
+        $templateData = [
+            'main_content' => $value,
+        ];
+
+        $this->renderString('{{ var_test :variable="main_content|raw" }}', $templateData, true);
+
+        $this->assertSame($data, VarTest::$var);
+
+        // Ensure we didn't break other modifiers.
+        $this->assertSame('<P>HELLO<STRONG>WORLD</STRONG></P>', $this->renderString('{{ main_content | upper }}', $templateData, true));
     }
 
     public function test_antlers_true_bard_fields_correct_for_html_encoded_values()
