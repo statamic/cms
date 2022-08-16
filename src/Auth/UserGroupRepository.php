15d98c29a2c1dc4598930eec304ecccd2486b8ca
Ryan Mitchell <ryan@thoughtcollective.com>
2022-08-16 17:45:58 +0100

    Title and handle are required

--- src/Auth/UserGroupRepository.php
+++ src/Auth/UserGroupRepository.php
@@ -17,8 +17,8 @@ abstract class UserGroupRepository implements RepositoryContract
     public function blueprint()
     {
         $blueprint = Blueprint::find('user_group') ?? Blueprint::makeFromFields([
-            'title' => ['type' => 'text', 'display' => __('Title'), 'listable' => true, 'instructions' => __('Usually a plural noun, like Editors or Photographers')],
-            'handle' => ['type' => 'slug', 'display' => __('Handle'), 'listable' => true, 'instructions' => __('Used to reference this user group on the frontend. It\'s non-trivial to change later.')],
+            'title' => ['type' => 'text', 'display' => __('Title'), 'listable' => true, 'validate' => ['required'], 'instructions' => __('Usually a plural noun, like Editors or Photographers')],
+            'handle' => ['type' => 'slug', 'display' => __('Handle'), 'listable' => true, 'validate' => ['required'], 'instructions' => __('Used to reference this user group on the frontend. It\'s non-trivial to change later.')],
             'roles' => ['type' => 'user_roles', 'mode' => 'select', 'listable' => true, 'instructions' => __('Assign roles to give users in this group all of their corresponding permissions.')],
         ])->setHandle('user_group');
 
