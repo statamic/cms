0d34ad9b984b3f9893d90d4e95179524a5e9d2c2
Jack McDade <jack@jackmcdade.com>
2022-08-22 10:39:36 -0400

    Tweaky tweaks

--- resources/js/components/fieldtypes/ToggleIndexFieldtype.vue
+++ resources/js/components/fieldtypes/ToggleIndexFieldtype.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
-        <svg-icon name="check" class="align-middle w-4" v-if="this.value" />
-        <svg-icon name="close" class="align-middle w-4 opacity-25" v-if="!this.value" />
+    <div class="flex items-center">
+        <svg-icon name="check" class="w-3 text-green" v-if="this.value" />
+        <svg-icon name="close" class="w-3 text-grey-50" v-if="!this.value" />
     </div>
 </template>
 
