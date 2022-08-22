c6e6626db2756c670d1f1645edc9aa9524e112d9
Jack Sleight <jacksleight@gmail.com>
2022-08-22 12:02:25 +0100

    Improve display of toggle fields in listings

---
+++ resources/js/components/fieldtypes/ToggleIndexFieldtype.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <svg-icon name="check" class="align-middle w-4" v-if="this.value" />
+        <svg-icon name="close" class="align-middle w-4 opacity-25" v-if="!this.value" />
+    </div>
+</template>
+
+<script>
+export default {
+
+    mixins: [IndexFieldtype],
+
+};
+</script>
