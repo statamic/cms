a7b479557e07ea6508d59998d982de3fa1f7805e
Jesse Leite <jesseleite@gmail.com>
2022-08-26 16:36:02 -0400

    Test bulk renaming.

--- tests/Assets/AssetTest.php
+++ tests/Assets/AssetTest.php
@@ -890,6 +890,76 @@ class AssetTest extends TestCase
     }
 
     /** @test */
+    public function it_bulk_renames()
+    {
+        Event::fake();
+        $disk = Storage::fake('local');
+        $container = Facades\AssetContainer::make('test')->disk('local');
+        Facades\AssetContainer::shouldReceive('save')->with($container);
+        Facades\AssetContainer::shouldReceive('findByHandle')->with('test')->andReturn($container);
+
+        foreach (['foo', 'bar', 'baz', 'fraz'] as $filename) {
+            $disk->put("old/{$filename}.txt", 'The asset contents');
+            $asset = $container->makeAsset("old/{$filename}.txt")->data(['test' => $filename]);
+            $asset->saveQuietly();
+            $oldMeta[] = $disk->get("old/.meta/{$filename}.txt.yaml");
+            $disk->assertExists("old/{$filename}.txt");
+            $disk->assertExists("old/.meta/{$filename}.txt.yaml");
+        }
+        $this->assertEquals([
+            'old/foo.txt',
+            'old/bar.txt',
+            'old/baz.txt',
+            'old/fraz.txt',
+        ], $container->files()->all());
+        $this->assertEquals([
+            'old/foo.txt' => ['test' => 'foo'],
+            'old/bar.txt' => ['test' => 'bar'],
+            'old/baz.txt' => ['test' => 'baz'],
+            'old/fraz.txt' => ['test' => 'fraz'],
+        ], $container->assets('/', true)->keyBy->path()->map(function ($item) {
+            return $item->data()->all();
+        })->all());
+
+        $assets = $container->assets()->each->rename('tokyo', true);
+
+        Event::assertDispatched(AssetSaved::class, 4);
+        foreach (['foo', 'bar', 'baz', 'fraz'] as $filename) {
+            $disk->assertMissing("old/{$filename}.txt");
+            $disk->assertMissing("old/.meta/{$filename}.txt.yaml");
+        }
+        foreach (['tokyo', 'tokyo-1', 'tokyo-2', 'tokyo-3'] as $filename) {
+            $disk->assertExists("old/{$filename}.txt");
+            $disk->assertExists("old/.meta/{$filename}.txt.yaml");
+        }
+        foreach ($oldMeta as $i => $meta) {
+            $filename = $i > 0 ? "tokyo-{$i}" : 'tokyo';
+            $this->assertEquals($meta, $disk->get("old/.meta/{$filename}.txt.yaml"));
+        }
+        $this->assertEquals([
+            'old/tokyo.txt',
+            'old/tokyo-1.txt',
+            'old/tokyo-2.txt',
+            'old/tokyo-3.txt',
+        ], $container->files()->all());
+        $this->assertEquals([
+            'old/tokyo.txt' => ['test' => 'foo'],
+            'old/tokyo-1.txt' => ['test' => 'bar'],
+            'old/tokyo-2.txt' => ['test' => 'baz'],
+            'old/tokyo-3.txt' => ['test' => 'fraz'],
+        ], $container->assets('/', true)->keyBy->path()->map(function ($item) {
+            return $item->data()->all();
+        })->all());
+        $this->assertEquals([
+            'old',
+            'old/tokyo.txt',
+            'old/tokyo-1.txt',
+            'old/tokyo-2.txt',
+            'old/tokyo-3.txt',
+        ], $container->contents()->cached()->keys()->all());
+    }
+
+    /** @test */
     public function it_gets_dimensions()
     {
         $file = UploadedFile::fake()->image('image.jpg', 30, 60);
