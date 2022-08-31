5a65986096b2c4dbb20daeb0fda6ffe7c440b9b0
Jesse Leite <jesseleite@gmail.com>
2022-08-31 11:46:28 -0400

    Use `download()` instead of custom JS callback in `DownloadAsset` action (#6599)

--- src/Actions/DownloadAsset.php
+++ src/Actions/DownloadAsset.php
@@ -28,13 +28,8 @@ class DownloadAsset extends Action
         return $authed->can('view', $asset);
     }
 
-    public function run($items, $values)
+    public function download($items, $values)
     {
-        $asset = $items->first();
-
-        return [
-            'message' => false,
-            'callback' => ['downloadUrl', $asset->absoluteUrl()],
-        ];
+        return $items->first()->download();
     }
 }
