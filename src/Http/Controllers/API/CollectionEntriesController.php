9056e6ea29fe3b405adb1d53bf54e22c42779032
Arthur Perton <arthur@webenapp.nl>
2022-09-01 11:02:17 +0200

    Add support for filtering by taxonomy terms

--- src/Http/Controllers/API/CollectionEntriesController.php
+++ src/Http/Controllers/API/CollectionEntriesController.php
@@ -5,12 +5,14 @@ namespace Statamic\Http\Controllers\API;
 use Statamic\Exceptions\NotFoundHttpException;
 use Statamic\Facades\Entry;
 use Statamic\Http\Resources\API\EntryResource;
+use Statamic\Support\Str;
 
 class CollectionEntriesController extends ApiController
 {
     protected $resourceConfigKey = 'collections';
     protected $routeResourceKey = 'collection';
     protected $filterPublished = true;
+    protected $query;
 
     public function index($collection)
     {
@@ -20,8 +22,10 @@ class CollectionEntriesController extends ApiController
             ->flatMap(fn ($blueprint) => $blueprint->fields()->all())
             ->filter->isRelationship()->keys()->all();
 
+        $this->query = $collection->queryEntries()->with($with);
+
         return app(EntryResource::class)::collection(
-            $this->filterSortAndPaginate($collection->queryEntries()->with($with))
+            $this->filterSortAndPaginate($this->query)
         );
     }
 
@@ -35,6 +39,35 @@ class CollectionEntriesController extends ApiController
         $this->abortIfUnpublished($entry);
 
         return app(EntryResource::class)::make($entry);
+    }
+
+    protected function getFilters()
+    {
+        return parent::getFilters()->filter(function ($value, $filter) {
+            if (! Str::startsWith($filter, 'taxonomy:')) {
+                return true;
+            }
+
+            $this->applyTaxonomyFilter($filter, $value);
+
+            return false;
+        });
+    }
+
+    protected function applyTaxonomyFilter($filter, $terms)
+    {
+        [$keyword, $taxonomy, $condition] = array_pad(explode(':', $filter), 3, null);
+
+        $terms = collect($this->getPipedValues($terms))
+            ->map(fn ($term) => "$taxonomy::$term");
+
+        if ($condition === 'in') {
+            $this->query->whereTaxonomyIn($terms->all());
+        } elseif ($condition === 'not_in') {
+            $this->query->whereTaxonomyNotIn($terms->all());
+        } else {
+            $terms->each(fn ($term) => $this->query->whereTaxonomy($term));
+        }
     }
 
     private function abortIfInvalid($entry, $collection)
