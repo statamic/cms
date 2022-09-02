afc5978e48b6dcb986735a3fd44150395829442c
edalzell <erin@silentz.co>
2022-09-02 11:55:33 -0700

    remove asset glide cache when the focus changes
    # Conflicts:
    #	src/Listeners/ClearAssetGlideCache.php

--- src/Listeners/ClearAssetGlideCache.php
+++ src/Listeners/ClearAssetGlideCache.php
@@ -5,6 +5,7 @@ namespace Statamic\Listeners;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Statamic\Events\AssetDeleted;
 use Statamic\Events\AssetReuploaded;
+use Statamic\Events\AssetSaved;
 use Statamic\Facades\Glide;
 
 class ClearAssetGlideCache implements ShouldQueue
@@ -18,15 +19,28 @@ class ClearAssetGlideCache implements ShouldQueue
     {
         $events->listen(AssetReuploaded::class, self::class.'@handle');
         $events->listen(AssetDeleted::class, self::class.'@handle');
+        $events->listen(AssetSaved::class, self::class.'@handleSaved');
     }
 
     /**
-     * Handle the events.
+     * Handle the AssetDeleted event.
      *
      * @param  AssetDeleted  $event
      */
     public function handle($event)
     {
         Glide::clearAsset($event->asset);
+    }
+
+    /**
+     * Handle the AssetSaved event.
+     *
+     * @param  AssetSaved  $event
+     */
+    public function handleSaved($event)
+    {
+        if ($event->asset->getOriginal('focus') != $event->asset->get('focus')) {
+            Glide::clearAsset($event->asset);
+        }
     }
 }
