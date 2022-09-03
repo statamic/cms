5fe446601e5ba39d6a375660c230a9b22263dbc8
Jesse Leite <jesseleite@gmail.com>
2022-09-02 21:59:57 -0400

    Nav preferences wippity wip.

--- src/CP/Navigation/Nav.php
+++ src/CP/Navigation/Nav.php
@@ -4,6 +4,7 @@ namespace Statamic\CP\Navigation;
 
 use Closure;
 use Exception;
+use Statamic\Facades\Preference;
 use Statamic\Facades\User;
 use Statamic\Support\Str;
 
@@ -11,6 +12,7 @@ class Nav
 {
     protected $items = [];
     protected $extensions = [];
+    protected $built;
     protected $withHidden = false;
 
     /**
@@ -91,7 +93,7 @@ class Nav
      */
     public function items()
     {
-        return $this->items;
+        return $this->items; // TODO: sometimes this is a closure though?
     }
 
     /**
@@ -115,7 +117,7 @@ class Nav
      */
     public function build()
     {
-        return $this->built = $this
+        return $this
             ->makeDefaultItems()
             ->buildExtensions()
             ->buildChildren()
@@ -123,7 +125,9 @@ class Nav
             ->validateViews()
             ->authorizeItems()
             ->authorizeChildren()
-            ->buildSections();
+            ->applyPreferenceOverrides()
+            ->buildSections()
+            ->getBuiltNav();
     }
 
     /**
@@ -266,7 +270,7 @@ class Nav
     /**
      * Build sections collection.
      *
-     * @return \Illuminate\Support\Collection
+     * @return $this
      */
     protected function buildSections()
     {
@@ -279,17 +283,109 @@ class Nav
             ->filter(function ($item) {
                 return $item->section();
             })
-            ->reject(function ($item) {
-                return $item->section() === 'Top Level'
-                    && ! in_array($item->name(), CoreNav::ALLOWED_TOP_LEVEL);
-            })
+            // ->reject(function ($item) {
+            //     return $item->section() === 'Top Level'
+            //         && ! in_array($item->name(), CoreNav::ALLOWED_TOP_LEVEL);
+            // })
             ->each(function ($item) use (&$sections) {
                 $sections[$item->section()][] = $item;
             });
 
-        return collect($sections)->map(function ($items) {
+        $this->built = collect($sections)->map(function ($items) {
             return collect($items);
         });
+
+        return $this;
+    }
+
+    protected function applyPreferenceOverrides()
+    {
+        if (! $userNav = Preference::get('nav')) {
+            return $this;
+        }
+
+        collect($userNav)
+            ->map(function ($overrides, $section) {
+                return $this->normalizeOverrides($overrides, $section);
+            })
+            ->each(function ($overrides, $section) {
+                $this->applyPreferenceOverridesForSection($overrides, $section);
+            });
+
+        return $this;
+    }
+
+    protected function normalizeOverrides($overrides, $section)
+    {
+        return collect($overrides)->map(function ($item) use ($section) {
+            return is_string($item) && ! Str::contains($item, '::')
+                ? "{$section}::{$item}"
+                : $item;
+        });
+    }
+
+    protected function applyPreferenceOverridesForSection($overrides, $section)
+    {
+        $overrides
+            ->map(function ($item) {
+                return $this->findOrCreateItem($item);
+            })
+            ->each(function ($item) use ($section) {
+                $item->section(Str::modifyMultiple($section, ['deslugify', 'title']));
+            });
+    }
+
+    protected function findOrCreateItem($id)
+    {
+        // cache this to class so we can keep building on it, or key them on class by id?
+        $items = $this->items->keyBy->id();
+
+        $options = collect(explode('@', $id));
+        $id = $options->shift();
+
+        $item = $items->get($id);
+
+        $idParts = collect(explode('::', $id));
+
+        if ($idParts->count() > 2) {
+            $parentId = $idParts[0].'::'.$idParts[1];
+        }
+
+        $parent = isset($parentId) ? $items->get($parentId) : null;
+
+        if ($parent && ! $item) {
+            $parent->resolveChildren();
+            $parent->children()->each(function ($item) use ($items) {
+                $items->put($item->id(), $item);
+            });
+            $item = $items->get($id);
+        }
+
+        if ($item) {
+            $cloned = clone $item;
+            $this->items[] = $cloned;
+
+            if ($options->contains('move')) {
+                $item->hidden(true);
+            }
+
+            if ($parent && $options->contains('move')) {
+                $parent->children(
+                    $parent->children()->reject(function ($item) use ($id) {
+                        return $id === $item->id();
+                    })
+                );
+            }
+
+            return $cloned;
+        }
+
+        return $this->create('WOTTT');
+    }
+
+    protected function getBuiltNav()
+    {
+        return $this->built;
     }
 
     /**
