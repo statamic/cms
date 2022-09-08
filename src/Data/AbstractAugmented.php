033672d74e11deda0ad5f33073edec0d946a2286
Arthur Perton <arthur@webenapp.nl>
2022-09-08 22:10:56 +0200

    Also check if hasSupplement exists

--- src/Data/AbstractAugmented.php
+++ src/Data/AbstractAugmented.php
@@ -85,9 +85,13 @@ abstract class AbstractAugmented implements Augmented
         $value = method_exists($this->data, 'value') ? $this->data->value($handle) : $this->data->get($handle);
 
         if (method_exists($this->data, 'getSupplement')) {
-            $value = $this->data->hasSupplement($handle)
-                ? $this->data->getSupplement($handle)
-                : $value;
+            if (method_exists($this->data, 'hasSupplement')) {
+                $value = $this->data->hasSupplement($handle)
+                    ? $this->data->getSupplement($handle)
+                    : $value;
+            } else {
+                $value = $this->data->getSupplement($handle) ?? $value;
+            }
         }
 
         return $value;
