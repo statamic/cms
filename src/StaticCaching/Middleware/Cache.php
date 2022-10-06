dd63c61a94c63aeea87feb3ec81931be5e4bcc59
Arthur Perton <arthur@webenapp.nl>
2022-10-06 15:43:01 +0200

    Only replace in specific cases

--- src/StaticCaching/Middleware/Cache.php
+++ src/StaticCaching/Middleware/Cache.php
@@ -50,7 +50,7 @@ class Cache
             $this->makeReplacementsAndCacheResponse($request, $response);
 
             $this->nocache->write();
-        } else {
+        } elseif ($this->shouldMakeReplacements($response)) {
             $this->makeReplacements($response);
         }
 
@@ -99,7 +99,7 @@ class Cache
         }
 
         // Draft and private pages should not be cached.
-        if ($response->headers->has('X-Statamic-Draft') || $response->headers->has('X-Statamic-Private')) {
+        if ($this->isDraftOrPrivate($response)) {
             return false;
         }
 
@@ -108,5 +108,15 @@ class Cache
         }
 
         return true;
+    }
+
+    private function shouldMakeReplacements($response)
+    {
+        return $this->isDraftOrPrivate($response) || $response->getStatusCode() === 404;
+    }
+
+    private function isDraftOrPrivate($response)
+    {
+        return $response->headers->has('X-Statamic-Draft') || $response->headers->has('X-Statamic-Private');
     }
 }
