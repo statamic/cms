f18078d455b8848c0275940ac64081b37f557d4f
Tom Diggelmann <tom.diggelmann@novu.ch>
2022-11-07 15:54:47 +0100

    fix augmented date test

--- tests/Data/AugmentedTestCase.php
+++ tests/Data/AugmentedTestCase.php
@@ -51,7 +51,7 @@ class AugmentedTestCase extends TestCase
             switch ($expectation['type']) {
                 case Carbon::class:
                     $this->assertTrue(
-                        Carbon::createFromFormat('Y-m-d H:i', $expectation['value'])->eq($actual),
+                        Carbon::createFromFormat(strlen($expectation['value']) === 19 ? 'Y-m-d H:i:s' : 'Y-m-d H:i', $expectation['value'])->eq($actual),
                         "Key '{$key}' does not match the expected date."
                     );
                     break;
