a9602080631a9e8e5906b0d4b6a170e73f8859eb
Daniel Shields <tao@users.noreply.github.com>
2022-11-30 16:34:47 +0200

    Allows localization variable to be saved as false. (#7087)
    Co-authored-by: Jason Varga <jason@pixelfear.com>

--- src/Fields/FieldTransformer.php
+++ src/Fields/FieldTransformer.php
@@ -4,6 +4,7 @@ namespace Statamic\Fields;
 
 use Facades\Statamic\Fields\FieldtypeRepository;
 use Statamic\Facades\Fieldset;
+use Statamic\Facades\Site;
 use Statamic\Support\Arr;
 
 class FieldTransformer
@@ -31,7 +32,7 @@ class FieldTransformer
             unset($field['width']);
         }
 
-        if (Arr::get($field, 'localizable', false) === false) {
+        if (Arr::get($field, 'localizable', false) === false && ! Site::hasMultiple()) {
             unset($field['localizable']);
         }
 
