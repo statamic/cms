1b4bef9cb5d5d7a8a073f8d8324dff60d111211b
Jesse Leite <jesseleite@gmail.com>
2022-12-06 18:53:09 -0500

    Only show section if roles exist.

--- resources/views/nav/index.blade.php
+++ resources/views/nav/index.blade.php
@@ -22,22 +22,24 @@
         </table>
     </div>
 
-    <h3 class="little-heading pl-0 mb-1">{{ __('Override Per Role') }}</h3>
-    <div class="card p-0 mb-2">
-        <table class="data-table">
-            @foreach(Statamic\Facades\Role::all() as $role)
-                <tr>
-                    <td>
-                        <div class="flex items-center">
-                            <div class="w-4 h-4 mr-2">@cp_svg('shield-key')</div>
-                            <a href="{{ cp_route('preferences.nav.role.edit', [$role->handle()]) }}">{{ __($role->title()) }}</a>
-                        </div>
-                    </td>
-                    <td class="text-right text-2xs"><a href="{{ cp_route('preferences.nav.role.edit', [$role->handle()]) }}" class="text-blue">Customize</a></td>
-                </tr>
-            @endforeach
-        </table>
-    </div>
+    @if (Statamic\Facades\Role::all()->isNotEmpty())
+        <h3 class="little-heading pl-0 mb-1">{{ __('Override Per Role') }}</h3>
+        <div class="card p-0 mb-2">
+            <table class="data-table">
+                @foreach (Statamic\Facades\Role::all() as $role)
+                    <tr>
+                        <td>
+                            <div class="flex items-center">
+                                <div class="w-4 h-4 mr-2">@cp_svg('shield-key')</div>
+                                <a href="{{ cp_route('preferences.nav.role.edit', [$role->handle()]) }}">{{ __($role->title()) }}</a>
+                            </div>
+                        </td>
+                        <td class="text-right text-2xs"><a href="{{ cp_route('preferences.nav.role.edit', [$role->handle()]) }}" class="text-blue">Customize</a></td>
+                    </tr>
+                @endforeach
+            </table>
+        </div>
+    @endif
 
     <h3 class="little-heading pl-0 mb-1">{{ __('Override Per User') }}</h3>
     <div class="card p-0 mb-2">
