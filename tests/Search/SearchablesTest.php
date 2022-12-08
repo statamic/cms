a66647044a1d3f8b58864e84b9b1bd55224ecd37
Ryan Mitchell <ryan@thoughtcollective.com>
2022-12-08 12:26:55 +0000

    Add test

--- tests/Search/SearchablesTest.php
+++ tests/Search/SearchablesTest.php
@@ -299,6 +299,31 @@ class SearchablesTest extends TestCase
     }
 
     /** @test */
+    public function it_transforms_by_a_class_set_in_the_config_file()
+    {
+        config()->set('statamic.search.indexes.default', [
+            'fields' => [
+                'title',
+            ],
+            'transformers' => [
+                'title' => SearchTransformerTest::class,
+            ],
+        ]);
+
+        $index = app(\Statamic\Search\Comb\Index::class, [
+            'name' => 'default',
+            'config' => config('statamic.search.indexes.default'),
+        ]);
+
+        $searchable = EntryFactory::collection('test')->data(['title' => 'Hello'])->make();
+        $searchables = new Searchables($index);
+
+        $this->assertEquals([
+            'title' => 'HELLO',
+        ], $searchables->fields($searchable));
+    }
+
+    /** @test */
     public function if_a_transformer_returns_an_array_it_gets_combined_into_the_results()
     {
         config()->set('statamic.search.indexes.default', [
@@ -342,4 +367,11 @@ class SearchablesTest extends TestCase
 class NotSearchable
 {
     //
+}
+
+class SearchTransformerTest
+{
+    public function handle($field, $value) {
+        return strtoupper($value);
+    }
 }
