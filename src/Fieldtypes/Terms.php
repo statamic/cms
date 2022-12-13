e9025f57b699cfd159fd0bb51b6dc40e1ffa9528
Frederik Sauer <fritten.keez@gmail.com>
2022-12-13 16:25:48 +0100

    Use lang instead of handle

--- src/Fieldtypes/Terms.php
+++ src/Fieldtypes/Terms.php
@@ -373,7 +373,7 @@ class Terms extends Relationship
     protected function createTermFromString($string, $taxonomy)
     {
         $term = Facades\Term::make()
-            ->slug(Str::slug($string, '-', Site::selected()->handle()))
+            ->slug(Str::slug($string, '-', Site::selected()->lang()))
             ->taxonomy(Facades\Taxonomy::findByHandle($taxonomy))
             ->set('title', $string);
 
