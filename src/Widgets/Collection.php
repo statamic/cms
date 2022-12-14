147d792c308cbc3aae2b842ec8fa774c2cab0530
Jason Varga <jason@pixelfear.com>
2022-12-14 11:03:25 -0500

    hide hidden blueprints

--- src/Widgets/Collection.php
+++ src/Widgets/Collection.php
@@ -36,7 +36,7 @@ class Collection extends Widget
             ]),
             'title' => $this->config('title', $collection->title()),
             'button' => $collection->createLabel(),
-            'blueprints' => $collection->entryBlueprints(),
+            'blueprints' => $collection->entryBlueprints()->reject->hidden()->values(),
             'limit' => $this->config('limit', 5),
             'sortColumn' => $sortColumn,
             'sortDirection' => $sortDirection,
