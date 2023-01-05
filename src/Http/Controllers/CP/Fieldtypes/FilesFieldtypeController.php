8041402b1c7b13a4cfb81108c8165744c0e59ba5
Jason Varga <jason@pixelfear.com>
2023-01-05 10:47:21 -0500

    Files fieldtype can accept a container so it can perform the appropriate processing

--- src/Http/Controllers/CP/Fieldtypes/FilesFieldtypeController.php
+++ src/Http/Controllers/CP/Fieldtypes/FilesFieldtypeController.php
@@ -3,13 +3,18 @@
 namespace Statamic\Http\Controllers\CP\Fieldtypes;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
+use League\Glide\ServerFactory;
+use Statamic\Facades\AssetContainer;
 use Statamic\Http\Controllers\CP\CpController;
 
 class FilesFieldtypeController extends CpController
 {
     public function upload(Request $request)
     {
+        $this->glideTmpPath = storage_path('statamic/glide/tmp');
+
         $request->validate([
             'file' => ['file', function ($attribute, $value, $fail) {
                 if (in_array(trim(strtolower($value->getClientOriginalExtension())), ['php', 'php3', 'php4', 'php5', 'phtml'])) {
@@ -22,8 +27,47 @@ class FilesFieldtypeController extends CpController
 
         $path = now()->timestamp.'/'.$file->getClientOriginalName();
 
-        Storage::disk('local')->putFileAs('statamic/file-uploads', $file, $path);
+        $sourcePath = $file->getRealPath();
+
+        if ($request->container) {
+            if ($preset = AssetContainer::find($request->container)->glideSourcePreset()) {
+                $params = config("statamic.assets.image_manipulation.presets.{$preset}", []);
+            }
+
+            $sourcePath = $preset && $params
+                ? $this->glideProcessUploadedFile($file, $params)
+                : $sourcePath;
+        }
+
+        $this->putFileOnDisk($sourcePath, $path);
 
         return ['data' => ['id' => $path]];
+    }
+
+    private function glideProcessUploadedFile(UploadedFile $file, $params)
+    {
+        $server = ServerFactory::create([
+            'source' => $file->getPath(),
+            'cache' => $this->glideTmpPath,
+            'driver' => config('statamic.assets.image_manipulation.driver'),
+            'watermarks' => public_path(),
+        ]);
+
+        try {
+            return $this->glideTmpPath.'/'.$server->makeImage($file->getFilename(), $params);
+        } catch (\Exception $exception) {
+            return $file->getRealPath();
+        }
+    }
+
+    private function putFileOnDisk($sourcePath, $destinationPath)
+    {
+        $stream = fopen($sourcePath, 'r');
+
+        Storage::disk('local')->put('statamic/file-uploads/'.$destinationPath, $stream);
+
+        if (is_resource($stream)) {
+            fclose($stream);
+        }
     }
 }
--- tests/Feature/Fieldtypes/FilesTest.php
+++ tests/Feature/Fieldtypes/FilesTest.php
@@ -2,8 +2,10 @@
 
 namespace Tests\Feature\Fieldtypes;
 
+use Carbon\Carbon;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
+use Statamic\Assets\AssetContainer;
 use Statamic\Facades\User;
 use Tests\PreventSavingStacheItemsToDisk;
 use Tests\TestCase;
@@ -12,22 +14,67 @@ class FilesTest extends TestCase
 {
     use PreventSavingStacheItemsToDisk;
 
-    /** @test */
-    public function it_uploads_a_file()
+    public function setUp(): void
     {
+        parent::setUp();
+
+        config(['statamic.assets.image_manipulation.presets.upload' => [
+            'w' => '15',
+            'h' => '20',
+            'fit' => 'crop',
+        ]]);
+
+        Storage::fake('with_preset');
+        Storage::fake('without_preset');
+
+        AssetContainer::make('without_preset')->disk('without_preset')->save();
+        AssetContainer::make('with_preset')->disk('with_preset')->glideSourcePreset('upload')->save();
+    }
+
+    /**
+     * @test
+     * @dataProvider uploadProvider
+     */
+    public function it_uploads_a_file($container, $file, $expectedPath, $expectedWidth, $expectedHeight)
+    {
+        Carbon::setTestNow(Carbon::createFromTimestamp(1671484636));
+
         $disk = Storage::fake('local');
-        $file = UploadedFile::fake()->image('test.jpg');
 
         $this
             ->actingAs(tap(User::make()->makeSuper())->save())
-            ->post('/cp/fieldtypes/files/upload', ['file' => $file])
+            ->post('/cp/fieldtypes/files/upload', [
+                'file' => $file,
+                'container' => $container,
+            ])
             ->assertOk()
             ->assertJson([
                 'data' => [
-                    'id' => $path = now()->timestamp.'/test.jpg',
+                    'id' => $path = $expectedPath,
                 ],
             ]);
 
         $disk->assertExists('statamic/file-uploads/'.$path);
+
+        if ($expectedWidth) {
+            [$width, $height] = getimagesize($disk->path('statamic/file-uploads/'.$path));
+            $this->assertEquals($expectedWidth, $width);
+            $this->assertEquals($expectedHeight, $height);
+        }
+    }
+
+    public function uploadProvider()
+    {
+        $image = UploadedFile::fake()->image('test.jpg', 50, 75);
+        $text = UploadedFile::fake()->create('test.txt');
+
+        return [
+            'no container' => [null, $image, '1671484636/test.jpg', 50, 75],
+            'container with no preset' => ['without_preset', $image, '1671484636/test.jpg', 50, 75],
+            'container with preset' => ['with_preset', $image, '1671484636/test.jpg', 15, 20],
+            'non-image with container' => [null, $text, '1671484636/test.txt', null, null],
+            'non-image with container with no preset' => ['without_preset', $text, '1671484636/test.txt', null, null],
+            'non-image with container with preset' => ['with_preset', $text, '1671484636/test.txt', null, null],
+        ];
     }
 }
