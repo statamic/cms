d65e105409e378bd6d761c4aac50d206d3fe8a09
Jason Varga <jason@pixelfear.com>
2023-01-10 15:30:25 -0500

    delete method requires putting data into config

--- resources/js/components/Preference.js
+++ resources/js/components/Preference.js
@@ -28,7 +28,7 @@ class Preference {
 
     remove(key, value=null, cleanup=true) {
         return this.commitOnSuccessAndReturnPromise(
-            this.instance.$axios.delete(`${this.url}/${key}`, {value, cleanup})
+            this.instance.$axios.delete(`${this.url}/${key}`, { data: { value, cleanup } })
         );
     }
 
