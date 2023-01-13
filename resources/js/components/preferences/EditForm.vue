421e81efdf38b1d194ec011edc0b17a066d0cf36
Jason Varga <jason@pixelfear.com>
2023-01-13 11:50:18 -0500

    Add heading

--- resources/js/components/preferences/EditForm.vue
+++ resources/js/components/preferences/EditForm.vue
@@ -30,6 +30,7 @@
                                 <svg-icon name="chevron-down-xs" class="w-2" />
                             </button>
                         </template>
+                        <h6 class="p-1">{{ __('Save to') }}...</h6>
                         <dropdown-item v-for="option in saveAsOptions" :key="option.url" @click="saveAs(option.url)">
                             <div class="flex items-start pr-2">
                                 <svg-icon :name="option.icon" class="text-grey flex-shrink-0 mr-1 w-4 group-hover:text-white" />
