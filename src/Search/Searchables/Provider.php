c7495e943e4a2d2e2ee115f5cc1a12b5c7aaf3a5
Jason Varga <jason@pixelfear.com>
2023-01-23 20:08:46 -0500

    handle and reference prefix sugar

--- src/Search/Searchables/Provider.php
+++ src/Search/Searchables/Provider.php
@@ -8,12 +8,32 @@ use Statamic\Search\ProvidesSearchables;
 
 abstract class Provider implements ProvidesSearchables
 {
+    protected static $handle;
+    protected static $referencePrefix;
     protected $index;
     protected $keys;
 
     public static function register()
     {
         Search::registerSearchableProvider(static::class);
+    }
+
+    public static function handle(): string
+    {
+        if (static::$handle) {
+            return static::$handle;
+        }
+
+        throw new \Exception('Searchable provider handle not defined.');
+    }
+
+    public static function referencePrefix(): string
+    {
+        if (static::$referencePrefix) {
+            return static::$referencePrefix;
+        }
+
+        throw new \Exception('Searchable provider referencePrefix not defined.');
     }
 
     public function setIndex(Index $index)
