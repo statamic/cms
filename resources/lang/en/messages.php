a1275352a2bb73e6a1a4c03ecc5890fb655bb83a
Jesse Leite <jesseleite@gmail.com>
2023-02-01 11:42:51 -0500

    Add toggle for `sometimes` rule in validation builder (#7416)
    Co-authored-by: Jason Varga <jason@pixelfear.com>

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -75,6 +75,7 @@ return [
     'field_synced_with_origin' => 'Synced with origin. Click or edit the field to desync.',
     'field_validation_advanced_instructions' => 'Add more advanced validation to this field.',
     'field_validation_required_instructions' => 'Control whether or not this field is required.',
+    'field_validation_sometimes_instructions' => 'Only validate when this field is visible or submitted.',
     'fields_always_save_instructions' => 'Always save field value, regardless of how field conditions are evaluated.',
     'fields_blueprints_description' => 'Blueprints define the fields for content structures like collections, taxonomies, users, and forms.',
     'fields_default_instructions' => 'Set the default value.',
