74075065c6c2857dc221df33d1920aed3d1b17cd
Jack Sleight <jacksleight@gmail.com>
2023-02-06 18:14:08 +0000

    Tidier without the "is"?

--- resources/js/components/fieldtypes/bard/BardFieldtype.vue
+++ resources/js/components/fieldtypes/bard/BardFieldtype.vue
@@ -547,8 +547,8 @@ export default {
         },
 
         buttonIsActive(button) {
-            if (button.hasOwnProperty('isActive')) {
-                return button.isActive(this.editor, button.args);
+            if (button.hasOwnProperty('active')) {
+                return button.active(this.editor, button.args);
             }
             const nameProperty = button.hasOwnProperty('activeName') ? 'activeName' : 'name';
             const name = button[nameProperty];
@@ -556,8 +556,8 @@ export default {
         },
 
         buttonIsVisible(button) {
-            if (button.hasOwnProperty('isVisible')) {
-                return button.isVisible(this.editor, button.args);
+            if (button.hasOwnProperty('visible')) {
+                return button.visible(this.editor, button.args);
             }
             if (! button.hasOwnProperty('visibleWhenActive')) return true;
             return this.editor.isActive(button.visibleWhenActive, button.args);
