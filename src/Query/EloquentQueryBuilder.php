3337f26436d28e0ad0f6ff3ff6e93568e25e700f
Ryan Mitchell <ryan@thoughtcollective.com>
2023-02-06 19:27:35 +0000

    Cache result of schema call to avoid duplication (#7466)

--- src/Query/EloquentQueryBuilder.php
+++ src/Query/EloquentQueryBuilder.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Carbon;
 use InvalidArgumentException;
 use Statamic\Contracts\Query\Builder;
 use Statamic\Extensions\Pagination\LengthAwarePaginator;
+use Statamic\Facades\Blink;
 use Statamic\Support\Arr;
 
 abstract class EloquentQueryBuilder implements Builder
@@ -394,7 +395,12 @@ abstract class EloquentQueryBuilder implements Builder
             // exception. Stripping out invalid columns is fine here. They
             // will still be sent through and used for augmentation.
             $model = $this->builder->getModel();
-            $schema = $model->getConnection()->getSchemaBuilder()->getColumnListing($model->getTable());
+            $table = $model->getTable();
+
+            $schema = Blink::once("eloquent-schema-{$table}", function () use ($model, $table) {
+                return $model->getConnection()->getSchemaBuilder()->getColumnListing($table);
+            });
+
             $selected = array_intersect($schema, $columns);
         }
 
