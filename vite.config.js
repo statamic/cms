63705317d6b49dfc0a7d1ed9520d44e68969eeab
Jason Varga <jason@pixelfear.com>
2023-02-07 10:53:36 -0500

    read valet tls from env file

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,38 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue2';
 import inject from '@rollup/plugin-inject';
 import svgLoader from './vite-svg-loader';
 
-export default defineConfig({
-    base: '/vendor/statamic/cp/build',
-    plugins: [
-        laravel({
-            valetTls: 'statamic3.test',
-            input: [
-                'resources/sass/cp.scss',
-                'resources/js/app.js'
-            ],
-            refresh: true,
-            publicDirectory: 'resources/dist',
-            hotFile: 'resources/dist/hot',
-        }),
-        vue(),
-        svgLoader(),
-        inject({
-            Vue: 'vue',
-            jQuery: 'jquery',
-            _: 'underscore',
-            include: 'resources/js/**'
-        })
-    ],
-    resolve: {
-        alias: {
-            vue: 'vue/dist/vue.esm.js',
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        base: '/vendor/statamic/cp/build',
+        plugins: [
+            laravel({
+                valetTls: env.VALET_TLS,
+                input: [
+                    'resources/sass/cp.scss',
+                    'resources/js/app.js'
+                ],
+                refresh: true,
+                publicDirectory: 'resources/dist',
+                hotFile: 'resources/dist/hot',
+            }),
+            vue(),
+            svgLoader(),
+            inject({
+                Vue: 'vue',
+                jQuery: 'jquery',
+                _: 'underscore',
+                include: 'resources/js/**'
+            })
+        ],
+        resolve: {
+            alias: {
+                vue: 'vue/dist/vue.esm.js',
+            }
         }
     }
 });
