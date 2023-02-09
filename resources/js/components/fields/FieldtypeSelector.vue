dd1f4f0e4b8662473f986e6bcedbd1a656b5d06c
Jesse Leite <jesseleite@gmail.com>
2023-02-08 23:24:25 -0500

    Replaced `pr-1.5` with `pr-3`

--- resources/js/components/fields/FieldtypeSelector.vue
+++ resources/js/components/fields/FieldtypeSelector.vue
@@ -21,7 +21,7 @@
                 <div class="fieldtype-selector">
                     <div class="fieldtype-list">
                         <div class="p-2___REPLACED" v-for="fieldtype in group.fieldtypes" :key="fieldtype.handle">
-                            <button class="bg-white border border-grey-50 flex items-center group w-full rounded hover:border-grey-60 shadow-sm hover:shadow-md pr-1.5"
+                            <button class="bg-white border border-grey-50 flex items-center group w-full rounded hover:border-grey-60 shadow-sm hover:shadow-md pr-3___REPLACED"
                                 @click="select(fieldtype)">
                                 <div class="p-2___REPLACED flex items-center border-r border-grey-50 group-hover:border-grey-60 bg-grey-20 rounded-l">
                                     <svg-icon class="h-5 w-5 text-grey-80" :name="fieldtype.icon" default="generic-field"></svg-icon>
