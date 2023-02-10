c2c4eb56d928384b967dc4c3c74eeb9e18276add
Arthur Perton <arthur@webenapp.nl>
2023-02-10 12:40:18 +0100

    Default to current site when locale is not set

--- src/StaticCaching/Cachers/FileCacher.php
+++ src/StaticCaching/Cachers/FileCacher.php
@@ -149,7 +149,7 @@ class FileCacher extends AbstractCacher
         $paths = $this->getCachePaths();
 
         if (! $site) {
-            $site = $this->config('locale');
+            $site = $this->config('locale', Site::current()->handle());
         }
 
         return $paths[$site];
