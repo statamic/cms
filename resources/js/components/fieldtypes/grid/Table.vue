198975d934d253f508b40c36c57f1a247ac4ab11
Jason Varga <jason@pixelfear.com>
2023-02-21 15:53:54 -0500

    refactor out reactiveProvide, and add fullscreen to grid

--- resources/js/components/fieldtypes/grid/Table.vue
+++ resources/js/components/fieldtypes/grid/Table.vue
@@ -9,7 +9,12 @@
                     :key="field.handle"
                     :field="field"
                 />
-                <th class="row-controls" v-if="!grid.isReadOnly"></th>
+                <th class="row-controls">
+                    <button @click="grid.toggleFullScreen" class="flex items-center w-full h-full justify-center text-gray-500 hover:text-gray-700">
+                        <svg-icon name="expand-2" class="h-3.5 w-3.5" v-show="! grid.fullScreenMode" />
+                        <svg-icon name="shrink-all" class="h-3.5 w-3.5" v-show="grid.fullScreenMode" />
+                    </button>
+                </th>
             </tr>
         </thead>
         <sortable-list
