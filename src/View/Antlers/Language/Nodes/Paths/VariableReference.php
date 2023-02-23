56e01d1a3d7cbd0f578e5228138a0562b739e5b4
John Koster <john@stillat.com>
2023-02-23 09:42:11 -0600

    Correctly maintains the correct assigned values when setting nested paths

--- src/View/Antlers/Language/Nodes/Paths/VariableReference.php
+++ src/View/Antlers/Language/Nodes/Paths/VariableReference.php
@@ -44,6 +44,11 @@ class VariableReference
     public $isFinal = false;
 
     /**
+     * @var VariableReference|null
+     */
+    private $cachedRoot = null;
+
+    /**
      * Determines if the provided path is a complex variable path or not.
      *
      * @return bool
@@ -66,6 +71,21 @@ class VariableReference
         }
 
         return false;
+    }
+
+    public function getRoot(): VariableReference
+    {
+        if ($this->cachedRoot == null) {
+            $this->cachedRoot = new VariableReference();
+
+            if (count($this->pathParts) == 0) {
+                return $this->cachedRoot;
+            }
+
+            $this->cachedRoot->pathParts[] = $this->pathParts[0];
+        }
+
+        return $this->cachedRoot;
     }
 
     /**
