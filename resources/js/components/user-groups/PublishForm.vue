fad84607395c33bc5a9af94578e6b4978154cc99
Jack McDade <jack@jackmcdade.com>
2023-02-23 16:15:00 -0500

    [4.x] Move all Save buttons to top (#7600)
    * Move all Save buttons to the top. The people have spoken.
    
    * Now with working features!
    
    * ...and words!

--- resources/js/components/user-groups/PublishForm.vue
+++ resources/js/components/user-groups/PublishForm.vue
@@ -1,6 +1,14 @@
 <template>
 
         <div>
+            <header class="mb-6">
+                <breadcrumb :url="breadcrumbUrl" :title="__('User Groups')" />
+                <div class="flex items-center">
+                    <h1 class="flex-1" v-text="title || __('Create Group')" />
+                    <button type="submit" class="btn-primary" @click="save">{{ __('Save') }}</button>
+                </div>
+            </header>
+
             <div class="card p-0 mb-6 publish-fields @container">
 
                 <form-group
@@ -49,10 +57,6 @@
                 </div>
 
             </div>
-            <div class="py-4 border-t flex justify-between">
-                <a :href="action" class="btn" v-text="__('Cancel') "/>
-                <button type="submit" class="btn-primary" @click="save">{{ __('Save') }}</button>
-            </div>
         </div>
 </template>
 
@@ -67,7 +71,8 @@ export default {
         initialUsers: Array,
         action: String,
         method: String,
-        creating: Boolean
+        creating: Boolean,
+        breadcrumbUrl: String,
     },
 
     data() {
