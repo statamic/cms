d1937101cbda66548dd6eface0c83100b0137b37
Jason Varga <jason@pixelfear.com>
2023-02-24 15:54:56 -0500

    [4.x] Routing (#7609)

--- routes/glide.php
+++ routes/glide.php
@@ -4,13 +4,14 @@ use Illuminate\Support\Facades\Route;
 use Statamic\Facades\Glide;
 use Statamic\Facades\Site;
 use Statamic\Facades\URL;
+use Statamic\Http\Controllers\GlideController;
 
 Site::all()->map(function ($site) {
     return URL::makeRelative($site->url());
 })->unique()->each(function ($sitePrefix) {
     Route::group(['prefix' => $sitePrefix.'/'.Glide::route()], function () {
-        Route::get('/asset/{container}/{path?}', 'GlideController@generateByAsset')->where('path', '.*');
-        Route::get('/http/{url}/{filename?}', 'GlideController@generateByUrl');
-        Route::get('{path}', 'GlideController@generateByPath')->where('path', '.*');
+        Route::get('/asset/{container}/{path?}', [GlideController::class, 'generateByAsset'])->where('path', '.*');
+        Route::get('/http/{url}/{filename?}', [GlideController::class, 'generateByUrl']);
+        Route::get('{path}', [GlideController::class, 'generateByPath'])->where('path', '.*');
     });
 });
