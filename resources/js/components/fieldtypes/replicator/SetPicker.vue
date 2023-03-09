91d10cfae38f5a9ffb4e87900e6e0a821859f034
Jason Varga <jason@pixelfear.com>
2023-03-09 15:21:04 -0500

    search

--- resources/js/components/fieldtypes/replicator/SetPicker.vue
+++ resources/js/components/fieldtypes/replicator/SetPicker.vue
@@ -6,14 +6,14 @@
         </template>
         <template #default>
             <div class="set-picker-header p-3 border-b text-xs flex items-center">
-                <input type="text" class="py-1 px-2 border rounded w-full" :placeholder="__('Search Sets')" v-show="showSearch" />
+                <input type="text" class="py-1 px-2 border rounded w-full" :placeholder="__('Search Sets')" v-show="showSearch" v-model="search" />
                 <button v-show="showGroupBreadcrumb" @click="unselectGroup" class="text-gray-700 hover:text-gray-900">
                     <svg-icon name="chevron-left" class="w-2 h-2 mx-1" />
                     {{ selectedGroupDisplayText }}
                 </button>
             </div>
             <div class="p-1">
-                <div v-if="!selectedGroup" v-for="group in sets" :key="group.handle" class="cursor-pointer rounded">
+                <div v-if="showGroups" v-for="group in sets" :key="group.handle" class="cursor-pointer rounded">
                     <div @click="selectGroup(group.handle)" class="flex items-center group px-2 py-1.5 hover:bg-gray-200 rounded-md">
                         <div class="h-10 w-10 rounded bg-white border border-gray-600 mr-2 p-2.5">
                             <svg-icon name="folder-generic" class="text-gray-800" />
@@ -25,16 +25,14 @@
                         <svg-icon name="chevron-right-thin" class="text-gray-600 group-hover:text-gray-800" />
                     </div>
                 </div>
-                <template v-for="group in sets">
-                    <div v-for="set in group.sets" :key="set.handle" v-show="group.handle === selectedGroupHandle" class="cursor-pointer rounded">
-                        <div @click="addSet(set.handle)" class="flex items-center group px-2 py-1.5 hover:bg-gray-200 rounded-md">
-                            <div class="flex-1">
-                                <div class="text-md font-medium text-gray-800 truncate w-52">{{ set.display || set.handle }}</div>
-                                <div v-if="set.instructions" class="text-2xs text-gray-700 truncate w-52">{{ set.instructions }}</div>
-                            </div>
+                <div v-for="set in visibleSets" :key="set.handle" class="cursor-pointer rounded">
+                    <div @click="addSet(set.handle)" class="flex items-center group px-2 py-1.5 hover:bg-gray-200 rounded-md">
+                        <div class="flex-1">
+                            <div class="text-md font-medium text-gray-800 truncate w-52">{{ set.display || set.handle }}</div>
+                            <div v-if="set.instructions" class="text-2xs text-gray-700 truncate w-52">{{ set.instructions }}</div>
                         </div>
                     </div>
-                </template>
+                </div>
             </div>
         </template>
     </popover>
@@ -54,7 +52,8 @@ export default {
 
     data() {
         return {
-            selectedGroupHandle: null
+            selectedGroupHandle: null,
+            search: null
         }
     },
 
@@ -68,6 +67,10 @@ export default {
             return this.hasMultipleGroups && this.selectedGroup;
         },
 
+        showGroups() {
+            return this.hasMultipleGroups && !this.selectedGroup && !this.search;
+        },
+
         hasMultipleGroups() {
             return this.sets.length > 1;
         },
@@ -78,6 +81,23 @@ export default {
 
         selectedGroupDisplayText() {
             return this.selectedGroup ? this.selectedGroup.display || this.selectedGroup.handle : null;
+        },
+
+        visibleSets() {
+            if (!this.selectedGroup && !this.search) return [];
+
+            let sets = this.selectedGroup ? this.selectedGroup.sets : this.sets.reduce((sets, group) => {
+                return sets.concat(group.sets);
+            }, []);
+
+            if (this.search) {
+                return sets.filter(set => {
+                    return set.display.toLowerCase().includes(this.search.toLowerCase())
+                        || set.handle.toLowerCase().includes(this.search.toLowerCase());
+                });
+            }
+
+            return sets;
         }
 
     },
