8e98203d83559f1659565b06fc7fd3392dad4173
Jesse Leite <jesseleite@gmail.com>
2023-03-15 15:05:03 -0400

    Cast to collection, and tweak graphql exception message.

--- src/GraphQL/Middleware/AllowedFilters.php
+++ src/GraphQL/Middleware/AllowedFilters.php
@@ -6,13 +6,12 @@ use Closure;
 use GraphQL\Type\Definition\ResolveInfo;
 use Illuminate\Validation\ValidationException;
 use Rebing\GraphQL\Support\Middleware;
-use Statamic\Support\Str;
 
 class AllowedFilters extends Middleware
 {
     public function handle($root, $args, $context, ResolveInfo $info, Closure $next)
     {
-        $allowedFilters = $root->allowedFilters($args);
+        $allowedFilters = collect($root->allowedFilters($args));
 
         $forbidden = collect($args['filter'] ?? [])
             ->keys()
@@ -20,7 +19,7 @@ class AllowedFilters extends Middleware
 
         if ($forbidden->isNotEmpty()) {
             throw ValidationException::withMessages([
-                'filter' => Str::plural('Forbidden filter', $forbidden).': '.$forbidden->join(', '),
+                'filter' => 'Forbidden: '.$forbidden->join(', '),
             ]);
         }
 
