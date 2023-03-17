69c61f6e5cc0dceddb9fa48270461f2e13dd9880
Joshua Blum <hallo@joshuablum.de>
2023-03-17 15:10:32 +0100

    Remove "Add toggle to user blueprint" from this branch
    This reverts commit 7d8b0774c8c64b0519b7bcf844100b90d47f032c.

--- src/Http/Controllers/CP/Users/UsersController.php
+++ src/Http/Controllers/CP/Users/UsersController.php
@@ -186,10 +186,6 @@ class UsersController extends CpController
             $blueprint->ensureField('groups', ['visibility' => 'read_only']);
         }
 
-        if (User::current()->isSuper()) {
-            $blueprint->ensureField('Super Admin', ['type' => 'toggle']);
-        }
-
         $values = $user->data()
             ->merge($user->computedData())
             ->merge(['email' => $user->email()]);
