e7c345df866fb1687939dacf23de54c8b32f8119
Jesse Leite <jesseleite@gmail.com>
2023-03-22 11:11:46 -0400

    Just use `filterQuery()` from trait now, as `password` filter is handled by middleware pre-query.

--- src/GraphQL/Queries/UsersQuery.php
+++ src/GraphQL/Queries/UsersQuery.php
@@ -8,15 +8,14 @@ use Statamic\Facades\GraphQL;
 use Statamic\Facades\User;
 use Statamic\GraphQL\Middleware\AuthorizeFilters;
 use Statamic\GraphQL\Middleware\ResolvePage;
+use Statamic\GraphQL\Queries\Concerns\FiltersQuery;
 use Statamic\GraphQL\Types\JsonArgument;
 use Statamic\GraphQL\Types\UserType;
-use Statamic\Support\Arr;
 use Statamic\Support\Str;
-use Statamic\Tags\Concerns\QueriesConditions;
 
 class UsersQuery extends Query
 {
-    use QueriesConditions;
+    use FiltersQuery;
 
     protected $attributes = [
         'name' => 'users',
@@ -55,29 +54,6 @@ class UsersQuery extends Query
         }
 
         return $query->paginate($args['limit'] ?? 1000);
-    }
-
-    private function filterQuery($query, $filters)
-    {
-        $filters = collect($filters)->reject(fn ($_, $filter) => Str::startsWith($filter, 'password'));
-
-        foreach ($filters as $field => $definitions) {
-            if (! is_array($definitions)) {
-                $definitions = [['equals' => $definitions]];
-            }
-
-            if (Arr::assoc($definitions)) {
-                $definitions = collect($definitions)->map(function ($value, $key) {
-                    return [$key => $value];
-                })->values()->all();
-            }
-
-            foreach ($definitions as $definition) {
-                $condition = array_keys($definition)[0];
-                $value = array_values($definition)[0];
-                $this->queryCondition($query, $field, $condition, $value);
-            }
-        }
     }
 
     private function sortQuery($query, $sorts)
