eec90914ac8655a786cd38a7bcf09e601c92c557
Jack McDade <jack@jackmcdade.com>
2023-03-22 12:37:30 -0400

    wip timepicker masking

--- resources/js/app.js
+++ resources/js/app.js
@@ -36,6 +36,7 @@ import Vuex from 'vuex';
 import StatamicStore from './store';
 import vSelect from 'vue-select'
 import VCalendar from 'v-calendar';
+import VueMask from 'v-mask'
 
 // Customize vSelect UI components
 vSelect.props.components.default = () => ({
@@ -72,6 +73,7 @@ Vue.use(PortalVue)
 Vue.use(VModal, { componentName: 'vue-modal' })
 Vue.use(Vuex);
 Vue.use(VCalendar);
+Vue.use(VueMask);
 
 Vue.component(vSelect)
 
