0ce8866da6081089aa052ced0a05a06d198b40ab
Jack McDade <jack@jackmcdade.com>
2023-03-29 08:58:12 -0400

    Add ability to disable Grid Fullscreen mode
    Closes #963.

--- resources/js/components/fieldtypes/grid/View.vue
+++ resources/js/components/fieldtypes/grid/View.vue
@@ -1,7 +1,7 @@
 <script>
 export default {
 
-    props: ['fields', 'rows', 'meta', 'name', 'canDeleteRows', 'canAddRows'],
+    props: ['fields', 'rows', 'meta', 'name', 'canDeleteRows', 'canAddRows', 'allowFullScreen'],
 
     inject: ['grid'],
 
