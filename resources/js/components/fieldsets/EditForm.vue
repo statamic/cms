cbebc37c47ab8d0c08f8b52a4dafa102e2ea97fc
Jason Varga <jason@pixelfear.com>
2023-03-30 16:06:49 -0400

    Fix weird padding on fieldset edit form. Not using config field as it looks weird with a single field.

--- resources/js/components/fieldsets/EditForm.vue
+++ resources/js/components/fieldsets/EditForm.vue
@@ -10,15 +10,20 @@
             </div>
         </header>
 
-        <div class="publish-form card p-0 mb-10">
-
-            <div class="form-group">
-                <label class="block">{{ __('Title') }}</label>
-                <small class="help-block">{{ __('messages.fieldsets_title_instructions') }}</small>
-                <div v-if="errors.title">
-                    <small class="help-block text-red-500" v-for="(error, i) in errors.title" :key="i" v-text="error" />
+        <div class="publish-form card p-0 @container mb-8">
+            <div class="publish-fields">
+                <div class="form-group w-full">
+                    <div class="field-inner">
+                        <label class="block">{{ __('Title') }}</label>
+                        <small class="help-block -mt-2">{{ __('messages.fieldsets_title_instructions') }}</small>
+                        <div v-if="errors.title">
+                            <small class="help-block text-red-500" v-for="(error, i) in errors.title" :key="i" v-text="error" />
+                        </div>
+                    </div>
+                    <div>
+                        <input type="text" name="title" class="input-text" v-model="fieldset.title" autofocus="autofocus">
+                    </div>
                 </div>
-                <input type="text" name="title" class="input-text" v-model="fieldset.title" autofocus="autofocus">
             </div>
 
         </div>
