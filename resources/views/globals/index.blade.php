732add4bec9aa0ddf68580ad6fa45daa32a16bfd
Jack McDade <jack@jackmcdade.com>
2023-04-02 12:23:42 -0400

    New empty container layout with new icons

--- resources/views/globals/index.blade.php
+++ resources/views/globals/index.blade.php
@@ -19,7 +19,7 @@
         @include('statamic::partials.empty-state', [
             'title' => __('Globals'),
             'description' => __('statamic::messages.global_set_config_intro'),
-            'svg' => 'empty/content',
+            'svg' => 'empty/globals',
             'button_url' => cp_route('globals.create'),
             'button_text' => __('Create Global Set'),
             'can' => $user->can('create', 'Statamic\Contracts\Globals\GlobalSet')
