25fa052cc4273089e0b5d27bc85345b77317d658
Jason Varga <jason@pixelfear.com>
2023-04-03 17:25:36 -0400

    fix indentation

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,46 +5,46 @@
 </head>
 
 <body>
-   <div id="statamic" :style="{ marginRight: panes.length ? `24rem` : null }">
+    <div id="statamic" :style="{ marginRight: panes.length ? `24rem` : null }">
 
-      @include('statamic::partials.session-expiry')
-      @include('statamic::partials.licensing-alerts')
-      @include('statamic::partials.global-header')
+        @include('statamic::partials.session-expiry')
+        @include('statamic::partials.licensing-alerts')
+        @include('statamic::partials.global-header')
 
-      <div id="main"
+        <div id="main"
             class="@yield('content-class')"
             :class="{
-                  'nav-closed': ! navOpen,
-                  'nav-mobile-open': mobileNavOpen,
-                  'showing-license-banner': showBanner
-            }">
+            'nav-closed': ! navOpen,
+            'nav-mobile-open': mobileNavOpen,
+            'showing-license-banner': showBanner
+        }">
             @include('statamic::partials.nav-main')
             @include('statamic::partials.nav-mobile')
 
             <div class="workspace">
-                  <div class="page-wrapper" :class="wrapperClass">
-                        @yield('content')
-                  </div>
+                <div class="page-wrapper" :class="wrapperClass">
+                    @yield('content')
+                </div>
             </div>
 
-      </div>
+        </div>
 
-      <component
-        v-for="component in appendedComponents"
-        :key="component.id"
-        :is="component.name"
-        v-bind="component.props"
-        v-on="component.events"
-      ></component>
+        <component
+            v-for="component in appendedComponents"
+            :key="component.id"
+            :is="component.name"
+            v-bind="component.props"
+            v-on="component.events"
+        ></component>
 
-      <keyboard-shortcuts-modal></keyboard-shortcuts-modal>
+        <keyboard-shortcuts-modal></keyboard-shortcuts-modal>
 
-      <portal-targets></portal-targets>
+        <portal-targets></portal-targets>
 
     </div>
 
-@include('statamic::partials.scripts')
-@yield('scripts')
+    @include('statamic::partials.scripts')
+    @yield('scripts')
 
 </body>
 </html>
