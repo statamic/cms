423619b7761e6f14b5f27cd666c13d78aa967049
Jason Varga <jason@pixelfear.com>
2023-04-13 17:11:55 -0400

    make each pane scrollable

--- resources/js/components/data-list/ColumnPicker.vue
+++ resources/js/components/data-list/ColumnPicker.vue
@@ -8,58 +8,61 @@
             <svg-icon name="light/settings-horizontal" class="w-4 h-4" />
         </button>
 
-        <modal v-if="open" name="column-picker" @closed="open = false" adaptive draggable=".modal-drag-handle" click-to-close>
+        <modal v-if="open" name="column-picker" @closed="open = false" draggable=".modal-drag-handle" click-to-close>
+            <div class="flex flex-col h-full">
 
-            <header class="modal-drag-handle p-4 bg-gray-200 border-b flex items-center justify-between cursor-grab active:cursor-grabbing">
-                <h2>{{ __('Customize Columns') }}</h2>
-                <button class="btn-close" @click="open = false" :aria-label="__('Close Editor')">&times;</button>
-            </header>
+                <header class="modal-drag-handle p-4 bg-gray-200 border-b flex items-center justify-between cursor-grab active:cursor-grabbing">
+                    <h2>{{ __('Customize Columns') }}</h2>
+                    <button class="btn-close" @click="open = false" :aria-label="__('Close Editor')">&times;</button>
+                </header>
 
-            <div class="column-picker flex rounded-t-md bg-gray-100 overflow-y-scroll h-screen-64">
+                <div class="flex grow min-h-0 rounded-t-md bg-gray-100">
+                    <!-- Available Columns -->
+                    <div class="outline-none text-left w-1/2 border-r flex flex-col">
+                        <header v-text="__('Available Columns')" class="border-b py-2 px-3 text-sm bg-white font-medium"/>
+                        <div class="flex flex-1 flex-col space-y-1 py-2 px-3 select-none shadow-inner overflow-y-scroll">
+                            <div class="column-picker-item" v-for="column in hiddenColumns" :key="column.field" v-if="hiddenColumns.length">
+                                <label class="flex items-center cursor-pointer">
+                                    <input type="checkbox" class="mr-2" v-model="column.visible" @change="columnToggled(column) "/>
+                                    {{ column.label }}
+                                </label>
+                            </div>
+                        </div>
+                    </div>
 
-                <!-- Available Columns -->
-                <div class="outline-none text-left w-1/2 border-r">
-                    <header v-text="__('Available Columns')" class="border-b py-2 px-3 text-sm bg-white font-medium"/>
-                    <div class="flex flex-col space-y-1 py-2 px-3 select-none shadow-inner">
-                        <div class="column-picker-item" v-for="column in hiddenColumns" :key="column.field" v-if="hiddenColumns.length">
-                            <label class="flex items-center cursor-pointer">
-                                <input type="checkbox" class="mr-2" v-model="column.visible" @change="columnToggled(column) "/>
-                                {{ column.label }}
-                            </label>
+                    <!-- Displayed Columns -->
+                    <div class="w-1/2 flex flex-col">
+                        <header v-text="__('Displayed Columns')" class="border-b px-3 py-2 text-sm bg-white rounded-tl-md font-medium flex-none"/>
+                        <div class="grow overflow-y-scroll shadow-inner">
+                            <sortable-list
+                                v-model="selectedColumns"
+                                :vertical="true"
+                                :distance="10"
+                                item-class="item"
+                                handle-class="item"
+                                append-to=".modal-body"
+                                constrain-dimensions
+                            >
+                                <div class="space-y-1 px-3 p-3 select-none">
+                                    <div class="item sortable cursor-grab" v-for="column in selectedColumns" :key="column.field">
+                                        <div class="item-move py-1">&nbsp;</div>
+                                        <div class="flex flex-1 ml-2 items-center p-0">
+                                            <input type="checkbox" class="mr-2" v-model="column.visible" @change="columnToggled(column)" :disabled="selectedColumns.length === 1" />
+                                            {{ column.label }}
+                                        </div>
+                                    </div>
+                                </div>
+                            </sortable-list>
                         </div>
                     </div>
                 </div>
 
-                <!-- Displayed Columns -->
-                <div class="w-1/2">
-                    <header v-text="__('Displayed Columns')" class="border-b px-3 py-2 text-sm bg-white rounded-tl-md font-medium"/>
-                    <sortable-list
-                        v-model="selectedColumns"
-                        :vertical="true"
-                        :distance="10"
-                        item-class="item"
-                        handle-class="item"
-                        append-to=".modal-body"
-                        constrain-dimensions
-                    >
-                        <div class="flex flex-col space-y-1 px-3 p-3 select-none shadow-inner">
-                            <div class="item sortable cursor-grab" v-for="column in selectedColumns" :key="column.field">
-                                <div class="item-move py-1">&nbsp;</div>
-                                <div class="flex flex-1 ml-2 items-center p-0">
-                                    <input type="checkbox" class="mr-2" v-model="column.visible" @change="columnToggled(column)" :disabled="selectedColumns.length === 1" />
-                                    {{ column.label }}
-                                </div>
-                            </div>
-                        </div>
-                    </sortable-list>
-                </div>
+                <footer class="px-3 py-2 border-t flex items-center justify-end" v-if="preferencesKey">
+                    <button class="btn" v-text="__('Reset')" @click="reset" :disabled="saving" />
+                    <button class="ml-3 btn-primary" v-text="__('Save')" @click="save" :disabled="saving" />
+                </footer>
+
             </div>
-
-            <footer class="px-3 py-2 border-t flex items-center justify-end" v-if="preferencesKey">
-                <button class="btn" v-text="__('Reset')" @click="reset" :disabled="saving" />
-                <button class="ml-3 btn-primary" v-text="__('Save')" @click="save" :disabled="saving" />
-            </footer>
-
         </modal>
     </div>
 </template>
