7f2b9c6a19a9ffa978831eedc7f8a81578ed57a7
Rob de Kort <rob@studio1902.nl>
2023-04-14 09:50:21 +0200

    Add Dutch passwords strings file

---
+++ resources/lang/nl/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Je wachtwoord is gereset.',
+    'sent' => 'We hebben een e-mail met een wachtwoordresetlink verstuurd.',
+    'throttled' => 'Wacht even voordat je het opnieuw probeert.',
+    'token' => 'Deze wachtwoordresettoken is niet geldig.',
+    'user' => "We kunnen geen gebruiker vinden met dat e-mailadres.",
+
+];
