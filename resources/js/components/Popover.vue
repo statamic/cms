31c59a530414e02fc1f9c10220277648ac89d081
Jason Varga <jason@pixelfear.com>
2023-04-21 16:34:05 -0400

    Add support for popover arrows and show them by default

--- resources/js/components/Popover.vue
+++ resources/js/components/Popover.vue
@@ -12,9 +12,11 @@
         >
             <div :class="`${isOpen ? 'popover-open' : ''}`">
                 <div ref="popover" class="popover" v-if="!disabled" v-on-clickaway="clickawayClose">
-                    <div class="popover-content bg-white shadow-popover rounded-md">
+                    <div class="popover-content bg-white shadow-popover rounded-md relative z-2">
                         <slot :close="close" />
                     </div>
+                    <div v-if="arrow" ref="arrow" class="absolute z-3 bg-white h-3 w-3 rotate-45" />
+                    <div v-if="arrow" ref="arrowShadow" class="absolute z-1 shadow-popover h-3 w-3 rotate-45" />
                 </div>
             </div>
         </portal>
@@ -24,7 +26,7 @@
 
 <script>
 import { mixin as clickaway } from 'vue-clickaway';
-import { computePosition, flip, shift, offset, autoUpdate } from '@floating-ui/dom';
+import { computePosition, flip, shift, offset, autoUpdate, arrow } from '@floating-ui/dom';
 
 export default {
 
@@ -51,6 +53,10 @@ export default {
             type: String,
             default: 'bottom-end',
         },
+        arrow: {
+            type: Boolean,
+            default: true
+        }
     },
 
     data() {
@@ -78,17 +84,54 @@ export default {
         computePosition() {
             if (! this.$refs.trigger) return;
 
+            // Adjust the offset so that the arrow doesn't overlap the trigger.
+            let mainAxisOffset = this.offset[0];
+            if (this.arrow) {
+                const arrowLen = this.$refs.arrow.offsetWidth;
+                const halfArrowHypotenuseLength = Math.sqrt(2 * arrowLen ** 2) / 2;
+                mainAxisOffset += halfArrowHypotenuseLength;
+            }
+
+            let middleware = [
+                offset({ mainAxis: mainAxisOffset, crossAxis: this.offset[1] }),
+                flip(), // If you place it on the right, and there's not enough room, it'll flip to the left, etc.
+                shift({ padding: 5 }), // If it'll end up positioned offscreen, it'll shift it enough to display it fully.
+            ];
+
+            if (this.arrow) {
+                middleware.push(arrow({ element: this.$refs.arrow, padding: 20 }));
+            }
+
             computePosition(this.$refs.trigger.firstChild, this.$refs.popover, {
                 placement: this.placement,
-                middleware: [
-                    offset({ mainAxis: this.offset[0], crossAxis: this.offset[1] }),
-                    flip(), // If you place it on the right, and there's not enough room, it'll flip to the left, etc.
-                    shift({ padding: 5 }), // If it'll end up positioned offscreen, it'll shift it enough to display it fully.
-                ],
-            }).then(({ x, y }) => {
+                middleware
+            }).then(({ x, y, middlewareData, placement }) => {
                 Object.assign(this.$refs.popover.style, {
                     transform: `translate(${Math.round(x)}px, ${Math.round(y)}px)`, // Round to avoid blurry text
                 });
+
+                if (middlewareData.arrow) {
+                    const { x, y } = middlewareData.arrow;
+                    const side = placement.split('-')[0];
+                    const staticSide = {
+                        top: 'bottom',
+                        right: 'left',
+                        bottom: 'top',
+                        left: 'right'
+                    }[side];
+
+                    [this.$refs.arrow, this.$refs.arrowShadow].forEach(el => {
+                        const len = el.offsetWidth;
+                        Object.assign(el.style, {
+                            left: x != null ? `${x}px` : '',
+                            top: y != null ? `${y}px` : '',
+                            // Ensure the static side gets unset when flipping to other placements' axes.
+                            right: '',
+                            bottom: '',
+                            [staticSide]: `${-len/2}px`,
+                        });
+                    });
+                }
             });
         },
 
