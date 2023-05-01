1000efd1dd10cae85ec759d352ec10afe2f0d9fb
Jason Varga <jason@pixelfear.com>
2023-05-01 16:59:36 -0400

    make this smaller so the min/max fields sit side by side

--- src/Fieldtypes/Assets/Assets.php
+++ src/Fieldtypes/Assets/Assets.php
@@ -40,7 +40,6 @@ class Assets extends Fieldtype
                 'type' => 'asset_container',
                 'max_items' => 1,
                 'mode' => 'select',
-                'width' => 50,
                 'default' => AssetContainer::all()->count() == 1 ? AssetContainer::all()->first()->handle() : null,
             ],
             'folder' => [
