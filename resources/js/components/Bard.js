ed1441c37d9a354fa641930e7376869915430e61
Jack Sleight <jacksleight@gmail.com>
2023-05-02 13:54:01 +0100

    And again

--- resources/js/components/Bard.js
+++ resources/js/components/Bard.js
@@ -10,7 +10,7 @@ class Bard {
         this.extensionReplacementCallbacks = [];
         this.buttonCallbacks = [];
     }
-    
+
     addExtension(callback) {
         this.extensionCallbacks.push(callback);
     }
