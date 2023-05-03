c03af997ebed27b12a1339ee0654094bb04e09b4
Jason Varga <jason@pixelfear.com>
2023-05-03 13:14:22 -0400

    remove some unused private methods

--- src/Fieldtypes/Sets.php
+++ src/Fieldtypes/Sets.php
@@ -119,39 +119,4 @@ class Sets extends Fieldtype
         })
         ->all();
     }
-
-    private function moveOutNameKey($fields)
-    {
-        $processed = [];
-
-        foreach ($fields as $field) {
-            $handle = $field['handle'];
-            unset($field['handle']);
-            $processed[$handle] = $this->recursivelyProcess($field);
-        }
-
-        return $processed;
-    }
-
-    private function recursivelyProcess($config)
-    {
-        // Get the fieldtype for this field
-        $type = $config['type'];
-        $config_fieldtype = FieldtypeFactory::create($type);
-
-        // Get the fieldtype's config fieldset
-        $fieldset = $config_fieldtype->getConfigFieldset();
-
-        // Process all the fields in the fieldset
-        foreach ($fieldset->fieldtypes() as $field) {
-            // Ignore if the field isn't in the config
-            if (! in_array($field->getName(), array_keys($config))) {
-                continue;
-            }
-
-            $config[$field->getName()] = $field->process($config[$field->getName()]);
-        }
-
-        return Fieldset::cleanFieldForSaving($config);
-    }
 }
