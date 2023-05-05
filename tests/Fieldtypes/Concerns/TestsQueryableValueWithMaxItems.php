40e5c6d9e392721494ef560c3c1a5fc62977d582
Jason Varga <jason@pixelfear.com>
2023-05-05 16:38:43 -0400

    use single trait, single test method, let data provider provides more cases with input and expectations

--- tests/Fieldtypes/Concerns/TestsQueryableValueWithMaxItems.php
+++ tests/Fieldtypes/Concerns/TestsQueryableValueWithMaxItems.php
@@ -4,40 +4,51 @@ namespace Tests\Fieldtypes\Concerns;
 
 trait TestsQueryableValueWithMaxItems
 {
-    public static function maxItemsConfigGreaterThanOne()
+    public static function maxItemsProvider()
     {
+        // * Maybe the config changed since the value was saved
+
         return [
-            [[]],
-            [['max_items' => 2]],
-            [['max_items' => 5]],
+            'max undefined, single item array' => [null, ['a'], ['a']],
+            'max undefined, multiple item array' => [null, ['a', 'b'], ['a', 'b']],
+            'max undefined, empty array' => [null, [], []],
+            'max undefined, null' => [null, null, []],
+            'max undefined, string' => [null, 'a', ['a']], // *
+
+            'max > 1, single item array' => [2, ['a'], ['a']],
+            'max > 1, multiple item array' => [2, ['a', 'b'], ['a', 'b']],
+            'max > 1, empty array' => [2, [], []],
+            'max > 1, null' => [2, null, []],
+            'max > 1, string' => [2, 'a', ['a']], // *
+
+            'max = 1, single item array' => [1, ['a'], 'a'], // *
+            'max = 1, multiple item array' => [1, ['a', 'b'], 'a'], // *
+            'max = 1, empty array' => [1, [], null], // *
+            'max = 1, null' => [1, null, null],
+            'max = 1, string' => [1, 'a', 'a'],
         ];
     }
 
     /**
      * @test
      *
-     * @dataProvider maxItemsConfigGreaterThanOne
+     * @dataProvider maxItemsProvider
      **/
-    public function it_normalizes_queryable_value($config)
+    public function it_normalizes_queryable_value($maxItems, $value, $expectedValue)
     {
+        $config = [];
+
+        if ($maxItems) {
+            $config[$this->maxItemsConfigKey()] = $maxItems;
+        }
+
         $field = $this->fieldtype($config);
 
-        $this->assertEquals(['123'], $field->toQueryableValue('123'));
-        $this->assertEquals(['123'], $field->toQueryableValue(['123']));
-
-        $this->assertEquals([], $this->fieldtype()->toQueryableValue([]));
-        $this->assertEquals([], $this->fieldtype()->toQueryableValue(null));
+        $this->assertEquals($expectedValue, $field->toQueryableValue($value));
     }
 
-    /** @test */
-    public function it_normalizes_queryable_value_when_max_items_is_one()
+    private function maxItemsConfigKey()
     {
-        $field = $this->fieldtype(['max_items' => 1]);
-
-        $this->assertEquals('123', $field->toQueryableValue('123'));
-        $this->assertEquals('123', $field->toQueryableValue(['123']));
-
-        $this->assertNull($field->toQueryableValue([]));
-        $this->assertNull($field->toQueryableValue(null));
+        return 'max_items';
     }
 }
