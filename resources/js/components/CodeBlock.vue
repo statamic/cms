d71e43ad25781c2c7e09d9ed880b9a491345c13c
Joshua Blum <hallo@joshuablum.de>
2023-05-17 21:25:28 +0200

    [4.x] Fix path for SVG copy icon in the Updater popover (#8161)

--- resources/js/components/CodeBlock.vue
+++ resources/js/components/CodeBlock.vue
@@ -3,7 +3,7 @@
         <div class="flex items-center bg-gray-800 py-3 px-4 rounded-md">
             <pre class="flex-1 p-0 m-0 leading-6"><code class="bg-transparent p-0 text-gray-400 leading-none" v-text="text" /></pre>
             <button v-if="copyable" class="flex" v-tooltip="__('Copy')" @click="copy">
-                <svg-icon name="entries" class="w-4 h-4 text-white" />
+                <svg-icon name="light/entries" class="w-4 h-4 text-white" />
             </button>
         </div>
     </div>
