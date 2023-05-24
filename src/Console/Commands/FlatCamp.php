c6ce1d00865d100c323f33130f2708f688ad684c
Jason Varga <jason@pixelfear.com>
2023-05-24 10:18:47 -0400

    wip

---
+++ src/Console/Commands/FlatCamp.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Statamic\Console\Commands;
+
+use Illuminate\Console\Command;
+use Statamic\Console\RunsInPlease;
+
+class FlatCamp extends Command
+{
+    use RunsInPlease;
+
+    protected $signature = 'flat:camp';
+    protected $description = ' Statamic';
+
+    protected $quotes = [
+        "No, you're right. Let's do it the dumbest way possible. Because it's easier for you. - Erin Dalzell",
+        'Butter is butter. - Convenience store lady',
+    ];
+
+    public function handle()
+    {
+        return $this->comment(collect($this->quotes)
+            ->map(fn ($quote) => $this->formatForConsole($quote))
+            ->random());
+    }
+
+    protected function formatForConsole($quote)
+    {
+        [$text, $author] = str($quote)->explode('-');
+
+        return sprintf(
+            "\n  <options=bold>“ %s ”</>\n  <fg=gray>— %s</>\n",
+            trim($text),
+            trim($author),
+        );
+    }
+}
