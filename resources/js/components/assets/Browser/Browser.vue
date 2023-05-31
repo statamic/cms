4f01cd2ac41cda25435b8a4b09dc22d1fd976f45
Jack Sleight <jacksleight@gmail.com>
2023-05-31 12:52:00 +0100

    Fix asset grid folder dropdown

--- resources/js/components/assets/Browser/Browser.vue
+++ resources/js/components/assets/Browser/Browser.vue
@@ -188,7 +188,7 @@
                                         <div class="asset-meta flex items-center">
                                             <div class="asset-filename text-center w-full px-2 py-1" v-text="folder.basename" :title="folder.basename" />
                                         </div>
-                                        <dropdown-list autoclose v-if="folderActions(folder).length" class="absolute top-1 right-2 opacity-0 group-hover:opacity-100">
+                                        <dropdown-list v-if="folderActions(folder).length" class="absolute top-1 right-2 opacity-0 group-hover:opacity-100">
                                              <data-list-inline-actions
                                                  :item="folder.path"
                                                  :url="folderActionUrl"
