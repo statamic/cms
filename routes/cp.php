b39ad80540e7ea945957d54d311b4fa1a8c72c2a
Duncan McClean <19637309+duncanmcclean@users.noreply.github.com>
2023-06-01 20:08:45 +0100

    [4.x] Fix form actions (#8240)
    Co-authored-by: Jason Varga <jason@pixelfear.com>

--- routes/cp.php
+++ routes/cp.php
@@ -6,7 +6,7 @@ use Statamic\Http\Controllers\CP\AddonEditionsController;
 use Statamic\Http\Controllers\CP\AddonsController;
 use Statamic\Http\Controllers\CP\API\AddonsController as AddonsApiController;
 use Statamic\Http\Controllers\CP\API\TemplatesController;
-use Statamic\Http\Controllers\CP\Assets\ActionController;
+use Statamic\Http\Controllers\CP\Assets\ActionController as AssetActionController;
 use Statamic\Http\Controllers\CP\Assets\AssetContainerBlueprintController;
 use Statamic\Http\Controllers\CP\Assets\AssetContainersController;
 use Statamic\Http\Controllers\CP\Assets\AssetsController;
@@ -47,6 +47,7 @@ use Statamic\Http\Controllers\CP\Fields\MetaController;
 use Statamic\Http\Controllers\CP\Fieldtypes\FilesFieldtypeController;
 use Statamic\Http\Controllers\CP\Fieldtypes\MarkdownFieldtypeController;
 use Statamic\Http\Controllers\CP\Fieldtypes\RelationshipFieldtypeController;
+use Statamic\Http\Controllers\CP\Forms\ActionController as FormActionController;
 use Statamic\Http\Controllers\CP\Forms\FormBlueprintController;
 use Statamic\Http\Controllers\CP\Forms\FormExportController;
 use Statamic\Http\Controllers\CP\Forms\FormsController;
@@ -212,8 +213,8 @@ Route::middleware('statamic.cp.authenticated')->group(function () {
     Route::patch('asset-containers/{asset_container}/folders/{path}', [FoldersController::class, 'update'])->where('path', '.*');
     Route::get('asset-containers/{asset_container}/blueprint', [AssetContainerBlueprintController::class, 'edit'])->name('asset-containers.blueprint.edit');
     Route::patch('asset-containers/{asset_container}/blueprint', [AssetContainerBlueprintController::class, 'update'])->name('asset-containers.blueprint.update');
-    Route::post('assets/actions', [ActionController::class, 'run'])->name('assets.actions.run');
-    Route::post('assets/actions/list', [ActionController::class, 'bulkActions'])->name('assets.actions.bulk');
+    Route::post('assets/actions', [AssetActionController::class, 'run'])->name('assets.actions.run');
+    Route::post('assets/actions/list', [AssetActionController::class, 'bulkActions'])->name('assets.actions.bulk');
     Route::get('assets/browse', [BrowserController::class, 'index'])->name('assets.browse.index');
     Route::get('assets/browse/search/{asset_container}/{path?}', [BrowserController::class, 'search'])->where('path', '.*');
     Route::post('assets/browse/folders/{asset_container}/actions', [FolderActionController::class, 'run'])->name('assets.folders.actions.run');
@@ -250,8 +251,8 @@ Route::middleware('statamic.cp.authenticated')->group(function () {
     Route::get('addons', [AddonsController::class, 'index'])->name('addons.index');
     Route::post('addons/editions', AddonEditionsController::class);
 
-    Route::post('forms/actions', [ActionController::class, 'run'])->name('forms.actions.run');
-    Route::post('forms/actions/list', [ActionController::class, 'bulkActions'])->name('forms.actions.bulk');
+    Route::post('forms/actions', [FormActionController::class, 'run'])->name('forms.actions.run');
+    Route::post('forms/actions/list', [FormActionController::class, 'bulkActions'])->name('forms.actions.bulk');
     Route::post('forms/{form}/submissions/actions', [SubmissionActionController::class, 'run'])->name('forms.submissions.actions.run');
     Route::post('forms/{form}/submissions/actions/list', [SubmissionActionController::class, 'bulkActions'])->name('forms.submissions.actions.bulk');
     Route::resource('forms', FormsController::class);
