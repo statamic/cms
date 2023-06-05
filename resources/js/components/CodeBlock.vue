61d770685faabd963097e114ceddfbc5eb265abc
Christoph Stockinger <christoph@morethings.digital>
2023-06-05 21:39:32 +0200

    feat: added check if navigator permissions are allowed

--- resources/js/components/CodeBlock.vue
+++ resources/js/components/CodeBlock.vue
@@ -1,37 +1,87 @@
 <template>
     <div>
-        <div class="flex items-center bg-gray-800 py-3 px-4 rounded-md">
-            <pre class="flex-1 p-0 m-0 leading-6"><code class="bg-transparent p-0 text-gray-400 leading-none" v-text="text" /></pre>
-            <button v-if="copyable" class="flex" v-tooltip="__('Copy')" @click="copy">
+        <div
+            class="flex items-center bg-gray-800 py-3 px-4 rounded-md"
+            ref="copyhighlight"
+        >
+            <pre
+                class="flex-1 p-0 m-0 leading-6"
+            ><code class="bg-transparent p-0 text-gray-400 leading-none" v-text="text" /></pre>
+            <button
+                v-if="copyable"
+                class="flex"
+                v-tooltip="__('Copy')"
+                @click="copy"
+            >
                 <svg-icon name="light/entries" class="w-4 h-4 text-white" />
             </button>
         </div>
     </div>
-
 </template>
 
 <script>
 export default {
-
     props: {
         text: {
             type: String,
-            required: true
+            required: true,
         },
         copyable: {
             type: Boolean,
-            default: false
-        }
+            default: false,
+        },
     },
-
     methods: {
-
         async copy() {
-            await navigator.clipboard.writeText(this.text);
-            Statamic.$toast.success(__('Copied to clipboard'));
-        }
+            // Workaround for Google Chrome
+            // Chrome allows navigator.clipboard only in secure context - https://web.dev/async-clipboard/
+            const permissionStatus = await navigator.permissions.query({
+                name: "clipboard-write",
+                allowWithoutGesture: false,
+            });
 
-    }
+            switch (permissionStatus.state) {
+                case "granted":
+                    this.nativeCopy();
+                    break;
+                case "denied":
+                case "prompt":
+                    this.workaroundCopy();
+                    break;
+            }
+        },
+        nativeCopy() {
+            navigator.clipboard.writeText(
+                this.text,
+                () => {
+                    Statamic.$toast.success(__("Copied to clipboard"));
+                },
+                () => {
+                    Statamic.$toast.error(__("Copied to clipboard failed"));
+                }
+            );
+        },
+        workaroundCopy() {
+            if (this.$refs.copyhighlight) {
+                const textarea = document.createElement("textarea");
+                textarea.classList.add("sr-only");
+                textarea.value = this.text;
 
-}
+                this.$refs.copyhighlight.appendChild(textarea);
+                textarea.select();
+                const copied = document.execCommand("copy");
+
+                if (copied) {
+                    Statamic.$toast.success(__("Copied to clipboard"));
+                } else {
+                    Statamic.$toast.error(__("Copied to clipboard failed"));
+                }
+
+                textarea.remove();
+            } else {
+                Statamic.$toast.error(__("Copied to clipboard failed"));
+            }
+        },
+    },
+};
 </script>
