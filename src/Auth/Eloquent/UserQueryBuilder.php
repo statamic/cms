996efd6f3ef820198db3a751f6b1663271aa3bdf
Gabriel Meza <gabrielmeza@msml.nl>
2023-07-13 15:00:39 +0200

    4.x-:

--- src/Auth/Eloquent/UserQueryBuilder.php
+++ src/Auth/Eloquent/UserQueryBuilder.php
@@ -2,12 +2,129 @@
 
 namespace Statamic\Auth\Eloquent;
 
+use Illuminate\Support\Facades\DB;
 use Statamic\Auth\UserCollection;
 use Statamic\Facades\User;
 use Statamic\Query\EloquentQueryBuilder;
 
 class UserQueryBuilder extends EloquentQueryBuilder
 {
+    public function whereGroup($value, $operator = '=', $boolean = 'and')
+    {
+        if ($this->hasGroupsRelation()) {
+            $method = $boolean == 'or' ? 'orWhereHas' : 'whereHas';
+            $this->$method('groups', function ($query) use ($value, $operator) {
+                return $query->where('handle', $operator, $value);
+            });
+
+            return $this;
+        }
+
+        $method = $boolean == 'or' ? 'orWhereExists' : 'whereExists';
+        $this->$method(function ($query) use ($operator, $value) {
+            $query->select(DB::raw(1))
+                ->from($this->groupsTable())
+                ->where('group_id', $operator, $value)
+                ->whereColumn($this->groupsTable().'.user_id', 'users.id');
+        });
+
+        return $this;
+    }
+
+    public function orWhereGroup($value, $operator = '=')
+    {
+        $this->whereGroup($value, $operator, 'or');
+
+        return $this;
+    }
+
+    public function whereGroupIn($groups, $boolean = 'and')
+    {
+        if ($this->hasGroupsRelation()) {
+            $method = $boolean == 'or' ? 'orWhereHas' : 'whereHas';
+            $this->$method('groups', function ($query) use ($groups) {
+                return $query->whereIn('handle', $groups);
+            });
+
+            return $this;
+        }
+
+        $method = $boolean == 'or' ? 'orWhereExists' : 'whereExists';
+        $this->$method(function ($query) use ($groups) {
+            $query->select(DB::raw(1))
+                ->from($this->groupsTable())
+                ->whereIn('group_id', $groups)
+                ->whereColumn($this->groupsTable().'.user_id', 'users.id');
+        });
+
+        return $this;
+    }
+
+    public function orWhereGroupIn($groups)
+    {
+        $this->whereGroupIn($groups, 'or');
+
+        return $this;
+    }
+
+    public function whereRole($value, $operator = '=', $boolean = 'and')
+    {
+        if ($this->hasRolesRelation()) {
+            $method = $boolean == 'or' ? 'orWhereHas' : 'whereHas';
+            $this->$method('roles', function ($query) use ($value, $operator) {
+                return $query->where('handle', $operator, $value);
+            });
+
+            return $this;
+        }
+
+        $method = $boolean == 'or' ? 'orWhereExists' : 'whereExists';
+        $this->$method(function ($query) use ($operator, $value) {
+            $query->select(DB::raw(1))
+                ->from($this->rolesTable())
+                ->where('role_id', $operator, $value)
+                ->whereColumn($this->rolesTable().'.user_id', 'users.id');
+        });
+
+        return $this;
+    }
+
+    public function orWhereRole($value, $operator = '=')
+    {
+        $this->whereRole($value, $operator, 'or');
+
+        return $this;
+    }
+
+    public function whereRoleIn($roles, $boolean = 'and')
+    {
+        if ($this->hasRolesRelation()) {
+            $method = $boolean == 'or' ? 'orWhereHas' : 'whereHas';
+            $this->$method('roles', function ($query) use ($roles) {
+                return $query->whereIn('handle', $roles);
+            });
+
+            return $this;
+        }
+
+        $method = $boolean == 'or' ? 'orWhereExists' : 'whereExists';
+        $this->$method(function ($query) use ($roles) {
+            $query->select(DB::raw(1))
+                ->from($this->rolesTable())
+                ->whereIn('role_id', $roles)
+                ->whereColumn($this->rolesTable().'.user_id', 'users.id');
+        });
+
+        return $this;
+    }
+
+    public function orWhereRoleIn($roles)
+    {
+        $this->whereRoleIn($roles, 'or');
+
+        return $this;
+    }
+
     protected function transform($items, $columns = ['*'])
     {
         return UserCollection::make($items)->map(function ($model) {
@@ -15,12 +132,23 @@ class UserQueryBuilder extends EloquentQueryBuilder
         });
     }
 
-    protected function column($column)
+    private function hasGroupsRelation()
     {
-        if ($column === 'id') {
-            return User::make()->model()->getKeyName();
-        }
+        return method_exists($this->builder->getModel(), 'groups');
+    }
 
-        return $column;
+    private function hasRolesRelation()
+    {
+        return method_exists($this->builder->getModel(), 'roles');
+    }
+
+    private function groupsTable()
+    {
+        return config('statamic.users.tables.group_user', 'group_user');
+    }
+
+    private function rolesTable()
+    {
+        return config('statamic.users.tables.role_user', 'role_user');
     }
 }
