3ed011b57f29b9ba88b1f0feb582b79b2b8898fb
Jason Varga <jason@pixelfear.com>
2023-07-13 14:58:08 -0400

    not used

--- resources/js/components/fieldtypes/markdown/MarkdownFieldtype.vue
+++ resources/js/components/fieldtypes/markdown/MarkdownFieldtype.vue
@@ -659,28 +659,7 @@ export default {
             });
 
             this.buttons = buttons;
-        },
-
-        buttonIsActive(button) {
-            if (button.hasOwnProperty('active')) {
-                return button.active(this.editor, button.args);
-            }
-            const nameProperty = button.hasOwnProperty('activeName') ? 'activeName' : 'name';
-            const name = button[nameProperty];
-            return this.editor.isActive(name, button.args);
-        },
-
-        buttonIsVisible(button) {
-            if (button.hasOwnProperty('visible')) {
-                return button.visible(this.editor, button.args);
-            }
-            if (! button.hasOwnProperty('visibleWhenActive')) return true;
-            return this.editor.isActive(button.visibleWhenActive, button.args);
-        },
-
-        visibleButtons(buttons) {
-            return buttons.filter(button => this.buttonIsVisible(button));
-        },
+        }
 
     },
 
