d669486c928f875fbb30ed68c309de8656f7e172
Jason Varga <jason@pixelfear.com>
2023-07-17 16:34:41 -0400

    [4.x] Only show code block copy button on https (#8457)

--- resources/js/components/CodeBlock.vue
+++ resources/js/components/CodeBlock.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="flex items-center bg-gray-800 py-3 px-4 rounded-md">
             <pre class="flex-1 p-0 m-0 leading-6"><code class="bg-transparent p-0 text-gray-400 leading-none" v-text="text" /></pre>
-            <button v-if="copyable" class="flex" v-tooltip="__('Copy')" @click="copy">
+            <button v-if="canCopy" class="flex" v-tooltip="__('Copy')" @click="copy">
                 <svg-icon name="light/entries" class="w-4 h-4 text-white" />
             </button>
         </div>
@@ -21,6 +21,12 @@ export default {
         copyable: {
             type: Boolean,
             default: false
+        }
+    },
+
+    computed: {
+        canCopy() {
+            return this.copyable && window.isSecureContext;
         }
     },
 
