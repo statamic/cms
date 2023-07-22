980d4c4cbaf5a1b608bd5d4fbb1c1d16bd4f22e2
Duncan McClean <duncan@duncanmcclean.com>
2023-07-22 15:49:18 +0100

    Hide 'Addons' and 'Unlisted Addons' sections on updater

--- resources/views/updater/index.blade.php
+++ resources/views/updater/index.blade.php
@@ -42,36 +42,40 @@
             </table>
         </div>
 
-        <h6 class="mt-8">{{ __('Addons') }}</h6>
-        <div class="card p-0 mt-2">
-            <table class="data-table">
-                @foreach ($addons as $addon)
-                <tr>
-                    <td class="w-64"><a href="{{ route('statamic.cp.updater.product', $addon -> slug()) }}"
-                            class="text-blue font-bold mr-2">{{ $addon -> name() }}</a>
-                    <td>{{ $addon -> version() }}</td>
-                    @if ($count = $addon->changelog()->availableUpdatesCount())
-                    <td class="text-right"><span
-                            class="badge-sm bg-green-600 btn-xs">{{ trans_choice('1 update|:count updates', $count) }}</span></td>
-                    @else
-                    <td class="text-right">{{ __('Up to date') }}</td>
-                    @endif
-                </tr>
-                @endforeach
-            </table>
-        </div>
-
-        <h6 class="mt-8">{{ __('Unlisted Addons') }}</h6>
-        <div class="card p-0 mt-2">
-            <table class="data-table">
-                @foreach ($unlistedAddons as $addon)
+        @if($addons->count())
+            <h6 class="mt-8">{{ __('Addons') }}</h6>
+            <div class="card p-0 mt-2">
+                <table class="data-table">
+                    @foreach ($addons as $addon)
                     <tr>
-                        <td class="w-64">{{ $addon->name() }}</td>
-                        <td>{{ $addon->version() }}</td>
+                        <td class="w-64"><a href="{{ route('statamic.cp.updater.product', $addon -> slug()) }}"
+                                class="text-blue font-bold mr-2">{{ $addon -> name() }}</a>
+                        <td>{{ $addon -> version() }}</td>
+                        @if ($count = $addon->changelog()->availableUpdatesCount())
+                        <td class="text-right"><span
+                                class="badge-sm bg-green-600 btn-xs">{{ trans_choice('1 update|:count updates', $count) }}</span></td>
+                        @else
+                        <td class="text-right">{{ __('Up to date') }}</td>
+                        @endif
                     </tr>
-                @endforeach
-            </table>
-        </div>
+                    @endforeach
+                </table>
+            </div>
+        @endif
+
+        @if($unlistedAddons->count())
+            <h6 class="mt-8">{{ __('Unlisted Addons') }}</h6>
+            <div class="card p-0 mt-2">
+                <table class="data-table">
+                    @foreach ($unlistedAddons as $addon)
+                        <tr>
+                            <td class="w-64">{{ $addon->name() }}</td>
+                            <td>{{ $addon->version() }}</td>
+                        </tr>
+                    @endforeach
+                </table>
+            </div>
+        @endif
 
         @include('statamic::partials.docs-callout', [
             'topic' => __('Updates'),
