45883b52d7cdf4c518f1a8d92c0ce302704aed0b
Jason Varga <jason@pixelfear.com>
2023-08-08 10:14:16 -0400

    only the "-ing" events should halt on non-nulls

--- src/Events/GlobalVariablesCreated.php
+++ src/Events/GlobalVariablesCreated.php
@@ -10,14 +10,4 @@ class GlobalVariablesCreated extends Event
     {
         $this->variables = $variables;
     }
-
-    /**
-     * Dispatch the event with the given arguments, and halt on first non-null listener response.
-     *
-     * @return mixed
-     */
-    public static function dispatch()
-    {
-        return event(new static(...func_get_args()), [], true);
-    }
 }
