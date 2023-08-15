5a09ea2c1a27eadf00959ed75e8796ed28c42a4b
Ryan Mitchell <ryan@thoughtcollective.com>
2023-08-15 10:08:50 +0100

    Seems we need to include the kernel now to run please

--- tests/Auth/Eloquent/EloquentUserTest.php
+++ tests/Auth/Eloquent/EloquentUserTest.php
@@ -25,6 +25,8 @@ class EloquentUserTest extends TestCase
 
     public function setUp(): void
     {
+        require_once __DIR__.'/../../Console/Kernel.php';
+
         parent::setUp();
 
         Carbon::setTestNow(Carbon::create(2019, 11, 21, 23, 39, 29));
