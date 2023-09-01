8c8aacbe3213e36b96f05a695bafe55611bd10e5
Ryan Mitchell <ryan@thoughtcollective.com>
2023-09-01 09:55:21 +0100

    Add option to append original filename to Glide URLS

--- src/Imaging/GlideUrlBuilder.php
+++ src/Imaging/GlideUrlBuilder.php
@@ -37,9 +37,19 @@ class GlideUrlBuilder extends ImageUrlBuilder
         switch ($this->itemType()) {
             case 'url':
                 $path = 'http/'.base64_encode($item);
+
+                if (! $filename) {
+                    $filename = $this->optionallySetFilename(Str::afterLast($item, '/'));
+                }
+
                 break;
             case 'asset':
                 $path = 'asset/'.base64_encode($this->item->containerId().'/'.$this->item->path());
+
+                if (! $filename) {
+                    $filename = $this->optionallySetFilename(Str::afterLast($this->item->path(), '/'));
+                }
+
                 break;
             case 'id':
                 $path = 'asset/'.base64_encode(str_replace('::', '/', $this->item));
@@ -63,5 +73,14 @@ class GlideUrlBuilder extends ImageUrlBuilder
         }
 
         return URL::prependSiteRoot($builder->getUrl($path, $params));
+    }
+
+    private function optionallySetFilename($filename)
+    {
+        if (! config('statamic.assets.image_manipulation.append_original_filename', false)) {
+            return false;
+        }
+
+        return $filename;
     }
 }
