1f7c35ba2188f61cb56f2ca26a857d5ad3934e3b
Ryan Mitchell <ryan@thoughtcollective.com>
2023-09-14 08:29:16 +0100

    Translate site names too

--- src/Query/Scopes/Filters/Site.php
+++ src/Query/Scopes/Filters/Site.php
@@ -41,7 +41,7 @@ class Site extends Filter
     {
         $site = Facades\Site::get($values['site']);
 
-        return __('Site').': '.$site->name();
+        return __('Site').': '.__($site->name());
     }
 
     public function visibleTo($key)
@@ -52,7 +52,7 @@ class Site extends Filter
     protected function options()
     {
         return Facades\Site::all()->mapWithKeys(function ($site) {
-            return [$site->handle() => $site->name()];
+            return [$site->handle() => __($site->name())];
         });
     }
 }
