19479419c2697bc52688bf7f8b47588971e490d6
John Koster <john@stillat.com>
2023-09-18 15:47:52 -0500

    Adds custom variable persistence to recursive loops

--- src/View/Antlers/Language/Runtime/NodeProcessor.php
+++ src/View/Antlers/Language/Runtime/NodeProcessor.php
@@ -1364,6 +1364,7 @@ class NodeProcessor
                             $recursiveParent->activeDepth += 1;
 
                             $rootData = RecursiveNodeManager::getRecursiveRootData($node);
+                            $rootData = array_merge($rootData, $this->getRuntimeAssignments());
 
                             // Prevent an infinite loop with arbitrary data.
                             if (array_key_exists($node->content, $rootData)) {
@@ -1371,6 +1372,16 @@ class NodeProcessor
                             }
 
                             $parentParameterValues = array_values($recursiveParent->getParameterValues($this));
+
+                            GlobalRuntimeState::$traceTagAssignments = true;
+                            GlobalRuntimeState::$activeTracerCount += 1;
+                            GlobalRuntimeState::$tracedRuntimeAssignments = $this->runtimeAssignments;
+
+                            $currentAssignments = $this->getRuntimeAssignments();
+
+                            $recursiveProcessor = $this->cloneProcessor();
+                            $recursiveProcessor->setRuntimeAssignments($this->runtimeAssignments);
+
                             // Substitute the current node with the original parent.
                             foreach ($children as $childData) {
                                 $namedDepthMapping = $node->content.'_depth';
@@ -1405,8 +1416,17 @@ class NodeProcessor
                                     $childDataToUse[$node->content] = [];
                                 }
 
-                                $result = $this->cloneProcessor()
-                                    ->setData($childDataToUse)->reduce($recursiveParent->children);
+                                $result = $recursiveProcessor->replaceData($childDataToUse)->reduce($recursiveParent->children);
+
+                                $recursiveAssignmentValues = $recursiveProcessor->getRuntimeAssignments();
+
+                                if (! empty($recursiveAssignmentValues)) {
+                                    foreach ($recursiveAssignmentValues as $assignVar => $assignVal) {
+                                        if (array_key_exists($assignVar, $currentAssignments)) {
+                                            GlobalRuntimeState::$tracedRuntimeAssignments[$assignVar] = $assignVal;
+                                        }
+                                    }
+                                }
 
                                 $buffer .= $this->measureBufferAppend($node, $result);
 
@@ -1417,6 +1437,11 @@ class NodeProcessor
 
                             $recursiveParent->activeDepth -= 1;
                             RecursiveNodeManager::releaseRecursiveNode($node);
+
+                            if (GlobalRuntimeState::$activeTracerCount == 0) {
+                                GlobalRuntimeState::$traceTagAssignments = false;
+                                GlobalRuntimeState::$tracedRuntimeAssignments = [];
+                            }
                         }
 
                         continue;
