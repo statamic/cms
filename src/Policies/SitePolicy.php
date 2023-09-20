4025faf3e21661ead1bad92b58ca430d9989efff
Jason Varga <jason@pixelfear.com>
2023-09-20 17:28:02 -0400

    checking permission of a particular site shouldn't care about your selected site

--- src/Policies/SitePolicy.php
+++ src/Policies/SitePolicy.php
@@ -2,24 +2,10 @@
 
 namespace Statamic\Policies;
 
-use Statamic\Facades\Site;
 use Statamic\Facades\User;
 
 class SitePolicy
 {
-    public function before($user, $ability)
-    {
-        if (! Site::hasMultiple()) {
-            return null;
-        }
-
-        $site = Site::selected();
-
-        if (! User::fromUser($user)->hasPermission("access {$site->handle()} site")) {
-            return false;
-        }
-    }
-
     public function index($user)
     {
         // handled by before()
