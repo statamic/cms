6b588074e98aa8d447ba570b1f9bbdeceafc3f32
Jason Varga <jason@pixelfear.com>
2023-09-20 17:08:13 -0400

    add authorization and tests on the select site route

--- src/Http/Controllers/CP/SelectSiteController.php
+++ src/Http/Controllers/CP/SelectSiteController.php
@@ -8,6 +8,12 @@ class SelectSiteController extends CpController
 {
     public function select($handle)
     {
+        if (! $site = Site::get($handle)) {
+            return back()->withError('Invalid site.');
+        }
+
+        $this->authorize('view', $site);
+
         Site::setSelected($handle);
 
         return back()->with('success', __('Site selected.'));
