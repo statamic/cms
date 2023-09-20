880cbeffb38244db9bfd6093bcb48550dc9044cc
Jason Varga <jason@pixelfear.com>
2023-09-20 17:28:33 -0400

    unused

--- src/Policies/SitePolicy.php
+++ src/Policies/SitePolicy.php
@@ -6,11 +6,6 @@ use Statamic\Facades\User;
 
 class SitePolicy
 {
-    public function index($user)
-    {
-        // handled by before()
-    }
-
     public function view($user, $site)
     {
         return User::fromUser($user)->hasPermission("access {$site->handle()} site");
