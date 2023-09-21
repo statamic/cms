df4eaa73eaada7f23b3c7b6e3b7027227db466f9
Jason Varga <jason@pixelfear.com>
2023-09-21 15:33:37 -0400

    avoid single site setups needing permission

--- src/Policies/SitePolicy.php
+++ src/Policies/SitePolicy.php
@@ -2,12 +2,17 @@
 
 namespace Statamic\Policies;
 
+use Statamic\Facades\Site;
 use Statamic\Facades\User;
 
 class SitePolicy
 {
     public function view($user, $site)
     {
+        if (! Site::hasMultiple()) {
+            return true;
+        }
+
         return User::fromUser($user)->hasPermission("access {$site->handle()} site");
     }
 }
