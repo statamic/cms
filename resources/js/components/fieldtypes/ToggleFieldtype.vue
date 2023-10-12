2316439032258a7999c9448a83f2cd3bbeede4de
Ryan Mitchell <ryan@thoughtcollective.com>
2023-10-12 20:55:15 +0100

    [4.x] Allow user defined fields in the CP to be translatable (#8664)
    Co-authored-by: Jason Varga <jason@pixelfear.com>

--- resources/js/components/fieldtypes/ToggleFieldtype.vue
+++ resources/js/components/fieldtypes/ToggleFieldtype.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="toggle-fieldtype-wrapper">
         <toggle-input :value="value" @input="update" :read-only="isReadOnly" :id="fieldId" />
-        <label v-if="inlineLabel" class="inline-label" v-html="$options.filters.markdown(inlineLabel)"></label>
+        <label v-if="inlineLabel" class="inline-label" v-html="$options.filters.markdown(__(inlineLabel))"></label>
     </div>
 </template>
 
@@ -16,7 +16,7 @@ export default {
         },
 
         replicatorPreview() {
-            return (this.value ? '✓' : '✗') + ' ' + this.config.display;
+            return (this.value ? '✓' : '✗') + ' ' + __(this.config.display);
         }
 
     }
