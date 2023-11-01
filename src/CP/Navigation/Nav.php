c26ee82da75643d7d6d14dd69d37780744217adb
Jesse Leite <jesseleite@gmail.com>
2023-11-01 12:38:17 -0400

    [4.x] Fix CP nav item active status regressions (#8880)

--- src/CP/Navigation/Nav.php
+++ src/CP/Navigation/Nav.php
@@ -144,6 +144,14 @@ class Nav
     }
 
     /**
+     * Clear cached urls.
+     */
+    public function clearCachedUrls()
+    {
+        return NavBuilder::clearCachedUrls();
+    }
+
+    /**
      * Make base items.
      *
      * @return $this
