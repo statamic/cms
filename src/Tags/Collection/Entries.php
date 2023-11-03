8617969d3d5cc514fff9bcd27c40372a53b42f59
Duncan McClean <duncan@duncanmcclean.com>
2023-11-03 12:49:23 +0000

    Get rid of empty lines between properties

--- src/Tags/Collection/Entries.php
+++ src/Tags/Collection/Entries.php
@@ -28,19 +28,12 @@ class Entries
     }
 
     protected $ignoredParams = ['as'];
-
     protected $params;
-
     protected $collections;
-
     protected $site;
-
     protected $showPublished;
-
     protected $showUnpublished;
-
     protected $since;
-
     protected $until;
 
     public function __construct($params)
