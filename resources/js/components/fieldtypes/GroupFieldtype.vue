6c7623390206898366f4616c31c4d3e91fa3f50e
Daniel Weaver <godismyjudge95@gmail.com>
2023-11-10 00:03:21 -0500

    Fixing meta error with nested Replicators and Grids

--- resources/js/components/fieldtypes/GroupFieldtype.vue
+++ resources/js/components/fieldtypes/GroupFieldtype.vue
@@ -1,57 +1,86 @@
 <template>
-    <portal name="group-fullscreen" :disabled="!fullScreenMode" :provide="provide">
-
+    <portal
+        name="group-fullscreen"
+        :disabled="!fullScreenMode"
+        :provide="provide"
+    >
         <element-container @resized="containerWidth = $event.width">
-            <div class="group-fieldtype-container" :class="{ 'grid-fullscreen bg-white': fullScreenMode }">
-
-                <header class="relative flex items-center justify-between py-3 pl-3 bg-gray-200 border-b"
-                    v-if="fullScreenMode">
+            <div
+                class="group-fieldtype-container"
+                :class="{ 'grid-fullscreen bg-white': fullScreenMode }"
+            >
+                <header
+                    v-if="fullScreenMode"
+                    class="relative flex items-center justify-between py-3 pl-3 bg-gray-200 border-b"
+                >
                     <h2 v-text="config.display" />
-                    <button class="absolute btn-close top-2 right-5" @click="fullScreenMode = false"
-                        :aria-label="__('Exit Fullscreen Mode')">&times;</button>
+                    <button
+                        @click="fullScreenMode = false"
+                        class="absolute btn-close top-2 right-5"
+                        :aria-label="__('Exit Fullscreen Mode')"
+                    >&times;</button>
                 </header>
-
                 <section :class="{ 'p-4': fullScreenMode }">
-                    <div class="flex justify-end absolute top-3 right-3 @md:right-6" v-if="!fullScreenMode">
-                        <button v-if="config.fullscreen" @click="toggleFullScreen" class="flex items-center btn btn-icon"
-                            v-tooltip="__('Toggle Fullscreen Mode')">
-                            <svg-icon name="expand-bold" class="h-3.5 px-0.5 text-gray-750" v-show="!fullScreenMode" />
-                            <svg-icon name="shrink-all" class="h-3.5 px-0.5 text-gray-750" v-show="fullScreenMode" />
+                    <div
+                        v-if="!fullScreenMode"
+                        class="flex justify-end absolute top-3 right-3 @md:right-6"
+                    >
+                        <button
+                            v-if="config.fullscreen"
+                            @click="toggleFullScreen"
+                            v-tooltip="__('Toggle Fullscreen Mode')"
+                            class="flex items-center btn btn-icon"
+                        >
+                            <svg-icon
+                                v-show="!fullScreenMode"
+                                name="expand-bold"
+                                class="h-3.5 px-0.5 text-gray-750"
+                            />
+                            <svg-icon
+                                v-show="fullScreenMode"
+                                name="shrink-all"
+                                class="h-3.5 px-0.5 text-gray-750"
+                            />
                         </button>
                     </div>
-
                     <div class="mb-4 border rounded shadow-sm replicator-set">
                         <div class="replicator-set-body publish-fields @container">
-                            <set-field v-for="field in fields " v-show="showField(field, fieldPath(field.handle))"
-                                :key="field.handle" :field="field" :meta="meta.existing[field.handle]"
-                                :value="value[field.handle]" :parent-name="name" :set-index="index"
-                                :errors="errors(field.handle)" :field-path="fieldPath(field.handle)" class="p-4"
-                                :read-only="isReadOnly" @updated="updated(field.handle, $event)"
-                                @meta-updated="metaupdated(field.handle, $event)" @focus="$emit('focus')"
-                                @blur="$emit('blur')" />
+                            <set-field
+                                v-for="field in fields"
+                                :key="field.handle"
+                                v-show="showField(field, fieldPath(field.handle))"
+                                :field="field"
+                                :meta="meta"
+                                :value="value[field.handle]"
+                                :parent-name="name"
+                                :set-index="index"
+                                :errors="errors(field.handle)"
+                                :field-path="fieldPath(field.handle)"
+                                :read-only="isReadOnly"
+                                @updated="updated(field.handle, $event)"
+                                @meta-updated="$emit('meta-updated', $event)"
+                                @focus="$emit('focus')"
+                                @blur="$emit('blur')"
+                                class="p-4"
+                            />
                         </div>
                     </div>
                 </section>
-
             </div>
         </element-container>
-
     </portal>
 </template>
-
 <script>
-import SetField from './replicator/Field.vue';
-import { ValidatesFieldConditions } from '../field-conditions/FieldConditions.js';
+import FieldType from '@/../../vendor/statamic/cms/resources/js/components/fieldtypes/Fieldtype.vue';
+import SetField from '@/../../vendor/statamic/cms/resources/js/components/fieldtypes/replicator/Field.vue';
+import { ValidatesFieldConditions } from '@/../../vendor/statamic/cms/resources/js/components/field-conditions/FieldConditions.js';
 
 export default {
-
     mixins: [
         FieldType,
-        ValidatesFieldConditions
+        ValidatesFieldConditions,
     ],
-
     components: { SetField },
-
     data() {
         return {
             containerWidth: null,
@@ -63,28 +92,19 @@ export default {
             },
         };
     },
-
     inject: ['storeName'],
-
+    onMounted() {
+        console.log({ meta: this.meta });
+    },
     computed: {
-
         fields() {
             return this.config.fields;
-        }
-
-    },
-
-    watch: {
-
-
-    },
-
-    methods: {
-
-        focus() {
-            // TODO
         },
-
+        replicatorPreview() {
+            return Object.values(this.value).join(', ');
+        }
+    },
+    methods: {
         blurred() {
             setTimeout(() => {
                 if (!this.$el.contains(document.activeElement)) {
@@ -103,7 +123,7 @@ export default {
                 config: { get: () => this.config },
                 isReadOnly: { get: () => this.isReadOnly },
                 handle: { get: () => this.handle },
-                fieldPathPrefix: { get: () => this.fieldPathPrefix },
+                fieldPathPrefix: { get: () => this.fieldPathPrefix || this.handle },
                 fullScreenMode: { get: () => this.fullScreenMode },
                 toggleFullScreen: { get: () => this.toggleFullScreen },
             });
@@ -129,12 +149,6 @@ export default {
             });
         },
 
-        metaUpdated(handle, value) {
-            let meta = clone(this.meta.existing);
-            meta[handle] = value;
-            this.$emit('meta-updated', meta);
-        },
-
         fieldPath(handle) {
             return `${this.fieldPathPrefix}.${handle}`;
         },
@@ -144,8 +158,6 @@ export default {
             if (!state) return [];
             return state.errors[this.fieldPath(handle)] || [];
         },
-
-    }
-
+    },
 };
 </script>
