43b392061db57772be2508073302eb02fe572d8c
Jason Varga <jason@pixelfear.com>
2023-11-10 16:30:10 -0500

    add tests for context filtering

--- tests/StaticCaching/NocacheTagsTest.php
+++ tests/StaticCaching/NocacheTagsTest.php
@@ -2,7 +2,10 @@
 
 namespace Tests\StaticCaching;
 
+use Mockery;
+use Statamic\Facades\Parse;
 use Statamic\StaticCaching\NoCache\Session;
+use Statamic\StaticCaching\NoCache\StringRegion;
 use Tests\FakesContent;
 use Tests\FakesViews;
 use Tests\PreventSavingStacheItemsToDisk;
@@ -98,5 +101,56 @@ EOT;
             ->get('/about')
             ->assertOk()
             ->assertSeeInOrder(['Updated', 'Updated', 'Existing', 'Updated']);
+    }
+
+    /** @test */
+    public function it_only_adds_appropriate_fields_of_context_to_session()
+    {
+        // We will not add `baz` to the session because it is not used in the template.
+        // We will not add `nope` to the session because it is not in the context.
+        $expectedFields = ['foo', 'bar'];
+        $template = '{{ nocache }}{{ foo }}{{ bar }}{{ nope }}{{ /nocache }}';
+        $context = [
+            'foo' => 'alfa',
+            'bar' => 'bravo',
+            'baz' => 'charlie',
+        ];
+
+        $region = Mockery::mock(StringRegion::class)->shouldReceive('placeholder')->andReturn('the placeholder')->getMock();
+
+        $this->mock(Session::class, fn ($mock) => $mock
+            ->shouldReceive('pushRegion')
+            ->withArgs(fn ($arg1, $arg2, $arg3) => array_keys($arg2) === $expectedFields)
+            ->once()->andReturn($region));
+
+        $this->assertEquals('the placeholder', $this->tag($template, $context));
+    }
+
+    /** @test */
+    public function it_only_adds_explicitly_defined_fields_of_context_to_session()
+    {
+        // We will not add `bar` to the session because it is not explicitly defined.
+        // We will not add `nope` to the session because it is not in the context.
+        $expectedFields = ['foo', 'baz'];
+        $template = '{{ nocache select="foo|baz|nope" }}{{ foo }}{{ bar }}{{ nope }}{{ /nocache }}';
+        $context = [
+            'foo' => 'alfa',
+            'bar' => 'bravo',
+            'baz' => 'charlie',
+        ];
+
+        $region = Mockery::mock(StringRegion::class)->shouldReceive('placeholder')->andReturn('the placeholder')->getMock();
+
+        $this->mock(Session::class, fn ($mock) => $mock
+            ->shouldReceive('pushRegion')
+            ->withArgs(fn ($arg1, $arg2, $arg3) => array_keys($arg2) === $expectedFields)
+            ->once()->andReturn($region));
+
+        $this->assertEquals('the placeholder', $this->tag($template, $context));
+    }
+
+    private function tag($tag, $data = [])
+    {
+        return (string) Parse::template($tag, $data);
     }
 }
