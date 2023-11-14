54060835545a19242eb5570808471353fbfc94d4
Ryan Mitchell <ryan@thoughtcollective.com>
2023-11-14 19:48:05 +0000

    More php file validation

--- src/Http/Requests/FrontendFormRequest.php
+++ src/Http/Requests/FrontendFormRequest.php
@@ -119,7 +119,7 @@ class FrontendFormRequest extends FormRequest
             })
             ->mapWithKeys(function ($field) {
                 return [$field->handle().'.*' => ['file', function ($attribute, $value, $fail) {
-                    if (in_array(trim(strtolower($value->getClientOriginalExtension())), ['php', 'php3', 'php4', 'php5', 'phtml'])) {
+                    if (in_array(trim(strtolower($value->getClientOriginalExtension())), ['php', 'php3', 'php4', 'php5', 'php7', 'php8', 'phtml'])) {
                         $fail(__('validation.uploaded'));
                     }
                 }]];
