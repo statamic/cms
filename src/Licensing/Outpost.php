ea8148e9f966e967bc3b76da32f01d1c874f1cc3
duncanmcclean <duncanmcclean@users.noreply.github.com>
2023-11-15 15:30:51 +0000

    Fix styling

--- src/Licensing/Outpost.php
+++ src/Licensing/Outpost.php
@@ -50,12 +50,13 @@ class Outpost
 
         try {
             $lock->block(5);
+
             return $this->performAndCacheRequest();
         } catch (ConnectException $e) {
             return $this->cacheAndReturnErrorResponse($e);
         } catch (RequestException $e) {
             return $this->handleRequestException($e);
-        } catch(LockTimeoutException $e) {
+        } catch (LockTimeoutException $e) {
             return $this->cacheAndReturnErrorResponse($e);
         } finally {
             $lock->release();
