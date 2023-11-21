14a14354d89a858f97a973b92f9a91990f13f275
Jason Varga <jason@pixelfear.com>
2023-11-21 12:13:35 -0500

    use global like addons would need to

--- resources/js/components/fieldtypes/assets/AssetsFieldtype.vue
+++ resources/js/components/fieldtypes/assets/AssetsFieldtype.vue
@@ -162,7 +162,6 @@ import Selector from '../../assets/Selector.vue';
 import Uploader from '../../assets/Uploader.vue';
 import Uploads from '../../assets/Uploads.vue';
 import { SortableList } from '../../sortable/Sortable';
-import PreviewHtml from '../replicator/PreviewHtml';
 
 export default {
 
@@ -329,7 +328,7 @@ export default {
         },
 
         replicatorPreview() {
-            return new PreviewHtml(_.map(this.assets, (asset) => {
+            return replicatorPreviewHtml(_.map(this.assets, (asset) => {
                 return (asset.isImage || asset.isSvg) ?
                     `<img src="${asset.thumbnail}" width="20" height="20" title="${asset.basename}" />`
                     : asset.basename;
