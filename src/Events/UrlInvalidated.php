2bb12cfea3ed4d8adde2db40eee57c67f13e5b01
Jason Varga <jason@pixelfear.com>
2023-11-22 11:18:36 -0500

    when no domain is passed in, use the base domain

--- src/Events/UrlInvalidated.php
+++ src/Events/UrlInvalidated.php
@@ -2,12 +2,18 @@
 
 namespace Statamic\Events;
 
+use Statamic\StaticCaching\Cacher;
+
 class UrlInvalidated extends Event
 {
     public $url;
 
     public function __construct($url, $domain = null)
     {
+        if (! $domain) {
+            $domain = app(Cacher::class)->getBaseUrl();
+        }
+
         $this->url = $domain.$url;
     }
 }
