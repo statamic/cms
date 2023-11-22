e264e91ed58ea44a110c8552d49a293047412293
Jason Varga <jason@pixelfear.com>
2023-11-22 10:59:59 -0500

    concat the url and just expose that

--- src/Events/UrlInvalidated.php
+++ src/Events/UrlInvalidated.php
@@ -4,14 +4,10 @@ namespace Statamic\Events;
 
 class UrlInvalidated extends Event
 {
-    public $domain;
-    public $fullUrl;
     public $url;
 
     public function __construct($url, $domain = null)
     {
-        $this->domain = $domain;
-        $this->fullUrl = url($url);
-        $this->url = $url;
+        $this->url = $domain.$url;
     }
 }
