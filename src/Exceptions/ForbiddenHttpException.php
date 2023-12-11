f26d933b3909dcdf30e09df423b2e1bcf7d381dd
Jason Varga <jason@pixelfear.com>
2023-12-11 16:13:20 -0500

    pint

--- src/Exceptions/ForbiddenHttpException.php
+++ src/Exceptions/ForbiddenHttpException.php
@@ -9,7 +9,7 @@ class ForbiddenHttpException extends HttpException
 {
     use RendersHttpExceptions;
 
-    public function __construct(string $message = '', \Throwable $previous = null, int $code = 0, array $headers = [])
+    public function __construct(string $message = '', ?\Throwable $previous = null, int $code = 0, array $headers = [])
     {
         parent::__construct(403, $message, $previous, $headers, $code);
     }
