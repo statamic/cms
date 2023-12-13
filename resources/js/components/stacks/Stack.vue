272d3938138008bd88dd02f1c3f89205b0c37806
Jack Sleight <jacksleight@gmail.com>
2023-12-13 15:46:30 +0000

    Fix stack hover offset on close

--- resources/js/components/stacks/Stack.vue
+++ resources/js/components/stacks/Stack.vue
@@ -118,16 +118,24 @@ export default {
     methods: {
 
         clickedHitArea() {
+            if (!this.visible) {
+                return;
+            }
             this.$events.$emit(`stacks.hit-area-clicked`, this.depth - 1);
         },
 
         mouseEnterHitArea() {
+            if (!this.visible) {
+                return;
+            }
             this.$events.$emit(`stacks.${this.depth - 1}.hit-area-mouseenter`);
         },
 
         mouseOutHitArea() {
+            if (!this.visible) {
+                return;
+            }
             this.$events.$emit(`stacks.${this.depth - 1}.hit-area-mouseout`);
-
         },
 
         runCloseCallback() {
