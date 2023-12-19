fc11080cbff576875b5f759ff0c85464b782ae00
Jack Sleight <jacksleight@gmail.com>
2023-12-19 10:52:55 +0000

    Remove duplicate callback call

--- resources/js/components/publish/HasActions.js
+++ resources/js/components/publish/HasActions.js
@@ -24,10 +24,6 @@ export default {
 
             this.$events.$emit('reset-action-modals');
 
-            if (response.callback) {
-                Statamic.$callbacks.call(response.callback[0], ...response.callback.slice(1));
-            }
-
             if (response.message !== false) {
                 this.$toast.success(response.message || __("Action completed"));
             }
