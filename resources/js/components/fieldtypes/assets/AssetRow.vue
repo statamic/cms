1b4f57710fd79524f41f46e0f6df26c283e8915a
Jason Varga <jason@pixelfear.com>
2024-01-08 12:25:55 -0500

    used x's like on the list fieldtype

--- resources/js/components/fieldtypes/assets/AssetRow.vue
+++ resources/js/components/fieldtypes/assets/AssetRow.vue
@@ -41,17 +41,11 @@
         </td>
         <td class="p-0 w-8 text-right align-middle" v-if="!readOnly">
             <button
-                class="flex items-center p-1 w-6 h-8 text-gray-600 hover:text-gray-900"
+                class="flex items-center p-1 w-6 h-8 text-lg antialiased text-gray-600 hover:text-gray-900"
                 @click="remove"
                 :aria-label="__('Remove Asset')"
             >
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    class="w-6 h-6"
-                >
-                    <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M10.082 9.5A4.47 4.47 0 0 0 6.75 8h-1.5a4.5 4.5 0 0 0 0 9h1.5a4.474 4.474 0 0 0 3.332-1.5m3.836-6A4.469 4.469 0 0 1 17.25 8h1.5a4.5 4.5 0 1 1 0 9h-1.5a4.472 4.472 0 0 1-3.332-1.5M6.75 12.499h10.5"></path>
-                </svg>
+                ×
             </button>
 
             <asset-editor
