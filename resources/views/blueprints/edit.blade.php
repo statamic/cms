557a6b332c72454f0e05c2247e1bb298a71084ca
Jason Varga <jason@pixelfear.com>
2024-01-11 13:28:37 -0500

    fix false not being treated as a boolean

--- resources/views/blueprints/edit.blade.php
+++ resources/views/blueprints/edit.blade.php
@@ -12,7 +12,7 @@
         show-title
         action="{{ cp_route('blueprints.update', [$blueprint->namespace(), $blueprint->handle()]) }}"
         :initial-blueprint="{{ json_encode($blueprintVueObject) }}"
-        show-hidden="false"
+        :show-hidden="false"
     ></blueprint-builder>
 
     @include('statamic::partials.docs-callout', [
