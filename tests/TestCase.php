df3caec80b34724facdff2adbaacff6319cd5650
Ryan Mitchell <ryan@thoughtcollective.com>
2024-01-11 14:59:04 +0000

    :beer:

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -74,7 +74,7 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
         ];
 
         foreach ($configs as $config) {
-            $app['config']->set("statamic.$config", require(__DIR__."/../config/{$config}.php"));
+            $app['config']->set("statamic.$config", require (__DIR__."/../config/{$config}.php"));
         }
 
         $app['config']->set('statamic.forms.submissions', __DIR__.'/__fixtures__/content/submissions');
