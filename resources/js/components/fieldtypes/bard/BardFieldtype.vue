508c783b4c89d2e89d2eacbfec7d7c73b9ba7624
Duncan McClean <duncan@duncanmcclean.com>
2024-01-16 20:36:55 +0000

    Stop blank line being added before new sets

--- resources/js/components/fieldtypes/bard/BardFieldtype.vue
+++ resources/js/components/fieldtypes/bard/BardFieldtype.vue
@@ -426,7 +426,6 @@ export default {
     methods: {
         addSet(handle) {
             const id = uniqid();
-            const pos = this.editor.view.state.selection.$head.pos;
             const values = Object.assign({}, { type: handle }, this.meta.defaults[handle]);
 
             let previews = {};
@@ -435,9 +434,16 @@ export default {
 
             this.updateSetMeta(id, this.meta.new[handle]);
 
+            const { $head } = this.editor.view.state.selection;
+            const { nodeBefore } = $head;
+
             // Perform this in nextTick because the meta data won't be ready until then.
             this.$nextTick(() => {
-                this.editor.commands.setAt({ attrs: { id, values }, pos });
+                if (nodeBefore) {
+                    this.editor.commands.setAt({ attrs: { id, values }, pos: $head.pos });
+                } else {
+                    this.editor.commands.set({ id, values });
+                }
             });
         },
 
