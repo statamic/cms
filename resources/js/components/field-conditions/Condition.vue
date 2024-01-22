253134af8a42c5e129a0376b8c61f0abd44d1a98
Duncan McClean <duncan@duncanmcclean.com>
2024-01-22 11:02:54 +0000

    Refactor field conditions into their own component

---
+++ resources/js/components/field-conditions/Condition.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="flex flex-wrap items-center py-4 border-t">
+        <div v-if="index === 0" class="help-block" v-text="__('messages.field_conditions_field_instructions')" />
+
+        <v-select
+            ref="fieldSelect"
+            :value="condition.field"
+            class="w-full md:w-1/3 mb-2 md:mb-0"
+            :options="fieldOptions"
+            :placeholder="__('Field')"
+            :taggable="true"
+            :push-tags="true"
+            :reduce="field => field.value"
+            :create-option="field => ({value: field, label: field })"
+            @input="fieldSelected"
+            @search:blur="fieldSelectBlur"
+        >
+            <template #no-options><div class="hidden" /></template>
+        </v-select>
+
+        <select-input
+            :value="condition.operator"
+            :options="operatorOptions"
+            :placeholder="false"
+            class="md:ml-4"
+            @input="operatorSelected" />
+
+        <text-input
+            :value="condition.value"
+            class="ml-4"
+            @input="valueUpdated" />
+
+        <button @click="remove" class="btn-close ml-2 group">
+            <svg-icon name="micro/trash" class="w-4 h-4 group-hover:text-red-500" />
+        </button>
+    </div>
+</template>
+
+<script>
+import HasInputOptions from '../fieldtypes/HasInputOptions.js';
+
+export default {
+    mixins: [HasInputOptions],
+
+    props: {
+        condition: {
+            type: Object,
+        },
+        index: {
+            type: Number,
+        },
+        fieldOptions: {
+            type: Array,
+        },
+    },
+
+    computed: {
+        operatorOptions() {
+            return this.normalizeInputOptions({
+                'equals': __('equals'),
+                'not': __('not'),
+                'contains': __('contains'),
+                'contains_any': __('contains any'),
+                '===': '===',
+                '!==': '!==',
+                '>': '>',
+                '>=': '>=',
+                '<': '<',
+                '<=': '<=',
+                'custom': __('custom'),
+            });
+        },
+    },
+
+    methods: {
+        fieldSelected(field) {
+            this.$parent.conditions[this.index].field = field;
+        },
+
+        fieldSelectBlur() {
+            const value = this.$refs.fieldSelect.$refs.search.value;
+            if (value) this.fieldUpdated(value);
+        },
+
+        operatorSelected(operator) {
+            this.$parent.conditions[this.index].operator = operator;
+        },
+
+        valueUpdated(value) {
+            this.$parent.conditions[this.index].value = value;
+        },
+
+        remove() {
+            this.$emit('removed');
+        }
+    }
+}
+</script>
