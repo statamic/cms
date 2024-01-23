ec9e5c51dc5237846385c48a5e7bb519d3c506cf
Jason Varga <jason@pixelfear.com>
2024-01-23 16:33:47 -0500

    [4.x] Only use site language for form submission validation messages if submitted from front-end (#9383)

--- src/Http/Requests/FrontendFormRequest.php
+++ src/Http/Requests/FrontendFormRequest.php
@@ -116,8 +116,11 @@ class FrontendFormRequest extends FormRequest
 
     public function validateResolved()
     {
-        $site = Site::findByUrl(URL::previous()) ?? Site::default();
+        // If this was submitted from a front-end form, we want to use the appropriate language
+        // for the translation messages. If there's no previous url, it was likely submitted
+        // directly in a headless format. In that case, we'll just use the default lang.
+        $site = ($previousUrl = session()->previousUrl()) ? Site::findByUrl($previousUrl) : null;
 
-        return $this->withLocale($site->lang(), fn () => parent::validateResolved());
+        return $this->withLocale($site?->lang(), fn () => parent::validateResolved());
     }
 }
