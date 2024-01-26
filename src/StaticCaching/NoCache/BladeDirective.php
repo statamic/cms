ba4cdd9fd98d16abfcbf94b86553514c1e147cee
Duncan McClean <duncan@duncanmcclean.com>
2024-01-26 15:14:33 +0000

    When request is missing middleware, fallback to nocache view

--- src/StaticCaching/NoCache/BladeDirective.php
+++ src/StaticCaching/NoCache/BladeDirective.php
@@ -2,6 +2,8 @@
 
 namespace Statamic\StaticCaching\NoCache;
 
+use Statamic\StaticCaching\Middleware\Cache;
+
 class BladeDirective
 {
     /**
@@ -22,8 +24,11 @@ class BladeDirective
         }
 
         $view = $expression;
-
         $context = array_merge($data, $params);
+
+        if (! Cache::$requestCanBeCached) {
+            return view($view, $context)->render();
+        }
 
         return $this->nocache->pushView($view, $context)->placeholder();
     }
