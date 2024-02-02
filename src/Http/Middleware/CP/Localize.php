c61932767e87bc0a52fb64947151a92dc6b66fd4
Peiman Nourani <nourani.peiman@gmail.com>
2024-02-02 09:41:02 +0000

    [4.x] switch locale on site switch

--- src/Http/Middleware/CP/Localize.php
+++ src/Http/Middleware/CP/Localize.php
@@ -4,6 +4,7 @@ namespace Statamic\Http\Middleware\CP;
 
 use Closure;
 use Statamic\Facades\Preference;
+use Statamic\Facades\Site;
 
 class Localize
 {
@@ -11,8 +12,16 @@ class Localize
     {
         $locale = Preference::get('locale') ?? app()->getLocale();
 
+        if (! Preference::get('locale')) {
+            $locale = Site::selected()->locale();
+        }
+
         // Make locale config with dashes backwards compatible, as they should be underscores.
         $locale = str_replace('-', '_', $locale);
+
+        if (str_contains($locale, '_')) {
+            $locale = strstr($locale, '_', true);
+        }
 
         app()->setLocale($locale);
 
