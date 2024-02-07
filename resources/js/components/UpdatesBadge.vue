f39e58bf3d0a20433d099544ed21f6d6eec971e2
Jason Varga <jason@pixelfear.com>
2024-02-07 15:42:27 -0500

    use vuex store so state is used across the two instances

--- resources/js/components/UpdatesBadge.vue
+++ resources/js/components/UpdatesBadge.vue
@@ -6,26 +6,49 @@
 
 <script>
     export default {
-        data() {
-            return {
-                count: 0,
-            };
-        },
 
-        mounted() {
-            this.getCount();
+        computed: {
+            count() {
+                return this.$store.state.updates.count;
+            }
         },
 
         created() {
-            this.$events.$on('recount-updates', this.getCount);
+            this.registerVuexModule();
+
+            this.getCount();
         },
 
         methods: {
+            registerVuexModule() {
+                if (this.$store.state.updates) return;
+
+                this.$store.registerModule('updates', {
+                    namespaced: true,
+                    state: {
+                        count: 0,
+                        requested: false,
+                    },
+                    mutations: {
+                        count(state, count) {
+                            state.count = count;
+                        },
+                        requested(state) {
+                            state.requested = true;
+                        },
+                    }
+                })
+            },
+
             getCount() {
-                this.$axios.get(cp_url('updater/count')).then(response => {
-                    this.count = !isNaN(response.data) ? response.data : 0;
-                });
-            }
+                if (this.$store.state.updates.requested) return;
+
+                this.$axios
+                    .get(cp_url('updater/count'))
+                    .then(response => this.$store.commit('updates/count', !isNaN(response.data) ? response.data : 0));
+
+                this.$store.commit('updates/requested');
+            },
         }
     }
 </script>
