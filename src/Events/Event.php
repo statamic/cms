9bd7c0738554686065a07998765524b35c31a635
Duncan McClean <duncan@duncanmcclean.com>
2024-02-08 18:01:34 +0000

    Remove type from `Event` class to prevent `authenticatedUser` error

--- src/Events/Event.php
+++ src/Events/Event.php
@@ -3,14 +3,13 @@
 namespace Statamic\Events;
 
 use Illuminate\Foundation\Events\Dispatchable;
-use Statamic\Contracts\Auth\User as UserContract;
 use Statamic\Facades\User;
 
 abstract class Event
 {
     use Dispatchable;
 
-    public ?UserContract $authenticatedUser;
+    public $authenticatedUser;
 
     public static function dispatch()
     {
