820350c79276f8c0cd138896473db17dafeb1a61
Jason Varga <jason@pixelfear.com>
2024-02-14 15:55:40 -0500

    wip

--- repos/rebing-graphql-laravel/src/Support/Mutation.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Rebing\GraphQL\Support;
-
-abstract class Mutation extends Field
-{
-}
--- repos/rebing-graphql-laravel/src/Support/Query.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Rebing\GraphQL\Support;
-
-abstract class Query extends Field
-{
-}
