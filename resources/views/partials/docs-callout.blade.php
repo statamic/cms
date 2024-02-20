f7a247b8cd98cc2ecd86e3349d2aa24dd8041d85
Duncan McClean <duncan@duncanmcclean.com>
2024-02-20 17:58:21 +0000

    [4.x] Fix arrays being returned by translations (#9525)

--- resources/views/partials/docs-callout.blade.php
+++ resources/views/partials/docs-callout.blade.php
@@ -1,3 +1,5 @@
+@php use function Statamic\trans as __; @endphp
+
 @if (config('statamic.cp.link_to_docs'))
     <div class="flex justify-center text-center mt-16">
         <div class="bg-white rounded-full px-6 py-2 shadow-sm text-sm text-gray-700">{{ $text ?? __('Learn more about') }} <a href="{{ $url }}" target="_blank" rel="noopener noopener" class="text-blue hover:text-blue-700">{{ $topic }}</a></div>
