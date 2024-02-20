5a4a625a53d29c1a837c8f4304cc01cfd9ba19ef
Jason Varga <jason@pixelfear.com>
2024-02-20 15:18:58 -0500

    rename

--- resources/js/components/SessionExpiry.vue
+++ resources/js/components/SessionExpiry.vue
@@ -5,7 +5,7 @@
         <button v-if="isWarning" class="session-expiry-stripe" @click="extend" v-text="warningText" />
 
         <modal name="session-timeout-login" v-if="isShowingLogin" height="auto" width="500px" :adaptive="true">
-            <div class="-max-h-screen-1">
+            <div class="-max-h-screen-px">
             <div class="flex items-center p-6 bg-gray-200 border-b text-center">
                 {{ __('Resume Your Session') }}
             </div>
