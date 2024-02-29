416bf9fb315465f298d4bd09b7bf36908968156a
Duncan McClean <duncan@duncanmcclean.com>
2024-02-29 13:23:05 +0000

    Skip relative modifier tests on leap years

--- tests/Modifiers/RelativeTest.php
+++ tests/Modifiers/RelativeTest.php
@@ -8,6 +8,16 @@ use Tests\TestCase;
 
 class RelativeTest extends TestCase
 {
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->markTestSkipped(
+            Carbon::now()->format('d-m') === '29-02',
+            'These tests fail on leap years.'
+        );
+    }
+
     /** @test */
     public function it_converts_a_date_to_relative()
     {
