03eab994411279e198c667a8a425310cf014bfa4
Ryan Mitchell <ryan@thoughtcollective.com>
2024-02-29 14:35:26 +0000

    Global variables

--- tests/Stache/Repositories/GlobalVariablesRepositoryTest.php
+++ tests/Stache/Repositories/GlobalVariablesRepositoryTest.php
@@ -3,7 +3,9 @@
 namespace Tests\Stache\Repositories;
 
 use Statamic\Contracts\Globals\Variables;
+use Statamic\Exceptions\GlobalVariablesNotFoundException;
 use Statamic\Facades\GlobalSet;
+use Statamic\Facades\GlobalVariables;
 use Statamic\Facades\Site;
 use Statamic\Globals\VariablesCollection;
 use Statamic\Stache\Repositories\GlobalRepository;
@@ -309,5 +311,27 @@ YAML;
         $this->assertNotNull($this->globalRepo->find('new'));
         $this->assertFileDoesNotExist($this->directory.'/en/new.yaml');
         @unlink($this->directory.'/new.yaml');
+    }
+
+    /** @test */
+    public function test_find_or_fail_gets_global()
+    {
+        $this->setUpSingleSite();
+
+        $var = $this->repo->findOrFail('global::en');
+
+        $this->assertInstanceOf(Variables::class, $var);
+        $this->assertEquals('General', $var->title());
+    }
+
+    /** @test */
+    public function test_find_or_fail_throws_exception_when_global_does_not_exist()
+    {
+        $this->setUpSingleSite();
+
+        $this->expectException(GlobalVariablesNotFoundException::class);
+        $this->expectExceptionMessage('Global Variables [does-not-exist] not found');
+
+        $this->repo->findOrFail('does-not-exist');
     }
 }
