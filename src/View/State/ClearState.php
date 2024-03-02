2529a062db02f3df1e2ff78aa4c864e8abca686c
John Koster <john@stillat.com>
2024-03-02 16:00:34 -0600

    Cache the `isApiRoute` results for the request

--- src/View/State/ClearState.php
+++ src/View/State/ClearState.php
@@ -2,10 +2,13 @@
 
 namespace Statamic\View\State;
 
+use Statamic\Statamic;
+
 class ClearState
 {
     public function handle()
     {
+        Statamic::clearApiRouteCache();
         StateManager::resetState();
     }
 }
