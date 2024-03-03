167f35326358afc05baf8e8cd7fc8a9ba65fb503
John Koster <john@stillat.com>
2024-03-03 09:04:18 -0600

    Cache external url/uri results

--- src/View/State/ClearState.php
+++ src/View/State/ClearState.php
@@ -2,10 +2,13 @@
 
 namespace Statamic\View\State;
 
+use Statamic\Facades\URL;
+
 class ClearState
 {
     public function handle()
     {
         StateManager::resetState();
+        URL::clearExternalUrlCache();
     }
 }
