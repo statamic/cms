fa9b9d5a36e62424f2ce58d74d8f5e89efe5e290
Jason Varga <jason@pixelfear.com>
2024-03-05 10:05:32 -0500

    Only prevent them from localizing into new sites. They should still be allowed to switch to others.

--- resources/js/components/entries/PublishForm.vue
+++ resources/js/components/entries/PublishForm.vue
@@ -166,10 +166,11 @@
                                     <div
                                         v-for="option in localizations"
                                         :key="option.handle"
-                                        class="text-sm flex items-center -mx-4 px-4 py-2 cursor-pointer"
-                                        :class="option.active ? 'bg-blue-100' : 'hover:bg-gray-200'"
-                                        :disabled="!canSave"
-                                        v-tooltip="!canSave ? __(`You don't have permission to localize this entry.`) : null"
+                                        class="text-sm flex items-center -mx-4 px-4 py-2"
+                                        :class="[
+                                            option.active ? 'bg-blue-100' : 'hover:bg-gray-200',
+                                            !canSave && !option.exists ? 'cursor-not-allowed' : 'cursor-pointer',
+                                        ]"
                                         @click="localizationSelected(option)"
                                     >
                                         <div class="flex-1 flex items-center" :class="{ 'line-through': !option.exists }">
@@ -628,7 +629,11 @@ export default {
         },
 
         localizationSelected(localization) {
-            if (!this.canSave) return;
+            if (!this.canSave) {
+                if (localization.exists) this.editLocalization(localization);
+                return;
+            }
+
             if (localization.active) return;
 
             if (this.isDirty) {
