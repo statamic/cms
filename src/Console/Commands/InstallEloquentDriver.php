b6955560f606a6f5347eb13015378eea76f3adca
Duncan McClean <duncan@duncanmcclean.com>
2024-03-07 14:58:55 +0000

    Remove checks around nav migrations
    Once statamic/eloquent-driver#253 has been merged, it won't be possible for the same migration to be published multiple times so this will no longer be an issue.

--- src/Console/Commands/InstallEloquentDriver.php
+++ src/Console/Commands/InstallEloquentDriver.php
@@ -172,10 +172,7 @@ class InstallEloquentDriver extends Command
         info('Migrating collections...');
 
         $this->runArtisanCommand('vendor:publish --tag=statamic-eloquent-collection-migrations');
-
-        if (! Schema::hasTable(config('statamic.eloquent-driver.table_prefix', '').'trees')) {
-            $this->runArtisanCommand('vendor:publish --tag=statamic-eloquent-navigation-migrations');
-        }
+        $this->runArtisanCommand('vendor:publish --tag=statamic-eloquent-navigation-migrations');
 
         $this->runArtisanCommand('migrate');
 
@@ -267,12 +264,7 @@ class InstallEloquentDriver extends Command
     {
         info('Migrating navs...');
 
-        if (
-            ! Schema::hasTable(config('statamic.eloquent-driver.table_prefix', '').'navigations')
-            && ! Schema::hasTable(config('statamic.eloquent-driver.table_prefix', '').'trees')
-        ) {
-            $this->runArtisanCommand('vendor:publish --tag=statamic-eloquent-navigation-migrations');
-        }
+        $this->runArtisanCommand('vendor:publish --tag=statamic-eloquent-navigation-migrations');
 
         $this->runArtisanCommand('migrate');
 
