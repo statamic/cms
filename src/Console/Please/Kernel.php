194bae2aad8ebe6c624195bfabc7ae2f0c4bdcd2
Jason Varga <jason@pixelfear.com>
2024-03-13 17:17:20 -0400

    wip

--- src/Console/Please/Kernel.php
+++ src/Console/Please/Kernel.php
@@ -2,7 +2,7 @@
 
 namespace Statamic\Console\Please;
 
-use App\Console\Kernel as ConsoleKernel;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use Statamic\Console\Please\Application as Please;
 use Statamic\Statamic;
 
