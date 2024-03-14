91835e32f53977fa369f4123120398625c53af21
Jesse Leite <jesseleite@gmail.com>
2024-03-14 17:39:46 -0400

    Encourage `php please multisite` right from the config.

--- config/sites.php
+++ config/sites.php
@@ -7,9 +7,10 @@ return [
     | Enable Multi-site
     |--------------------------------------------------------------------------
     |
-    | Whether Statamic's multi-site functionality should be enabled.
-    | This assumes you have Statamic Pro enabled. After enabling,
-    | you can manage your Statamic sites in the control panel.
+    | Whether Statamic's multi-site functionality should be enabled. It is
+    | assumed Statamic Pro is also enabled. To get started, you can run
+    | the `php please multisite` command to update your content file
+    | structure, after which you can manage your sites in the CP.
     |
     | Read more: https://statamic.dev/multi-site
     |
