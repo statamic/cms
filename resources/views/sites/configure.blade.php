2658699905bcc2fb79edbe8a6164169737a660e5
Jesse Leite <jesseleite@gmail.com>
2024-03-14 17:50:44 -0400

    Docs callout.

--- resources/views/sites/configure.blade.php
+++ resources/views/sites/configure.blade.php
@@ -10,6 +10,12 @@
         :initial-values="{{ json_encode($values) }}"
         :meta="{{ json_encode($meta) }}"
         url="{{ cp_route('sites.update') }}"
+        class="-mb-8"
     ></sites-edit-form>
+
+    @include('statamic::partials.docs-callout', [
+        'topic' => __('Multi-Site'),
+        'url' => Statamic::docsUrl('multi-site')
+    ])
 
 @stop
