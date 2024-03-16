351f6876be25cfa52ceb8e22823920662ec8b1d2
Ostap Brehin <osbre@protonmail.com>
2024-03-16 04:20:30 +0000

    Remove undocumented methods from `Markdown` facade DocBlocks

--- src/Facades/Markdown.php
+++ src/Facades/Markdown.php
@@ -11,8 +11,6 @@ use Statamic\Markdown\Manager;
  * @method static bool hasParser(string $name)
  * @method static void extend(string $name, \Closure $closure)
  * @method static string parse(string $markdown)
- * @method static \League\CommonMark\CommonMarkConverter converter()
- * @method static void environment()
  * @method static \Statamic\Markdown\Parser addExtension(\Closure $closure)
  * @method static \Statamic\Markdown\Parser addExtensions(\Closure $closure)
  * @method static array extensions()
@@ -23,12 +21,7 @@ use Statamic\Markdown\Manager;
  * @method static \Statamic\Markdown\Parser withSmartPunctuation()
  * @method static \Statamic\Markdown\Parser withTableOfContents()
  * @method static \Statamic\Markdown\Parser withHeadingPermalinks()
- * @method static void config(void $key = null)
  * @method static void newInstance(array $config = [])
- * @method static void macro(string $name, object|callable $macro)
- * @method static void mixin(object $mixin, bool $replace = true)
- * @method static bool hasMacro(string $name)
- * @method static void flushMacros()
  *
  * @see \Statamic\Markdown\Manager
  */
