5f70c33692e478310f9ee434dca765ec6599b6a5
Jason Varga <jason@pixelfear.com>
2024-03-18 16:18:14 -0400

    more

--- resources/views/collections/empty.blade.php
+++ resources/views/collections/empty.blade.php
@@ -40,7 +40,7 @@
                 @if ($multipleBlueprints)
                     @foreach ($collection->entryBlueprints() as $blueprint)
                         <a href="{{ cp_route('collections.entries.create', [$collection->handle(), $site, 'blueprint' => $blueprint->handle()]) }}"
-                        class="text-blue text-sm mr-2">{{ $blueprint->title() }} &rarr;</a>
+                           class="text-blue text-sm mr-2">{{ $blueprint->title() }} &rarr;</a>
                     @endforeach
                 @endif
             </div>
