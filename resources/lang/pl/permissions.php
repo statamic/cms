4532629571ec8ef1a668f8a33cf6204e3da0ee84
Seweryn Czabanowski <seweryn_cz@outlook.com>
2024-03-21 17:19:57 +0100

    Final translation fixes

--- resources/lang/pl/permissions.php
+++ resources/lang/pl/permissions.php
@@ -3,7 +3,6 @@
 return [
     'super' => 'Superużytkownik',
     'super_desc' => 'Super administratorzy posiadają pełną kontrolę i dostęp do wszystkich aspektów panelu sterowania. Przydzielaj tę rolę z rozwagą.',
-
     'group_cp' => 'Panel kontrolny',
     'access_cp' => 'Dostęp do panelu sterowania',
     'access_cp_desc' => 'Umożliwia dostęp do panelu sterowania, lecz sam dostęp nie gwarantuje możliwości wykonywania operacji',
@@ -11,7 +10,10 @@ return [
     'configure_fields_desc' => 'Pozwala na edytowanie schematów, zestawów pól oraz indywidualnych pól',
     'configure_addons' => 'Konfiguracja rozszerzeń',
     'configure_addons_desc' => 'Umożliwia dostęp do obszaru dodatków w celu ich instalacji i deinstalacji',
-
+    'manage_preferences' => 'Zarządzanie preferencjami',
+    'manage_preferences_desc' => 'Możliwość dostosowywania globalnych i rolowych preferencji.',
+    'group_sites' => 'Strony',
+    'access_{site}_site' => 'Dostęp do strony :site',
     'group_collections' => 'Kolekcje',
     'configure_collections' => 'Konfiguracja kolekcji',
     'configure_collections_desc' => 'Udziela dostępu do wszystkich uprawnień powiązanych z kolekcjami',
@@ -26,7 +28,6 @@ return [
     'edit_other_authors_{collection}_entries' => 'Edycja rekordów innych autorów',
     'publish_other_authors_{collection}_entries' => 'Zarządzanie stanem publikacji rekordów innych autorów',
     'delete_other_authors_{collection}_entries' => 'Usuwanie rekordów innych autorów',
-
     'group_taxonomies' => 'Taksonomie',
     'configure_taxonomies' => 'Konfiguracja taksonomii',
     'configure_taxonomies_desc' => 'Udziela dostępu do wszystkich uprawnień związanych z taksonomiami',
@@ -36,18 +37,15 @@ return [
     'delete_{taxonomy}_terms' => 'Usuwanie pojęć',
     'publish_{taxonomy}_terms' => 'Zarządzanie stanem publikowania',
     'reorder_{taxonomy}_terms' => 'Zmiana kolejność pojęć',
-
     'group_navigation' => 'Nawigacja',
     'configure_navs' => 'Konfiguracja nawigacji',
     'configure_navs_desc' => 'Udziela dostępu do wszystkich uprawnień dotyczących nawigacji',
     'view_{nav}_nav' => 'Wyświetlanie nawigacji :nav',
     'edit_{nav}_nav' => 'Edycja nawigacji',
-
     'group_globals' => 'Konfiguracja globalna',
     'configure_globals' => 'Konfiguracja globalna',
     'configure_globals_desc' => 'Udziela dostępu do wszystkich globalnych uprawnień konfiguracyjnych',
     'edit_{global}_globals' => 'Edycja konfiguracji globalnej :global',
-
     'group_assets' => 'Zasoby',
     'configure_asset_containers' => 'Konfiguracja kontenerów zasobów',
     'configure_asset_containers_desc' => 'Przyznaje dostęp do wszystkich uprawnień związanych z zasobami',
@@ -57,13 +55,13 @@ return [
     'move_{container}_assets' => 'Przenoszenie zasobów',
     'rename_{container}_assets' => 'Zmiana nazwy zasobów',
     'delete_{container}_assets' => 'Usuwanie zasobów',
-
     'group_forms' => 'Formularze',
     'configure_forms' => 'Konfiguracja formularzy',
     'configure_forms_desc' => 'Przyznaje dostęp do wszystkich uprawnień związanych z formularzami',
+    'configure_form_fields' => 'Konfiguracja pól formularza',
+    'configure_form_fields_desc' => 'Możliwość edycji planów formularzy, zestawów pól i ich pól.',
     'view_{form}_form_submissions' => 'Wyświetlanie przesłanych formularzy :form',
     'delete_{form}_form_submissions' => 'Usuwanie przesłanych formularzy :form',
-
     'group_users' => 'Użytkownicy',
     'view_users' => 'Wyświetlanie użytkowników',
     'edit_users' => 'Edycja użytkowników',
@@ -72,15 +70,17 @@ return [
     'change_passwords' => 'Zmiana haseł',
     'edit_user_groups' => 'Edycja grup',
     'edit_roles' => 'Edycja ról',
-
+    'assign_user_groups' => 'Przypisywanie grup użytkownikom',
+    'assign_roles' => 'Przypisywanie ról',
+    'impersonate_users' => 'Podszywanie się za użytkowników',
     'group_updates' => 'Aktualizacje',
     'view_updates' => 'Wyświetlanie aktualizacji',
-
     'group_utilities' => 'Narzędzia',
     'access_utility' => ':title',
     'access_utility_desc' => 'Udziela dostępu do narzędzia :title',
-
     'group_misc' => 'Różne',
     'resolve_duplicate_ids' => 'Napraw zduplikowane ID',
     'resolve_duplicate_ids_desc' => 'Daje możliwość przeglądania i naprawiania zduplikowanych ID.',
+    'view_graphql' => 'Przeglądanie GraphQL',
+    'view_graphql_desc' => 'Udostępnia dostęp do przeglądarki GraphQL',
 ];
