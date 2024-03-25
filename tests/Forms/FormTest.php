7a9e1ecb3ccbc50c875a1cbb93a7ff5daaa99069
Duncan McClean <duncan@duncanmcclean.com>
2024-03-25 18:02:32 +0000

    wip

--- tests/Forms/FormTest.php
+++ tests/Forms/FormTest.php
@@ -26,7 +26,7 @@ class FormTest extends TestCase
     /** @test */
     public function it_saves_a_form()
     {
-        $this->assertTrue(false);
+        $this->assertFalse(true);
 
         Event::fake();
 
