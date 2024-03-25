4b797084500066570b74bfaa633a0df5bf68a9f0
Jason Varga <jason@pixelfear.com>
2024-03-25 11:35:39 -0400

    nitpick

--- src/Rules/Handle.php
+++ src/Rules/Handle.php
@@ -7,9 +7,6 @@ use Illuminate\Contracts\Validation\ValidationRule;
 
 class Handle implements ValidationRule
 {
-    /**
-     * Run the validation rule.
-     */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
         if (! preg_match('/^[a-z][a-z0-9]*(?:_{0,1}[a-z0-9])*$/', $value)) {
--- src/Rules/Slug.php
+++ src/Rules/Slug.php
@@ -7,9 +7,6 @@ use Illuminate\Contracts\Validation\ValidationRule;
 
 class Slug implements ValidationRule
 {
-    /**
-     * Run the validation rule.
-     */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
         if (! preg_match('/^[a-zA-Z0-9]+(?:-{0,1}[a-zA-Z0-9])*$/', $value)) {
