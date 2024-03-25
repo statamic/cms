134912f4e51c85903d5ef9afe9acb7b24aba7ed3
Duncan McClean <duncan@duncanmcclean.com>
2024-03-25 10:42:46 +0000

    dont need this

--- src/StaticCaching/Cachers/ApplicationCacher.php
+++ src/StaticCaching/Cachers/ApplicationCacher.php
@@ -110,7 +110,6 @@ class ApplicationCacher extends AbstractCacher
         $this->getDomains()->each(function ($domain) {
             $this->getUrls($domain)->keys()->each(function ($key) {
                 $this->cache->forget($this->normalizeKey('responses:'.$key));
-                $this->cache->forget($this->normalizeKey('headers:'.$key));
             });
         });
 
@@ -131,7 +130,6 @@ class ApplicationCacher extends AbstractCacher
             ->filter(fn ($value) => $value === $url || str_starts_with($value, $url.'?'))
             ->each(function ($value, $key) {
                 $this->cache->forget($this->normalizeKey('responses:'.$key));
-                $this->cache->forget($this->normalizeKey('headers:'.$key));
                 $this->forgetUrl($key);
             });
 
