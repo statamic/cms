786866138bb2afc5ade125abf0124c584deda662
Jesse Leite <jesseleite@gmail.com>
2024-03-25 15:36:49 -0400

    Remove `unique_entry_value` string based implementation.

--- src/Providers/ValidationServiceProvider.php
+++ src/Providers/ValidationServiceProvider.php
@@ -5,7 +5,6 @@ namespace Statamic\Providers;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 use Statamic\Validation\CodeFieldtypeRulers;
-use Statamic\Validation\UniqueEntryValue;
 use Statamic\Validation\UniqueFormHandle;
 use Statamic\Validation\UniqueTermValue;
 use Statamic\Validation\UniqueUserValue;
@@ -13,7 +12,6 @@ use Statamic\Validation\UniqueUserValue;
 class ValidationServiceProvider extends ServiceProvider
 {
     protected $rules = [
-        'unique_entry_value' => UniqueEntryValue::class,
         'unique_term_value' => UniqueTermValue::class,
         'unique_user_value' => UniqueUserValue::class,
         'unique_form_handle' => UniqueFormHandle::class,
