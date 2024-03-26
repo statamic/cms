797760c8b82f53c32c02c2062ea2514e30ea68cd
Jesse Leite <jesseleite@gmail.com>
2024-03-26 17:28:01 -0400

    Remove `unique_term_value` string based implementation.

--- src/Providers/ValidationServiceProvider.php
+++ src/Providers/ValidationServiceProvider.php
@@ -5,13 +5,11 @@ namespace Statamic\Providers;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 use Statamic\Validation\UniqueFormHandle;
-use Statamic\Validation\UniqueTermValue;
 use Statamic\Validation\UniqueUserValue;
 
 class ValidationServiceProvider extends ServiceProvider
 {
     protected $rules = [
-        'unique_term_value' => UniqueTermValue::class,
         'unique_user_value' => UniqueUserValue::class,
         'unique_form_handle' => UniqueFormHandle::class,
     ];
