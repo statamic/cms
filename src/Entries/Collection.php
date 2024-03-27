b1760b00649b827a860a8ada2c5b7c7a169f4d46
Duncan McClean <duncan@duncanmcclean.com>
2024-03-27 19:54:10 +0000

    [4.x] Hide "Create Entry" button when all collection blueprints are hidden (#9744)
    Co-authored-by: Jason Varga <jason@pixelfear.com>

--- src/Entries/Collection.php
+++ src/Entries/Collection.php
@@ -268,6 +268,11 @@ class Collection implements Arrayable, ArrayAccess, AugmentableContract, Contrac
         return Facades\Entry::query()->where('collection', $this->handle());
     }
 
+    public function hasVisibleEntryBlueprint()
+    {
+        return $this->entryBlueprints()->reject->hidden()->isNotEmpty();
+    }
+
     public function entryBlueprints()
     {
         $blink = 'collection-entry-blueprints-'.$this->handle();
@@ -316,7 +321,7 @@ class Collection implements Arrayable, ArrayAccess, AugmentableContract, Contrac
 
         return Blink::once($blink, function () use ($blueprint) {
             if (is_null($blueprint)) {
-                return $this->entryBlueprints()->reject->hidden()->first();
+                return $this->entryBlueprints()->reject->hidden()->first() ?? $this->entryBlueprints()->first();
             }
 
             return $this->entryBlueprints()->keyBy->handle()->get($blueprint)
