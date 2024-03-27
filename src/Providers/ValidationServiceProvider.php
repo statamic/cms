0ff4f4d28ee33dbde433661a2e4034c800318655
Jesse Leite <jesseleite@gmail.com>
2024-03-27 14:37:03 -0400

    Remove `unique_form_handle` string based implementation.

--- src/Providers/ValidationServiceProvider.php
+++ src/Providers/ValidationServiceProvider.php
@@ -4,12 +4,11 @@ namespace Statamic\Providers;
 
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
-use Statamic\Validation\UniqueFormHandle;
 
 class ValidationServiceProvider extends ServiceProvider
 {
     protected $rules = [
-        'unique_form_handle' => UniqueFormHandle::class,
+        //
     ];
 
     public function boot()
