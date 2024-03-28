770dc485dd2b330b40816c68727f080e45f18bfc
Duncan McClean <duncan@duncanmcclean.com>
2024-03-28 17:59:23 +0000

    [4.x] Use the configured Git binary in commands (#9793)
    Co-authored-by: duncanmcclean <duncanmcclean@users.noreply.github.com>

--- config/git.php
+++ config/git.php
@@ -132,8 +132,8 @@ return [
     */
 
     'commands' => [
-        'git add {{ paths }}',
-        'git -c "user.name={{ name }}" -c "user.email={{ email }}" commit -m "{{ message }}"',
+        config('statamic.git.binary').' add {{ paths }}',
+        config('statamic.git.binary').' -c "user.name={{ name }}" -c "user.email={{ email }}" commit -m "{{ message }}"',
     ],
 
     /*
