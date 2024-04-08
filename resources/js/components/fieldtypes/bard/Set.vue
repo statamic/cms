4e034e4dccf643939d391ce390f2459a86a0c6ea
Duncan McClean <duncan@duncanmcclean.com>
2024-04-08 11:02:32 +0100

    Use a `<span>` instead
    I was still having issues with a div for whatever reason 🤷‍♂️

--- resources/js/components/fieldtypes/bard/Set.vue
+++ resources/js/components/fieldtypes/bard/Set.vue
@@ -10,10 +10,10 @@
                 <div class="item-move sortable-handle" data-drag-handle />
                 <div class="flex items-center flex-1 p-2 replicator-set-header-inner cursor-pointer" :class="{'flex items-center': collapsed}" @click="toggleCollapsedState">
                     <label class="text-xs whitespace-nowrap rtl:ml-2 ltr:mr-2">
-                        <div v-if="setGroup" class="inline-block">
+                        <span v-if="setGroup">
                             {{ setGroup.display }}
                             <svg-icon name="micro/chevron-right" class="w-4" />
-                        </div>
+                        </span>
                         {{ display || config.handle }}
                     </label>
                     <div class="flex items-center" v-if="config.instructions && !collapsed">
