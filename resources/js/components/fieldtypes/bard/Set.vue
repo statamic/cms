21ecddde19286b44bdf5e696e374fc391f075129
Duncan McClean <duncan@duncanmcclean.com>
2024-04-08 11:30:59 +0100

    [4.x] Fix overlapping set group & set name in Safari (#9837)
    * Use `<div>` instead of a `<template>`
    
    * Use a `<span>` instead
    
    I was still having issues with a div for whatever reason 🤷‍♂️

--- resources/js/components/fieldtypes/bard/Set.vue
+++ resources/js/components/fieldtypes/bard/Set.vue
@@ -10,10 +10,10 @@
                 <div class="item-move sortable-handle" data-drag-handle />
                 <div class="flex items-center flex-1 p-2 replicator-set-header-inner cursor-pointer" :class="{'flex items-center': collapsed}" @click="toggleCollapsedState">
                     <label class="text-xs whitespace-nowrap rtl:ml-2 ltr:mr-2">
-                        <template v-if="setGroup">
+                        <span v-if="setGroup">
                             {{ setGroup.display }}
                             <svg-icon name="micro/chevron-right" class="w-4" />
-                        </template>
+                        </span>
                         {{ display || config.handle }}
                     </label>
                     <div class="flex items-center" v-if="config.instructions && !collapsed">
--- resources/js/components/fieldtypes/replicator/Set.vue
+++ resources/js/components/fieldtypes/replicator/Set.vue
@@ -8,10 +8,10 @@
                 <div class="item-move sortable-handle" :class="sortableHandleClass" v-if="!isReadOnly"></div>
                 <div class="flex items-center flex-1 p-2 replicator-set-header-inner cursor-pointer" :class="{'flex items-center': collapsed}" @click="toggleCollapsedState">
                     <label class="text-xs whitespace-nowrap rtl:ml-2 ltr:mr-2 cursor-pointer">
-                        <template v-if="setGroup">
+                        <span v-if="setGroup">
                             {{ setGroup.display }}
                             <svg-icon name="micro/chevron-right" class="w-4" />
-                        </template>
+                        </span>
                         {{ display || config.handle }}
                     </label>
                     <div class="flex items-center" v-if="config.instructions && !collapsed">
