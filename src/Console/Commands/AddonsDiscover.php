1cc34738a1ceef9d2e7c29fe02511c070b64424f
duncanmcclean <duncanmcclean@users.noreply.github.com>
2024-04-12 15:45:26 +0000

    Fix styling

--- src/Console/Commands/AddonsDiscover.php
+++ src/Console/Commands/AddonsDiscover.php
@@ -34,7 +34,7 @@ class AddonsDiscover extends Command
         $this->newLine();
         $manifest->build();
 
-        $this->components->info("Discovering addons.");
+        $this->components->info('Discovering addons.');
 
         foreach (array_keys($manifest->manifest) as $package) {
             $this->components->task("Discovered Addon: <info>{$package}</info>");
