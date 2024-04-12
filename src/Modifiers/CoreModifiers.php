7737d841317cd8c77533cb73f61b6419adbd3e0a
Jason Varga <jason@pixelfear.com>
2024-04-12 11:45:59 -0400

    add to_json modifier tests and bring back handling for query builders

--- src/Modifiers/CoreModifiers.php
+++ src/Modifiers/CoreModifiers.php
@@ -2540,13 +2540,10 @@ class CoreModifiers extends Modifier
     public function toJson($value, $params)
     {
         $options = Arr::get($params, 0) === 'pretty' ? JSON_PRETTY_PRINT : 0;
-        //        if (Compare::isQueryBuilder($value)) {
-        //            $value = $value->get();
-        //        }
-        //
-        //        if ($value instanceof Collection || $value instanceof Augmentable) {
-        //            $value = $value->toAugmentedArray();
-        //        }
+
+        if (Compare::isQueryBuilder($value)) {
+            $value = $value->get();
+        }
 
         return json_encode($value, $options);
     }
