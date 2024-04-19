ae0f46d838e8b6cb701375633739058f4d7edbcf
Duncan McClean <duncan@duncanmcclean.com>
2024-04-19 14:14:31 +0100

    Allow passing options to the Taggable fieldtype

--- src/Fieldtypes/Taggable.php
+++ src/Fieldtypes/Taggable.php
@@ -20,6 +20,28 @@ class Taggable extends Fieldtype
                 'type' => 'text',
                 'default' => __('statamic::fieldtypes.taggable.config.placeholder'),
             ],
+            'options' => [
+                'display' => __('Options'),
+                'instructions' => __('statamic::fieldtypes.select.config.options'),
+                'type' => 'array',
+                'key_header' => __('Key'),
+                'value_header' => __('Label').' ('.__('Optional').')',
+                'add_button' => __('Add Option'),
+                'validate' => [function ($attribute, $value, $fail) {
+                    $optionsWithoutKeys = collect($value)->keys()->filter(fn ($key) => empty($key) || $key === 'null');
+
+                    if ($optionsWithoutKeys->isNotEmpty()) {
+                        $fail(__('statamic::validation.options_require_keys'));
+                    }
+                }],
+            ],
+        ];
+    }
+
+    public function preload()
+    {
+        return [
+            'options' => $this->config('options', []),
         ];
     }
 
