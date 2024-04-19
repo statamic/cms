723bc2070bb9d66fcf932616a5eb8b7191412ddb
Jason Varga <jason@pixelfear.com>
2024-04-19 18:02:27 -0400

    blink the entry when a single one should be returned

--- src/Fieldtypes/Entries.php
+++ src/Fieldtypes/Entries.php
@@ -7,6 +7,7 @@ use Statamic\Contracts\Data\Localization;
 use Statamic\Contracts\Entries\Entry as EntryContract;
 use Statamic\CP\Column;
 use Statamic\Exceptions\CollectionNotFoundException;
+use Statamic\Facades\Blink;
 use Statamic\Facades\Collection;
 use Statamic\Facades\Entry;
 use Statamic\Facades\GraphQL;
@@ -340,9 +341,19 @@ class Entries extends Relationship
 
     public function augment($values)
     {
+        $single = $this->config('max_items') === 1;
+
+        if ($single && Blink::has($key = 'entries-augment-'.$values)) {
+            return Blink::get($key);
+        }
+
         $query = $this->queryBuilder($values);
 
-        return $this->config('max_items') === 1 ? $query->first() : $query;
+        if ($single) {
+            return tap($query->first(), fn ($entry) => Blink::put($key, $entry));
+        } else {
+            return $query;
+        }
     }
 
     public function shallowAugment($values)
