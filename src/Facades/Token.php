10a6907c4f52a645170068e8454f3bb793c6a6fd
Jason Varga <jason@pixelfear.com>
2024-04-26 13:21:16 -0400

    Fill out facade docblock

--- src/Facades/Token.php
+++ src/Facades/Token.php
@@ -5,6 +5,13 @@ namespace Statamic\Facades;
 use Illuminate\Support\Facades\Facade;
 use Statamic\Tokens\TokenRepository;
 
+/**
+ * @method static \Statamic\Contracts\Tokens\Token make(?string $token, string $handler, array $data = [])
+ * @method static \Statamic\Contracts\Tokens\Token find(string $token)
+ * @method static bool save(\Statamic\Contracts\Tokens\Token $token)
+ * @method static bool delete(\Statamic\Contracts\Tokens\Token $token)
+ * @method static void collectGarbage()
+ */
 class Token extends Facade
 {
     protected static function getFacadeAccessor()
