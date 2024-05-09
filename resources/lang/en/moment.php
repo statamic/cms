c28dc31b3161f33b60adae4ac0871239c6842272
Peiman Nourani <nourani.peiman@gmail.com>
2024-05-09 19:56:10 +0100

    fix php linter error

--- resources/lang/en/moment.php
+++ resources/lang/en/moment.php
@@ -1,18 +1,18 @@
 <?php
 
 return [
-    "future" => "in %s",
-    "past" => "%s ago",
-    "s" => 'a few seconds',
-    "ss" => '%d seconds',
-    "m" => "a minute",
-    "mm" => "%d minutes",
-    "h" => "an hour",
-    "hh" => "%d hours",
-    "d" => "a day",
-    "dd" => "%d days",
-    "M" => "a month",
-    "MM" => "%d months",
-    "y" => "a year",
-    "yy" => "%d years"
+    'future' => 'in %s',
+    'past' => '%s ago',
+    's' => 'a few seconds',
+    'ss' => '%d seconds',
+    'm' => 'a minute',
+    'mm' => '%d minutes',
+    'h' => 'an hour',
+    'hh' => '%d hours',
+    'd' => 'a day',
+    'dd' => '%d days',
+    'M' => 'a month',
+    'MM' => '%d months',
+    'y' => 'a year',
+    'yy' => '%d years',
 ];
