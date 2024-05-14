f5aa9788211b1b3130b843d447d174b5ed715d72
Duncan McClean <duncan@duncanmcclean.com>
2024-05-14 16:55:07 +0100

    Put this back to the way it was

--- resources/js/components/fieldtypes/TagsFieldtype.vue
+++ resources/js/components/fieldtypes/TagsFieldtype.vue
@@ -4,7 +4,7 @@
         :name="name"
         :clearable="config.clearable"
         :close-on-select="false"
-        :options="options"
+        :options="config.options"
         :disabled="config.disabled || isReadOnly"
         :multiple="true"
         :placeholder="__(config.placeholder)"
@@ -70,12 +70,6 @@ export default {
     },
 
     mixins: [Fieldtype, HasInputOptions],
-
-    computed: {
-        options() {
-            return this.normalizeInputOptions(this.config.options);
-        },
-    },
 
     methods: {
         focus() {
