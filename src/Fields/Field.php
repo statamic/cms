ba6e5e4d5ce9da55cfc3a04ed11e1ec24d32576c
Jason Varga <jason@pixelfear.com>
2024-05-15 12:51:08 -0400

    don't filter anything out

--- src/Fields/Field.php
+++ src/Fields/Field.php
@@ -367,7 +367,6 @@ class Field implements Arrayable
     {
         $defaultValues = Field::commonFieldOptions()->all()
             ->merge($this->fieldtype()->configFields()->all())
-            ->filter(fn (ConfigField $configField) => $configField->get('default'))
             ->map(fn (ConfigField $configField) => $configField->get('default'))
             ->all();
 
