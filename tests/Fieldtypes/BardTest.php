5688c4f4afa791cf69841a7b1ffd912560584f9c
Duncan McClean <duncan@duncanmcclean.com>
2024-05-16 12:26:25 +0100

    Add test to prevent regression of #6045

--- tests/Fieldtypes/BardTest.php
+++ tests/Fieldtypes/BardTest.php
@@ -185,6 +185,28 @@ class BardTest extends TestCase
     }
 
     /** @test */
+    public function it_augments_when_save_html_is_enabled_but_field_has_existing_content()
+    {
+        $data = [
+            [
+                'type' => 'paragraph',
+                'content' => [
+                    ['type' => 'text', 'text' => 'This is a paragraph with '],
+                    ['type' => 'text', 'marks' => [['type' => 'bold']], 'text' => 'bold'],
+                    ['type' => 'text', 'text' => ' and '],
+                    ['type' => 'text', 'marks' => [['type' => 'italic']], 'text' => 'italic'],
+                    ['type' => 'text', 'text' => ' text.'],
+                ],
+            ],
+        ];
+
+        $expected = '<p>This is a paragraph with <strong>bold</strong> and <em>italic</em> text.</p>';
+
+        $this->assertEquals($expected, $this->bard(['save_html' => true, 'sets' => []])->augment($data));
+        $this->assertEquals($expected, $this->bard(['save_html' => true, 'sets' => null])->augment($data));
+    }
+
+    /** @test */
     public function it_augments_tiptap_v1_snake_case_types_to_v2_camel_case_types()
     {
         Augmentor::addExtension('customNode', new class extends Node
