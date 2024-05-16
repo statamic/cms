c5fa2afb5eac7958bcff6203fc832c86839e5fc5
Jason Varga <jason@pixelfear.com>
2024-05-16 16:40:24 -0400

    use trait

--- src/Http/Controllers/CP/Collections/EntryActionController.php
+++ src/Http/Controllers/CP/Collections/EntryActionController.php
@@ -8,6 +8,8 @@ use Statamic\Http\Controllers\CP\ActionController;
 
 class EntryActionController extends ActionController
 {
+    use ExtractsFromEntryFields;
+
     protected function getSelectedItems($items, $context)
     {
         return $items->map(function ($item) {
@@ -27,41 +29,5 @@ class EntryActionController extends ActionController
             'values' => array_merge($values, ['id' => $entry->id()]),
             'itemActions' => Action::for($entry, $context),
         ];
-    }
-
-    protected function extractFromFields($entry, $blueprint)
-    {
-        // The values should only be data merged with the origin data.
-        // We don't want injected collection values, which $entry->values() would have given us.
-        $target = $entry;
-        $values = $target->data();
-        while ($target->hasOrigin()) {
-            $target = $target->origin();
-            $values = $target->data()->merge($values);
-        }
-        $values = $values->all();
-
-        if ($entry->hasStructure()) {
-            $values['parent'] = array_filter([optional($entry->parent())->id()]);
-        }
-
-        if ($entry->collection()->dated()) {
-            $datetime = substr($entry->date()->toDateTimeString(), 0, 16);
-            $datetime = ($entry->hasTime()) ? $datetime : substr($datetime, 0, 10);
-            $values['date'] = $datetime;
-        }
-
-        $fields = $blueprint
-            ->fields()
-            ->addValues($values)
-            ->preProcess();
-
-        $values = $fields->values()->merge([
-            'title' => $entry->value('title'),
-            'slug' => $entry->slug(),
-            'published' => $entry->published(),
-        ]);
-
-        return [$values->all(), $fields->meta()];
     }
 }
