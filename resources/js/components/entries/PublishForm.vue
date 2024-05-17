5b564336d7c105ba7e74e627b0625d36a7b79a49
Jason Varga <jason@pixelfear.com>
2024-05-16 20:21:38 -0400

    The actions werent being updated ...
    This could happen if the state of the entry causes different actions to be available.
    For example when its published, the unpublished action is visible but not the publish action, and visa versa.

--- resources/js/components/entries/PublishForm.vue
+++ resources/js/components/entries/PublishForm.vue
@@ -819,6 +819,7 @@ export default {
                 this.title = response.data.title;
                 if (!this.revisionsEnabled) this.permalink = response.data.permalink;
                 this.values = this.resetValuesFromResponse(response.data.values);
+                this.itemActions = response.data.itemActions;
             }
         },
 
