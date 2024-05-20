statamic/cms

commit f8f022672414ab18859f315aa7d9365c648acfcc

[5.x] Reduce the number of times the `fieldsCache` is reset (#9585)
Co-authored-by: Jason Varga <jason@pixelfear.com>

src/Fields/Blueprint.php
@@ -42,6 +42,8 @@ class Blueprint implements Arrayable, ArrayAccess, Augmentable, QueryableValue
     protected $ensuredFields = [];
     protected $afterSaveCallbacks = [];
     protected $withEvents = true;
+    private $lastBlueprintHandle = null;
+
     private ?Columns $columns = null;
 
     public function setHandle(string $handle)
@@ -635,6 +637,16 @@ class Blueprint implements Arrayable, ArrayAccess, Augmentable, QueryableValue
 
     protected function resetFieldsCache()
     {
+        if ($this->parent) {
+            $blueprint = (fn () => property_exists($this, 'blueprint') ? $this->blueprint : null)->call($this->parent);
+
+            if ($blueprint && $blueprint === $this->lastBlueprintHandle) {
+                return $this;
+            }
+
+            $this->lastBlueprintHandle = $blueprint;
+        }
+
         $this->fieldsCache = null;
 
         Blink::forget($this->contentsBlinkKey());

commit abf641af9a7bd862a4f658954d9d32b3684381c5

[5.x] Add failing test for issue caused by PR #9585 (#10061)
Co-authored-by: John Koster <john@stillat.com>

src/Fields/Blueprint.php
@@ -514,7 +514,7 @@ class Blueprint implements Arrayable, ArrayAccess, Augmentable, QueryableValue
 
         $this->ensuredFields[$handle] = compact('handle', 'tab', 'prepend', 'config');
 
-        $this->resetFieldsCache();
+        $this->resetBlueprintCache()->resetFieldsCache();
 
         return $this;
     }
@@ -573,7 +573,7 @@ class Blueprint implements Arrayable, ArrayAccess, Augmentable, QueryableValue
 
         Arr::pull($this->contents['tabs'], $handle);
 
-        return $this->resetFieldsCache();
+        return $this->resetBlueprintCache()->resetFieldsCache();
     }
 
     public function removeFieldFromTab($handle, $tab)
@@ -591,7 +591,7 @@ class Blueprint implements Arrayable, ArrayAccess, Augmentable, QueryableValue
         // Pull it out.
         Arr::pull($this->contents['tabs'][$tab]['sections'][$sectionIndex]['fields'], $fieldKey);
 
-        return $this->resetFieldsCache();
+        return $this->resetBlueprintCache()->resetFieldsCache();
     }
 
     private function getTabFields($tab)
@@ -621,7 +621,7 @@ class Blueprint implements Arrayable, ArrayAccess, Augmentable, QueryableValue
         // Merge in new field config.
         $this->contents['tabs'][$tab]['sections'][$sectionKey]['fields'][$fieldKey]['field'] = array_merge($existingConfig, $config);
 
-        return $this->resetFieldsCache();
+        return $this->resetBlueprintCache()->resetFieldsCache();
     }
 
     public function validateUniqueHandles()
@@ -633,6 +633,13 @@ class Blueprint implements Arrayable, ArrayAccess, Augmentable, QueryableValue
         if ($field = $handles->duplicates()->first()) {
             throw new DuplicateFieldException($field, $this);
         }
+    }
+
+    protected function resetBlueprintCache()
+    {
+        $this->lastBlueprintHandle = null;
+
+        return $this;
     }
 
     protected function resetFieldsCache()

tests/Fields/BlueprintTest.php
@@ -11,6 +11,7 @@ use Statamic\Contracts\Data\Augmentable;
 use Statamic\Contracts\Query\QueryableValue;
 use Statamic\CP\Column;
 use Statamic\CP\Columns;
+use Statamic\Entries\Entry;
 use Statamic\Events\BlueprintCreated;
 use Statamic\Events\BlueprintCreating;
 use Statamic\Events\BlueprintDeleted;
@@ -781,6 +782,29 @@ class BlueprintTest extends TestCase
             ],
         ]], $blueprint->contents());
         $this->assertEquals(['type' => 'textarea'], $blueprint->fields()->get('new')->config());
+    }
+
+    /** @test */
+    public function it_can_add_fields_multiple_times()
+    {
+        $blueprint = (new Blueprint)
+            ->setNamespace('collections/collection_one')
+            ->setHandle('blueprint_one');
+
+        $entry = (new Entry)
+            ->collection('collection_one')
+            ->blueprint($blueprint);
+
+        $blueprint->setParent($entry);
+
+        $blueprint->ensureFieldsInTab(['field_one' => ['type' => 'text']], 'tab_one');
+
+        $this->assertTrue($blueprint->hasField('field_one'));
+
+        $blueprint->ensureField('field_two', ['type' => 'textarea']);
+
+        $this->assertTrue($blueprint->hasField('field_two'));
+
     }
 
     /** @test */