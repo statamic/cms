35fcf82a81ca7222b0fca30d2205ea9c4562d018
Jason Varga <jason@pixelfear.com>
2024-05-21 14:10:43 -0400

    just do the bare minimum needed to stop erroring. we dont use any of that returned data at the moment.

--- src/Http/Controllers/CP/Assets/ActionController.php
+++ src/Http/Controllers/CP/Assets/ActionController.php
@@ -2,10 +2,8 @@
 
 namespace Statamic\Http\Controllers\CP\Assets;
 
-use Statamic\Facades\Action;
 use Statamic\Facades\Asset;
 use Statamic\Http\Controllers\CP\ActionController as Controller;
-use Statamic\Http\Resources\API\AssetResource;
 
 class ActionController extends Controller
 {
@@ -20,25 +18,6 @@ class ActionController extends Controller
 
     protected function getItemData($asset, $context): array
     {
-        $blueprint = $asset->blueprint();
-
-        [$values] = $this->extractFromFields($asset, $blueprint);
-
-        return array_merge((new AssetResource($asset))->resolve(), [
-            'values' => $values,
-            'itemActions' => Action::for($asset, $context),
-        ]);
-    }
-
-    private function extractFromFields($asset, $blueprint)
-    {
-        $values = $asset->data();
-
-        $fields = $blueprint
-            ->fields()
-            ->addValues($values->all())
-            ->preProcess();
-
-        return [$fields->values()->all(), $fields->meta()->all()];
+        return [];
     }
 }
