73c48a448d4be476f4cc7e70e55e0d7c24e4f1b8
Potsky <potsky@me.com>
2024-05-21 03:16:14 +0200

    [5.x] Attribute Modifier (#9796)
    Co-authored-by: Duncan McClean <duncan@duncanmcclean.com>
    Co-authored-by: Jason Varga <jason@pixelfear.com>

--- src/Modifiers/CoreModifiers.php
+++ src/Modifiers/CoreModifiers.php
@@ -158,6 +158,48 @@ class CoreModifiers extends Modifier
     }
 
     /**
+     * Returns an attribute ($params[0]) with its value when the given $value variable is not empty.
+     *
+     * @param  string  $value
+     * @param  array  $params
+     * @return string
+     */
+    public function attribute($value, $params)
+    {
+        if (! $name = Arr::get($params, 0)) {
+            throw new \Exception('Attribute name is required.');
+        }
+
+        if ($value instanceof Collection) {
+            $value = $value->all();
+        }
+
+        if (\is_array($value)) {
+            if (empty($value)) {
+                return '';
+            }
+
+            $value = \json_encode($value);
+        }
+
+        if (\is_bool($value)) {
+            return $value ? ' '.$name : '';
+        }
+
+        if (\is_object($value)) {
+            $value = (string) $value;
+        }
+
+        $value = trim($value);
+
+        if ($value === '') {
+            return '';
+        }
+
+        return sprintf(' %s="%s"', $name, Html::entities($value));
+    }
+
+    /**
      * Returns a focal point as a background-position CSS value.
      *
      * @return string
