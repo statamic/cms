aaddd5e0e3b18caf5fd793fd312429c84c71b7e9
Daniel Weaver <godismyjudge95@gmail.com>
2024-05-23 14:35:51 -0400

    Add only modifier

--- src/Modifiers/CoreModifiers.php
+++ src/Modifiers/CoreModifiers.php
@@ -1755,6 +1755,41 @@ class CoreModifiers extends Modifier
     }
 
     /**
+     * Gets certain values from a collection of items.
+     *
+     * @param  array|Collection  $value
+     * @param  array  $params
+     * @return array|Collection
+     */
+    public function only($value, $params)
+    {
+        $keys = Arr::wrap($params);
+
+        if ($wasArray = is_array($value)) {
+            $value = collect($value);
+        }
+
+        if (Compare::isQueryBuilder($value)) {
+            $value = $value->get();
+        }
+
+        $items = $value->map(function ($item) use ($keys) {
+            return collect($keys)->mapWithKeys(function ($key) use ($item) {
+                $value = null;
+                if (is_array($item) || $item instanceof ArrayAccess) {
+                    $value = Arr::get($item, $key);
+                } else {
+                    $value = method_exists($item, 'value') ? $item->value($key) : $item->get($key);
+                }
+
+                return [$key => $value];
+            })->all();
+        });
+
+        return $wasArray ? $items->all() : $items;
+    }
+
+    /**
      * Get the plural form of an English word with access to $context.
      *
      * @param  string  $value
