1a56ad64a60964b084596cf427dd66b681dd29ee
Daniel Weaver <godismyjudge95@gmail.com>
2024-05-23 16:25:06 -0400

    Add depth parameter to flatten modifier

--- tests/Modifiers/FlattenTest.php
+++ tests/Modifiers/FlattenTest.php
@@ -35,10 +35,19 @@ class FlattenTest extends TestCase
 
         $modified = $this->modify($input);
         $this->assertEquals($expected, $modified);
+
+        $expected = [
+            ['garlic', 'cumin', 'ginger', 'turmeric', 'paprika', 'curry powder'],
+            ['tomatoes', 'onion'],
+            ['chicken'],
+        ];
+
+        $modified = $this->modify($input, 1);
+        $this->assertEquals($expected, $modified);
     }
 
-    private function modify(array $value)
+    private function modify(array $value, $param = null)
     {
-        return Modify::value($value)->flatten()->fetch();
+        return Modify::value($value)->flatten($param)->fetch();
     }
 }
