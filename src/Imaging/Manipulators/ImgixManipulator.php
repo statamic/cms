a15d43961afb1589d9bb969fff83af6ad70e6311
Jason Varga <jason@pixelfear.com>
2024-05-27 22:51:50 -0400

    stub imgix

---
+++ src/Imaging/Manipulators/ImgixManipulator.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Statamic\Imaging\Manipulators;
+
+class ImgixManipulator extends Manipulator
+{
+    public function getAvailableParams(): array
+    {
+        //
+    }
+
+    public function getUrl(): string
+    {
+        //
+    }
+
+    public function getDataUrl(): string
+    {
+        //
+    }
+
+    public function getAttributes(): array
+    {
+        // To get attributes in imgix, use the same url but set fmt=json.
+    }
+}
