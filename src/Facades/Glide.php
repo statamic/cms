b71be802b03389cb38c9828996b8ebf9826f0ad6
Jason Varga <jason@pixelfear.com>
2024-05-27 22:53:38 -0400

    deprecations

--- src/Facades/Glide.php
+++ src/Facades/Glide.php
@@ -4,7 +4,12 @@ namespace Statamic\Facades;
 
 use Illuminate\Support\Facades\Facade;
 use Statamic\Imaging\GlideManager;
+use Statamic\Imaging\Manipulators\GlideManipulator;
 
+/**
+ * @deprecated Glide should be accessed through its manipulation class.
+ * @see GlideManipulator
+ */
 class Glide extends Facade
 {
     protected static function getFacadeAccessor()
