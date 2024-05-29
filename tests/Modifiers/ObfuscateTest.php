d014adacc75f0df7d8d8f05b6825177b077871d7
vvlrad <lukas.radtke@visuellverstehen.de>
2024-05-29 09:43:45 +0200

    add tests for multibyte string

--- tests/Modifiers/ObfuscateTest.php
+++ tests/Modifiers/ObfuscateTest.php
@@ -14,6 +14,16 @@ class ObfuscateTest extends TestCase
         $this->assertTrue(in_array($modified, ['&#65;', '&#x41;', 'A']));
     }
 
+    /** @test */
+    public function it_obfuscates_a_multibyte_string(): void
+    {
+        $modified = $this->modify('é');
+        $this->assertTrue(in_array($modified, ['&#233;', '&#xE9;', 'é']));
+
+        $modified = $this->modify('ß');
+        $this->assertTrue(in_array($modified, ['&#223;', '&#xDF;', 'ß']));
+    }
+
     private function modify($value)
     {
         return Modify::value($value)->obfuscate()->fetch();
