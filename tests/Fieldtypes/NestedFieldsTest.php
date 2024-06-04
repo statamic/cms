b510392431c6149e12986b2c28363d50d7b81299
Ryan Mitchell <ryan@thoughtcollective.com>
2024-06-04 10:04:45 +0100

    Add test expectations

--- tests/Fieldtypes/NestedFieldsTest.php
+++ tests/Fieldtypes/NestedFieldsTest.php
@@ -79,6 +79,7 @@ class NestedFieldsTest extends TestCase
                 'instructions' => 'Some instructions',
                 'instructions_position' => 'above',
                 'listable' => 'hidden',
+                'sortable' => true,
                 'visibility' => 'visible',
                 'replicator_preview' => true,
                 'duplicate' => true,
