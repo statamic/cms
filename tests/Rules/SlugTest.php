312451cbc651a53a4d4f0215950d300f9a895922
Oliver Mesieh <olivermesieh@gmail.com>
2024-06-05 10:50:46 +0200

    Fix failed slug validation when slug contains underscores

--- tests/Rules/SlugTest.php
+++ tests/Rules/SlugTest.php
@@ -17,19 +17,26 @@ class SlugTest extends TestCase
         $this->assertPasses('foo');
         $this->assertPasses('foo-bar');
         $this->assertPasses('foo-bar-baz');
+        $this->assertPasses('foo_bar_baz');
         $this->assertPasses('foo-bar-baz-qux');
         $this->assertPasses('1-foo-bar234-baz-qux-5');
+        $this->assertPasses('1_foo_bar234_baz_qux_5');
+        $this->assertPasses('1-foo_bar234-baz_qux-5');
 
-        $this->assertFails('foo_bar');
         $this->assertFails('-foo');
         $this->assertFails('foo-');
         $this->assertFails('-foo-bar');
+        $this->assertFails('_foo-bar');
         $this->assertFails('foo-bar-');
+        $this->assertFails('foo-bar_');
         $this->assertFails('foo--bar');
+        $this->assertFails('foo__bar');
         $this->assertFails('foo---bar');
+        $this->assertFails('foo___bar');
         $this->assertFails('*foo');
         $this->assertFails('foo#');
         $this->assertFails('foo-!bar');
+        $this->assertFails('foo_!bar');
         $this->assertFails('foo-_-bar');
     }
 
