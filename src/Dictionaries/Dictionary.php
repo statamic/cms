2ea020a21ca93230dc13ed8a4fe19357bd4db600
Duncan McClean <duncan@duncanmcclean.com>
2024-06-20 21:53:16 +0100

    wip

--- src/Dictionaries/Dictionary.php
+++ src/Dictionaries/Dictionary.php
@@ -12,11 +12,7 @@ abstract class Dictionary
 
     protected static $binding = 'dictionaries';
 
-    abstract public function options(string $search = null): array;
+    abstract public function options(?string $search = null): array;
 
     abstract public function get(string $option);
-
-    // Goal: To have an easy way to point at a JSON or YAML file and have it return its data
-    // If the data is a key/value pair, it should return the key as the value and the value as the label
-    // If it's not, try and identify the key and value and return them as such
 }
