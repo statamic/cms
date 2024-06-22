544b8eab281c6d9334497b20535a1e321896e7b9
rezbouchabou <abderrezakbouchabou@gmail.com>
2024-06-22 15:15:02 +0300

    Added Arabic Translation Fix Updater Widget Icon in Dark mode

---
+++ resources/lang/ar/markdown.php
@@ -0,0 +1,57 @@
+<?php
+
+return [
+
+    'cheatsheet' => '
+<p>ماركداون هو نص يحول إلى HTML تنسيقي لكتاب الويب. يسمح لك ماركداون بالكتابة باستخدام تنسيق نصي بسيط وسهل القراءة والكتابة يتم تحويله إلى نص HTML صالح هيكليًا.</p>
+
+<h3>العناوين</h3>
+<pre class="language-markdown"><code># هذا عنوان من المستوى الاول
+## هذا عنوان من المستوى الثاني
+### هذا عنوان من المستوى الثالث، وما إلى ذلك.
+</code></pre>
+
+<h3>عريض و مائل</h3>
+<pre class="language-markdown"><code>يمكنك جعل الأشياء *مشددة*, **عريضة**, أو _**كليهما**_.</code></pre>
+
+<h3>الروابط</h3>
+<pre class="language-markdown"><code>هذا [مثال الرابط] (http://example.com).</code></pre>
+
+<h3>الكود</h3>
+<p>
+قم بلف الكود الخاص بك بثلاث علامات (<code>```</code>) قبل وبعد السطر.
+</p>
+
+<pre class="language-markdown"><code>```
+هذا: بعض yaml
+```</code></pre>
+
+<p>يمكنك أيضًا إضافة كود داخل النص عن طريق لفه بعلامات . <code>`</code> على شكل واحد.
+
+<h3>الاقتباس</h3>
+
+<p>أنشئ علامة اقتباس عن طريق بدء النص بـ <code>> </code>.</p>
+
+<pre class="language-markdown"><code>> سيكون هذا بمثابة اقتباس.</code></pre>
+
+<h3>الصور</h3>
+<pre class="language-markdown"><code>![نص بديل](http://example.com/image.jpg)</code></pre>
+
+<h3>القائمة الغير مرتبة</h3>
+<pre class="language-markdown"><code>- دجاج
+- ستيك
+- مشروب</code></pre>
+
+<h3>القائمة مرتبة</h3>
+<pre class="language-markdown"><code>1. كل
+2. اشرب
+3. كن مرحا</code></pre>
+
+<h3>الجداول</h3>
+
+<pre class="language-markdown"><code>العنوان الثاني  | العنوان الأول
+------------- | -------------
+محتوى الخلية  | محتوى الخلية
+محتوى الخلية  | محتوى الخلية</code></pre>',
+
+];
---
+++ resources/lang/ar/moment.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'relativeTime.M' => 'شهر',
+    'relativeTime.MM' => '%d شهرًا',
+    'relativeTime.d' => 'يوم',
+    'relativeTime.dd' => '%d يوم',
+    'relativeTime.future' => 'في %s',
+    'relativeTime.h' => 'ساعة',
+    'relativeTime.hh' => '%d ساعة',
+    'relativeTime.m' => 'دقيقة',
+    'relativeTime.mm' => '%d دقيقة',
+    'relativeTime.past' => 'منذ %s',
+    'relativeTime.s' => 'بضع ثوان',
+    'relativeTime.ss' => '%d ثانية',
+    'relativeTime.y' => 'سنة',
+    'relativeTime.yy' => '%d سنة',
+];
