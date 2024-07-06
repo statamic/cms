3db0504ff7262430b64f094641688cade87f87a8
Rob de Kort <rob@studio1902.nl>
2024-07-06 12:21:12 +0200

    Update StaticWarmJob.php
    Make the jobs unique based on the request path.

--- src/Console/Commands/StaticWarmJob.php
+++ src/Console/Commands/StaticWarmJob.php
@@ -6,21 +6,30 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Request;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 
-class StaticWarmJob implements ShouldQueue
+class StaticWarmJob implements ShouldQueue, ShouldBeUnique
 {
     use Dispatchable, InteractsWithQueue, Queueable;
 
     public $tries = 1;
+    
+    private $id;
 
     public function __construct(public Request $request, public array $clientConfig)
     {
+        $this->id = $request->getUri()->getPath();
     }
 
     public function handle()
     {
         (new Client($this->clientConfig))->send($this->request);
+    }
+
+    public function uniqueId(): string
+    {
+        return $this->id;
     }
 }
