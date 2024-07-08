0256d8f6f88c9cda24f13bc2fd077418feb9d493
Jelle Roorda <info@roordaict.nl>
2024-07-08 15:46:30 +0200

    Make data list search work with new modelValue instead of value.

--- resources/js/components/data-list/Search.vue
+++ resources/js/components/data-list/Search.vue
@@ -1,45 +1,43 @@
+<script setup>
+import { ref } from 'vue';
+import _ from 'underscore';
+
+const props = defineProps({
+    placeholder: {
+        type: String,
+        default: 'Search...'
+    },
+})
+
+const modelValue = defineModel();
+const input = ref(null)
+
+const emitEvent = _.debounce(function (e) {
+    modelValue.value = e.target.value;
+}, 300);
+
+function reset() {
+    modelValue.value = '';
+}
+
+function focus() {
+    input.value?.focus();
+}
+
+defineExpose({
+    focus,
+})
+</script>
+
 <template>
     <input
         type="text"
         ref="input"
         :placeholder="__(placeholder)"
-        :value="value"
+        :value="modelValue"
         @input="emitEvent"
         @keyup.esc="reset"
         autofocus
-        class="input-text flex-1 bg-white dark:bg-dark-600 text-sm focus:border-blue-300 dark:focus:border-dark-blue-125 outline-0">
+        class="input-text flex-1 bg-white dark:bg-dark-600 text-sm focus:border-blue-300 dark:focus:border-dark-blue-125 outline-0"
+    >
 </template>
-
-<script>
-export default {
-
-    props: [
-        'value'
-    ],
-
-    props: {
-        placeholder: {
-            type: String,
-            default: 'Search...'
-        },
-        value: {
-            type: String,
-            default: ''
-        }
-    },
-
-    methods: {
-        emitEvent: _.debounce(function (event) {
-            this.$emit('input', event.target.value);
-        }, 300),
-
-        reset() {
-            this.$emit('input', '');
-        },
-
-        focus() {
-            this.$refs.input.focus();
-        }
-    },
-}
-</script>
