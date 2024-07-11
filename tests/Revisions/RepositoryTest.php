62b1702769a96caff2a61ad205e0ba7113a7ad6e
Ryan Mitchell <ryan@thoughtcollective.com>
2024-07-11 11:27:55 +0100

    Test the query builder

--- tests/Revisions/RepositoryTest.php
+++ tests/Revisions/RepositoryTest.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Collection;
 use PHPUnit\Framework\Attributes\Test;
 use Statamic\Revisions\Revision;
 use Statamic\Revisions\RevisionRepository;
+use Statamic\Contracts\Revisions\RevisionQueryBuilder;
 use Tests\TestCase;
 
 class RepositoryTest extends TestCase
@@ -27,5 +28,36 @@ class RepositoryTest extends TestCase
         $this->assertInstanceOf(Collection::class, $revisions);
         $this->assertCount(2, $revisions);
         $this->assertContainsOnlyInstancesOf(Revision::class, $revisions);
+    }
+
+    #[Test]
+    public function it_returns_a_query_builder()
+    {
+        $builder = $this->repo->query();
+
+        $this->assertInstanceOf(RevisionQueryBuilder::class, $builder);
+    }
+
+    #[Test]
+    public function it_gets_and_filters_items_using_query_builder()
+    {
+        $builder = $this->repo->query();
+
+        $revisions = $builder->get();
+
+        $this->assertInstanceOf(Collection::class, $revisions);
+        $this->assertCount(3, $revisions);
+        $this->assertContainsOnlyInstancesOf(Revision::class, $revisions);
+
+        $revisions = $builder->where('key','123')->get();
+
+        $this->assertInstanceOf(Collection::class, $revisions);
+        $this->assertCount(2, $revisions);
+        $this->assertContainsOnlyInstancesOf(Revision::class, $revisions);
+
+        $revisions = $builder->where('key','1234')->get();
+
+        $this->assertInstanceOf(Collection::class, $revisions);
+        $this->assertCount(0, $revisions);
     }
 }
