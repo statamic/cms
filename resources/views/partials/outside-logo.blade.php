1c243413a660e39db6adf6a18ac6f8e40024a7c3
Duncan McClean <duncan@duncanmcclean.com>
2024-07-12 11:27:37 +0100

    Prevent dark mode gradient affecting custom logos

--- resources/views/partials/outside-logo.blade.php
+++ resources/views/partials/outside-logo.blade.php
@@ -1,4 +1,4 @@
-<div class="logo pt-20">
+<div class="logo pt-20 relative z-10">
     @if ($customLogo)
         <img src="{{ $customLogo }}" alt="{{ config('statamic.cp.custom_cms_name') }}" class="white-label-logo dark:hidden">
         <img src="{{ $customDarkLogo }}" alt="{{ config('statamic.cp.custom_cms_name') }}" class="white-label-logo hidden dark:block">
