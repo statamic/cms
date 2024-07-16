5b4b5b704cc121c77ead72acd8d0b3f641b819c8
Duncan McClean <duncan@duncanmcclean.com>
2024-07-16 15:50:46 +0100

    add some padding

--- resources/js/components/publish/SaveButtonOptions.vue
+++ resources/js/components/publish/SaveButtonOptions.vue
@@ -13,7 +13,7 @@
                 </button>
             </template>
             <h6 v-text="__('After Saving')" class="p-2" />
-            <div class="publish-fields px-2">
+            <div class="publish-fields px-2 py-1">
                 <div class="publish-field save-and-continue-options radio-fieldtype">
                     <radio-fieldtype
                         handle="save_and_continue_options"
