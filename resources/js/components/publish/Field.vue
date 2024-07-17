e792c1105757a92a9b77766b424b4028fec2418f
Jelle Roorda <info@roordaict.nl>
2024-07-17 12:49:20 +0200

    Add back in the publish field label.

--- resources/js/components/publish/Field.vue
+++ resources/js/components/publish/Field.vue
@@ -7,60 +7,60 @@
         >
             <template #default="{ meta, loading: loadingMeta }">
                 <div class="field-inner">
-                    <!--                    <label-->
-                    <!--                        v-if="showLabel" class="publish-field-label" :class="{'font-bold': config.bold}" :for="fieldId"-->
-                    <!--                    >-->
-                    <!--                        <span-->
-                    <!--                            v-if="showLabelText"-->
-                    <!--                            class="rtl:ml-1 ltr:mr-1"-->
-                    <!--                            :class="{ 'text-gray-600': syncable && isSynced }"-->
-                    <!--                            v-text="__(labelText)"-->
-                    <!--                            v-tooltip="{content: config.handle, delay: 500, autoHide: false}"-->
-                    <!--                        />-->
-                    <!--                        <i class="required rtl:ml-1 ltr:mr-1" v-if="showLabelText && config.required">*</i>-->
-                    <!--                        <avatar-->
-                    <!--                            v-if="isLocked"-->
-                    <!--                            :user="lockingUser"-->
-                    <!--                            class="w-6 h-6 rounded-full -mt-px rtl:mr-2 ltr:ml-2 rtl:ml-2 ltr:mr-2"-->
-                    <!--                            v-tooltip="lockingUser.name"-->
-                    <!--                        />-->
-                    <!--                        <span-->
-                    <!--                            v-if="isReadOnly && !isTab && !isSection"-->
-                    <!--                            class="text-gray-500 font-normal text-2xs rtl:ml-1 ltr:mr-1"-->
-                    <!--                        >-->
-                    <!--                            {{ isLocked ? __('Locked') : __('Read Only') }}-->
-                    <!--                        </span>-->
-                    <!--                        <svg-icon-->
-                    <!--                            name="translate" class="h-4 rtl:ml-1 ltr:mr-1 w-4 text-gray-600"-->
-                    <!--                            v-if="isLocalizable && !isTab" v-tooltip.top="__('Localizable field')"-->
-                    <!--                        />-->
+                    <label
+                        v-if="showLabel" class="publish-field-label" :class="{'font-bold': config.bold}" :for="fieldId"
+                    >
+                        <span
+                            v-if="showLabelText"
+                            class="rtl:ml-1 ltr:mr-1"
+                            :class="{ 'text-gray-600': syncable && isSynced }"
+                            v-text="__(labelText)"
+                            v-tooltip="{content: config.handle, delay: 500, autoHide: false}"
+                        />
+                        <i class="required rtl:ml-1 ltr:mr-1" v-if="showLabelText && config.required">*</i>
+                        <avatar
+                            v-if="isLocked"
+                            :user="lockingUser"
+                            class="w-6 h-6 rounded-full -mt-px rtl:mr-2 ltr:ml-2 rtl:ml-2 ltr:mr-2"
+                            v-tooltip="lockingUser.name"
+                        />
+                        <span
+                            v-if="isReadOnly && !isTab && !isSection"
+                            class="text-gray-500 font-normal text-2xs rtl:ml-1 ltr:mr-1"
+                        >
+                            {{ isLocked ? __('Locked') : __('Read Only') }}
+                        </span>
+                        <svg-icon
+                            name="translate" class="h-4 rtl:ml-1 ltr:mr-1 w-4 text-gray-600"
+                            v-if="isLocalizable && !isTab" v-tooltip.top="__('Localizable field')"
+                        />
 
-                    <!--                        <button-->
-                    <!--                            v-if="!isReadOnly && !isTab"-->
-                    <!--                            v-show="syncable && isSynced"-->
-                    <!--                            class="outline-none"-->
-                    <!--                            :class="{ flex: syncable && isSynced }"-->
-                    <!--                            @click="$emit('desynced')"-->
-                    <!--                        >-->
-                    <!--                            <svg-icon-->
-                    <!--                                name="light/hyperlink" class="h-4 w-4 rtl:ml-1.5 ltr:mr-1.5 mb-1 text-gray-600"-->
-                    <!--                                v-tooltip.top="__('messages.field_synced_with_origin')"-->
-                    <!--                            />-->
-                    <!--                        </button>-->
+                        <button
+                            v-if="!isReadOnly && !isTab"
+                            v-show="syncable && isSynced"
+                            class="outline-none"
+                            :class="{ flex: syncable && isSynced }"
+                            @click="$emit('desynced')"
+                        >
+                            <svg-icon
+                                name="light/hyperlink" class="h-4 w-4 rtl:ml-1.5 ltr:mr-1.5 mb-1 text-gray-600"
+                                v-tooltip.top="__('messages.field_synced_with_origin')"
+                            />
+                        </button>
 
-                    <!--                        <button-->
-                    <!--                            v-if="!isReadOnly && !isTab"-->
-                    <!--                            v-show="syncable && !isSynced"-->
-                    <!--                            class="outline-none"-->
-                    <!--                            :class="{ flex: syncable && !isSynced }"-->
-                    <!--                            @click="$emit('synced')"-->
-                    <!--                        >-->
-                    <!--                            <svg-icon-->
-                    <!--                                name="light/hyperlink-broken" class="h-4 w-4 rtl:ml-1.5 ltr:mr-1.5 mb-1 text-gray-600"-->
-                    <!--                                v-tooltip.top="__('messages.field_desynced_from_origin')"-->
-                    <!--                            />-->
-                    <!--                        </button>-->
-                    <!--                    </label>-->
+                        <button
+                            v-if="!isReadOnly && !isTab"
+                            v-show="syncable && !isSynced"
+                            class="outline-none"
+                            :class="{ flex: syncable && !isSynced }"
+                            @click="$emit('synced')"
+                        >
+                            <svg-icon
+                                name="light/hyperlink-broken" class="h-4 w-4 rtl:ml-1.5 ltr:mr-1.5 mb-1 text-gray-600"
+                                v-tooltip.top="__('messages.field_desynced_from_origin')"
+                            />
+                        </button>
+                    </label>
 
                     <div
                         class="help-block" :class="{ '-mt-2': showLabel }"
