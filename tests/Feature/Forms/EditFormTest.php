f9777b8c8c0ec38047b55028a9ad4ff79e60b8bb
Ryan Mitchell <ryan@thoughtcollective.com>
2024-07-30 02:35:25 +0100

    [5.x] Make it possible to add to form configuration screen (#8491)
    Co-authored-by: Duncan McClean <duncan@duncanmcclean.com>
    Co-authored-by: Jason Varga <jason@pixelfear.com>

--- tests/Feature/Forms/EditFormTest.php
+++ tests/Feature/Forms/EditFormTest.php
@@ -49,4 +49,38 @@ class EditFormTest extends TestCase
             ->assertRedirect('/original')
             ->assertSessionHas('error');
     }
+
+    #[Test]
+    public function fields_can_be_added()
+    {
+        $this->setTestRoles(['test' => ['access cp', 'configure forms']]);
+        $user = User::make()->assignRole('test')->save();
+        $form = tap(Form::make('test'))->save();
+
+        Form::appendConfigFields('*', 'Fields', [
+            'a' => ['type' => 'text', 'display' => 'First injected into fields section'],
+            'b' => ['type' => 'text', 'display' => 'Second injected into fields section'],
+        ]);
+        Form::appendConfigFields('*', 'Additional Section', [
+            'c' => ['type' => 'text', 'display' => 'First injected into additional section'],
+            'd' => ['type' => 'text', 'display' => 'Second injected into additional section'],
+        ]);
+
+        $this
+            ->actingAs($user)
+            ->get(cp_route('forms.edit', $form->handle()))
+            ->assertSuccessful()
+            ->assertViewHas('form', $form)
+            ->assertSeeInOrder([
+                'Title',
+                'Blueprint',
+                'Honeypot',
+                'First injected into fields section',
+                'Second injected into fields section',
+                'Store Submissions',
+                'Additional Section',
+                'First injected into additional section',
+                'Second injected into additional section',
+            ]);
+    }
 }
