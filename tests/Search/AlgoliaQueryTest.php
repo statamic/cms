46b2d3ac64ba158723177b0ebb29055433bc43a5
Sebastian Hilger <hilger@kraenk.de>
2024-07-31 13:39:36 +0200

    add query test

--- tests/Search/AlgoliaQueryTest.php
+++ tests/Search/AlgoliaQueryTest.php
@@ -7,6 +7,7 @@ use PHPUnit\Framework\Attributes\Test;
 use Statamic\Search\Algolia\Index;
 use Statamic\Search\Algolia\Query;
 use Tests\TestCase;
+use Illuminate\Support\Facades\Config;
 
 class AlgoliaQueryTest extends TestCase
 {
@@ -14,6 +15,28 @@ class AlgoliaQueryTest extends TestCase
     public function it_adds_scores()
     {
         $index = Mockery::mock(Index::class);
+        $index->shouldReceive('searchUsingApi')->with('foo')->once()->andReturn(collect([
+            ['reference' => 'a'],
+            ['reference' => 'b'],
+            ['reference' => 'c'],
+        ]));
+
+        $query = new Query($index);
+
+        $this->assertEquals([
+            ['reference' => 'a', 'search_score' => 3],
+            ['reference' => 'b', 'search_score' => 2],
+            ['reference' => 'c', 'search_score' => 1],
+        ], $query->getSearchResults('foo')->all());
+    }
+
+    #[Test]
+    public function it_can_get_results_after_adding_prefix()
+    {
+        $index = Mockery::mock(Index::class);
+
+        Config::set('statamic.search.drivers.algolia.prefix', 'env_');
+
         $index->shouldReceive('searchUsingApi')->with('foo')->once()->andReturn(collect([
             ['reference' => 'a'],
             ['reference' => 'b'],
