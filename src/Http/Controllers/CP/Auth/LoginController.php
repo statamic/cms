1a152731e738518dd0c499984585dfa76fa498fa
Jason Varga <jason@pixelfear.com>
2024-08-01 14:16:46 -0400

    simplify

--- src/Http/Controllers/CP/Auth/LoginController.php
+++ src/Http/Controllers/CP/Auth/LoginController.php
@@ -101,20 +101,11 @@ class LoginController extends CpController
 
     public function redirectPath()
     {
+        $cp = cp_route('index');
         $referer = request('referer');
+        $referredFromCp = Str::startsWith($referer, $cp) && ! Str::startsWith($referer, $cp.'/auth/');
 
-        $authRoutes = [
-            cp_route('login'),
-            cp_route('password.request'),
-        ];
-
-        if (in_array($referer, $authRoutes)) {
-            return cp_route('index');
-        }
-
-        return Str::contains($referer, '/'.config('statamic.cp.route'))
-            ? $referer
-            : cp_route('index');
+        return $referredFromCp ? $referer : $cp;
     }
 
     protected function authenticated(Request $request, $user)
