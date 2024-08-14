statamic/cms

commit 0b854030a495d73438ac14963eaf59c0e212dc17

[5.x] Adjust behavior of array fields (#10467)
Co-authored-by: duncanmcclean <duncanmcclean@users.noreply.github.com>
Co-authored-by: Jason Varga <jason@pixelfear.com>

src/Fieldtypes/HasSelectOptions.php
@@ -14,6 +14,35 @@ trait HasSelectOptions
         return $this->config('multiple');
     }
 
+    public function preload(): array
+    {
+        return [
+            'options' => $this->getOptions(),
+        ];
+    }
+
+    protected function getOptions(): array
+    {
+        $options = $this->config('options') ?? [];
+
+        if (array_is_list($options) && ! is_array(Arr::first($options))) {
+            $options = collect($options)
+                ->map(fn ($value) => ['key' => $value, 'value' => $value])
+                ->all();
+        }
+
+        if (Arr::isAssoc($options)) {
+            $options = collect($options)
+                ->map(fn ($value, $key) => ['key' => $key, 'value' => $value])
+                ->all();
+        }
+
+        return collect($options)
+            ->map(fn ($item) => ['value' => $item['key'], 'label' => $item['value']])
+            ->values()
+            ->all();
+    }
+
     public function preProcessIndex($value)
     {
         $values = $this->preProcess($value);

resources/js/components/fieldtypes/ArrayFieldtype.vue
@@ -7,7 +7,7 @@
                      <select class="bg-transparent appearance-none shadow-none outline-none border-0 text-sm" @input="setKey($event.target.value)">
                         <option
                             v-for="(element, index) in keyedData"
-                            v-text="config.keys[element.key] || element.key"
+                            v-text="meta.keys[element.key] || element.key"
                             :key="element._id"
                             :value="element.key"
                             :selected="element.key === selectedKey" />
@@ -29,7 +29,7 @@
             <table class="array-table">
                 <tbody>
                     <tr v-if="data" v-for="(element, index) in keyedData" :key="element._id">
-                        <th class="w-1/4"><label :for="fieldId+'__'+element.key">{{ config.keys[element.key] || element.key }}</label></th>
+                        <th class="w-1/4"><label :for="fieldId+'__'+element.key">{{ meta.keys[element.key] || element.key }}</label></th>
                         <td>
                             <input type="text" class="input-text-minimal" :id="fieldId+'__'+element.key" v-model="data[index].value" :readonly="isReadOnly" />
                         </td>
@@ -130,7 +130,7 @@ export default {
 
     computed: {
         isKeyed() {
-            return Boolean(Object.keys(this.config.keys).length);
+            return Boolean(Object.keys(this.meta.keys).length);
         },
 
         isDynamic() {
@@ -142,7 +142,7 @@ export default {
         },
 
         keyedData() {
-            return this.data.filter(element => this.config.keys.hasOwnProperty(element.key));
+            return this.data.filter(element => this.meta.keys.hasOwnProperty(element.key));
         },
 
         maxItems() {

resources/js/components/fieldtypes/ButtonGroupFieldtype.vue
@@ -39,7 +39,7 @@ export default {
 
     computed: {
         options() {
-            return this.normalizeInputOptions(this.config.options);
+            return this.normalizeInputOptions(this.meta.options);
         },
 
         replicatorPreview() {

resources/js/components/fieldtypes/CheckboxesFieldtype.vue
@@ -30,7 +30,7 @@ export default {
 
     computed: {
         options() {
-            return this.normalizeInputOptions(this.config.options);
+            return this.normalizeInputOptions(this.meta.options);
         },
 
         replicatorPreview() {

resources/js/components/fieldtypes/HasInputOptions.js
@@ -1,10 +1,26 @@
 export default {
     methods: {
         normalizeInputOptions(options) {
-            return _.map(options, (value, key) => {
+            if (! Array.isArray(options)) {
+                return _.map(options, (value, key) => {
+                    return {
+                        'value': Array.isArray(options) ? value : key,
+                        'label': __(value) || key
+                    };
+                });
+            }
+
+            return _.map(options, (option) => {
+                if (typeof option === 'object') {
+                    return {
+                        'value': option.value,
+                        'label': __(option.label) || option.value
+                    };
+                }
+
                 return {
-                    'value': Array.isArray(options) ? value : key,
-                    'label': __(value) || key
+                    'value': option,
+                    'label': __(option) || option
                 };
             });
         }

resources/js/components/fieldtypes/RadioFieldtype.vue
@@ -48,7 +48,7 @@ export default {
 
     computed: {
         options() {
-            return this.normalizeInputOptions(this.config.options);
+            return this.normalizeInputOptions(this.meta.options);
         },
 
         replicatorPreview() {

resources/js/components/fieldtypes/SelectFieldtype.vue
@@ -96,7 +96,7 @@ export default {
 
     computed: {
         selectedOptions() {
-            let selections = this.value || [];
+            let selections = this.value === null ? [] : this.value;
             if (typeof selections === 'string' || typeof selections === 'number') {
                 selections = [selections];
             }
@@ -106,7 +106,7 @@ export default {
         },
 
         options() {
-            return this.normalizeInputOptions(this.config.options);
+            return this.normalizeInputOptions(this.meta.options);
         },
 
         replicatorPreview() {

resources/js/tests/NormalizeInputOptions.test.js
@@ -1,0 +1,57 @@
+import _ from 'underscore';
+import { __ } from '../bootstrap/globals';
+import hasInputOptions from "../components/fieldtypes/HasInputOptions";
+const normalizeInputOptions = hasInputOptions.methods.normalizeInputOptions;
+
+window._ = _;
+window.__ = __;
+
+const config = {
+    translations: {
+        '*.One': 'Uno'
+    }
+}
+
+window.Statamic = {
+    $config: {
+        get: (key) => config[key]
+    }
+}
+
+it('normalizes input options with simple array', () => {
+   expect(normalizeInputOptions([
+       'one',
+       'two'
+   ])).toEqual([
+       {value: 'one', label: 'one'},
+       {value: 'two', label: 'two'}
+   ]);
+
+    expect(normalizeInputOptions([
+        'One',
+        'Two'
+    ])).toEqual([
+        {value: 'One', label: 'Uno'},
+        {value: 'Two', label: 'Two'}
+    ]);
+});
+
+it('normalizes input options with object', () => {
+    expect(normalizeInputOptions({
+        one: 'One',
+        two: 'Two'
+    })).toEqual([
+        {value: 'one', label: 'Uno'},
+        {value: 'two', label: 'Two'}
+    ]);
+});
+
+it('normalizes input options with array of objects', () => {
+    expect(normalizeInputOptions([
+        {value: 'one', label: 'One'},
+        {value: 'two', label: 'Two'}
+    ])).toEqual([
+        {value: 'one', label: 'Uno'},
+        {value: 'two', label: 'Two'}
+    ]);
+});

commit 452885b1c2b67c3e1f41d0add094379e6a7119d3

Bug introduced in https://github.com/statamic/cms/pull/10467 Cast to array to prevent issues with listings when data is returned as a Statamic\Entries\EntryCollection

src/Fieldtypes/HasSelectOptions.php
@@ -23,7 +23,7 @@ trait HasSelectOptions
 
     protected function getOptions(): array
     {
-        $options = $this->config('options') ?? [];
+        $options = (array) $this->config('options') ?? [];
 
         if (array_is_list($options) && ! is_array(Arr::first($options))) {
             $options = collect($options)