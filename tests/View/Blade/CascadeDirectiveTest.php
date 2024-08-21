59bde82b3b1f16af040ccdf3d8aec1f2b5861068
Jack Sleight <jacksleight@gmail.com>
2024-08-21 15:33:31 +0100

    WIP

--- tests/View/Blade/CascadeDirectiveTest.php
+++ tests/View/Blade/CascadeDirectiveTest.php
@@ -43,6 +43,7 @@ class CascadeDirectiveTest extends TestCase
     public function it_throws_exception_for_missing_data()
     {
         $this->expectException(CascadeDataNotFoundException::class);
+        $this->expectExceptionMessage('Cascade data [live_preview] not found');
 
         CascadeDirective::handle([
             'live_preview',
@@ -53,11 +54,14 @@ class CascadeDirectiveTest extends TestCase
     public function it_uses_fallback_for_missing_data()
     {
         $data = CascadeDirective::handle([
+            'homepage',
             'live_preview' => false,
         ]);
 
-        $this->assertCount(1, $data);
+        $this->assertCount(2, $data);
+        $this->assertArrayHasKey('homepage', $data);
         $this->assertArrayHasKey('live_preview', $data);
+        $this->assertEquals($data['homepage'], 'http://localhost');
         $this->assertEquals($data['live_preview'], false);
     }
 }
