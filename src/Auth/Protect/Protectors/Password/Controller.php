c0cf94fda138b5ad11b6fa60275ff7001a8ce80c
Duncan McClean <duncan@duncanmcclean.com>
2024-08-28 12:39:00 +0100

    Only do this when we actually have a token.

--- src/Auth/Protect/Protectors/Password/Controller.php
+++ src/Auth/Protect/Protectors/Password/Controller.php
@@ -14,11 +14,12 @@ class Controller extends BaseController
     public function show()
     {
         $this->password = request('password');
-        $this->tokenData = session('statamic:protect:password.tokens.'.request('token'));
 
-        $site = Site::findByUrl($this->getUrl());
+        if ($this->tokenData = session('statamic:protect:password.tokens.'.request('token'))) {
+            $site = Site::findByUrl($this->getUrl());
 
-        app()->setLocale($site->shortLocale());
+            app()->setLocale($site->shortLocale());
+        }
 
         return View::make('statamic::auth.protect.password');
     }
