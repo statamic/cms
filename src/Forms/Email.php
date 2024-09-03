6a1ba7b919732ffa11a1b85cc53502477e68a1ef
Duncan McClean <duncan@duncanmcclean.com>
2024-09-03 14:47:52 +0100

    [5.x] Filter out Spacer fields from form emails (#10710)

--- src/Forms/Email.php
+++ src/Forms/Email.php
@@ -153,11 +153,11 @@ class Email extends Mailable
     protected function addData()
     {
         $augmented = $this->submission->toAugmentedArray();
-        $fields = $this->getRenderableFieldData(Arr::except($augmented, ['id', 'date', 'form']));
-
-        if (Arr::has($this->config, 'attachments')) {
-            $fields = $fields->reject(fn ($field) => in_array($field['fieldtype'], ['assets', 'files']));
-        }
+        $fields = $this->getRenderableFieldData(Arr::except($augmented, ['id', 'date', 'form']))
+            ->reject(fn ($field) => $field['fieldtype'] === 'spacer')
+            ->when(Arr::has($this->config, 'attachments'), function ($fields) {
+                return $fields->reject(fn ($field) => in_array($field['fieldtype'], ['assets', 'files']));
+            });
 
         $data = array_merge($augmented, $this->getGlobalsData(), [
             'config' => config()->all(),
