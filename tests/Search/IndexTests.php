6b63d8cadf6ba73ac9c063c730035b616ab2bed9
Alexander Jensen <alexander@jensen.dev>
2024-09-12 22:04:49 +0200

    Add test

--- tests/Search/IndexTests.php
+++ tests/Search/IndexTests.php
@@ -5,6 +5,7 @@ namespace Tests\Search;
 use Illuminate\Support\Facades\Event;
 use PHPUnit\Framework\Attributes\Test;
 use Statamic\Events\SearchQueryPerformed;
+use Statamic\Search\Index;
 
 trait IndexTests
 {
@@ -22,6 +23,28 @@ trait IndexTests
         Event::assertDispatched(SearchQueryPerformed::class, function ($event) {
             return $event->query === 'foo';
         });
+    }
+
+    #[Test]
+    public function it_respects_an_index_prefix()
+    {
+        $index = $this->getIndex();
+
+        $originalName = $index->name();
+
+        $prefix = 'foo';
+
+        $index::resolveNameUsing(function ($name) use ($prefix) {
+            return $prefix.'_'.$name;
+        });
+
+        $this->assertEquals($prefix.'_'.$originalName, $index->name());
+    }
+
+    public function tearDown(): void
+    {
+        // Reset the static state of the Index class
+        Index::resolveNameUsing(null);
     }
 
     protected function beforeSearched()
