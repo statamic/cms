60d39c86567d82608984ce9241286479a1be66c7
Jason Varga <jason@pixelfear.com>
2024-09-13 10:29:08 -0400

    wrap in condition for consistency

--- resources/js/components/entries/Listing.vue
+++ resources/js/components/entries/Listing.vue
@@ -220,7 +220,9 @@ export default {
             // When reordering, we *need* a site, since mixing them up would be awkward.
             // If we're dealing with multiple sites, it's possible the user "cleared"
             // the site filter so we'll want to fall back to the initial site.
-            this.setSiteFilter(this.currentSite || this.initialSite);
+            if (this.sites.length > 1) {
+                this.setSiteFilter(this.currentSite || this.initialSite);
+            }
 
             this.page = 1;
             this.sortColumn = 'order';
