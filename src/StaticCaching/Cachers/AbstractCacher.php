b9eb1dda72e58a23c6e4eec678720fe6f0a69b55
Damien HUTEAU <damien.huteau@gmail.com>
2024-09-25 12:14:48 +0200

    use existing getUrlsCacheKey method
    the getUrls where recreating manually the cache key wherease there already is a method to do that ...

--- src/StaticCaching/Cachers/AbstractCacher.php
+++ src/StaticCaching/Cachers/AbstractCacher.php
@@ -139,10 +139,10 @@ abstract class AbstractCacher implements Cacher
     public function getUrls($domain = null)
     {
         $domain = $domain ?: $this->getBaseUrl();
-
-        $domain = $this->makeHash($domain);
-
-        return collect($this->cache->get($this->normalizeKey($domain.'.urls'), []));
+        
+        $key = $this->getUrlsCacheKey($domain);
+        
+        return collect($this->cache->get($key, []));
     }
 
     /**
