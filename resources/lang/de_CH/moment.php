13329cdeea0b580f2be7e4b349835e381cb5fd2f
Daniel Kaufmann <hello@danielkaufmann.ch>
2024-09-26 15:49:29 +0200

    [5.x] German translations (#10849)

--- resources/lang/de_CH/moment.php
+++ resources/lang/de_CH/moment.php
@@ -1,18 +1,18 @@
 <?php
 
 return [
-    'relativeTime.M' => 'einem Monat',
-    'relativeTime.MM' => '%d Monaten',
-    'relativeTime.d' => 'einem Tag',
-    'relativeTime.dd' => '%d Tagen',
     'relativeTime.future' => 'in %s',
-    'relativeTime.h' => 'einer Stunde',
-    'relativeTime.hh' => '%d Stunden',
-    'relativeTime.m' => 'einer Minute',
-    'relativeTime.mm' => '%d Minuten',
     'relativeTime.past' => 'vor %s',
     'relativeTime.s' => 'ein paar Sekunden',
     'relativeTime.ss' => '%d Sekunden',
+    'relativeTime.m' => 'einer Minute',
+    'relativeTime.mm' => '%d Minuten',
+    'relativeTime.h' => 'einer Stunde',
+    'relativeTime.hh' => '%d Stunden',
+    'relativeTime.d' => 'einem Tag',
+    'relativeTime.dd' => '%d Tagen',
+    'relativeTime.M' => 'einem Monat',
+    'relativeTime.MM' => '%d Monaten',
     'relativeTime.y' => 'einem Jahr',
     'relativeTime.yy' => '%d Jahren',
 ];
