e4dad4a3dc6c6639952116d081aa87fb84ae18b5
Ryan Mitchell <ryan@thoughtcollective.com>
2024-10-01 08:06:20 +0100

    :beer:

--- tests/Antlers/Runtime/ModelTest.php
+++ tests/Antlers/Runtime/ModelTest.php
@@ -3,15 +3,6 @@
 namespace Tests\Antlers\Runtime;
 
 use Illuminate\Database\Eloquent\Casts\Attribute;
-use Illuminate\Support\Collection;
-use Mockery;
-use PHPUnit\Framework\Attributes\Test;
-use Statamic\Contracts\Query\Builder;
-use Statamic\Tags\Tags;
-use Statamic\View\Antlers\Language\Runtime\GlobalRuntimeState;
-use Statamic\View\Antlers\Language\Runtime\NodeProcessor;
-use Tests\Antlers\Fixtures\Addon\Modifiers\IsBuilder;
-use Tests\Antlers\Fixtures\Addon\Tags\VarTestTags as VarTest;
 use Tests\Antlers\ParserTestCase;
 
 class ModelTest extends ParserTestCase
@@ -38,7 +29,7 @@ class FakeModel extends \Illuminate\Database\Eloquent\Model
     public function fooBar(): Attribute
     {
         return Attribute::make(
-            get: fn() => 'FooBar',
+            get: fn () => 'FooBar',
         );
     }
 }
