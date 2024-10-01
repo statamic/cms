a63bb7153d612fefc8cd0fbcd16436ad92cbebff
Jack Sleight <jacksleight@gmail.com>
2024-10-01 14:28:15 +0100

    Blink cache Algolia searches

--- src/Search/Algolia/Query.php
+++ src/Search/Algolia/Query.php
@@ -2,13 +2,18 @@
 
 namespace Statamic\Search\Algolia;
 
+use Statamic\Facades\Blink;
 use Statamic\Search\QueryBuilder;
 
 class Query extends QueryBuilder
 {
     public function getSearchResults($query)
     {
-        $results = $this->index->searchUsingApi($query);
+        $key = "search-algolia-{$this->index->name()}-".md5($query);
+
+        $results = Blink::once($key, function () use ($query) {
+            return $this->index->searchUsingApi($query);
+        });
 
         return $results->map(function ($result, $i) use ($results) {
             $result['search_score'] = $results->count() - $i;
