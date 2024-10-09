0ae67a234a4017e4e3aec0b5e33f8761e153a36a
Jason Varga <jason@pixelfear.com>
2024-10-09 15:41:03 -0400

    nitpick

--- src/Modifiers/CoreModifiers.php
+++ src/Modifiers/CoreModifiers.php
@@ -2358,12 +2358,13 @@ class CoreModifiers extends Modifier
     public function sort($value, $params)
     {
         $key = Arr::get($params, 0, 'true');
-        $desc = strtolower(Arr::get($params, 1, 'asc')) == 'desc';
+        $order = strtolower(Arr::get($params, 1, 'asc'));
+        $desc = $order == 'desc';
 
         if (Compare::isQueryBuilder($value)) {
             return ($key === 'random')
                 ? $value->inRandomOrder()
-                : $value->orderBy($key, Arr::get($params, 1, 'asc'));
+                : $value->orderBy($key, $order);
         }
 
         $value = $value instanceof Collection ? $value : collect($value);
