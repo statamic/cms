statamic/cms

commit 0afece13a2d7405bc75fac29e1b458dfca4d1f90

[5.x] Paginate folders in asset browser (#10419)
Co-authored-by: duncanmcclean <duncanmcclean@users.noreply.github.com>
Co-authored-by: Jason Varga <jason@pixelfear.com>

src/Http/Controllers/CP/Assets/BrowserController.php
@@ -3,13 +3,15 @@
 namespace Statamic\Http\Controllers\CP\Assets;
 
 use Illuminate\Http\Request;
+use Illuminate\Pagination\Paginator;
 use Statamic\Contracts\Assets\AssetContainer as AssetContainerContract;
 use Statamic\Exceptions\AuthorizationException;
 use Statamic\Facades\Asset;
 use Statamic\Facades\Scope;
 use Statamic\Facades\User;
 use Statamic\Http\Controllers\CP\CpController;
-use Statamic\Http\Resources\CP\Assets\FolderAssetsCollection;
+use Statamic\Http\Resources\CP\Assets\Folder;
+use Statamic\Http\Resources\CP\Assets\FolderAsset;
 use Statamic\Http\Resources\CP\Assets\SearchedAssetsCollection;
 use Statamic\Support\Arr;
 
@@ -74,20 +76,59 @@ class BrowserController extends CpController
         $this->authorize('view', $container);
 
         $folder = $container->assetFolder($path);
+        $perPage = $request->perPage ?? config('statamic.cp.pagination_size');
+        $page = Paginator::resolveCurrentPage();
 
-        $query = $folder->queryAssets();
+        $folders = $folder->assetFolders();
+        $totalFolders = $folders->count();
+        $folders = $folders->slice(($page - 1) * $perPage, $perPage);
+        $lastFolderPage = (int) ceil($totalFolders / $perPage) ?: 1;
 
-        if ($request->sort) {
-            $query->orderBy($request->sort, $request->order ?? 'asc');
-        } else {
-            $query->orderBy($container->sortField(), $container->sortDirection());
+        $totalAssets = $folder->queryAssets()->count();
+        $totalItems = $totalAssets + $totalFolders;
+
+        if ($page >= $lastFolderPage) {
+            $query = $folder->queryAssets();
+
+            if ($request->sort) {
+                $query->orderBy($request->sort, $request->order ?? 'asc');
+            } else {
+                $query->orderBy($container->sortField(), $container->sortDirection());
+            }
+
+            $this->applyQueryScopes($query, $request->all());
+
+            $offset = $page === $lastFolderPage
+                ? 0
+                : $perPage * ($page - $lastFolderPage) - ($totalFolders % $perPage);
+
+            $assets = $query
+                ->offset($offset)
+                ->limit($perPage - $folders->count())
+                ->get();
         }
 
-        $this->applyQueryScopes($query, $request->all());
-
-        $assets = $query->paginate(request('perPage'));
-
-        return (new FolderAssetsCollection($assets))->folder($folder);
+        return [
+            'data' => [
+                'assets' => FolderAsset::collection($assets ?? collect())->resolve(),
+                'folder' => array_merge((new Folder($folder))->resolve(), [
+                    'folders' => $folders->values(),
+                ]),
+            ],
+            'links' => [
+                'asset_action' => cp_route('assets.actions.run'),
+                'folder_action' => cp_route('assets.folders.actions.run', $container->id()),
+            ],
+            'meta' => [
+                'current_page' => $page,
+                'from' => $totalItems > 0 ? ($page - 1) * $perPage + 1 : null,
+                'last_page' => $totalItems > 0 ? max((int) ceil($totalItems / $perPage), 1) : null,
+                'path' => Paginator::resolveCurrentPath(),
+                'per_page' => $perPage,
+                'to' => $totalItems > 0 ? $page * $perPage : null,
+                'total' => $totalItems,
+            ],
+        ];
     }
 
     public function search(Request $request, $container, $path = null)

resources/js/components/assets/Browser/Browser.vue
@@ -98,7 +98,7 @@
                                         </td>
                                         <td :colspan="columns.length" />
                                     </tr>
-                                    <tr v-for="(folder, i) in folders" :key="folder.path" v-if="!restrictFolderNavigation && page === 1">
+                                    <tr v-for="(folder, i) in folders" :key="folder.path" v-if="!restrictFolderNavigation">
                                         <td />
                                         <td @click="selectFolder(folder.path)">
                                             <a class="flex items-center cursor-pointer group">

src/Http/Resources/CP/Assets/FolderAssetsCollection.php
@@ -1,36 +1,0 @@
-<?php
-
-namespace Statamic\Http\Resources\CP\Assets;
-
-use Illuminate\Http\Resources\Json\ResourceCollection;
-
-class FolderAssetsCollection extends ResourceCollection
-{
-    public $collects = FolderAsset::class;
-    protected $folder;
-
-    public function folder($folder)
-    {
-        $this->folder = $folder;
-
-        return $this;
-    }
-
-    public function toArray($request)
-    {
-        return [
-            'assets' => $this->collection->values(),
-            'folder' => (new Folder($this->folder))->withChildFolders(),
-        ];
-    }
-
-    public function with($request)
-    {
-        return [
-            'links' => [
-                'asset_action' => cp_route('assets.actions.run'),
-                'folder_action' => cp_route('assets.folders.actions.run', $this->folder->container()->id()),
-            ],
-        ];
-    }
-}

commit ca8b0ccc5cf1ca40228070e7503278be040939e1

Ensure actions for specific folders are returned
The `actions` array wasn't being returned for individual folders, only for the "current" folder.

This was probably caused by the changes in #10419.

src/Http/Controllers/CP/Assets/BrowserController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Pagination\Paginator;
 use Statamic\Contracts\Assets\AssetContainer as AssetContainerContract;
 use Statamic\Exceptions\AuthorizationException;
+use Statamic\Facades\Action;
 use Statamic\Facades\Asset;
 use Statamic\Facades\Scope;
 use Statamic\Facades\User;
@@ -112,7 +113,14 @@ class BrowserController extends CpController
             'data' => [
                 'assets' => FolderAsset::collection($assets ?? collect())->resolve(),
                 'folder' => array_merge((new Folder($folder))->resolve(), [
-                    'folders' => $folders->values(),
+                    'folders' => $folders->values()->map(function ($folder) use ($container) {
+                        return array_merge($folder->toArray(), [
+                            'actions' => Action::for($folder, [
+                                'container' => $container->handle(),
+                                'folder' => $folder->path(),
+                            ]),
+                        ]);
+                    }),
                 ]),
             ],
             'links' => [