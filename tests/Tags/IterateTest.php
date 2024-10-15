9b611b363c02bc923637d88b73362a9b13c26ecf
edalzell <erin@silentz.co>
2024-10-15 10:28:51 -0700

    tests

--- tests/Tags/IterateTest.php
+++ tests/Tags/IterateTest.php
@@ -8,28 +8,42 @@ use Tests\TestCase;
 
 class IterateTest extends TestCase
 {
-    protected $data = [
-        'group_field' => [
-            'first' => 'One',
-            'second' => 'Two',
-        ],
-    ];
+    #[Test]
+    public function arrays_work()
+    {
+        $template = '{{ foreach:group_field }}{{ key }} - {{ value }} {{ /foreach:group_field }}';
+
+        $this->assertSame(
+            'first - One second - Two ',
+            $this->tag($template, [
+                'group_field' => [
+                    'first' => 'One',
+                    'second' => 'Two',
+                ],
+            ])
+        );
+    }
+
+    #[Test]
+    public function values_work()
+    {
+        $this->markTestSkipped('needs implementation');
+
+        $template = '{{ foreach:group_field }}{{ key }} - {{ value }} {{ /foreach:group_field }}';
+
+        $this->assertSame(
+            'first - One second - Two ',
+            $this->tag($template, [
+                'group_field' => [
+                    'first' => 'One',
+                    'second' => 'Two',
+                ],
+            ])
+        );
+    }
 
     private function tag($tag, $context = [])
     {
         return (string) Parse::template($tag, $context);
-    }
-
-    #[Test]
-    public function arrays_work()
-    {
-        $template = <<<'EOT'
-{{ foreach:group_field }}{{ key }} - {{ value }} {{ /foreach:group_field }}
-EOT;
-
-        $this->assertSame(
-            'first - One second - Two ',
-            $this->tag($template, $this->data)
-        );
     }
 }
