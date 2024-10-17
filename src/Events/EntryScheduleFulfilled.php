b3f48607ed2b04bb19a5688e62cbeb14c7b9ef0e
Jason Varga <jason@pixelfear.com>
2024-10-17 13:45:14 -0400

    adjust commit message

--- src/Events/EntryScheduleFulfilled.php
+++ src/Events/EntryScheduleFulfilled.php
@@ -2,12 +2,19 @@
 
 namespace Statamic\Events;
 
-class EntryScheduleFulfilled extends Event
+use Statamic\Contracts\Git\ProvidesCommitMessage;
+
+class EntryScheduleFulfilled extends Event implements ProvidesCommitMessage
 {
     public $entry;
 
     public function __construct($entry)
     {
         $this->entry = $entry;
+    }
+
+    public function commitMessage()
+    {
+        return __('Entry schedule fulfilled', [], config('statamic.git.locale'));
     }
 }
