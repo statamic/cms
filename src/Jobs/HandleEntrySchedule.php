c7750200cd9aae67217f492c6e9af7889b5da557
Jason Varga <jason@pixelfear.com>
2024-10-17 21:36:10 -0400

    add test, refactor, and have the schedule check the previous minute

--- src/Jobs/HandleEntrySchedule.php
+++ src/Jobs/HandleEntrySchedule.php
@@ -6,9 +6,9 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Collection;
+use Statamic\Entries\MinuteEntries;
 use Statamic\Events\EntryScheduleReached;
-use Statamic\Facades\Collection;
-use Statamic\Facades\Entry;
 
 class HandleEntrySchedule implements ShouldQueue
 {
@@ -19,21 +19,14 @@ class HandleEntrySchedule implements ShouldQueue
         $this->entries()->each(fn ($entry) => EntryScheduleReached::dispatch($entry));
     }
 
-    private function entries()
+    private function entries(): Collection
     {
-        $now = now()->startOfMinute();
-        $field = 'date';
+        // We want to target the PREVIOUS minute because we can be sure that any entries that
+        // were scheduled for then would now be considered published. If we were targeting
+        // the current minute and the entry has defined a time with seconds later in the
+        // same minute, it may still be considered scheduled when it gets dispatched.
+        $minute = now()->subMinute();
 
-        return Entry::query()
-            ->whereIn('collection', Collection::all()->filter->dated()->map->handle()->all())
-            ->whereDate($field, $now->format('Y-m-d'))
-            ->where(function ($query) use ($now, $field) {
-                $query->where(function ($query) use ($field, $now) {
-                    $query
-                        ->whereTime($field, '>=', $now->format('H:i').':00')
-                        ->whereTime($field, '<=', $now->format('H:i').':59');
-                })->orWhereTime($field, '=', $now->format('H:i'));
-            })
-            ->get();
+        return (new MinuteEntries($minute))();
     }
 }
