34d91b27b428bf5e624e874bb473ff98285ceaee
Alexander Jensen <alexander@jensen.dev>
2024-11-01 18:03:16 +0100

    Add LabeledValue

--- src/View/Blade/helpers.php
+++ src/View/Blade/helpers.php
@@ -2,6 +2,7 @@
 
 namespace Statamic\View\Blade;
 
+use Statamic\Fields\LabeledValue;
 use Statamic\Fields\Value;
 use Statamic\Fields\Values;
 use Statamic\Modifiers\Modify;
@@ -11,7 +12,7 @@ use Statamic\View\Antlers\Language\Runtime\GlobalRuntimeState;
 
 function value(mixed $value): mixed
 {
-    if ($value instanceof Value) {
+    if ($value instanceof Value || $value instanceof LabeledValue) {
         $value = $value->value();
     } elseif ($value instanceof Values) {
         $value = $value->all();
