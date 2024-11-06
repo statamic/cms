f01281268ae2fec73d52954dc536998db7d9c2a1
Jason Varga <jason@pixelfear.com>
2024-11-06 14:44:21 -0500

    [5.x] Adjust legacy ignition classes (#11073)

--- src/Exceptions/ComposerJsonMissingPreUpdateCmdException.php
+++ src/Exceptions/ComposerJsonMissingPreUpdateCmdException.php
@@ -3,8 +3,8 @@
 namespace Statamic\Exceptions;
 
 use Exception;
+use Spatie\ErrorSolutions\Contracts\ProvidesSolution;
 use Spatie\ErrorSolutions\Contracts\Solution;
-use Spatie\Ignition\Contracts\ProvidesSolution;
 use Statamic\Ignition\Solutions\EnableComposerUpdateScripts;
 
 class ComposerJsonMissingPreUpdateCmdException extends Exception implements ProvidesSolution
