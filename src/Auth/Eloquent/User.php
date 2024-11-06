4f53b28a2c4e9792f0009a68ff6ab60c5ec2107b
Duncan McClean <duncan@duncanmcclean.com>
2024-11-06 12:13:27 +0000

    Filter out deleted roles.

--- src/Auth/Eloquent/User.php
+++ src/Auth/Eloquent/User.php
@@ -105,7 +105,7 @@ class User extends BaseUser
         return $this->roles = $this->roles
             ?? (new Roles($this))->all()->map(function ($row) {
                 return Role::find($row->role_id);
-            })->keyBy->handle();
+            })->filter()->keyBy->handle();
     }
 
     protected function saveRoles()
