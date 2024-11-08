21cac5542ecc6dd356dd04bccfb465879cc493f2
Duncan McClean <duncan@duncanmcclean.com>
2024-11-08 12:17:48 +0000

    Add `fullyQualifiedHandle` method to `Blueprint`

--- src/Fields/Blueprint.php
+++ src/Fields/Blueprint.php
@@ -71,6 +71,19 @@ class Blueprint implements Arrayable, ArrayAccess, Augmentable, QueryableValue
         return $this->namespace;
     }
 
+    public function fullyQualifiedHandle(): string
+    {
+        $handle = $this->handle();
+
+        if ($this->namespace()) {
+            $handle = $this->isNamespaced()
+                ? $this->namespace().'::'.$handle
+                : $this->namespace().'.'.$handle;
+        }
+
+        return $handle;
+    }
+
     public function setOrder($order)
     {
         if (! is_null($order)) {
