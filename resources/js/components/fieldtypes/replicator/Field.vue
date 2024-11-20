statamic/cms

commit 9ab940da1139dd9573b845476f8401e322d2a28d

handle situations when the field reference doesnt exist - like lazy loaded meta. and make consistent in replicator field.

resources/js/components/fieldtypes/replicator/Field.vue
@@ -13,7 +13,7 @@
                 v-if="instructions && field.instructions_position !== 'below'"
                 v-html="instructions" />
 
-            <publish-field-actions v-if="mounted && fieldActions.length" :actions="fieldActions" />
+            <publish-field-actions v-if="shouldShowFieldActions" :actions="fieldActions" />
         </div>
 
         <component
@@ -81,12 +81,8 @@ export default {
 
     data() {
         return {
-            mounted: false,
+            fieldActions: [],
         }
-    },
-
-    mounted() {
-        this.mounted = true;
     },
 
     computed: {
@@ -157,11 +153,15 @@ export default {
             return prefix+'field_'+this.field.handle;
         },
 
-        fieldActions() {
-            return this.$refs.field.fieldActions;
+        shouldShowFieldActions() {
+            return this.fieldActions.length > 0;
         }
 
-    }
+    },
+
+    mounted() {
+        if (this.$refs.field) this.fieldActions = this.$refs.field.fieldActions;
+    },
 
 }
 </script>

resources/js/components/publish/Field.vue
@@ -4,6 +4,7 @@
         :config="config"
         :initial-value="value"
         :initial-meta="meta"
+        @loaded="metaLoaded"
     >
     <div slot-scope="{ meta, value, loading: loadingMeta }" :class="classes">
         <div class="field-inner">
@@ -50,7 +51,7 @@
                 v-if="instructions && config.instructions_position !== 'below'"
                 v-html="instructions" />
 
-            <publish-field-actions v-if="mounted && hasDropdown" :actions="fieldActions" />
+            <publish-field-actions v-if="shouldShowFieldActions" :actions="fieldActions" />
         </div>
 
         <loading-graphic v-if="loadingMeta" :size="16" :inline="true" />
@@ -120,12 +121,8 @@ export default {
 
     data() {
         return {
-            mounted: false,
+            fieldActions: [],
         }
-    },
-
-    mounted() {
-        this.mounted = true;
     },
 
     computed: {
@@ -173,7 +170,7 @@ export default {
                 `${this.config.component || this.config.type}-fieldtype`,,
                 this.isReadOnly ? 'read-only-field' : '',
                 this.isInsideConfigFields ? 'config-field' : `${tailwind_width_class(this.config.width)}`,
-                this.mounted && this.hasDropdown && !this.isInsideConfigFields ? 'has-dropdown' : '',
+                this.shouldShowFieldActions && !this.isInsideConfigFields ? 'has-dropdown' : '',
                 this.config.classes || '',
                 this.config.full_width_setting ? 'full-width-setting' : '',
                 { 'has-error': this.hasError || this.hasNestedError }
@@ -232,14 +229,14 @@ export default {
                 || this.syncable // Need to see the icon
         },
 
-        fieldActions() {
-            return this.$refs.field.fieldActions;
-        },
-
-        hasDropdown() {
+        shouldShowFieldActions() {
             return this.fieldActions.length > 0;
         },
 
+    },
+
+    mounted() {
+        if (this.$refs.field) this.fieldActions = this.$refs.field.fieldActions;
     },
 
     methods: {
@@ -269,6 +266,10 @@ export default {
             });
 
             return marked(text);
+        },
+
+        metaLoaded() {
+            this.$nextTick(() => this.fieldActions = this.$refs.field.fieldActions);
         }
 
     }

resources/js/components/publish/FieldMeta.vue
@@ -67,6 +67,7 @@ export default {
                 this.meta = response.data.meta;
                 this.value = response.data.value;
                 this.loading = false;
+                this.$emit('loaded');
             });
         },
 

commit f922f2dec778d9f5e4500e560641ab1e2acff082

make reactive again by putting it back into a computed. oopsie i broke it in 9ab940da

resources/js/components/fieldtypes/replicator/Field.vue
@@ -84,7 +84,7 @@ export default {
 
     data() {
         return {
-            fieldActions: [],
+            hasField: false,
         }
     },
 
@@ -158,12 +158,16 @@ export default {
 
         shouldShowFieldActions() {
             return !this.isInsideConfigFields && this.fieldActions.length > 0;
+        },
+
+        fieldActions() {
+            return this.hasField ? this.$refs.field.fieldActions : [];
         }
 
     },
 
     mounted() {
-        if (this.$refs.field) this.fieldActions = this.$refs.field.fieldActions;
+        if (this.$refs.field) this.hasField = true;
     },
 
 }

resources/js/components/publish/Field.vue
@@ -121,7 +121,7 @@ export default {
 
     data() {
         return {
-            fieldActions: [],
+            hasField: false,
         }
     },
 
@@ -233,10 +233,14 @@ export default {
             return !this.isInsideConfigFields && this.fieldActions.length > 0;
         },
 
+        fieldActions() {
+            return this.hasField ? this.$refs.field.fieldActions : [];
+        },
+
     },
 
     mounted() {
-        if (this.$refs.field) this.fieldActions = this.$refs.field.fieldActions;
+        if (this.$refs.field) this.hasField = true;
     },
 
     methods: {
@@ -269,7 +273,7 @@ export default {
         },
 
         metaLoaded() {
-            this.$nextTick(() => this.fieldActions = this.$refs.field.fieldActions);
+            this.$nextTick(() => this.hasField = true);
         }
 
     }
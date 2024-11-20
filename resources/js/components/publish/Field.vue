f922f2dec778d9f5e4500e560641ab1e2acff082
Jason Varga <jason@pixelfear.com>
2024-11-20 15:44:14 -0500

    make reactive again by putting it back into a computed. oopsie i broke it in 9ab940da

--- resources/js/components/publish/Field.vue
+++ resources/js/components/publish/Field.vue
@@ -121,7 +121,7 @@ export default {
 
     data() {
         return {
-            fieldActions: [],
+            hasField: false,
         }
     },
 
@@ -233,10 +233,14 @@ export default {
             return !this.isInsideConfigFields && this.fieldActions.length > 0;
         },
 
+        fieldActions() {
+            return this.hasField ? this.$refs.field.fieldActions : [];
+        },
+
     },
 
     mounted() {
-        if (this.$refs.field) this.fieldActions = this.$refs.field.fieldActions;
+        if (this.$refs.field) this.hasField = true;
     },
 
     methods: {
@@ -269,7 +273,7 @@ export default {
         },
 
         metaLoaded() {
-            this.$nextTick(() => this.fieldActions = this.$refs.field.fieldActions);
+            this.$nextTick(() => this.hasField = true);
         }
 
     }
