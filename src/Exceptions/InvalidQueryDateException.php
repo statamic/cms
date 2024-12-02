99035d8d515e386c1260001049b37cf06e24ec24
Ryan Mitchell <ryan@thoughtcollective.com>
2024-12-02 20:14:01 +0000

    remove copy/paste

--- src/Exceptions/InvalidQueryDateException.php
+++ src/Exceptions/InvalidQueryDateException.php
@@ -2,10 +2,6 @@
 
 namespace Statamic\Exceptions;
 
-/**
- * When an action for one type of entry is performed on another.
- * For example, trying to access the date of an entry when it is ordered numerically.
- */
 class InvalidQueryDateException extends \Exception
 {
 }
