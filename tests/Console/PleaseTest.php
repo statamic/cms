07b29e6f1d33827b72bd47887aa05f62093a24bc
Duncan McClean <duncan@duncanmcclean.com>
2024-12-03 11:04:42 +0000

    Use `stache:clear` in our tests instead.
    Asserting the output of please commands gets complicated.
    
    We need to duplicate Laravel's `PendingCommand` class to swap out the kernel, amoungst other things.

--- tests/Console/PleaseTest.php
+++ tests/Console/PleaseTest.php
@@ -4,6 +4,7 @@ namespace Tests\Console;
 
 use PHPUnit\Framework\Attributes\Test;
 use Statamic\Console\Please\Kernel;
+use Statamic\Facades\Stache;
 use Symfony\Component\Console\Exception\CommandNotFoundException;
 use Tests\TestCase;
 
@@ -19,26 +20,33 @@ class PleaseTest extends TestCase
     #[Test]
     public function it_can_run_an_artisan_command_with_statamic_prefix()
     {
-        $this->artisan('statamic:flat:camp');
+        Stache::shouldReceive('clear')->once();
+
+        $this->artisan('statamic:stache:clear');
     }
 
     #[Test]
     public function statamic_prefixed_commands_will_throw_exception_when_running_in_artisan_without_prefix()
     {
         $this->expectException(CommandNotFoundException::class);
+
         $this->artisan('flat:camp');
     }
 
     #[Test]
     public function it_can_run_a_please_command_without_statamic_prefix()
     {
-        $this->please('flat:camp');
+        Stache::shouldReceive('clear')->once();
+
+        $this->please('stache:clear');
     }
 
     #[Test]
     public function it_can_run_a_please_command_with_statamic_prefix()
     {
-        $this->please('statamic:flat:camp');
+        Stache::shouldReceive('clear')->once();
+
+        $this->please('statamic:stache:clear');
     }
 
     public function please($command, $parameters = [])
