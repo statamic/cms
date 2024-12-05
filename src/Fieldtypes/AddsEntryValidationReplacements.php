8560641676c0c47953d36a59d744ec60b8b975f5
Jason Varga <jason@pixelfear.com>
2024-12-05 15:10:30 -0500

    Add todo with explanation

--- src/Fieldtypes/AddsEntryValidationReplacements.php
+++ src/Fieldtypes/AddsEntryValidationReplacements.php
@@ -6,6 +6,13 @@ use Statamic\Entries\Entry;
 use Statamic\Fields\Field;
 use Statamic\Fields\Validator;
 
+/**
+ * TODO
+ * This allows Grid/Replicator/Bard fields to add validation replacements.
+ * It adds the same replacements that get added in EntriesController@update.
+ * Ideally those would get passed down into the field automatically somehow,
+ * so this can be considered a workaround until that happens.
+ */
 trait AddsEntryValidationReplacements
 {
     protected function addEntryValidationReplacements(Field $field, Validator $rules): Validator
