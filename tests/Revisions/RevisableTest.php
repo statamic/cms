d7cb17677ec8d95c4b8ad42b9517b2a2f581e35e
edalzell <erin@silentz.co>
2024-12-13 10:59:30 -0800

    test makeFromRevision

--- tests/Revisions/RevisableTest.php
+++ tests/Revisions/RevisableTest.php
@@ -7,12 +7,22 @@ use PHPUnit\Framework\Attributes\Test;
 use Statamic\Entries\Entry;
 use Statamic\Facades\Blueprint;
 use Statamic\Facades\Collection;
+use Statamic\Revisions\RevisionRepository;
 use Tests\PreventSavingStacheItemsToDisk;
 use Tests\TestCase;
 
 class RevisableTest extends TestCase
 {
     use PreventSavingStacheItemsToDisk;
+
+    private $repo;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        config(['statamic.revisions.path' => __DIR__.'/__fixtures__']);
+        $this->repo = (new RevisionRepository);
+    }
 
     #[Test]
     public function it_gets_non_revisable_fields()
@@ -45,5 +55,32 @@ class RevisableTest extends TestCase
             ->set('non_revisable', "don't see me");
 
         $this->assertEquals(['revisable' => 'see me'], $entry->makeRevision()->attributes()['data']);
+    }
+
+    #[Test]
+    public function it_can_make_entry_from_revision()
+    {
+        $blueprint = Blueprint::makeFromFields([
+            'revisable' => ['type' => 'text'],
+            'non_revisable' => ['type' => 'text', 'revisable' => false],
+        ]);
+
+        BlueprintRepository::shouldReceive('in')->with('collections/blog')->andReturn(collect(['blog' => $blueprint]));
+        Collection::make('blog')->save();
+
+        $entry = (new Entry)->collection('blog')->id('123');
+        $entry->set('revisable', 'override me')->set('non_revisable', "don't override me");
+
+        $revision = $this->repo->whereKey('123')->first();
+
+        $revisedEntry = $entry->makeFromRevision($revision);
+
+        $this->assertEquals(
+            collect([
+                "revisable" => "overridden",
+                "non_revisable" => "don't override me",
+            ]),
+            $entry->makeFromRevision($revision)->data()
+        );
     }
 }
