bc3645abec1a5d9242ec947960874a5668ccd955
edalzell <erin@silentz.co>
2024-12-13 10:47:34 -0800

    test revision attributes

--- tests/Revisions/RevisableTest.php
+++ tests/Revisions/RevisableTest.php
@@ -28,4 +28,22 @@ class RevisableTest extends TestCase
 
         $this->assertEquals(['non_revisable'], $entry->nonRevisableFields());
     }
+
+    #[Test]
+    public function it_sets_proper_revision_attributes()
+    {
+        $blueprint = Blueprint::makeFromFields([
+            'revisable' => ['type' => 'text'],
+            'non_revisable' => ['type' => 'text', 'revisable' => false],
+        ]);
+        BlueprintRepository::shouldReceive('in')->with('collections/blog')->andReturn(collect(['blog' => $blueprint]));
+        Collection::make('blog')->save();
+
+        $entry = (new Entry)->collection('blog')->id('123');
+        $entry
+            ->set('revisable', 'see me')
+            ->set('non_revisable', "don't see me");
+
+        $this->assertEquals(['revisable' => 'see me'], $entry->makeRevision()->attributes()['data']);
+    }
 }
