b37ae05aba20a271ef49b67e3c1440d9d385e24b
Marty Friedel <marty.dev@mity.com.au>
2024-12-17 10:31:07 +1030

    Update modifier and write tests

--- tests/Modifiers/EmbedUrlTest.php
+++ tests/Modifiers/EmbedUrlTest.php
@@ -84,7 +84,36 @@ class EmbedUrlTest extends TestCase
         );
     }
 
-    public function embed($url)
+    #[Test]
+    public function it_ensures_url_with_query_parameters_are_valid()
+    {
+        $embedUrl = 'https://www.youtube-nocookie.com/embed/s72r_wu_NVY?pp=player_params';
+
+        $this->assertEquals(
+            $embedUrl,
+            $this->embed('https://www.youtube.com/watch?v=s72r_wu_NVY&pp=player_params'),
+            'It transforms the youtube video link with additional query string params'
+        );
+        $this->assertEquals(
+            $embedUrl,
+            $this->embed('https://youtu.be/s72r_wu_NVY?pp=player_params'),
+            'It transforms shortened youtube video sharing links with additional query string params'
+        );
+
+        $this->assertEquals(
+            'https://www.youtube-nocookie.com/embed/s72r_wu_NVY?start=559&pp=player_params',
+            $this->embed('https://youtu.be/s72r_wu_NVY?t=559&pp=player_params'),
+            'It transforms the start time parameter of shortened sharing links with additional query string params'
+        );
+
+        $this->assertEquals(
+            'https://www.youtube-nocookie.com/embed/hyJ7CBs_2RQ?start=2&pp=player_params',
+            $this->embed('https://www.youtube.com/watch?v=hyJ7CBs_2RQ&t=2&pp=player_params'),
+            'It transforms the start time parameter of full youtube links with additional query string params'
+        );
+    }
+
+        public function embed($url)
     {
         return Modify::value($url)->embedUrl()->fetch();
     }
