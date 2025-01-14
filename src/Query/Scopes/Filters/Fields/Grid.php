3770e0f14aee153340e973d13745dab7712501be
Jack Sleight <jacksleight@gmail.com>
2025-01-14 14:00:14 +0000

    Add empty/not empty filters for replicator and grid

--- src/Query/Scopes/Filters/Fields/Grid.php
+++ src/Query/Scopes/Filters/Fields/Grid.php
@@ -2,7 +2,58 @@
 
 namespace Statamic\Query\Scopes\Filters\Fields;
 
-class Grid extends Textarea
+use Statamic\Support\Str;
+use Statamic\Support\Arr;
+
+class Grid extends FieldtypeFilter
 {
-    //
+    public function fieldItems()
+    {
+        return [
+            'operator' => [
+                'type' => 'select',
+                'placeholder' => __('Select Operator'),
+                'options' => [
+                    'like' => __('Contains'),
+                    'null' => __('Empty'),
+                    'not-null' => __('Not empty'),
+                ],
+                'default' => 'like',
+            ],
+            'value' => [
+                'type' => 'text',
+                'if' => [
+                    'operator' => 'like',
+                ],
+                'required' => false,
+            ],
+        ];
+    }
+
+    public function apply($query, $handle, $values)
+    {
+        $operator = $values['operator'];
+        $value = $values['value'];
+
+        if ($operator === 'like') {
+            $value = Str::ensureLeft($value, '%');
+            $value = Str::ensureRight($value, '%');
+        }
+
+        match ($operator) {
+            'null' => $query->whereNull($handle),
+            'not-null' => $query->whereNotNull($handle),
+            default => $query->where($handle, $operator, $value),
+        };
+    }
+
+    public function badge($values)
+    {
+        $field = $this->fieldtype->field()->display();
+        $operator = $values['operator'];
+        $translatedOperator = Arr::get($this->fieldItems(), "operator.options.{$operator}");
+        $value = $values['value'];
+
+        return $field.' '.strtolower($translatedOperator).' '.$value;
+    }
 }
