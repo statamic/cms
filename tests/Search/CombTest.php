6f43378ef4eb50db5e85d91b7ec5eff0d27edcdb
Jason Varga <jason@pixelfear.com>
2025-01-22 11:47:10 -0500

    Add test for non-arrays which worked before this pr

--- tests/Search/CombTest.php
+++ tests/Search/CombTest.php
@@ -257,6 +257,20 @@ EOT;
     public function it_filters_out_results_with_disallowed_words()
     {
         $comb = new Comb([
+            ['title' => 'Pizza', 'ingredients' => 'Tomato, Cheese, Bread'],
+            ['title' => 'Tomato Soup', 'ingredients' => 'Tomato, Water, Salt'],
+            ['title' => 'Chicken & Sweetcorn Soup', 'ingredients' => 'Chicken, Sweetcorn, Water'],
+        ]);
+
+        $results = $comb->lookUp('soup -tomato');
+
+        $this->assertEquals(['Chicken & Sweetcorn Soup'], collect($results['data'] ?? [])->pluck('data.title')->all());
+    }
+
+    #[Test]
+    public function it_filters_out_results_with_disallowed_words_where_results_are_arrays()
+    {
+        $comb = new Comb([
             ['title' => 'Pizza', 'ingredients' => ['Tomato', 'Cheese', 'Bread']],
             ['title' => 'Tomato Soup', 'ingredients' => ['Tomato', 'Water', 'Salt']],
             ['title' => 'Chicken & Sweetcorn Soup', 'ingredients' => ['Chicken', 'Sweetcorn', 'Water']],
