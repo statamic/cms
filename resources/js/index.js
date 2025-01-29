77256eeb7839116bb3ff1557af663a0b1ecdc694
Jason Varga <jason@pixelfear.com>
2025-01-29 13:18:14 -0500

    Expose Vue on window so addons etc can use the same instance

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,4 +1,5 @@
 import Statamic from './bootstrap/statamic.js';
+import * as Vue from 'vue';
 import Alpine from 'alpinejs'
 import * as Globals from './bootstrap/globals'
 import { default as underscore } from 'underscore'
@@ -10,6 +11,7 @@ import.meta.glob(['../img/**']);
 let global_functions = Object.keys(Globals)
 global_functions.forEach(fnName => { window[fnName] = Globals[fnName] })
 
+window.Vue = Vue;
 window.Cookies = Cookies;
 window.Alpine = Alpine
 window.Statamic = Statamic;
