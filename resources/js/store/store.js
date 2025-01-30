c307907cf06681fa70757a2c27f8f17d7dee5d01
Jason Varga <jason@pixelfear.com>
2025-01-30 16:22:25 -0500

    Unused

--- resources/js/store/store.js
+++ resources/js/store/store.js
@@ -1,11 +1,9 @@
 import { createStore } from 'vuex';
 import statamic from './statamic.js'
-// import portals from './portals.ts'
 
 export const store = createStore({
     modules: {
         statamic,
-        // portals,
         publish: {
             namespaced: true
         }
