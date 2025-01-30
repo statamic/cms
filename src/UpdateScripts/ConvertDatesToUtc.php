d8452be33115bf49ab8180d4bf0cb29436069959
Duncan McClean <duncan@duncanmcclean.com>
2025-01-30 14:51:49 +0000

    Support nested fields in UTC update script

--- src/UpdateScripts/ConvertDatesToUtc.php
+++ src/UpdateScripts/ConvertDatesToUtc.php
@@ -5,8 +5,10 @@ namespace Statamic\UpdateScripts;
 use Carbon\Carbon;
 use Statamic\Contracts\Entries\Entry as EntryContract;
 use Statamic\Fields\Field;
+use Statamic\Fields\Fields;
 use Statamic\Fieldtypes\Date as DateFieldtype;
 use Statamic\Listeners\Concerns\GetsItemsContainingData;
+use Statamic\Support\Arr;
 
 class ConvertDatesToUtc extends UpdateScript
 {
@@ -24,44 +26,152 @@ class ConvertDatesToUtc extends UpdateScript
 
         $this
             ->getItemsContainingData()
-            ->filter(fn ($item) => $item instanceof EntryContract) // todo: code needs abstracting for all types of content
+            // todo: code needs abstracting for all types of content
+            ->filter(fn ($item) => $item instanceof EntryContract)
             ->each(function ($item) {
-                $dateFields = $item->blueprint()->fields()->all()->filter(fn ($field) => $field->type() === 'date');
+                /** @var Fields $fields */
+                $fields = $item->blueprint()->fields();
 
-                if ($dateFields->isEmpty()) {
-                    return;
-                }
-
-                $dateFields->each(function (Field $field) use ($item) {
-                    if (
-                        $item instanceof EntryContract
-                        && $item->collection()->dated()
-                        && $field->handle() === 'date'
-                    ) {
-                        $format = $this->formatForEntryDateField($field);
-
-                        $item->date($item->date()->setTimezone('UTC')->format($format));
-
-                        return;
-                    }
-
-                    $value = $item->get($field->handle());
-
-                    if (! $value) {
-                        return;
-                    }
-
-                    $value = $field->get('mode') === 'range'
-                        ? $this->processRange($value, $field)
-                        : $this->processSingle($value, $field);
-
-                    $item->set($field->handle(), $value);
-                });
+                $this->recursivelyUpdateFields($item, $fields);
 
                 if ($item->isDirty()) {
                     $item->saveQuietly();
                 }
             });
+    }
+
+    private function recursivelyUpdateFields($item, Fields $fields, ?string $dottedPrefix = null): void
+    {
+        $this
+            ->updateDateFields($item, $fields, $dottedPrefix)
+            ->updateDateFieldsInGroups($item, $fields, $dottedPrefix)
+            ->updateDateFieldsInGrids($item, $fields, $dottedPrefix)
+            ->updateDateFieldsInReplicators($item, $fields, $dottedPrefix)
+            ->updateDateFieldsInBard($item, $fields, $dottedPrefix);
+    }
+
+    private function updateDateFields($item, Fields $fields, ?string $dottedPrefix = null): self
+    {
+        $fields->all()
+            ->filter(fn (Field $field) => $field->type() === 'date')
+            ->each(function (Field $field) use ($item, $dottedPrefix) {
+                if (
+                    $item instanceof EntryContract
+                    && $item->collection()->dated()
+                    && empty($dottedPrefix)
+                    && $field->handle() === 'date'
+                ) {
+                    $format = $this->formatForEntryDateField($field);
+
+                    $item->date($item->date()->setTimezone('UTC')->format($format));
+
+                    return;
+                }
+
+                $data = $item->data()->all();
+
+                $dottedKey = $dottedPrefix.$field->handle();
+
+                if (! Arr::has($data, $dottedKey)) {
+                    return;
+                }
+
+                $value = Arr::get($data, $dottedKey);
+
+                $value = $field->get('mode') === 'range'
+                    ? $this->processRange($value, $field)
+                    : $this->processSingle($value, $field);
+
+                Arr::set($data, $dottedKey, $value);
+
+                $item->data($data);
+            });
+
+        return $this;
+    }
+
+    private function updateDateFieldsInGroups($item, Fields $fields, ?string $dottedPrefix = null): self
+    {
+        $fields->all()
+            ->filter(fn (Field $field) => $field->type() === 'group')
+            ->each(function (Field $field) use ($item, $dottedPrefix) {
+                $dottedKey = "{$dottedPrefix}{$field->handle()}";
+
+                $this->updateDateFields($item, $field->fieldtype()->fields(), $dottedKey.'.');
+            });
+
+        return $this;
+    }
+
+    private function updateDateFieldsInGrids($item, Fields $fields, ?string $dottedPrefix = null): self
+    {
+        $fields->all()
+            ->filter(fn (Field $field) => $field->type() === 'grid')
+            ->each(function (Field $field) use ($item, $dottedPrefix) {
+                $data = $item->data();
+                $dottedKey = "{$dottedPrefix}{$field->handle()}";
+
+                $rows = Arr::get($data, $dottedKey, []);
+
+                collect($rows)->each(function ($set, $setKey) use ($item, $dottedKey, $field) {
+                    $dottedPrefix = "{$dottedKey}.{$setKey}.";
+                    $fields = Arr::get($field->config(), 'fields');
+
+                    if ($fields) {
+                        $this->recursivelyUpdateFields($item, new Fields($fields), $dottedPrefix);
+                    }
+                });
+            });
+
+        return $this;
+    }
+
+    private function updateDateFieldsInReplicators($item, Fields $fields, ?string $dottedPrefix = null): self
+    {
+        $fields->all()
+            ->filter(fn (Field $field) => $field->type() === 'replicator')
+            ->each(function (Field $field) use ($item, $dottedPrefix) {
+                $data = $item->data();
+                $dottedKey = "{$dottedPrefix}{$field->handle()}";
+
+                $sets = Arr::get($data, $dottedKey);
+
+                collect($sets)->each(function ($set, $setKey) use ($item, $dottedKey, $field) {
+                    $dottedPrefix = "{$dottedKey}.{$setKey}.";
+                    $setHandle = Arr::get($set, 'type');
+                    $fields = Arr::get($field->fieldtype()->flattenedSetsConfig(), "{$setHandle}.fields");
+
+                    if ($setHandle && $fields) {
+                        $this->recursivelyUpdateFields($item, new Fields($fields), $dottedPrefix);
+                    }
+                });
+            });
+
+        return $this;
+    }
+
+    private function updateDateFieldsInBard($item, Fields $fields, ?string $dottedPrefix = null): self
+    {
+        $fields->all()
+            ->filter(fn (Field $field) => $field->type() === 'bard')
+            ->each(function (Field $field) use ($item, $dottedPrefix) {
+                $data = $item->data();
+                $dottedKey = "{$dottedPrefix}{$field->handle()}";
+
+                $sets = Arr::get($data, $dottedKey);
+
+                collect($sets)->each(function ($set, $setKey) use ($item, $dottedKey, $field) {
+                    $dottedPrefix = "{$dottedKey}.{$setKey}.attrs.values.";
+                    $setHandle = Arr::get($set, 'attrs.values.type');
+                    $fields = Arr::get($field->fieldtype()->flattenedSetsConfig(), "{$setHandle}.fields");
+
+                    if ($setHandle && $fields) {
+                        $this->recursivelyUpdateFields($item, new Fields($fields), $dottedPrefix);
+                    }
+                });
+            });
+
+        return $this;
     }
 
     private function processRange(array $value, Field $field): array
--- tests/UpdateScripts/ConvertDatesToUtcTest.php
+++ tests/UpdateScripts/ConvertDatesToUtcTest.php
@@ -42,6 +42,34 @@ class ConvertDatesToUtcTest extends TestCase
                 ['handle' => 'date_with_time_and_custom_format', 'field' => ['type' => 'date', 'time_enabled' => true, 'format' => 'U']],
                 ['handle' => 'date_without_time', 'field' => ['type' => 'date']],
                 ['handle' => 'date_range', 'field' => ['type' => 'date', 'mode' => 'range']],
+                ['handle' => 'group_field', 'field' => ['type' => 'group', 'fields' => [
+                    ['handle' => 'date_and_time', 'field' => ['type' => 'date', 'time_enabled' => true]],
+                    ['handle' => 'date_range', 'field' => ['type' => 'date', 'mode' => 'range']],
+                ]]],
+                ['handle' => 'grid_field', 'field' => ['type' => 'grid', 'fields' => [
+                    ['handle' => 'date_and_time', 'field' => ['type' => 'date', 'time_enabled' => true]],
+                    ['handle' => 'date_range', 'field' => ['type' => 'date', 'mode' => 'range']],
+                    ['handle' => 'second_grid_field', 'field' => ['type' => 'grid', 'fields' => [
+                        ['handle' => 'date_and_time', 'field' => ['type' => 'date', 'time_enabled' => true]],
+                        ['handle' => 'date_range', 'field' => ['type' => 'date', 'mode' => 'range']],
+                    ]]],
+                ]]],
+                ['handle' => 'replicator_field', 'field' => ['type' => 'replicator', 'sets' => [
+                    'set_group' => ['sets' => [
+                        'set_name' => ['fields' => [
+                            ['handle' => 'date_and_time', 'field' => ['type' => 'date', 'time_enabled' => true]],
+                            ['handle' => 'date_range', 'field' => ['type' => 'date', 'mode' => 'range']],
+                        ]],
+                    ]],
+                ]]],
+                ['handle' => 'bard_field', 'field' => ['type' => 'bard', 'sets' => [
+                    'set_group' => ['sets' => [
+                        'set_name' => ['fields' => [
+                            ['handle' => 'date_and_time', 'field' => ['type' => 'date', 'time_enabled' => true]],
+                            ['handle' => 'date_range', 'field' => ['type' => 'date', 'mode' => 'range']],
+                        ]],
+                    ]],
+                ]]],
             ],
         ])->save();
 
@@ -54,6 +82,22 @@ class ConvertDatesToUtcTest extends TestCase
                 'date_with_time_and_custom_format' => 1735689600,
                 'date_without_time' => '2025-01-01',
                 'date_range' => ['start' => '2025-01-01', 'end' => '2025-01-07'],
+                'group_field' => ['date_and_time' => '2025-01-01 12:00', 'date_range' => ['start' => '2025-01-01', 'end' => '2025-01-07']],
+                'grid_field' => [
+                    ['date_and_time' => '2025-01-01 12:00', 'date_range' => ['start' => '2025-01-01', 'end' => '2025-01-07'], 'second_grid_field' => [
+                        ['date_and_time' => '2025-01-01 12:00', 'date_range' => ['start' => '2025-01-01', 'end' => '2025-01-07']],
+                    ]],
+                ],
+                'replicator_field' => [
+                    ['type' => 'set_name', 'date_and_time' => '2025-01-01 12:00', 'date_range' => ['start' => '2025-01-01', 'end' => '2025-01-07']],
+                ],
+                'bard_field' => [
+                    ['type' => 'set', 'attrs' => ['id' => 'abc', 'values' => [
+                        'type' => 'set_name',
+                        'date_and_time' => '2025-01-01 12:00',
+                        'date_range' => ['start' => '2025-01-01', 'end' => '2025-01-07'],
+                    ]]],
+                ],
             ]);
 
         $entry->save();
@@ -67,10 +111,32 @@ class ConvertDatesToUtcTest extends TestCase
         $this->assertEquals('2025-01-01 17:00:15', $entry->get('date_with_time_and_seconds'));
         $this->assertEquals(1735689600, $entry->get('date_with_time_and_custom_format'));
         $this->assertEquals('2025-01-01', $entry->get('date_without_time'));
-        $this->assertEquals(['start' => '2025-01-01', 'end' => '2025-01-07'], $entry->get('date_range'));
+
+        $this->assertEquals([
+            'date_and_time' => '2025-01-01 17:00',
+            'date_range' => ['start' => '2025-01-01', 'end' => '2025-01-07'],
+        ], $entry->get('group_field'));
+
+        $this->assertEquals([
+            ['date_and_time' => '2025-01-01 17:00', 'date_range' => ['start' => '2025-01-01', 'end' => '2025-01-07'], 'second_grid_field' => [
+                ['date_and_time' => '2025-01-01 17:00', 'date_range' => ['start' => '2025-01-01', 'end' => '2025-01-07']],
+            ]],
+        ], $entry->get('grid_field'));
+
+        $this->assertEquals([
+            ['type' => 'set_name', 'date_and_time' => '2025-01-01 17:00', 'date_range' => ['start' => '2025-01-01', 'end' => '2025-01-07']],
+        ], $entry->get('replicator_field'));
+
+        $this->assertEquals([
+            ['type' => 'set', 'attrs' => ['id' => 'abc', 'values' => [
+                'type' => 'set_name',
+                'date_and_time' => '2025-01-01 17:00',
+                'date_range' => ['start' => '2025-01-01', 'end' => '2025-01-07'],
+            ]]],
+        ], $entry->get('bard_field'));
     }
 
-    // TODO: Handle date fields in Bards/Replicators/Grids/Groups
+    // todo: ensure fields inside fieldsets are updated
     // TODO: Refactor test to use data provider
     // TODO: Add tests for other content types (terms, globals, users)
 }
