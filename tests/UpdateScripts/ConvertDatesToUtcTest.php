2b61ad84178ff4ff2b8b7cefec9417f290aad85b
Duncan McClean <duncan@duncanmcclean.com>
2025-01-30 19:09:35 +0000

    Use data provider in tests

--- tests/UpdateScripts/ConvertDatesToUtcTest.php
+++ tests/UpdateScripts/ConvertDatesToUtcTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\UpdateScripts;
 
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 use Statamic\Facades\Collection;
 use Statamic\Facades\Entry;
@@ -14,6 +15,15 @@ use Tests\UpdateScripts\Concerns\RunsUpdateScripts;
 class ConvertDatesToUtcTest extends TestCase
 {
     use PreventSavingStacheItemsToDisk, RunsUpdateScripts;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        Fieldset::make('date_fieldset')->setContents(['fields' => [
+            ['handle' => 'fieldset_date', 'field' => ['type' => 'date', 'time_enabled' => true]],
+        ]])->save();
+    }
 
     #[Test]
     public function it_is_registered()
@@ -28,37 +38,89 @@ class ConvertDatesToUtcTest extends TestCase
     }
 
     #[Test]
-    public function it_can_convert_date_fields_in_entries()
+    #[DataProvider('dateFieldsProvider')]
+    public function it_can_convert_date_fields_in_entries(string $fieldHandle, array $field, $original, $expected)
     {
         config()->set('app.timezone', 'America/New_York'); // -05:00
         date_default_timezone_set('America/New_York');
 
-        $collection = tap(Collection::make('articles')->dated(true))->save();
+        $collection = tap(Collection::make('articles'))->save();
 
-        Fieldset::make('date_fieldset')->setContents(['fields' => [
-            ['handle' => 'fieldset_date', 'field' => ['type' => 'date', 'time_enabled' => true]],
-        ]])->save();
+        $collection->entryBlueprint()->setContents(['fields' => [$field]])->save();
 
-        $collection->entryBlueprint()->setContents([
-            'fields' => [
-                ['handle' => 'date', 'field' => ['type' => 'date', 'time_enabled' => true]],
-                ['handle' => 'date_with_time', 'field' => ['type' => 'date', 'time_enabled' => true]],
-                ['handle' => 'date_with_time_and_seconds', 'field' => ['type' => 'date', 'time_enabled' => true, 'time_seconds_enabled' => true]],
-                ['handle' => 'date_with_time_and_custom_format', 'field' => ['type' => 'date', 'time_enabled' => true, 'format' => 'U']],
-                ['handle' => 'date_without_time', 'field' => ['type' => 'date']],
-                ['handle' => 'date_range', 'field' => ['type' => 'date', 'mode' => 'range']],
+        $entry = Entry::make()->collection('articles')->data([$fieldHandle => $original]);
+        $entry->save();
+
+        $this->runUpdateScript(ConvertDatesToUtc::class);
+
+        $this->assertEquals($expected, $entry->fresh()->get($fieldHandle));
+    }
+
+    public static function dateFieldsProvider(): array
+    {
+        return [
+            'Date field' => [
+                'date_field',
+                ['handle' => 'date_field', 'field' => ['type' => 'date']],
+                '2025-01-01',
+                '2025-01-01',
+            ],
+            'Date field with time enabled' => [
+                'date_field',
+                ['handle' => 'date_field', 'field' => ['type' => 'date', 'time_enabled' => true]],
+                '2025-01-01 12:00',
+                '2025-01-01 17:00',
+            ],
+            'Date field with time and seconds enabled' => [
+                'date_field',
+                ['handle' => 'date_field', 'field' => ['type' => 'date', 'time_enabled' => true, 'time_seconds_enabled' => true]],
+                '2025-01-01 12:00:15',
+                '2025-01-01 17:00:15',
+            ],
+            'Date field with time enabled, and a custom format' => [
+                'date_field',
+                ['handle' => 'date_field', 'field' => ['type' => 'date', 'time_enabled' => true, 'format' => 'U']],
+                1735689600,
+                1735689600,
+            ],
+            'Date range' => [
+                'date_field',
+                ['handle' => 'date_field', 'field' => ['type' => 'date', 'mode' => 'range']],
+                ['start' => '2025-01-01', 'end' => '2025-01-07'],
+                ['start' => '2025-01-01', 'end' => '2025-01-07'],
+            ],
+            'Imported date field' => [
+                'fieldset_date',
+                ['import' => 'date_fieldset'],
+                '2025-01-01 12:00',
+                '2025-01-01 17:00',
+            ],
+            'Group field with nested date fields' => [
+                'group_field',
                 ['handle' => 'group_field', 'field' => ['type' => 'group', 'fields' => [
                     ['handle' => 'date_and_time', 'field' => ['type' => 'date', 'time_enabled' => true]],
                     ['handle' => 'date_range', 'field' => ['type' => 'date', 'mode' => 'range']],
                 ]]],
+                [
+                    'date_and_time' => '2025-01-01 12:00',
+                    'date_range' => ['start' => '2025-01-01', 'end' => '2025-01-07'],
+                ],
+                [
+                    'date_and_time' => '2025-01-01 17:00',
+                    'date_range' => ['start' => '2025-01-01', 'end' => '2025-01-07'],
+                ],
+            ],
+            'Grid field with nested date fields' => [
+                'grid_field',
                 ['handle' => 'grid_field', 'field' => ['type' => 'grid', 'fields' => [
                     ['handle' => 'date_and_time', 'field' => ['type' => 'date', 'time_enabled' => true]],
                     ['handle' => 'date_range', 'field' => ['type' => 'date', 'mode' => 'range']],
-                    ['handle' => 'second_grid_field', 'field' => ['type' => 'grid', 'fields' => [
-                        ['handle' => 'date_and_time', 'field' => ['type' => 'date', 'time_enabled' => true]],
-                        ['handle' => 'date_range', 'field' => ['type' => 'date', 'mode' => 'range']],
-                    ]]],
                 ]]],
+                [['date_and_time' => '2025-01-01 12:00', 'date_range' => ['start' => '2025-01-01', 'end' => '2025-01-07']]],
+                [['date_and_time' => '2025-01-01 17:00', 'date_range' => ['start' => '2025-01-01', 'end' => '2025-01-07']]],
+            ],
+            'Replicator field with nested date fields' => [
+                'replicator_field',
                 ['handle' => 'replicator_field', 'field' => ['type' => 'replicator', 'sets' => [
                     'set_group' => ['sets' => [
                         'set_name' => ['fields' => [
@@ -67,6 +129,11 @@ class ConvertDatesToUtcTest extends TestCase
                         ]],
                     ]],
                 ]]],
+                [['type' => 'set_name', 'date_and_time' => '2025-01-01 12:00', 'date_range' => ['start' => '2025-01-01', 'end' => '2025-01-07']]],
+                [['type' => 'set_name', 'date_and_time' => '2025-01-01 17:00', 'date_range' => ['start' => '2025-01-01', 'end' => '2025-01-07']]],
+            ],
+            'Bard field with nested date fields' => [
+                'bard_field',
                 ['handle' => 'bard_field', 'field' => ['type' => 'bard', 'sets' => [
                     'set_group' => ['sets' => [
                         'set_name' => ['fields' => [
@@ -75,82 +142,34 @@ class ConvertDatesToUtcTest extends TestCase
                         ]],
                     ]],
                 ]]],
-                ['import' => 'date_fieldset'],
-                ['import' => 'date_fieldset', 'prefix' => 'prefixed_'],
-                ['handle' => 'date_fieldset_single_field', 'field' => 'date_fieldset.fieldset_date'],
+                [['type' => 'set', 'attrs' => ['id' => 'abc', 'values' => [
+                    'type' => 'set_name',
+                    'date_and_time' => '2025-01-01 12:00',
+                    'date_range' => ['start' => '2025-01-01', 'end' => '2025-01-07'],
+                ]]]],
+                [['type' => 'set', 'attrs' => ['id' => 'abc', 'values' => [
+                    'type' => 'set_name',
+                    'date_and_time' => '2025-01-01 17:00',
+                    'date_range' => ['start' => '2025-01-01', 'end' => '2025-01-07'],
+                ]]]],
             ],
-        ])->save();
-
-        $entry = Entry::make()
-            ->collection('articles')
-            ->date('2025-01-01-1200')
-            ->data([
-                'date_with_time' => '2025-01-01 12:00',
-                'date_with_time_and_seconds' => '2025-01-01 12:00:15',
-                'date_with_time_and_custom_format' => 1735689600,
-                'date_without_time' => '2025-01-01',
-                'date_range' => ['start' => '2025-01-01', 'end' => '2025-01-07'],
-                'group_field' => ['date_and_time' => '2025-01-01 12:00', 'date_range' => ['start' => '2025-01-01', 'end' => '2025-01-07']],
-                'grid_field' => [
-                    ['date_and_time' => '2025-01-01 12:00', 'date_range' => ['start' => '2025-01-01', 'end' => '2025-01-07'], 'second_grid_field' => [
-                        ['date_and_time' => '2025-01-01 12:00', 'date_range' => ['start' => '2025-01-01', 'end' => '2025-01-07']],
-                    ]],
-                ],
-                'replicator_field' => [
-                    ['type' => 'set_name', 'date_and_time' => '2025-01-01 12:00', 'date_range' => ['start' => '2025-01-01', 'end' => '2025-01-07']],
-                ],
-                'bard_field' => [
-                    ['type' => 'set', 'attrs' => ['id' => 'abc', 'values' => [
-                        'type' => 'set_name',
-                        'date_and_time' => '2025-01-01 12:00',
-                        'date_range' => ['start' => '2025-01-01', 'end' => '2025-01-07'],
+            'Deeply nested date fields' => [
+                'deeply_nested_date_fields',
+                ['handle' => 'deeply_nested_date_fields', 'field' => ['type' => 'grid', 'fields' => [
+                    ['handle' => 'nested_group', 'field' => ['type' => 'group', 'fields' => [
+                        ['handle' => 'date_and_time', 'field' => ['type' => 'date', 'time_enabled' => true]],
+                        ['handle' => 'date_range', 'field' => ['type' => 'date', 'mode' => 'range']],
                     ]]],
-                ],
-                'fieldset_date' => '2025-01-01 12:00',
-                'prefixed_fieldset_date' => '2025-01-01 12:00',
-                'date_fieldset_single_field' => '2025-01-01 12:00',
-            ]);
-
-        $entry->save();
-
-        $this->runUpdateScript(ConvertDatesToUtc::class);
-
-        $entry->fresh();
-
-        $this->assertEquals('2025-01-01 17:00', $entry->date()->format('Y-m-d H:i'));
-        $this->assertEquals('2025-01-01 17:00', $entry->get('date_with_time'));
-        $this->assertEquals('2025-01-01 17:00:15', $entry->get('date_with_time_and_seconds'));
-        $this->assertEquals(1735689600, $entry->get('date_with_time_and_custom_format'));
-        $this->assertEquals('2025-01-01', $entry->get('date_without_time'));
-
-        $this->assertEquals([
-            'date_and_time' => '2025-01-01 17:00',
-            'date_range' => ['start' => '2025-01-01', 'end' => '2025-01-07'],
-        ], $entry->get('group_field'));
-
-        $this->assertEquals([
-            ['date_and_time' => '2025-01-01 17:00', 'date_range' => ['start' => '2025-01-01', 'end' => '2025-01-07'], 'second_grid_field' => [
-                ['date_and_time' => '2025-01-01 17:00', 'date_range' => ['start' => '2025-01-01', 'end' => '2025-01-07']],
-            ]],
-        ], $entry->get('grid_field'));
-
-        $this->assertEquals([
-            ['type' => 'set_name', 'date_and_time' => '2025-01-01 17:00', 'date_range' => ['start' => '2025-01-01', 'end' => '2025-01-07']],
-        ], $entry->get('replicator_field'));
-
-        $this->assertEquals([
-            ['type' => 'set', 'attrs' => ['id' => 'abc', 'values' => [
-                'type' => 'set_name',
-                'date_and_time' => '2025-01-01 17:00',
-                'date_range' => ['start' => '2025-01-01', 'end' => '2025-01-07'],
-            ]]],
-        ], $entry->get('bard_field'));
-
-        $this->assertEquals('2025-01-01 17:00', $entry->get('fieldset_date'));
-        $this->assertEquals('2025-01-01 17:00', $entry->get('prefixed_fieldset_date'));
-        $this->assertEquals('2025-01-01 17:00', $entry->get('date_fieldset_single_field'));
+                ]]],
+                [['nested_group' => [
+                    'date_and_time' => '2025-01-01 12:00', 'date_range' => ['start' => '2025-01-01', 'end' => '2025-01-07'],
+                ]]],
+                [['nested_group' => [
+                    'date_and_time' => '2025-01-01 17:00', 'date_range' => ['start' => '2025-01-01', 'end' => '2025-01-07'],
+                ]]],
+            ],
+        ];
     }
 
-    // TODO: Refactor test to use data provider
     // TODO: Add tests for other content types (terms, globals, users)
 }
