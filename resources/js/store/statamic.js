1233821bf24112dacc6099d904b72ec92fc2ac37
Jason Varga <jason@pixelfear.com>
2025-02-07 14:01:59 -0500

    Remove unused composer in vuex. It was removed in #7703

--- resources/js/store/statamic.js
+++ resources/js/store/statamic.js
@@ -2,15 +2,10 @@ export default {
     namespaced: true,
 
     state: {
-        composer: {},
         conditions: {},
     },
 
     mutations: {
-        composer(state, composer) {
-            state.composer = composer;
-        },
-
         condition(state, payload) {
             state.conditions[payload.name] = payload.condition;
         },
