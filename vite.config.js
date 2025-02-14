6957b0e7e5873a8b1319ae875a572d019651d660
Jason Varga <jason@pixelfear.com>
2025-02-14 16:39:25 -0500

    [6.x] Add rollup bundle visualizer (#11456)

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import inject from '@rollup/plugin-inject';
+import { visualizer } from 'rollup-plugin-visualizer';
 import svgLoader from 'vite-svg-loader';
 import path from 'path';
 
@@ -34,6 +35,13 @@ export default defineConfig(({ mode }) => {
         },
         optimizeDeps: {
             include: ['vue'],
+        },
+        build: {
+            rollupOptions: {
+                output: {
+                    plugins: [visualizer({ filename: 'bundle-stats.html' })],
+                },
+            },
         },
         test: {
             environment: 'jsdom',
