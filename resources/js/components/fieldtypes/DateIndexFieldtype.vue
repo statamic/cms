c5e9103195910986087e92b8206e58cae2cfb94c
Duncan McClean <duncan@duncanmcclean.com>
2025-02-19 08:13:33 -0500

    Prettier.

--- resources/js/components/fieldtypes/DateIndexFieldtype.vue
+++ resources/js/components/fieldtypes/DateIndexFieldtype.vue
@@ -16,13 +16,19 @@ export default {
 
             if (this.value.mode === 'range') {
                 return (
-                    this.$moment(this.value.start.date + 'T' + (this.value.start.time || '00:00:00') + 'Z').format(this.value.display_format) +
+                    this.$moment(this.value.start.date + 'T' + (this.value.start.time || '00:00:00') + 'Z').format(
+                        this.value.display_format,
+                    ) +
                     ' – ' +
-                    this.$moment(this.value.end.date + 'T' + (this.value.end.time || '00:00:00') + 'Z').format(this.value.display_format)
+                    this.$moment(this.value.end.date + 'T' + (this.value.end.time || '00:00:00') + 'Z').format(
+                        this.value.display_format,
+                    )
                 );
             }
 
-            return this.$moment(this.value.date + 'T' + (this.value.time || '00:00:00') + 'Z').format(this.value.display_format);
+            return this.$moment(this.value.date + 'T' + (this.value.time || '00:00:00') + 'Z').format(
+                this.value.display_format,
+            );
         },
     },
 };
