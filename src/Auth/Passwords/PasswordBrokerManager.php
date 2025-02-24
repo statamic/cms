c203d2b0e6262660953a2a073021b13aaf5a15a6
Jason Varga <jason@pixelfear.com>
2025-02-24 16:02:08 -0500

    Update token repository to be in line with the parent class

--- src/Auth/Passwords/PasswordBrokerManager.php
+++ src/Auth/Passwords/PasswordBrokerManager.php
@@ -20,7 +20,7 @@ class PasswordBrokerManager extends BaseManager
             $this->app['hash'],
             $config['table'],
             $key,
-            $config['expire'],
+            ($config['expire'] ?? 60) * 60,
             $config['throttle'] ?? 0
         );
     }
--- src/Auth/Passwords/TokenRepository.php
+++ src/Auth/Passwords/TokenRepository.php
@@ -11,20 +11,16 @@ use Statamic\Facades\YAML;
 
 class TokenRepository extends DatabaseTokenRepository
 {
-    protected $files;
-    protected $hasher;
-    protected $hashKey;
-    protected $expires;
     protected $path;
 
-    public function __construct(Filesystem $files, HasherContract $hasher, $table, $hashKey, $expires = 60, $throttle = 60)
-    {
-        $this->files = $files;
-        $this->hasher = $hasher;
-        $this->hashKey = $hashKey;
-        $this->expires = $expires * 60;
-        $this->throttle = $throttle;
-
+    public function __construct(
+        protected Filesystem $files,
+        protected HasherContract $hasher,
+        protected string $table,
+        protected string $hashKey,
+        protected int $expires = 3600,
+        protected int $throttle = 60
+    ) {
         $this->path = storage_path("statamic/password_resets/$table.yaml");
     }
 
