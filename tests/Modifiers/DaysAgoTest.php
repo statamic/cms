c1b1fe0f0a9c92eafce46d70700d977a8ae8a54e
Duncan McClean <duncan@duncanmcclean.com>
2025-02-25 12:04:11 +0000

    Break the "ago" date modifiers

--- tests/Modifiers/DaysAgoTest.php
+++ tests/Modifiers/DaysAgoTest.php
@@ -22,18 +22,13 @@ class DaysAgoTest extends TestCase
     public static function dateProvider()
     {
         return [
-            // Carbon 3 would return floats but to preserve backwards compatibility
-            // with Carbon 2 we will cast to integers.
-            'same time' => ['2025-02-20 00:00', 0],
-            'less than a day ago' => ['2025-02-19 11:00', 0],
-            '1 day ago' => ['2025-02-19 00:00', 1],
-            '2 days ago' => ['2025-02-18 00:00', 2],
-
-            // Future dates would return negative numbers in Carbon 3 but to preserve
-            // backwards compatibility with Carbon 2, we keep them positive.
-            'one day from now' => ['2025-02-21 00:00', 1],
-            'less than a day from now' => ['2025-02-20 13:00', 0],
-            'more than a day from now' => ['2025-02-21 13:00', 1],
+            'same time' => ['2025-02-20 00:00', 0.0],
+            'less than a day ago' => ['2025-02-19 11:00', 0.5416666666666666],
+            '1 day ago' => ['2025-02-19 00:00', 1.0],
+            '2 days ago' => ['2025-02-18 00:00', 2.0],
+            'one day from now' => ['2025-02-21 00:00', -1.0],
+            'less than a day from now' => ['2025-02-20 13:00', -0.5416666666666666],
+            'more than a day from now' => ['2025-02-21 13:00', -1.5416666666666667],
         ];
     }
 
