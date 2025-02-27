54f1f9b3ac597883ed4d1c60c759f0000503930e
Duncan McClean <duncan@duncanmcclean.com>
2025-02-27 10:10:05 +0000

    Improve validation message when handle starts with a number

--- src/Rules/Handle.php
+++ src/Rules/Handle.php
@@ -4,11 +4,16 @@ namespace Statamic\Rules;
 
 use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Support\Str;
 
 class Handle implements ValidationRule
 {
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
+        if (Str::startsWith($value, range(0, 9))) {
+            $fail('statamic::validation.handle_starts_with_number')->translate();
+        }
+
         if (! preg_match('/^[a-zA-Z][a-zA-Z0-9]*(?:_{0,1}[a-zA-Z0-9])*$/', $value)) {
             $fail('statamic::validation.handle')->translate();
         }
