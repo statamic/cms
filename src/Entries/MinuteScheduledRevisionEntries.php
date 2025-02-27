5837d5e80abf28fbacff159869b3e6f8016fe935
edalzell <erin@silentz.co>
2025-02-27 14:52:58 -0800

    get all the revisions that are ready to be published

---
+++ src/Entries/MinuteScheduledRevisionEntries.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Statamic\Entries;
+
+use Carbon\CarbonInterface;
+use Illuminate\Support\Collection;
+use Statamic\Facades\Entry as EntryFacade;
+
+class MinuteScheduledRevisionEntries
+{
+    public function __construct(private readonly CarbonInterface $minute) {}
+
+    public function __invoke(): Collection
+    {
+        return EntryFacade::all()
+            ->filter(fn (Entry $entry) => $entry->hasRevisions())
+            ->filter(fn (Entry $entry) => $entry->latestRevision()->publishAt())
+            ->filter(
+                fn (Entry $entry) => $entry
+                    ->latestRevision()
+                    ->publishAt()
+                    ->isSameMinute($this->minute)
+            );
+    }
+}
