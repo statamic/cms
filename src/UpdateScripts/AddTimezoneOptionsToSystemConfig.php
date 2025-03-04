f196c67d9f55203c3c5a1482b62e2c4be34be5da
Duncan McClean <duncan@duncanmcclean.com>
2025-03-04 19:57:33 +0100

    We don't want to convert dates during upgrade anymore.
    Instead, we're going to parse/save dates into the app's timezone.

---
+++ src/UpdateScripts/AddTimezoneOptionsToSystemConfig.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Statamic\UpdateScripts;
+
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
+use Statamic\Listeners\Concerns\GetsItemsContainingData;
+
+class AddTimezoneOptionsToSystemConfig extends UpdateScript
+{
+    use GetsItemsContainingData;
+
+    public function shouldUpdate($newVersion, $oldVersion): bool
+    {
+        return $this->isUpdatingTo('6.0.0');
+    }
+
+    public function update(): void
+    {
+        if (! File::exists($path = app()->configPath('statamic/system.php'))) {
+            return;
+        }
+
+        $systemConfig = File::get($path);
+
+        if (Str::contains($systemConfig, 'display_timezone')) {
+            return;
+        }
+
+        $lineNumberOfDateFormatOption = collect(explode("\n", $systemConfig))
+            ->filter(fn ($line) => Str::contains($line, 'date_format'))
+            ->keys()
+            ->first();
+
+        $stub = Str::of(File::get(__DIR__.'/stubs/system_timezone_config.php.stub'))
+            ->replace('TIMEZONE', config('app.timezone'))
+            ->__toString();
+
+        $systemConfig = Str::of($systemConfig)
+            ->explode("\n")
+            ->put($lineNumberOfDateFormatOption + 1, $stub)
+            ->implode("\n");
+
+        File::put(app()->configPath('statamic/system.php'), $systemConfig);
+    }
+}
---
+++ tests/UpdateScripts/AddTimezoneOptionsToSystemConfigTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\UpdateScripts;
+
+use Illuminate\Support\Facades\File;
+use PHPUnit\Framework\Attributes\Test;
+use Statamic\UpdateScripts\AddTimezoneOptionsToSystemConfig;
+use Tests\TestCase;
+use Tests\UpdateScripts\Concerns\RunsUpdateScripts;
+
+class AddTimezoneOptionsToSystemConfigTest extends TestCase
+{
+    use RunsUpdateScripts;
+
+    #[Test]
+    public function it_is_registered()
+    {
+        $this->assertUpdateScriptRegistered(AddTimezoneOptionsToSystemConfig::class);
+    }
+
+    #[Test]
+    public function it_appends_timezone_option_to_system_config()
+    {
+        config()->set('app.timezone', 'America/New_York'); // -05:00
+        date_default_timezone_set('America/New_York');
+
+        File::ensureDirectoryExists(app()->configPath('statamic'));
+
+        File::put(app()->configPath('statamic/system.php'), <<<'EOT'
+<?php
+
+return [
+
+    'above' => 'this',
+
+    'date_format' => 'F jS, Y',
+
+    'below' => 'that',
+
+];
+EOT
+        );
+
+        $this->runUpdateScript(AddTimezoneOptionsToSystemConfig::class);
+
+        $systemConfig = File::get(app()->configPath('statamic/system.php'));
+
+        $this->assertStringContainsString("'above' => 'this',", $systemConfig);
+        $this->assertStringContainsString("'date_format' => 'F jS, Y',", $systemConfig);
+        $this->assertStringContainsString("'display_timezone' => 'America/New_York',", $systemConfig);
+        $this->assertStringContainsString("'localize_dates_in_modifiers' => true,", $systemConfig);
+        $this->assertStringContainsString("'below' => 'that',", $systemConfig);
+    }
+}
