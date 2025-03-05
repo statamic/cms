statamic/cms

commit 31fe38ce37d8b4549cef1e1732c6e2fcf0f1ac84

Replace underscore with lodash ...
Remove global _ helper.
Use imports everywhere.
Most things are the same method. Replaced where necessary. Replaced with native JS in some places.

resources/js/components/assets/Editor/Editor.vue
@@ -292,7 +292,7 @@ import FocalPointEditor from './FocalPointEditor.vue';
 import PdfViewer from './PdfViewer.vue';
 import PublishFields from '../../publish/Fields.vue';
 import HasHiddenFields from '../../publish/HasHiddenFields';
-import pick from 'underscore/modules/pick';
+import { pick, isArray, map, flatten } from 'lodash-es';
 
 export default {
     emits: ['saved', 'closed', 'action-completed'],
@@ -415,19 +415,20 @@ export default {
 
                 // If there are no fields, it will be an empty array when PHP encodes
                 // it into JSON on the server. We'll ensure it's always an object.
-                this.values = _.isArray(data.values) ? {} : data.values;
+                this.values = isArray(data.values) ? {} : data.values;
 
                 this.meta = data.meta;
                 this.actionUrl = data.actionUrl;
                 this.actions = data.actions;
 
                 this.fieldset = data.blueprint;
-                this.fields = _.chain(this.fieldset.tabs)
-                    .map((tab) => tab.sections)
-                    .flatten(true)
-                    .map((section) => section.fields)
-                    .flatten(true)
-                    .value();
+
+                let fields = this.fields.tabs;
+                fields = map(fields, (tab) => tab.sections);
+                fields = flatten(fields);
+                fields = map(fields, (section) => section.fields);
+                fields = flatten(fields);
+                this.fields = fields;
 
                 this.extraValues = pick(this.asset, [
                     'filename',
@@ -514,7 +515,7 @@ export default {
         },
 
         canRunAction(handle) {
-            return _.find(this.actions, (action) => action.handle == handle);
+            return this.actions.find((action) => action.handle == handle);
         },
 
         runAction(handle) {

commit 2dbbf2d0f2e3463693b6e91e6e0b1549a8ae6b64

Fix typo introduced in 31fe38ce

resources/js/components/assets/Editor/Editor.vue
@@ -426,7 +426,7 @@ export default {
 
                 this.fieldset = data.blueprint;
 
-                let fields = this.fields.tabs;
+                let fields = this.fieldset.tabs;
                 fields = fields.map((tab) => tab.sections);
                 fields = flatten(fields);
                 fields = fields.map((section) => section.fields);