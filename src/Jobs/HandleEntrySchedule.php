725a5a1ce046881e6ac9042750713b2cf0a430f7
Duncan McClean <duncan@duncanmcclean.com>
2025-03-06 10:23:05 +0000

    This change is unnecessary now.

--- src/Jobs/HandleEntrySchedule.php
+++ src/Jobs/HandleEntrySchedule.php
@@ -9,7 +9,6 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Collection;
 use Statamic\Entries\MinuteEntries;
 use Statamic\Events\EntryScheduleReached;
-use Statamic\Facades\Entry;
 
 class HandleEntrySchedule implements ShouldQueue
 {
@@ -26,7 +25,7 @@ class HandleEntrySchedule implements ShouldQueue
         // were scheduled for then would now be considered published. If we were targeting
         // the current minute and the entry has defined a time with seconds later in the
         // same minute, it may still be considered scheduled when it gets dispatched.
-        $minute = now('UTC')->subMinute();
+        $minute = now()->subMinute();
 
         return (new MinuteEntries($minute))();
     }
