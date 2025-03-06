222c442e50bcae39e3e552485d0048f95a9917bf
Duncan McClean <duncan@duncanmcclean.com>
2025-03-06 17:39:29 +0000

    Show the "Localizable" toggle on asset blueprints

--- resources/views/assets/containers/blueprints/edit.blade.php
+++ resources/views/assets/containers/blueprints/edit.blade.php
@@ -18,7 +18,6 @@
         action="{{ cp_route('asset-containers.blueprint.update', $container->handle()) }}"
         :initial-blueprint="{{ json_encode($blueprintVueObject) }}"
         :use-tabs="false"
-        :can-define-localizable="false"
     ></blueprint-builder>
 
     @include(
