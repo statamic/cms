49b44e9ba56a2cbfe142c557f2f35702f09f0a6d
Jesse Leite <jesseleite@gmail.com>
2025-03-07 15:31:25 -0500

    Extract `showField()` test helper.

--- resources/js/tests/Frontend/FieldConditionsTest.test.js
+++ resources/js/tests/Frontend/FieldConditionsTest.test.js
@@ -2,28 +2,31 @@ import { test, expect } from 'vitest';
 import '../../frontend/helpers.js';
 
 let formData = {
+    target: 'test',
     first_name: 'Bilbo',
     last_name: 'Baggins',
     hobby: '',
     bio: null,
 };
 
+let showField = function (conditions, dottedFieldPath = null) {
+    return Statamic.$conditions.showField(conditions, formData, dottedFieldPath || 'target');
+};
+
 test('it shows field by default', () => {
-    expect(Statamic.$conditions.showField([], formData)).toBe(true);
-    expect(Statamic.$conditions.showField({}, formData)).toBe(true);
+    expect(showField([])).toBe(true);
+    expect(showField({})).toBe(true);
 });
 
 test('it can show field based on empty checks', () => {
-    expect(Statamic.$conditions.showField({ if: { hobby: 'empty' } }, formData)).toBe(true);
-    expect(Statamic.$conditions.showField({ if: { bio: 'empty' } }, formData)).toBe(true);
-    expect(Statamic.$conditions.showField({ if: { first_name: 'empty' } }, formData)).toBe(false);
-    expect(Statamic.$conditions.showField({ if: { first_name: 'not empty' } }, formData)).toBe(true);
+    expect(showField({ if: { hobby: 'empty' } })).toBe(true);
+    expect(showField({ if: { bio: 'empty' } })).toBe(true);
+    expect(showField({ if: { first_name: 'empty' } })).toBe(false);
+    expect(showField({ if: { first_name: 'not empty' } })).toBe(true);
 });
 
 test('it can show field if multiple conditions are met', () => {
-    expect(Statamic.$conditions.showField({ if: { first_name: 'Bilbo', last_name: 'Baggins' } }, formData)).toBe(true);
-    expect(Statamic.$conditions.showField({ if: { first_name: 'Frodo', last_name: 'Baggins' } }, formData)).toBe(false);
-    expect(Statamic.$conditions.showField({ if_any: { first_name: 'Frodo', last_name: 'Baggins' } }, formData)).toBe(
-        true,
-    );
+    expect(showField({ if: { first_name: 'Bilbo', last_name: 'Baggins' } })).toBe(true);
+    expect(showField({ if: { first_name: 'Frodo', last_name: 'Baggins' } })).toBe(false);
+    expect(showField({ if_any: { first_name: 'Frodo', last_name: 'Baggins' } })).toBe(true);
 });
