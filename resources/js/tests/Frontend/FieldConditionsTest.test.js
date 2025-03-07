aa393cba73340b878c74dcee588049e2b6ebd36f
Jesse Leite <jesseleite@gmail.com>
2025-03-07 15:46:30 -0500

    Add frontend conditions coverage for group field and `$parent` and `$root` syntax.

--- resources/js/tests/Frontend/FieldConditionsTest.test.js
+++ resources/js/tests/Frontend/FieldConditionsTest.test.js
@@ -7,6 +7,14 @@ let formData = {
     last_name: 'Baggins',
     hobby: '',
     bio: null,
+    group_field: {
+        dwelling: 'Bag End',
+        village: 'Hobbiton',
+        nested_group_field: {
+            birthday: 'Sept 22',
+            age: '111',
+        },
+    },
 };
 
 let showField = function (conditions, dottedFieldPath = null) {
@@ -29,4 +37,26 @@ test('it can show field if multiple conditions are met', () => {
     expect(showField({ if: { first_name: 'Bilbo', last_name: 'Baggins' } })).toBe(true);
     expect(showField({ if: { first_name: 'Frodo', last_name: 'Baggins' } })).toBe(false);
     expect(showField({ if_any: { first_name: 'Frodo', last_name: 'Baggins' } })).toBe(true);
+});
+
+test('it can show field nested in group based on sibling field value', () => {
+    expect(showField({ if: { village: 'Hobbiton' } }, 'group_field.dwelling')).toBe(true);
+    expect(showField({ if: { village: 'Mordor' } }, 'group_field.dwelling')).toBe(false);
+});
+
+test('it can show deeply field nested in group based on sibling field value', () => {
+    expect(showField({ if: { age: 111 } }, 'group_field.nested_group_field.birthday')).toBe(true);
+    expect(showField({ if: { age: 112 } }, 'group_field.nested_group_field.birthday')).toBe(false);
+});
+
+test('it can show deeply field nested in group based on parent field value', () => {
+    expect(showField({ if: { '$parent.village': 'Hobbiton' } }, 'group_field.nested_group_field.birthday')).toBe(true);
+    expect(showField({ if: { '$parent.village': 'Mordor' } }, 'group_field.nested_group_field.birthday')).toBe(false);
+    expect(showField({ if: { '$parent.$parent.first_name': 'not empty' } }, 'group_field.nested_group_field.birthday')).toBe(true);
+    expect(showField({ if: { '$parent.$parent.hobby': 'not empty' } }, 'group_field.nested_group_field.birthday')).toBe(false);
+});
+
+test('it can show deeply field nested in group based on root field value', () => {
+    expect(showField({ if: { '$root.first_name': 'not empty' } }, 'group_field.nested_group_field.birthday')).toBe(true);
+    expect(showField({ if: { '$root.hobby': 'not empty' } }, 'group_field.nested_group_field.birthday')).toBe(false);
 });
