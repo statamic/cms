4957838b1c18932ab5032702f4363ef795d72a44
Duncan McClean <duncan@duncanmcclean.com>
2025-03-13 13:32:51 +0000

    Remove tests covering "PHP -> ISO" date conversions

--- tests/Support/DateFormatTest.php
+++ tests/Support/DateFormatTest.php
@@ -2,26 +2,11 @@
 
 namespace Tests\Support;
 
-use PHPUnit\Framework\Attributes\DataProvider;
-use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 use Statamic\Support\DateFormat;
 
 class DateFormatTest extends TestCase
 {
-    #[Test]
-    public function it_has_conversions()
-    {
-        $this->assertCount(38, DateFormat::phpToIsoConversions());
-    }
-
-    #[Test]
-    #[DataProvider('formatProvider')]
-    public function it_converts_from_php_to_iso($php, $iso)
-    {
-        $this->assertEquals($iso, DateFormat::toIso($php));
-    }
-
     public static function formatProvider()
     {
         return collect(DateFormat::phpToIsoConversions())->mapWithKeys(function ($iso, $php) {
