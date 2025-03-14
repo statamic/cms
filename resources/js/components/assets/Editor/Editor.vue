eeff9eb454a8852e74a11c37faa29855aeb723f2
Jason Varga <jason@pixelfear.com>
2025-03-14 13:12:38 -0400

    Use date helper instead of method

--- resources/js/components/assets/Editor/Editor.vue
+++ resources/js/components/assets/Editor/Editor.vue
@@ -251,7 +251,9 @@
                             class="flex items-center rounded bg-gray-400 py-1 dark:bg-dark-600 ltr:pl-2 ltr:pr-3 rtl:pl-3 rtl:pr-2"
                         >
                             <svg-icon name="thumbprint" class="h-3 ltr:mr-2 rtl:ml-2" />
-                            <div class="" :title="formatDate(asset.lastModified)">{{ asset.lastModifiedRelative }}</div>
+                            <div class="" :title="$date.of(asset.lastModified)">
+                                {{ asset.lastModifiedRelative }}
+                            </div>
                         </div>
                     </div>
                     <div class="flex items-center space-x-3 rtl:space-x-reverse">
@@ -538,12 +540,6 @@ export default {
             if (successful) {
                 this.close();
             }
-        },
-
-        formatDate(value) {
-            return Intl.DateTimeFormat(navigator.language, { dateStyle: 'short', timeStyle: 'short' }).format(
-                new Date(value),
-            );
         },
     },
 };
