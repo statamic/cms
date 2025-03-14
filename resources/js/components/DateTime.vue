368beab603d7bc98c6f03d827af9eda7c21b379d
Jason Varga <jason@pixelfear.com>
2025-03-14 18:23:50 -0400

    Vue component can use time-only and date-only props

--- resources/js/components/DateTime.vue
+++ resources/js/components/DateTime.vue
@@ -5,11 +5,19 @@ import DateFormatter from '@statamic/components/DateFormatter.js';
 const props = defineProps({
     of: { type: String },
     options: { type: Object },
+    dateOnly: { type: Boolean },
+    timeOnly: { type: Boolean },
 });
 
 const date = computed(() => (props.of ? new Date(props.of) : Date.now()));
 
-const text = computed(() => DateFormatter.format(date.value, props.options));
+const options = computed(() => {
+    if (props.dateOnly) return 'date';
+    if (props.timeOnly) return 'time';
+    return props.options;
+});
+
+const text = computed(() => DateFormatter.format(date.value, options.value));
 </script>
 
 <template>{{ text }}</template>
