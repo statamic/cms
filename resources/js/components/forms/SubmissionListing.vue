560bdd5d9c415b51d2f9537c61363fc9652ff47d
Jason Varga <jason@pixelfear.com>
2025-03-14 13:05:31 -0400

    Use component rather than a new method

--- resources/js/components/forms/SubmissionListing.vue
+++ resources/js/components/forms/SubmissionListing.vue
@@ -90,7 +90,9 @@
                             @sorted="sorted"
                         >
                             <template #cell-datestamp="{ row: submission, value }">
-                                <a :href="submission.url" class="text-blue">{{ formatDate(value) }}</a>
+                                <a :href="submission.url" class="text-blue">
+                                    <date-time :of="date" />
+                                </a>
                             </template>
                             <template #actions="{ row: submission, index }">
                                 <dropdown-list>
@@ -142,14 +144,6 @@ export default {
     computed: {
         actionContext() {
             return { form: this.form };
-        },
-    },
-
-    methods: {
-        formatDate(value) {
-            return Intl.DateTimeFormat(navigator.language, { dateStyle: 'short', timeStyle: 'short' }).format(
-                new Date(value),
-            );
         },
     },
 };
