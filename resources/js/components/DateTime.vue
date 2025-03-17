ff4af9df64b35100b68b2c4fce9fab0307cd3009
Jason Varga <jason@pixelfear.com>
2025-03-17 15:35:13 -0400

    import not needed

--- resources/js/components/DateTime.vue
+++ resources/js/components/DateTime.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed, defineProps } from 'vue';
+import { computed } from 'vue';
 import DateFormatter from '@statamic/components/DateFormatter.js';
 
 const props = defineProps({
