4fb13005d98f5f640e489b400f0353449fa37511
Duncan McClean <duncan@duncanmcclean.com>
2025-03-18 13:12:09 +0000

    Add test to ensure edit form is shown even when variables don't exist.

--- tests/Feature/Globals/EditGlobalVariablesTest.php
+++ tests/Feature/Globals/EditGlobalVariablesTest.php
@@ -50,4 +50,28 @@ class EditGlobalVariablesTest extends TestCase
             ->assertSuccessful()
             ->assertViewHas('values', ['foo' => 'bar', 'unused' => null]);
     }
+
+    #[Test]
+    public function it_shows_the_form_even_if_localization_does_not_exist()
+    {
+        $blueprint = Blueprint::make()->setContents(['fields' => [
+            ['handle' => 'foo', 'field' => ['type' => 'text']],
+            ['handle' => 'unused', 'field' => ['type' => 'text']],
+        ]]);
+        Blueprint::partialMock();
+        Blueprint::shouldReceive('find')->with('globals.test')->andReturn($blueprint);
+        $this->setTestRoles(['test' => ['access cp', 'edit test globals']]);
+        $user = User::make()->assignRole('test')->save();
+
+        $global = GlobalFactory::handle('test')->create();
+
+        // GlobalFactory would have created the variables/localization, so we'll remove it for this test.
+        $global->removeLocalization($global->in('en'))->save();
+
+        $this
+            ->actingAs($user)
+            ->get($global->in('en')->editUrl())
+            ->assertSuccessful()
+            ->assertViewHas('values', ['foo' => null, 'unused' => null]);
+    }
 }
