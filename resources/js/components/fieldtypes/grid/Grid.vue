statamic/cms

commit e8ad32e6c64c68675bdb9c7df5fa2edd1ecd4fbc

[6.x] Replace underscore with lodash (#11529)

resources/js/components/fieldtypes/grid/Grid.vue
@@ -53,6 +53,7 @@ import uniqid from 'uniqid';
 import GridTable from './Table.vue';
 import GridStacked from './Stacked.vue';
 import ManagesRowMeta from './ManagesRowMeta';
+import { mapValues, keyBy } from 'lodash-es';
 
 export default {
     mixins: [Fieldtype, ManagesRowMeta],
@@ -171,10 +172,7 @@ export default {
         addRow() {
             const id = uniqid();
 
-            const row = _.chain(this.fields)
-                .indexBy('handle')
-                .mapObject((field) => this.meta.defaults[field.handle])
-                .value();
+            const row = mapValues(keyBy(this.field, 'handle'), (field) => this.meta.defaults[field.handle]);
 
             row._id = id;
 

commit 2e9a365a41b038f9264787690a1f4f96e6459bb7

Fix adding a grid row not getting correct values ...
This was really just because this.field should have been this.fields - typo introduced in #11529. But, before I realized that, I simplified to native JS thinking that was part of it. Nope. But keeping the native JS is nice to get rid of a couple of lodash imports.

resources/js/components/fieldtypes/grid/Grid.vue
@@ -53,7 +53,6 @@ import uniqid from 'uniqid';
 import GridTable from './Table.vue';
 import GridStacked from './Stacked.vue';
 import ManagesRowMeta from './ManagesRowMeta';
-import { mapValues, keyBy } from 'lodash-es';
 
 export default {
     mixins: [Fieldtype, ManagesRowMeta],
@@ -172,7 +171,9 @@ export default {
         addRow() {
             const id = uniqid();
 
-            const row = mapValues(keyBy(this.field, 'handle'), (field) => this.meta.defaults[field.handle]);
+            const row = Object.fromEntries(
+                this.fields.map((field) => [field.handle, this.meta.defaults[field.handle]]),
+            );
 
             row._id = id;
 