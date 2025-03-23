e679ea136f09db7e31fe93b5322f6aa302bb8f09
Jesse Leite <jesseleite@gmail.com>
2025-03-22 20:08:39 -0400

    Implement `Link` command type (ie. for ‘Navigation’ items).

---
+++ src/CommandPalette/Link.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Statamic\CommandPalette;
+
+class Link extends Command
+{
+    protected $url;
+
+    public function url(string $url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function toArray(): array
+    {
+        return array_merge(parent::toArray(), [
+            'url' => $this->url,
+        ]);
+    }
+}
