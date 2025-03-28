e8b4d5513c1683247130b0269c5780ce9521f705
Simon Geoghegan <simon@workhouseadvertising.com.au>
2025-03-28 14:27:11 +0800

    Updated the behaviour of HasSelectOptions trait pre-processing to match old expectations

--- src/Fieldtypes/HasSelectOptions.php
+++ src/Fieldtypes/HasSelectOptions.php
@@ -52,9 +52,8 @@ trait HasSelectOptions
     {
         $values = $this->preProcess($value);
 
-        $values = collect($values)->toArray();
-
-        return $values->map(function ($value) {
+        // NOTE: Null-coalescing into `[null]` as that matches old behaviour.
+        return collect($values ?? [null])->map(function ($value) {
             return $this->getLabel($value);
         })->all();
     }
@@ -67,9 +66,8 @@ trait HasSelectOptions
             return [];
         }
 
-        $value = collect($value)->toArray();
-
-        $values = collect($value)->map(function ($value) {
+        // NOTE: Null-coalescing into `[null]` as that matches old behaviour.
+        $values = collect($value ?? [null])->map(function ($value) {
             return $this->config('cast_booleans') ? $this->castFromBoolean($value) : $value;
         });
 
