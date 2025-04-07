ce0fdd67e831ec4e19549bec657fcc6ab9e6fcc0
Jesse Leite <jesseleite@gmail.com>
2025-04-07 15:29:26 -0400

    Treat dialog as modal.

--- resources/js/components/ui/CommandPalette/Index.vue
+++ resources/js/components/ui/CommandPalette/Index.vue
@@ -86,7 +86,7 @@ const modalClasses = cva({
 </script>
 
 <template>
-    <DialogRoot>
+    <DialogRoot :modal="true">
         <DialogTrigger data-ui-command-palette-trigger>
             <button type="button" aria-expanded="false" class="cursor-text data-[focus-visible]:outline-focus flex items-center gap-x-2 text-xs text-gray-400 outline-none md:w-32 rounded-md md:py-[calc(5/16*1rem)] md:ps-2 md:pe-1.5 md:shadow-[0_1px_5px_-4px_rgba(19,19,22,0.4),0_2px_5px_rgba(32,42,54,0.06)] ring-1 ring-gray-900/10 hover bg-gray-900 shadow-[0_-1px_rgba(255,255,255,0.06),0_4px_8px_rgba(0,0,0,0.05),0_1px_6px_-4px_#000] hover:ring-white/10" >
                 <ui-icon name="magnifying-glass" class="size-5 flex-none text-gray-600" />
