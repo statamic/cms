d764b1ca209ace677f8b017bbb5f76fdb6928ebb
Philipp Daun <post@philippdaun.net>
2025-04-07 23:16:49 +0200

    Add tests for re-indexing
    Signed-off-by: Philipp Daun <post@philippdaun.net>

--- tests/Modifiers/FilterEmptyTest.php
+++ tests/Modifiers/FilterEmptyTest.php
@@ -18,10 +18,24 @@ class FilterEmptyTest extends TestCase
     }
 
     #[Test]
+    public function it_reindexes_numerical_arrays(): void
+    {
+        $modified = $this->modify(['one', null, 'two', null, 'four']);
+        $this->assertEquals([0 => 'one', 1 => 'two', 2 => 'four'], $modified);
+    }
+
+    #[Test]
     public function it_removes_null_values_from_a_collection(): void
     {
         $modified = $this->modify(collect(['one' => 'one', null, 'two' => 'two', 'three' => null, 'four' => 'four']));
         $this->assertEquals(['one' => 'one', 'two' => 'two', 'four' => 'four'], $modified->all());
+    }
+
+    #[Test]
+    public function it_reindexes_numerical_collections(): void
+    {
+        $modified = $this->modify(collect(['one', null, 'two', null, 'four']));
+        $this->assertEquals([0 => 'one', 1 => 'two', 2 => 'four'], $modified->all());
     }
 
     private function modify($value, $params = [])
