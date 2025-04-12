d88035f47aff8035450d3a1af9220d7fa4b81879
Jack McDade <jack@jackmcdade.com>
2025-04-12 17:28:28 -0400

    Missed a few little things.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -26,7 +26,7 @@
                     {{-- @include('statamic::partials.nav-mobile') --}}
 
                     <main class="
-                        flex bg-gray-50 dark:bg-gray-900 dark:border-t rounded-t-xl dark:border-white/10
+                        flex bg-gray-100 dark:bg-gray-900 dark:border-t rounded-t-xl dark:border-white/10
                         fixed top-14 inset-x-0 bottom-0 min-h-[calc(100vh-3.5rem)]
                     ">
                         @include('statamic::partials.nav-main')
