fb220876b13f53d3d795db9d31a64a51170c4e8a
Jesse Leite <jesseleite@gmail.com>
2025-04-15 11:12:53 -0400

    Pre-selection workaround.

--- resources/js/components/command-palette/CommandPalette.vue
+++ resources/js/components/command-palette/CommandPalette.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, computed, watch } from 'vue';
+import { ref, computed, watch, nextTick } from 'vue';
 import CommandPaletteItem from './Item.vue';
 import axios from 'axios';
 import debounce from '@statamic/util/debounce';
@@ -80,6 +80,13 @@ watch(open, (isOpen) => {
     if (isOpen) return;
     reset();
 })
+
+// When aggregatedItems changes, we lose our pre-selected item.
+// This is a bit of a workaround to pre-select the first item, so the user can just hit enter on the first result.
+// TODO: Maybe there's a better way?
+watch(aggregatedItems, () => {
+    nextTick(() => document.activeElement.dispatchEvent(new KeyboardEvent('keydown', {'key': 'ArrowDown'})));
+});
 
 function getItems() {
     axios.get('/cp/command-palette').then((response) => {
