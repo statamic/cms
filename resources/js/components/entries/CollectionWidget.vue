999033834cd46351f95ce29064425dbefadc34a0
Jack McDade <jack@jackmcdade.com>
2025-04-15 20:47:34 -0400

    Extract a nice reusable base ui Widget component

---
+++ resources/js/components/entries/CollectionWidget.vue
@@ -0,0 +1,70 @@
+<script>
+import Listing from '../Listing.vue'
+
+export default {
+
+    mixins: [Listing],
+
+    props: {
+        additionalColumns: Array,
+        collection: String,
+        title: String,
+    },
+
+    data() {
+        return {
+            cols: [{ label: 'Title', field: 'title', visible: true }, ...this.additionalColumns],
+            listingKey: 'entries',
+            requestUrl: cp_url(`collections/${this.collection}/entries`),
+        }
+    },
+
+    methods: {
+        columnShowing(column) {
+            return this.cols.find((c) => c.field === column)
+        },
+    },
+}
+</script>
+
+<template>
+    <ui-widget :title="title" icon="collections">
+        <data-list
+            v-if="!initializing && items.length"
+            :rows="items"
+            :columns="cols"
+            :sort="false"
+        >
+            <div v-if="initializing" class="loading">
+                <loading-graphic />
+            </div>
+
+            <data-list-table v-else :loading="loading" unstyled class="[&_td]:text-sm [&_td]:p-0.5 [&_thead]:hidden">
+                <template #cell-title="{ row: entry }">
+                    <div class="flex items-center gap-2">
+                        <ui-status-indicator v-if="!columnShowing('status')" :status="entry.status" />
+                        <a :href="entry.edit_url">{{ entry.title }}</a>
+                    </div>
+                </template>
+                <template #cell-status="{ row: entry }">
+                    <ui-status-indicator :status="entry.status" :show-dot="false" show-label />
+                </template>
+            </data-list-table>
+        </data-list>
+
+        <p v-if="!initializing && !items.length" class="p-3 text-center text-sm text-gray-600">
+            {{ __('There are no entries in this collection') }}
+        </p>
+
+        <template #actions>
+            <data-list-pagination
+                v-if="meta.last_page != 1"
+                :resource-meta="meta"
+                @page-selected="selectPage"
+                :scroll-to-top="false"
+                :show-page-links="false"
+            />
+            <slot name="actions" />
+        </template>
+    </ui-widget>
+</template>
---
+++ resources/js/components/ui/Widget.vue
@@ -0,0 +1,31 @@
+<script setup>
+defineProps({
+    title: String,
+    icon: {
+        type: String,
+        default: 'collections'
+    }
+})
+</script>
+
+<template>
+    <ui-card inset>
+        <div class="flex h-full flex-col justify-between">
+            <div>
+                <header class="flex items-center justify-between px-4 py-2.5 border-b border-gray-200 dark:border-gray-700">
+                    <div class="flex items-center gap-3">
+                        <ui-icon :name="icon" class="size-5 text-gray-500" />
+                        <span v-text="title" />
+                    </div>
+                    <div class="flex items-center gap-4">
+                        <slot name="actions" />
+                    </div>
+                </header>
+                <div class="p-4">
+                    <slot />
+                </div>
+            </div>
+            <slot name="footer" />
+        </div>
+    </ui-card>
+</template>
