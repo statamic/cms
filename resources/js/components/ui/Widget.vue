13277b72d12a4f2e8d4905c7bd5006f54cfbe81c
Jack McDade <jack@jackmcdade.com>
2025-04-16 20:45:03 -0400

    Localize the widget @container

--- resources/js/components/ui/Widget.vue
+++ resources/js/components/ui/Widget.vue
@@ -6,12 +6,12 @@ defineProps({
 </script>
 
 <template>
-    <ui-card inset>
+    <ui-card inset class="@container/widget">
         <div class="flex h-full flex-col justify-between min-h-54">
             <div>
                 <header class="flex items-center justify-between px-4 py-2 h-15 border-b border-gray-200 dark:border-gray-700">
                     <div class="flex items-center gap-3">
-                        <ui-icon :name="icon" class="size-5 text-gray-500 hidden! @xs/card:block!" />
+                        <ui-icon :name="icon" class="size-5 text-gray-500 hidden! @xs/widget:block!" />
                         <span v-text="title" />
                     </div>
                     <div class="flex items-center gap-4">
