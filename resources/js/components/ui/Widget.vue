c4e7a8fe4e2c4d5cee4396c7da7d6dae9e67b786
Jack McDade <jack@jackmcdade.com>
2025-04-16 20:29:02 -0400

    responsive tweaks for widgets

--- resources/js/components/ui/Widget.vue
+++ resources/js/components/ui/Widget.vue
@@ -1,10 +1,7 @@
 <script setup>
 defineProps({
     title: String,
-    icon: {
-        type: String,
-        default: 'collections'
-    }
+    icon: { type: String, default: 'collections' }
 })
 </script>
 
@@ -14,14 +11,14 @@ defineProps({
             <div>
                 <header class="flex items-center justify-between px-4 py-2 h-15 border-b border-gray-200 dark:border-gray-700">
                     <div class="flex items-center gap-3">
-                        <ui-icon :name="icon" class="size-5 text-gray-500" />
+                        <ui-icon :name="icon" class="size-5 text-gray-500 hidden! @xs/card:block!" />
                         <span v-text="title" />
                     </div>
                     <div class="flex items-center gap-4">
                         <slot name="actions" />
                     </div>
                 </header>
-                <div class="p-4">
+                <div class="px-4 py-3">
                     <slot />
                 </div>
             </div>
