5e126a0fdca111f67162de164ab255c42644ce2e
Duncan McClean <duncan@duncanmcclean.com>
2025-04-23 10:44:32 +0100

    Exception isn't needed anymore.

--- src/Exceptions/InvalidChallengeModeException.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Statamic\Exceptions;
-
-class InvalidChallengeModeException extends \Exception
-{
-    protected $message = 'The challenge mode can only be "code" or "recovery_code".';
-}
