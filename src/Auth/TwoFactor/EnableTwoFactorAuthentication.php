4e8e8caeb9b1fb6e0669d38ccd203f176b313db5
Duncan McClean <duncan@duncanmcclean.com>
2025-04-24 14:31:52 +0100

    Call `$user->set()` instead
    `$user->merge()` seems to be causing issues with the `roles`/`groups` column when storing users in a database.
    
    We can fix merge outside of this PR, but for now, let's go back to calling `->set()`

--- src/Auth/TwoFactor/EnableTwoFactorAuthentication.php
+++ src/Auth/TwoFactor/EnableTwoFactorAuthentication.php
@@ -14,12 +14,12 @@ class EnableTwoFactorAuthentication
 
     public function __invoke(User $user)
     {
-        $user->merge([
-            'two_factor_secret' => encrypt($this->provider->generateSecretKey()),
-            'two_factor_recovery_codes' => encrypt(json_encode(Collection::times(8, function () {
+        $user
+            ->set('two_factor_secret', encrypt($this->provider->generateSecretKey()))
+            ->set('two_factor_recovery_codes', encrypt(json_encode(Collection::times(8, function () {
                 return RecoveryCode::generate();
-            })->all())),
-        ])->save();
+            })->all())))
+            ->save();
 
         TwoFactorAuthenticationEnabled::dispatch($user);
     }
