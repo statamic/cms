de6fcdade009c1e443b8c83d176c8b14af406133
Duncan McClean <duncan@duncanmcclean.com>
2025-04-24 16:20:30 +0100

    Wire up elevated session modal

--- resources/js/mixins/HasElevatedSession.js
+++ resources/js/mixins/HasElevatedSession.js
@@ -5,24 +5,15 @@ export default {
 
             if (response.data.elevated) return;
 
-            const password = await this.askForPassword();
+            return new Promise((resolve, reject) => {
+                const component = Statamic.$components.append('elevated-session-modal', {
+                    props: {},
+                });
 
-            if (!password) throw new Error('User cancelled');
-
-            try {
-                await this.$axios.post(cp_url('elevated-session'), { password });
-            } catch (error) {
-                this.$toast.error(error.response.data.message);
-                throw error;
-            }
-        },
-
-        async askForPassword() {
-            // TODO: This should be an actual modal at some point.
-            return new Promise((resolve) => {
-                const password = prompt('You need to enter your password to continue.');
-
-                resolve(password);
+                component.on('closed', (shouldResolve) => {
+                    shouldResolve ? resolve() : reject();
+                    component.destroy();
+                });
             });
         },
     },
