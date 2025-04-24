3cdd919c5eeb9c0ff855653144dd3e1bb9aae646
Duncan McClean <duncan@duncanmcclean.com>
2025-04-24 16:30:15 +0100

    Make sure we're dealing with a Statamic user instance
    Otherwise, `$user->password()` will error out when storing users in a database.

--- src/Http/Controllers/CP/Auth/ElevatedSessionController.php
+++ src/Http/Controllers/CP/Auth/ElevatedSessionController.php
@@ -6,31 +6,36 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
+use Statamic\Facades\User;
 
 class ElevatedSessionController
 {
     public function index(Request $request)
     {
-        $isElevated = session()->get("statamic_elevated_session_{$request->user()->id}") > now()->timestamp;
-        $timeRemaining = $isElevated ? Carbon::parse(session()->get("statamic_elevated_session_{$request->user()->id}"))->diffInSeconds(absolute: true) : 0;
+        $user = User::current();
+
+        $isElevated = session()->get("statamic_elevated_session_{$user->id}") > now()->timestamp;
+        $timeRemaining = $isElevated ? Carbon::parse(session()->get("statamic_elevated_session_{$user->id}"))->diffInSeconds(absolute: true) : 0;
 
         return ['elevated' => $isElevated, 'time_remaining' => $timeRemaining];
     }
 
     public function store(Request $request)
     {
+        $user = User::current();
+
         $validated = $request->validate([
             'password' => 'required',
         ]);
 
-        if (! Hash::check($validated['password'], $request->user()->password())) {
+        if (! Hash::check($validated['password'], $user->password())) {
             throw ValidationException::withMessages([
                 'password' => [__('statamic::validation.current_password')],
             ]);
         }
 
         session()->put(
-            "statamic_elevated_session_{$request->user()->id}",
+            "statamic_elevated_session_{$user->id}",
             now()->addMinutes(config('statamic.users.elevated_session_duration', 15))->timestamp
         );
 
