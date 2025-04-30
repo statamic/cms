2e06122606bf2d3ebbb76ceffb5f18710f48cae1
Jason Varga <jason@pixelfear.com>
2025-04-30 16:41:29 -0400

    same a couple of translations, just have an icon

--- resources/js/components/users/Listing.vue
+++ resources/js/components/users/Listing.vue
@@ -123,11 +123,9 @@
                                 <div class="flex items-center space-x-2">
                                     <template v-if="value">
                                         <svg-icon name="light/check" class="w-3 text-green-600" />
-                                        <div>{{ __('Set up') }}</div>
                                     </template>
                                     <template v-else>
                                         <svg-icon name="light/close" class="w-3 text-gray-500" />
-                                        <div>{{ __('Not set up') }}</div>
                                     </template>
                                 </div>
                             </template>
