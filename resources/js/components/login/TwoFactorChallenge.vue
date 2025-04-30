e9136ba5b9a0da39572ca798f740ad9514b57f8c
Jason Varga <jason@pixelfear.com>
2025-04-30 14:36:03 -0400

    Move the challenge form into the component ...
    That was inspired by the login form but it should get updated eventually. Might as well use better standards going ahead.

--- resources/js/components/login/TwoFactorChallenge.vue
+++ resources/js/components/login/TwoFactorChallenge.vue
@@ -6,30 +6,102 @@ const props = defineProps({
         type: String,
         default: 'code',
     },
-    hasError: {
-        default: false,
+    errors: {
+        type: Object,
+    },
+    formAction: {
+        type: String,
+    },
+    csrfToken: {
+        type: String,
+    },
+    redirect: {
+        type: String,
     },
 });
 
-const wrapper = ref(null);
+const formEl = ref(null);
 const busy = ref(false);
 const mode = ref(props.initialMode);
 
 onMounted(() => {
-    if (props.hasError) {
-        wrapper.value.parentElement.parentElement.classList.add('animation-shake');
+    if (hasErrors()) {
+        formEl.value.parentElement.parentElement.classList.add('animation-shake');
     }
 });
 
-function toggleMode() {
-    mode.value = mode.value === 'code' ? 'recovery_code' : 'code';
+function hasErrors() {
+    return Object.keys(props.errors).length > 0;
 }
 
-function setBusy(state = null) {
-    busy.value = state ?? true;
+function hasError(field) {
+    return !!props.errors[field];
+}
+
+function errorFor(field) {
+    return props.errors[field][0];
 }
 </script>
 
 <template>
-    <div ref="wrapper"><slot v-bind="{ busy, setBusy, mode, toggleMode, hasError }"></slot></div>
+    <form ref="formEl" method="POST" :action="formAction" class="email-login select-none" @submit="busy = true">
+        <input type="hidden" name="_token" :value="csrfToken" />
+        <input v-if="redirect" type="hidden" name="redirect" :value="redirect" />
+
+        <h1 class="mb-2 text-lg text-gray-800 dark:text-dark-175">
+            {{ __('Two Factor Authentication') }}
+        </h1>
+        <p v-if="mode === 'code'" class="mb-4 text-sm text-gray dark:text-dark-175">
+            {{ __('statamic::messages.two_factor_challenge_code_instructions') }}
+        </p>
+        <p v-if="mode === 'recovery_code'" class="mb-4 text-sm text-gray dark:text-dark-175">
+            {{ __('statamic::messages.two_factor_recovery_code_instructions') }}
+        </p>
+
+        <div v-if="mode === 'code'" class="mb-8">
+            <label class="mb-2" for="input-code">{{ __('Code') }}</label>
+            <input
+                type="text"
+                class="input-text"
+                name="code"
+                pattern="[0-9]*"
+                maxlength="6"
+                inputmode="numeric"
+                autofocus
+                autocomplete="one-time-code"
+                id="input-code"
+            />
+            <div class="mt-2 text-xs text-red-500" v-if="hasError('code')" v-text="errorFor('code')" />
+        </div>
+
+        <div v-if="mode === 'recovery_code'" class="mb-8">
+            <label class="mb-2" for="input-recovery-code">{{ __('Recovery Code') }}</label>
+            <input
+                type="text"
+                class="input-text"
+                name="recovery_code"
+                maxlength="21"
+                autofocus
+                autocomplete="off"
+                id="input-recovery-code"
+            />
+            <div
+                class="mt-2 text-xs text-red-500"
+                v-if="hasError('recovery_code')"
+                v-text="errorFor('recovery_code')"
+            />
+        </div>
+
+        <div class="flex items-center justify-between">
+            <button v-if="mode === 'code'" class="text-btn text-xs" type="button" @click="mode = 'recovery_code'">
+                {{ __('Use recovery code') }}
+            </button>
+
+            <button v-if="mode === 'recovery_code'" class="text-btn text-xs" type="button" @click="mode = 'code'">
+                {{ __('Use one-time code') }}
+            </button>
+
+            <button type="submit" class="btn-primary" :disabled="busy">{{ __('Continue') }}</button>
+        </div>
+    </form>
 </template>
--- resources/views/auth/two-factor/challenge.blade.php
+++ resources/views/auth/two-factor/challenge.blade.php
@@ -14,97 +14,10 @@
         <div class="card auth-card">
             <two-factor-challenge
                 initial-mode="{{ $mode }}"
-                :has-error="{{ $str::bool(count($errors) > 0) }}"
-                v-slot="{ busy, setBusy, mode, toggleMode, hasError }"
-            >
-                <form method="POST" action="{{ $action }}" class="email-login select-none" @submit="setBusy">
-                    @csrf
-
-                    @if (request("redirect"))
-                        <input type="hidden" name="redirect" value="{{ request("redirect") }}" />
-                    @endif
-
-                    <h1 class="mb-2 text-lg text-gray-800 dark:text-dark-175">
-                        {{ __("Two Factor Authentication") }}
-                    </h1>
-                    <p
-                        v-if="mode === 'code'"
-                        @if ($mode === "recovery_code")
-                            v-cloak
-                        @endif
-                        class="mb-4 text-sm text-gray dark:text-dark-175"
-                    >
-                        {{ __("statamic::messages.two_factor_challenge_code_instructions") }}
-                    </p>
-                    <p
-                        v-if="mode === 'recovery_code'"
-                        @if ($mode === "code")
-                            v-cloak
-                        @endif
-                        class="mb-4 text-sm text-gray dark:text-dark-175"
-                    >
-                        {{ __("statamic::messages.two_factor_recovery_code_instructions") }}
-                    </p>
-
-                    <div v-if="mode === 'code'" @if($mode === 'recovery_code') v-cloak @endif class="mb-8">
-                        <label class="mb-2" for="input-code">{{ __("Code") }}</label>
-                        <input
-                            type="text"
-                            class="input-text"
-                            name="code"
-                            pattern="[0-9]*"
-                            maxlength="6"
-                            inputmode="numeric"
-                            autofocus
-                            autocomplete="one-time-code"
-                            id="input-code"
-                        />
-                        @if ($hasError("code"))
-                            <div class="mt-2 text-xs text-red-500">{{ $errors->first("code") }}</div>
-                        @endif
-                    </div>
-
-                    <div v-if="mode === 'recovery_code'" @if($mode === 'code') v-cloak @endif class="mb-8">
-                        <label class="mb-2" for="input-recovery-code">{{ __("Recovery Code") }}</label>
-                        <input
-                            type="text"
-                            class="input-text"
-                            name="recovery_code"
-                            maxlength="21"
-                            autofocus
-                            autocomplete="off"
-                            id="input-recovery-code"
-                        />
-                        @if ($hasError("recovery_code"))
-                            <div class="mt-2 text-xs text-red-500">{{ $errors->first("recovery_code") }}</div>
-                        @endif
-                    </div>
-
-                    <div class="flex items-center justify-between">
-                        <button
-                            v-if="mode === 'code'"
-                            @if($mode === 'recovery_code') v-cloak @endif
-                            class="text-btn text-xs"
-                            type="button"
-                            @click="toggleMode"
-                        >
-                            {{ __("Use recovery code") }}
-                        </button>
-
-                        <button
-                            v-if="mode === 'recovery_code'"
-                            @if($mode === 'code') v-cloak @endif
-                            class="text-btn text-xs"
-                            type="button"
-                            @click="toggleMode"
-                        >
-                            {{ __("Use one-time code") }}
-                        </button>
-
-                        <button type="submit" class="btn-primary" :disabled="busy">{{ __("Continue") }}</button>
-                    </div>
-                </form>
-            </two-factor-challenge>
+                :errors="{{ $errors->isEmpty() ? '{}' : json_encode($errors->getMessages()) }}"
+                csrf-token="{{ csrf_token() }}"
+                redirect="{{ request('redirect') }}"
+            ></two-factor-challenge>
         </div>
     </div>
 @endsection
