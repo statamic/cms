c7426b7a963149166246571b78acd6b81fdb3591
Jason Varga <jason@pixelfear.com>
2025-04-30 14:41:12 -0400

    nitpick

--- src/Auth/TwoFactor/EnableTwoFactorAuthentication.php
+++ src/Auth/TwoFactor/EnableTwoFactorAuthentication.php
@@ -16,11 +16,14 @@ class EnableTwoFactorAuthentication
     {
         $user
             ->set('two_factor_secret', encrypt($this->provider->generateSecretKey()))
-            ->set('two_factor_recovery_codes', encrypt(json_encode(Collection::times(8, function () {
-                return RecoveryCode::generate();
-            })->all())))
+            ->set('two_factor_recovery_codes', $this->generateCodes())
             ->save();
 
         TwoFactorAuthenticationEnabled::dispatch($user);
+    }
+
+    private function generateCodes(): string
+    {
+        return encrypt(json_encode(Collection::times(8, fn () => RecoveryCode::generate())->all()));
     }
 }
--- src/Auth/TwoFactor/GenerateNewRecoveryCodes.php
+++ src/Auth/TwoFactor/GenerateNewRecoveryCodes.php
@@ -10,9 +10,12 @@ class GenerateNewRecoveryCodes
     public function __invoke(User $user)
     {
         $user
-            ->set('two_factor_recovery_codes', encrypt(json_encode(Collection::times(8, function () {
-                return RecoveryCode::generate();
-            })->all())))
+            ->set('two_factor_recovery_codes', $this->generateCodes())
             ->save();
+    }
+
+    private function generateCodes(): string
+    {
+        return encrypt(json_encode(Collection::times(8, fn () => RecoveryCode::generate())->all()));
     }
 }
