0c7b5faa81f40e0e2fea0cd74092833839d748cf
Jason Varga <jason@pixelfear.com>
2025-05-01 16:11:07 -0400

    prettier

--- resources/views/auth/two-factor/challenge.blade.php
+++ resources/views/auth/two-factor/challenge.blade.php
@@ -2,12 +2,12 @@
     use function Statamic\trans as __;
 @endphp
 
-@inject("str", "Statamic\Support\Str")
-@extends("statamic::outside")
-@section("title", __("Two Factor Authentication"))
+@inject('str', 'Statamic\Support\Str')
+@extends('statamic::outside')
+@section('title', __('Two Factor Authentication'))
 
-@section("content")
-    @include("statamic::partials.outside-logo")
+@section('content')
+    @include('statamic::partials.outside-logo')
 
     <div class="relative mx-auto flex max-w-xs items-center justify-center rounded shadow-lg">
         <div class="outside-shadow absolute inset-0"></div>
