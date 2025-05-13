186e3f13c3567252e3aca4fda42aae540b51a575
Jason Varga <jason@pixelfear.com>
2025-05-13 11:49:57 -0400

    Save pipeline

--- resources/js/components/ui/Publish/Form.vue
+++ resources/js/components/ui/Publish/Form.vue
@@ -3,8 +3,9 @@ import Container from './Container.vue';
 import Tabs from './Tabs.vue';
 import { Header, Button } from '@statamic/ui';
 import uniqid from 'uniqid';
-import { ref } from 'vue';
-import axios from 'axios';
+import { ref, useTemplateRef } from 'vue';
+import { SavePipeline } from '@statamic/exports.js';
+const { Pipeline, Request, BeforeSaveHooks, AfterSaveHooks } = SavePipeline;
 
 const props = defineProps({
     title: {
@@ -32,29 +33,32 @@ const props = defineProps({
 });
 
 const containerName = Statamic.$slug.separatedBy('_').create(props.title);
+const container = useTemplateRef('container');
 const values = ref(props.initialValues);
 const meta = ref(props.initialMeta);
 const errors = ref({});
+const saving = ref(false);
 
 function save() {
-    axios
-        .patch(props.saveUrl, { values: values.value })
-        .then((response) => console.log('Values saved successfully:', response.data))
-        .catch((e) => {
-            if (e.response && e.response.status === 422) {
-                const { errors: messages, message } = e.response.data;
-                errors.value = messages;
-                Statamic.$toast.error(message);
-            }
+    new Pipeline()
+        .provide({ container, errors, saving })
+        .through([
+            new BeforeSaveHooks('entry'),
+            new Request(props.saveUrl, 'patch', { values: values.value }),
+            new AfterSaveHooks('entry'),
+        ])
+        .then((response) => {
+            Statamic.$toast.success('Saved');
         });
 }
 </script>
 
 <template>
     <Header :title="title">
-        <Button variant="primary" text="Save" @click="save" />
+        <Button variant="primary" text="Save" @click="save" :disabled="saving" />
     </Header>
     <Container
+        ref="container"
         :name="containerName"
         :blueprint="blueprint"
         :values="values"
