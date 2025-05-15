129547006513dbf209eba3f24caff5fd9babcdfa
Jason Varga <jason@pixelfear.com>
2025-05-15 15:43:57 -0400

    prettier

--- resources/js/components/fieldtypes/BlueprintsFieldtype.vue
+++ resources/js/components/fieldtypes/BlueprintsFieldtype.vue
@@ -1,14 +1,3 @@
-<template>
-    <div class="flex items-center gap-2 mt-4">
-        <Button v-if="config.options.length > 0" :href="blueprint.edit_url" variant="filled" size="sm" v-for="blueprint in config.options" :key="blueprint.handle">
-            {{ blueprint.title }}
-        </Button>
-        <Button size="sm" :href="config.all_blueprints_url">
-            {{ __('View All Blueprints') }}
-        </Button>
-    </div>
-</template>
-
 <script>
 import Fieldtype from './Fieldtype.vue';
 import { Button } from '@statamic/ui';
@@ -21,3 +10,21 @@ export default {
     },
 };
 </script>
+
+<template>
+    <div class="mt-4 flex items-center gap-2">
+        <Button
+            v-if="config.options.length > 0"
+            :href="blueprint.edit_url"
+            variant="filled"
+            size="sm"
+            v-for="blueprint in config.options"
+            :key="blueprint.handle"
+        >
+            {{ blueprint.title }}
+        </Button>
+        <Button size="sm" :href="config.all_blueprints_url">
+            {{ __('View All Blueprints') }}
+        </Button>
+    </div>
+</template>
