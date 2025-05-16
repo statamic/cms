e69acb69832a8b2c46e7de75070e5052b76776b0
Jason Varga <jason@pixelfear.com>
2025-05-16 15:32:27 -0400

    Update field ...
    Pass disabled as a prop.
    Style disable based on prop, not sub-control position.
    The cva classes are now a computed so that it'll update when props change, such as disabled.
    Adjust slots so you can specify combinations of label and actions.

--- resources/js/components/ui/Field.vue
+++ resources/js/components/ui/Field.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { cva } from 'cva';
+import { computed } from 'vue';
 import { Description, Label } from '@statamic/components/ui/index.js';
 
 defineOptions({
@@ -40,39 +41,58 @@ const props = defineProps({
         type: Object,
         default: (props) => (props.error ? [props.error] : []),
     },
+    disabled: {
+        type: Boolean,
+        default: false,
+    },
 });
 
-const classes = cva({
-    base: [
-        'min-w-0',
-        /* When label exists but no description follows */
-        '[&>[data-ui-label]:not(:has(+[data-ui-description]))]:mb-2',
-        /* When label is followed by description */
-        '*:data-ui-description:mb-2',
-        /* Dim label when a child input control is disabled */
-        '[&:not(:has([data-ui-field])):has([data-ui-control][disabled])>[data-ui-label]]:opacity-50',
-    ],
-    variants: {
-        variant: {
-            block: 'w-full',
-            inline: [
-                'grid gap-x-3 gap-y-1.5',
-                'has-[[data-ui-label]~[data-ui-control]]:grid-cols-[1fr_auto]',
-                'has-[[data-ui-control]~[data-ui-label]]:grid-cols-[auto_1fr]',
-                '[&>[data-ui-control]~[data-ui-description]]:row-start-2 [&>[data-ui-control]~[data-ui-description]]:col-start-2',
-                '[&>[data-ui-label]~[data-ui-control]]:row-start-1 [&>[data-ui-label]~[data-ui-control]]:col-start-2',
-            ],
+const labelProps = computed(() => ({
+    text: props.label,
+    for: props.id,
+    required: props.required,
+    badge: props.badge,
+}));
+
+const classes = computed(() =>
+    cva({
+        base: [
+            'min-w-0',
+            /* When label exists but no description follows */
+            '[&>[data-ui-label]:not(:has(+[data-ui-description]))]:mb-2',
+            /* When label is followed by description */
+            '*:data-ui-description:mb-2',
+        ],
+        variants: {
+            variant: {
+                block: 'w-full',
+                inline: [
+                    'grid gap-x-3 gap-y-1.5',
+                    'has-[[data-ui-label]~[data-ui-control]]:grid-cols-[1fr_auto]',
+                    'has-[[data-ui-control]~[data-ui-label]]:grid-cols-[auto_1fr]',
+                    '[&>[data-ui-control]~[data-ui-description]]:row-start-2 [&>[data-ui-control]~[data-ui-description]]:col-start-2',
+                    '[&>[data-ui-label]~[data-ui-control]]:row-start-1 [&>[data-ui-label]~[data-ui-control]]:col-start-2',
+                ],
+            },
+            disabled: {
+                true: 'opacity-50',
+            },
         },
-    },
-})({ ...props });
+    })({ ...props }),
+);
 </script>
 
 <template>
     <div :class="[classes, $attrs.class]" data-ui-input-group>
-        <div class="flex items-center justify-between">
-            <Label v-if="label" :text="label" :for="id" :required="required" :badge="badge" class="flex-1" />
+        <div v-if="$slots.actions" class="mb-2 flex items-center justify-between gap-x-1">
+            <slot name="label">
+                <Label v-if="label" v-bind="labelProps" class="flex-1" />
+            </slot>
             <slot name="actions" />
         </div>
+        <slot v-if="!$slots.actions" name="label">
+            <Label v-if="label" v-bind="labelProps" class="flex-1" />
+        </slot>
         <Description :text="instructions" v-if="instructions && !instructionsBelow" />
         <slot />
         <Description :text="instructions" v-if="instructions && instructionsBelow" />
