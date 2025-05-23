927280e9df02e26151b40e4818539706e920e2e7
Jack McDade <jack@jackmcdade.com>
2025-05-23 16:06:09 -0400

    and this one

--- resources/js/components/fieldsets/CreateForm.vue
+++ resources/js/components/fieldsets/CreateForm.vue
@@ -1,36 +1,26 @@
 <template>
-    <div class="mx-auto mt-4 max-w-2xl">
-        <div class="rounded-sm bg-white p-6 shadow dark:bg-dark-600 dark:shadow-lg lg:px-20 lg:py-10">
-            <header class="mb-16 text-center">
-                <h1 class="mb-6">{{ __('Create Fieldset') }}</h1>
-                <p class="text-gray" v-text="__('messages.fields_fieldsets_description')" />
-            </header>
-            <div class="mb-10">
-                <label class="mb-1 text-base font-bold" for="name">{{ __('Title') }}</label>
-                <input type="text" v-model="title" class="input-text" autofocus tabindex="1" />
-                <div class="mt-2 flex items-center text-2xs text-gray-600">
-                    {{ __('messages.fieldsets_title_instructions') }}
-                </div>
+    <CreateForm
+        :title="__('Create Fieldset')"
+        :subtitle="__('messages.fields_fieldsets_description')"
+        icon="fieldsets"
+        @submit="submit"
+    >
+        <ui-card-panel :heading="__('Fieldset Details')">
+            <div class="space-y-8">
+                <ui-field :label="__('Title')" :instructions="__('messages.fieldsets_title_instructions')" :instructions-below="true">
+                    <ui-input v-model="title" autofocus tabindex="1" />
+                </ui-field>
+                <ui-field :label="__('Handle')" :instructions="__('messages.fieldsets_handle_instructions')" :instructions-below="true">
+                    <ui-input v-model="handle" tabindex="2" :loading="slug.busy" />
+                </ui-field>
             </div>
-            <div class="mb-4">
-                <label class="mb-1 text-base font-bold" for="name">{{ __('Handle') }}</label>
-                <div class="relative">
-                    <loading-graphic inline text="" v-if="slug.busy" class="absolute right-3 top-3" />
-                    <input type="text" v-model="handle" class="input-text" tabindex="2" />
-                </div>
-                <div class="mt-2 flex items-center text-2xs text-gray-600">
-                    {{ __('messages.fieldsets_handle_instructions') }}
-                </div>
-            </div>
-        </div>
-
-        <div class="mt-8 flex justify-center">
-            <button tabindex="4" class="btn-primary btn-lg mx-auto" :disabled="!canSubmit" @click="submit">
-                {{ __('Create Fieldset') }}
-            </button>
-        </div>
-    </div>
+        </ui-card-panel>
+    </CreateForm>
 </template>
+
+<script setup>
+import { CreateForm } from '@statamic/ui';
+</script>
 
 <script>
 export default {
@@ -51,12 +41,6 @@ export default {
     watch: {
         title(title) {
             this.slug.create(title).then((slug) => (this.handle = slug));
-        },
-    },
-
-    computed: {
-        canSubmit() {
-            return Boolean(this.title && this.handle && !this.slug.busy);
         },
     },
 
