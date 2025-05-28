6a683c12aaa341d71a98c2a6fc6c7e9fa54239af
Duncan McClean <duncan@duncanmcclean.com>
2025-05-28 10:20:05 +0100

    Move next to the other filter methods

--- src/Tags/Assets.php
+++ src/Tags/Assets.php
@@ -160,6 +160,22 @@ class Assets extends Tags
     }
 
     /**
+     * Filter out assets from a requested folder.
+     *
+     * @return void
+     */
+    private function filterNotIn()
+    {
+        if ($not_in = $this->params->get('not_in')) {
+            $regex = '#^('.$not_in.')#';
+
+            $this->assets = $this->assets->reject(function ($path) use ($regex) {
+                return preg_match($regex, $path);
+            });
+        }
+    }
+
+    /**
      * Perform the asset lookups.
      *
      * @param  string|array  $urls  One URL, or array of URLs.
@@ -189,20 +205,6 @@ class Assets extends Tags
         });
 
         return $this->output();
-    }
-
-    /**
-     * Filter out assets from a requested folder.
-     */
-    private function filterNotIn()
-    {
-        if ($not_in = $this->params->get('not_in')) {
-            $regex = '#^('.$not_in.')#';
-
-            $this->assets = $this->assets->reject(function ($path) use ($regex) {
-                return preg_match($regex, $path);
-            });
-        }
     }
 
     private function output()
