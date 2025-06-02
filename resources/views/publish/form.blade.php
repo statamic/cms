9c8a306623835e685f8297c62bef5bd584c16987
Duncan McClean <duncan@duncanmcclean.com>
2025-06-02 12:49:28 +0100

    Allow passing title to `PublishForm` class

--- resources/views/publish/form.blade.php
+++ resources/views/publish/form.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <ui-publish-form
-        title="Generic Publish Form"
+        title="{{ $title }}"
         :blueprint="{{ Js::from($blueprint) }}"
         :initial-values="{{ Js::from($values) }}"
         :initial-meta="{{ Js::from($meta) }}"
