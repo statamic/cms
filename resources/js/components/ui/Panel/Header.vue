ac55837f418f4c2786d85f694781d366f14a6639
Jack McDade <jack@jackmcdade.com>
2025-06-02 11:37:07 -0400

    Remove unintentional border

--- resources/js/components/ui/Panel/Header.vue
+++ resources/js/components/ui/Panel/Header.vue
@@ -7,7 +7,7 @@ const props = defineProps({
 </script>
 
 <template>
-    <header class="px-4.5 py-2 antialiased border border-black" data-ui-panel-header>
+    <header class="px-4.5 py-2 antialiased" data-ui-panel-header>
         <Heading v-text="props.title" v-if="props.title" />
         <slot v-else />
     </header>
