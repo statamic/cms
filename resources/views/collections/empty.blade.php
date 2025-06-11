20571e3f73013e2a5b15fb703263e99221b58ff5
Duncan McClean <duncan@duncanmcclean.com>
2025-06-11 17:45:58 +0100

    Indent everything or nothing

--- resources/views/collections/empty.blade.php
+++ resources/views/collections/empty.blade.php
@@ -7,20 +7,20 @@
 
 @section('content')
 
-<header class="mb-6">
-    <h1 v-pre>{{ __($collection->title()) }}</h1>
-</header>
+    <header class="mb-6">
+        <h1 v-pre>{{ __($collection->title()) }}</h1>
+    </header>
 
-<collection-empty-state
-    :collection='@json($collection)'
-    :create-label="{{ $collection->createLabel() }}"
-    :blueprints="{{ Js::from($blueprints) }}"
-    site="{{ $site }}"
-    :can-edit="{{ $str::bool($user->can('edit', $collection)) }}"
-    :can-create="{{ $str::bool($user->can('create', ['Statamic\Contracts\Entries\Entry', $collection, \Statamic\Facades\Site::get($site)])) }}"
-    :can-configure-fields="{{ $str::bool($user->can('configure fields')) }}"
-    :can-store="{{ $str::bool($user->can('create', 'Statamic\Contracts\Entries\Collection')) }}"
-></collection-empty-state>
+    <collection-empty-state
+        :collection='@json($collection)'
+        :create-label="{{ $collection->createLabel() }}"
+        :blueprints="{{ Js::from($blueprints) }}"
+        site="{{ $site }}"
+        :can-edit="{{ $str::bool($user->can('edit', $collection)) }}"
+        :can-create="{{ $str::bool($user->can('create', ['Statamic\Contracts\Entries\Entry', $collection, \Statamic\Facades\Site::get($site)])) }}"
+        :can-configure-fields="{{ $str::bool($user->can('configure fields')) }}"
+        :can-store="{{ $str::bool($user->can('create', 'Statamic\Contracts\Entries\Collection')) }}"
+    ></collection-empty-state>
 
     @include('statamic::partials.docs-callout', [
         'topic' => __('Collections'),
