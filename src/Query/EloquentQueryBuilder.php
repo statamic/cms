c071cd205827c3ea32487ba5d4f64c11f6fab853
Martin Dub <49438284+martinoak@users.noreply.github.com>
2025-06-12 10:05:08 +0200

    Prevent null in strtolower()
    Since PHP 8.2, passing null to parameter #1 ($string) of type string in strtolower() is deprecated, this throws the error. A simple not null check fixes the problem.

--- src/Query/EloquentQueryBuilder.php
+++ src/Query/EloquentQueryBuilder.php
@@ -160,7 +160,7 @@ abstract class EloquentQueryBuilder implements Builder
             return $this->whereNested($column, $boolean);
         }
 
-        if (strtolower($operator) == 'like') {
+        if ($operator !== null && strtolower($operator) == 'like') {
             $grammar = $this->builder->getConnection()->getQueryGrammar();
             $this->builder->whereRaw('LOWER('.$grammar->wrap($this->column($column)).') LIKE ?', strtolower($value), $boolean);
 
