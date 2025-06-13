c7a1713cec42ea3b9f3a422b96fda73c10580ec0
Duncan McClean <duncan@duncanmcclean.com>
2025-06-13 15:46:47 +0100

    Make sure the empty state item is a div/button/a a the right times

--- resources/js/components/ui/EmptyState/Item.vue
+++ resources/js/components/ui/EmptyState/Item.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { CardPanel, Icon } from '@statamic/ui';
+import { useSlots } from 'vue';
 
 const props = defineProps({
     href: {
@@ -19,18 +20,21 @@ const props = defineProps({
         default: '',
     },
 });
+
+const slots = useSlots();
+const hasSlot = !!slots.default;
 </script>
 
 <template>
     <component
-        :is="href ? 'a' : 'div'"
+        :is="hasSlot ? 'div' : (href ? 'a' : 'button')"
         :href="href"
         class="w-full flex gap-2 px-3 py-4 pt-5 pb-6.5 items-start hover:bg-gray-100 dark:hover:bg-dark-550 rounded-md group"
     >
         <div class="[&_svg]:size-9 [&_svg]:text-gray-500 rtl:ml-4 ltr:mr-4 text-gray-800 dark:text-dark-175">
             <Icon :name="icon" />
         </div>
-        <div class="flex-1 mb-4 md:mb-0 md:rtl:ml-6 md:ltr:mr-6">
+        <div class="flex-1 mb-4 md:mb-0 md:rtl:ml-6 md:ltr:mr-6 text-start">
             <h3 class="text-lg font-medium trim-text-start text-grey-800 mb-1.5" v-text="heading"></h3>
             <p v-if="description" class="text-gray-500 text-sm" v-text="description"></p>
             <slot />
