c5ab111b3193b6d0e30ab9a20a74d95c465b8bde
Duncan McClean <duncan@duncanmcclean.com>
2025-06-13 16:51:32 +0100

    Wire up form submission publish form
    It's not using the `PublishForm` abstraction because we need to control the title using JavaScript.

--- resources/views/forms/submission.blade.php
+++ resources/views/forms/submission.blade.php
@@ -2,11 +2,13 @@
 @section('title', Statamic::crumb('Submission ' . $submission->id(), $submission->form->title(), 'Forms'))
 
 @section('content')
-    <publish-form
+    <ui-publish-form
+        icon="forms"
         :title="$date.format('{{ $submission->date() }}')"
         :blueprint="{{ Js::from($blueprint) }}"
-        :meta="{{ Js::from($meta) }}"
-        :values="{{ Js::from($values) }}"
+        :initial-values="{{ Js::from($values) }}"
+        :initial-meta="{{ Js::from($meta) }}"
+        :submit-url="null"
         read-only
-    ></publish-form>
+    ></ui-publish-form>
 @endsection
