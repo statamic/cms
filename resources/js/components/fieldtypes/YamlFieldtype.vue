15ff7b699b32507ea66f2a88e0bdd8fdf183e167
Duncan McClean <duncan@duncanmcclean.com>
2025-06-17 17:47:45 +0100

    Refactor YAML fieldtype to use `CodeEditor` primitive

--- resources/js/components/fieldtypes/YamlFieldtype.vue
+++ resources/js/components/fieldtypes/YamlFieldtype.vue
@@ -1,61 +1,25 @@
 <template>
-    <div class="yaml-fieldtype-container relative">
-        <div class="code-mode">YAML</div>
-        <div ref="codemirror"></div>
-    </div>
+    <CodeEditor
+        ref="codeEditor"
+        :theme="config.theme"
+        :disabled="isReadOnly"
+        :tab-size="2"
+        :indent-type="'spaces'"
+        :allow-mode-selection="false"
+        :show-mode-label="false"
+        :mode="'yaml'"
+        :model-value="value"
+        @update:model-value="update"
+    />
 </template>
 
 <script>
 import Fieldtype from './Fieldtype.vue';
-import CodeMirror from 'codemirror';
-import 'codemirror/mode/yaml/yaml';
+import { CodeEditor } from '@statamic/ui';
 
 export default {
     mixins: [Fieldtype],
 
-    data() {
-        return {
-            codemirror: null,
-        };
-    },
-
-    computed: {
-        readOnlyOption() {
-            return this.isReadOnly ? 'nocursor' : false;
-        },
-    },
-
-    mounted() {
-        this.codemirror = CodeMirror(this.$refs.codemirror, {
-            value: this.value || '',
-            mode: 'yaml',
-            direction: document.querySelector('html').getAttribute('dir') ?? 'ltr',
-            tabSize: 2,
-            indentUnit: 2,
-            autoRefresh: true,
-            indentWithTabs: false,
-            lineNumbers: true,
-            lineWrapping: true,
-            readOnly: this.readOnlyOption,
-            theme: this.config.theme || 'material',
-            inputStyle: 'contenteditable',
-        });
-
-        this.codemirror.on('change', (cm) => {
-            this.updateDebounced(cm.doc.getValue());
-        });
-    },
-
-    watch: {
-        readOnlyOption(val) {
-            this.codemirror.setOption('readOnly', val);
-        },
-    },
-
-    methods: {
-        focus() {
-            this.codemirror.focus();
-        },
-    },
+    components: { CodeEditor },
 };
 </script>
