cd367cb797d6fe6445e96f210389b19da7ba5fe0
Jesse Leite <jesseleite@gmail.com>
2025-06-19 16:35:31 -0400

    Remove confusing `prependSiteRoot()` method (which is basically a duplicate of `prependSiteUrl()`).

--- src/Facades/Endpoint/URL.php
+++ src/Facades/Endpoint/URL.php
@@ -119,24 +119,6 @@ class URL
     }
 
     /**
-     * Make sure the site root is prepended to a URL.
-     */
-    public function prependSiteRoot(?string $url, ?string $locale = null, bool $controller = true): string
-    {
-        // Backwards compatibility fix:
-        // 2.1 added the $locale argument in the second position to match prependSiteurl.
-        // Before 2.1, the second argument was controller. We'll handle that here.
-        if ($locale === true || $locale === false) {
-            $controller = $locale;
-            $locale = null;
-        }
-
-        return self::makeRelative(
-            self::prependSiteUrl($url, $locale, $controller)
-        );
-    }
-
-    /**
      * Make sure the site root url is prepended to a URL.
      */
     public function prependSiteUrl(?string $url, ?string $locale = null, bool $controller = true): string
