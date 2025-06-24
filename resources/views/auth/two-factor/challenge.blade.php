15bc2bc7e53b4b09b428db4dbaa57e4731e67e8c
Jack McDade <jack@jackmcdade.com>
2025-06-24 15:26:44 -0400

    Style the 2fa screens

--- resources/views/auth/two-factor/challenge.blade.php
+++ resources/views/auth/two-factor/challenge.blade.php
@@ -4,20 +4,18 @@
 
 @inject('str', 'Statamic\Support\Str')
 @extends('statamic::outside')
-@section('title', __('Two Factor Authentication'))
+@section('title', __('Two-Factor Authentication'))
 
 @section('content')
-    @include('statamic::partials.outside-logo')
-
-    <div class="relative mx-auto flex max-w-xs items-center justify-center rounded shadow-lg">
-        <div class="outside-shadow absolute inset-0"></div>
-        <div class="card auth-card">
-            <two-factor-challenge
-                initial-mode="{{ $mode }}"
-                :errors="{{ $errors->isEmpty() ? '{}' : json_encode($errors->getMessages()) }}"
-                csrf-token="{{ csrf_token() }}"
-                redirect="{{ request('redirect') }}"
-            ></two-factor-challenge>
+    <div class="relative mx-auto max-w-[400px] items-center justify-center pt-20">
+        <div class="flex items-center justify-center py-6">
+            <x-statamic::outside-logo />
         </div>
+        <two-factor-challenge
+            initial-mode="{{ $mode }}"
+            :errors="{{ $errors->isEmpty() ? '{}' : json_encode($errors->getMessages()) }}"
+            csrf-token="{{ csrf_token() }}"
+            redirect="{{ request('redirect') }}"
+        ></two-factor-challenge>
     </div>
 @endsection
--- resources/views/auth/two-factor/setup.blade.php
+++ resources/views/auth/two-factor/setup.blade.php
@@ -4,28 +4,29 @@
 
 @inject('str', 'Statamic\Support\Str')
 @extends('statamic::outside')
-@section('title', __('Set up Two Factor Authentication'))
+@section('title', __('Two-Factor Authentication'))
 
 @section('content')
-    @include('statamic::partials.outside-logo')
-
-    <div class="relative mx-auto flex max-w-xs items-center justify-center rounded shadow-lg">
-        <div class="outside-shadow absolute inset-0"></div>
-        <div class="card auth-card">
-            <div>
-                <div class="email-login select-none">
-                    <h1 class="mb-2 text-lg text-gray-800 dark:text-dark-175">
+    <div class="relative mx-auto max-w-[400px] items-center justify-center pt-20 scheme-light">
+        <div class="flex items-center justify-center py-6">
+            <x-statamic::outside-logo />
+        </div>
+        <div class="bg-white backdrop-blur-[2px] border border-gray-200 rounded-2xl p-2 shadow-[0_8px_5px_-6px_rgba(0,0,0,0.12),_0_3px_8px_0_rgba(0,0,0,0.02),_0_30px_22px_-22px_rgba(39,39,42,0.35)]">
+            <div class="relative space-y-3 rounded-xl border border-gray-300 bg-white p-4 shadow-[0_1px_16px_-2px_rgba(63,63,71,0.2)]">
+                <header class="flex flex-col justify-center items-center mb-8 py-3">
+                    <ui-card class="p-2! mb-4 flex items-center justify-center">
+                        <ui-icon name="phone-lock" class="size-5" />
+                    </ui-card>
+                    <ui-heading :level="1" size="xl">
                         {{ __('Set up Two Factor Authentication') }}
-                    </h1>
-                    <p class="mb-4 text-sm text-gray dark:text-dark-175">
-                        {{ __('statamic::messages.two_factor_account_requirement') }}
-                    </p>
+                    </ui-heading>
+                    <ui-description :text="__('statamic::messages.two_factor_account_requirement')" class="text-center" />
+                </header>
 
-                    <enable-two-factor-authentication
-                        :routes="{{ json_encode($routes) }}"
-                        redirect="{{ $redirect }}"
-                    />
-                </div>
+                <enable-two-factor-authentication
+                    :routes="{{ json_encode($routes) }}"
+                    redirect="{{ $redirect }}"
+                />
             </div>
         </div>
     </div>
