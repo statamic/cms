079531a7c2c9966289f6f92938dfd7783a908388
Jason Varga <jason@pixelfear.com>
2025-06-27 12:25:30 -0400

    blueprint listings use new listings

--- resources/js/components/blueprints/Listing.vue
+++ resources/js/components/blueprints/Listing.vue
@@ -1,64 +1,70 @@
-<template>
-    <data-list :visible-columns="columns" :columns="columns" :rows="rows" :sort="false" v-slot="{ filteredRows: rows }">
-        <ui-panel>
-            <data-list-table :reorderable="reorderable" @reordered="$emit('reordered', $event)">
-                <template #cell-title="{ row: blueprint }">
-                    <div class="flex items-center">
-                        <div
-                            class="little-dot ltr:mr-2 rtl:ml-2"
-                            :class="[blueprint.hidden ? 'hollow' : 'bg-green-600']"
-                        />
-                        <a :href="blueprint.edit_url">{{ __(blueprint.title) }}</a>
-                    </div>
-                </template>
-                <template #cell-handle="{ value }">
-                    <span class="font-mono text-xs">{{ value }}</span>
-                </template>
-                <template #actions="{ row: blueprint, index }">
-                    <Dropdown placement="left-start" class="me-3">
-                        <DropdownMenu>
-                            <DropdownItem :text="__('Edit')" icon="edit" :href="blueprint.edit_url" />
-                            <DropdownItem :text="__('Delete')" icon="trash" variant="destructive" @click="$refs[`deleter_${blueprint.id}`].confirm()" />
-                        </DropdownMenu>
-                    </Dropdown>
+<script setup>
+import { ref, watch } from 'vue';
+import { DropdownItem, Listing } from '@statamic/ui';
 
-                    <resource-deleter
-                        :ref="`deleter_${blueprint.id}`"
-                        :resource="blueprint"
-                        @deleted="removeRow(blueprint)"
-                    />
-                </template>
-            </data-list-table>
-        </ui-panel>
-    </data-list>
-</template>
+defineEmits(['reordered']);
 
-<script>
-import Listing from '../Listing.vue';
-import { Dropdown, DropdownItem, DropdownLabel, DropdownMenu, DropdownSeparator } from '@statamic/ui';
+const props = defineProps(['initialRows', 'reorderable']);
 
-export default {
-    mixins: [Listing],
+const rows = ref(props.initialRows);
 
-    components: { Dropdown, DropdownMenu, DropdownLabel, DropdownSeparator, DropdownItem },
+const columns = ref([
+    { label: __('Title'), field: 'title' },
+    { label: __('Handle'), field: 'handle' },
+    { label: __('Fields'), field: 'fields' },
+]);
 
-    props: ['initialRows', 'reorderable'],
+watch(
+    () => props.initialRows,
+    (newRows) => (rows.value = newRows),
+    { deep: true },
+);
 
-    data() {
-        return {
-            rows: this.initialRows,
-            columns: [
-                { label: __('Title'), field: 'title' },
-                { label: __('Handle'), field: 'handle' },
-                { label: __('Fields'), field: 'fields' },
-            ],
-        };
-    },
+function reloadPage() {
+    window.location.reload();
+}
 
-    watch: {
-        initialRows(rows) {
-            this.rows = rows;
-        },
-    },
-};
+function removeRow(row) {
+    const i = rows.value.findIndex((r) => r.id === row.id);
+    rows.value.splice(i, 1);
+}
 </script>
+
+<template>
+    <Listing
+        :items="rows"
+        :columns="columns"
+        :allow-search="false"
+        :allow-customizing-columns="false"
+        :reorderable="reorderable"
+        :sortable="false"
+        :allow-actions-while-reordering="true"
+        @refreshing="reloadPage"
+        @reordered="$emit('reordered', $event)"
+    >
+        <template #cell-title="{ row: blueprint }">
+            <div class="flex items-center">
+                <div class="little-dot ltr:mr-2 rtl:ml-2" :class="[blueprint.hidden ? 'hollow' : 'bg-green-600']" />
+                <a :href="blueprint.edit_url">{{ __(blueprint.title) }}</a>
+
+                <resource-deleter
+                    :ref="`deleter_${blueprint.id}`"
+                    :resource="blueprint"
+                    @deleted="removeRow(blueprint)"
+                />
+            </div>
+        </template>
+        <template #cell-handle="{ value }">
+            <span class="font-mono text-xs">{{ value }}</span>
+        </template>
+        <template #prepended-row-actions="{ row: blueprint }">
+            <DropdownItem :text="__('Edit')" icon="edit" :href="blueprint.edit_url" />
+            <DropdownItem
+                :text="__('Delete')"
+                icon="trash"
+                variant="destructive"
+                @click="$refs[`deleter_${blueprint.id}`].confirm()"
+            />
+        </template>
+    </Listing>
+</template>
