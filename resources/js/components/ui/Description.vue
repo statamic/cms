8396ee20d9505657782398d0dd9d94d9276939bb
Jay George <contact@jaygeorge.co.uk>
2025-06-30 12:41:15 +0100

    Improve how the switch is "floated" right

--- resources/js/components/ui/Description.vue
+++ resources/js/components/ui/Description.vue
@@ -11,7 +11,7 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="text-[0.8125rem] font-normal tracking-tight text-gray-500 dark:text-gray-400 trim-text-start [[data-ui-panel-header]_&]:text-gray-600" data-ui-description>
+    <div class="text-xs font-normal tracking-tight text-gray-500 dark:text-gray-400 trim-text-start [[data-ui-panel-header]_&]:text-gray-600" data-ui-description>
         <slot v-if="hasDefaultSlot" />
         <span v-else v-html="text" />
     </div>
