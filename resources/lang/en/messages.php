ec9ff34932066505b9300f826fe737f49aac9d2d
Jack McDade <jack@jackmcdade.com>
2025-06-30 15:35:07 -0400

    Empty Navs state

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -185,7 +185,7 @@ return [
     'navigation_configure_blueprint_instructions' => 'Choose from existing Blueprints or create a new one.',
     'navigation_configure_collections_instructions' => 'Enable linking to entries in these collections.',
     'navigation_configure_handle_instructions' => 'Used to reference this navigation on the frontend. It\'s non-trivial to change later.',
-    'navigation_configure_intro' => 'Navigations are multi-level lists of links that can be used to build navbars, footers, sitemaps, and other forms of frontend navigation.',
+    'navigation_configure_intro' => 'Create multi-level lists of links for navbars, footers, sitemaps, and other forms of site navigation.',
     'navigation_configure_select_across_sites' => 'Allow selecting entries from other sites.',
     'navigation_configure_settings_intro' => 'Enable linking to collections, set a max depth, and other behaviors.',
     'navigation_configure_title_instructions' => 'Use a name that matches where it will be used, like \'Main Nav\' or \'Footer Nav\'',
---
+++ resources/views/navigation/empty.blade.php
@@ -0,0 +1,28 @@
+@php use function Statamic\trans as __; @endphp
+
+@extends('statamic::layout')
+@section('title', Statamic::crumb('Navigation'))
+@section('content-card-modifiers', 'bg-architectural-lines')
+
+@section('content')
+    <header class="py-8 mt-8 text-center">
+        <h1 class="text-[25px] font-medium antialiased flex justify-center items-center gap-2">
+            <ui-icon name="navigation" class="size-5 text-gray-500"></ui-icon>
+            {{ __('Navigation') }}
+        </h1>
+    </header>
+
+    <ui-empty-state-menu heading="{{ __('statamic::messages.navigation_configure_intro') }}">
+        <ui-empty-state-item
+            href="{{ cp_route('navigation.create') }}"
+            icon="navigation"
+            heading="{{ __('Create a Navigation') }}"
+            description="{{ __('Get started by creating your first navigation.') }}"
+        ></ui-empty-state-item>
+    </ui-empty-state-menu>
+
+    <x-statamic::docs-callout
+        :topic="__('Navigation')"
+        :url="Statamic::docsUrl('navigation')"
+    />
+@endsection
