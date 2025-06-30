21cf576e38b8fb79e2ae345b5b06808111ece8a9
Jesse Leite <jesseleite@gmail.com>
2025-06-30 15:35:22 -0400

    Update method signatures in URL facade docblock.

--- src/Facades/URL.php
+++ src/Facades/URL.php
@@ -5,28 +5,26 @@ namespace Statamic\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * TODO: Update these...
- *
- * @method static string tidy($url)
- * @method static string assemble($args)
- * @method static string slug($url)
- * @method static string replaceSlug($url, $slug)
- * @method static string parent($url)
- * @method static bool isAncestor($parent_uri, $uri = null)
- * @method static string prependSiteRoot($url, $locale = null, $controller = true)
- * @method static string prependSiteUrl($url, $locale = null, $controller = true)
- * @method static string removeSiteUrl($url)
- * @method static string makeRelative($url)
- * @method static string makeAbsolute($url)
- * @method static string getCurrent()
- * @method static string format($url)
- * @method static bool isExternal($url)
- * @method static bool isExternalToApplication($url)
- * @method static string getSiteUrl()
- * @method static string encode($url)
- * @method static mixed getDefaultUri($locale, $uri)
- * @method static string gravatar($email, $size = null)
+ * @method static void enforceTrailingSlashes(bool $bool = true)
  * @method static bool isEnforcingTrailingSlashes()
+ * @method static string tidy(?string $url, ?bool $external = false, ?bool $withTrailingSlash = null)
+ * @method static string assemble(?string ...$segments)
+ * @method static ?string slug(?string $url)
+ * @method static string replaceSlug(?string $url, string $slug)
+ * @method static string parent(?string $url)
+ * @method static bool isAncestorOf(?string $child, ?string $ancestor)
+ * @method static string prependSiteUrl(?string $url, ?string $locale = null, bool $controller = true)
+ * @method static string removeSiteUrl(?string $url)
+ * @method static string makeRelative(?string $url)
+ * @method static string makeAbsolute(?string $url)
+ * @method static string getCurrent()
+ * @method static bool isAbsolute(?string $url)
+ * @method static bool isExternal(?string $url)
+ * @method static bool isExternalToApplication(?string $url)
+ * @method static string encode(?string $url)
+ * @method static string gravatar(string $email, ?int $size = null)
+ * @method static ?string removeQueryAndFragment(?string $url)
+ * @method static void clearUrlCache()
  *
  * @see \Statamic\Facades\Endpoint\Url
  */
