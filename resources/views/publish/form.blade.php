e3f29171c6f665ac58326ad94b8ff236d8bc93f1
Jason Varga <jason@pixelfear.com>
2025-07-03 13:46:48 -0400

    Wrap the generic form in a max width if youre using it for config

--- resources/views/publish/form.blade.php
+++ resources/views/publish/form.blade.php
@@ -3,15 +3,17 @@
 @extends('statamic::layout')
 
 @section('content')
-    <ui-publish-form
-        @if ($icon)icon="{{ $icon }}"@endif
-        title="{{ $title }}"
-        :blueprint="{{ Js::from($blueprint) }}"
-        :initial-values="{{ Js::from($values) }}"
-        :initial-meta="{{ Js::from($meta) }}"
-        submit-url="{{ $submitUrl }}"
-        submit-method="{{ $submitMethod }}"
-        :read-only="{{ Js::from($readOnly) }}"
-        :as-config="{{ Str::bool($usingConfigLayout) }}"
-    ></ui-publish-form>
+    <div class="@if ($usingConfigLayout) max-w-5xl mx-auto @endif">
+        <ui-publish-form
+            @if ($icon)icon="{{ $icon }}"@endif
+            title="{{ $title }}"
+            :blueprint="{{ Js::from($blueprint) }}"
+            :initial-values="{{ Js::from($values) }}"
+            :initial-meta="{{ Js::from($meta) }}"
+            submit-url="{{ $submitUrl }}"
+            submit-method="{{ $submitMethod }}"
+            :read-only="{{ Js::from($readOnly) }}"
+            :as-config="{{ Str::bool($usingConfigLayout) }}"
+        ></ui-publish-form>
+    </div>
 @endsection
