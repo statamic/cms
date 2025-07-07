73ca5f66385445893c30524cbb97c290a32008a2
Jay George <contact@jaygeorge.co.uk>
2025-07-07 11:42:57 +0100

    Empty dashboard view -- tweaks

--- resources/js/components/ui/EmptyState/Item.vue
+++ resources/js/components/ui/EmptyState/Item.vue
@@ -29,9 +29,9 @@ const hasSlot = !!slots.default;
     <component
         :is="hasSlot ? 'div' : (href ? 'a' : 'button')"
         :href="href"
-        class="w-full flex gap-2 px-3 py-4 pt-5 pb-6.5 items-start hover:bg-gray-100 dark:hover:bg-gray-800 rounded-md group"
+        class="w-full flex gap-2 px-3 py-4 pt-4 pb-5.5 items-start hover:bg-gray-100 dark:hover:bg-gray-800 rounded-md group"
     >
-        <Icon :name="icon" class="size-6 me-4 mt-1 text-gray-500" />
+        <Icon :name="icon" class="size-6 me-4 mt-1 text-gray-400" />
         <div class="flex-1 mb-4 md:mb-0 me-6 text-start">
             <ui-heading size="xl" :level="3" :text="heading" class="mb-1.5" />
             <ui-description v-if="description" :text="description" />
--- resources/views/forms/empty.blade.php
+++ resources/views/forms/empty.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
     <header class="py-8 mt-8 text-center">
-        <h1 class="text-[25px] font-medium antialiased flex justify-center items-center gap-2">
+        <h1 class="text-[25px] font-medium antialiased flex justify-center items-center gap-3">
             <ui-icon name="collections" class="size-5 text-gray-500"></ui-icon>
             {{ __('Forms') }}
         </h1>
