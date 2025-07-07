393988195c0199b8b98391eab7ad881753a420e2
Duncan McClean <duncan@duncanmcclean.com>
2025-07-07 11:43:11 +0100

    Optionally enable it based on environment variable

--- resources/js/bootstrap/statamic.js
+++ resources/js/bootstrap/statamic.js
@@ -147,7 +147,7 @@ export default {
         this.$app.use(PortalVue, { portalName: 'v-portal' });
         this.$app.use(VueClickAway);
         this.$app.use(FloatingVue, { disposeTimeout: 30000, distance: 10 });
-        this.$app.use(VueComponentDebug);
+        this.$app.use(VueComponentDebug, { enabled: import.meta.env.VITE_VUE_COMPONENT_DEBUG === 'true' });
 
         const portals = markRaw(new Portals());
 
