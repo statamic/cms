b0036ec76b12f9408fdfef53be7853e760a87c03
Jay George <contact@jaygeorge.co.uk>
2025-07-07 11:44:56 +0100

    Remove unnecessary class

--- resources/js/components/ui/EmptyState/Item.vue
+++ resources/js/components/ui/EmptyState/Item.vue
@@ -29,7 +29,7 @@ const hasSlot = !!slots.default;
     <component
         :is="hasSlot ? 'div' : (href ? 'a' : 'button')"
         :href="href"
-        class="w-full flex gap-2 px-3 py-4 pt-4 pb-5.5 items-start hover:bg-gray-100 dark:hover:bg-gray-800 rounded-md group"
+        class="w-full flex gap-2 px-3 pt-4 pb-5.5 items-start hover:bg-gray-100 dark:hover:bg-gray-800 rounded-md group"
     >
         <Icon :name="icon" class="size-6 me-4 mt-1 text-gray-400" />
         <div class="flex-1 mb-4 md:mb-0 me-6 text-start">
