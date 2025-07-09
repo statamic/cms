ec08a5f93814d0246606f321e375c28fc3191eb8
Jack McDade <jack@jackmcdade.com>
2025-07-09 09:37:48 -0400

    Style links in fieldtype help text

--- resources/js/components/ui/Description.vue
+++ resources/js/components/ui/Description.vue
@@ -11,7 +11,7 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="text-sm font-normal text-gray-500 dark:text-gray-400 trim-text-start [[data-ui-panel-header]_&]:text-gray-600" data-ui-description>
+    <div class="text-sm font-normal text-gray-500 dark:text-gray-400 trim-text-start [[data-ui-panel-header]_&]:text-gray-600 [&_a]:underline [&_a:hover]:text-gray-700 dark:[&_a:hover]:text-gray-300" data-ui-description>
         <slot v-if="hasDefaultSlot" />
         <span v-else v-html="text" />
     </div>
