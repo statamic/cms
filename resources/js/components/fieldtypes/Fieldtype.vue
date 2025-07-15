bfab1dcc1c3f377c851e668efc0b42678b46ccf9
Jason Varga <jason@pixelfear.com>
2025-07-15 14:39:11 -0400

    Add publishContainer computed that unwraps an the publish container injection

--- resources/js/components/fieldtypes/Fieldtype.vue
+++ resources/js/components/fieldtypes/Fieldtype.vue
@@ -3,6 +3,8 @@ import HasFieldActions from '../field-actions/HasFieldActions';
 import debounce from '@statamic/util/debounce.js';
 import props from './props.js';
 import emits from './emits.js';
+import { containerContextKey } from '@statamic/components/ui/Publish/Container.vue';
+import { isRef } from 'vue';
 
 export default {
     emits,
@@ -17,6 +19,9 @@ export default {
         fieldActionStoreName: {
             from: 'storeName',
             default: null,
+        },
+        injectedPublishContainer: {
+            from: containerContextKey
         },
     },
 
@@ -37,6 +42,20 @@ export default {
     },
 
     computed: {
+        publishContainer() {
+            // The injectedPublishContainer contains refs. We'll unwrap everything so that we can do
+            // this.publishContainer.someValue instead of this.publishContainer.someValue.value
+            // When using the Options API, this feels more natural. However since this is a
+            // computed, it won't be avaialble within data(). In those cases you will
+            // need to use this.injectedPublishContainer.someValue.value directly.
+            return Object.fromEntries(
+               Object.entries(this.injectedPublishContainer).map(([key, value]) => [
+                   key,
+                   isRef(value) ? value.value : value,
+               ])
+           );
+        },
+
         name() {
             if (this.namePrefix) {
                 return `${this.namePrefix}[${this.handle}]`;
