statamic/cms

commit c547b72234a5338c1d6f02a4e01d92be72021ab9

Blueprint Builder: Use ui tab components

src/Fieldtypes/Sets.php
@@ -14,7 +14,6 @@ class Sets extends Fieldtype
     protected $selectable = false;
 
     protected static $iconsDirectory = null;
-    protected static $iconsFolder = 'plump';
 
     /**
      * Converts the "sets" array of a Replicator (or Bard) field into what the
@@ -157,7 +156,7 @@ class Sets extends Fieldtype
         // Then provide to script for <icon-fieldtype> selector components in blueprint config
         Statamic::provideToScript([
             'setIconsDirectory' => static::$iconsDirectory,
-            'setIconsFolder' => static::$iconsFolder,
+            'setIconsFolder' => null,
         ]);
 
         // And finally, provide the file contents of all custom svg icons to script,

resources/js/components/blueprints/Tab.vue
@@ -1,16 +1,6 @@
 <template>
-    <button
-        class="blueprint-tab tab-button"
-        role="tab"
-        :class="{ active: isActive }"
-        :aria-controls="`tab-panel-${tab._id}`"
-        :aria-selected="isActive"
-        :id="`tab-${tab._id}`"
-        :tabindex="isActive ? 0 : -1"
-        @click="$emit('selected')"
-        @mouseenter="$emit('mouseenter')"
-    >
-        <svg-icon
+    <TabTrigger :name="tab._id">
+        <Icon
             v-if="tab.icon"
             :name="iconName(tab.icon)"
             :directory="iconBaseDirectory"
@@ -22,10 +12,10 @@
         <Dropdown v-if="isActive" placement="left-start" class="me-3">
             <template #trigger>
                 <button
-                    class="hover:text-gray-900 active:text-gray-900 dark:hover:text-gray-400 ltr:ml-2 rtl:mr-2"
+                    class="hover:text-gray-900 active:text-gray-900 dark:hover:text-gray-400 ms-1"
                     :aria-label="__('Open Dropdown')"
                 >
-                    <svg-icon name="micro/chevron-down-xs" class="w-2" />
+                    <Icon name="ui/chevron-down" />
                 </button>
             </template>
             <DropdownMenu>
@@ -37,42 +27,28 @@
         <confirmation-modal
             v-if="editing"
             :title="editText"
-            @opened="$refs.title.focus()"
+            @opened="() => {
+                this.$nextTick(() => {
+                    this.$refs.title.focus()
+                });
+            }"
             @confirm="editConfirmed"
             @cancel="editCancelled"
         >
-            <div class="publish-fields @container">
-                <div class="form-group w-full">
-                    <label v-text="__('Title')" />
-                    <input
-                        ref="title"
-                        type="text"
-                        :value="display"
-                        @input="fieldUpdated('display', $event.target.value)"
-                        class="input-text"
-                    />
-                </div>
-                <div class="form-group w-full">
-                    <label v-text="__('Handle')" />
-                    <input
-                        type="text"
-                        :value="handle"
-                        @input="fieldUpdated('handle', $event.target.value)"
-                        class="input-text font-mono text-sm"
-                    />
-                </div>
-                <div class="form-group w-full" v-if="showInstructions">
-                    <label v-text="__('Instructions')" />
-                    <input
-                        type="text"
-                        :value="instructions"
-                        @input="fieldUpdated('instructions', $event.target.value)"
-                        class="input-text text-sm"
-                    />
-                </div>
+            <div class="publish-fields">
+                <Field :label="__('Title')" class="form-group field-w-100">
+                    <Input ref="title" autofocus :model-value="display" @update:model-value="fieldUpdated('display', $event.target.value)" />
+                </Field>
 
-                <div class="form-group w-full" v-if="showInstructions">
-                    <label v-text="__('Icon')" />
+                <Field :label="__('Handle')" class="form-group field-w-100">
+                    <Input class="font-mono" :model-value="handle" @update:model-value="fieldUpdated('handle', $event.target.value)" />
+                </Field>
+
+                <Field v-if="showInstructions" :label="__('Instructions')" class="form-group field-w-100">
+                    <Input :model-value="instructions" @update:model-value="fieldUpdated('instructions', $event.target.value)" />
+                </Field>
+
+                <Field v-if="showInstructions" :label="__('Icon')" class="form-group field-w-100">
                     <publish-field-meta
                         :config="{
                             handle: 'icon',
@@ -89,20 +65,20 @@
                             :config="config"
                             :meta="meta"
                             :value="value"
-                            @input="fieldUpdated('icon', $event)"
+                            @update:value="fieldUpdated('icon', $event)"
                         />
                     </publish-field-meta>
-                </div>
+                </Field>
             </div>
         </confirmation-modal>
-    </button>
+    </TabTrigger>
 </template>
 
 <script>
-import { Dropdown, DropdownMenu, DropdownItem } from '@statamic/ui';
+import { TabTrigger, Dropdown, DropdownMenu, DropdownItem, Icon, Field, Input } from '@statamic/ui';
 
 export default {
-    components: { Dropdown, DropdownMenu, DropdownItem },
+    components: { TabTrigger, Dropdown, DropdownMenu, DropdownItem, Icon, Field, Input },
 
     props: {
         tab: {

resources/js/components/blueprints/TabContent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div role="tabpanel" :aria-labelledby="`tab-${tab._id}`" :id="`tab-panel-${tab._id}`">
+    <TabContent :name="tab._id">
         <sections
             ref="sections"
             :tab-id="tab._id"
@@ -12,18 +12,20 @@
             :can-define-localizable="canDefineLocalizable"
             @updated="sectionsUpdated($event)"
         />
-    </div>
+    </TabContent>
 </template>
 
 <script>
 import Sections from './Sections.vue';
 import CanDefineLocalizable from '../fields/CanDefineLocalizable';
+import { TabContent } from '@statamic/ui';
 
 export default {
     mixins: [CanDefineLocalizable],
 
     components: {
         Sections,
+        TabContent,
     },
 
     props: {

resources/js/components/blueprints/Tabs.vue
@@ -1,67 +1,70 @@
 <template>
     <div>
-        <div v-if="!singleTab && tabs.length > 0" class="tabs-container relative">
-            <div
-                ref="tabs"
-                class="tabs flex flex-1 space-x-3 overflow-auto pe-6"
-                role="tablist"
-            >
-                <tab
-                    ref="tab"
+        <Button
+            v-if="!singleTab && tabs.length === 0"
+            :text="addTabText"
+            @click="addAndEditTab"
+        />
+
+        <div v-if="!singleTab && tabs.length > 0">
+            <Tabs v-model="currentTab">
+                <div class="flex items-center justify-between gap-x-2 mb-6">
+                    <TabList class="flex-1">
+                        <BlueprintTab
+                            ref="tab"
+                            v-for="tab in tabs"
+                            :key="tab._id"
+                            :tab="tab"
+                            :current-tab="currentTab"
+                            :show-instructions="showTabInstructionsField"
+                            :edit-text="editTabText"
+                            @removed="removeTab(tab._id)"
+                            @updated="updateTab(tab._id, $event)"
+                        />
+                    </TabList>
+
+                    <Button icon="plus" size="sm" round v-tooltip="addTabText" @click="addAndEditTab" />
+                </div>
+
+                <Description v-if="errors" v-for="(error, i) in errors" :key="i" :text="error" class="mt-2 text-red-500" />
+
+                <BlueprintTabContent
                     v-for="tab in tabs"
+                    ref="tabContent"
                     :key="tab._id"
                     :tab="tab"
-                    :current-tab="currentTab"
-                    :show-instructions="showTabInstructionsField"
-                    :edit-text="editTabText"
-                    @selected="selectTab(tab._id)"
-                    @removed="removeTab(tab._id)"
+                    v-show="currentTab === tab._id"
+                    :show-section-handle-field="showSectionHandleField"
+                    :show-section-hide-field="showSectionHideField"
+                    :new-section-text="newSectionText"
+                    :edit-section-text="editSectionText"
+                    :add-section-text="addSectionText"
+                    :can-define-localizable="canDefineLocalizable"
                     @updated="updateTab(tab._id, $event)"
-                    @mouseenter="mouseEnteredTab(tab._id)"
                 />
-            </div>
-            <button
-                class="btn-round relative top-1 flex items-center justify-center ms-2"
-                @click="addAndEditTab"
-                v-tooltip="addTabText"
-            >
-                <svg-icon name="add" class="size-3" />
-            </button>
+            </Tabs>
         </div>
-        <button v-if="!singleTab && tabs.length === 0" class="btn" @click="addAndEditTab" v-text="addTabText" />
-        <div v-if="errors" class="-mt-2">
-            <small class="help-block text-red-500" v-for="(error, i) in errors" :key="i" v-text="error" />
-        </div>
-        <tab-content
-            v-for="tab in tabs"
-            ref="tabContent"
-            :key="tab._id"
-            :tab="tab"
-            v-show="currentTab === tab._id"
-            :show-section-handle-field="showSectionHandleField"
-            :show-section-hide-field="showSectionHideField"
-            :new-section-text="newSectionText"
-            :edit-section-text="editSectionText"
-            :add-section-text="addSectionText"
-            :can-define-localizable="canDefineLocalizable"
-            @updated="updateTab(tab._id, $event)"
-        />
     </div>
 </template>
 
 <script>
 import { Sortable, Plugins } from '@shopify/draggable';
 import uniqid from 'uniqid';
-import Tab from './Tab.vue';
-import TabContent from './TabContent.vue';
+import BlueprintTab from './Tab.vue';
+import BlueprintTabContent from './TabContent.vue';
 import CanDefineLocalizable from '../fields/CanDefineLocalizable';
+import { Tabs, TabList, Button, Description } from '@statamic/ui';
 
 export default {
     mixins: [CanDefineLocalizable],
 
     components: {
-        Tab,
-        TabContent,
+        BlueprintTab,
+        BlueprintTabContent,
+        Tabs,
+        TabList,
+        Button,
+        Description,
     },
 
     props: {

commit a60ace914d63883943c53f701ddf18f6cf0c8eeb

Fix set icons
I broke them in c547b722 when I removed the default `plump` folder setting.

src/Fieldtypes/Sets.php
@@ -15,6 +15,8 @@ class Sets extends Fieldtype
 
     protected static $iconsDirectory = null;
 
+    protected static $iconsFolder = null;
+
     /**
      * Converts the "sets" array of a Replicator (or Bard) field into what the
      * <sets-fieldtype> Vue component is expecting, within either the Blueprint
@@ -156,7 +158,7 @@ class Sets extends Fieldtype
         // Then provide to script for <icon-fieldtype> selector components in blueprint config
         Statamic::provideToScript([
             'setIconsDirectory' => static::$iconsDirectory,
-            'setIconsFolder' => null,
+            'setIconsFolder' => static::$iconsFolder,
         ]);
 
         // And finally, provide the file contents of all custom svg icons to script,

tests/Fieldtypes/SetsTest.php
@@ -422,15 +422,6 @@ class SetsTest extends TestCase
     }
 
     #[Test]
-    public function it_provides_statamic_plump_icons_to_script_by_default()
-    {
-        $jsonVariables = Statamic::jsonVariables(request());
-
-        $this->assertNull($jsonVariables['setIconsDirectory']);
-        $this->assertEquals('plump', $jsonVariables['setIconsFolder']);
-    }
-
-    #[Test]
     public function it_can_provide_custom_user_icons_subfolder()
     {
         Sets::setIconsDirectory(folder: 'light');