4f9698ae663cf2394090ac56acf6288267851684
Jesse Leite <jesseleite@gmail.com>
2025-07-16 14:22:32 -0400

    Add class for Jay.

--- resources/js/components/nav/Branch.vue
+++ resources/js/components/nav/Branch.vue
@@ -1,5 +1,9 @@
 <template>
-    <div class="page-tree-branch flex" :class="{ 'ml-[-1.2rem]': isTopLevel, 'page-tree-branch--has-children': hasChildren }">
+    <div class="page-tree-branch flex" :class="{
+        'ml-[-1.2rem]': isTopLevel,
+        'page-tree-branch--is-section': isSection,
+        'page-tree-branch--has-children': hasChildren,
+    }">
         <div class="page-move w-6" />
         <div class="flex flex-1 items-center p-1.5 text-xs leading-normal">
             <div class="flex gap-3 grow items-center" :class="{ 'opacity-50': isHidden || isInHiddenSection }">
