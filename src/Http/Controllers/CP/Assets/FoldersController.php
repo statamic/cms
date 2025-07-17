ff5548f64101ab1e3c2eed899afbe288b9156c6a
Duncan McClean <duncan@duncanmcclean.com>
2025-07-17 11:56:19 +0100

    Fix actions for newly created folders

--- src/Http/Controllers/CP/Assets/FoldersController.php
+++ src/Http/Controllers/CP/Assets/FoldersController.php
@@ -7,6 +7,8 @@ use Illuminate\Validation\ValidationException;
 use Statamic\Assets\AssetUploader;
 use Statamic\Facades\Path;
 use Statamic\Http\Controllers\CP\CpController;
+use Statamic\Http\Resources\CP\Assets\Folder;
+use Statamic\Http\Resources\CP\Assets\FolderAsset;
 use Statamic\Rules\AlphaDashSpace;
 
 class FoldersController extends CpController
@@ -34,11 +36,15 @@ class FoldersController extends CpController
             $path = strtolower($path);
         }
 
-        return $container->assetFolder($path)->save();
+        $folder = $container->assetFolder($path)->save();
+
+        return (new Folder($folder))->resolve();
     }
 
     public function update(Request $request, $container, $folder)
     {
-        return $container->assetFolder($folder)->save();
+        $folder = $container->assetFolder($folder)->save();
+
+        return (new Folder($folder))->resolve();
     }
 }
