f8e791c3283a663981b1b4b656036a6cdf02e6aa
Jesse Leite <jesseleite@gmail.com>
2025-07-17 11:43:56 -0400

    Always reset this when user starts dragging.

--- resources/js/components/nav/Builder.vue
+++ resources/js/components/nav/Builder.vue
@@ -335,6 +335,8 @@ export default {
             // Prevent the top level section item being dragged. It should always stay at the top.
             if (stat.data.text === 'Top Level') return false;
 
+            this.isDraggingIntoTopLevelSection = false;
+
             return true;
         },
 
