2fc22800e974db5888fab664b284a066ec3a9124
Jesse Leite <jesseleite@gmail.com>
2025-07-17 16:36:16 -0400

    Fix background behind top level section placeholder.

--- resources/js/components/nav/TopLevelSectionBranch.vue
+++ resources/js/components/nav/TopLevelSectionBranch.vue
@@ -2,7 +2,7 @@
     <div class="flex first-branch">
         <div
             v-if="!hasChildren"
-            class="w-full rounded-lg border border-dashed border-blue-400 bg-blue-500/10 p-2"
+            class="is-top-level-section-placeholder w-full rounded-lg border border-dashed border-blue-400 bg-blue-500/10 p-2"
         >
             &nbsp;
         </div>
