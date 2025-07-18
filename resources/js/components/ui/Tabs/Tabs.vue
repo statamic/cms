56b6b0019c167a7b64d8b5e9da901de9b3b197f3
Duncan McClean <duncan@duncanmcclean.com>
2025-07-18 15:19:42 +0100

    Fix dragging & dropping fields in blueprint builder

--- resources/js/components/ui/Tabs/Tabs.vue
+++ resources/js/components/ui/Tabs/Tabs.vue
@@ -5,11 +5,12 @@ const emit = defineEmits(['update:modelValue']);
 
 defineProps({
     modelValue: { type: String, default: null },
+    unmountOnHide: { type: Boolean, default: true },
 });
 </script>
 
 <template>
-    <TabsRoot :model-value="modelValue" @update:model-value="emit('update:modelValue', $event)">
+    <TabsRoot :model-value="modelValue" @update:model-value="emit('update:modelValue', $event)" :unmount-on-hide>
         <slot />
     </TabsRoot>
 </template>
