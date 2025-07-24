312d0cb684c4b1f24c83a3662c9bb247e149990b
Jesse Leite <jesseleite@gmail.com>
2025-07-24 16:41:03 -0400

    Flesh out JS API.

--- resources/js/components/CommandPalette.js
+++ resources/js/components/CommandPalette.js
@@ -1,6 +1,27 @@
 import { ref } from 'vue';
 
-const commands = ref({});
+const commands = ref([]);
+
+function validate(command) {
+    if (typeof command.text !== 'string') {
+        console.error('You must provide a `text:` string in your command object!');
+    }
+
+    if (command.type === 'action' && typeof command.action !== 'function') {
+        console.error('You must provide an `action()` function to be run with your `'+command.text+'` command!');
+    }
+
+    return command;
+}
+
+function normalize(command) {
+    if (command.category === undefined) command.category = 'Actions';
+    if (command.type === undefined) command.type = 'action';
+    if (command.icon === undefined) command.icon = 'wand';
+    if (command.when === undefined) command.when = () => true;
+
+    return validate(command);
+}
 
 export default class CommandPalette {
     categories() {
@@ -8,10 +29,14 @@ export default class CommandPalette {
     }
 
     add(command) {
-        commands.value.push(command);
+        commands.value.push(normalize(command));
     }
 
-    get() {
-        return commands.value;
+    actions() {
+        return commands.value.filter(command => command.category === 'Actions');
+    }
+
+    misc() {
+        return commands.value.filter(command => command.category !== 'Actions');
     }
 }
