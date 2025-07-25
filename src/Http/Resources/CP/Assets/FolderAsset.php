1c25241bfa556f11c3e756d9cade5b6eaeaecbec
John Koster <john@stillat.com>
2025-07-25 04:28:49 -0500

    [6.x] Asset video thumbnail generation (#11841)
    * Generate thumbnails for video files
    
    * Rename to mentally separate from console output
    
    * Refactor to method on base class
    
    * Auto discover ffmpeg, refactor config
    
    * Better handling to ensure fallback SVG icon
    
    * Update Ffmpeg.php
    
    * Revert this
    
    * Update ImageGenerator.php
    
    * Update FolderAsset.php
    
    * Adjust ordering of merged data
    
    This will allow people to customize the thumbnails much more easily
    
    * Move private methods to the bottom
    
    * Use a match statement here instead
    
    * A couple of nitpicky changes
    
    * Move `video_thumbnails` config next to the other thumbnail-related options
    
    * return types
    
    * Laravel Herd doesn't inherit the user's PATH, so we need to check the Homebrew path manually
    
    * Convert `if` statements to a `match` statement
    
    * Formatting
    
    * Code cleanup 🧹
    
    ---------
    
    Co-authored-by: Duncan McClean <duncan@duncanmcclean.com>

--- src/Http/Resources/CP/Assets/FolderAsset.php
+++ src/Http/Resources/CP/Assets/FolderAsset.php
@@ -3,11 +3,15 @@
 namespace Statamic\Http\Resources\CP\Assets;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Fluent;
 use Statamic\Facades\Action;
 use Statamic\Support\Str;
+use Statamic\Support\Traits\Hookable;
 
 class FolderAsset extends JsonResource
 {
+    use Hookable;
+
     protected $blueprint;
     protected $columns;
 
@@ -35,22 +39,14 @@ class FolderAsset extends JsonResource
             'size_formatted' => Str::fileSizeForHumans($this->size(), 0),
             'last_modified_relative' => $this->lastModified()->diffForHumans(),
 
-            $this->mergeWhen($this->isImage() || $this->isSvg(), function () {
-                return [
-                    'is_image' => true,
-                    'thumbnail' => $this->thumbnailUrl('small'),
-                    'can_be_transparent' => $this->isSvg() || $this->extensionIsOneOf(['svg', 'png', 'webp', 'avif']),
-                    'alt' => $this->alt,
-                    'orientation' => $this->orientation(),
-                ];
-            }),
-
             $this->merge($this->values()),
 
             'actions' => Action::for($this->resource, [
                 'container' => $this->container()->handle(),
                 'folder' => $this->folder(),
             ]),
+
+            $this->merge($this->thumbnails()),
         ];
     }
 
@@ -73,5 +69,43 @@ class FolderAsset extends JsonResource
 
             return [$key => $value];
         });
+    }
+
+    private function thumbnails(): array
+    {
+        $data = match (true) {
+            $this->isImage() || $this->isSvg() => $this->getImageThumbnail(),
+            $this->isVideo() && config('statamic.assets.video_thumbnails', true) => $this->getVideoThumbnail(),
+            default => ['thumbnail' => null],
+        };
+
+        return array_merge($data, $this->runAssetHook() ?? []);
+    }
+
+    private function getImageThumbnail(): array
+    {
+        return [
+            'is_image' => true,
+            'thumbnail' => $this->thumbnailUrl('small'),
+            'can_be_transparent' => $this->isSvg() || $this->extensionIsOneOf(['svg', 'png', 'webp', 'avif']),
+            'alt' => $this->alt,
+            'orientation' => $this->orientation(),
+        ];
+    }
+
+    private function getVideoThumbnail(): array
+    {
+        return [
+            'thumbnail' => $this->thumbnailUrl('small'),
+        ];
+    }
+
+    private function runAssetHook(): array
+    {
+        $payload = $this->runHooksWith('asset', [
+            'data' => new Fluent,
+        ]);
+
+        return $payload->data->toArray();
     }
 }
---
+++ src/Imaging/ThumbnailExtractor.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Statamic\Imaging;
+
+use Statamic\Console\Processes\Ffmpeg;
+use Statamic\Contracts\Assets\Asset;
+use Statamic\Facades\Path;
+
+class ThumbnailExtractor
+{
+    public function __construct(private Ffmpeg $ffmpeg)
+    {
+    }
+
+    public static function enabled()
+    {
+        return config(
+            'statamic.assets.video_thumbnails',
+            true
+        );
+    }
+
+    public static function cachePath()
+    {
+        return config(
+            'statamic.assets.ffmpeg.cache_path',
+            storage_path('statamic/glide/ffmpeg')
+        );
+    }
+
+    public static function getCachePath(Asset $asset)
+    {
+        $fileName = 'thumb_'.md5($asset->id()).'.jpg';
+        $cacheDirectory = static::cachePath();
+        $finalPath = Path::tidy($cacheDirectory.'/'.$fileName);
+
+        if (! file_exists($cacheDirectory)) {
+            mkdir($cacheDirectory, 0755, true);
+        }
+
+        return $finalPath;
+    }
+
+    public static function canGenerateThumbnail(Asset $asset)
+    {
+        $resolvedPath = $asset->resolvedPath();
+
+        if (file_exists($resolvedPath)) {
+            return true;
+        }
+
+        return $asset->container()->accessible();
+    }
+
+    public function generateThumbnail(Asset $asset)
+    {
+        if (! static::canGenerateThumbnail($asset)) {
+            return '';
+        }
+
+        $cachePath = static::getCachePath($asset);
+
+        if (file_exists($cachePath)) {
+            return $cachePath;
+        }
+
+        $ffmpegInput = match (true) {
+            file_exists($asset->resolvedPath()) => $asset->resolvedPath(),
+            $asset->container()->accessible() => $asset->absoluteUrl(),
+            default => null,
+        };
+
+        return $this->ffmpeg->extractThumbnail(
+            $ffmpegInput,
+            static::getCachePath($asset)
+        );
+    }
+}
