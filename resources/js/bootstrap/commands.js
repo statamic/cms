7b8d63f5c708c3a3fec9844933fe7d8c842a32da
Jesse Leite <jesseleite@gmail.com>
2025-08-01 10:13:22 -0600

    Add ‘Statamic Documentation’ link to command palette.

--- resources/js/bootstrap/commands.js
+++ resources/js/bootstrap/commands.js
@@ -1,3 +1,9 @@
 export default function registerGlobalCommandPalette() {
-    //
+    Statamic.$commandPalette.add({
+        text: __('Statamic Documentation'),
+        category: 'Miscellaneous',
+        icon: 'book-next-page',
+        url: 'https://statamic.dev',
+        when: () => Statamic.$permissions.has('super'),
+    });
 }
