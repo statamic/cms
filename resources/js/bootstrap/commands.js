44f2998dc6a82bb583a6205bdd379540b0a179f9
Jesse Leite <jesseleite@gmail.com>
2025-08-05 12:47:18 -0400

    Use open new tab prop in an example.

--- resources/js/bootstrap/commands.js
+++ resources/js/bootstrap/commands.js
@@ -4,6 +4,7 @@ export default function registerGlobalCommandPalette() {
         category: 'Miscellaneous',
         icon: 'book-next-page',
         url: 'https://statamic.dev',
+        openNewTab: true,
         when: () => Statamic.$permissions.has('super'),
     });
 }
