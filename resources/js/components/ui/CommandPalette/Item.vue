a259a0b6f9cea681a3b7930b2de252ac9a9d150c
Jesse Leite <jesseleite@gmail.com>
2025-08-05 12:46:49 -0400

    Add template-friendly headless ui component for when we can’t easily JS.

---
+++ resources/js/components/ui/CommandPalette/Item.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { onUnmounted } from 'vue';
+
+const props = defineProps({
+    category: { type: String },
+    type: { type: String },
+    icon: { type: String },
+    when: { type: Function },
+    text: { type: String },
+    url: { type: String },
+    action: { type: Function },
+    openNewTab: { type: Boolean },
+});
+
+const command = Statamic.$commandPalette.add(props);
+
+onUnmounted(() => command.remove());
+</script>
+
+<template>
+    <slot
+        :category="category"
+        :type="type"
+        :icon="icon"
+        :when="when"
+        :text="text"
+        :url="url"
+        :action="action"
+    />
+</template>
