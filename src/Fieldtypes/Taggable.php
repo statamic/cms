33606635822c29e88b265597244b0f3e8eb172b8
Daniel Weaver <godismyjudge95@gmail.com>
2025-08-06 12:21:34 -0400

    [5.x] Support multiple defaults for checkboxes fieldtype (#12021)
    Co-authored-by: Jason Varga <jason@pixelfear.com>

--- src/Fieldtypes/Taggable.php
+++ src/Fieldtypes/Taggable.php
@@ -4,6 +4,7 @@ namespace Statamic\Fieldtypes;
 
 use Statamic\Facades\GraphQL;
 use Statamic\Fields\Fieldtype;
+use Statamic\Support\Arr;
 
 class Taggable extends Fieldtype
 {
@@ -38,7 +39,7 @@ class Taggable extends Fieldtype
 
     public function preProcess($data)
     {
-        return ($data) ? $data : [];
+        return Arr::wrap($data);
     }
 
     public function toGqlType()
