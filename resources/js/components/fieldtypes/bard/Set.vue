37a98b0c53f34f4956030cdb905958e24ed245a1
Jason Varga <jason@pixelfear.com>
2025-08-06 17:03:35 -0400

    Remove the bard set connectors and add some margin

--- resources/js/components/fieldtypes/bard/Set.vue
+++ resources/js/components/fieldtypes/bard/Set.vue
@@ -1,20 +1,5 @@
 <template>
-    <node-view-wrapper>
-        <Motion
-            layout
-            class="flex justify-center py-3 relative group"
-            :initial="{ paddingTop: '0.75rem', paddingBottom: '0.75rem' }"
-            :hover="{ paddingTop: '1.25rem', paddingBottom: '1.25rem' }"
-            :transition="{ duration: 0.2 }"
-        >
-            <div v-if="showConnector" class="absolute group-hover:opacity-0 transition-opacity delay-25 duration-125 inset-y-0 h-full left-3.5 border-l-1 border-gray-400 dark:border-gray-600 border-dashed z-0 dark:bg-dark-700" />
-            <button class="w-full absolute inset-0 h-full opacity-0 group-hover:opacity-100 transition-opacity delay-25 duration-75 cursor-pointer">
-                <div class="h-full flex flex-col justify-center">
-                    <div class="rounded-full bg-gray-200 h-2" />
-                </div>
-            </button>
-            <Button v-if="enabled" round icon="plus" size="sm" class="-my-4 z-3 opacity-0 group-hover:opacity-100 transition-opacity delay-25 duration-75" />
-        </Motion>
+    <node-view-wrapper class="my-4">
         <div
             class="shadow-ui-sm relative z-2 w-full rounded-lg border border-gray-200 bg-white text-base dark:border-x-0 dark:border-t-0 dark:border-white/10 dark:bg-gray-900 dark:inset-shadow-2xs dark:inset-shadow-black"
             :class="{
@@ -128,7 +113,6 @@ export default {
         getPos: { type: Function, required: true },
         updateAttributes: { type: Function, required: true },
         deleteNode: { type: Function, required: true },
-        showConnector: { type: Boolean, default: true },
     },
 
     components: {
