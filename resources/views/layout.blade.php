d4895973130d90e2e62b7b8f09939c044d6f08d2
Jay George <contact@jaygeorge.co.uk>
2025-08-10 12:23:32 +0200

    Add skip links

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -33,7 +33,7 @@
                         fixed top-14 inset-x-0 bottom-0 min-h-[calc(100vh-3.5rem)]
                     ">
                         @include('statamic::partials.nav-main')
-                        <div class="main-content p-2 h-full flex-1 overflow-y-auto">
+                        <div id="main-content" class="main-content p-2 h-full flex-1 overflow-y-auto">
                             <div class="relative content-card @yield('content-card-modifiers') min-h-full transition-padding duration-300">
                                 @yield('content')
                             </div>
--- resources/views/partials/global-header.blade.php
+++ resources/views/partials/global-header.blade.php
@@ -3,6 +3,12 @@
 @endphp
 
 <header class="h-14 bg-gray-800 flex justify-between space-x-2 items-center text-white px-4 dark fixed top-0 inset-x-0 z-[3]">
+    <a class="c-skip-link z-(--z-index-header) px-4 py-2 bg-blue-800 text-sm top-2.5 left-2.25 fixed opacity-0 -translate-y-24 focus:translate-y-0 focus:opacity-100 motion-safe:transition-all motion-safe:ease-[var(--animation-timing-function-fast-out-slow-in)] rounded-md" href="#main">
+        Skip to sidebar
+    </a>
+    <a class="c-skip-link z-(--z-index-header) px-4 py-2 bg-blue-800 text-sm top-2.5 left-2.25 fixed opacity-0 -translate-y-24 focus:translate-y-0 focus:opacity-100 motion-safe:transition-all motion-safe:duration-200 motion-safe:ease-[var(--animation-timing-function-fast-out-slow-in)] rounded-md" href="#main-content">
+        Skip to content
+    </a>
     <div class="flex items-center gap-2 text-[0.8125rem] text-gray-300">
         {{-- Logo --}}
         @if ($customDarkLogo)
