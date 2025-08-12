7cf0778ac10e16e7f6de50ec4a0467f73a40420d
Jack McDade <jack@jackmcdade.com>
2025-08-12 14:41:51 -0400

    Fix dark mode asset browser issues

--- resources/js/components/assets/Browser/Grid.vue
+++ resources/js/components/assets/Browser/Grid.vue
@@ -95,7 +95,7 @@
                     <Context>
                         <template #trigger>
                             <div
-                                class="asset-tile group relative bg-white"
+                                class="asset-tile group relative bg-white dark:bg-gray-900"
                                 :class="{
                                     'bg-checkerboard!': asset.can_be_transparent,
                                     'opacity-50!': draggingAsset === asset.id,
