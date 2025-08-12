8cc37853c7c2bff7284414cbdcd16352264211d2
Duncan McClean <duncan@duncanmcclean.com>
2025-08-12 17:43:08 +0100

    Combobox: Truncate selected option label when it's really long
    Otherwise, it ends up on two lines and looks gorpy.
    
    You can still see the full label by opening the combobox.

--- resources/js/components/ui/Combobox.vue
+++ resources/js/components/ui/Combobox.vue
@@ -277,11 +277,11 @@ defineExpose({
                             @blur="pushTaggableOption"
                         />
 
-                        <button type="button" class="flex-1 text-start" v-else-if="!searchable && (dropdownOpen || !modelValue)">
+                        <button type="button" class="flex-1 text-start truncate" v-else-if="!searchable && (dropdownOpen || !modelValue)">
                             <span class="text-gray-400 dark:text-gray-500 placeholder-text-xs" v-text="placeholder" />
                         </button>
 
-                        <button type="button" v-else class="flex-1 text-start cursor-pointer">
+                        <button type="button" v-else class="flex-1 text-start cursor-pointer truncate">
                             <slot name="selected-option" v-bind="{ option: selectedOption }">
                                 <span v-if="labelHtml" v-html="getOptionLabel(selectedOption)" />
                                 <span v-else v-text="getOptionLabel(selectedOption)" />
