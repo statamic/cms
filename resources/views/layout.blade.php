8fa7b58581daa361eb5a20a01848a56a6653a221
Jason Varga <jason@pixelfear.com>
2025-08-12 23:06:11 -0400

    lol, wrong spot in 744c910a

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -28,12 +28,12 @@
                 >
                     {{-- @include('statamic::partials.nav-mobile') --}}
 
-                    <main id="main" v-cloak class="
+                    <main id="main" class="
                     flex bg-gray-100 dark:bg-gray-900 dark:border-t rounded-t-2xl dark:border-white/10
                         fixed top-14 inset-x-0 bottom-0 min-h-[calc(100vh-3.5rem)]
                     ">
                         @include('statamic::partials.nav-main')
-                        <div id="main-content" class="main-content p-2 h-full flex-1 overflow-y-auto">
+                        <div id="main-content" v-cloak class="main-content p-2 h-full flex-1 overflow-y-auto">
                             <div class="relative content-card @yield('content-card-modifiers') min-h-full transition-padding duration-300">
                                 @yield('content')
                             </div>
