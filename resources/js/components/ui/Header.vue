9b1fa413679207185aa14aada8cf74ca9588919b
Jay George <contact@jaygeorge.co.uk>
2025-08-16 18:59:10 +0200

    header -- Wrap icon in a fixed size div (the same size as the icon) to prevent layout shift once it loads

--- resources/js/components/ui/Header.vue
+++ resources/js/components/ui/Header.vue
@@ -10,7 +10,10 @@ const props = defineProps({
 <template>
     <header class="flex flex-wrap items-center justify-between gap-4 py-8" data-ui-header>
         <h1 class="text-[25px] leading-[1.25] st-text-legibility font-medium antialiased flex items-center gap-2.5 flex-1">
-            <Icon v-if="icon" :name="icon" class="size-5 text-gray-500"></Icon>
+            <!-- Wrap icon in a fixed size div (the same size as the icon) to prevent layout shift once it loads -->
+            <div class="size-5">
+                <Icon v-if="icon" :name="icon" class="size-5 text-gray-500"></Icon>
+            </div>
             <slot name="title">{{ title }}</slot>
         </h1>
         <div class="flex items-center gap-3">
