27f7d2a0a41bfc6bab22d978e2e7e10dc790f679
Jason Varga <jason@pixelfear.com>
2025-08-19 14:22:14 -0400

    remove unused bits

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig, loadEnv } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-// import inject from '@rollup/plugin-inject';
 import { visualizer } from 'rollup-plugin-visualizer';
 import svgLoader from 'vite-svg-loader';
 import path from 'path';
@@ -26,7 +25,6 @@ export default defineConfig(({ mode, command }) => {
             }),
             vue(),
             svgLoader(),
-            // inject({ Vue: 'vue', include: 'resources/js/**' }),
         ],
         css: {
             devSourcemap: true,
@@ -37,12 +35,9 @@ export default defineConfig(({ mode, command }) => {
                 '@': path.resolve(__dirname, 'resources/js'),
             },
         },
-        optimizeDeps: { include: ['vue'] },
         build: {
             rollupOptions: {
-                // external: ['vue'],
                 output: {
-                    // globals: { vue: 'Vue' },
                     plugins: [visualizer({ filename: 'bundle-stats.html' })]
                 },
             },
