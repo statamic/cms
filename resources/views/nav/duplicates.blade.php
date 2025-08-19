a8a951deba9f2c795b8b0f3689d760c902e8d835
Jack McDade <jack@jackmcdade.com>
2025-08-19 18:42:30 -0400

    Fix updates + blueprints nav state/svg color

--- resources/views/nav/duplicates.blade.php
+++ resources/views/nav/duplicates.blade.php
@@ -1,8 +1,8 @@
-<li class="{{ $item->isActive() ? 'current' : '' }}">
-    <a href="{{ $item->url() }}">
+<li>
+    <a href="{{ $item->url() }}" class="flex items-center gap-3 {{ $item->isActive() ? 'active' : '' }}">
         <i>{!! $item->svg() !!}</i>
         <span>{{ __($item->name()) }}</span>
-        <span class="badge-sm bg-red-500 dark:bg-blue-900 ltr:ml-2 rtl:mr-2">
+        <span class="badge-sm bg-red-500 dark:bg-blue-900">
             {{ Statamic\Facades\Stache::duplicates()->count() }}
         </span>
     </a>
