ec3204afb0d781b08db1fef260f14f076a90f01b
Duncan McClean <duncan@duncanmcclean.com>
2025-08-19 23:20:19 +0100

    Fix error when building addon with multiple input files

--- resources/js/package/vite-plugin.js
+++ resources/js/package/vite-plugin.js
@@ -11,32 +11,43 @@ const statamic = function (options) {
             config.build.rollupOptions.external = config.build.rollupOptions.external || [];
             config.build.rollupOptions.output = config.build.rollupOptions.output || {};
 
-            // Add Vue and all Statamic modules as external
-            const existingExternal = config.build.rollupOptions.external;
-            config.build.rollupOptions.external = [
-                ...existingExternal,
-                'vue',
-                // Match @statamic/cms and any subpath
-                /^@statamic\/cms(\/.*)?$/,
-            ];
-
-            // Set up globals for browser usage
-            const existingGlobals = config.build.rollupOptions.output.globals || {};
-            config.build.rollupOptions.output.globals = {
-                ...existingGlobals,
-                'vue': 'Vue',
-                '@statamic/cms': '__STATAMIC__.core',
-                '@statamic/cms/ui': '__STATAMIC__.ui',
-                '@statamic/cms/bard': '__STATAMIC__.bard',
-                '@statamic/cms/save-pipeline': '__STATAMIC__.savePipeline',
-            };
-
-            // Set default format if not specified
-            if (!config.build.rollupOptions.output.format) {
-                config.build.rollupOptions.output.format = 'iife';
-            }
-
             return config;
+        },
+
+        configResolved(resolvedConfig) {
+            const inputs = resolvedConfig.build?.rollupOptions?.input;
+            const hasMultipleInputs = (Array.isArray(inputs) && inputs.length > 1) ||
+                (typeof inputs === 'object' && inputs !== null && !Array.isArray(inputs) && Object.keys(inputs).length > 1);
+
+            if (hasMultipleInputs) {
+                // For multiple inputs, just disable inlineDynamicImports
+                resolvedConfig.build.rollupOptions.output.inlineDynamicImports = false;
+            } else {
+                // Single input - set up as external modules for addon development
+                const existingExternal = resolvedConfig.build.rollupOptions.external;
+                resolvedConfig.build.rollupOptions.external = [
+                    ...existingExternal,
+                    'vue',
+                    // Match @statamic/cms and any subpath
+                    /^@statamic\/cms(\/.*)?$/,
+                ];
+
+                // Set up globals for browser usage
+                const existingGlobals = resolvedConfig.build.rollupOptions.output.globals || {};
+                resolvedConfig.build.rollupOptions.output.globals = {
+                    ...existingGlobals,
+                    'vue': 'Vue',
+                    '@statamic/cms': '__STATAMIC__.core',
+                    '@statamic/cms/ui': '__STATAMIC__.ui',
+                    '@statamic/cms/bard': '__STATAMIC__.bard',
+                    '@statamic/cms/save-pipeline': '__STATAMIC__.savePipeline',
+                };
+
+                // Set default format if not specified
+                if (!resolvedConfig.build.rollupOptions.output.format) {
+                    resolvedConfig.build.rollupOptions.output.format = 'iife';
+                }
+            }
         }
     };
 };
