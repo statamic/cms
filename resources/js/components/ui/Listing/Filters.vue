e09b59e203561dc81ba335ed5b3eeafa5b1128bc
Jay George <contact@jaygeorge.co.uk>
2025-08-20 15:02:08 +0200

    Filters - fix dark mode

--- resources/js/components/ui/Listing/Filters.vue
+++ resources/js/components/ui/Listing/Filters.vue
@@ -107,7 +107,7 @@ function handleStackClosed() {
         </div>
 
         <stack half name="filters" v-if="open" @closed="handleStackClosed">
-            <div ref="stackContentRef" class="flex-1 p-3 bg-white h-full overflow-auto rounded-l-2xl relative">
+            <div ref="stackContentRef" class="flex-1 p-3 bg-white dark:bg-gray-800 h-full overflow-auto rounded-l-2xl relative">
                 <Button
                     icon="x"
                     variant="ghost"
