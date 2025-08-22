d5b051e98f8a9ae4a811173ab8cf6dd702a0cc06
Jack McDade <jack@jackmcdade.com>
2025-08-22 17:56:51 -0400

    [6.x] Fix bard floating toolbar (#12121)
    Fix Bard Floating Toolbar. Closes #12089.

--- resources/js/components/fieldtypes/bard/BardFieldtype.vue
+++ resources/js/components/fieldtypes/bard/BardFieldtype.vue
@@ -59,11 +59,11 @@
                         tabindex="0"
                     >
                         <bubble-menu
-                            class="bard-floating-toolbar"
                             :editor="editor"
                             :options="{ placement: 'top', offset: [0, 10] }"
                             v-if="editor && toolbarIsFloating && !readOnly"
                         >
+                        <div class="bard-floating-toolbar">
                             <component
                                 v-for="button in visibleButtons(buttons)"
                                 :key="button.name"
@@ -74,6 +74,7 @@
                                 :config="config"
                                 :editor="editor"
                             />
+                        </div>
                         </bubble-menu>
 
                         <floating-menu
