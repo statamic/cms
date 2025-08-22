0ff0008afb1abd1076ad625b362bc521a5a7b78f
Jack McDade <jack@jackmcdade.com>
2025-08-22 17:56:03 -0400

    Fix Bard Floating Toolbar. Closes #12089.

--- resources/js/components/fieldtypes/bard/ToolbarButton.vue
+++ resources/js/components/fieldtypes/bard/ToolbarButton.vue
@@ -1,14 +1,13 @@
 <template>
     <Button
-        class="px-2!"
-        :class="{ active }"
-        variant="ghost"
+        class="px-2! group"
+        variant="subtle"
         size="sm"
         :aria-label="button.text"
         v-tooltip="button.text"
         @click="button.command(editor, button.args)"
     >
-        <ui-icon :name="button.svg" v-if="button.svg" class="size-3.5!" />
+        <ui-icon :name="button.svg" v-if="button.svg" class="size-3.5! group-hover:text-white" :class="{ 'text-yellow-300!': active }" />
         <div class="flex items-center" v-html="button.html" v-if="button.html" />
     </Button>
 </template>
