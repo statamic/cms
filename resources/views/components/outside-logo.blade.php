e823f2d08da7826c218632fddd21fa42f42e92d6
Duncan McClean <duncan@duncanmcclean.com>
2025-08-22 14:20:27 +0100

    [6.x] Fix missing logo on auth pages (#12087)
    * Handle situation where `$customLogo` or `$customLogoText` may be false
    
    * We don't even need the `isset()` check
    
    The variables are always going to be there, since they'll be passed from the OutsideLogo class.
    
    We just need to check if a value exists.
    
    * remove unnecessary changes

--- resources/views/components/outside-logo.blade.php
+++ resources/views/components/outside-logo.blade.php
@@ -1,5 +1,5 @@
 <div class="logo relative z-10 md:pt-18">
-    @if (isset($customLogo))
+    @if ($customLogo)
         <img
             src="{{ $customLogo }}"
             alt="{{ config('statamic.cp.custom_cms_name') }}"
@@ -10,7 +10,7 @@
             alt="{{ config('statamic.cp.custom_cms_name') }}"
             class="white-label-logo hidden dark:block"
         />
-    @elseif (isset($customLogoText))
+    @elseif ($customLogoText)
         <div class="mx-auto mb-8 max-w-xs text-center text-lg font-medium opacity-50">{{ $customLogoText }}</div>
     @else
         @cp_svg('ui/statamic-logo-lime', 'h-6')
