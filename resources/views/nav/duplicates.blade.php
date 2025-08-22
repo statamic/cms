8b16d626925c4e250f40db6f8d322c2da5787432
Duncan McClean <duncan@duncanmcclean.com>
2025-08-22 11:57:50 +0100

    Add missing icon & fix badge on nav item

--- resources/views/nav/duplicates.blade.php
+++ resources/views/nav/duplicates.blade.php
@@ -2,8 +2,8 @@
     <a href="{{ $item->url() }}" class="flex items-center gap-3 {{ $item->isActive() ? 'active' : '' }}">
         <i>{!! $item->svg() !!}</i>
         <span>{{ __($item->name()) }}</span>
-        <span class="badge-sm bg-red-500 dark:bg-blue-900">
+        <ui-badge color="red" size="sm" variant="flat" pill>
             {{ Statamic\Facades\Stache::duplicates()->count() }}
-        </span>
+        </ui-badge>
     </a>
 </li>
