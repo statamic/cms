569f16beeacae157cfa9c15198fc298fde42e663
Jack McDade <jack@jackmcdade.com>
2025-08-23 13:41:41 -0400

    Configurable app shell (body and main content area) colors
    This will help folks with different contrast preferences, as well as make it easier to to test and experiment with perhaps some new defaults.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -25,7 +25,7 @@
                         'showing-license-banner': showBanner
                     }"
                 >
-                    <main id="main" class="flex bg-gray-100 dark:bg-gray-900 dark:border-t rounded-t-2xl dark:border-white/10 fixed top-14 inset-x-0 bottom-0 min-h-[calc(100vh-3.5rem)]">
+                    <main id="main" class="flex bg-body-bg dark:bg-body-dark-bg dark:border-t rounded-t-2xl dark:border-body-dark-border fixed top-14 inset-x-0 bottom-0 min-h-[calc(100vh-3.5rem)]">
                         @include('statamic::partials.nav-main')
                         <div id="main-content" v-cloak class="main-content p-2 h-full flex-1 overflow-y-auto">
                             <div class="relative content-card @yield('content-card-modifiers') min-h-full">
