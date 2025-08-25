723070e63f9fb589a38c3af24613139476b40ea1
Duncan McClean <duncan@duncanmcclean.com>
2025-08-25 11:48:30 +0100

    Fix custom labels on replicator's add set button

--- resources/js/components/fieldtypes/replicator/AddSetButton.vue
+++ resources/js/components/fieldtypes/replicator/AddSetButton.vue
@@ -3,7 +3,7 @@
         <template #trigger>
             <div class="flex relative pt-2" :class="{ 'pt-6': showConnector }" v-if="variant === 'button'">
                 <div v-if="showConnector" class="absolute group-hover:opacity-0 transition-opacity delay-25 duration-125 inset-y-0 h-full left-3.5 border-l-1 border-gray-400 dark:border-gray-600 border-dashed z-0 dark:bg-dark-700" />
-                <Button v-if="enabled" size="sm" :text="__('Add Block')" icon="plus" class="relative z-2" />
+                <Button v-if="enabled" size="sm" :text="__(label)" icon="plus" class="relative z-2" />
             </div>
             <Motion
                 v-if="variant === 'between'"
@@ -44,7 +44,7 @@ export default {
         groups: Array,
         index: Number,
         enabled: { type: Boolean, default: true },
-        label: String,
+        label: { type: String, default: 'Add Block' },
         showConnector: { type: Boolean, default: true },
         variant: { type: String, default: 'button' },
     },
