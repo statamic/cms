36a6b45fb85eb104ba2bdc31e4dce56fd15fca22
Jason Varga <jason@pixelfear.com>
2025-08-25 10:21:54 -0400

    Use computed so translator tool continues to pick it up, and doesnt get double wrapped in translate method ...
    Also switch to composition api while here

--- resources/js/components/fieldtypes/replicator/AddSetButton.vue
+++ resources/js/components/fieldtypes/replicator/AddSetButton.vue
@@ -3,7 +3,7 @@
         <template #trigger>
             <div class="flex relative pt-2" :class="{ 'pt-6': showConnector }" v-if="variant === 'button'">
                 <div v-if="showConnector" class="absolute group-hover:opacity-0 transition-opacity delay-25 duration-125 inset-y-0 h-full left-3.5 border-l-1 border-gray-400 dark:border-gray-600 border-dashed z-0 dark:bg-dark-700" />
-                <Button v-if="enabled" size="sm" :text="__(label)" icon="plus" class="relative z-2" />
+                <Button v-if="enabled" size="sm" :text="label" icon="plus" class="relative z-2" />
             </div>
             <Motion
                 v-if="variant === 'between'"
@@ -25,34 +25,27 @@
     </set-picker>
 </template>
 
-<script>
+<script setup>
 import SetPicker from './SetPicker.vue';
 import { Button } from '@/components/ui';
 import { Motion } from 'motion-v';
+import { computed } from 'vue';
 
-export default {
-    components: {
-        SetPicker,
-        Button,
-        Motion,
-    },
+const emit = defineEmits(['added']);
 
-    emits: ['added'],
+const props = defineProps({
+    sets: Array,
+    groups: Array,
+    index: Number,
+    enabled: { type: Boolean, default: true },
+    label: { type: String },
+    showConnector: { type: Boolean, default: true },
+    variant: { type: String, default: 'button' },
+});
 
-    props: {
-        sets: Array,
-        groups: Array,
-        index: Number,
-        enabled: { type: Boolean, default: true },
-        label: { type: String, default: 'Add Block' },
-        showConnector: { type: Boolean, default: true },
-        variant: { type: String, default: 'button' },
-    },
+const label = computed(() => props.label ? __(props.label) : __('Add Block'));
 
-    methods: {
-        addSet(handle) {
-            this.$emit('added', handle, this.index);
-        }
-    },
-};
+function addSet(handle) {
+    emit('added', handle, props.index);
+}
 </script>
