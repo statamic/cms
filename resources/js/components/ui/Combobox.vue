8d4bb6807f9d228857c67e2a7ee0a25d626bb177
Duncan McClean <duncan@duncanmcclean.com>
2025-08-26 10:36:59 +0100

    Simplify `openOnSpace` function

--- resources/js/components/ui/Combobox.vue
+++ resources/js/components/ui/Combobox.vue
@@ -242,31 +242,12 @@ function pushTaggableOption(e) {
 }
 
 function openOnSpace(e) {
-	const target = e && e.target ? e.target : null;
-	const tag = (target && target.tagName ? target.tagName : '').toLowerCase();
-	const isEditable = target && (tag === 'input' || tag === 'textarea' || target.isContentEditable);
+    if (dropdownOpen.value) return;
+    if (typeof e.preventDefault === 'function') e.preventDefault();
 
-	// If already open, do nothing so Space behaves normally
-	if (dropdownOpen.value) return;
+    updateDropdownOpen(true);
 
-	// If focused element is editable but dropdown is closed, intercept to open
-	if (isEditable) {
-		if (e && typeof e.preventDefault === 'function') e.preventDefault();
-		updateDropdownOpen(true);
-		nextTick(() => {
-			const inputEl = searchInputRef?.value?.$el || searchInputRef?.value;
-			if (inputEl && typeof inputEl.focus === 'function') inputEl.focus();
-		});
-		return;
-	}
-
-	// Non-editable target and closed: open
-	if (e && typeof e.preventDefault === 'function') e.preventDefault();
-	updateDropdownOpen(true);
-	nextTick(() => {
-		const inputEl = searchInputRef?.value?.$el || searchInputRef?.value;
-		if (inputEl && typeof inputEl.focus === 'function') inputEl.focus();
-	});
+    nextTick(() => searchInputRef?.value?.$el?.focus());
 }
 
 defineExpose({
@@ -292,7 +273,7 @@ defineExpose({
                 @update:model-value="updateModelValue"
             >
                 <ComboboxAnchor :class="['w-full flex items-center justify-between gap-2 text-gray-900 dark:text-gray-300 antialiased appearance-none', $attrs.class]" data-ui-combobox-anchor>
-                    <ComboboxTrigger as="div" ref="trigger" :class="triggerClasses" @keydown.space="openOnSpace">
+                    <ComboboxTrigger as="div" ref="trigger" :class="triggerClasses" @keydown.space.prevent="openOnSpace">
                         <ComboboxInput
                             v-if="searchable && (dropdownOpen || !modelValue || (multiple && placeholder))"
                             ref="search"
@@ -303,16 +284,16 @@ defineExpose({
                             :placeholder
                             autocomplete="off"
                             @paste.prevent="onPaste"
-                            @keydown.enter.prevent="pushTaggableOption"
                             @blur="pushTaggableOption"
                             @keydown.space="openOnSpace"
+                            @keydown.enter.prevent="pushTaggableOption"
                         />
 
-                        <button type="button" class="flex-1 text-start truncate" v-else-if="!searchable && (dropdownOpen || !modelValue)" @keydown.space="openOnSpace">
+                        <button type="button" class="flex-1 text-start truncate" v-else-if="!searchable && (dropdownOpen || !modelValue)" @keydown.space.prevent="openOnSpace">
                             <span class="text-gray-400 dark:text-gray-500 placeholder-text-xs" v-text="placeholder" />
                         </button>
 
-                        <button type="button" v-else class="flex-1 text-start cursor-pointer truncate" @keydown.space="openOnSpace">
+                        <button type="button" v-else class="flex-1 text-start cursor-pointer truncate" @keydown.space.prevent="openOnSpace">
                             <slot name="selected-option" v-bind="{ option: selectedOption }">
                                 <span v-if="labelHtml" v-html="getOptionLabel(selectedOption)" />
                                 <span v-else v-text="getOptionLabel(selectedOption)" />
