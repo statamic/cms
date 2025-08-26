bcc2aa7f4e886ffa08bd02d2e9275724b871fe39
Jason Varga <jason@pixelfear.com>
2025-08-26 19:59:55 -0400

    serve

--- resources/js/package/vite-plugin.js
+++ resources/js/package/vite-plugin.js
@@ -6,9 +6,9 @@ const statamic = function (options) {
         name: 'statamic',
 
         config(config, { command }) {
-            if (command === 'serve' && !process.env.STATAMIC_FORCE_SERVER) {
+            if (command === 'serve' && !process.env.STATAMIC_FORCE_SERVE) {
                 console.log('\x1b[33m[Statamic] Vite dev server current not supported. Automatically running "vite build --watch" instead...\x1b[0m');
-                console.log('\x1b[90m[Statamic] Use STATAMIC_FORCE_SERVER=1 to bypass this behavior.\x1b[0m');
+                console.log('\x1b[90m[Statamic] Use STATAMIC_FORCE_SERVE=1 to bypass this behavior.\x1b[0m');
 
                 const child = spawn('npx', ['vite', 'build', '--watch'], {
                     stdio: 'inherit',
