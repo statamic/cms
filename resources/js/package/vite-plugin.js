76b4a3d052312b6de546246b92834f058ede2f3a
Jason Varga <jason@pixelfear.com>
2025-08-27 12:36:54 -0400

    wip

--- resources/js/package/vite-plugin.js
+++ resources/js/package/vite-plugin.js
@@ -1,7 +1,12 @@
 import vue from '@vitejs/plugin-vue';
 import { spawn } from 'child_process';
+import { readFileSync } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 
 const statamic = function (options) {
+    const { excludeStatamicClasses = true, additionalExclusions = [] } = options;
+    
     return {
         name: 'statamic',
 
@@ -37,6 +42,21 @@ const statamic = function (options) {
         },
 
         configResolved(resolvedConfig) {
+            // Load Tailwind exclusions if enabled
+            if (excludeStatamicClasses) {
+                const __dirname = dirname(fileURLToPath(import.meta.url));
+                const exclusionsPath = join(__dirname, 'tailwind-exclusions.css');
+                
+                this.exclusions = readFileSync(exclusionsPath, 'utf8');
+                console.log('\x1b[32m[Statamic] Loaded Tailwind class exclusions\x1b[0m');
+                
+                // Add any additional exclusions
+                if (additionalExclusions.length > 0) {
+                    const additional = `@source not inline("${additionalExclusions.join(' ')}");`;
+                    this.exclusions += '\n' + additional;
+                }
+            }
+
             resolvedConfig.build.rollupOptions.plugins = resolvedConfig.build.rollupOptions.plugins || [];
             resolvedConfig.build.rollupOptions.plugins.push({
                 name: 'statamic-global-externals',
@@ -51,6 +71,18 @@ const statamic = function (options) {
                     return code;
                 }
             });
+        },
+
+        transform(code, id) {
+            // Inject Tailwind exclusions into CSS files
+            if (this.exclusions && id.endsWith('.css') && code.includes("@import 'tailwindcss'")) {
+                console.log('\x1b[36m[Statamic] Injecting Tailwind exclusions into CSS\x1b[0m');
+                return code.replace(
+                    /(@import ['"]tailwindcss['"];?)/,
+                    `$1\n${this.exclusions}`
+                );
+            }
+            return null;
         }
     };
 };
