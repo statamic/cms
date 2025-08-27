fa68e83c42522263648f62be5e93e1a19db715f7
Marty Friedel <marty.dev@mity.com.au>
2025-08-27 10:58:42 +0930

    Refactor to Composition

--- resources/js/components/Avatar.vue
+++ resources/js/components/Avatar.vue
@@ -1,46 +1,51 @@
+<script setup>
+import { cva } from 'cva';
+import { twMerge } from 'tailwind-merge';
+import {computed, ref} from "vue";
+
+const props = defineProps({
+    user: Object,
+    class: {
+        type: String,
+        default: ''
+    }
+})
+
+const hasAvatarError = ref(false);
+
+const hasAvatar = computed(() => {
+    return !!props.user.avatar && !hasAvatarError.value
+});
+
+const avatarSrc = computed(() => {
+    if (!hasAvatar.value) return null;
+
+    return props.user.avatar.permalink || props.user.avatar;
+})
+
+const avatarClasses = computed(() => {
+    const classes = cva({
+        base: 'size-7 rounded-full [button:has(&)]:rounded-full',
+        variants: {
+            type: {
+                avatar: '',
+                initials: 'text-white text-2xs font-medium flex flex-shrink-0 items-center justify-center bg-gradient-to-tr from-purple-500 to-red-600'
+            }
+        }
+    })({
+        type: hasAvatar.value ? 'avatar' : 'initials'
+    })
+
+    return twMerge(classes, props.class);
+})
+</script>
 <template>
     <template v-if="hasAvatar">
-        <img :src="avatarSrc" class="size-7 rounded-full [button:has(&)]:rounded-full" :alt="user.name" @error="hasAvatarError = true" />
+        <img :src="avatarSrc" :class="avatarClasses" :alt="user.name" @error="hasAvatarError = true" />
     </template>
     <template v-else>
-        <div :aria-label="user.name" class="size-7 text-white text-2xs font-medium flex items-center justify-center rounded-full [button:has(&)]:rounded-full bg-gradient-to-tr from-purple-500 to-red-600">{{ initials }}</div>
+        <div :aria-label="user.name" :class="avatarClasses">
+            {{ user.initials || '?' }}
+        </div>
     </template>
 </template>
-
-<script>
-export default {
-    props: {
-        user: Object,
-    },
-
-    data() {
-        return {
-            hasAvatarError: false,
-        };
-    },
-
-    computed: {
-        initials() {
-            return this.user.initials || '?';
-        },
-
-        useAvatar() {
-            return this.hasAvatar && !this.hasAvatarError;
-        },
-
-        hasAvatar() {
-            return !!this.user.avatar;
-        },
-
-        avatarSrc() {
-            if (!this.hasAvatar) return null;
-
-            return this.user.avatar.permalink || this.user.avatar;
-        },
-
-        useInitials() {
-            return !this.hasAvatar || this.hasAvatarError;
-        },
-    },
-};
-</script>
