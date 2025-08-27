58d87c45c40aa9d036d014efbca11dd69c181dd3
Jason Varga <jason@pixelfear.com>
2025-08-27 18:54:16 -0400

    these appear to be unnecessary now

--- resources/js/package/vite-plugin/externals.js
+++ resources/js/package/vite-plugin/externals.js
@@ -22,12 +22,7 @@ export default function() {
                 name: 'statamic-externals',
                 renderChunk(code, chunk) {
                     return code
-                        .replace(/import\s+(.+?)\s+from\s+['"]vue['"];?/g, 'const $1 = window.Vue;')
-                        .replace(/import\s+(.+?)\s+from\s+['"]@statamic\/cms['"];?/g, 'const $1 = window.__STATAMIC__.core;')
-                        .replace(/import\s+(.+?)\s+from\s+['"]@statamic\/cms\/ui['"];?/g, 'const $1 = window.__STATAMIC__.ui;')
-                        .replace(/import\s+(.+?)\s+from\s+['"]@statamic\/cms\/bard['"];?/g, 'const $1 = window.__STATAMIC__.bard;')
-                        .replace(/import\s+(.+?)\s+from\s+['"]@statamic\/cms\/save-pipeline['"];?/g, 'const $1 = window.__STATAMIC__.savePipeline;')
-                        .replace(/import\s+(.+?)\s+from\s+['"]@statamic\/cms\/temporary['"];?/g, 'const $1 = window.__STATAMIC__.temporary;');
+                        .replace(/import\s+(.+?)\s+from\s+['"]vue['"];?/g, 'const $1 = window.Vue;');
                 }
             });
         }
