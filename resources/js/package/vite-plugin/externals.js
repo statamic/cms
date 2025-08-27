c7d7c1d4554cc1ad8cab55c3085606be60807128
Jason Varga <jason@pixelfear.com>
2025-08-27 18:32:32 -0400

    move to file

---
+++ resources/js/package/vite-plugin/externals.js
@@ -0,0 +1,36 @@
+export default function() {
+    return {
+        name: 'statamic-externals',
+
+        config(config, { command }) {
+            // Ensure rollupOptions exists
+            config.build = config.build || {};
+            config.build.rollupOptions = config.build.rollupOptions || {};
+            config.build.rollupOptions.external = config.build.rollupOptions.external || [];
+            config.build.rollupOptions.output = config.build.rollupOptions.output || {};
+
+            // Add Vue as external
+            const existingExternal = config.build.rollupOptions.external;
+            config.build.rollupOptions.external = [...existingExternal, 'vue'];
+
+            return config;
+        },
+
+        configResolved(resolvedConfig) {
+            resolvedConfig.build.rollupOptions.plugins = resolvedConfig.build.rollupOptions.plugins || [];
+            resolvedConfig.build.rollupOptions.plugins.push({
+                name: 'statamic-global-externals',
+                renderChunk(code, chunk) {
+                    code = code.replace(/import\s+(.+?)\s+from\s+['"]vue['"];?/g, 'const $1 = window.Vue;');
+                    code = code.replace(/import\s+(.+?)\s+from\s+['"]@statamic\/cms['"];?/g, 'const $1 = window.__STATAMIC__.core;');
+                    code = code.replace(/import\s+(.+?)\s+from\s+['"]@statamic\/cms\/ui['"];?/g, 'const $1 = window.__STATAMIC__.ui;');
+                    code = code.replace(/import\s+(.+?)\s+from\s+['"]@statamic\/cms\/bard['"];?/g, 'const $1 = window.__STATAMIC__.bard;');
+                    code = code.replace(/import\s+(.+?)\s+from\s+['"]@statamic\/cms\/save-pipeline['"];?/g, 'const $1 = window.__STATAMIC__.savePipeline;');
+                    code = code.replace(/import\s+(.+?)\s+from\s+['"]@statamic\/cms\/temporary['"];?/g, 'const $1 = window.__STATAMIC__.temporary;');
+
+                    return code;
+                }
+            });
+        }
+    };
+}
--- resources/js/package/vite-plugin/index.js
+++ resources/js/package/vite-plugin/index.js
@@ -1,48 +1,12 @@
 import vue from '@vitejs/plugin-vue';
+import externals from './externals.js';
 import tailwindExclusions from './tailwind-exclusions.js';
 import preventServer from './prevent-server.js';
 
-const statamic = function (options) {
-    return {
-        name: 'statamic',
-
-        config(config, { command }) {
-            // Ensure rollupOptions exists
-            config.build = config.build || {};
-            config.build.rollupOptions = config.build.rollupOptions || {};
-            config.build.rollupOptions.external = config.build.rollupOptions.external || [];
-            config.build.rollupOptions.output = config.build.rollupOptions.output || {};
-
-            // Add Vue as external
-            const existingExternal = config.build.rollupOptions.external;
-            config.build.rollupOptions.external = [...existingExternal, 'vue'];
-
-            return config;
-        },
-
-        configResolved(resolvedConfig) {
-            resolvedConfig.build.rollupOptions.plugins = resolvedConfig.build.rollupOptions.plugins || [];
-            resolvedConfig.build.rollupOptions.plugins.push({
-                name: 'statamic-global-externals',
-                renderChunk(code, chunk) {
-                    code = code.replace(/import\s+(.+?)\s+from\s+['"]vue['"];?/g, 'const $1 = window.Vue;');
-                    code = code.replace(/import\s+(.+?)\s+from\s+['"]@statamic\/cms['"];?/g, 'const $1 = window.__STATAMIC__.core;');
-                    code = code.replace(/import\s+(.+?)\s+from\s+['"]@statamic\/cms\/ui['"];?/g, 'const $1 = window.__STATAMIC__.ui;');
-                    code = code.replace(/import\s+(.+?)\s+from\s+['"]@statamic\/cms\/bard['"];?/g, 'const $1 = window.__STATAMIC__.bard;');
-                    code = code.replace(/import\s+(.+?)\s+from\s+['"]@statamic\/cms\/save-pipeline['"];?/g, 'const $1 = window.__STATAMIC__.savePipeline;');
-                    code = code.replace(/import\s+(.+?)\s+from\s+['"]@statamic\/cms\/temporary['"];?/g, 'const $1 = window.__STATAMIC__.temporary;');
-
-                    return code;
-                }
-            });
-        }
-    };
-};
-
 export default function (options = {}) {
     return [
-        statamic(options),
         preventServer(),
+        externals(),
         tailwindExclusions(),
         vue(options.vue || {}),
     ];
