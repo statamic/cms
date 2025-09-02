1210c292f0110a3f13fc785c09c28c4cfbd78b08
Jay George <contact@jaygeorge.co.uk>
2025-09-02 12:52:10 +0100

    Remove accidental commit

--- resources/js/components/ui/Input/GroupPrepend.vue
+++ resources/js/components/ui/Input/GroupPrepend.vue
@@ -9,7 +9,7 @@ const props = defineProps({
         :class="[
             'flex items-center justify-center',
             'shadow-ui-sm disabled:shadow-none',
-            'border border-gray-300 border-e-0 bg-gray-50 text-gray-600 antialiased dark:border-white/10 dark:bg-gray-950 dark:text-gray-300',
+            'border border-gray-300 bg-gray-50 text-gray-600 antialiased dark:border-white/10 dark:bg-gray-950 dark:text-gray-300',
             'rounded-s-lg px-3 text-sm leading-[1.375rem] shrink-0',
         ]"
         data-ui-input-group-prepend
