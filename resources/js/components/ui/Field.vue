9ff62636e7c5c19b52f0a1f7311c363b0c0d078e
Jay George <contact@jaygeorge.co.uk>
2025-09-02 13:08:25 +0100

    Tweak distance between field instructions and fields an itsy bitsy amount

--- resources/js/components/ui/Field.vue
+++ resources/js/components/ui/Field.vue
@@ -76,7 +76,7 @@ const wrapperComponent = computed(() => props.as === 'card' ? Card : 'div');
             <slot v-if="!$slots.actions" name="label">
                 <Label v-if="label" v-bind="labelProps" class="flex-1" />
             </slot>
-            <Description :text="instructions" v-if="instructions && !instructionsBelow" :class="variant === 'inline' ? '-mt-0.5' : 'mb-1.75 -mt-0.5'" />
+            <Description :text="instructions" v-if="instructions && !instructionsBelow" :class="variant === 'inline' ? '-mt-0.5' : 'mb-2 -mt-0.5'" />
         </div>
         <slot />
         <Description :text="instructions" v-if="instructions && instructionsBelow" class="mt-2" />
