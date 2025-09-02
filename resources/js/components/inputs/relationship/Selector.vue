658f3aa96fd9b9cb2cd32377bccc03d242efc137
Duncan McClean <duncan@duncanmcclean.com>
2025-09-02 12:13:36 +0100

    Clicking on entry title should select entry, not go to publish form page

--- resources/js/components/inputs/relationship/Selector.vue
+++ resources/js/components/inputs/relationship/Selector.vue
@@ -35,7 +35,7 @@
                     <Panel class="relative mb-0! overflow-x-auto overscroll-x-contain">
                         <Table>
                             <template #cell-title="{ row: entry, isColumnVisible }">
-                                <a class="title-index-field" :href="entry.edit_url" @click.stop>
+                                <a class="title-index-field" :href="entry.edit_url" @click.prevent="toggleSelection(entry.id)">
                                     <StatusIndicator v-if="!isColumnVisible('status')" :status="entry.status" />
                                     <span v-text="entry.title" />
                                 </a>
