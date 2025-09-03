e96097045978736663cf1d0dd5a9f8430599712b
Jay George <contact@jaygeorge.co.uk>
2025-09-03 16:50:54 +0100

    Replicator legibility -- add a fade transition to the add set component and darken slightly

--- resources/js/components/fieldtypes/replicator/AddSetButton.vue
+++ resources/js/components/fieldtypes/replicator/AddSetButton.vue
@@ -7,15 +7,15 @@
             </div>
             <div
                 v-if="variant === 'between'"
-                class="flex justify-center py-4 relative group opacity-0 hover:opacity-100"
+                class="flex justify-center py-3 relative group"
             >
-                <div v-if="showConnector" class="absolute group-hover:opacity-0 transition-opacity delay-25 duration-125 inset-y-0 h-full left-3.5 border-l-1 border-gray-400 dark:border-gray-600 border-dashed z-0 dark:bg-dark-700" />
-                <button class="w-full absolute inset-0 h-full opacity-0 group-hover:opacity-100 transition-opacity delay-25 duration-75 cursor-pointer">
+                <div v-if="showConnector" class="absolute opacity-100 group-hover:opacity-0 transition-opacity delay-10 duration-250 inset-y-0 h-full left-3.5 border-l-1 border-gray-400 dark:border-gray-600 border-dashed z-0 dark:bg-dark-700" />
+                <button class="w-full absolute inset-0 h-full opacity-0 group-hover:opacity-100 transition-opacity delay-10 duration-250 cursor-pointer">
                     <div class="h-full flex flex-col justify-center">
-                        <div class="rounded-full bg-gray-200 h-2" />
+                        <div class="rounded-full bg-gray-300 h-2" />
                     </div>
                 </button>
-                <Button v-if="enabled" round icon="plus" size="sm" class="-my-4 z-3 opacity-0 group-hover:opacity-100 transition-opacity delay-25 duration-75" />
+                <Button v-if="enabled" round icon="plus" size="sm" class="-my-4 z-3 opacity-0 group-hover:opacity-100 transition-opacity delay-10 duration-250" />
             </div>
         </template>
     </set-picker>
