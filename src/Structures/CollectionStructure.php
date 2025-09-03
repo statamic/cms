6933e7f39ecd108727732a6b5f3bea886c6353d0
Ryan Mitchell <ryan@thoughtcollective.com>
2025-09-03 16:33:10 +0100

    Fix failing tests

--- src/Structures/CollectionStructure.php
+++ src/Structures/CollectionStructure.php
@@ -72,8 +72,8 @@ class CollectionStructure extends Structure
             throw new \Exception("Duplicate entry [{$entryId}] in [{$this->collection()->handle()}] collection's structure.");
         }
 
-        $thisCollectionsEntries = Blink::once('collection-structure-tree-entries::'.$this->collection()->handle().'::'.$locale, fn () => $this->collection()->queryEntries()->where('site', $locale)->get())
-            ->pluck('id');
+        $thisCollectionsEntries = Blink::once('collection-structure-tree-entries::'.$this->handle().'::'.$locale, fn () => $this->collection()->queryEntries())
+            ->where('site', $locale)->pluck('id');
 
         $otherCollectionEntries = $entryIds->diff($thisCollectionsEntries);
 
