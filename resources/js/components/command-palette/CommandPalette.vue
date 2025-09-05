1253a6023977840d03a1018f0eb83e2194b0892f
Duncan McClean <duncan@doublethree.digital>
2025-09-05 15:27:34 +0100

    Make "Search" translatable too

--- resources/js/components/command-palette/CommandPalette.vue
+++ resources/js/components/command-palette/CommandPalette.vue
@@ -245,7 +245,7 @@ const modalClasses = cva({
                 hover:bg-black/45 hover:text-white/70
             ">
                 <Icon name="magnifying-glass" class="size-5 flex-none text-white/50 group-hover:text-white/70" />
-                <span class="sr-only leading-none md:not-sr-only st-text-trim-cap">Search</span>
+                <span class="sr-only leading-none md:not-sr-only st-text-trim-cap">{{ __('Search') }}</span>
                 <kbd class="ml-auto hidden self-center rounded bg-white/5 px-[0.3125rem] py-[0.0625rem] text-[0.625rem]/4 font-medium text-white/60 group-hover:text-white/70 ring-1 ring-white/7.5 [word-spacing:-0.15em] ring-inset md:block">
                     <kbd class="font-sans">⌘ </kbd><kbd class="font-sans">K</kbd>
                 </kbd>
