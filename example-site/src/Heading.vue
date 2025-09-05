15b05a40ac7298656fc3afadf93174076af82901
Jason Varga <jason@pixelfear.com>
2025-09-05 15:33:46 -0400

    delete example

--- example-site/src/Heading.vue
+++
@@ -1,6 +0,0 @@
-<script setup>
-defineProps(['text'])
-</script>
-<template>
-    <h2 class="text-xl font-medium mb-4">{{ text }}</h2>
-</template>
--- example-site/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './style.css'
-// import { Heading } from '@statamic/ui';
-
-const app = createApp(App);
-// app.component('ui-heading', Heading);
-app.mount('#app');
