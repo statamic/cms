aba5b0df9d5e1fc2da120682329679bcea4efd33
Jay George <github@jaygeorge.co.uk>
2025-09-08 19:16:44 +0100

    [6.x] Replicator improvements including Bard replicators (#12361)

--- resources/js/components/fieldtypes/replicator/Set.vue
+++ resources/js/components/fieldtypes/replicator/Set.vue
@@ -135,9 +135,9 @@ function destroy() {
             :data-type="config.handle"
         >
             <header
-                class="group/header animate-border-color flex items-center rounded-lg px-1.5 antialiased duration-200 bg-gray-100/50 dark:bg-gray-950 hover:bg-gray-100 dark:hover:bg-gray-950 border-gray-300 dark:border-white/10"
+                class="group/header animate-border-color flex items-center rounded-lg px-1.5 antialiased duration-200 bg-gray-100/50 dark:bg-gray-950 hover:bg-gray-100 dark:hover:bg-gray-950 border-gray-300 border-b-1 border-b-transparent"
                 :class="{ 
-                    'bg-gray-200/50 rounded-b-none border-b border-gray-300': !collapsed
+                    'bg-gray-200/50 rounded-b-none border-b-gray-300! dark:border-b-white/10!': !collapsed
                 }"
             >
                 <Icon
