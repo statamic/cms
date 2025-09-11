61c04003933e2aee262a9b3da62d0809c95b9ee7
Jason Varga <jason@pixelfear.com>
2025-09-11 14:03:17 -0400

    Move drag-dots out of ui package - its not part of the set

--- packages/ui/src/DragHandle.vue
+++ packages/ui/src/DragHandle.vue
@@ -1,9 +1,13 @@
+<script setup>
+import DragDots from '@/../svg/drag-dots.svg';
+</script>
+
 <template>
     <button
         class="h-full cursor-grab flex items-center justify-center text-gray-400 dark:text-gray-500 hover:text-gray-700 dark:hover:text-gray-400"
         data-drag-handle
         :aria-label="__('Drag to reorder')"
     >
-        <ui-icon name="drag-dots" class="w-[7px] h-[17px]" />
+        <DragDots class="w-[7px] h-[17px]" />
     </button>
 </template>
