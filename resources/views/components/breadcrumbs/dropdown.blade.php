450006c868cff7b532a0a4a55ad1f9393297056b
Jason Varga <jason@pixelfear.com>
2025-09-15 14:59:22 -0400

    Buttons in blade to prevent pop-in

--- resources/views/components/breadcrumbs/dropdown.blade.php
+++ resources/views/components/breadcrumbs/dropdown.blade.php
@@ -1,4 +1,22 @@
-<ui-dropdown class="relative" aria-label="{{ __('More options for') }} {{ __($breadcrumb->text()) }}">
+{{-- Hardcoded duplicate of the dropdown trigger in blade to prevent Vue pop in --}}
+<button
+    v-if="false"
+    class="
+        inline-flex items-center justify-center whitespace-nowrap shrink-0
+        font-medium antialiased cursor-pointer no-underline
+        disabled:text-gray-400 dark:disabled:text-gray-600 disabled:cursor-not-allowed
+        [&_svg]:shrink-0 [&_svg]:text-black [&_svg]:opacity-60 dark:[&_svg]:text-white
+        bg-transparent hover:bg-gray-400/10 text-gray-900 dark:text-gray-300 dark:hover:bg-white/15 dark:hover:text-gray-200
+        h-10 text-sm rounded-lg px-0 gap-0 w-10
+        [&_svg]:size-4.5 [&_svg]:size-3! h-8! w-4! hover:bg-gray-300/5! -ml-3 mr-1
+    "
+    type="button"
+    data-ui-dropdown-trigger
+>
+    @cp_svg('icons/chevron-vertical')
+</button>
+
+<ui-dropdown v-cloak class="relative" aria-label="{{ __('More options for') }} {{ __($breadcrumb->text()) }}">
     <template #trigger>
         <ui-button
             variant="ghost"
--- resources/views/components/breadcrumbs/index.blade.php
+++ resources/views/components/breadcrumbs/index.blade.php
@@ -1,8 +1,21 @@
-<div class="items-center gap-2 hidden md:flex" data-global-header-breadcrumbs v-cloak>
+<div class="items-center gap-2 hidden md:flex" data-global-header-breadcrumbs>
     @foreach($breadcrumbs as $breadcrumb)
         <div class="items-center gap-2 md:flex entry-animate-in entry-animate-in--quick">
             <span class="text-white/30">/</span>
-            <ui-button href="{{ $breadcrumb->url() }}" text="{{ __($breadcrumb->text()) }}" size="sm" variant="ghost" class="dark:text-white/85! hover:text-white! px-2! mr-1.75"></ui-button>
+            <a
+                class="
+                    inline-flex items-center justify-center whitespace-nowrap shrink-0
+                    font-medium antialiased cursor-pointer no-underline
+                    disabled:text-gray-400 dark:disabled:text-gray-600 disabled:cursor-not-allowed
+                    bg-transparent hover:bg-gray-400/10 text-gray-900 dark:text-gray-300 dark:hover:bg-white/15 dark:hover:text-gray-200 px-3 h-8
+                    text-[0.8125rem] leading-tight gap-2 rounded-lg
+                    dark:text-white/85! hover:text-white! px-2! mr-1.75
+                "
+                href="{{ $breadcrumb->url() }}"
+            >
+                {{ __($breadcrumb->text()) }}
+            </a>
+
             @if($breadcrumb->hasLinks() || $breadcrumb->createUrl())
                 <x-statamic::breadcrumbs.dropdown :$breadcrumb />
             @endif
