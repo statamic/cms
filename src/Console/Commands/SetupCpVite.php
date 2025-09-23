1ffa989697823b073ad401239ab57750649e28b0
Duncan McClean <duncan@duncanmcclean.com>
2025-09-23 15:24:30 +0100

    okay claude, let's try your suggestion

--- src/Console/Commands/SetupCpVite.php
+++ src/Console/Commands/SetupCpVite.php
@@ -62,7 +62,11 @@ class SetupCpVite extends Command
 
                 File::put($packageJsonPath, json_encode($contents, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
 
-                Process::path(base_path())->tty($this->input->isInteractive())->run('npm install');
+//                Process::path(base_path())->tty($this->input->isInteractive())->run('npm install');
+
+                $this->input->isInteractive()
+                    ? Process::path(base_path())->tty()->run('npm install')
+                    : Process::path(base_path())->run('npm install');
             },
             message: 'Installing dependencies...'
         );
