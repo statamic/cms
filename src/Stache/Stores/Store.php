statamic/cms

commit 46e285d6d5ee14f494fb66a181881342c21e331b

[5.x] Updated the Stache Store to not remove stache items that exist (#11863)

src/Stache/Stores/Store.php
@@ -208,7 +208,7 @@ abstract class Store
 
         // Get all the deleted files.
         // This would be any paths that exist in the cached array that aren't there anymore.
-        $deleted = $existing->keys()->diff($files->keys())->values();
+        $deleted = $existing->keys()->map(fn ($path) => $this->getKeyFromPath($path))->diff($files->keys()->map(fn ($path) => $this->getKeyFromPath($path)))->values();
 
         // If there are no modified or deleted files, there's nothing to update.
         if ($modified->isEmpty() && $deleted->isEmpty()) {

commit 14b2f20109620a9e908ccc747b3bc5e854ddda34

Addressed an issue with a performance regression caused by PR #11863

src/Stache/Stores/Store.php
@@ -208,7 +208,8 @@ abstract class Store
 
         // Get all the deleted files.
         // This would be any paths that exist in the cached array that aren't there anymore.
-        $deleted = $existing->keys()->map(fn ($path) => $this->getKeyFromPath($path))->diff($files->keys()->map(fn ($path) => $this->getKeyFromPath($path)))->values();
+        $keys = $this->paths()->flip();
+        $deleted = $existing->keys()->map(fn ($path) => $keys->get($path))->diff($files->keys()->map(fn ($path) => $keys->get($path)))->values();
 
         // If there are no modified or deleted files, there's nothing to update.
         if ($modified->isEmpty() && $deleted->isEmpty()) {