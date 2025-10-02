46ee5ebc57dcd41e84354f26cda539bc8bab965f
Jason Varga <jason@pixelfear.com>
2025-10-02 15:56:34 -0400

    Move nav methods inside function so they dont get run until you execute the function

--- resources/js/components/nav/navigation.js
+++ resources/js/components/nav/navigation.js
@@ -6,53 +6,53 @@ import { router } from '@inertiajs/vue3';
 let navData = null;
 let breadcrumbsData = null;
 
-function unsetActiveItem(data) {
-    data.forEach(section => {
-        section.items.forEach(item => {
-            item.active = false;
-            item.children.forEach(child => child.active = false);
-        });
-    });
-}
-
-function findItemInNav(data, id) {
-    for (const section of data) {
-        for (const item of section.items) {
-            if (item.id === id) {
-                return item;
-            }
-        }
-    }
-}
-
-function findItemInChildren(parent, id) {
-    return parent.children.find(c => c.id === id);
-}
-
-function applyUpdate(data, update) {
-    unsetActiveItem(data);
-
-    if (update.type === 'parent') {
-        findItemInNav(data, update.parentId).active = true;
-    } else if (update.type === 'child') {
-        const parent = findItemInNav(data, update.parentId);
-        parent.active = true;
-        findItemInChildren(parent, update.childId).active = true;
-    }
-}
-
-router.on('success', () => {
-    const freshNav = deepClone(useStatamicPageProps().nav);
-    navData.value = freshNav;
-    breadcrumbsData.value = deepClone(freshNav);
-});
-
 export default function useNavigation() {
     if (!navData) {
         const { nav: navProp } = useStatamicPageProps();
         const cloned = deepClone(navProp);
         navData = ref(cloned);
         breadcrumbsData = ref(deepClone(cloned));
+    }
+
+    router.on('success', () => {
+        const freshNav = deepClone(useStatamicPageProps().nav);
+        navData.value = freshNav;
+        breadcrumbsData.value = deepClone(freshNav);
+    });
+
+    function unsetActiveItem(data) {
+        data.forEach(section => {
+            section.items.forEach(item => {
+                item.active = false;
+                item.children.forEach(child => child.active = false);
+            });
+        });
+    }
+
+    function findItemInNav(data, id) {
+        for (const section of data) {
+            for (const item of section.items) {
+                if (item.id === id) {
+                    return item;
+                }
+            }
+        }
+    }
+
+    function findItemInChildren(parent, id) {
+        return parent.children.find(c => c.id === id);
+    }
+
+    function applyUpdate(data, update) {
+        unsetActiveItem(data);
+
+        if (update.type === 'parent') {
+            findItemInNav(data, update.parentId).active = true;
+        } else if (update.type === 'child') {
+            const parent = findItemInNav(data, update.parentId);
+            parent.active = true;
+            findItemInChildren(parent, update.childId).active = true;
+        }
     }
 
     function setParentActive(parent, source = 'nav') {
