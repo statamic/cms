e1fba7465280110a634eadec7af6eb67429a7173
Jason Varga <jason@pixelfear.com>
2025-10-02 18:09:14 -0400

    consolidate listing and page components

--- resources/js/pages/addons/Index.vue
+++ resources/js/pages/addons/Index.vue
@@ -1,11 +1,25 @@
 <script setup>
+import { ref, onMounted } from 'vue';
 import Head from '@/pages/layout/Head.vue';
-import AddonListing from '@/components/addons/Listing.vue';
-import { Header, Button, DocsCallout, CommandPaletteItem } from '@ui';
+import { Header, Button, DocsCallout, CommandPaletteItem, Listing, Badge, DropdownItem } from '@ui';
 
-defineProps({
+const props = defineProps({
     addons: Array,
     columns: Array,
+});
+
+const rows = ref(props.addons);
+
+onMounted(() => {
+    props.addons.forEach(addon => {
+        Statamic.$commandPalette.add({
+            category: Statamic.$commandPalette.category.Actions,
+            text: [__('Browse the Marketplace'), addon.name],
+            icon: 'external-link',
+            url: addon.marketplace_url,
+            openNewTab: true,
+        });
+    });
 });
 </script>
 
@@ -32,10 +46,28 @@ defineProps({
         </CommandPaletteItem>
     </Header>
 
-    <AddonListing
-        :initial-rows="addons"
-        :initial-columns="columns"
-    />
+    <Listing
+        :items="rows"
+        :columns="columns"
+        :allow-search="false"
+        :allow-customizing-columns="false"
+    >
+        <template #cell-name="{ row: addon }">
+            <a v-if="addon.marketplace_url" :href="addon.marketplace_url" target="_blank">{{ __(addon.name) }}</a>
+            <span v-else>
+                {{ __(addon.name) }}
+                <Badge class="ml-1" size="sm" :text="__('Unlisted')" />
+            </span>
+        </template>
+        <template #cell-version="{ value: handle }">
+            <span class="font-mono text-xs">{{ handle }}</span>
+        </template>
+        <template #prepended-row-actions="{ row: addon }">
+            <DropdownItem v-if="addon.marketplace_url" :text="__('View on the Marketplace')" icon="external-link" :href="addon.marketplace_url" target="_blank" />
+            <DropdownItem v-if="addon.updates_url" :text="__('Release Notes')" icon="updates" :href="addon.updates_url" />
+            <DropdownItem v-if="addon.settings_url" :text="__('Settings')" icon="cog" :href="addon.settings_url" />
+        </template>
+    </Listing>
 
     <DocsCallout :topic="__('Addons')" url="addons" />
 </template>
