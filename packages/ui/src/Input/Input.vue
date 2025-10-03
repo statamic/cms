b424d81bdbf8aa57a570e0a14e918f95911cb2a9
Jay George <contact@jaygeorge.co.uk>
2025-10-03 17:28:19 +0100

    Dark mode - Chill out inputs

--- packages/ui/src/Input/Input.vue
+++ packages/ui/src/Input/Input.vue
@@ -39,7 +39,7 @@ const hasAppendedIcon = computed(() => !!props.iconAppend || !!slots.append || p
 const inputClasses = computed(() => {
     const classes = cva({
         base: [
-            'w-full block bg-white dark:bg-gray-950',
+            'w-full block bg-white dark:bg-gray-900',
             'border border-gray-300 with-contrast:border-gray-500 dark:border-white/15 dark:inset-shadow-2xs dark:inset-shadow-black',
             'text-gray-950 dark:text-gray-300 placeholder:text-gray-500 dark:placeholder:text-gray-400/85',
             'appearance-none antialiased shadow-ui-sm disabled:shadow-none disabled:opacity-50 read-only:border-dashed not-prose',
