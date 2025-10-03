620d1919db75825a502d1608641f5b8546d0c0a3
Jason Varga <jason@pixelfear.com>
2025-10-03 12:50:10 -0400

    [6.x] Wire up more Inertia pages (#12646)

--- resources/js/pages/layout/Head.vue
+++ resources/js/pages/layout/Head.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, inject } from 'vue';
 import { Head } from '@inertiajs/vue3';
+import useStatamicPageProps from '@/composables/page-props.js';
 
 const props = defineProps<{
     title?: string | string[];
 }>();
 
+const { cmsName } = useStatamicPageProps();
+
 const title = computed(() => {
     let title = props.title;
     if (typeof title === 'string') title = [title];
-    title.push(Statamic.$config.get('cmsName'));
+    title.push(cmsName);
     const divider = Statamic.$config.get('direction') === 'ltr' ? '‹' : '›';
     return title.join(` ${divider} `);
 });
---
+++ resources/js/pages/taxonomies/Empty.vue
@@ -0,0 +1,51 @@
+<script setup>
+import Head from '@/pages/layout/Head.vue';
+import { Icon, EmptyStateMenu, EmptyStateItem, DocsCallout } from '@ui';
+
+defineProps([
+    'taxonomyTitle',
+    'createUrl',
+    'taxonomyEditUrl',
+    'taxonomyBlueprintsUrl',
+    'canCreate',
+    'canEdit',
+    'canConfigureFields',
+]);
+</script>
+
+<template>
+    <Head :title="taxonomyTitle" />
+
+    <header class="py-8 mt-8 text-center">
+        <h1 class="text-[25px] font-medium antialiased flex justify-center items-center gap-2">
+            <Icon name="taxonomies" class="size-5 text-gray-500" />
+            <span>{{ __(taxonomyTitle) }}</span>
+        </h1>
+    </header>
+
+    <EmptyStateMenu :heading="__('Start designing your taxonomy with these steps')">
+        <EmptyStateItem
+            v-if="canEdit"
+            :href="taxonomyEditUrl"
+            icon="configure"
+            :heading="__('Configure Taxonomy')"
+            :description="__('statamic::messages.taxonomy_next_steps_configure_description')"
+        />
+        <EmptyStateItem
+            v-if="canCreate"
+            :href="createUrl"
+            icon="fieldtype-taxonomy"
+            :heading="__('Create Term')"
+            :description="__('statamic::messages.taxonomy_next_steps_create_term_description')"
+        />
+        <EmptyStateItem
+            v-if="canConfigureFields"
+            :href="taxonomyBlueprintsUrl"
+            icon="blueprints"
+            :heading="__('Configure Blueprints')"
+            :description="__('statamic::messages.taxonomy_next_steps_blueprints_description')"
+        />
+    </EmptyStateMenu>
+
+    <DocsCallout :topic="__('Taxonomies')" url="taxonomies" />
+</template>
