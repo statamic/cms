8e7d52de59d6a829991d512782c384f5be179413
Duncan McClean <duncan@duncanmcclean.com>
2025-10-03 18:28:19 +0100

    Wire up `PublishForm::make()` as an Inertia page

---
+++ resources/js/pages/PublishForm.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { PublishForm } from '@/components/ui';
+
+const props = defineProps({
+    icon: { type: String, required: false },
+    title: { type: String, required: true },
+    blueprint: { type: Object, required: true },
+    values: { type: Object, required: true },
+    meta: { type: Object, required: true },
+    submitUrl: { type: String, required: true },
+    submitMethod: { type: String, required: true },
+    readOnly: { type: Boolean, required: false },
+    asConfig: { type: Boolean, required: false },
+});
+</script>
+
+<template>
+    <div :class="{ 'max-w-5xl mx-auto': asConfig }">
+        <PublishForm
+            :icon
+            :title
+            :blueprint
+            :initial-values="values"
+            :initial-meta="meta"
+            :submit-url
+            :submit-method
+            :read-only
+            :as-config
+        />
+    </div>
+</template>
