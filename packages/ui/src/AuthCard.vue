c0b6cf3b26878102a776ee1c47240f966045e0be
Jay George <contact@jaygeorge.co.uk>
2025-10-06 11:11:30 +0100

    Dark mode - finish login

--- packages/ui/src/AuthCard.vue
+++ packages/ui/src/AuthCard.vue
@@ -13,8 +13,8 @@ const hasHeader = computed(() => {
 </script>
 
 <template>
-    <div class="bg-white dark:bg-gray-950 backdrop-blur-[2px] border border-gray-200 dark:border-none rounded-2xl p-2 shadow-[0_8px_5px_-6px_rgba(0,0,0,0.12),_0_3px_8px_0_rgba(0,0,0,0.02),_0_30px_22px_-22px_rgba(39,39,42,0.35)] dark:inset-shadow-2xs dark:inset-shadow-black">
-        <div class="relative space-y-3 rounded-xl ring ring-gray-300 bg-white dark:bg-gray-850 dark:ring-x-0 dark:ring-b-0 dark:ring-gray-700 p-4 shadow-[0_1px_16px_-2px_rgba(63,63,71,0.2)]">
+    <div class="bg-white dark:bg-gray-950 backdrop-blur-[2px] border border-gray-200 dark:border-none rounded-2xl p-2 shadow-[0_8px_5px_-6px_rgba(0,0,0,0.12),_0_3px_8px_0_rgba(0,0,0,0.02),_0_30px_22px_-22px_rgba(39,39,42,0.35)] dark:shadow-[0_8px_5px_-6px_rgba(0,0,0,0.3),_0_3px_8px_0_rgba(0,0,0,0.15),_0_30px_22px_-22px_rgba(0,0,0,0.4)]">
+        <div class="relative space-y-3 rounded-xl border border-gray-300 bg-white dark:bg-gray-850 dark:border-gray-700 p-4 shadow-[0_1px_16px_-2px_rgba(63,63,71,0.2)]">
             <header class="flex flex-col justify-center items-center mb-8 py-3" v-if="hasHeader">
                 <ui-card class="p-2! mb-4 flex items-center justify-center" v-if="icon">
                     <ui-icon :name="icon" class="size-5" />
