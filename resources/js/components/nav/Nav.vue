10223398127bdd0db0f28a11c06258659860f933
Jason Varga <jason@pixelfear.com>
2025-10-06 18:48:50 -0400

    translate nav

--- resources/js/components/nav/Nav.vue
+++ resources/js/components/nav/Nav.vue
@@ -40,7 +40,7 @@ Statamic.$events.$on('nav.toggle', toggle);
             <div
                 class="section-title"
                 v-if="section.display !== 'Top Level'"
-                v-text="section.display"
+                v-text="__(section.display)"
             />
             <ul>
                 <li v-for="(item, i) in section.items" :key="i">
@@ -53,14 +53,14 @@ Statamic.$events.$on('nav.toggle', toggle);
                             @click="setParentActive(item)"
                         >
                             <Icon :name="item.icon" />
-                            <span v-text="item.display" />
+                            <span v-text="__(item.display)" />
                         </Link>
                         <ul v-if="item.children.length && item.active">
                             <li v-for="(child, i) in item.children" :key="i">
                                 <Link
                                     :href="child.url"
                                     v-bind="child.attributes"
-                                    v-text="child.display"
+                                    v-text="__(child.display)"
                                     :class="{ 'active': child.active }"
                                     @click="setChildActive(item, child)"
                                 />
