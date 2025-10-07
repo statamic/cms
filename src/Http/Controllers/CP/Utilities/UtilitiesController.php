3737445837bf9df0192e82d17c5d00c3efa153ef
Jason Varga <jason@pixelfear.com>
2025-10-07 12:41:35 -0400

    utility show view with template extraction so existing views that extend layout can continue to just work

--- src/Http/Controllers/CP/Utilities/UtilitiesController.php
+++ src/Http/Controllers/CP/Utilities/UtilitiesController.php
@@ -26,10 +26,45 @@ class UtilitiesController extends CpController
         $utility = Utility::findBySlug($this->getUtilityHandle($request));
 
         if ($view = $utility->view()) {
-            return view($view, $utility->viewData($request));
+            return Inertia::render('utilities/Show', [
+                'html' => $this->renderView($view, $utility->viewData($request)),
+            ]);
         }
 
         throw new \Exception("Utility [{$utility->handle()}] has not been provided with an action or view.");
+    }
+
+    private function renderView($view, $data)
+    {
+        $html = view($view, $data)->render();
+
+        return $this->extractTemplateFromLayout($html);
+    }
+
+    private function extractTemplateFromLayout($html)
+    {
+        // If it doesn't contain the layout's #statamic div, it probably didn't extend the layout.
+        if (! str_contains($html, 'id="statamic"')) {
+            return $html;
+        }
+
+        $dom = new \DOMDocument();
+        @$dom->loadHTML($html, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+
+        if ($statamicDiv = $dom->getElementById('statamic')) {
+            $content = '';
+            foreach ($statamicDiv->childNodes as $node) {
+                // Skip the blade-title div
+                if ($node->nodeType === XML_ELEMENT_NODE && $node->getAttribute('id') === 'blade-title') {
+                    continue;
+                }
+                $content .= $dom->saveHTML($node);
+            }
+
+            return $content;
+        }
+
+        return $html;
     }
 
     private function getUtilityHandle($request)
