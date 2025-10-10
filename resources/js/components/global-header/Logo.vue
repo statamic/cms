fb82f0448bbf7ea922460414d67d4493c5d7abb0
Jay George <contact@jaygeorge.co.uk>
2025-10-10 14:59:04 +0100

    Nav icon UX - add hover effect

--- resources/js/components/global-header/Logo.vue
+++ resources/js/components/global-header/Logo.vue
@@ -30,7 +30,7 @@ function toggleNav() {
         <div class="flex items-center gap-1.5 sm:gap-2 relative">
             <button class="flex items-center group rounded-lg cursor-pointer" type="button" @click="toggleNav" aria-label="{{ __('Toggle Nav') }}" style="--focus-outline-offset: 0.2rem;">
                 <div class="p-1 mr-1 sm:mr-2 size-7 flex items-center justify-center">
-                    <Icon name="burger-menu" class="size-5 opacity-60" />
+                    <Icon name="burger-menu" class="size-5 opacity-60 hover:opacity-100" />
                 </div>
                 <StatamicLogo class="size-7" />
             </button>
