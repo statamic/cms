f8d5c20fddde07d513ea0482d864761bb56b1061
Jay George <contact@jaygeorge.co.uk>
2025-10-15 12:25:11 +0100

    Nav UX - another version where 1) the hamburger is always visible on mobile, where things feel less busy anyway 2) The hamburger is revealed on hover on desktop 3) The hamburger logo is visible on desktop if hover isn't supported

--- resources/js/components/global-header/Logo.vue
+++ resources/js/components/global-header/Logo.vue
@@ -29,10 +29,10 @@ function toggleNav() {
     <template v-else>
         <div class="flex items-center gap-1.5 sm:gap-2 relative">
             <button class="flex items-center group rounded-lg cursor-pointer" type="button" @click="toggleNav" aria-label="{{ __('Toggle Nav') }}" style="--focus-outline-offset: 0.2rem;">
-                <div class="p-1 mr-1 sm:mr-2 size-7 flex items-center justify-center">
-                    <Icon name="burger-menu" class="size-5 opacity-60 hover:opacity-100" />
+                <div class="p-1 mr-1 sm:mr-2 size-7 flex items-center justify-center [@media(hover:hover)]:lg:opacity-0 [@media(hover:hover)]:lg:group-hover:opacity-100 [@media(hover:hover)]:lg:absolute lg:inset-0">
+                    <Icon name="burger-menu" class="size-5 max-lg:opacity-60 [@media(hover:none)]:lg:opacity-60 max-lg:hover:opacity-100" />
                 </div>
-                <StatamicLogo class="size-7" />
+                <StatamicLogo class="size-7 group-hover:opacity-0 transition-opacity duration-150" />
             </button>
             <Link :href="cp_url('/')" class="max-[350px]:hidden text-white/85 rounded-xs whitespace-nowrap" style="--focus-outline-offset: var(--outline-offset-button);">
                 {{ logos.text ?? logos.siteName }}
