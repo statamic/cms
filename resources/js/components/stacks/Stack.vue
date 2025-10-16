e7b5794d94a4ae89ac752ad66d575ff1638afc78
Jack McDade <jack@jackmcdade.com>
2025-10-16 09:59:16 -0400

    [6.x] Stacks facelift (#12761)
    Co-authored-by: Jason Varga <jason@pixelfear.com>

--- resources/js/components/stacks/Stack.vue
+++ resources/js/components/stacks/Stack.vue
@@ -1,15 +1,15 @@
 <template>
-    <teleport :to="portal" :order="depth" v-if="visible">
+    <teleport :to="portal" :order="depth" v-if="mounted">
         <div class="vue-portal-target stack">
             <div
                 class="stack-container"
-                :class="{ 'stack-is-current': isTopStack, hovering: isHovering, 'p-2 shadow-lg': full }"
+                :class="{ 'stack-is-current': isTopStack }"
                 :style="direction === 'ltr' ? { left: `${leftOffset}px` } : { right: `${leftOffset}px` }"
             >
                 <transition name="stack-overlay-fade">
                     <div
-                        class="stack-overlay"
                         v-if="visible"
+                        class="stack-overlay fixed inset-0 bg-gray-800/20 dark:bg-gray-800/50 backdrop-blur-[2px]"
                         :style="direction === 'ltr' ? { left: `-${leftOffset}px` } : { right: `-${leftOffset}px` }"
                     />
                 </transition>
@@ -23,7 +23,14 @@
                 />
 
                 <transition name="stack-slide">
-                    <div class="stack-content" v-if="visible">
+                    <div
+                        v-if="visible"
+                        class="stack-content fixed flex flex-col end-1.5 overflow-auto bg-white dark:bg-gray-850 rounded-xl shadow-[0_8px_5px_-6px_rgba(0,0,0,0.12),_0_3px_8px_0_rgba(0,0,0,0.02),_0_30px_22px_-22px_rgba(39,39,42,0.35)] dark:shadow-[0_5px_20px_rgba(0,0,0,.5)] transition-transform duration-150 ease-out"
+                        :class="[
+                            full ? 'inset-2' : 'inset-y-2',
+                            { '-translate-x-4 rtl:translate-x-4': isHovering }
+                        ]"
+                    >
                         <slot name="default" :depth="depth" :close="close" />
                     </div>
                 </transition>
@@ -59,6 +66,7 @@ export default {
     data() {
         return {
             stack: null,
+            mounted: false,
             visible: false,
             isHovering: false,
             escBinding: null,
@@ -165,13 +173,17 @@ export default {
         close() {
             this.visible = false;
             this.$wait(300).then(() => {
+                this.mounted = false;
                 this.$emit('closed');
             });
         },
     },
 
     mounted() {
-        this.visible = true;
+        this.mounted = true;
+        this.$nextTick(() => {
+            this.visible = true;
+        });
     },
 };
 </script>
