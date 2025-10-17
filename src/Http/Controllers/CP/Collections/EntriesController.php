68f91bb0d5ccc36ac6d060d690550fb8b5bb3acd
Duncan McClean <duncan@duncanmcclean.com>
2025-10-17 16:07:09 +0100

    Fix missing props on entry create form

--- src/Http/Controllers/CP/Collections/EntriesController.php
+++ src/Http/Controllers/CP/Collections/EntriesController.php
@@ -348,6 +348,9 @@ class EntriesController extends CpController
 
         return Inertia::render('entries/Create', [
             ...$viewData,
+            'canEditBlueprint' => User::current()->can('configure fields'),
+            'createAnotherUrl' => cp_route('collections.entries.create', [$collection, $site->handle(), 'blueprint' => $blueprint['handle'], 'parent' => $values['parent'] ?? null]),
+            'initialListingUrl' => cp_route('collections.show', $collection),
         ]);
     }
 
