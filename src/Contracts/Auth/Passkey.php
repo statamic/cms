930d96d03d88807db7df06446d868b9faa2410e3
Jason Varga <jason@pixelfear.com>
2025-10-21 16:23:04 -0400

    forgot contract

--- src/Contracts/Auth/Passkey.php
+++ src/Contracts/Auth/Passkey.php
@@ -2,13 +2,26 @@
 
 namespace Statamic\Contracts\Auth;
 
+use Carbon\Carbon;
+use Webauthn\PublicKeyCredentialSource;
+
 interface Passkey
 {
-    public function id($id = null);
+    public function id(): string;
 
-    public function user($user = null);
+    public function user(): ?User;
 
-    public function save();
+    public function setUser(string|User $user): self;
 
-    public function delete();
+    public function credential(): PublicKeyCredentialSource;
+
+    public function setCredential(array|PublicKeyCredentialSource $credential): self;
+
+    public function lastLogin(): ?Carbon;
+
+    public function setLastLogin(null|int|Carbon $time): self;
+
+    public function save(): bool;
+
+    public function delete(): bool;
 }
