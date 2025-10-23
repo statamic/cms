06c05af4b185a86b46aa0d74bfec027080ac217a
Jay George <contact@jaygeorge.co.uk>
2025-10-23 12:55:21 +0100

    Correct replicator previews with images so they're laid out horizontally using flexbox

--- packages/ui/src/Subheading.vue
+++ packages/ui/src/Subheading.vue
@@ -8,7 +8,7 @@ const props = defineProps({
 });
 
 const classes = cva({
-    base: 'text-gray-600 text-pretty tracking-tight dark:text-gray-400 [&_code]:text-xs [&_code]:bg-gray-600/10 [&_code]:rounded-sm [&_code]:px-1 [&_code]:py-0.5',
+    base: 'flex items-center gap-2 text-gray-600 text-pretty tracking-tight dark:text-gray-400 [&_code]:text-xs [&_code]:bg-gray-600/10 [&_code]:rounded-sm [&_code]:px-1 [&_code]:py-0.5',
     variants: {
         size: {
             sm: 'text-xs',
