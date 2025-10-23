c3445e335f69c07745a74b09f35a84c823c46840
Jay George <contact@jaygeorge.co.uk>
2025-10-23 16:08:55 +0100

    Remove hover support query and persist with the sidebar menu

--- resources/js/components/global-header/Logo.vue
+++ resources/js/components/global-header/Logo.vue
@@ -28,11 +28,11 @@ function toggleNav() {
     </template>
     <template v-else>
         <div class="flex items-center gap-1.5 sm:gap-2 relative">
-            <button class="flex items-center group rounded-lg cursor-pointer" type="button" @click="toggleNav" aria-label="{{ __('Toggle Nav') }}" style="--focus-outline-offset: 0.2rem;">
-                <div class="p-1 mr-1 sm:mr-2 size-7 flex items-center justify-center [@media(hover:hover)]:lg:opacity-0 [@media(hover:hover)]:lg:group-hover:opacity-100 [@media(hover:hover)]:lg:absolute lg:inset-0">
-                    <Icon name="burger-menu" class="size-5 max-lg:opacity-60 [@media(hover:none)]:lg:opacity-60 max-lg:hover:opacity-100" />
+            <button class="flex items-center group rounded-lg cursor-pointer" type="button" @click="toggleNav" :aria-label="__('Toggle Nav')" style="--focus-outline-offset: 0.2rem;">
+                <div class="p-1 max-sm:ps-2 mr-2 size-5 flex items-center justify-center lg:inset-0">
+                    <Icon name="burger-menu" class="size-3.5! sm:size-3.25! opacity-75 hover:opacity-100" />
                 </div>
-                <StatamicLogo class="size-7 [@media(hover:hover)]:lg:group-hover:opacity-0 transition-opacity duration-150" />
+                <StatamicLogo class="size-7" />
             </button>
             <Link :href="cp_url('/')" class="max-[350px]:hidden text-white/85 rounded-xs whitespace-nowrap" style="--focus-outline-offset: var(--outline-offset-button);">
                 {{ logos.text ?? logos.siteName }}
