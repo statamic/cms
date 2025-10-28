557f58882a55a4a378ddeac1e64aaa0b52465551
Jason Varga <jason@pixelfear.com>
2025-10-27 22:35:41 -0400

    resolve from real dictionaries. not really performant but it doesn't really matter here.

--- src/View/Scaffolding/Fieldtypes/Variables/DictionaryVariables.php
+++ src/View/Scaffolding/Fieldtypes/Variables/DictionaryVariables.php
@@ -2,24 +2,20 @@
 
 namespace Statamic\View\Scaffolding\Fieldtypes\Variables;
 
+use Statamic\Facades\Dictionary;
+
 class DictionaryVariables
 {
-    private static array $defaults = ['label', 'value'];
-
-    protected array $dictionaryVariables = [
-        'countries' => ['name', 'iso3', 'iso2', 'region', 'subregion', 'emoji'],
-        'currencies' => ['code', 'name', 'symbol', 'decimals'],
-        'languages' => ['code', 'name'],
-        'locales' => ['name'],
-        'timezones' => ['name', 'offset'],
-    ];
-
     public function resolve(string $dictionaryType): array
     {
-        return collect($this->dictionaryVariables[$dictionaryType] ?? [])
-            ->merge(self::$defaults)
-            ->unique()
-            ->values()
+        $dictionary = Dictionary::find($dictionaryType);
+
+        $firstOption = collect($dictionary->options())->keys()->first();
+
+        return collect($dictionary->get($firstOption)->toArray())
+            ->except(['key', 'value'])
+            ->keys()
+            ->push('value')
             ->all();
     }
 }
