9c70e878a8a4f6aa2b6a2bbdc8e0684d502ac726
Jay George <contact@jaygeorge.co.uk>
2025-10-30 11:13:55 +0000

    Step 3 - Add a ui-accent-text for dark mode, which needs to be lighter, since text is now against a dark background

--- src/CP/Color.php
+++ src/CP/Color.php
@@ -393,7 +393,8 @@ class Color
             'progress-bar' => self::Indigo[700],
             'ui-accent-bg' => self::Indigo[700],
             'ui-accent-text' => 'var(--theme-color-ui-accent-bg)',
-            'dark-ui-accent-bg' => self::Indigo[400],
+            'dark-ui-accent-bg' => self::Indigo[700],
+            'dark-ui-accent-text' => self::Indigo[400],
             'switch-bg' => 'var(--theme-color-ui-accent-bg)',
             'dark-switch-bg' => 'var(--theme-color-dark-ui-accent-bg)',
         ];
