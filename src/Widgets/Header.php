75a40ded082bc507871669e4660c4ad833a58bc9
Duncan McClean <duncan@duncanmcclean.com>
2025-11-04 12:13:43 +0000

    Convert header widget

--- src/Widgets/Header.php
+++ src/Widgets/Header.php
@@ -4,16 +4,13 @@ namespace Statamic\Widgets;
 
 class Header extends Widget
 {
-    /**
-     * The HTML that should be shown in the widget.
-     *
-     * @return \Illuminate\View\View
-     */
-    public function html()
+    public function component()
     {
         $classes = $this->config('classes', 'w-full');
         $text = $this->config('text');
 
-        return view('statamic::widgets.header', compact('classes', 'text'));
+        return VueComponent::render('dynamic-html-renderer', [
+            'html' => "<h2 class=\"{$classes}\">{$text}</h2>",
+        ]);
     }
 }
