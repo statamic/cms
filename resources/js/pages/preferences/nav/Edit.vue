6640cb38c39dab0c9ff6fe4f1d759dc14b6c295d
Duncan McClean <duncan@duncanmcclean.com>
2025-11-04 11:53:47 +0000

    Convert nav preferences edit

---
+++ resources/js/pages/preferences/nav/Edit.vue
@@ -0,0 +1,26 @@
+<script setup>
+import NavBuilder from '@/components/nav/Builder.vue';
+import Head from '@/pages/layout/Head.vue';
+
+defineProps([
+    'title',
+    'indexUrl',
+    'updateUrl',
+    'destroyUrl',
+    'saveAsOptions',
+    'nav',
+]);
+</script>
+
+<template>
+    <Head :title />
+
+    <nav-builder
+        :title="title"
+        :index-url="indexUrl"
+        :update-url="updateUrl"
+        :destroy-url="destroyUrl"
+        :save-as-options="saveAsOptions"
+        :nav="nav"
+    />
+</template>
