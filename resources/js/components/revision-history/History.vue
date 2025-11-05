e219059fb442035a3145f3ca3c2d5ecb1771c315
Jay George <github@jaygeorge.co.uk>
2025-11-05 15:25:41 +0000

    [6.x] Revision history tweaks (#12955)

--- resources/js/components/revision-history/History.vue
+++ resources/js/components/revision-history/History.vue
@@ -19,14 +19,15 @@
             <div v-for="group in revisions" :key="group.day">
                 <Heading size="sm" class="p-3 text-gray-600 dark:text-gray-300" v-text="formatRelativeDate(group.day)" />
                 <div class="relative grid gap-3">
-                    <div class="absolute inset-y-0 left-6 border-l-1 border-gray-400 dark:border-gray-600 border-dashed" />
+                    <div class="absolute inset-y-0 left-6 top-3 border-l-1 border-gray-400 dark:border-gray-600 border-dashed" />
                     <revision
-                        v-for="revision in group.revisions"
+                        v-for="(revision, index) in group.revisions"
                         :key="revision.date"
                         :revision="revision"
                         :restore-url="restoreUrl"
                         :reference="reference"
                         :can-restore-revisions="canRestoreRevisions"
+                        :is-last="index === group.revisions.length - 1"
                         @working-copy-selected="close"
                     />
                 </div>
