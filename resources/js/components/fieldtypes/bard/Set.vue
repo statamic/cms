4d862a5748798aefd7a39785f7049830f8af6a04
Duncan McClean <duncan@duncanmcclean.com>
2025-11-06 18:29:13 +0000

    [6.x] Various Bard fixes (#12963)

--- resources/js/components/fieldtypes/bard/Set.vue
+++ resources/js/components/fieldtypes/bard/Set.vue
@@ -24,7 +24,7 @@
             >
                 <Icon data-drag-handle name="handles" class="size-4 cursor-grab text-gray-400" v-if="!isReadOnly" />
                 <button type="button" class="flex flex-1 items-center gap-4 p-2 min-w-0 cursor-pointer [&:focus-visible]:outline-none [&:focus-visible]:[&_[data-ui-badge]]:focus-outline" @click="toggleCollapsedState">
-                    <Badge size="lg" pill="true" color="white" class="px-3">
+                    <Badge size="lg" :pill="true" color="white" class="px-3">
                         <span v-if="isSetGroupVisible" class="flex items-center gap-2">
                             {{ __(setGroup.display) }}
                             <Icon name="chevron-right" class="relative top-px size-3" />
@@ -114,6 +114,7 @@ import {
     PublishFields as Fields
 } from '@ui';
 import { containerContextKey } from '@/components/ui/Publish/Container.vue';
+import { watch } from 'vue';
 
 export default {
     props: nodeViewProps,
@@ -318,6 +319,16 @@ export default {
                 this.getPos() + this.node.nodeSize,
             );
         },
+    },
+
+    mounted() {
+        watch(
+            () => data_get(this.publishContainer.values.value, this.fieldPathPrefix),
+            (values) => {
+                this.updateAttributes({ values });
+            },
+            { deep: true }
+        );
     },
 
     updated() {
