b1696a481cdc6477120464a7f678989725251e8e
Jason Varga <jason@pixelfear.com>
2025-11-06 14:22:07 -0500

    [6.x] Show hidden fieldtype in form submission (#12970)

--- resources/js/components/fieldtypes/HiddenFieldtype.vue
+++ resources/js/components/fieldtypes/HiddenFieldtype.vue
@@ -1,11 +1,26 @@
-<template>
-    <input type="hidden" :name="name" :value="value" />
-</template>
+<script setup>
+import Fieldtype from '@/components/fieldtypes/fieldtype.js';
+import { Input } from '@ui';
 
-<script>
-import Fieldtype from './Fieldtype.vue';
+const emit = defineEmits(Fieldtype.emits);
+const props = defineProps(Fieldtype.props);
+const {
+    name,
+    isReadOnly,
+    updateDebounced,
+    expose
+} = Fieldtype.use(emit, props);
 
-export default {
-    mixins: [Fieldtype],
-};
+defineExpose(expose);
 </script>
+
+<template>
+    <Input
+        :name="name"
+        :id="id"
+        :model-value="value"
+        :read-only="isReadOnly"
+        :direction="config.direction"
+        @update:model-value="updateDebounced"
+    />
+</template>
