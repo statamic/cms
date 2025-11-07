90161b72425fefd054214b042c5dff025d5969e9
Philipp Daun <post@philippdaun.net>
2025-11-07 09:39:15 +0100

    Add header slot to widget component
    Signed-off-by: Philipp Daun <post@philippdaun.net>

--- resources/js/components/ui/Widget.vue
+++ resources/js/components/ui/Widget.vue
@@ -12,15 +12,17 @@ defineProps({
 <template>
     <Card inset class="@container/widget min-h-54 starting-style-transition" v-cloak>
         <div class="flex h-full min-h-54 flex-col starting-style-transition">
-            <header class="flex items-center min-h-[49px] justify-between border-b border-gray-200 px-4.5 py-2 dark:border-gray-700">
-                <component :is="href ? Link : 'div'" class="flex items-center gap-2 sm:gap-3" :href>
-                    <Icon :name="icon" class="hidden! size-5 text-gray-500 @xs/widget:block!" />
-                    <span v-text="title" />
-                </component>
-                <div class="flex items-center gap-4 -mr-2.5">
-                    <slot name="actions" />
-                </div>
-            </header>
+            <slot name="header">
+                <header class="flex items-center min-h-[49px] justify-between border-b border-gray-200 px-4.5 py-2 dark:border-gray-700">
+                    <component :is="href ? Link : 'div'" class="flex items-center gap-2 sm:gap-3" :href>
+                        <Icon :name="icon" class="hidden! size-5 text-gray-500 @xs/widget:block!" />
+                        <span v-text="title" />
+                    </component>
+                    <div class="flex items-center gap-4 -mr-2.5">
+                        <slot name="actions" />
+                    </div>
+                </header>
+            </slot>
             <slot />
             <slot name="footer" />
         </div>
