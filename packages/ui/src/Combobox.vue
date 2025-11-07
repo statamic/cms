6477d5eed636354f7e8fd9e5cc8d8839a51ea6a9
Jay George <contact@jaygeorge.co.uk>
2025-11-07 15:29:02 +0000

    Fix combobox z-index for the live preview device dropdown

--- packages/ui/src/Combobox.vue
+++ packages/ui/src/Combobox.vue
@@ -436,7 +436,7 @@ defineExpose({
 
 <style scoped>
     /* We can't use a direct descendant selector because the stack is inside a portal, so instead we'll check to see if there is a stack present. */
-    body:has(.stack) [data-reka-popper-content-wrapper] {
+    body:has(.stack, .live-preview) [data-reka-popper-content-wrapper] {
         z-index: var(--z-index-portal)!important;
     }
 
