64eeaddc2169c31d32a5984f96ba9555492f5809
Jack McDade <jack@jackmcdade.com>
2025-11-10 15:53:24 -0500

    Don't even need a slot. Also account for actions.

--- resources/js/components/ui/Widget.vue
+++ resources/js/components/ui/Widget.vue
@@ -10,24 +10,22 @@ const props = defineProps({
 });
 
 const slots = useSlots();
-const hasHeader = computed(() => Boolean(props.title || props.icon || slots.header));
+const hasHeader = computed(() => Boolean(props.title || props.icon || slots.header || slots.actions));
 
 </script>
 
 <template>
     <Card inset class="@container/widget min-h-54 starting-style-transition" v-cloak>
         <div class="flex h-full min-h-54 flex-col starting-style-transition">
-            <slot name="header" v-if="hasHeader">
-                <header class="flex items-center min-h-[49px] justify-between border-b border-gray-200 px-4.5 py-2 dark:border-gray-700">
-                    <component :is="href ? Link : 'div'" class="flex items-center gap-2 sm:gap-3" :href>
-                        <Icon v-if="icon" :name="icon" class="hidden! size-5 text-gray-500 @xs/widget:block!" />
-                        <span v-text="title" />
-                    </component>
-                    <div class="flex items-center gap-4 -mr-2.5">
-                        <slot name="actions" />
-                    </div>
-                </header>
-            </slot>
+            <header v-if="hasHeader" class="flex items-center min-h-[49px] justify-between border-b border-gray-200 px-4.5 py-2 dark:border-gray-700">
+                <component :is="href ? Link : 'div'" class="flex items-center gap-2 sm:gap-3" :href>
+                    <Icon v-if="icon" :name="icon" class="hidden! size-5 text-gray-500 @xs/widget:block!" />
+                    <span v-text="title" />
+                </component>
+                <div class="flex items-center gap-4 -mr-2.5">
+                    <slot name="actions" />
+                </div>
+            </header>
             <slot />
             <slot name="footer" />
         </div>
