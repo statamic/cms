a4ac4f94441662d44f2a2166d079bd249deec258
Andreas Schantl <andreas@schantl.io>
2025-11-11 16:27:22 +0100

    WIP

--- resources/js/pages/layout/Layout.vue
+++ resources/js/pages/layout/Layout.vue
@@ -29,7 +29,7 @@ provide('layout', {
         <LicensingAlert />
         <Header />
 
-        <main id="main" class="flex bg-body-bg dark:bg-dark-body-bg dark:border-t rounded-t-2xl dark:border-dark-body-border fixed top-14 inset-x-0 bottom-0 min-h-[calc(100vh-3.5rem)]">
+        <main id="main" class="flex bg-body-bg dark:border-t dark:border-body-border rounded-t-2xl fixed top-14 inset-x-0 bottom-0 min-h-[calc(100vh-3.5rem)]">
             <Nav />
             <div id="main-content" class="main-content sm:p-2 h-full flex-1 overflow-y-auto rounded-t-2xl">
                 <div id="content-card" class="relative content-card min-h-full">
