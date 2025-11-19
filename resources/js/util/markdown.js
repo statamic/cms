16003e145c25ff80c9e06c63648729f95417253a
Jason Varga <jason@pixelfear.com>
2025-11-19 15:09:25 -0500

    dry

--- resources/js/util/markdown.js
+++ resources/js/util/markdown.js
@@ -1,17 +1,1 @@
-import { marked } from 'marked';
-
-export default function (markdown, options = {}) {
-    if (!markdown) return '';
-
-    const renderer = new marked.Renderer();
-
-    if (options.openLinksInNewTabs) {
-        renderer.link = function(href, title, text) {
-            return marked.Renderer.prototype.link
-                .call(this, href, title, text)
-                .replace("<a", "<a target='_blank' ");
-        };
-    }
-
-    return marked.parse(markdown, { renderer });
-}
+export { default } from '@statamic/ui/util/markdown';
