9c2d4a912dc5b9e1a198a8d2b61e6c6d6d00454b
Jason Varga <jason@pixelfear.com>
2025-11-21 10:49:10 -0500

    gradient in the js object was breaking the controls tab

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,12 +1,12 @@
 import { addons } from 'storybook/manager-api';
 import { create } from 'storybook/theming';
+import './manager.css';
 
 const theme = create({
   base: 'light',
   brandTitle: 'Statamic UI Components',
   brandUrl: 'https://statamic.dev',
   brandImage: '/logo.png',
-  appBg: 'linear-gradient(225deg, #e6f8ff, #f9e6ff, hsl(35deg 100% 95%))',
   fontBase: '"Lexend", sans-serif',
   fontCode: '"Source Code Pro", monospace'
 });
