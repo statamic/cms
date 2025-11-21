ac7287d7dac28088cad1e421b71cebe7407aa7a3
Jason Varga <jason@pixelfear.com>
2025-11-21 15:18:19 -0500

    nitpick

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,19 @@
 import type { StorybookConfig } from '@storybook/vue3-vite';
 
 const config: StorybookConfig = {
-  stories: [
-    "../resources/js/stories/**/*.mdx",
-    "../resources/js/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  addons: [
-    "@storybook/addon-docs",
-    "@storybook/addon-a11y"
-  ],
-  staticDirs: ['./public'],
-  framework: {
-    name: "@storybook/vue3-vite",
-    options: {}
-  },
+    stories: [
+        '../resources/js/stories/**/*.mdx',
+        '../resources/js/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'
+    ],
+    addons: [
+        '@storybook/addon-docs',
+        '@storybook/addon-a11y'
+    ],
+    staticDirs: ['./public'],
+    framework: {
+        name: '@storybook/vue3-vite',
+        options: {}
+    },
 };
 
 export default config;
