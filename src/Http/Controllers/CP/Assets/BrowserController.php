dff054bebd09bbd6c0c4afafe3c4683c66112acb
Philipp Daun <post@philippdaun.net>
2025-11-30 15:59:53 +0100

    Add width and height columns to asset browser
    Signed-off-by: Philipp Daun <post@philippdaun.net>

--- src/Http/Controllers/CP/Assets/BrowserController.php
+++ src/Http/Controllers/CP/Assets/BrowserController.php
@@ -231,9 +231,25 @@ class BrowserController extends CpController
             ->defaultVisibility(true)
             ->sortable(true);
 
+        $width = Column::make('width')
+            ->label(__('Width'))
+            ->value('width')
+            ->visible(true)
+            ->defaultVisibility(false)
+            ->sortable(true);
+
+        $height = Column::make('height')
+            ->label(__('Height'))
+            ->value('height')
+            ->visible(true)
+            ->defaultVisibility(false)
+            ->sortable(true);
+
         $columns->put('basename', $basename);
         $columns->put('size', $size);
         $columns->put('last_modified', $lastModified);
+        $columns->put('width', $width);
+        $columns->put('height', $height);
 
         $columns->setPreferred("assets.{$container->handle()}.columns");
 
