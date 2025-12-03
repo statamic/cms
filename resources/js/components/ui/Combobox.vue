1e7ab2ac4f7a2bd1dace3696d8e3b5e6a4561118
Jay George <github@jaygeorge.co.uk>
2025-12-03 20:02:21 +0000

    [6.x] Optically align quick actions (#13236)

--- resources/js/components/ui/Combobox.vue
+++ resources/js/components/ui/Combobox.vue
@@ -387,7 +387,7 @@ defineExpose({
                             </button>
                         </div>
 
-                        <div v-if="(clearable && modelValue) || (options.length || ignoreFilter)" class="flex gap-1.5 items-center ms-1.5 -me-2">
+                        <div v-if="(clearable && modelValue) || (options.length || ignoreFilter)" class="flex gap-1.5 items-center ms-1.5 -me-1">
                             <Button v-if="clearable && modelValue" icon="x" variant="ghost" size="xs" round @click="clear" data-ui-combobox-clear-button />
                             <Icon v-if="options.length || ignoreFilter" name="chevron-down" class="text-gray-400 dark:text-white/40 size-4" data-ui-combobox-chevron />
                         </div>
