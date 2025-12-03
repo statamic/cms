b44476c36b82b7cd2f22928f68abd9c8a3b35261
Marty Friedel <marty.dev@mity.com.au>
2025-12-04 08:26:05 +1030

    Toggle visibility of fullscreen quick action

--- resources/js/components/fieldtypes/replicator/Replicator.vue
+++ resources/js/components/fieldtypes/replicator/Replicator.vue
@@ -172,6 +172,7 @@ export default {
                     title: __('Toggle Fullscreen Mode'),
                     icon: ({ vm }) => (vm.fullScreenMode ? 'fullscreen-close' : 'fullscreen-open'),
                     quick: true,
+                    visible: this.config.fullscreen,
                     visibleWhenReadOnly: true,
                     run: this.toggleFullscreen,
                 },
