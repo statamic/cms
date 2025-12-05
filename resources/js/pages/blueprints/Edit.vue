statamic/cms

commit 4ea74652e4f116abc712dba50c7dd5c03453d0a0

[6.x] Only show relevent fieldtypes when editing form blueprint (#13248)
* Only show relevent fieldtypes when editing form blueprint

* simplify

---------

Co-authored-by: Jason Varga <jason@pixelfear.com>

resources/js/pages/blueprints/Edit.vue
@@ -2,15 +2,16 @@
 import Head from '@/pages/layout/Head.vue';
 import { DocsCallout } from '@ui';
 
-defineProps([
-    'blueprint',
-    'action',
-    'showTitle',
-    'useTabs',
-    'canDefineLocalizable',
-    'resetRoute',
-    'isResettable',
-]);
+defineProps({
+	blueprint: Object,
+	action: String,
+	showTitle: Boolean,
+	useTabs: Boolean,
+	canDefineLocalizable: Boolean,
+	resetRoute: String,
+	isResettable: Boolean,
+	isFormBlueprint: Boolean,
+});
 </script>
 
 <template>
@@ -23,6 +24,7 @@ defineProps([
             :initial-blueprint="blueprint"
             :use-tabs="useTabs"
             :can-define-localizable="canDefineLocalizable"
+            :is-form-blueprint="isFormBlueprint"
         >
             <template v-if="isResettable" #actions>
                 <ui-dropdown>

src/Http/Controllers/CP/Forms/FormBlueprintController.php
@@ -44,6 +44,7 @@ class FormBlueprintController extends CpController
         return $this->renderEditPage([
             'blueprint' => $this->toVueObject($blueprint),
             'action' => cp_route('blueprints.forms.update', $form->handle()),
+            'isFormBlueprint' => true,
         ]);
     }
 

commit d16b1b3019f841758b9d1e1ce1158c0606a73868

Fix incorrect tab default value introduced in #13248

resources/js/pages/blueprints/Edit.vue
@@ -6,7 +6,7 @@ defineProps({
 	blueprint: Object,
 	action: String,
 	showTitle: Boolean,
-	useTabs: Boolean,
+	useTabs: { type: Boolean, default: true },
 	canDefineLocalizable: Boolean,
 	resetRoute: String,
 	isResettable: Boolean,