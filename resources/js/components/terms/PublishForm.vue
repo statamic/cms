4ef97e296a932f41a1a354fa2b3dc2ed6a51a3ee
Duncan McClean <duncan@duncanmcclean.com>
2025-12-08 16:13:33 +0000

    Ensure saveKeyBinding is "unbound" when moving pages

--- resources/js/components/terms/PublishForm.vue
+++ resources/js/components/terms/PublishForm.vue
@@ -520,7 +520,7 @@ export default {
         window.history.replaceState({}, document.title, document.location.href.replace('created=true', ''));
     },
 
-    unmounted() {
+	beforeUnmount() {
         this.saveKeyBinding.destroy();
         this.quickSaveKeyBinding.destroy();
     },
