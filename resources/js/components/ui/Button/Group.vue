6537e7597a6bde2f3a616ee3c47bcce30338ce5d
Philipp Daun <post@philippdaun.net>
2025-12-11 23:50:01 +0100

    Add auto orientation option to button groups
    Signed-off-by: Philipp Daun <post@philippdaun.net>

--- resources/js/components/ui/Button/Group.vue
+++ resources/js/components/ui/Button/Group.vue
@@ -1,7 +1,7 @@
 <template>
     <div
         :class="[
-            'group/button flex flex-wrap [[data-floating-toolbar]_&]:justify-center [[data-floating-toolbar]_&]:gap-1 [[data-floating-toolbar]_&]:lg:gap-x-0',
+            'group/button flex flex-wrap relative [[data-floating-toolbar]_&]:justify-center [[data-floating-toolbar]_&]:gap-1 [[data-floating-toolbar]_&]:lg:gap-x-0',
             '[&>[data-ui-group-target]:not(:first-child):not(:last-child)]:rounded-none',
             '[&>[data-ui-group-target]:first-child:not(:last-child)]:rounded-e-none',
             '[&>[data-ui-group-target]:last-child:not(:first-child)]:rounded-s-none',
@@ -10,13 +10,63 @@
             '[&>*:last-child:not(:first-child)_[data-ui-group-target]]:rounded-s-none',
             'dark:[&_button]:ring-0',
             'max-lg:[[data-floating-toolbar]_&_button]:rounded-md!',
-            'shadow-ui-sm rounded-lg'
+            'shadow-ui-sm rounded-lg',
         ]"
         data-ui-button-group
     >
         <slot />
     </div>
 </template>
+
+<script>
+export default {
+    props: {
+        orientation: {
+            type: String,
+            default: 'horizontal',
+        },
+    },
+
+    data() {
+        return {
+            resizeObserver: null,
+        };
+    },
+
+    mounted() {
+        this.$el.dataset.orientation = this.orientation;
+        if (this.orientation === 'auto') {
+            this.observeOrientation();
+        }
+    },
+
+    beforeUnmount() {
+        this.unobserveOrientation();
+    },
+
+    methods: {
+        observeOrientation() {
+            this.resizeObserver = new ResizeObserver(() => this.checkOrientation(this.$el));
+            this.resizeObserver.observe(this.$el);
+        },
+
+        unobserveOrientation() {
+            this.resizeObserver?.disconnect();
+        },
+
+        checkOrientation(node) {
+            delete node.dataset.orientation;
+
+            const child = node.lastElementChild;
+            if (!child) return;
+
+            node.dataset.orientation = child.offsetTop > node.clientTop
+                ? 'vertical'
+                : 'horizontal';
+        },
+    },
+};
+</script>
 
 <style>
     [data-ui-button-group] [data-ui-group-target] {
@@ -35,5 +85,9 @@
                 }
             }
         }
+    }
+
+    [data-ui-button-group][data-orientation='vertical'] {
+        @apply flex! flex-col;
     }
 </style>
