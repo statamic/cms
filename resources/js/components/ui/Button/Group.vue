293d6a695d76588358f940c2f81a135774c38240
Philipp Daun <post@philippdaun.net>
2025-12-12 12:41:16 +0100

    Add wrapper to allow observing resize
    Signed-off-by: Philipp Daun <post@philippdaun.net>

--- resources/js/components/ui/Button/Group.vue
+++ resources/js/components/ui/Button/Group.vue
@@ -1,37 +1,44 @@
 <template>
-    <div
-        ref="el"
-        :class="[
-            'group/button flex flex-wrap relative [[data-floating-toolbar]_&]:justify-center [[data-floating-toolbar]_&]:gap-1 [[data-floating-toolbar]_&]:lg:gap-x-0',
-            'dark:[&_button]:ring-0',
-            'max-lg:[[data-floating-toolbar]_&_button]:rounded-md!',
-            'shadow-ui-sm rounded-lg',
+    <div ref="wrapper">
+        <div
+            ref="group"
+            :class="[
+                'group/button flex flex-wrap relative [[data-floating-toolbar]_&]:justify-center [[data-floating-toolbar]_&]:gap-1 [[data-floating-toolbar]_&]:lg:gap-x-0',
+                'dark:[&_button]:ring-0',
+                'max-lg:[[data-floating-toolbar]_&_button]:rounded-md!',
+                'shadow-ui-sm rounded-lg',
 
-            // Horizontal orientation
-            '[&[data-orientation=horizontal]>[data-ui-group-target]:not(:first-child):not(:last-child)]:rounded-none',
-            '[&[data-orientation=horizontal]>[data-ui-group-target]:first-child:not(:last-child)]:rounded-e-none',
-            '[&[data-orientation=horizontal]>[data-ui-group-target]:last-child:not(:first-child)]:rounded-s-none',
-            '[&[data-orientation=horizontal]>*:not(:first-child):not(:last-child):not(:only-child)_[data-ui-group-target]]:rounded-none',
-            '[&[data-orientation=horizontal]>*:first-child:not(:last-child)_[data-ui-group-target]]:rounded-e-none',
-            '[&[data-orientation=horizontal]>*:last-child:not(:first-child)_[data-ui-group-target]]:rounded-s-none',
+                // Orientation determined: limit width to ensure shadow hugs content
+                '[&[data-orientation]]:inline-flex',
 
-            // Vertical orientation
-            '[&[data-orientation=vertical]]:flex-col',
-            '[&[data-orientation=vertical]>[data-ui-group-target]:not(:first-child):not(:last-child)]:rounded-none',
-            '[&[data-orientation=vertical]>[data-ui-group-target]:first-child:not(:last-child)]:rounded-b-none',
-            '[&[data-orientation=vertical]>[data-ui-group-target]:last-child:not(:first-child)]:rounded-t-none',
-            '[&[data-orientation=vertical]>*:not(:first-child):not(:last-child):not(:only-child)_[data-ui-group-target]]:rounded-none',
-            '[&[data-orientation=vertical]>*:first-child:not(:last-child)_[data-ui-group-target]]:rounded-b-none',
-            '[&[data-orientation=vertical]>*:last-child:not(:first-child)_[data-ui-group-target]]:rounded-t-none',
-        ]"
-        data-ui-button-group
-    >
-        <slot />
+                // Horizontal orientation
+                '[&[data-orientation=horizontal]>[data-ui-group-target]:not(:first-child):not(:last-child)]:rounded-none',
+                '[&[data-orientation=horizontal]>[data-ui-group-target]:first-child:not(:last-child)]:rounded-e-none',
+                '[&[data-orientation=horizontal]>[data-ui-group-target]:last-child:not(:first-child)]:rounded-s-none',
+                '[&[data-orientation=horizontal]>*:not(:first-child):not(:last-child):not(:only-child)_[data-ui-group-target]]:rounded-none',
+                '[&[data-orientation=horizontal]>*:first-child:not(:last-child)_[data-ui-group-target]]:rounded-e-none',
+                '[&[data-orientation=horizontal]>*:last-child:not(:first-child)_[data-ui-group-target]]:rounded-s-none',
+
+                // Vertical orientation
+                '[&[data-orientation=vertical]]:flex-col',
+                '[&[data-orientation=vertical]>[data-ui-group-target]:not(:first-child):not(:last-child)]:rounded-none',
+                '[&[data-orientation=vertical]>[data-ui-group-target]:first-child:not(:last-child)]:rounded-b-none',
+                '[&[data-orientation=vertical]>[data-ui-group-target]:last-child:not(:first-child)]:rounded-t-none',
+                '[&[data-orientation=vertical]>*:not(:first-child):not(:last-child):not(:only-child)_[data-ui-group-target]]:rounded-none',
+                '[&[data-orientation=vertical]>*:first-child:not(:last-child)_[data-ui-group-target]]:rounded-b-none',
+                '[&[data-orientation=vertical]>*:last-child:not(:first-child)_[data-ui-group-target]]:rounded-t-none',
+            ]"
+            data-ui-button-group
+        >
+            <slot />
+        </div>
     </div>
 </template>
 
 <script setup>
 import { ref, nextTick, onMounted, onBeforeUnmount } from 'vue';
+
+import debounce from '@/util/debounce';
 
 const props = defineProps({
     orientation: {
@@ -40,42 +47,46 @@ const props = defineProps({
     },
 });
 
-const el = ref(null);
+const wrapper = ref(null);
+const group = ref(null);
 let resizeObserver = null;
 
 function observeOrientation() {
-    resizeObserver = new ResizeObserver(() => checkOrientation());
-    resizeObserver.observe(el.value);
+    resizeObserver = new ResizeObserver(() => checkOrientationDebounced());
+    resizeObserver.observe(wrapper.value);
 }
 
 function unobserveOrientation() {
     resizeObserver?.disconnect();
 }
 
-async function checkOrientation() {
-    const node = el.value;
-    if (!node) return;
-
+function checkOrientation() {
     // 1) Remove orientation to allow natural layout (grow + wrap)
-    delete node.dataset.orientation;
+    setOrientation(null);
 
-    // 2) Wait for Vue to commit DOM updates
-    await nextTick();
+    // 2) Force reflow and measure
+    const child = group.value.lastElementChild;
+    const vertical = child && child.offsetTop > group.value.clientTop;
 
-    // 3) Force reflow and measure
-    const child = node.lastElementChild;
-    const orientation = child && child.offsetTop > node.clientTop
-        ? 'vertical'
-        : 'horizontal';
+    // 3) Apply the final orientation
+    setOrientation(vertical ? 'vertical' : 'horizontal');
+}
 
-    // 4) Apply the final orientation
-    node.dataset.orientation = orientation;
+const checkOrientationDebounced = debounce(checkOrientation, 50);
+
+function setOrientation(value) {
+    if (value) {
+        group.value.dataset.orientation = value;
+    } else {
+        delete group.value.dataset.orientation;
+    }
 }
 
 onMounted(() => {
-    el.value.dataset.orientation = props.orientation;
     if (props.orientation === 'auto') {
         observeOrientation();
+    } else {
+        setOrientation(props.orientation);
     }
 });
 
