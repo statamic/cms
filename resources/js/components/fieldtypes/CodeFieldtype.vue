11a9c4802b293fc834757898e235d8218eba207d
Jay George <contact@jaygeorge.co.uk>
2025-12-15 15:32:53 +0000

    Code fieldtype - make the system option a simpler implementation

--- resources/js/components/fieldtypes/CodeFieldtype.vue
+++ resources/js/components/fieldtypes/CodeFieldtype.vue
@@ -33,10 +33,7 @@ export default {
     data() {
         return {
             escBinding: null,
-            systemColorMode: null,
-            colorModeWatcher: null,
-            mediaQueryListener: null,
-            mutationObserver: null,
+            isDark: document.documentElement.classList.contains('dark'),
         };
     },
 
@@ -49,7 +46,7 @@ export default {
             const colorMode = this.config.color_mode || 'material';
             
             if (colorMode === 'system') {
-                return this.systemColorMode || this.getSystemColorMode();
+                return this.isDark ? 'material' : 'light';
             }
             
             return colorMode;
@@ -74,105 +71,27 @@ export default {
         },
     },
 
-    watch: {
-        'config.color_mode'() {
-            if (this.config.color_mode === 'system') {
-                this.updateSystemColorMode();
-            }
-        },
-    },
-
     mounted() {
-        this.updateSystemColorMode();
-        this.watchSystemColorMode();
+        // Watch for dark class changes on document element (ColorMode already manages this)
+        this.mutationObserver = new MutationObserver(() => {
+            if (this.config.color_mode === 'system') {
+                this.isDark = document.documentElement.classList.contains('dark');
+            }
+        });
+
+        this.mutationObserver.observe(document.documentElement, {
+            attributes: true,
+            attributeFilter: ['class'],
+        });
     },
 
     beforeUnmount() {
-        this.cleanupWatchers();
+        if (this.mutationObserver) {
+            this.mutationObserver.disconnect();
+        }
     },
 
     methods: {
-        getSystemColorMode() {
-            const preference = Statamic.$colorMode?.preference?.value || 'auto';
-            
-            if (preference === 'dark') {
-                return 'material';
-            }
-            
-            if (preference === 'light') {
-                return 'light';
-            }
-            
-            // preference === 'auto' - check system preference
-            return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'material' : 'light';
-        },
-
-        updateSystemColorMode() {
-            this.systemColorMode = this.getSystemColorMode();
-        },
-
-        watchSystemColorMode() {
-            // Watch for changes in the system color mode preference
-            if (Statamic.$colorMode?.preference) {
-                this.colorModeWatcher = this.$watch(
-                    () => Statamic.$colorMode.preference.value,
-                    () => {
-                        if (this.config.color_mode === 'system') {
-                            this.updateSystemColorMode();
-                        }
-                    }
-                );
-            }
-
-            // Watch for system preference changes (when preference is 'auto')
-            const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-            const handleChange = () => {
-                if (this.config.color_mode === 'system') {
-                    this.updateSystemColorMode();
-                }
-            };
-            
-            if (mediaQuery.addEventListener) {
-                mediaQuery.addEventListener('change', handleChange);
-                this.mediaQueryListener = handleChange;
-            } else {
-                // Fallback for older browsers
-                mediaQuery.addListener(handleChange);
-                this.mediaQueryListener = handleChange;
-            }
-
-            // Watch for dark class changes on document element
-            this.mutationObserver = new MutationObserver(() => {
-                if (this.config.color_mode === 'system') {
-                    this.updateSystemColorMode();
-                }
-            });
-
-            this.mutationObserver.observe(document.documentElement, {
-                attributes: true,
-                attributeFilter: ['class'],
-            });
-        },
-
-        cleanupWatchers() {
-            if (this.colorModeWatcher) {
-                this.colorModeWatcher();
-            }
-
-            if (this.mediaQueryListener) {
-                const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-                if (mediaQuery.removeEventListener) {
-                    mediaQuery.removeEventListener('change', this.mediaQueryListener);
-                } else {
-                    mediaQuery.removeListener(this.mediaQueryListener);
-                }
-            }
-
-            if (this.mutationObserver) {
-                this.mutationObserver.disconnect();
-            }
-        },
-
         toggleFullscreen() {
             const wasFullscreen = this.$refs.codeEditor.fullScreenMode;
             this.$refs.codeEditor.toggleFullscreen();
