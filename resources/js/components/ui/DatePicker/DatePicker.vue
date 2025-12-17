30d35d15953604ad99e2be481011a80713683fef
Jay George <contact@jaygeorge.co.uk>
2025-12-17 15:10:54 +0000

    Make date and time field borders consistent with other fieldtypes

--- resources/js/components/ui/DatePicker/DatePicker.vue
+++ resources/js/components/ui/DatePicker/DatePicker.vue
@@ -135,7 +135,7 @@ const getInputLabel = (part) => {
                     <div
                         :class="[
                             'flex w-full items-center bg-white uppercase dark:bg-gray-900',
-                            'border border-gray-300 dark:border-x-0 dark:border-t-0 dark:border-white/10 dark:inset-shadow-2xs dark:inset-shadow-black',
+                            'border border-gray-300 dark:border-gray-700',
                             'text-gray-600 dark:text-gray-300',
                             'shadow-ui-sm not-prose h-10 rounded-lg px-2 disabled:shadow-none',
                             'data-invalid:border-red-500',
--- resources/js/components/ui/DateRangePicker/DateRangePicker.vue
+++ resources/js/components/ui/DateRangePicker/DateRangePicker.vue
@@ -103,7 +103,7 @@ const calendarEvents = computed(() => ({
                 <div
                     :class="[
                         'flex items-center w-full bg-white dark:bg-gray-900',
-                        'border border-gray-300 dark:border-x-0 dark:border-t-0 dark:border-white/10 dark:inset-shadow-2xs dark:inset-shadow-black',
+                        'border border-gray-300 dark:border-gray-700',
                         'leading-[1.375rem] text-gray-600 dark:text-gray-300',
                         'shadow-ui-sm not-prose h-10 rounded-lg py-2 px-2.5 disabled:shadow-none',
                         'data-invalid:border-red-500',
