96d8837595819413358bb7ea59bc1f68eda0765d
Jay George <contact@jaygeorge.co.uk>
2025-12-18 14:13:22 +0000

    Automatically close the datepicker when you select a date

--- resources/js/components/ui/DatePicker/DatePicker.vue
+++ resources/js/components/ui/DatePicker/DatePicker.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import {
     DatePickerAnchor,
     DatePickerContent,
@@ -26,6 +26,8 @@ import Calendar from '../Calendar/Calendar.vue';
 import Icon from '../Icon/Icon.vue';
 
 const emit = defineEmits(['update:modelValue']);
+
+const open = ref(false);
 
 const props = defineProps({
     date: { type: String, default: null },
@@ -82,6 +84,11 @@ const calendarEvents = computed(() => ({
             event.minute = 0;
             event.second = 0;
             event.millisecond = 0;
+            
+            // Close the popup when a date is selected
+            if (!props.inline) {
+                open.value = false;
+            }
         }
 
         emit('update:modelValue', event);
@@ -122,6 +129,8 @@ const getInputLabel = (part) => {
             :granularity="granularity"
             :locale="$date.locale"
             :disabled="disabled || readOnly"
+            :open="open"
+            @update:open="open = $event"
             @update:model-value="emit('update:modelValue', $event)"
             v-bind="$attrs"
             prevent-deselect
