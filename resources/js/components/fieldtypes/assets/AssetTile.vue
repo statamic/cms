d443c2cac429e39ad7ae5ff477fe4427afd6f58d
Jay George <contact@jaygeorge.co.uk>
2026-01-05 16:50:34 +0100

    Asset fieldtype / grid mode - fix transparency checkerboard not showing

--- resources/js/components/fieldtypes/assets/AssetTile.vue
+++ resources/js/components/fieldtypes/assets/AssetTile.vue
@@ -18,8 +18,8 @@
         >
         </asset-editor>
 
-        <div class="flex h-full border-b dark:border-gray-700 rounded-b-md relative">
-            <div class="p-1 flex flex-col items-center justify-center h-full" :class="{ 'bg-checkerboard rounded-md': canBeTransparent }">
+        <div class="flex h-full border-b dark:border-gray-700 rounded-b-md relative" :class="{ 'bg-checkerboard rounded-md': canBeTransparent }">
+            <div class="p-1 flex flex-col items-center justify-center h-full">
                 <!-- Solo Bard -->
                 <template v-if="isImage && isInBardField && !isInAssetBrowser">
                     <img :src="asset.url" />
