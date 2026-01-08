d3ddea37e549abb3d059dcc9156fe36b8684ec86
Jason Varga <jason@pixelfear.com>
2026-01-08 10:58:02 -0500

    move more

--- packages/cms/src/temporary.js
+++ packages/cms/src/temporary.js
@@ -1,14 +1,3 @@
 export const {
-    HasInputOptions,
-    InlineEditForm,
-    RelatedItem,
     ConfirmationModal,
-    RevisionHistory,
-    RestoreRevision,
-    RevisionPreview,
-    SaveButtonOptions,
-    SortableList,
-    HasPreferences,
-    HasActions,
-    resetValuesFromResponse,
 } = __STATAMIC__.temporary;
