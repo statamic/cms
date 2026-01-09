2d3cf0ff66c5f421fc4bf6fb8944c1d286fe7053
Duncan McClean <duncan@duncanmcclean.com>
2026-01-09 10:33:12 +0000

    Reduce spacing between top/bottom of dropdown & window edge

--- resources/js/components/ui/Combobox/Combobox.vue
+++ resources/js/components/ui/Combobox/Combobox.vue
@@ -442,7 +442,7 @@ defineExpose({
                             <div class="relative">
                                 <ComboboxViewport
                                     ref="viewport"
-                                    class="max-h-[calc(var(--reka-combobox-content-available-height)-5rem)] overflow-y-scroll"
+                                    class="max-h-[calc(var(--reka-combobox-content-available-height)-2rem)] overflow-y-scroll"
                                     data-ui-combobox-viewport
                                 >
                                     <ComboboxEmpty class="p-2 text-sm" data-ui-combobox-empty>
