33c1e52284bd179b7f29b819e3ab56832cb5b2f5
Duncan McClean <duncan@duncanmcclean.com>
2026-01-13 15:13:24 +0000

    [6.x] Bring back logo version tooltip (#13528)

--- resources/js/components/global-header/Logo.vue
+++ resources/js/components/global-header/Logo.vue
@@ -6,7 +6,7 @@ import ProBadge from './ProBadge.vue';
 import { Link } from '@inertiajs/vue3';
 import useStatamicPageProps from '@/composables/page-props.js';
 
-const { logos, isPro, cmsName } = useStatamicPageProps();
+const { logos, isPro, cmsName, version } = useStatamicPageProps();
 const customLogoImage = computed(() => {
     if (! logos) return null
     return logos.dark.nav ?? logos.light.nav;
@@ -26,9 +26,9 @@ function toggleNav() {
                 <div class="p-1 max-sm:ps-2 mr-2 size-5 flex items-center justify-center lg:inset-0">
                     <Icon name="burger-menu-no-border" class="size-3.5! sm:size-3.25! opacity-75 hover:opacity-100" />
                 </div>
-                <img v-if="customLogoImage" :src="customLogoImage" :alt="cmsName" class="w-full max-w-[260px] max-h-7">
+                <img v-if="customLogoImage" :src="customLogoImage" :alt="cmsName" class="w-full max-w-[260px] max-h-7" v-tooltip="version">
             </button>
-            <Link v-if="customLogoText && !customLogoImage" :href="cp_url('/')" class="mr-2 font-medium text-white whitespace-nowrap" style="--focus-outline-offset: var(--outline-offset-button);">
+            <Link v-if="customLogoText && !customLogoImage" :href="cp_url('/')" class="mr-2 font-medium text-white whitespace-nowrap" v-tooltip="version" style="--focus-outline-offset: var(--outline-offset-button);">
                 {{ customLogoText }}
             </Link>
         </div>
@@ -39,7 +39,7 @@ function toggleNav() {
                 <div class="p-1 max-sm:ps-2 mr-2 size-5 flex items-center justify-center lg:inset-0">
                     <Icon name="burger-menu-no-border" class="size-3.5! sm:size-3.25! opacity-75 hover:opacity-100" />
                 </div>
-                <StatamicLogo class="size-7" />
+                <StatamicLogo class="size-7" v-tooltip="version" />
             </button>
             <Link :href="cp_url('/')" class="max-[350px]:hidden text-white/85 rounded-xs whitespace-nowrap" style="--focus-outline-offset: var(--outline-offset-button);">
                 {{ logos.text ?? logos.siteName }}
