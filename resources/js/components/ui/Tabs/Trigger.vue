4466ff61492246dae904e2582ca8199ba113ee1b
Jay George <contact@jaygeorge.co.uk>
2026-01-13 13:51:07 +0000

    Effectively use Reka "tabs indicator" instead

--- resources/js/components/ui/Tabs/Trigger.vue
+++ resources/js/components/ui/Tabs/Trigger.vue
@@ -16,7 +16,7 @@ defineProps({
 <template>
     <TabsTrigger
         :value="name"
-        class="translate-y-px cursor-pointer px-2 py-1 focus-visible:rounded-lg hover:text-gray-600 border-b border-gray-200 dark:border-gray-800 data-[state=active]:border-gray-800 data-[state=active]:text-gray-900 dark:hover:text-gray-400 dark:data-[state=active]:border-gray-200 dark:data-[state=active]:text-gray-200"
+        class="translate-y-px cursor-pointer px-2 py-1 focus-visible:rounded-lg hover:text-gray-600 data-[state=active]:text-gray-900 dark:hover:text-gray-400 dark:data-[state=active]:text-gray-200"
     >
         <slot v-if="hasSlot" />
         <span v-else>{{ text }}</span>
