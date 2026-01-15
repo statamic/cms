2af3af91409e8366d931bedbdea9d85c03b52857
Jay George <contact@jaygeorge.co.uk>
2026-01-15 14:44:45 +0000

    separate header logo and nav button - wip

--- resources/js/components/global-header/Logo.vue
+++ resources/js/components/global-header/Logo.vue
@@ -23,7 +23,7 @@ function toggleNav() {
     <template v-if="customLogo">
         <div class="flex items-center gap-1 relative">
             <button class="flex items-center group rounded-xs cursor-pointer" type="button" @click="toggleNav" :aria-label="__('Toggle Nav')" style="--focus-outline-offset: 0.2rem;">
-                <div class="p-1 max-sm:ps-2 mr-2 test size-5 flex items-center justify-center lg:inset-0">
+                <div class="p-1 max-sm:ps-2 size-5 flex items-center justify-center lg:inset-0">
                     <Icon name="burger-menu-no-border" class="size-3.5! sm:size-3.25! opacity-75 hover:opacity-100" />
                 </div>
                 <!-- <img v-if="customLogoImage" :src="customLogoImage" :alt="cmsName" class="w-full max-w-[260px] max-h-7" v-tooltip="version"> -->
@@ -41,7 +41,7 @@ function toggleNav() {
                 </div>
             </button>
             <Link :href="cp_url('/')" class="flex items-center gap-1 max-[350px]:hidden text-white/85 rounded-xs whitespace-nowrap" style="--focus-outline-offset: var(--outline-offset-button);">
-                <StatamicLogo class="size-7 site-logo" v-tooltip="version" />
+                <StatamicLogo class="size-7 site-logo [&:focus:not(:focus-visible)]:outline-none" v-tooltip="version" />
                 <span>{{ logos.text ?? logos.siteName }}</span>
             </Link>
             <ProBadge v-if="isPro" />
