9e702303930dd238b69d67623b9995c39a45605f
Jay George <contact@jaygeorge.co.uk>
2026-01-15 14:31:17 +0000

    separate header logo and nav button - wip

--- resources/js/components/global-header/Logo.vue
+++ resources/js/components/global-header/Logo.vue
@@ -23,10 +23,10 @@ function toggleNav() {
     <template v-if="customLogo">
         <div class="flex items-center gap-1 relative">
             <button class="flex items-center group rounded-xs cursor-pointer" type="button" @click="toggleNav" :aria-label="__('Toggle Nav')" style="--focus-outline-offset: 0.2rem;">
-                <div class="p-1 max-sm:ps-2 mr-2 size-5 flex items-center justify-center lg:inset-0">
+                <div class="p-1 max-sm:ps-2 mr-2 test size-5 flex items-center justify-center lg:inset-0">
                     <Icon name="burger-menu-no-border" class="size-3.5! sm:size-3.25! opacity-75 hover:opacity-100" />
                 </div>
-                <img v-if="customLogoImage" :src="customLogoImage" :alt="cmsName" class="w-full max-w-[260px] max-h-7" v-tooltip="version">
+                <!-- <img v-if="customLogoImage" :src="customLogoImage" :alt="cmsName" class="w-full max-w-[260px] max-h-7" v-tooltip="version"> -->
             </button>
             <Link v-if="customLogoText && !customLogoImage" :href="cp_url('/')" class="mr-2 font-medium text-white whitespace-nowrap" v-tooltip="version" style="--focus-outline-offset: var(--outline-offset-button);">
                 {{ customLogoText }}
@@ -36,13 +36,13 @@ function toggleNav() {
     <template v-else>
         <div class="flex items-center gap-1.5 sm:gap-2 relative">
             <button class="flex items-center group rounded-xs cursor-pointer" type="button" @click="toggleNav" :aria-label="__('Toggle Nav')" style="--focus-outline-offset: 0.2rem;">
-                <div class="p-1 max-sm:ps-2 mr-2 size-5 flex items-center justify-center lg:inset-0">
+                <div class="p-1 max-sm:ps-2 size-5 flex items-center justify-center lg:inset-0">
                     <Icon name="burger-menu-no-border" class="size-3.5! sm:size-3.25! opacity-75 hover:opacity-100" />
                 </div>
-                <StatamicLogo class="size-7" v-tooltip="version" />
             </button>
-            <Link :href="cp_url('/')" class="max-[350px]:hidden text-white/85 rounded-xs whitespace-nowrap" style="--focus-outline-offset: var(--outline-offset-button);">
-                {{ logos.text ?? logos.siteName }}
+            <Link :href="cp_url('/')" class="flex items-center gap-1 max-[350px]:hidden text-white/85 rounded-xs whitespace-nowrap" style="--focus-outline-offset: var(--outline-offset-button);">
+                <StatamicLogo class="size-7 site-logo" v-tooltip="version" />
+                <span>{{ logos.text ?? logos.siteName }}</span>
             </Link>
             <ProBadge v-if="isPro" />
         </div>
