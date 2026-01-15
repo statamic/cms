80d8c6e904af9be4763ee4cb99b998a5c6a33e79
Jay George <contact@jaygeorge.co.uk>
2026-01-15 14:47:14 +0000

    separate header logo and nav button - custom logo wip

--- resources/js/components/global-header/Logo.vue
+++ resources/js/components/global-header/Logo.vue
@@ -26,10 +26,10 @@ function toggleNav() {
                 <div class="p-1 max-sm:ps-2 size-5 flex items-center justify-center lg:inset-0">
                     <Icon name="burger-menu-no-border" class="size-3.5! sm:size-3.25! opacity-75 hover:opacity-100" />
                 </div>
-                <!-- <img v-if="customLogoImage" :src="customLogoImage" :alt="cmsName" class="w-full max-w-[260px] max-h-7" v-tooltip="version"> -->
             </button>
-            <Link v-if="customLogoText && !customLogoImage" :href="cp_url('/')" class="mr-2 font-medium text-white whitespace-nowrap" v-tooltip="version" style="--focus-outline-offset: var(--outline-offset-button);">
-                {{ customLogoText }}
+            <Link :href="cp_url('/')" v-tooltip="version" style="--focus-outline-offset: var(--outline-offset-button);">
+                <img v-if="customLogoImage" :src="customLogoImage" :alt="cmsName" class="w-full max-w-[260px] max-h-7" v-tooltip="version">
+                <span v-if="customLogoText && !customLogoImage" class="mr-2 font-medium text-white whitespace-nowrap">{{ customLogoText }}</span>
             </Link>
         </div>
     </template>
