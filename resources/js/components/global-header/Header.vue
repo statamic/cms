aa269b1b7b31ae9b8992d786d057bee764bf4e5a
Jay George <contact@jaygeorge.co.uk>
2026-01-22 12:26:57 +0000

    Hide the toggle width button for smaller screens; it's just noise and doesn't do anything meaningful at that width

--- resources/js/components/global-header/Header.vue
+++ resources/js/components/global-header/Header.vue
@@ -37,7 +37,7 @@ const toggleMaxWidth = layout.toggleMaxWidth;
                 @click="toggleMaxWidth"
                 :aria-label="isMaxWidthEnabled ? __('Disable max width') : __('Enable max width')"
                 :title="isMaxWidthEnabled ? __('Disable max width') : __('Enable max width')"
-                class="inline-flex items-center justify-center whitespace-nowrap shrink-0 font-medium antialiased cursor-pointer no-underline disabled:text-white/60 dark:disabled:text-white/50 disabled:cursor-not-allowed [&_svg]:shrink-0 [&_svg]:text-gray-925 [&_svg]:opacity-60 dark:[&_svg]:text-white bg-transparent hover:bg-gray-400/10 text-gray-900 dark:text-gray-300 dark:hover:bg-white/15 dark:hover:text-gray-200 h-8 text-[0.8125rem] leading-tight rounded-lg px-0 gap-0 w-8 [&_svg]:size-4 -me-2 [&_svg]:text-white/85! v-popper--has-tooltip"
+                class="hidden [@media(min-width:1800px)]:inline-flex items-center justify-center whitespace-nowrap shrink-0 font-medium antialiased cursor-pointer no-underline disabled:text-white/60 dark:disabled:text-white/50 disabled:cursor-not-allowed [&_svg]:shrink-0 [&_svg]:text-gray-925 [&_svg]:opacity-60 dark:[&_svg]:text-white bg-transparent hover:bg-gray-400/10 text-gray-900 dark:text-gray-300 dark:hover:bg-white/15 dark:hover:text-gray-200 h-8 text-[0.8125rem] leading-tight rounded-lg px-0 gap-0 w-8 [&_svg]:size-4 -me-2 [&_svg]:text-white/85! v-popper--has-tooltip"
             >
                 <Icon :name="isMaxWidthEnabled ? 'zoom-fit-screen' : 'fit-screen'" />
             </button>
