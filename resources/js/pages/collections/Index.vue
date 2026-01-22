563a1471b7c1368c771ccc65c1c664b179dc92b3
Jay George <contact@jaygeorge.co.uk>
2026-01-22 17:08:49 +0000

    Fix /cp/collections not responding to the layout toggle control

--- resources/js/pages/collections/Index.vue
+++ resources/js/pages/collections/Index.vue
@@ -18,34 +18,32 @@ if (props.collections.length === 0) useArchitecturalBackground();
 <template>
     <Head :title="__('Collections')" />
 
-    <div class="max-w-page mx-auto">
-        <Listing
-            v-if="collections.length"
-            :initial-rows="collections"
-            :initial-columns="columns"
-            :can-create-collections="canCreate"
-            :create-url="createUrl"
-            :action-url="actionUrl"
-        />
+    <Listing
+        v-if="collections.length"
+        :initial-rows="collections"
+        :initial-columns="columns"
+        :can-create-collections="canCreate"
+        :create-url="createUrl"
+        :action-url="actionUrl"
+    />
 
-        <template v-else>
-            <header class="py-8 pt-16 text-center">
-                <h1 class="text-[25px] font-medium antialiased flex justify-center items-center gap-2 sm:gap-3">
-                    <Icon name="collections" class="size-5 text-gray-500" />
-                    {{ __('Collections') }}
-                </h1>
-            </header>
+    <template v-else>
+        <header class="py-8 pt-16 text-center">
+            <h1 class="text-[25px] font-medium antialiased flex justify-center items-center gap-2 sm:gap-3">
+                <Icon name="collections" class="size-5 text-gray-500" />
+                {{ __('Collections') }}
+            </h1>
+        </header>
 
-            <EmptyStateMenu :heading="__('statamic::messages.collection_configure_intro')">
-                <EmptyStateItem
-                    :href="createUrl"
-                    icon="collections"
-                    :heading="__('Create Collection')"
-                    :description="__('Get started by creating your first collection.')"
-                />
-            </EmptyStateMenu>
-        </template>
+        <EmptyStateMenu :heading="__('statamic::messages.collection_configure_intro')">
+            <EmptyStateItem
+                :href="createUrl"
+                icon="collections"
+                :heading="__('Create Collection')"
+                :description="__('Get started by creating your first collection.')"
+            />
+        </EmptyStateMenu>
+    </template>
 
-        <DocsCallout :topic="__('Collections')" url="collections" />
-    </div>
+    <DocsCallout :topic="__('Collections')" url="collections" />
 </template>
