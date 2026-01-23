3aa30c014f7fb578bf6965ce8656fce06aaf5a25
Jay George <contact@jaygeorge.co.uk>
2026-01-23 14:29:36 +0000

    Tidy - Add a few comments

--- resources/js/pages/layout/Layout.vue
+++ resources/js/pages/layout/Layout.vue
@@ -20,8 +20,8 @@ const props = defineProps({
 const additionalBreadcrumbs = ref(props.additionalBreadcrumbs);
 watch(() => props.additionalBreadcrumbs, (newVal) => additionalBreadcrumbs.value = newVal);
 
+// Function to toggle the max-width state
 const { isMaxWidthEnabled, toggle } = useMaxWidthToggle();
-
 provide('layout', {
     additionalBreadcrumbs,
     isMaxWidthEnabled,
@@ -37,8 +37,10 @@ provide('layout', {
 
         <main id="main" class="flex bg-body-bg dark:border-t dark:border-body-border rounded-t-2xl fixed top-14 inset-x-0 bottom-0 min-h-[calc(100vh-3.5rem)]">
             <Nav />
+            <!-- The data attribute allows CSS to target elements when max-width is disabled. -->
             <div id="main-content" class="main-content sm:p-2 h-full flex-1 overflow-y-auto rounded-t-2xl" :data-max-width-enabled="isMaxWidthEnabled">
                 <div id="content-card" class="relative content-card grid min-h-full mx-auto">
+                    <!-- Data attribute used by the CSS style tag below to override max-width when disabled.-->
                     <div class="w-full mx-auto max-w-page" data-max-width-wrapper>
                         <slot />
                     </div>
@@ -72,6 +74,14 @@ provide('layout', {
 </template>
 
 <style>
+/* 
+    Max-width override CSS:
+    When max-width is disabled (data-max-width-enabled="false"), 
+    this rule removes the max-width constraint from elements tagged with data-max-width-wrapper.
+
+    This allows the content to expand to full width when the toggle is disabled,
+    overriding Tailwind max-width class constraints.
+*/
 [data-max-width-enabled="false"] [data-max-width-wrapper] {
     width: 100%;
     max-width: none;
