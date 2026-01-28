498d1f25f5dffbf5d90c0455005dfbca25da65a9
Jay George <contact@jaygeorge.co.uk>
2026-01-28 12:16:20 +0000

    Add a preview of how many assets remain

--- resources/js/components/fieldtypes/assets/AssetsIndexFieldtype.vue
+++ resources/js/components/fieldtypes/assets/AssetsIndexFieldtype.vue
@@ -3,6 +3,12 @@
         <a v-for="asset in value.slice(0, 5)" :key="asset.id" :href="asset.url" target="_blank">
             <asset-thumbnail :asset="asset" class="-my-1 h-8 max-w-3xs" />
         </a>
+        <span
+            v-if="value.length > 5"
+            class="-my-1 flex h-8 min-w-8 items-center justify-center px-1.5 font-mono text-gray-600 dark:text-gray-400"
+        >
+            +&thinsp;{{ value.length - 5 }}
+        </span>
     </div>
 </template>
 
