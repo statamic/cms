7736d2f2af61d16d7222103199716d5094db3269
Jay George <contact@jaygeorge.co.uk>
2026-01-28 12:06:38 +0000

    Limit assets to 5 as a preview by default

--- resources/js/components/fieldtypes/assets/AssetsIndexFieldtype.vue
+++ resources/js/components/fieldtypes/assets/AssetsIndexFieldtype.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex text-2xs gap-2">
-        <a v-for="asset in value" :key="asset.id" :href="asset.url" target="_blank">
+        <a v-for="asset in value.slice(0, 5)" :key="asset.id" :href="asset.url" target="_blank">
             <asset-thumbnail :asset="asset" class="-my-1 h-8 max-w-3xs" />
         </a>
     </div>
