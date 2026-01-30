1c05fe7624b67bdd3c70e3691cd3dce860b3d85a
Jason Varga <jason@pixelfear.com>
2026-01-30 11:43:56 -0500

    [5.x] Avoid showing large number of assets in listing (#13758)

--- resources/js/components/fieldtypes/assets/AssetsIndexFieldtype.vue
+++ resources/js/components/fieldtypes/assets/AssetsIndexFieldtype.vue
@@ -2,13 +2,19 @@
 
     <div class="text-2xs flex">
         <a
-            v-for="asset in value"
+            v-for="asset in value.assets.slice(0, value.total > 6 ? 5 : 6)"
             :key="asset.id"
             :href="asset.url"
             target="_blank"
         >
             <asset-thumbnail :asset="asset" class="h-8 max-w-3xs -my-1" />
         </a>
+        <span
+            v-if="value.total > 6"
+            class="-my-1 flex h-8 min-w-8 items-center justify-center px-1.5 font-mono text-gray-600 dark:text-gray-400"
+        >
+            +&thinsp;{{ value.total - 5 }}
+        </span>
     </div>
 
 </template>
