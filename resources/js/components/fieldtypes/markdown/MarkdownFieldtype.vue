ae3f7d04c26f607855618f9ea382139f5d7987f4
Jay George <github@jaygeorge.co.uk>
2026-01-30 15:09:17 +0000

    [6.x] Fix the z-index of the sticky markdown toolbar, plus position on mobile (#13751)
    Fix the z-index of the sticky markdown toolbar, plus position on mobile

--- resources/js/components/fieldtypes/markdown/MarkdownFieldtype.vue
+++ resources/js/components/fieldtypes/markdown/MarkdownFieldtype.vue
@@ -53,7 +53,7 @@
                             :is-fullscreen="false"
                             @toggle-dark-mode="toggleDarkMode"
                             @button-click="handleButtonClick"
-                            class="sticky z-(--z-index-above) -top-2 mb-2 [&~*]:-mt-2"
+                            class="sticky z-(--z-index-portal) top-0 sm:-top-2 mb-2 [&~*]:-mt-2"
                         />
 
                         <div class="drag-notification" v-show="dragging">
