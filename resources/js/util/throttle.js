66ad791f68ccc137a76cf02a36612e5999b22c6b
Duncan McClean <duncan@duncanmcclean.com>
2026-02-17 11:45:16 +0000

    Fix sidebar not showing after quick resize

--- resources/js/util/throttle.js
+++ resources/js/util/throttle.js
@@ -1,12 +1,21 @@
 export default function (func, timeFrame) {
     let lastTime = 0;
+    let timeoutId = null;
 
     return function (...args) {
-        const now = new Date();
+        const now = Date.now();
+
+        clearTimeout(timeoutId);
 
         if (now - lastTime >= timeFrame) {
             func(...args);
             lastTime = now;
+        } else {
+            timeoutId = setTimeout(() => {
+                func(...args);
+                lastTime = Date.now();
+                timeoutId = null;
+            }, timeFrame - (now - lastTime));
         }
     };
 }
