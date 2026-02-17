2d873942ec2485e08e63f00231a3a8eb90a6e618
Jason Varga <jason@pixelfear.com>
2026-02-17 16:20:47 -0500

    tweak wording

--- config/static_caching.php
+++ config/static_caching.php
@@ -134,12 +134,10 @@ return [
     | Replace Nocache Regions In Initial Response
     |--------------------------------------------------------------------------
     |
-    | Nocache regions are usually replaced server-side for the initial request,
-    | while the cached file stores placeholders with JS to fetch them via
-    | AJAX. You may wish to disable this behavior when using a CDN,
+    | When using full-measure, nocache regions are replaced for the initial
+    | request to avoid needing to immediately re-request nocache regions
+    | via AJAX. You may wish to disable this behavior when using a CDN,
     | so the initial response always includes the placeholders.
-    |
-    | Only applies to full-measure static caching.
     |
     */
 
