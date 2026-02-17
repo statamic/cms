e47ae873ad09f2df312651ce20e346659501e013
Duncan McClean <duncan@duncanmcclean.com>
2026-02-17 17:47:59 +0000

    Fix dropdown z-index

--- resources/js/components/ui/Combobox/Combobox.vue
+++ resources/js/components/ui/Combobox/Combobox.vue
@@ -441,7 +441,7 @@ defineExpose({
 	                    :side-offset="5"
                         position="popper"
                         :class="[
-                            'shadow-ui-sm z-(-well-z-index-above) rounded-lg border border-gray-200 bg-white dark:border-white/10 dark:bg-gray-800',
+                            'shadow-ui-sm z-(--z-index-above) rounded-lg border border-gray-200 bg-white dark:border-white/10 dark:bg-gray-800',
                             'max-h-[var(--reka-combobox-content-available-height)] min-w-[var(--reka-combobox-trigger-width)] overflow-hidden',
                             adaptiveWidth && 'w-max max-w-md',
                         ]"
