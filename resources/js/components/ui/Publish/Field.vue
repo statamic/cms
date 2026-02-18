f96ceaa4306528c63974d9eecd8c70df3682936b
Jay George <contact@jaygeorge.co.uk>
2026-02-18 16:49:23 +0000

    Remove some more text to be safe

--- resources/js/components/ui/Publish/Field.vue
+++ resources/js/components/ui/Publish/Field.vue
@@ -251,7 +251,7 @@ const fieldtypeComponentEvents = computed(() => ({
                 <FieldActions :actions="fieldActions" />
             </template>
             <div class="text-xs text-red-600" v-if="!fieldtypeComponentExists">
-                <code v-text="fieldtypeComponent"></code> does not exist.
+                <code v-text="fieldtypeComponent"></code>
             </div>
             <div :dir="direction">
                 <Component
