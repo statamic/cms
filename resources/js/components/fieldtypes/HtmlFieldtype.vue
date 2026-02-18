922c75c1b29fddf7eeecf6fb16829932194b35d6
Jason Varga <jason@pixelfear.com>
2026-02-18 13:13:31 -0500

    sanitize

--- resources/js/components/fieldtypes/HtmlFieldtype.vue
+++ resources/js/components/fieldtypes/HtmlFieldtype.vue
@@ -1,11 +1,18 @@
 <template>
-    <div v-html="config.html" />
+    <div v-html="html" />
 </template>
 
 <script>
 import Fieldtype from './Fieldtype.vue';
+import DOMPurify from 'dompurify';
 
 export default {
     mixins: [Fieldtype],
+
+    computed: {
+        html() {
+            return DOMPurify.sanitize(this.config.html);
+        },
+    },
 };
 </script>
