5d2888ea5f2e01f2740c0e7b946ac2f84a2b0812
Jason Varga <jason@pixelfear.com>
2026-02-20 14:33:55 -0500

    share data provider

--- tests/Facades/UrlTest.php
+++ tests/Facades/UrlTest.php
@@ -10,6 +10,8 @@ use Tests\TestCase;
 
 class UrlTest extends TestCase
 {
+    use Concerns\ProvidesExternalUrls;
+
     public function tearDown(): void
     {
         URL::enforceTrailingSlashes(false);
@@ -233,7 +235,7 @@ class UrlTest extends TestCase
     }
 
     #[Test]
-    #[DataProvider('externalProvider')]
+    #[DataProvider('externalUrlProvider')]
     public function it_determines_if_external_url_to_application($url, $expected)
     {
         $this->setSites([
@@ -245,64 +247,14 @@ class UrlTest extends TestCase
         $this->assertEquals($expected, URL::isExternalToApplication($url));
     }
 
-    public static function externalProvider()
+    #[Test]
+    public function it_determines_if_external_url_to_application_when_only_current_request_domain_matches()
     {
-        return [
-            ['http://this-site.com', false],
-            ['http://this-site.com?foo', false],
-            ['http://this-site.com#anchor', false],
-            ['http://this-site.com/', false],
-            ['http://this-site.com/?foo', false],
-            ['http://this-site.com/#anchor', false],
+        $this->setSites([
+            'a' => ['name' => 'A', 'locale' => 'en_US', 'url' => 'http://this-site.com/'],
+        ]);
 
-            ['http://that-site.com', true],
-            ['http://that-site.com/', true],
-            ['http://that-site.com/?foo', true],
-            ['http://that-site.com/#anchor', true],
-            ['http://that-site.com/some-slug', true],
-            ['http://that-site.com/some-slug?foo', true],
-            ['http://that-site.com/some-slug#anchor', true],
-
-            ['http://subdomain.this-site.com', false],
-            ['http://subdomain.this-site.com/', false],
-            ['http://subdomain.this-site.com/?foo', false],
-            ['http://subdomain.this-site.com/#anchor', false],
-            ['http://subdomain.this-site.com/some-slug', false],
-            ['http://subdomain.this-site.com/some-slug?foo', false],
-            ['http://subdomain.this-site.com/some-slug#anchor', false],
-
-            ['http://absolute-url-resolved-from-request.com', false],
-            ['http://absolute-url-resolved-from-request.com/', false],
-            ['http://absolute-url-resolved-from-request.com/?foo', false],
-            ['http://absolute-url-resolved-from-request.com/?anchor', false],
-            ['http://absolute-url-resolved-from-request.com/some-slug', false],
-            ['http://absolute-url-resolved-from-request.com/some-slug?foo', false],
-            ['http://absolute-url-resolved-from-request.com/some-slug#anchor', false],
-            ['/', false],
-            ['/?foo', false],
-            ['/#anchor', false],
-            ['/some-slug', false],
-            ['?foo', false],
-            ['#anchor', false],
-            ['', false],
-            [null, false],
-
-            // External domain that starts with a valid domain
-            ['http://this-site.com.au', true],
-            ['http://this-site.com.au/', true],
-            ['http://this-site.com.au/?foo', true],
-            ['http://this-site.com.au/#anchor', true],
-            ['http://this-site.com.au/some-slug', true],
-            ['http://this-site.com.au/some-slug?foo', true],
-            ['http://this-site.com.au/some-slug#anchor', true],
-            ['http://subdomain.this-site.com.au', true],
-            ['http://subdomain.this-site.com.au/', true],
-            ['http://subdomain.this-site.com.au/?foo', true],
-            ['http://subdomain.this-site.com.au/#anchor', true],
-            ['http://subdomain.this-site.com.au/some-slug', true],
-            ['http://subdomain.this-site.com.au/some-slug?foo', true],
-            ['http://subdomain.this-site.com.au/some-slug#anchor', true],
-        ];
+        $this->assertFalse(URL::isExternalToApplication('http://absolute-url-resolved-from-request.com/some-slug'));
     }
 
     #[Test]
