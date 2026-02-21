fb5e472f934d74db779c2a92a56419791bcee691
Philipp Daun <post@philippdaun.net>
2026-02-21 12:37:48 +0100

    Add comment explaining duration filter
    Signed-off-by: Philipp Daun <post@philippdaun.net>

--- src/Query/Scopes/Filters/Fields/Duration.php
+++ src/Query/Scopes/Filters/Fields/Duration.php
@@ -5,6 +5,9 @@ namespace Statamic\Query\Scopes\Filters\Fields;
 use Illuminate\Support\Arr;
 use Statamic\Support\Str;
 
+/**
+ * Integer filter but with a "seconds" appended to the input for clarity
+ */
 class Duration extends Integer
 {
     public function fieldItems()
