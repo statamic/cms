9b1d320f5935c5ff2bbc89fbb2e729e0652a53a8
Duncan McClean <duncan@duncanmcclean.com>
2026-02-25 16:23:11 +0000

    add tests

--- tests/Fieldtypes/IconTest.php
+++ tests/Fieldtypes/IconTest.php
@@ -2,6 +2,9 @@
 
 namespace Tests\Fieldtypes;
 
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 use Statamic\Facades\Antlers;
 use Statamic\Fields\Field;
@@ -25,6 +28,44 @@ class IconTest extends TestCase
         $result = (string) Antlers::parse('{{ svg :src="test" class="w-4 h-4" sanitize="false" }}', ['test' => new Value('add-circle', $this->fieldtype())]);
 
         $this->assertStringContainsString('<svg class="w-4 h-4"', $result);
+    }
+
+    #[Test]
+    #[DataProvider('validationProvider')]
+    public function it_validates($input, $passes)
+    {
+        $field = $this->fieldtype()->field();
+        $messages = collect();
+
+        try {
+            Validator::validate(['test' => $input], $field->rules(), [], $field->validationAttributes());
+        } catch (ValidationException $e) {
+            $messages = $e->validator->errors();
+        }
+
+        if ($passes) {
+            $this->assertCount(0, $messages);
+        } else {
+            $this->assertEquals('The test field must be a valid icon name.', $messages->first());
+        }
+    }
+
+    public static function validationProvider()
+    {
+        return [
+            'valid icon name' => [
+                'add-circle',
+                true,
+            ],
+            'html string' => [
+                '<script>alert("xss")</script>',
+                false,
+            ],
+            'svg string' => [
+                '<svg></svg>',
+                false,
+            ],
+        ];
     }
 
     private function fieldtype($config = [])
