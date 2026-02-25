d81345447cc55b82dbc0c6ed0b1c5bd4b078c1bc
Jay George <contact@jaygeorge.co.uk>
2026-02-25 11:46:55 +0000

    Solve tab overflow with clipping for small mobile-like viewports

--- resources/js/components/blueprints/Tabs.vue
+++ resources/js/components/blueprints/Tabs.vue
@@ -3,9 +3,9 @@
         <div>
             <Tabs v-model="currentTab" :unmount-on-hide="false">
                 <div v-if="!singleTab && tabs.length > 0" class="flex items-center justify-between gap-x-2 mb-6">
-                    <TabList class="flex-1 min-w-0">
+                    <TabList class="flex-1 min-w-0 overflow-x-clip overflow-y-visible">
                         <div ref="tabs" class="flex-1 flex items-center gap-x-2.5 min-w-0">
-                            <div ref="tabWrapper" class="min-w-0 flex-1 flex overflow-hidden">
+                            <div ref="tabWrapper" class="min-w-0 flex-1 flex overflow-clip">
                                 <div ref="tabInner" class="flex items-center gap-x-2.5 shrink-0">
                                     <BlueprintTab
                                         ref="tab"
