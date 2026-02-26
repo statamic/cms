110b3568ffa6ec8607ca4da00118e5f627825ca7
Duncan McClean <duncan@duncanmcclean.com>
2026-02-26 14:38:33 +0000

    no comments

--- tests/Tags/User/LoginFormTest.php
+++ tests/Tags/User/LoginFormTest.php
@@ -236,7 +236,7 @@ EOT
                 'password' => 'wrong',
                 '_error_redirect' => 'https://evil.com',
             ])
-            ->assertLocation('/'); // Falls back to back() which is /
+            ->assertLocation('/');
     }
 
     #[Test]
