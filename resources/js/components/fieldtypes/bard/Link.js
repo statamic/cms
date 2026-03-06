66c2d281ad96779c49be66419929f20a439cd7ca
Jason Varga <jason@pixelfear.com>
2026-03-06 12:02:23 -0500

    switch it back to mod

--- resources/js/components/fieldtypes/bard/Link.js
+++ resources/js/components/fieldtypes/bard/Link.js
@@ -63,7 +63,7 @@ export const Link = Mark.create({
 
     addKeyboardShortcuts() {
         return {
-            'Ctrl-k': () => {
+            'Mod-k': () => {
                 if (this.editor.state.selection.empty) return false;
                 this.options.vm.events.emit('open-link-toolbar');
                 return true;
