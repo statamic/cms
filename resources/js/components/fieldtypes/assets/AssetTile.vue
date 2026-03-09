cbc8337862b45a72396b0c8765f09f8ef9a29e4e
Jason Varga <jason@pixelfear.com>
2026-03-09 14:22:43 -0400

    apply to asset fieldtype

--- resources/js/components/fieldtypes/assets/AssetTile.vue
+++ resources/js/components/fieldtypes/assets/AssetTile.vue
@@ -18,7 +18,10 @@
         >
         </asset-editor>
 
-        <div class="flex h-full w-full justify-center rounded-b-md relative" :class="{ 'bg-checkerboard rounded-lg!': canBeTransparent, 'border-b dark:border-gray-700': showFilename }">
+        <div class="flex h-full w-full justify-center rounded-b-md relative" :class="[
+            { 'border-b dark:border-gray-700': showFilename },
+            canBeTransparent && checkerboardMode !== 'transparent' ? `bg-checkerboard bg-checkerboard-${checkerboardMode} rounded-lg` : ''
+        ]">
             <div class="p-1 flex flex-col items-center justify-center h-full">
                 <!-- Solo Bard -->
                 <template v-if="isImage && isInBardField && !isInAssetBrowser">
@@ -80,6 +83,13 @@ export default {
     },
 
     mixins: [Asset],
+
+    props: {
+        checkerboardMode: {
+            type: String,
+            default: 'transparent',
+        },
+    },
 
     computed: {
         isInAssetBrowser() {
