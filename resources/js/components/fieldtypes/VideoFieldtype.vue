5548b80560e9b7b03bfb8bd2136435c9db57e188
edalzell <erin@silentz.co>
2026-03-18 13:40:10 -0700

    fix bad merge

--- resources/js/components/fieldtypes/VideoFieldtype.vue
+++ resources/js/components/fieldtypes/VideoFieldtype.vue
@@ -72,9 +72,65 @@ export default {
     },
 
     computed: {
+        shouldShowPreview() {
+            return !this.isInvalid && (this.isEmbeddable || this.isVideo);
+        },
+
+        embedUrl() {
+            let embed_url = this.value || '';
+
+            if (embed_url.includes('youtube')) {
+                embed_url = embed_url.includes('shorts/')
+                    ? embed_url.replace('shorts/', 'embed/')
+                    : embed_url.replace('watch?v=', 'embed/');
+            }
+
+            if (embed_url.includes('youtu.be')) {
+                embed_url = embed_url.replace('youtu.be', 'www.youtube.com/embed');
+            }
+
+            if (embed_url.includes('vimeo')) {
+                embed_url = embed_url.replace('/vimeo.com', '/player.vimeo.com/video');
+
+                if (!this.value.includes('progressive_redirect') && embed_url.split('/').length > 5) {
+                    let hash = embed_url.substr(embed_url.lastIndexOf('/') + 1);
+                    embed_url = embed_url.substr(0, embed_url.lastIndexOf('/')) + '?h=' + hash.replace('?', '&');
+                }
+            }
+
+            if (embed_url.includes('&') && !embed_url.includes('?')) {
+                embed_url = embed_url.replace('&', '?');
+            }
+
+            return embed_url;
+        },
+
+        isEmbeddable() {
+            const url = this.value || '';
+            const isYoutube = url.includes('youtube') || url.includes('youtu.be');
+            const isVimeo = url.includes('vimeo');
+            return isYoutube || isVimeo;
+        },
+
+        isInvalid() {
+            let htmlRegex = new RegExp(/<([A-Z][A-Z0-9]*)\b[^>]*>.*?<\/\1>|<([A-Z][A-Z0-9]*)\b[^\/]*\/>/i);
+            return htmlRegex.test(this.value || '');
+        },
+
+        isUrl() {
+            const url = this.value || '';
+            return url.startsWith('http://') || url.startsWith('https://');
+        },
+
+        isVideo() {
+            const url = this.value || '';
+            const isVideo = url.includes('.mp4') || url.includes('.ogv') || url.includes('.mov') || url.includes('.webm');
+            return !this.isEmbeddable && isVideo;
+        },
+
         providers() {
             return this.meta.providers;
-        },
+        }
     },
 
     methods: {
