d8e6c0c1ce67a72017bfc83abd17373c0368a5c1
Jack McDade <jack@jackmcdade.com>
2026-03-17 23:43:13 -0400

    Actually show the Fieldset title (not just the handle)

--- resources/js/components/blueprints/ImportField.vue
+++ resources/js/components/blueprints/ImportField.vue
@@ -8,8 +8,7 @@
                         <div class="flex flex-1 items-center py-2">
                             <ui-icon class="size-4 me-2 text-ui-accent-text/80" name="fieldsets" />
                             <div class="flex items-center gap-2">
-                            <!-- @TODO: Show fieldset.title -->
-                                <button class="cursor-pointer overflow-hidden text-ellipsis text-sm text-ui-accent-text hover:text-ui-accent-text/80" v-text="field.fieldset" @click="$emit('edit')" />
+                                <button class="cursor-pointer overflow-hidden text-ellipsis text-sm text-ui-accent-text hover:text-ui-accent-text/80" v-text="fieldsetTitle" @click="$emit('edit')" />
                                 <ui-icon name="link" class="text-gray-400" />
                                 <span class="text-gray-500 font-mono text-2xs" v-text="__('Fieldset')" />
                             </div>
@@ -50,6 +49,12 @@ export default {
     },
 
     computed: {
+        fieldsetTitle() {
+            const title = this.$page?.props?.fieldsets?.[this.field.fieldset]?.title;
+
+            return title ? __(title) : this.field.fieldset;
+        },
+
         fieldConfig() {
             const { _id, type, ...config } = this.field;
             return config;
