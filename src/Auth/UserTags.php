ebf287b9d31279c68be9050bca4f8eef45af22ce
Jason Varga <jason@pixelfear.com>
2026-03-18 17:24:36 -0400

    method not on this branch

--- src/Auth/UserTags.php
+++ src/Auth/UserTags.php
@@ -380,7 +380,7 @@ class UserTags extends Tags
             return null;
         }
 
-        if (! URL::isAbsolute($url) && ! str_starts_with($url, '/')) {
+        if (! preg_match('#^https?://#', $url) && ! str_starts_with($url, '/')) {
             $url = '/'.$url;
         }
 
