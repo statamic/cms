a5506fab39f06c10ff042f91d44532b25994d1f8
Jason Varga <jason@pixelfear.com>
2026-03-18 15:09:24 -0400

    adjust provider trait so that we can get internal/external urls on their own.

--- tests/Facades/Concerns/ProvidesExternalUrls.php
+++ tests/Facades/Concerns/ProvidesExternalUrls.php
@@ -4,84 +4,102 @@ namespace Tests\Facades\Concerns;
 
 trait ProvidesExternalUrls
 {
-    public static function externalUrlProvider()
+    private static function internalUrls()
     {
         return [
-            ['http://this-site.com', false],
-            ['http://this-site.com?foo', false],
-            ['http://this-site.com#anchor', false],
-            ['http://this-site.com/', false],
-            ['http://this-site.com/?foo', false],
-            ['http://this-site.com/#anchor', false],
+            'http://this-site.com',
+            'http://this-site.com?foo',
+            'http://this-site.com#anchor',
+            'http://this-site.com/',
+            'http://this-site.com/?foo',
+            'http://this-site.com/#anchor',
 
-            ['http://that-site.com', true],
-            ['http://that-site.com/', true],
-            ['http://that-site.com/?foo', true],
-            ['http://that-site.com/#anchor', true],
-            ['http://that-site.com/some-slug', true],
-            ['http://that-site.com/some-slug?foo', true],
-            ['http://that-site.com/some-slug#anchor', true],
+            'http://subdomain.this-site.com',
+            'http://subdomain.this-site.com/',
+            'http://subdomain.this-site.com/?foo',
+            'http://subdomain.this-site.com/#anchor',
+            'http://subdomain.this-site.com/some-slug',
+            'http://subdomain.this-site.com/some-slug?foo',
+            'http://subdomain.this-site.com/some-slug#anchor',
 
-            ['http://subdomain.this-site.com', false],
-            ['http://subdomain.this-site.com/', false],
-            ['http://subdomain.this-site.com/?foo', false],
-            ['http://subdomain.this-site.com/#anchor', false],
-            ['http://subdomain.this-site.com/some-slug', false],
-            ['http://subdomain.this-site.com/some-slug?foo', false],
-            ['http://subdomain.this-site.com/some-slug#anchor', false],
+            'http://absolute-url-resolved-from-request.com',
+            'http://absolute-url-resolved-from-request.com/',
+            'http://absolute-url-resolved-from-request.com/?foo',
+            'http://absolute-url-resolved-from-request.com/?anchor',
+            'http://absolute-url-resolved-from-request.com/some-slug',
+            'http://absolute-url-resolved-from-request.com/some-slug?foo',
+            'http://absolute-url-resolved-from-request.com/some-slug#anchor',
 
-            ['http://absolute-url-resolved-from-request.com', false],
-            ['http://absolute-url-resolved-from-request.com/', false],
-            ['http://absolute-url-resolved-from-request.com/?foo', false],
-            ['http://absolute-url-resolved-from-request.com/?anchor', false],
-            ['http://absolute-url-resolved-from-request.com/some-slug', false],
-            ['http://absolute-url-resolved-from-request.com/some-slug?foo', false],
-            ['http://absolute-url-resolved-from-request.com/some-slug#anchor', false],
-            ['/', false],
-            ['/?foo', false],
-            ['/#anchor', false],
-            ['/some-slug', false],
-            ['?foo', false],
-            ['#anchor', false],
-            ['', false],
-            [null, false],
+            '/',
+            '/?foo',
+            '/#anchor',
+            '/some-slug',
+            '?foo',
+            '#anchor',
+            '',
+            null,
+        ];
+    }
+
+    private static function externalUrls()
+    {
+        return [
+            'http://that-site.com',
+            'http://that-site.com/',
+            'http://that-site.com/?foo',
+            'http://that-site.com/#anchor',
+            'http://that-site.com/some-slug',
+            'http://that-site.com/some-slug?foo',
+            'http://that-site.com/some-slug#anchor',
 
             // Protocol-relative URLs are external
-            ['//evil.com', true],
-            ['//evil.com/', true],
-            ['//evil.com/path', true],
-            ['//this-site.com', true],
+            '//evil.com',
+            '//evil.com/',
+            '//evil.com/path',
+            '//this-site.com',
 
             // External domain that starts with a valid domain.
-            ['http://this-site.com.au', true],
-            ['http://this-site.com.au/', true],
-            ['http://this-site.com.au/?foo', true],
-            ['http://this-site.com.au/#anchor', true],
-            ['http://this-site.com.au/some-slug', true],
-            ['http://this-site.com.au/some-slug?foo', true],
-            ['http://this-site.com.au/some-slug#anchor', true],
-            ['http://subdomain.this-site.com.au', true],
-            ['http://subdomain.this-site.com.au/', true],
-            ['http://subdomain.this-site.com.au/?foo', true],
-            ['http://subdomain.this-site.com.au/#anchor', true],
-            ['http://subdomain.this-site.com.au/some-slug', true],
-            ['http://subdomain.this-site.com.au/some-slug?foo', true],
-            ['http://subdomain.this-site.com.au/some-slug#anchor', true],
+            'http://this-site.com.au',
+            'http://this-site.com.au/',
+            'http://this-site.com.au/?foo',
+            'http://this-site.com.au/#anchor',
+            'http://this-site.com.au/some-slug',
+            'http://this-site.com.au/some-slug?foo',
+            'http://this-site.com.au/some-slug#anchor',
+            'http://subdomain.this-site.com.au',
+            'http://subdomain.this-site.com.au/',
+            'http://subdomain.this-site.com.au/?foo',
+            'http://subdomain.this-site.com.au/#anchor',
+            'http://subdomain.this-site.com.au/some-slug',
+            'http://subdomain.this-site.com.au/some-slug?foo',
+            'http://subdomain.this-site.com.au/some-slug#anchor',
 
             // Credential injection
-            ['http://this-site.com@evil.com', true],
-            ['http://this-site.com@evil.com/', true],
-            ['http://this-site.com@evil.com/path', true],
-            ['http://this-site.com@evil.com/path?query', true],
-            ['http://this-site.com:password@evil.com', true],
-            ['http://user:pass@evil.com', true],
-            ['http://absolute-url-resolved-from-request.com@evil.com', true],
-            ['http://absolute-url-resolved-from-request.com@evil.com/path', true],
-            ['http://subdomain.this-site.com@evil.com', true],
-            ['http://subdomain.this-site.com@evil.com/path', true],
-            ['http://this-site.com:8000@evil.com', true],
-            ['http://this-site.com:8000@evil.com/path', true],
-            ['http://this-site.com:8000@webhook.site/token', true],
+            'http://this-site.com@evil.com',
+            'http://this-site.com@evil.com/',
+            'http://this-site.com@evil.com/path',
+            'http://this-site.com@evil.com/path?query',
+            'http://this-site.com:password@evil.com',
+            'http://user:pass@evil.com',
+            'http://absolute-url-resolved-from-request.com@evil.com',
+            'http://absolute-url-resolved-from-request.com@evil.com/path',
+            'http://subdomain.this-site.com@evil.com',
+            'http://subdomain.this-site.com@evil.com/path',
+            'http://this-site.com:8000@evil.com',
+            'http://this-site.com:8000@evil.com/path',
+            'http://this-site.com:8000@webhook.site/token',
+        ];
+    }
+
+    public static function externalUrlProvider()
+    {
+        $keyFn = function ($key) {
+            return is_null($key) ? 'null' : $key;
+        };
+
+        return [
+            ...collect(static::internalUrls())->mapWithKeys(fn ($url) => [$keyFn($url) => [$url, false]])->all(),
+            ...collect(static::externalUrls())->mapWithKeys(fn ($url) => [$keyFn($url) => [$url, true]])->all(),
         ];
     }
 }
