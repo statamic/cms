f1dcc5e93b6ff80a1cd3dbfa07f54fd9ddeb86da
Jay George <contact@jaygeorge.co.uk>
2026-03-24 16:58:31 +0000

    Copy set.js from v6

--- resources/js/components/fieldtypes/bard/Set.js
+++ resources/js/components/fieldtypes/bard/Set.js
@@ -75,35 +75,6 @@ export const Set = Node.create({
         };
     },
 
-    addKeyboardShortcuts() {
-        const shortcuts = {};
-        const type = this.type;
-
-        const isSetSelected = (state) => {
-            const { selection } = state;
-            return selection instanceof NodeSelection && selection.node.type === type;
-        };
-
-        const blockCharacterKey = () => isSetSelected(this.editor.state);
-
-        // Letters a-z
-        for (let i = 97; i <= 122; i++) {
-            shortcuts[String.fromCharCode(i)] = blockCharacterKey;
-        }
-
-        // Numbers 0-9
-        for (let i = 0; i <= 9; i++) {
-            shortcuts[String(i)] = blockCharacterKey;
-        }
-
-        // Common punctuation/symbols
-        [' ', '-', '=', '[', ']', '\\', ';', "'", ',', '.', '/', '`'].forEach(
-            (key) => (shortcuts[key] = blockCharacterKey),
-        );
-
-        return shortcuts;
-    },
-
     addProseMirrorPlugins() {
         const bard = this.options.bard;
         const type = this.type;
