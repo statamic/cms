f2e2b5c21bf76ea96c4bfb403fd71b4193b4f963
Jason Varga <jason@pixelfear.com>
2026-03-26 15:00:04 -0400

    improve ui, allow arbitrarily entered values to get their names and samples displayed. handle invalid values.

--- resources/js/components/fieldtypes/FormattingLocalesFieldtype.vue
+++ resources/js/components/fieldtypes/FormattingLocalesFieldtype.vue
@@ -7,12 +7,33 @@
         :placeholder="null"
         :model-value="value"
         @update:modelValue="comboboxUpdated"
-    />
+    >
+        <template #selected-option="{ option: { value, label, sample } }">
+            <template v-if="value === 'language'">{{ label }}</template>
+            <div v-else class="w-full flex justify-between">
+                <div class="text-start flex-1">
+                    {{ getLabel(value) }}
+                    <span class="ms-4 text-gray-500 dark:text-gray-400" v-text="value" />
+                </div>
+                <span class="text-gray-500 dark:text-gray-400" v-text="getSample(value)" />
+            </div>
+        </template>
+        <template #option="{ value, label, sample }">
+            <template v-if="value === 'language'">{{ label }}</template>
+            <div v-else class="w-full flex justify-between">
+                <div class="text-start flex-1">
+                    {{ label }}
+                    <span class="ms-4 text-gray-500 dark:text-gray-400" v-text="value" />
+                </div>
+                <span class="text-gray-500 dark:text-gray-400" v-text="sample" />
+            </div>
+        </template>
+    </Combobox>
 </template>
 
 <script setup>
 import Fieldtype from '@/components/fieldtypes/fieldtype.js';
-import { dateFormatter } from '@api';
+import { dateFormatter, toast } from '@api';
 import { Combobox } from '@/components/ui';
 import { computed } from 'vue';
 
@@ -32,20 +53,12 @@ const displayNames = typeof Intl.DisplayNames !== 'undefined'
 
 const options = computed(() => {
     const locales = Intl.DateTimeFormat.supportedLocalesOf(candidateLocales);
-    const now = new Date();
 
-    const formatted = locales.map((locale) => {
-        const language = locale.split('-')[0];
-        const display = displayNames?.of(language);
-        const sample = dateFormatter.withLocale(locale, (formatter) => formatter.format(now, 'datetime'));
-
-        return {
-            value: locale,
-            label: display
-                ? `${locale} (${display}) - ${sample}`
-                : `${locale} - ${sample}`,
-        };
-    });
+    const formatted = locales.map((locale) => ({
+        value: locale,
+        label: getLabel(locale),
+        sample: getSample(locale),
+    }));
 
     return [
         { value: 'language', label: __('Same as language') },
@@ -53,7 +66,33 @@ const options = computed(() => {
     ];
 });
 
+function getLabel(locale) {
+    return displayNames?.of(locale.split('-')[0]);
+}
+
+function getSample(locale) {
+    return dateFormatter.withLocale(locale, (formatter) => formatter.format(new Date, 'datetime'));
+}
+
 function comboboxUpdated(value) {
+    if (value && !isValidLocale(value)) {
+        update(null);
+        toast.error(__('statamic::messages.preference_formatting_locale_invalid'));
+        return;
+    }
+
     update(value || null);
+}
+
+function isValidLocale(value) {
+    if (value === 'language') {
+        return true;
+    }
+
+    try {
+        return Intl.DateTimeFormat.supportedLocalesOf([value]).length > 0;
+    } catch {
+        return false;
+    }
 }
 </script>
