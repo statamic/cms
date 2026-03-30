2a97d5b3d53f6c1be100071c371d460f7ea5e687
Jay George <contact@jaygeorge.co.uk>
2026-03-30 14:11:43 +0100

    Tweak background position

--- resources/js/components/nav/Nav.vue
+++ resources/js/components/nav/Nav.vue
@@ -164,7 +164,7 @@ Statamic.$events.$on('nav.toggle', toggle);
 
 <template>
     <div class="cp-sidebar-start" :class="sidebarStartSizeClass">
-        <nav v-if="showPrototypeNavBlock" style="--graph-paper-y-offset: 2.5rem;" class="bg-graph-paper [&_button]:rounded-xl [&_button]:w-full [&_button]:font-normal [&_button]:justify-start [&_button]:h-9 [&_button_svg]:size-3.5">
+        <nav v-if="showPrototypeNavBlock" style="--graph-paper-y-offset: 1.75rem;" class="bg-graph-paper [&_button]:rounded-xl [&_button]:w-full [&_button]:font-normal [&_button]:justify-start [&_button]:h-9 [&_button_svg]:size-3.5">
             <ul class="px-0.5 grid gap-8">
                 <li>
                     <h2 class="px-1.5 pb-1.5 text-sm text-gray-950 font-medium">Information</h2>
