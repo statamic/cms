ce2f54e99480fc3c30aa01f2fab18b887fdb558e
Jay George <contact@jaygeorge.co.uk>
2026-04-02 13:16:49 +0100

    Rename some things

--- resources/js/pages/forms/Fields.vue
+++ resources/js/pages/forms/Fields.vue
@@ -20,12 +20,12 @@ const props = defineProps({
         <div style="--graph-paper-y-offset: 4.5rem;" class="bg-graph-paper [&_button]:w-full [&_button>div]:truncate [&_button>div]:block [&_button]:rounded-xl [&_button]:font-normal [&_button]:justify-start [&_button]:h-9 [&_button_svg]:size-3.5">
 
 
-            <div class="form-builder-popover min-[1000px]:hidden">
-                <button class="form-builder-popover__menu-button" id="anchor-form-builder-mobile" popovertarget="popover-form-builder-mobile">
+            <div class="left-panel-popover min-[1000px]:hidden">
+                <button class="left-panel-popover__menu-button" id="anchor-left-panel" popovertarget="popover-left-panel">
                     Form Builder
                 </button>
-                <div id="popover-form-builder-mobile" class="form-builder-popover__menu" popover>
-                    <button class="form-builder-popover__close-button" title="Close" popovertarget="popover-form-builder-mobile">
+                <div id="popover-left-panel" class="left-panel-popover__menu" popover>
+                    <button class="left-panel-popover__close-button" title="Close" popovertarget="popover-left-panel">
                         <svg height="100pt" aria-hidden="true" viewBox="0 0 100 100" width="100pt" xmlns="http://www.w3.org/2000/svg"><path d="m91.668 13.676-5.3398-5.3398-36.328 36.324-36.328-36.324-5.3398 5.3398 36.328 36.324-36.328 36.324 5.3398 5.3398 36.328-36.324 36.328 36.324 5.3398-5.3398-36.328-36.324z"/></svg>
                     </button>
                     <ul style="--graph-paper-y-offset: 4.5rem;" class="bg-graph-paper px-0.5 grid gap-8 @container py-10">
