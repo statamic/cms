8b135cf7bfcae3c5fe7e531772cdb3f9ec07db2a
Nic Bovee <nic@nicbovee.com>
2026-04-08 14:21:06 -0600

    Disable in middleware if elevated sessions are disabled in config.

--- src/Http/Middleware/CP/RequireElevatedSession.php
+++ src/Http/Middleware/CP/RequireElevatedSession.php
@@ -9,7 +9,7 @@ class RequireElevatedSession
 {
     public function handle($request, Closure $next)
     {
-        if (! $request->hasElevatedSession()) {
+        if (! config('statamic.users.elevated_session_disabled') && ! $request->hasElevatedSession()) {
             throw new ElevatedSessionAuthorizationException;
         }
 
