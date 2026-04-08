99e6da1c5d054a72a99d21250795ca0a7a57244b
Nic Bovee <nic@nicbovee.com>
2026-04-08 14:21:00 -0600

    Disable in CP if elevated sessions are disabled in config.

--- src/Http/Controllers/CP/CpController.php
+++ src/Http/Controllers/CP/CpController.php
@@ -72,7 +72,7 @@ class CpController extends Controller
 
     public function requireElevatedSession(): void
     {
-        if (! request()->hasElevatedSession()) {
+        if (! config('statamic.users.elevated_session_disabled') && ! request()->hasElevatedSession()) {
             throw new ElevatedSessionAuthorizationException;
         }
     }
