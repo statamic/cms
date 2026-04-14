de82a0ecf6a02434d3cd6a83a52548f834a76c27
Jay George <contact@jaygeorge.co.uk>
2026-04-14 09:59:14 +0100

    Fix main column - whoops

--- resources/js/pages/layout/Layout.vue
+++ resources/js/pages/layout/Layout.vue
@@ -148,7 +148,7 @@ onUnmounted(() => {
             <!-- The data attribute allows CSS to target elements when max-width is disabled. -->
             <div
                 id="main-content"
-                class="main-content sm:p-2 py-0! h-full flex-wrap overflow-y-auto focus:outline-none rounded-t-2xl"
+                class="main-content py-0! sm:p-2 h-full flex-1 overflow-y-auto focus:outline-none rounded-t-2xl"
                 :class="{
                     'flex': leftPanelActive || rightPanelActive,
                 }"
