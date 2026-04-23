005f16adbf936ac8492c2b80ccb9a221e05aef80
Jay George <contact@jaygeorge.co.uk>
2026-04-23 15:03:44 +0100

    Revert other form views to the standard view. I don't think we need the three panels oustide the edit view.

--- resources/js/pages/forms/Connect.vue
+++ resources/js/pages/forms/Connect.vue
@@ -1,9 +1,8 @@
 <script setup>
 import Layout from '@/pages/layout/Layout.vue';
-import PanelLayout from '@/pages/layout/PanelLayout.vue';
 import FormsLayout from './Layout.vue';
 
-defineOptions({ layout: [Layout, PanelLayout, FormsLayout] });
+defineOptions({ layout: [Layout, FormsLayout] });
 
 const props = defineProps({
     form: Object,
--- resources/js/pages/forms/Logic.vue
+++ resources/js/pages/forms/Logic.vue
@@ -1,10 +1,9 @@
 <script setup>
 import Layout from '@/pages/layout/Layout.vue';
-import PanelLayout from '@/pages/layout/PanelLayout.vue';
 import FormsLayout from './Layout.vue';
 import { Button, Icon } from '@ui';
 
-defineOptions({ layout: [Layout, PanelLayout, FormsLayout] });
+defineOptions({ layout: [Layout, FormsLayout] });
 
 const props = defineProps({
     form: Object,
