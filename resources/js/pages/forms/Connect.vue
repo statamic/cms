16cbe84378d85ce47a9c10045399eb3c51987523
Jay George <contact@jaygeorge.co.uk>
2026-04-23 15:13:18 +0100

    Add basic layouts for Connect and Results

--- resources/js/pages/forms/Connect.vue
+++ resources/js/pages/forms/Connect.vue
@@ -1,16 +1,32 @@
 <script setup>
 import Layout from '@/pages/layout/Layout.vue';
 import FormsLayout from './Layout.vue';
+import { Card, Header, Heading, Panel, PanelHeader, StatusIndicator } from '@ui';
+import { computed } from 'vue';
 
 defineOptions({ layout: [Layout, FormsLayout] });
 
 const props = defineProps({
     form: Object,
 });
+
+const formTitle = computed(() => props.form?.title || __('Untitled Form'));
 </script>
 
 <template>
-    <div class="py-4">
-        <p>TODO: Connect</p>
+    <div class="py-4 mx-auto max-w-5xl">
+        <Header class="mb-2">
+            <template #title>
+                <StatusIndicator status="published" />
+                {{ formTitle }}
+            </template>
+        </Header>
+
+        <Panel>
+            <PanelHeader>
+                <Heading :text="__('Section')" />
+            </PanelHeader>
+            <Card class="min-h-32"></Card>
+        </Panel>
     </div>
 </template>
--- resources/js/pages/forms/Logic.vue
+++ resources/js/pages/forms/Logic.vue
@@ -1,13 +1,16 @@
 <script setup>
 import Layout from '@/pages/layout/Layout.vue';
 import FormsLayout from './Layout.vue';
-import { Button, Icon } from '@ui';
+import { Button, Card, Header, Heading, Icon, Panel, PanelHeader, StatusIndicator } from '@ui';
+import { computed } from 'vue';
 
 defineOptions({ layout: [Layout, FormsLayout] });
 
 const props = defineProps({
     form: Object,
 });
+
+const formTitle = computed(() => props.form?.title || __('Untitled Form'));
 </script>
 
 <template>
@@ -18,7 +21,19 @@ const props = defineProps({
         </Button>
     </Teleport>
 
-    <div class="py-4">
-        <p>TODO: Logic</p>
+    <div class="py-4 mx-auto max-w-5xl">
+        <Header class="mb-2">
+            <template #title>
+                <StatusIndicator status="published" />
+                {{ formTitle }}
+            </template>
+        </Header>
+
+        <Panel>
+            <PanelHeader>
+                <Heading :text="__('Section')" />
+            </PanelHeader>
+            <Card class="min-h-32"></Card>
+        </Panel>
     </div>
 </template>
