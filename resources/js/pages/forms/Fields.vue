eb94cea89fe1f7f49ded5827a060cd7a703f1f49
Jay George <contact@jaygeorge.co.uk>
2026-05-01 20:54:07 +0100

    Paging experiment 2 - spacing tweak

--- resources/js/pages/forms/Fields.vue
+++ resources/js/pages/forms/Fields.vue
@@ -513,7 +513,7 @@ const notificationOptions = [
         </Header>
 
         <div
-            class="mx-auto max-w-5xl max-[600px]:px-5 px-4 mb-4 -mt-2"
+            class="mx-auto max-w-5xl max-[600px]:px-5 px-5.75 sm:px-6.25 mb-4 -mt-2"
             role="separator"
             :aria-label="__('Page :current of :total', { current: 1, total: formPageTotal })"
             data-form-page-label
@@ -817,14 +817,6 @@ const notificationOptions = [
                         </template>
                         <Switch v-model="wantsFreeDrinkVoucher" />
                         </Field>
-
-                        <Button
-                            variant="primary"
-                            @click.prevent
-                            class="hover:cursor-not-allowed border-0! dark:border-0! ring-0! shadow-none!"
-                            style="--theme-color-primary: var(--theme-color-gray-950)"
-                            :text="__('Submit')"
-                        />
                     </div>
                 </Card>
             </div>
@@ -897,6 +889,14 @@ const notificationOptions = [
                             </template>
                             <Textarea id="post-page-note-field" v-model="postPageFinalNote" :rows="3" resize="vertical" />
                         </Field>
+
+                        <Button
+                            variant="primary"
+                            @click.prevent
+                            class="hover:cursor-not-allowed border-0! dark:border-0! ring-0! shadow-none!"
+                            style="--theme-color-primary: var(--theme-color-gray-950)"
+                            :text="__('Submit')"
+                        />
                     </div>
                 </Card>
             </div>
