2a186efddfd86ac578c50c2066e93ab7a55f776f
Duncan McClean <duncan@duncanmcclean.com>
2026-05-11 10:47:58 +0100

    wip

---
+++ resources/js/components/forms/Builder/FieldtypeSelector.vue
@@ -0,0 +1,198 @@
+<script setup>
+import { computed, ref } from 'vue';
+import { mapValues } from 'lodash-es';
+import fuzzysort from 'fuzzysort';
+import { Button, Input } from '@ui';
+
+const props = defineProps({
+    fieldtypes: Array,
+    fieldView: { type: String, default: 'expanded' },
+});
+
+const search = ref('');
+const isSearching = computed(() => search.value.length > 0);
+
+const categories = {
+    structure: {
+        title: __('Structure'),
+        color: 'bg-purple-500',
+    },
+    information: {
+        title: __('Information'),
+        color: 'bg-pink-500',
+    },
+    text: {
+        title: __('Text'),
+        color: 'bg-purple-500',
+    },
+    choice: {
+        title: __('Choice'),
+        color: 'bg-orange-500',
+    },
+    rate: {
+        title: __('Rate'),
+        color: 'bg-amber-500',
+    },
+    contact: {
+        title: __('Contact Info'),
+        color: 'bg-blue-500',
+    },
+    number: {
+        title: __('Number'),
+        color: 'bg-teal-500',
+    },
+    datetime: {
+        title: __('Date and Time'),
+        color: 'bg-fuchsia-500',
+    },
+    media: {
+        title: __('Media'),
+        color: 'bg-cyan-500',
+    },
+    payment: {
+        title: __('Payment'),
+        color: 'bg-green-500',
+    },
+};
+
+const allFieldtypes = computed(() => {
+    let options = [...props.fieldtypes];
+
+    options.push({
+        handle: 'section',
+        title: __('Section'),
+        categories: ['structure'],
+        keywords: [],
+        icon: 'add-section',
+        config: [],
+    });
+
+    // TODO: Only show this when Forms Pro is installed
+    options.push({
+        handle: 'page_break',
+        title: __('Page Break'),
+        categories: ['structure'],
+        keywords: [],
+        icon: 'page',
+        config: [],
+    });
+
+    return options;
+});
+
+const groupedFieldtypes = computed(() => {
+    return mapValues(categories, (category, handle) => {
+        category.handle = handle;
+        category.fieldtypes = [];
+
+        allFieldtypes.value.forEach((fieldtype) => {
+            let categories = fieldtype.categories;
+            if (categories.length === 0) categories = ['other'];
+            if (categories.includes(handle)) category.fieldtypes.push(fieldtype);
+        });
+
+        return category;
+    });
+});
+
+const searchFieldtypes = computed(() => {
+    let options = allFieldtypes.value;
+
+    if (search.value) {
+        return fuzzysort
+            .go(search.value, options, {
+                all: true,
+                keys: ['title', (obj) => obj.categories?.join(), (obj) => obj.keywords?.join()],
+                scoreFn: (scores) => {
+                    const textScore = scores[0]?.score * 1;
+                    const categoriesScore = scores[1]?.score * 0.1;
+                    const keywordsScore = scores[2]?.score * 0.4;
+                    return Math.max(textScore, categoriesScore, keywordsScore);
+                },
+            })
+            .map((result) => result.obj);
+    }
+
+    return options;
+});
+
+const displayedFieldtypes = computed(() => isSearching.value ? [{ fieldtypes: searchFieldtypes.value }] : groupedFieldtypes.value);
+</script>
+
+<template>
+    <div
+        style="--graph-paper-y-offset: 4.5rem;"
+        class="bg-graph-paper [&_button]:w-full [&_button>div]:truncate [&_button>div]:block [&_button]:rounded-xl [&_button]:font-normal [&_button]:justify-start [&_button]:h-9 [&_button_svg]:size-3.5"
+    >
+        <div class="left-panel-popover min-[1000px]:hidden">
+            <div id="popover-left-panel" class="left-panel-popover__menu" popover>
+                <button class="left-panel-popover__close-button" title="Close" popovertarget="popover-left-panel">
+                    <svg height="100pt" aria-hidden="true" viewBox="0 0 100 100" width="100pt" xmlns="http://www.w3.org/2000/svg"><path d="m91.668 13.676-5.3398-5.3398-36.328 36.324-36.328-36.324-5.3398 5.3398 36.328 36.324-36.328 36.324 5.3398 5.3398 36.328-36.324 36.328 36.324 5.3398-5.3398-36.328-36.324z"/></svg>
+                </button>
+                <ul style="--graph-paper-y-offset: 4.5rem;" class="bg-graph-paper px-0.5 grid gap-8 @container py-10 pb-40">
+                    <li
+                        v-for="group in displayedFieldtypes"
+                        :key="group.handle"
+                        v-show="group.fieldtypes.length > 0"
+                    >
+                        <h2
+                            v-if="group.title"
+                            class="inline-flex items-center px-1.5 pb-1 text-sm text-gray-950 dark:text-gray-200 font-medium"
+                            :class="fieldView === 'collapsed' ? 'gap-1.5' : 'gap-0'"
+                        >
+                                <span
+                                    class="h-2 shrink-0 rounded-full"
+                                    :class="{
+                                        [group.color]: true,
+                                        'w-2 opacity-100': fieldView === 'collapsed',
+                                        'w-0 opacity-0': fieldView === 'expanded',
+                                    }"
+                                    aria-hidden="true"
+                                />
+                            {{ group.title }}
+                        </h2>
+                        <ul class="grid gap-2 gap-y-1.75 @min-[250px]:grid-cols-2">
+                            <li v-for="fieldtype in group.fieldtypes" :key="fieldtype.handle">
+                                <Button :text="__(fieldtype.title)" :title="__(fieldtype.title)" :icon="fieldtype.icon" />
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!-- This is the desktop nav - the content is repeated from the left panel -->
+        <div class="px-0.5 pt-6 max-[1000px]:hidden">
+            <Input icon="magnifying-glass" :placeholder="__('Search Field Types...')" v-model="search" />
+
+            <ul class="py-10 grid gap-8 @container">
+                <li
+                    v-for="group in displayedFieldtypes"
+                    :key="group.handle"
+                    v-show="group.fieldtypes.length > 0"
+                >
+                    <h2
+                        v-if="group.title"
+                        class="inline-flex items-center px-1.5 pb-1 text-sm text-gray-950 dark:text-gray-200 font-medium"
+                        :class="fieldView === 'collapsed' ? 'gap-1.5' : 'gap-0'"
+                    >
+                        <span
+                            class="h-2 shrink-0 rounded-full"
+                            :class="{
+                                [group.color]: true,
+                                'w-2 opacity-100': fieldView === 'collapsed',
+                                'w-0 opacity-0': fieldView === 'expanded',
+                            }"
+                            aria-hidden="true"
+                        />
+                        {{ group.title }}
+                    </h2>
+                    <ul class="grid gap-2 gap-y-1.75 @min-[250px]:grid-cols-2">
+                        <li v-for="fieldtype in group.fieldtypes" :key="fieldtype.handle">
+                            <Button :text="__(fieldtype.title)" :title="__(fieldtype.title)" :icon="fieldtype.icon" />
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
