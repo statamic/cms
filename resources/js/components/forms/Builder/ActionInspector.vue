e8095902780459bb53c8341fa49354e1109a55ce
Duncan McClean <duncan@duncanmcclean.com>
2026-05-19 16:07:49 +0100

    wip

--- resources/js/components/forms/Builder/ActionInspector.vue
+++ resources/js/components/forms/Builder/ActionInspector.vue
@@ -15,50 +15,53 @@ const activeTab = ref('settings');
 const isFirstPage = computed(() => pages.value.findIndex((p) => p._id === page.value._id) === 0);
 const isLastPage = computed(() => pages.value.findIndex((p) => p._id === page.value._id) === pages.value.length - 1);
 
-const title = computed(() => isLastPage.value
-    ? __('Submit button')
-    : __('Next Page button'));
+const title = computed(() => isLastPage.value ? __('Submit button') : __('Next Page button'));
+const submitButtonLabel = computed(() => isLastPage.value ? __('Submit Button Label') : __('Next Button Label'));
+const submitButtonPlaceholder = computed(() => isLastPage.value ? __('Submit') : __('Next Page'));
 </script>
 
 <template>
     <div>
-<!--        <div class="right-panel-popover min-[1000px]:hidden">-->
-<!--            <div id="popover-right-panel" class="right-panel-popover__menu" popover>-->
-<!--                <button class="right-panel-popover__close-button" title="Close" popovertarget="popover-right-panel">-->
-<!--                    <svg height="100pt" aria-hidden="true" viewBox="0 0 100 100" width="100pt" xmlns="http://www.w3.org/2000/svg"><path d="m91.668 13.676-5.3398-5.3398-36.328 36.324-36.328-36.324-5.3398 5.3398 36.328 36.324-36.328 36.324 5.3398 5.3398 36.328-36.324 36.328 36.324 5.3398-5.3398-36.328-36.324z"/></svg>-->
-<!--                </button>-->
-<!--                <div class="@container pt-6 pb-40 px-2.5">-->
-<!--                    <Tabs v-model:modelValue="activeTab" :unmount-on-hide="false">-->
-<!--                        <TabList class="inline-flex flex-wrap [&_button]:w-auto! mb-4 mx-0!">-->
-<!--                            <TabTrigger name="settings" :text="__('Settings')" />-->
-<!--                        </TabList>-->
+        <!-- Mobile -->
+        <div class="right-panel-popover min-[1000px]:hidden">
+            <div id="popover-right-panel" class="right-panel-popover__menu" popover>
+                <button class="right-panel-popover__close-button" :title="__('Close')" popovertarget="popover-right-panel">
+                    <svg height="100pt" aria-hidden="true" viewBox="0 0 100 100" width="100pt" xmlns="http://www.w3.org/2000/svg"><path d="m91.668 13.676-5.3398-5.3398-36.328 36.324-36.328-36.324-5.3398 5.3398 36.328 36.324-36.328 36.324 5.3398 5.3398 36.328-36.324 36.328 36.324 5.3398-5.3398-36.328-36.324z"/></svg>
+                </button>
+                <div class="@container pt-6 pb-40 px-2.5">
+                    <Tabs v-model:modelValue="activeTab" :unmount-on-hide="false">
+                        <TabList class="inline-flex flex-wrap [&_button]:w-auto! mb-4 mx-0!">
+                            <TabTrigger :name="ActionInspectorTabs.Settings" :text="__('Settings')" />
+                        </TabList>
 
-<!--                        <TabContent name="settings">-->
-<!--                            <div class="space-y-6 pt-8">-->
-<!--                                <div class="flex items-center gap-2.5">-->
-<!--                                    <div class="size-4">-->
-<!--                                        <Icon name="page" class="size-4 text-gray-500 dark:text-gray-300" />-->
-<!--                                    </div>-->
-<!--                                    <a :href="selectedActionButtonAnchor" class="inline-flex items-center gap-1.5 text-xl font-medium antialiased">-->
-<!--                                        {{ selectedActionButtonHeading }}-->
-<!--                                        <div class="grid *:[grid-area:1/1]">-->
-<!--                                            <Icon name="arrow-up" data-field-direction-up aria-hidden="true" />-->
-<!--                                            <Icon name="arrow-down" data-field-direction-down aria-hidden="true" />-->
-<!--                                        </div>-->
-<!--                                    </a>-->
-<!--                                </div>-->
-<!--                                <Field :label="selectedActionPrimaryLabel">-->
-<!--                                    <Input v-model="selectedActionButtonLabel" />-->
-<!--                                </Field>-->
-<!--                                <Field v-if="inspectorTarget === 'action_next'" :label="__('Previous Button Label')">-->
-<!--                                    <Input v-model="previousPageButtonLabel" />-->
-<!--                                </Field>-->
-<!--                            </div>-->
-<!--                        </TabContent>-->
-<!--                    </Tabs>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--        </div>-->
+                        <TabContent :name="ActionInspectorTabs.Settings">
+                            <div class="space-y-6 pt-8">
+                                <div class="flex items-center gap-2.5">
+                                    <div class="size-4">
+                                        <Icon name="page" class="size-4 text-gray-500 dark:text-gray-300" />
+                                    </div>
+                                    <a :href="`#actions-${page._id}`" class="inline-flex items-center gap-1.5 text-xl font-medium antialiased">
+                                        {{ title }}
+                                        <div class="grid *:[grid-area:1/1]">
+                                            <Icon name="arrow-up" data-field-direction-up aria-hidden="true" />
+                                            <Icon name="arrow-down" data-field-direction-down aria-hidden="true" />
+                                        </div>
+                                    </a>
+                                </div>
+
+                                <Field :label="submitButtonLabel">
+                                    <Input v-model="page.button_label" :placeholder="submitButtonPlaceholder" />
+                                </Field>
+
+                                <Field v-if="!isFirstPage" :label="__('Previous Button Label')">
+                                    <Input v-model="page.previous_page_label"  :placeholder="__('Previous Page')" />
+                                </Field>
+                            </div>
+                        </TabContent>
+                    </Tabs>
+                </div>
+            </div>
+        </div>
 
         <!-- Desktop -->
         <div class="@container relative pt-6 pb-12 px-2.5 pe-4.5 max-[1000px]:hidden">
@@ -82,12 +85,12 @@ const title = computed(() => isLastPage.value
                             </a>
                         </div>
 
-                        <Field :label="isLastPage ? __('Submit Button Label') : __('Next Button Label')">
-                            <Input v-model="page.button_label" :placeholder="isLastPage ? __('Submit') : __('Next Page')" />
+                        <Field :label="submitButtonLabel">
+                            <Input v-model="page.button_label" :placeholder="submitButtonPlaceholder" />
                         </Field>
 
                         <Field v-if="!isFirstPage" :label="__('Previous Button Label')">
-                            <Input v-model="page.previous_page_label" />
+                            <Input v-model="page.previous_page_label" :placeholder="__('Previous Page')" />
                         </Field>
                     </div>
                 </TabContent>
