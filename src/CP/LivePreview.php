151ac7ec88ff7ddf7424e5bd3ae0a70a6d5dd3a4
Duncan McClean <duncan@duncanmcclean.com>
2026-05-21 14:44:20 +0100

    Skip token lookup when token is empty
    Avoids calling Token::find() with an empty or null token value,
    which would otherwise trigger the path traversal protection and
    throw an exception.

--- src/CP/LivePreview.php
+++ src/CP/LivePreview.php
@@ -11,7 +11,8 @@ class LivePreview
 {
     public function tokenize($token, $item): TokenContract
     {
-        $token = Token::find($token) ?? tap(Token::make($token, Handler::class))->save();
+        $token = ($token ? Token::find($token) : null)
+            ?? tap(Token::make($token, Handler::class))->save();
 
         Cache::put('statamic.live-preview.'.$token->token(), $item, now()->addHour());
 
