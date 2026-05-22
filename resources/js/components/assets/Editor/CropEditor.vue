27d1d311f0fd6c44fc09c700657c86d9abde06cf
Jason Varga <jason@pixelfear.com>
2026-05-22 13:04:43 -0400

    use shorthand

--- resources/js/components/assets/Editor/CropEditor.vue
+++ resources/js/components/assets/Editor/CropEditor.vue
@@ -478,7 +478,7 @@ function close() {
                         size="sm"
                         class="w-48"
                         :aria-label="__('Select aspect ratio')"
-                        :adaptive-width="true"
+                        adaptive-width
                         @update:modelValue="setAspectRatio"
                     />
                     <Button
