fa16be035fa3a3f6fadd6a4144fcfb0b178fc443
Jason Varga <jason@pixelfear.com>
2026-05-22 19:43:48 -0400

    wip

--- resources/js/components/fieldtypes/assets/AssetRow.vue
+++ resources/js/components/fieldtypes/assets/AssetRow.vue
@@ -18,7 +18,7 @@
                     :alt="asset.basename"
                     v-if="isImage"
                 />
-                <file-icon :extension="asset.extension" v-else class="w-7 h-7" />
+                <file-icon :extension="asset.extension ?? 'generic'" v-else class="w-7 h-7" />
             </button>
             <button
                 v-if="showFilename"
