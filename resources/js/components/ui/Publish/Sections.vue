21f32da534e473478ae03145b3c45c0b90799793
Jay George <contact@jaygeorge.co.uk>
2026-05-27 16:23:33 +0100

    Fix form box-shadows being clipped by expanding/collapsing sections

--- resources/js/components/ui/Publish/Sections.vue
+++ resources/js/components/ui/Publish/Sections.vue
@@ -117,6 +117,7 @@ function toggleSection(section) {
 .publish-section-collapsible--expanded .publish-section-collapsible__inner {
     animation: calc(var(--speed) * 2) var(--timing) section-fade-in both;
     overflow: clip;
+    overflow-clip-margin: 4px;
 }
 
 .publish-section-collapsible--collapsed .publish-section-collapsible__inner {
