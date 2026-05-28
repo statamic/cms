ef264b7c08590af1d25d957c1b33c3e088d0ab66
Jay George <contact@jaygeorge.co.uk>
2026-05-28 17:08:12 +0100

    Undo some things from earlier since we're not using the indent icon anymore

--- resources/js/components/field-conditions/Builder.vue
+++ resources/js/components/field-conditions/Builder.vue
@@ -168,15 +168,6 @@ onMounted(() => {
                                 :size
                                 inset
                                 variant="subtle"
-                                class="mb-2.5 mt-[0.5px] p-2.5 size-6 ms-0.25 rounded-full [&_div]:-translate-y-[1px] [&_svg]:opacity-80"
-                                icon="indent-right"
-                                :aria-label="__('Indent right')"
-                                :title="__('Indent right')"
-                            />
-                            <Button
-                                :size
-                                inset
-                                variant="subtle"
                                 class="mb-2.5 mt-[0.5px] p-2.5 size-6 ms-0.25 rounded-full [&_div]:-translate-y-[1px]"
                                 :text="'×'"
                                 :aria-label="__('Remove condition')"
