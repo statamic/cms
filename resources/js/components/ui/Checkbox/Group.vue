993bacf58e3a044ce72eca6a1fe50b4e3ee61258
Jay George <contact@jaygeorge.co.uk>
2026-06-01 16:01:46 +0100

    Checkboxes - style chips

--- resources/js/components/ui/Checkbox/Group.vue
+++ resources/js/components/ui/Checkbox/Group.vue
@@ -45,7 +45,7 @@ defineExpose({ focus });
         class="relative block w-full space-y-2"
         :class="{
             'flex flex-wrap space-y-0 gap-x-4 gap-y-2': resolvedAppearance === 'inline' || resolvedAppearance === 'chips',
-            'gap-x-2!': resolvedAppearance === 'chips',
+            'gap-x-2.5!': resolvedAppearance === 'chips',
         }"
         :data-appearance="resolvedAppearance !== 'default' ? resolvedAppearance : undefined"
         data-ui-input
