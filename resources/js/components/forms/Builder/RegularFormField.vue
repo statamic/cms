c5a71adbfa352f85c87c9d1e305fd20bcb360250
Jay George <contact@jaygeorge.co.uk>
2026-06-01 17:43:05 +0100

    Fix hidden field layout

--- resources/js/components/forms/Builder/RegularFormField.vue
+++ resources/js/components/forms/Builder/RegularFormField.vue
@@ -114,7 +114,7 @@ const hasErrors = computed(() => {
                         <span v-if="field.config.validate?.includes('required')" class="relative -top-px ms-0.5 text-red-600">*</span>
                     </span>
                     <Icon v-if="field.type === 'reference'" name="link" class="inline-block size-3! text-indigo-500 dark:text-indigo-400 mb-0.5 ms-2" :aria-label="__('Linked Field')" v-tooltip="__('Linked Field')" />
-                    <Icon v-if="field.config.hidden" name="eye-closed" class="size-3.5! text-gray-400 dark:text-gray-500" :aria-label="__('Hidden')" v-tooltip="__('Hidden')" />
+                    <Icon v-if="field.config.hidden" name="eye-closed" class="inline-block size-3.5! text-gray-400 dark:text-gray-500 mb-0.5 ms-2" :aria-label="__('Hidden')" v-tooltip="__('Hidden')" />
                 </Label>
             </template>
             <div v-if="field.preview" inert>
