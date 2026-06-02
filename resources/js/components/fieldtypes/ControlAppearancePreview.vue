d407ca5dbeed4412a9c7312d4d4d24b11907f6a6
Jay George <contact@jaygeorge.co.uk>
2026-06-02 16:05:00 +0100

    Radio appearance - improve dark mode hover

--- resources/js/components/fieldtypes/ControlAppearancePreview.vue
+++ resources/js/components/fieldtypes/ControlAppearancePreview.vue
@@ -34,12 +34,20 @@ const chipClass = isRadio
             :class="appearance === 'chips' ? `border border-gray-300 shadow-ui-xs dark:border-gray-700 ${chipClass}` : null"
         >
             <div
-                class="shrink-0 border border-gray-500 bg-white dark:border-none dark:bg-gray-500"
+                class="relative flex shrink-0 items-center justify-center border"
                 :class="[
                     isRadio ? 'size-2 rounded-full' : 'size-2 rounded-sm',
-                    selected ? 'border-ui-accent-bg bg-ui-accent-bg/15 dark:border-none dark:bg-gray-300' : null,
+                    selected
+                        ? 'border-ui-accent-bg bg-ui-accent-bg/15 dark:border-none dark:bg-gray-300'
+                        : 'border-gray-400/75 bg-white dark:border-none dark:bg-gray-500',
                 ]"
-            />
+            >
+                <div
+                    v-if="selected"
+                    class="bg-ui-accent-bg/10 dark:bg-ui-accent-bg"
+                    :class="isRadio ? 'size-1 rounded-full' : 'size-1 rounded-sm'"
+                />
+            </div>
             <div
                 class="h-1 rounded-sm bg-gray-300 dark:bg-gray-600"
                 :class="[selected ? 'w-5 dark:bg-gray-500' : 'w-4 dark:bg-gray-700']"
