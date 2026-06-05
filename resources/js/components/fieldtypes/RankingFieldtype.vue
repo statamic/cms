68825ed473f399c7f670e4044326603f0590d33e
Jay George <contact@jaygeorge.co.uk>
2026-06-05 11:55:14 +0100

    Ranking field type - improve appearance

--- resources/js/components/fieldtypes/RankingFieldtype.vue
+++ resources/js/components/fieldtypes/RankingFieldtype.vue
@@ -130,15 +130,15 @@ defineExpose({
                 v-for="row in orderedRows"
                 :key="row.value"
                 :class="sortableItemClass"
-                class="flex items-center gap-2 rounded-lg border border-gray-300 bg-gray-50 p-2 dark:border-gray-600 dark:bg-gray-900"
+                class="flex items-center gap-2 rounded-lg border border-gray-300 bg-white p-2 dark:border-gray-700 dark:bg-gray-900"
             >
                 <ui-drag-handle
                     v-if="!isDisabled"
                     :class="sortableHandleClass"
-                    class="cursor-grab text-gray-400"
+                    class="cursor-grab [&_svg]:opacity-75 dark:[&_svg]:opacity-60"
                 />
                 <span
-                    class="flex size-6 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-white text-xs font-bold text-gray-800 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-200"
+                    class="flex size-6 shrink-0 items-center justify-center rounded-md border border-gray-300 shadow-ui-xs bg-white text-xs font-bold text-gray-800 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-200"
                     aria-hidden="true"
                 >
                     {{ row.index + 1 }}
