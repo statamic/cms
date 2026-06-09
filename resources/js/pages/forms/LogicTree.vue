b6c104c3ed3878a2d22a8234bad9b483f3c1b7d0
Jay George <contact@jaygeorge.co.uk>
2026-06-09 14:06:19 +0100

    Tree view CSS - Get multi columns working

--- resources/js/pages/forms/LogicTree.vue
+++ resources/js/pages/forms/LogicTree.vue
@@ -10,7 +10,7 @@
             <li>
                 Item 2
             </li>
-            <li class="linked-list__linked" style="--destination: --page-2;">
+            <li class="linked-list__linked" style="--destination: --page-3;">
                 Item 3
             </li>
             <li class="linked-list__linked" style="--destination: --page-2;">
@@ -21,6 +21,33 @@
         <ul>
             <li class="linked-list__page-label" style="anchor-name: --page-2;">
                 {{ __('Page :number', { number: 2 }) }}
+            </li>
+            <li>
+                Item 1
+            </li>
+            <li>
+                Item 2
+            </li>
+            <li>
+                Item 3
+            </li>
+            <li>
+                Item 4
+            </li>
+            <li>
+                Item 5
+            </li>
+            <li>
+                Item 6
+            </li>
+            <li>
+                Item 7
+            </li>
+        </ul>
+
+        <ul>
+            <li class="linked-list__page-label" style="anchor-name: --page-3;">
+                {{ __('Page :number', { number: 3 }) }}
             </li>
             <li>
                 Item 1
