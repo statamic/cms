8b63443685d0b1f7cfa42eecc7249a8bb488c568
Jay George <contact@jaygeorge.co.uk>
2026-06-09 17:22:24 +0100

    Tree view CSS - finish tidying

--- resources/js/pages/forms/LogicTree.vue
+++ resources/js/pages/forms/LogicTree.vue
@@ -4,18 +4,18 @@
             <li class="linked-list__page-label" style="anchor-name: --page-1;">
                 {{ __('Page :number', { number: 1 }) }}
             </li>
-            <li class="linked-list__connector" style="--destination: --page-2;">
+            <li class="linked-list__connector" style="--end-connection: --page-2;">
                 Item 1
             </li>
-            <li class="linked-list__connector linked-list__page-leap" style="--destination: --page-3;">
+            <li class="linked-list__connector linked-list__page-leap" style="--end-connection: --page-3;">
                 <div class="linked-list__extra-leap-connector"></div>
                 Item 2
             </li>
-            <li class="linked-list__connector linked-list__page-leap" style="--destination: --page-3;">
+            <li class="linked-list__connector linked-list__page-leap" style="--end-connection: --page-3;">
                 <div class="linked-list__extra-leap-connector"></div>
                 Item 3
             </li>
-            <li class="linked-list__connector" style="--destination: --page-2;">
+            <li class="linked-list__connector" style="--end-connection: --page-2;">
                 Item 4
             </li>
         </ul>
