eccae060a3274b43a91c467ede2732a82a3435b0
Jay George <contact@jaygeorge.co.uk>
2026-06-09 11:01:40 +0100

    Tree view CSS - experiment with radios at the top

--- resources/js/pages/forms/LogicTree.vue
+++ resources/js/pages/forms/LogicTree.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { onMounted, ref } from 'vue';
 
-// https://codepen.io/cbolson/pen/emzegWP — JS only sets --relation; connectors are pure CSS anchor positioning
+// https://codepen.io/cbolson/pen/emzegWP
 const root = ref(null);
 const REL = ['-1', '0', '1'];
 
@@ -9,13 +9,13 @@ function updateRelations() {
     const rootEl = root.value;
     if (!rootEl) return;
 
-    const radioLi = rootEl.querySelector('.options li:has(input:checked)');
+    const radioLi = rootEl.querySelector('.options li:has(input[type=radio]:checked)');
     if (!radioLi) return;
 
     const radioIndex = [...radioLi.parentElement.children].indexOf(radioLi);
 
-    rootEl.querySelectorAll('.items li').forEach((li, i) => {
-        li.style.setProperty('--relation', REL[Math.sign(i - radioIndex) + 1]);
+    rootEl.querySelectorAll('.items li:has(input[type=checkbox]:checked)').forEach((li) => {
+        const i = [...li.parentElement.children].indexOf(li);
         li.dataset.relation = REL[Math.sign(i - radioIndex) + 1];
     });
 }
@@ -24,78 +24,84 @@ onMounted(updateRelations);
 </script>
 
 <template>
-    <!-- https://codepen.io/cbolson/pen/emzegWP -->
-    <div id="linked-list" ref="root" class="linked-list" @change="updateRelations">
-        <ul class="list items" aria-labelledby="items-label">
-            <li>
-                <label for="logic-tree-item-1">
-                    <input id="logic-tree-item-1" type="checkbox" class="cb" value="1" checked>
-                    Item 1
-                </label>
-            </li>
-            <li>
-                <label for="logic-tree-item-2">
-                    <input id="logic-tree-item-2" type="checkbox" class="cb" value="2">
-                    Item 2
-                </label>
-            </li>
-            <li>
-                <label for="logic-tree-item-3">
-                    <input id="logic-tree-item-3" type="checkbox" class="cb" value="3" checked>
-                    Item 3
-                </label>
-            </li>
-            <li>
-                <label for="logic-tree-item-4">
-                    <input id="logic-tree-item-4" type="checkbox" class="cb" value="4" checked>
-                    Item 4
-                </label>
-            </li>
-        </ul>
+  <div id="linked-list" ref="root" class="linked-list" @change="updateRelations">
+    <ul class="list items">
+      <li class="linked-list__page-label">{{ __('Page :number', { number: 1 }) }}</li>
+      <li>
+        <label for="logic-tree-item-1">
+          <input id="logic-tree-item-1" type="checkbox" class="cb" value="1" checked>
+          Item 1
+        </label>
+      </li>
+      <li>
+        <label for="logic-tree-item-2">
+          <input id="logic-tree-item-2" type="checkbox" class="cb" value="2">
+          Item 2
+        </label>
+      </li>
+      <li>
+        <label for="logic-tree-item-3">
+          <input id="logic-tree-item-3" type="checkbox" class="cb" value="3" checked>
+          Item 3
+        </label>
+      </li>
+      <li>
+        <label for="logic-tree-item-4">
+          <input id="logic-tree-item-4" type="checkbox" class="cb" value="4" checked>
+          Item 4
+        </label>
+      </li>
+    </ul>
 
-        <ul class="list options" role="radiogroup" aria-labelledby="visit-label">
-            <li>
-                <label for="logic-tree-radio-1">
-                    <input id="logic-tree-radio-1" type="radio" name="logic-tree-visit" class="rb" value="1">
-                    Option 1
-                </label>
-            </li>
-            <li>
-                <label for="logic-tree-radio-2">
-                    <input id="logic-tree-radio-2" type="radio" name="logic-tree-visit" class="rb" value="2">
-                    Option 2
-                </label>
-            </li>
-            <li>
-                <label for="logic-tree-radio-3">
-                    <input id="logic-tree-radio-3" type="radio" name="logic-tree-visit" class="rb" value="3">
-                    Option 3
-                </label>
-            </li>
-            <li>
-                <label for="logic-tree-radio-4">
-                    <input id="logic-tree-radio-4" type="radio" name="logic-tree-visit" class="rb" value="4">
-                    Option 4
-                </label>
-            </li>
-            <li>
-                <label for="logic-tree-radio-5">
-                    <input id="logic-tree-radio-5" type="radio" name="logic-tree-visit" class="rb" value="5">
-                    Option 5
-                </label>
-            </li>
-            <li>
-                <label for="logic-tree-radio-6">
-                    <input id="logic-tree-radio-6" type="radio" name="logic-tree-visit" class="rb" value="6" checked>
-                    Option 6
-                </label>
-            </li>
-            <li>
-                <label for="logic-tree-radio-7">
-                    <input id="logic-tree-radio-7" type="radio" name="logic-tree-visit" class="rb" value="7">
-                    Option 7
-                </label>
-            </li>
-        </ul>
-    </div>
+    <ul class="list options" role="radiogroup" :aria-label="__('Destination page')">
+      <li class="linked-list__page-label">
+        <label for="logic-tree-page-2">
+          <input id="logic-tree-page-2" type="radio" name="logic-tree-destination" class="rb" value="2" checked>
+          {{ __('Page :number', { number: 2 }) }}
+        </label>
+      </li>
+      <li>
+        <label for="logic-tree-page-2-item-1">
+          <input id="logic-tree-page-2-item-1" type="checkbox" class="cb" value="1">
+          Item 1
+        </label>
+      </li>
+      <li>
+        <label for="logic-tree-page-2-item-2">
+          <input id="logic-tree-page-2-item-2" type="checkbox" class="cb" value="2">
+          Item 2
+        </label>
+      </li>
+      <li>
+        <label for="logic-tree-page-2-item-3">
+          <input id="logic-tree-page-2-item-3" type="checkbox" class="cb" value="3">
+          Item 3
+        </label>
+      </li>
+      <li>
+        <label for="logic-tree-page-2-item-4">
+          <input id="logic-tree-page-2-item-4" type="checkbox" class="cb" value="4">
+          Item 4
+        </label>
+      </li>
+      <li>
+        <label for="logic-tree-page-2-item-5">
+          <input id="logic-tree-page-2-item-5" type="checkbox" class="cb" value="5">
+          Item 5
+        </label>
+      </li>
+      <li>
+        <label for="logic-tree-page-2-item-6">
+          <input id="logic-tree-page-2-item-6" type="checkbox" class="cb" value="6">
+          Item 6
+        </label>
+      </li>
+      <li>
+        <label for="logic-tree-page-2-item-7">
+          <input id="logic-tree-page-2-item-7" type="checkbox" class="cb" value="7">
+          Item 7
+        </label>
+      </li>
+    </ul>
+  </div>
 </template>
