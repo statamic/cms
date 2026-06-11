38d9227fea1a588da04bb514fb32db037d5c25de
Duncan McClean <duncan@duncanmcclean.com>
2026-06-11 10:19:23 +0100

    "generate" opinion scale values in php

--- src/Fieldtypes/OpinionScale.php
+++ src/Fieldtypes/OpinionScale.php
@@ -7,4 +7,16 @@ use Statamic\Fields\Fieldtype;
 class OpinionScale extends Fieldtype
 {
     protected $selectable = false;
+
+    public function preload()
+    {
+        return [
+            'scaleValues' => $this->scaleValues(),
+        ];
+    }
+
+    private function scaleValues(): array
+    {
+        return range($this->config('min'), $this->config('max'));
+    }
 }
