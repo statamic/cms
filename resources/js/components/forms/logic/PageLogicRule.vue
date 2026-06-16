cc39fd35c58b5e6b913cc5e0b02b26f0df64c5d0
Jay George <contact@jaygeorge.co.uk>
2026-06-16 16:53:29 +0100

    Logic rule formatting

--- resources/js/components/forms/logic/PageLogicRule.vue
+++ resources/js/components/forms/logic/PageLogicRule.vue
@@ -131,7 +131,7 @@ const toggleCollapsedState = () => props.collapsed ? emit('expanded') : emit('co
                 }"
             >
                 <button type="button" class="show-focus-within_target flex flex-1 items-center gap-1.75 p-2 py-1.75 ps-0 min-w-0 focus:outline-none cursor-pointer" @click="toggleCollapsedState">
-                    <Badge v-if="collapsed" pill color="white" class="px-1.5 font-medium text-gray-800 dark:text-gray-200">
+                    <Badge v-if="collapsed" pill size="sm" color="white" class="font-medium text-gray-800 dark:text-gray-200">
                         {{ __('If') }}
                     </Badge>
                     <Badge v-if="collapsed && firstFieldConfig" size="lg" pill color="white" class="px-3 text-gray-950 gap-1">
@@ -152,19 +152,17 @@ const toggleCollapsedState = () => props.collapsed ? emit('expanded') : emit('co
                                 <Badge
                                     v-if="part.type === 'operator'"
                                     pill
-                                    size="sm"
-                                    color="white"
-                                    class="px-1.5 font-medium bg-gray-100 text-gray-700 dark:bg-gray-800 dark:text-gray-300"
+                                    class="inline-block px-1.5 font-medium lowercase bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-300"
+                                    style="text-box: trim-start text;"
                                 >
                                     {{ part.text }}
                                 </Badge>
-                                <span v-else-if="part.type === 'value'" class="text-gray-900 dark:text-gray-100">{{ part.text }}</span>
+                                <span v-else-if="part.type === 'value'" class="font-mono text-[0.75rem] text-gray-900 dark:text-gray-100">{{ part.text }}</span>
                                 <Badge
                                     v-else-if="part.type === 'join'"
                                     pill
-                                    size="sm"
-                                    color="white"
-                                    class="px-1.5 font-medium text-gray-700 dark:text-gray-300"
+                                    class="inline-block px-1.5 font-medium lowercase bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-300"
+                                    style="text-box: trim-start text;"
                                 >
                                     {{ part.text }}
                                 </Badge>
@@ -172,9 +170,8 @@ const toggleCollapsedState = () => props.collapsed ? emit('expanded') : emit('co
                                 <Badge
                                     v-else-if="part.type === 'goto'"
                                     pill
-                                    size="sm"
-                                    color="white"
-                                    class="px-1.5 font-medium text-gray-700 dark:text-gray-300"
+                                    class="inline-block px-1.5 font-medium lowercase bg-white dark:bg-gray-800"
+                                    style="text-box: trim-start text;"
                                 >
                                     {{ part.text }}
                                 </Badge>
