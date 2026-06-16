b964cfa92ed4479947f0e997b34d4ccaa935919a
Duncan McClean <duncan@duncanmcclean.com>
2026-06-16 08:53:24 +0100

    move styles to the bottom of the templates so you see the <input> first

--- resources/views/extend/forms/blade/fields/star_rating.blade.php
+++ resources/views/extend/forms/blade/fields/star_rating.blade.php
@@ -1,3 +1,30 @@
+@php
+    $min = $min ?? 1;
+    $ratingValue = $value ?? 0;
+    $isUnrated = $ratingValue == 0;
+@endphp
+
+<input
+    id="{{ $id }}"
+    type="range"
+    name="{{ $name }}"
+    @class(['star-rating-input', 'star-rating-input--unrated' => $isUnrated])
+    data-star-rating
+    min="{{ $min }}"
+    max="{{ $max_stars ?? 5 }}"
+    step="{{ $step ?? 1 }}"
+    value="{{ $isUnrated ? $min : $ratingValue }}"
+    style="--star-rating-max: {{ $max_stars ?? 5 }}; --star-rating-step: {{ $step ?? 1 }};"
+    @if (isset($js_driver)) {!! $js_attributes !!} @endif
+    @required(in_array('required', $validate ?? []))
+    @if (! empty($display)) aria-label="{{ $display }}" @endif
+    @if ($error)
+        aria-invalid="true" aria-describedby="{{ $id }}-error"
+    @elseif ($instructions)
+        aria-describedby="{{ $id }}-instructions"
+    @endif
+>
+
 @once
     <style>
         .star-rating-input {
@@ -98,30 +125,3 @@
         }
     </style>
 @endonce
-
-@php
-    $min = $min ?? 1;
-    $ratingValue = $value ?? 0;
-    $isUnrated = $ratingValue == 0;
-@endphp
-
-<input
-    id="{{ $id }}"
-    type="range"
-    name="{{ $name }}"
-    @class(['star-rating-input', 'star-rating-input--unrated' => $isUnrated])
-    data-star-rating
-    min="{{ $min }}"
-    max="{{ $max_stars ?? 5 }}"
-    step="{{ $step ?? 1 }}"
-    value="{{ $isUnrated ? $min : $ratingValue }}"
-    style="--star-rating-max: {{ $max_stars ?? 5 }}; --star-rating-step: {{ $step ?? 1 }};"
-    @if (isset($js_driver)) {!! $js_attributes !!} @endif
-    @required(in_array('required', $validate ?? []))
-    @if (! empty($display)) aria-label="{{ $display }}" @endif
-    @if ($error)
-        aria-invalid="true" aria-describedby="{{ $id }}-error"
-    @elseif ($instructions)
-        aria-describedby="{{ $id }}-instructions"
-    @endif
->
