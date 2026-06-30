e67245b1a09fdc3833f7e1dc04f074f03fa45f27
Jay George <contact@jaygeorge.co.uk>
2026-06-30 14:22:29 +0100

    Image Choice Field Type - start

--- resources/js/pages/forms/Submissions.vue
+++ resources/js/pages/forms/Submissions.vue
@@ -597,7 +597,31 @@ function exportSubmissions() {
                             icon="image-select"
                             icon-class="size-4 text-gray-500 hidden @xs/widget:block"
                         >
-                            
+                            <figure class="p-6 grid" :aria-labelledby="imageChoiceBarChart1CaptionId">
+                                <ol class="m-0 list-none grid grid-cols-[auto_auto_max-content_1fr] items-center gap-2.25 p-0 pt-4">
+                                    <li class="contents">
+                                        <span class="text-xs font-medium tabular-nums text-gray-700 dark:text-gray-400" aria-hidden="true">55%</span>
+                                        <span class="size-2.5 rounded-xs bg-chart-1" />
+                                        <span class="truncate max-w-25 me-2 text-xs text-gray-900 dark:text-gray-100">Yep</span>
+                                        <div class="h-2.5 rounded-full w-[55%] bg-chart-1" />
+                                    </li>
+                                    <li class="contents">
+                                        <span class="text-xs font-medium tabular-nums text-gray-700 dark:text-gray-400" aria-hidden="true">35%</span>
+                                        <span class="size-2.5 rounded-xs bg-chart-2" />
+                                        <span class="truncate max-w-25 me-2 text-xs text-gray-900 dark:text-gray-100">Nope</span>
+                                        <div class="h-2.5 rounded-full w-[35%] bg-chart-2" />
+                                    </li>
+                                    <li class="contents">
+                                        <span class="text-xs font-medium tabular-nums text-gray-700 dark:text-gray-400" aria-hidden="true">10%</span>
+                                        <span class="size-2.5 rounded-xs bg-chart-3" />
+                                        <span class="truncate max-w-25 me-2 text-xs text-gray-900 dark:text-gray-100">Maybe</span>
+                                        <div class="h-2.5 rounded-full w-[10%] bg-chart-3" />
+                                    </li>
+                                </ol>
+                                <figcaption :id="imageChoiceBarChart1CaptionId" class="sr-only">
+                                    {{ __('Have you seen us live before?: Yep 55%, Nope 35%, Maybe 10%') }}
+                                </figcaption>
+                            </figure>
                         </Widget>
                     </div>
                 </div>
