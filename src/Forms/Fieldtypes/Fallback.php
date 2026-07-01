ffde83e310347b07a2add1c9faa72496bbd1015e
Duncan McClean <duncan@duncanmcclean.com>
2026-07-01 15:19:22 +0100

    don't inherit categories from wrapped fieldtype. they're intentionally separate

--- src/Forms/Fieldtypes/Fallback.php
+++ src/Forms/Fieldtypes/Fallback.php
@@ -51,19 +51,6 @@ class Fallback extends FormFieldtype
         return $this->wrappedFieldtype->icon();
     }
 
-    public function categories(): array
-    {
-        if (! $this->wrappedFieldtype) {
-            return parent::categories();
-        }
-
-        if ($equivalent = $this->equivalentFormFieldtype()) {
-            return $equivalent->categories();
-        }
-
-        return $this->wrappedFieldtype->categories();
-    }
-
     public function toArray(): array
     {
         if (! $this->wrappedFieldtype) {
