70bcdbb7cce2ef5b21355233b0d03e3e2f450bfd
Duncan McClean <duncan@duncanmcclean.com>
2026-07-02 14:42:10 +0100

    add numbered fields toggle to submission publish form
    it does blink slightly when you toggle it because it has to remount the PublishContainer component.
    
    to avoid the blink, we'd need to swap PublishForm for using PublishContainer directly, then add slots in PublishFields and  PublishField.
    
    it's not a major issue so going to keep it simple for now w/ the blink. if someone complains we can change it later.

--- resources/js/pages/forms/Submission.vue
+++ resources/js/pages/forms/Submission.vue
@@ -7,6 +7,8 @@ import Layout from '@/pages/layout/Layout.vue';
 import PanelLayout from '@/pages/layout/PanelLayout.vue';
 import FormsLayout from '@/pages/forms/Layout.vue';
 import SubmissionStatusIndicator from '@/components/forms/SubmissionStatusIndicator.vue';
+import FieldNumberingToggle from '@/components/forms/FieldNumberingToggle.vue';
+import { useFieldNumberingPreference } from '@/composables/forms/field-numbering';
 
 defineOptions({ layout: [Layout, PanelLayout, FormsLayout] });
 
@@ -21,8 +23,30 @@ const props = defineProps([
     'meta',
 ]);
 
+const { showFieldNumbers } = useFieldNumberingPreference();
+
 const formattedDate = computed(() => dateFormatter.format(props.date));
 const title = computed(() => `${__('Form Submission')} ${props.id}`);
+
+const numberedBlueprint = computed(() => {
+    if (!showFieldNumbers.value) return props.blueprint;
+
+    let number = 0;
+
+    return {
+        ...props.blueprint,
+        tabs: (props.blueprint.tabs || []).map((tab) => ({
+            ...tab,
+            sections: (tab.sections || []).map((section) => ({
+                ...section,
+                fields: (section.fields || []).map((field) => ({
+                    ...field,
+                    display: `${++number}. ${field.display ?? field.handle}`,
+                })),
+            })),
+        })),
+    };
+});
 
 provide('isFormSubmission', true);
 </script>
@@ -32,8 +56,9 @@ provide('isFormSubmission', true);
         <Head :title="[title, __(formTitle), __('Forms')]" />
 
         <PublishForm
+            :key="showFieldNumbers ? 'numbered' : null"
             :title="formattedDate"
-            :blueprint="blueprint"
+            :blueprint="numberedBlueprint"
             :initial-values="values"
             :initial-meta="meta"
             :submit-url="null"
@@ -42,6 +67,9 @@ provide('isFormSubmission', true);
             <template #title>
                 <SubmissionStatusIndicator :status="status" />
                 {{ formattedDate }}
+            </template>
+            <template #actions>
+                <FieldNumberingToggle />
             </template>
         </PublishForm>
     </div>
