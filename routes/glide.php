492e8321739f9204191750edb282a9a2a0b51384
Jason Varga <jason@pixelfear.com>
2026-07-02 13:33:28 -0400

    trim in the map

--- routes/glide.php
+++ routes/glide.php
@@ -7,9 +7,9 @@ use Statamic\Facades\URL;
 use Statamic\Http\Controllers\GlideController;
 
 Site::all()->map(function ($site) {
-    return URL::makeRelative($site->url());
+    return trim(URL::makeRelative($site->url()), '/');
 })->unique()->each(function ($sitePrefix) {
-    Route::group(['prefix' => trim($sitePrefix, '/').'/'.Glide::route()], function () {
+    Route::group(['prefix' => $sitePrefix.'/'.Glide::route()], function () {
         Route::get('/asset/{container}/{path?}', [GlideController::class, 'generateByAsset'])->where('path', '.*');
         Route::get('/http/{url}/{filename?}', [GlideController::class, 'generateByUrl']);
         Route::get('{path}', [GlideController::class, 'generateByPath'])->where('path', '.*');
