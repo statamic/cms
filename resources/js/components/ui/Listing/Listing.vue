73b69e5d024f5bd5197c1ed04a1b17c57d52a0bb
Jay George <contact@jaygeorge.co.uk>
2026-07-10 14:26:34 +0100

    Prevent vertical focus clipping on listing pages like /forms by adding a touch of vertical padding

--- resources/js/components/ui/Listing/Listing.vue
+++ resources/js/components/ui/Listing/Listing.vue
@@ -732,7 +732,7 @@ autoApplyState();
         <slot v-if="!initializing" :items="items" :is-column-visible="isColumnVisible" :loading="loading">
             <Presets v-if="showPresets" />
             <div v-if="allowSearch || hasFilters || allowCustomizingColumns" class="relative overflow-clip flex items-center gap-2 sm:gap-3 min-h-16 starting-style-transition st-overflow-clip-margin">
-                <div class="flex flex-1 items-center gap-2 sm:gap-3 overflow-x-auto -ms-1 ps-1">
+                <div class="flex flex-1 items-center gap-2 sm:gap-3 overflow-x-auto -ms-1 ps-1 py-1">
                     <Search v-if="allowSearch" />
                     <Filters v-if="hasFilters" />
                 </div>
