7a59b134544acee1b82765f0177297b7498d84cb
Jay George <contact@jaygeorge.co.uk>
2026-07-14 11:54:45 +0100

    Align empty message with top row

--- resources/js/components/forms/builder/EmptyInspector.vue
+++ resources/js/components/forms/builder/EmptyInspector.vue
@@ -4,7 +4,7 @@
 
 <template>
 <div class="@container relative pt-6 pb-40 max-[1000px]:pb-12 px-2.5 pe-4.5">
-    <div class="space-y-6 pt-8">
+    <div class="space-y-6 pt-5">
         <div class="flex items-center gap-2">
             <div class="size-4">
                 <Icon name="forms" class="size-4 text-gray-500 dark:text-gray-300" />
