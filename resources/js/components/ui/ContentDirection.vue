5240d81533120d091c44b5b0452fb864c5a71249
Jason Varga <jason@pixelfear.com>
2026-07-15 17:32:39 -0400

    Add useContentDirection composable and ContentDirection component

---
+++ resources/js/components/ui/ContentDirection.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { useContentDirection } from '@/composables/content-direction';
+
+const { direction } = useContentDirection();
+</script>
+
+<template>
+    <div :dir="direction">
+        <slot />
+    </div>
+</template>
