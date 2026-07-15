07bc4f99dc01077ed667627956f14be07da20bf7
Jay George <contact@jaygeorge.co.uk>
2026-07-15 13:26:41 +0100

    Tweak empty message decoration slightly, ready for a larger sidebar with blueprints

--- resources/js/components/forms/builder/EmptyInspector.vue
+++ resources/js/components/forms/builder/EmptyInspector.vue
@@ -4,7 +4,7 @@ import { Icon } from '@ui';
 
 <template>
     <div class="@container relative pt-6 pb-40 max-[1000px]:pb-12 px-2.5 pe-4.5">
-        <div class="space-y-6 pt-5">
+        <div class="space-y-4.5 pt-5 px-2 @xs:px-4">
             <div class="flex items-center gap-2">
                 <div class="size-4">
                     <Icon name="forms" class="size-4 text-gray-500 dark:text-gray-300" />
@@ -13,7 +13,7 @@ import { Icon } from '@ui';
                     <span class="truncate">{{ __('Edit this form') }}</span>
                 </span>
             </div>
-            <p class="text-zinc-600 dark:text-zinc-400">{{ __('statamic::messages.form_builder_empty_inspector') }}</p>
+            <p class="text-zinc-600 dark:text-zinc-400 text-[0.95rem] text-pretty">{{ __('statamic::messages.form_builder_empty_inspector') }}</p>
         </div>
     </div>
 </template>
