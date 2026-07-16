6a2a69bcf734a50dca9972eeedc03f6af83839ae
Jason Varga <jason@pixelfear.com>
2026-07-16 11:35:06 -0400

    Make Tabs primitive follow UI direction by default
    reka-ui's TabsRoot defaults to dir="ltr", forcing the tab-panel
    subtree LTR even when the CP is RTL. Default the dir prop to
    useUiDirection()'s direction while keeping it overridable.

--- resources/js/components/ui/Tabs/Tabs.vue
+++ resources/js/components/ui/Tabs/Tabs.vue
@@ -1,18 +1,26 @@
 <script setup>
+import { computed } from 'vue';
 import { TabsRoot } from 'reka-ui';
+import { useUiDirection } from '@/composables/ui-direction';
 
 const emit = defineEmits(['update:modelValue']);
 
-defineProps({
+const props = defineProps({
     /** The controlled value of the tabs */
     modelValue: { type: String, default: null },
     /** When `true`, the element will be unmounted on closed state */
     unmountOnHide: { type: Boolean, default: true },
+    /** The reading direction of the tabs */
+    dir: { type: String, default: null },
 });
+
+const { direction } = useUiDirection();
+
+const dir = computed(() => props.dir ?? direction.value);
 </script>
 
 <template>
-    <TabsRoot :model-value="modelValue" @update:model-value="emit('update:modelValue', $event)" :unmount-on-hide>
+    <TabsRoot :model-value="modelValue" @update:model-value="emit('update:modelValue', $event)" :unmount-on-hide :dir>
         <slot />
     </TabsRoot>
 </template>
