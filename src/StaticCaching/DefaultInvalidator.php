8dd409d55296c1ddc84c7d3fd1551197bd5232a8
Marco Rieser <marco@rieser.dev>
2026-07-18 19:13:14 +0200

    Make DefaultInvalidator refresh() exception-safe and reentrant

--- src/StaticCaching/DefaultInvalidator.php
+++ src/StaticCaching/DefaultInvalidator.php
@@ -22,6 +22,9 @@ class DefaultInvalidator implements Invalidator
 {
     protected $cacher;
     protected $rules;
+
+    // Read by invalidate() to pick refreshUrls() over invalidateUrls()/flush(). Subclasses
+    // overriding invalidate() must check this themselves to get refresh-vs-invalidate semantics.
     protected $refreshing = false;
 
     public function __construct(Cacher $cacher, $rules = [])
@@ -55,11 +58,14 @@ class DefaultInvalidator implements Invalidator
             return;
         }
 
+        $previous = $this->refreshing;
         $this->refreshing = true;
 
-        $this->invalidate($item);
-
-        $this->refreshing = false;
+        try {
+            $this->invalidate($item);
+        } finally {
+            $this->refreshing = $previous;
+        }
     }
 
     protected function getItemUrls($item)
