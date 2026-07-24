da8a8eee98278c1f3f19636dc2fcbef802eba0de
Philipp Daun <post@philippdaun.net>
2026-07-24 17:59:10 +0200

    Create API auth exception class

---
+++ src/Exceptions/ApiAuthenticationException.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Statamic\Exceptions;
+
+use Illuminate\Auth\AuthenticationException as Exception;
+use Illuminate\Contracts\Support\Responsable;
+
+class ApiAuthenticationException extends Exception implements Responsable
+{
+    public function toResponse($request)
+    {
+        return response()->json(
+            ['message' => $this->getMessage()],
+            401,
+        );
+    }
+}
