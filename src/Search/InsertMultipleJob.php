2d86c9533442efefeb41beb754033b3fe6e31988
edalzell <erin@silentz.co>
2026-07-30 22:57:13 -0400

    try this?

--- src/Search/InsertMultipleJob.php
+++ src/Search/InsertMultipleJob.php
@@ -36,7 +36,7 @@ class InsertMultipleJob implements ShouldQueue
         $this->onConnection($connection = config('statamic.search.queue_connection', config('queue.default')));
         $this->onQueue(config('statamic.search.queue', config("queue.connections.{$connection}.queue")));
 
-        $this->timeout = config()->integer('statamic.search.queue_timeout', null);
+        $this->timeout = config()->integer('statamic.search.queue_timeout', 0);
     }
 
     /**
