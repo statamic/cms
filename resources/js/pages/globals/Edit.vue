32afec855e22babbe4102191673805e4224c7057
Jack McDade <jack@jackmcdade.com>
2026-08-14 15:21:45 -0400

    Go full-width as long as we're at it

--- resources/js/pages/globals/Edit.vue
+++ resources/js/pages/globals/Edit.vue
@@ -29,7 +29,7 @@ defineProps([
 </script>
 
 <template>
-    <div class="max-w-5xl 3xl:max-w-6xl mx-auto" data-max-width-wrapper>
+    <div :class="asConfig ? 'max-w-page mx-auto' : 'max-w-5xl 3xl:max-w-6xl mx-auto'" data-max-width-wrapper>
         <Head :title="__('Edit Global Set')" />
 
         <GlobalPublishForm
