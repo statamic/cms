e2bb3c5c6c5f81735b1d2c3ad72b52ce5de010f7
Jay George <github@jaygeorge.co.uk>
2026-08-17 16:26:24 -0400

    [6.x] Multisite - turn stacked sites into a table view for larger viewports (#15202)

--- src/Sites/Sites.php
+++ src/Sites/Sites.php
@@ -254,6 +254,10 @@ class Sites
 
         // If multisite, nest fields in a grid
         if ($this->multiEnabled()) {
+            $tableWidths = [
+                'attributes' => 30,
+            ];
+
             $siteFields = [
                 [
                     'handle' => 'sites',
@@ -262,9 +266,15 @@ class Sites
                         'hide_display' => true,
                         'actions' => false,
                         'fullscreen' => false,
-                        'mode' => 'stacked',
+                        'mode' => 'table',
+                        'stack_at' => 925,
                         'add_row' => __('Add Site'),
-                        'fields' => $siteFields,
+                        'fields' => collect($siteFields)->map(function ($field) use ($tableWidths) {
+                            $field['field']['width'] = $tableWidths[$field['handle']] ?? 14;
+                            $field['field']['classes'] = 'max-w-48 min-w-0 overflow-hidden';
+
+                            return $field;
+                        })->all(),
                         'required' => true,
                     ],
                 ],
