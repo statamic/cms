20d8a5a607c804949432c8c7f88685b8e4a8d657
Jay George <contact@jaygeorge.co.uk>
2026-08-20 13:25:39 +0100

    Change the border radius to compensate for custom scrollbar

--- resources/js/components/ui/Combobox/Combobox.vue
+++ resources/js/components/ui/Combobox/Combobox.vue
@@ -461,7 +461,7 @@ defineExpose({
                         :side-offset="5"
                         position="popper"
                         :class="[
-                            'shadow-ui-sm z-(--z-index-above) rounded-lg border border-gray-200 bg-white dark:border-white/10 dark:bg-gray-800',
+                            'shadow-ui-sm z-(--z-index-above) rounded-md !rounded-se-sm border border-gray-200 bg-white dark:border-white/10 dark:bg-gray-800',
                             'max-h-[var(--reka-combobox-content-available-height)] min-w-[var(--reka-combobox-trigger-width)] overflow-hidden',
                             adaptiveWidth && 'w-max max-w-md',
                         ]"
