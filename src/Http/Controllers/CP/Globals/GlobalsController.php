baa594c54ae8dfe89b9a3ebde61ba755f6aaae11
Jay George <contact@jaygeorge.co.uk>
2026-08-24 16:06:39 +0100

    Use existing Localizable label for globals sites section
    Avoid a new Localization string; Localizable is already translated across locales.

--- src/Http/Controllers/CP/Globals/GlobalsController.php
+++ src/Http/Controllers/CP/Globals/GlobalsController.php
@@ -225,7 +225,7 @@ class GlobalsController extends CpController
 
         if (Site::multiEnabled()) {
             $fields['sites'] = [
-                'display' => __('Localization'),
+                'display' => __('Localizable'),
                 'fields' => [
                     'sites' => [
                         'type' => 'global_set_sites',
