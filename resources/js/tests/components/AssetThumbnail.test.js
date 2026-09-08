9ce64f69b3f5c3c3b72b796245493c2658c9a51d
Jack McDade <jack@jackmcdade.com>
2026-09-07 20:09:27 -0400

    Add alt to the browser table and asset index fieldtype asset thumbnails

--- resources/js/tests/components/AssetThumbnail.test.js
+++ resources/js/tests/components/AssetThumbnail.test.js
@@ -1,12 +1,13 @@
 import { mount } from '@vue/test-utils';
 import { expect, test } from 'vitest';
 import Thumbnail from '@/components/assets/Browser/Thumbnail.vue';
+import AssetsIndexFieldtype from '@/components/fieldtypes/assets/AssetsIndexFieldtype.vue';
 
 const stubs = { 'file-icon': true };
 
-test('a raster thumbnail is decorative by default', () => {
+test('a raster thumbnail can be decorative', () => {
     const img = mount(Thumbnail, {
-        props: { asset: { thumbnail: '/cp/thumbnails/abc/small' } },
+        props: { asset: { thumbnail: '/cp/thumbnails/abc/small' }, alt: '' },
         global: { stubs },
     }).find('img');
 
@@ -14,9 +15,9 @@ test('a raster thumbnail is decorative by default', () => {
     expect(img.attributes('alt')).toBe('');
 });
 
-test('an svg thumbnail is decorative by default', () => {
+test('an svg thumbnail can be decorative', () => {
     const img = mount(Thumbnail, {
-        props: { asset: { is_svg: true, url: '/assets/logo.svg' } },
+        props: { asset: { is_svg: true, url: '/assets/logo.svg' }, alt: '' },
         global: { stubs },
     }).find('img');
 
@@ -24,11 +25,23 @@ test('an svg thumbnail is decorative by default', () => {
     expect(img.attributes('alt')).toBe('');
 });
 
-test('alt can be supplied when the thumbnail has no adjacent name', () => {
-    const img = mount(Thumbnail, {
-        props: { asset: { thumbnail: '/cp/thumbnails/abc/small' }, alt: 'sample-one.png' },
+test('an asset index thumbnail names its link with the asset basename', () => {
+    const wrapper = mount(AssetsIndexFieldtype, {
+        props: {
+            handle: 'assets',
+            value: {
+                assets: [{
+                    id: 'assets::sample-one.png',
+                    basename: 'sample-one.png',
+                    thumbnail: '/cp/thumbnails/abc/small',
+                    url: '/assets/sample-one.png',
+                }],
+                total: 1,
+            },
+            values: {},
+        },
         global: { stubs },
-    }).find('img');
+    });
 
-    expect(img.attributes('alt')).toBe('sample-one.png');
+    expect(wrapper.get('a img').attributes('alt')).toBe('sample-one.png');
 });
