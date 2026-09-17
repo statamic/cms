48c63996b2610b063251f6c83ae777df9b795ad8
lazerg <lazerg2@gmail.com>
2026-09-17 11:16:43 +0500

    Bind the icon manager as a singleton

--- src/Providers/AppServiceProvider.php
+++ src/Providers/AppServiceProvider.php
@@ -23,6 +23,7 @@ use Statamic\Facades\Token;
 use Statamic\Facades\User;
 use Statamic\Fields\FieldsetRecursionStack;
 use Statamic\Http\Middleware\PingOutpost;
+use Statamic\Icons\IconManager;
 use Statamic\Jobs\HandleEntrySchedule;
 use Statamic\Licensing\Radio;
 use Statamic\Notifications\ElevatedSessionVerificationCode;
@@ -167,6 +168,8 @@ class AppServiceProvider extends ServiceProvider
         });
 
         $this->app->singleton(Sites::class);
+
+        $this->app->singleton(IconManager::class);
 
         collect([
             \Statamic\Contracts\Entries\EntryRepository::class => \Statamic\Stache\Repositories\EntryRepository::class,
---
+++ tests/Icons/IconManagerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Icons;
+
+use Illuminate\Support\Facades\Facade;
+use PHPUnit\Framework\Attributes\Test;
+use Statamic\Facades\Icon;
+use Tests\TestCase;
+
+class IconManagerTest extends TestCase
+{
+    #[Test]
+    public function it_keeps_registered_sets_when_resolved_facade_instances_are_cleared()
+    {
+        Icon::register('test', statamic_path('resources/svg/icons'));
+
+        Facade::clearResolvedInstances();
+
+        $this->assertTrue(Icon::sets()->has('test'));
+    }
+}
